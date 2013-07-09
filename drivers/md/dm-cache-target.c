27e42e507a6bf49503817031a6d6c35416c07fca
Mike Snitzer <snitzer@redhat.com>
2013-07-09 19:29:51 -0400

    [md] dm-cache: add call to mark_tech_preview
    Message-id: <1373398192-27701-1-git-send-email-snitzer@redhat.com>
    Patchwork-id: 61380
    O-Subject: [RHEL7 1/2] dm cache: add call to mark_tech_preview
    Bugzilla: 982752
    RH-Acked-by: Alasdair Kergon <agk@redhat.com>
    
    BZ: 982752
    
    Device-mapper thin provisioning is tech preview - warn user and mark
    kernel as tainted.
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/md/dm-cache-target.c
+++ drivers/md/dm-cache-target.c
@@ -2108,6 +2108,7 @@ static int cache_ctr(struct dm_target *ti, unsigned argc, char **argv)
 	int r = -EINVAL;
 	struct cache_args *ca;
 	struct cache *cache = NULL;
+	static bool seen = false;
 
 	ca = kzalloc(sizeof(*ca), GFP_KERNEL);
 	if (!ca) {
@@ -2132,6 +2133,10 @@ static int cache_ctr(struct dm_target *ti, unsigned argc, char **argv)
 
 	ti->private = cache;
 
+	if (!seen) {
+		mark_tech_preview("DM cache", THIS_MODULE);
+		seen = true;
+	}
 out:
 	destroy_cache_args(ca);
 	return r;
