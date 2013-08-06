bd5ee74aceb73ff2b2f2be959c76ed4be4edd32d
Lenny Szubowicz <lszubowi@redhat.com>
2013-08-05 20:10:11 -0400

    [pci] Lock down BAR access in secure boot environments
    Message-id: <1375733411-27419-1-git-send-email-lszubowi@redhat.com>
    Patchwork-id: 65100
    O-Subject: [RHEL 7.0 PATCH BZ 903815 V2 05/24] PCI: Lock down BAR access in secure boot environments
    Bugzilla: 903815
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=903815
    
    V2 of this patch is a direct application of Fedora 18 patch 09/19 from
    secure-boot-20130506.patch instead of using the F19/F20 version.
    
    Note that the F18 version has been updated to fix the libvirt/qemu issue
    raised in BZ 908888. This updated patch has not been propagated to either
    F19 or F20, which is why I missed it in V1 of this patch set for RHEL 7.0.
    
    With V2 of this patch, if secure boot is not enabled, this allows direct
    assignment of PCI devices to QEMU quests even if the host system does not
    have IOMMU hardware and the vfio driver on the host is not being used.
    
    If secure boot is enabled, then device assignment to a quest requires an
    IOMMU and requires use of the vfio driver.
    
    This is a RHEL-only change that was posted for upstream by Matthew:
        https://patchwork.kernel.org/patch/2056881/
        https://lkml.org/lkml/2013/1/28/400
    It was not taken due to lack of consensus about whether UEFI Secure Boot
    should also cause the kernel to block any operation that is initiated from
    user mode that might change the kernel in an unverifiable way.
    
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=6127612
    
    Subject: [PATCH 09/19] PCI: Lock down BAR access in secure boot environments
    From: Matthew Garrett <mjg@redhat.com>
    Date: Thu, 20 Sep 2012 10:40:57 -0400
    Subject: [PATCH 09/19] PCI: Lock down BAR access in secure boot environments
    
        Any hardware that can potentially generate DMA has to be locked down from
        userspace in order to avoid it being possible for an attacker to cause
        arbitrary kernel behaviour. Default to paranoid - in future we can
        potentially relax this for sufficiently IOMMU-isolated devices.
    
        Signed-off-by: Matthew Garrett <mjg@redhat.com>
    
        jwb: Temporarily work around https://bugzilla.redhat.com/show_bug.cgi?id=908888
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/pci/proc.c
+++ drivers/pci/proc.c
@@ -136,6 +136,9 @@ proc_bus_pci_write(struct file *file, const char __user *buf, size_t nbytes, lof
 	int size = dev->cfg_size;
 	int cnt;
 
+	if (!capable(CAP_COMPROMISE_KERNEL))
+		return -EPERM;
+
 	if (pos >= size)
 		return 0;
 	if (nbytes >= size)
@@ -215,6 +218,9 @@ static long proc_bus_pci_ioctl(struct file *file, unsigned int cmd,
 #endif /* HAVE_PCI_MMAP */
 	int ret = 0;
 
+	if (!capable(CAP_COMPROMISE_KERNEL))
+		return -EPERM;
+
 	switch (cmd) {
 	case PCIIOC_CONTROLLER:
 		ret = pci_domain_nr(dev->bus);
@@ -253,7 +259,7 @@ static int proc_bus_pci_mmap(struct file *file, struct vm_area_struct *vma)
 	struct pci_filp_private *fpriv = file->private_data;
 	int i, ret;
 
-	if (!capable(CAP_SYS_RAWIO))
+	if (!capable(CAP_SYS_RAWIO) || !capable(CAP_COMPROMISE_KERNEL))
 		return -EPERM;
 
 	/* Make sure the caller is mapping a real resource for this device */
