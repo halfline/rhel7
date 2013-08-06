4b5eead8f32ec27d8ed91e5dfd392f211fa5b3d6
Steve Best <sbest@redhat.com>
2013-08-06 10:05:09 -0400

    [powerpc] mm: Fix fallthrough bug in hpte_decode
    Message-id: <20130806100509.7556.84883.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 65110
    O-Subject: [PATCH RHEL7.0 BZ993326] powerpc/mm: Fix fallthrough bug in hpte_decode
    Bugzilla: 993326
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=993326
    
    Description:
    ------------
    We should not fallthrough different case statements in hpte_decode. Add
    break statement to break out of the switch. The regression is introduced by
    dcda287a9b26309ae43a091d0ecde16f8f61b4c0 "powerpc/mm: Simplify hpte_decode"
    
    Reported-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    
    RHEL Version Found:
    -------------------
    RHEL 7.0
    
    Brew:
    -----
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=6124144
    
    Upstream:
    ---------
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=83383b73ad5fbf52ea4b77aed900a927875f2529
    
    Test Status:
    ------------
    Aneesh Kumar K.V helped me test this.
    
    ---------------------------------------------------------------
    Steve Best
    IBM on-site partner
    
    Proposed Patch:
    ---------------
    --
    cgit v0.9.2
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- arch/powerpc/mm/hash_native_64.c
+++ arch/powerpc/mm/hash_native_64.c
@@ -560,6 +560,7 @@ static void hpte_decode(struct hash_pte *hpte, unsigned long slot,
 			seg_off |= vpi << shift;
 		}
 		*vpn = vsid << (SID_SHIFT - VPN_SHIFT) | seg_off >> VPN_SHIFT;
+		break;
 	case MMU_SEGSIZE_1T:
 		/* We only have 40 - 23 bits of seg_off in avpn */
 		seg_off = (avpn & 0x1ffff) << 23;
@@ -569,6 +570,7 @@ static void hpte_decode(struct hash_pte *hpte, unsigned long slot,
 			seg_off |= vpi << shift;
 		}
 		*vpn = vsid << (SID_SHIFT_1T - VPN_SHIFT) | seg_off >> VPN_SHIFT;
+		break;
 	default:
 		*vpn = size = 0;
 	}
