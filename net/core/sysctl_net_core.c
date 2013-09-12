057ba827508f853ebaad3894f16c024de07934a2
Jesper Brouer <brouer@redhat.com>
2013-09-12 19:04:07 -0400

    [net] qdisc: allow setting default queuing discipline
    Message-id: <20130912190407.7069.36392.stgit@dragon>
    Patchwork-id: 67094
    O-Subject: [RHEL7 net PATCH 7/9] qdisc: allow setting default queuing discipline
    Bugzilla: 1000395
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Thomas Graf <tgraf@redhat.com>
    RH-Acked-by: Amerigo Wang <amwang@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1000395
    
    Backport of upstream commit: 6da7c8fcbcbdb5
    
    Minor adjustments to net/core/sysctl_net_core.c, because we have not
    backported commit 99bbc7074190 (rps: selective flow shedding during
    softnet overflow) in this backport series.
    
     commit 6da7c8fcbcbdb50ec68c61b40d554c74850fdb91
     Author: stephen hemminger <stephen@networkplumber.org>
     Date:   Tue Aug 27 16:19:08 2013 -0700
    
        qdisc: allow setting default queuing discipline
    
        By default, the pfifo_fast queue discipline has been used by default
        for all devices. But we have better choices now.
    
        This patch allow setting the default queueing discipline with sysctl.
        This allows easy use of better queueing disciplines on all devices
        without having to use tc qdisc scripts. It is intended to allow
        an easy path for distributions to make fq_codel or sfq the default
        qdisc.
    
        This patch also makes pfifo_fast more of a first class qdisc, since
        it is now possible to manually override the default and explicitly
        use pfifo_fast. The behavior for systems who do not use the sysctl
        is unchanged, they still get pfifo_fast
    
        Also removes leftover random # in sysctl net core.
    
        Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
        Acked-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -20,6 +20,7 @@
 #include <net/sock.h>
 #include <net/net_ratelimit.h>
 #include <net/busy_poll.h>
+#include <net/pkt_sched.h>
 
 static int one = 1;
 
@@ -87,6 +88,26 @@ static int rps_sock_flow_sysctl(ctl_table *table, int write,
 	return ret;
 }
 #endif /* CONFIG_RPS */
+
+#ifdef CONFIG_NET_SCHED
+static int set_default_qdisc(struct ctl_table *table, int write,
+			     void __user *buffer, size_t *lenp, loff_t *ppos)
+{
+	char id[IFNAMSIZ];
+	struct ctl_table tbl = {
+		.data = id,
+		.maxlen = IFNAMSIZ,
+	};
+	int ret;
+
+	qdisc_get_default(id, IFNAMSIZ);
+
+	ret = proc_dostring(&tbl, write, buffer, lenp, ppos);
+	if (write && ret == 0)
+		ret = qdisc_set_default(id);
+	return ret;
+}
+#endif
 
 static struct ctl_table net_core_table[] = {
 #ifdef CONFIG_NET
@@ -196,7 +217,14 @@ static struct ctl_table net_core_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec
 	},
-#
+#endif
+#ifdef CONFIG_NET_SCHED
+	{
+		.procname	= "default_qdisc",
+		.mode		= 0644,
+		.maxlen		= IFNAMSIZ,
+		.proc_handler	= set_default_qdisc
+	},
 #endif
 #endif /* CONFIG_NET */
 	{
