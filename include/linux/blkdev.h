539d02d873c6c1be80c635eb1c06c395a9611a84
Don Zickus <dzickus@redhat.com>
2013-09-11 21:08:30 -0400

    [block] add padding for kabi to block_device_operations
    Message-id: <1378933710-77819-1-git-send-email-dzickus@redhat.com>
    Patchwork-id: 66998
    O-Subject: [RHEL7 PATCH v2] block: add padding for kabi to block_device_operations
    Bugzilla: 988500
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=988500
    RHEL-7 Only
    
    We have customers working on NVDIMM (persistent memory) technology that
    won't be ready until 7.1.  Based on early alpha code for their drivers it
    has been determined these are probably the kernel hooks needed later in
    RHEL-7.
    
    To limit the headaches with kabi, add stubs for now until they are used.
    
    Compiled tested by me.
    
    Cc: Jeff Moyer <jmoyer@redhat.com>
    Cc: Rik van Riel <riel@redhat.com>
    Signed-off-by: Don Zickus <dzickus@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1572,6 +1572,10 @@ struct block_device_operations {
 	int (*getgeo)(struct block_device *, struct hd_geometry *);
 	/* this callback is with swap_lock and sometimes page table lock held */
 	void (*swap_slot_free_notify) (struct block_device *, unsigned long);
+
+	/* future placeholders for nvdimm */
+	void *rh_reserved_ptrs[5];
+
 	struct module *owner;
 };
 
