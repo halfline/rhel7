1d269d044ecaefff0b05cde2575012bca52290a7
Veaceslav Falico <vfalico@redhat.com>
2013-10-10 14:03:45 -0400

    [fs] fuse: invalidate inode attributes on xattr modification
    Message-id: <1381413926-15908-720-git-send-email-vfalico@redhat.com>
    Patchwork-id: 69349
    O-Subject: [RHEL7 PATCH 719/820] fuse: invalidate inode attributes on xattr modification
    Bugzilla: 1006938
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Brian Foster <bfoster@redhat.com>
    
    RHBZ: 1006938
    Upstream: d331a415aef98717393dda0be69b7947da08eba3
    https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=d331a415aef98717393dda0be69b7947da08eba3
    Stable: eb97a45d12c9517b7846ebe3c97ee554b777ad34
    http://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?id=eb97a45d12c9517b7846ebe3c97ee554b777ad34
    
    Description
    ----------------------------
    Backport from linux-3.10.y stable.
    
    From: Anand Avati <avati@redhat.com>
    Subject: fuse: invalidate inode attributes on xattr modification
    
    commit d331a415aef98717393dda0be69b7947da08eba3 upstream.
    
    Calls like setxattr and removexattr result in updation of ctime.
    Therefore invalidate inode attributes to force a refresh.
    
    Signed-off-by: Anand Avati <avati@redhat.com>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit eb97a45d12c9517b7846ebe3c97ee554b777ad34)
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1752,6 +1752,8 @@ static int fuse_setxattr(struct dentry *entry, const char *name,
 		fc->no_setxattr = 1;
 		err = -EOPNOTSUPP;
 	}
+	if (!err)
+		fuse_invalidate_attr(inode);
 	return err;
 }
 
@@ -1881,6 +1883,8 @@ static int fuse_removexattr(struct dentry *entry, const char *name)
 		fc->no_removexattr = 1;
 		err = -EOPNOTSUPP;
 	}
+	if (!err)
+		fuse_invalidate_attr(inode);
 	return err;
 }
 
