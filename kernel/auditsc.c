4fef1f19e367aee241b86dda7ba78c0db89c4aef
Richard Guy Briggs <rgb@redhat.com>
2013-11-03 03:01:16 -0500

    [kernel] audit: remove unused envc member of audit_aux_data_execve
    Message-id: <38ddb8db98f57be42ac697f64fa7b0f817049a6b.1383445982.git.rgb@redhat.com>
    Patchwork-id: 70469
    O-Subject: [RHEL7 PATCH 2/4] audit: remove unused envc member of audit_aux_data_execve
    Bugzilla: 1010367
    RH-Acked-by: Eric Paris <eparis@redhat.com>
    RH-Acked-by: Oleg Nesterov <oleg@redhat.com>
    
    Get rid of write-only audit_aux_data_exeve structure member envc.
    
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>
    (cherry picked from rgb's audit-for-next commit f6442bfd234bb8a21836e060c5af3b082bb9da3a)
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -98,7 +98,6 @@ struct audit_aux_data {
 struct audit_aux_data_execve {
 	struct audit_aux_data	d;
 	int argc;
-	int envc;
 	struct mm_struct *mm;
 };
 
@@ -2132,7 +2131,6 @@ int __audit_bprm(struct linux_binprm *bprm)
 		return -ENOMEM;
 
 	ax->argc = bprm->argc;
-	ax->envc = bprm->envc;
 	ax->mm = bprm->mm;
 	ax->d.type = AUDIT_EXECVE;
 	ax->d.next = context->aux;
