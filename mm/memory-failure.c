87f46c89aeb64174d3fbae1d46805d294b343dba
Naoya Horiguchi <nhoriguc@redhat.com>
2014-01-12 16:22:59 -0500

    [mm] hwpoison: always unset MIGRATE_ISOLATE before returning from soft_offline_page()
    Message-id: <1389543792-24529-3-git-send-email-nhoriguc@redhat.com>
    Patchwork-id: 73966
    O-Subject: [RHEL7 PATCH 02/15] hwpoison: always unset MIGRATE_ISOLATE before returning from soft_offline_page()
    Bugzilla: 1051704
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1051704
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=6846791
    Test: NEC confirmed that this patch fixes the reported problem.
    
    Backporting the following commit.
    
      commit 0d6fdbdb2a651f0c9bb979e1d92b1e15dadffc4f
      Author: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
      Date:   Wed Sep 11 14:22:49 2013 -0700
    
        hwpoison: always unset MIGRATE_ISOLATE before returning from soft_offline_page()
    
        Soft offline code expects that MIGRATE_ISOLATE is set on the target page
        only during soft offlining work.  But currenly it doesn't work as expected
        when get_any_page() fails and returns negative value.  In the result, end
        users can have unexpectedly isolated pages.  This patch just fixes it.
    
        Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
        Reviewed-by: Wanpeng Li <liwanp@linux.vnet.ibm.com>
        Cc: Andi Kleen <andi@firstfloor.org>
        Cc: Fengguang Wu <fengguang.wu@intel.com>
        Cc: Tony Luck <tony.luck@intel.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Naoya Horiguchi <nhoriguc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1544,7 +1544,7 @@ int soft_offline_page(struct page *page, int flags)
 
 	ret = get_any_page(page, pfn, flags);
 	if (ret < 0)
-		return ret;
+		goto unset;
 	if (ret) { /* for in-use pages */
 		if (PageHuge(page))
 			ret = soft_offline_huge_page(page, flags);
@@ -1561,6 +1561,7 @@ int soft_offline_page(struct page *page, int flags)
 			atomic_long_inc(&num_poisoned_pages);
 		}
 	}
+unset:
 	unset_migratetype_isolate(page, MIGRATE_MOVABLE);
 	return ret;
 }
