22504b6ec008dd5b97992e9db1ffac636b95dce6
Rob Evers <revers@redhat.com>
2014-01-15 19:20:42 -0500

    [scsi] lpfc: Fix wrong assignment in lpfc_debugfs.c
    Message-id: <1389813649-31388-3-git-send-email-revers@redhat.com>
    Patchwork-id: 74245
    O-Subject: [RHEL7.0 e-stor PATCH 2/9] scsi: lpfc: Fix wrong assignment in lpfc_debugfs.c
    Bugzilla: 1025854
    RH-Acked-by: Tomas Henzl <thenzl@redhat.com>
    RH-Acked-by: Maurizio Lombardi <mlombard@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    From: Felipe Pena <felipensp@gmail.com>
    
    On lpfc_debugfs_initialize function the dumpHostSlim member setup happens
    when 'phba->sli_rev < LPFC_SLI_REV4' is true, however when it is false NULL
    has been assigned to debug_dumpHBASlim instead of debug_dumpHostSlim.
    
    Signed-off-by: Felipe Pena <felipensp@gmail.com>
    Acked-by: James Smart <james.smart@emulex.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>
    (cherry picked from commit 2573a9a4751e38eaf1aed2a9720f8dbb0cc685d0)
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/scsi/lpfc/lpfc_debugfs.c
+++ drivers/scsi/lpfc/lpfc_debugfs.c
@@ -4015,7 +4015,7 @@ lpfc_debugfs_initialize(struct lpfc_vport *vport)
 				goto debug_failed;
 			}
 		} else
-			phba->debug_dumpHBASlim = NULL;
+			phba->debug_dumpHostSlim = NULL;
 
 		/* Setup dumpData */
 		snprintf(name, sizeof(name), "dumpData");
