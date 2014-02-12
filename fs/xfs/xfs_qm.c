268d5ca826b5d9c45d0b9cac137c024df3a5422e
Brian Foster <bfoster@redhat.com>
2014-02-12 00:19:35 -0500

    [fs] xfs: split dquot buffer operations out
    Message-id: <1392164463-36939-29-git-send-email-bfoster@redhat.com>
    Patchwork-id: 76182
    O-Subject: [RHEL7 PATCH 028/116] xfs: split dquot buffer operations out
    Bugzilla: 1059843
    RH-Acked-by: Dave Chinner <dchinner@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    commit 9aede1d81b12a95771db0e2f9196dd37e10aaf20
    Author: Dave Chinner <dchinner@redhat.com>
    Date:   Tue Oct 15 09:17:52 2013 +1100
    
        xfs: split dquot buffer operations out
    
        Parts of userspace want to be able to read and modify dquot buffers
        (e.g. xfs_db) so we need to split out the reading and writing of
        these buffers so it is easy to shared code with libxfs in userspace.
    
        Signed-off-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Ben Myers <bpm@sgi.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/xfs/xfs_qm.c
+++ fs/xfs/xfs_qm.c
@@ -667,20 +667,6 @@ xfs_qm_dqdetach(
 	}
 }
 
-int
-xfs_qm_calc_dquots_per_chunk(
-	struct xfs_mount	*mp,
-	unsigned int		nbblks)	/* basic block units */
-{
-	unsigned int	ndquots;
-
-	ASSERT(nbblks > 0);
-	ndquots = BBTOB(nbblks);
-	do_div(ndquots, sizeof(xfs_dqblk_t));
-
-	return ndquots;
-}
-
 /*
  * This initializes all the quota information that's kept in the
  * mount structure
@@ -721,7 +707,7 @@ xfs_qm_init_quotainfo(
 
 	/* Precalc some constants */
 	qinf->qi_dqchunklen = XFS_FSB_TO_BB(mp, XFS_DQUOT_CLUSTER_SIZE_FSB);
-	qinf->qi_dqperchunk = xfs_qm_calc_dquots_per_chunk(mp,
+	qinf->qi_dqperchunk = xfs_calc_dquots_per_chunk(mp,
 							qinf->qi_dqchunklen);
 
 	mp->m_qflags |= (mp->m_sb.sb_qflags & XFS_ALL_QUOTA_CHKD);
@@ -952,10 +938,10 @@ xfs_qm_reset_dqcounts(
 		/*
 		 * Do a sanity check, and if needed, repair the dqblk. Don't
 		 * output any warnings because it's perfectly possible to
-		 * find uninitialised dquot blks. See comment in xfs_qm_dqcheck.
+		 * find uninitialised dquot blks. See comment in xfs_dqcheck.
 		 */
-		(void) xfs_qm_dqcheck(mp, ddq, id+j, type, XFS_QMOPT_DQREPAIR,
-				      "xfs_quotacheck");
+		xfs_dqcheck(mp, ddq, id+j, type, XFS_QMOPT_DQREPAIR,
+			    "xfs_quotacheck");
 		ddq->d_bcount = 0;
 		ddq->d_icount = 0;
 		ddq->d_rtbcount = 0;
