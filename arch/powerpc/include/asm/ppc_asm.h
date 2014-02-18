d57bfb6ccb2e8d610c225c2e21c5a4191f526c2f
Steve Best <sbest@redhat.com>
2014-02-17 20:02:38 -0500

    [powerpc] Little endian builds double word swap VSX state during context save/restore
    Message-id: <20140217200238.40980.10565.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 76972
    O-Subject: [PATCH RHEL7.0 BZ1051192 32/97 v2] powerpc: Little endian builds double word swap VSX state during context save/restore
    Bugzilla: 1051192
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1051192
    
    Description:
    ------------
    The elements within VSX loads and stores are big endian ordered
    regardless of endianness. Our VSX context save/restore code uses
    lxvd2x and stxvd2x which is a 2x doubleword operation. This means
    the two doublewords will be swapped and we have to perform another
    swap to undo it.
    
    We need to do this on save and restore.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    
    v2- minor line changes to get this to apply and also
    THREAD_VSR0 is no longer in tree it was removed by bz 1042526 which came into kernel 85.
    PATCH RHEL7.0 BZ1042526 5/9] powerpc: Put FP/VSX and VR state into structures
    so do the fix ups for this change.
    
    RHEL Version Found:
    -------------------
    7.0
    
    Brew:
    -----
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=7067926
    
    Upstream Status:
    ----------------
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=926f160f460170b0361f600f365369685ad74009
    
    ----------------
    Steve Best
    IBM on-site partner
    
    Proposed Patch:
    ---------------
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- arch/powerpc/include/asm/ppc_asm.h
+++ arch/powerpc/include/asm/ppc_asm.h
@@ -124,27 +124,39 @@ END_FW_FTR_SECTION_IFSET(FW_FEATURE_SPLPAR)
 #define REST_16VRS(n,b,base)	REST_8VRS(n,b,base); REST_8VRS(n+8,b,base)
 #define REST_32VRS(n,b,base)	REST_16VRS(n,b,base); REST_16VRS(n+16,b,base)
 
+#ifdef __BIG_ENDIAN__
+#define STXVD2X_ROT(n,b,base)		STXVD2X(n,b,base)
+#define LXVD2X_ROT(n,b,base)		LXVD2X(n,b,base)
+#else
+#define STXVD2X_ROT(n,b,base)		XXSWAPD(n,n);		\
+					STXVD2X(n,b,base);	\
+					XXSWAPD(n,n)
+
+#define LXVD2X_ROT(n,b,base)		LXVD2X(n,b,base);	\
+					XXSWAPD(n,n)
+#endif
+
 /* Save the lower 32 VSRs in the thread VSR region */
-#define SAVE_VSR(n,b,base)	li b,16*(n);  STXVD2X(n,R##base,R##b)
+#define SAVE_VSR(n,b,base)	li b,16*(n);  STXVD2X_ROT(n,R##base,R##b)
 #define SAVE_2VSRS(n,b,base)	SAVE_VSR(n,b,base); SAVE_VSR(n+1,b,base)
 #define SAVE_4VSRS(n,b,base)	SAVE_2VSRS(n,b,base); SAVE_2VSRS(n+2,b,base)
 #define SAVE_8VSRS(n,b,base)	SAVE_4VSRS(n,b,base); SAVE_4VSRS(n+4,b,base)
 #define SAVE_16VSRS(n,b,base)	SAVE_8VSRS(n,b,base); SAVE_8VSRS(n+8,b,base)
 #define SAVE_32VSRS(n,b,base)	SAVE_16VSRS(n,b,base); SAVE_16VSRS(n+16,b,base)
-#define REST_VSR(n,b,base)	li b,16*(n); LXVD2X(n,R##base,R##b)
+#define REST_VSR(n,b,base)	li b,16*(n); LXVD2X_ROT(n,R##base,R##b)
 #define REST_2VSRS(n,b,base)	REST_VSR(n,b,base); REST_VSR(n+1,b,base)
 #define REST_4VSRS(n,b,base)	REST_2VSRS(n,b,base); REST_2VSRS(n+2,b,base)
 #define REST_8VSRS(n,b,base)	REST_4VSRS(n,b,base); REST_4VSRS(n+4,b,base)
 #define REST_16VSRS(n,b,base)	REST_8VSRS(n,b,base); REST_8VSRS(n+8,b,base)
 #define REST_32VSRS(n,b,base)	REST_16VSRS(n,b,base); REST_16VSRS(n+16,b,base)
 /* Save the upper 32 VSRs (32-63) in the thread VSX region (0-31) */
-#define SAVE_VSRU(n,b,base)	li b,THREAD_VR0+(16*(n));  STXVD2X(n+32,R##base,R##b)
+#define SAVE_VSRU(n,b,base)	li b,16*(n);  STXVD2X(n+32,R##base,R##b)
 #define SAVE_2VSRSU(n,b,base)	SAVE_VSRU(n,b,base); SAVE_VSRU(n+1,b,base)
 #define SAVE_4VSRSU(n,b,base)	SAVE_2VSRSU(n,b,base); SAVE_2VSRSU(n+2,b,base)
 #define SAVE_8VSRSU(n,b,base)	SAVE_4VSRSU(n,b,base); SAVE_4VSRSU(n+4,b,base)
 #define SAVE_16VSRSU(n,b,base)	SAVE_8VSRSU(n,b,base); SAVE_8VSRSU(n+8,b,base)
 #define SAVE_32VSRSU(n,b,base)	SAVE_16VSRSU(n,b,base); SAVE_16VSRSU(n+16,b,base)
-#define REST_VSRU(n,b,base)	li b,THREAD_VR0+(16*(n)); LXVD2X(n+32,R##base,R##b)
+#define REST_VSRU(n,b,base)	li b,16*(n); LXVD2X(n+32,R##base,R##b)
 #define REST_2VSRSU(n,b,base)	REST_VSRU(n,b,base); REST_VSRU(n+1,b,base)
 #define REST_4VSRSU(n,b,base)	REST_2VSRSU(n,b,base); REST_2VSRSU(n+2,b,base)
 #define REST_8VSRSU(n,b,base)	REST_4VSRSU(n,b,base); REST_4VSRSU(n+4,b,base)
