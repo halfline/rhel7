e947162cadbb8a3c90fce1270006c646d0456609
Mike Snitzer <snitzer@redhat.com>
2014-03-07 21:07:35 -0500

    [md] dm-thin: synchronize the pool mode during suspend
    Message-id: <1394226467-8194-6-git-send-email-snitzer@redhat.com>
    Patchwork-id: 78032
    O-Subject: [RHEL7 PATCH 05/17] dm thin: synchronize the pool mode during suspend
    Bugzilla: 1065051
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    
    BZ: 1065051
    
    Upstream commit cdc2b4158405f1975f9d5205096f08430eda1c0e
    Author: Mike Snitzer <snitzer@redhat.com>
    Date:   Fri Feb 14 18:10:55 2014 -0500
    
        dm thin: synchronize the pool mode during suspend
    
        Commit b5330655 ("dm thin: handle metadata failures more consistently")
        increased potential for the pool's mode to be changed in response to
        metadata operation failures.
    
        When the pool mode is changed it isn't synchronized with the mode in
        pool_features stored in the target's context (ti->private) that is used
        as the basis for (re)establishing the pool mode during resume via
        bind_control_target.
    
        It is important that we synchronize the pool mode when it is changed
        otherwise the pool may experience and unexpected mode transition on the
        next resume (especially if there was no new table load).
    
        Signed-off-by: Mike Snitzer <snitzer@redhat.com>
        Acked-by: Joe Thornber <ejt@redhat.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/md/dm-thin.c
+++ drivers/md/dm-thin.c
@@ -1396,6 +1396,7 @@ static enum pool_mode get_pool_mode(struct pool *pool)
 static void set_pool_mode(struct pool *pool, enum pool_mode new_mode)
 {
 	int r;
+	struct pool_c *pt = pool->ti->private;
 	enum pool_mode old_mode = pool->pf.mode;
 
 	switch (new_mode) {
@@ -1442,6 +1443,11 @@ static void set_pool_mode(struct pool *pool, enum pool_mode new_mode)
 	}
 
 	pool->pf.mode = new_mode;
+	/*
+	 * The pool mode may have changed, sync it so bind_control_target()
+	 * doesn't cause an unexpected mode transition on resume.
+	 */
+	pt->adjusted_pf.mode = new_mode;
 }
 
 /*
