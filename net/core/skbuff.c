a755f600474aece3705b050efe8e90010b87a5fd
Michael S. Tsirkin <mst@redhat.com>
2014-03-18 15:04:14 -0400

    [net] skbuff: skb_segment: orphan frags before copying
    Message-id: <1394571172-11849-6-git-send-email-mst@redhat.com>
    Patchwork-id: 78377
    O-Subject: [PATCH RHEL7.0 v3 6/6] skbuff: skb_segment: orphan frags before copying
    Bugzilla: 1056934
    CVE: CVE-2014-0131
    RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    RH-Acked-by: Jason Wang <jasowang@redhat.com>
    RH-Acked-by: Herbert Xu <herbert.xu@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1056934
    Brew build: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=7177079
    
    commit 1fd819ecb90cc9b822cd84d3056ddba315d3340f
    Author: Michael S. Tsirkin <mst@redhat.com>
    Date:   Mon Mar 10 19:28:08 2014 +0200
    
        skbuff: skb_segment: orphan frags before copying
    
        skb_segment copies frags around, so we need
        to copy them carefully to avoid accessing
        user memory after reporting completion to userspace
        through a callback.
    
        skb_segment doesn't normally happen on datapath:
        TSO needs to be disabled - so disabling zero copy
        in this case does not look like a big deal.
    
        Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
        Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -2835,6 +2835,7 @@ struct sk_buff *skb_segment(struct sk_buff *head_skb,
 	skb_frag_t *frag = skb_shinfo(head_skb)->frags;
 	unsigned int mss = skb_shinfo(head_skb)->gso_size;
 	unsigned int doffset = head_skb->data - skb_mac_header(head_skb);
+	struct sk_buff *frag_skb = head_skb;
 	unsigned int offset = doffset;
 	unsigned int tnl_hlen = skb_tnl_header_len(head_skb);
 	unsigned int headroom;
@@ -2879,6 +2880,7 @@ struct sk_buff *skb_segment(struct sk_buff *head_skb,
 			i = 0;
 			nfrags = skb_shinfo(list_skb)->nr_frags;
 			frag = skb_shinfo(list_skb)->frags;
+			frag_skb = list_skb;
 			pos += skb_headlen(list_skb);
 
 			while (pos < offset + len) {
@@ -2966,6 +2968,7 @@ struct sk_buff *skb_segment(struct sk_buff *head_skb,
 				i = 0;
 				nfrags = skb_shinfo(list_skb)->nr_frags;
 				frag = skb_shinfo(list_skb)->frags;
+				frag_skb = list_skb;
 
 				BUG_ON(!nfrags);
 
@@ -2979,6 +2982,9 @@ struct sk_buff *skb_segment(struct sk_buff *head_skb,
 					pos, mss);
 				goto err;
 			}
+
+			if (unlikely(skb_orphan_frags(frag_skb, GFP_ATOMIC)))
+				goto err;
 
 			*nskb_frag = *frag;
 			__skb_frag_ref(nskb_frag);
