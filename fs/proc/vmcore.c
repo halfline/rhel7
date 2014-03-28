halfline/rhel7

commit c8fcd76e779aea0fd29b58c22de5d5f365eec5c3

[fs] proc/vmcore: prevent PT_NOTE p_memsz overflow during header update
Message-id: <1393453392-13338-1-git-send-email-ncroxon@redhat.com>
Patchwork-id: 77570
O-Subject: [RHEL7.0 PATCH] vmcore: prevent PT_NOTE p_memsz overflow during header update
Bugzilla: 1061368
RH-Acked-by: Vivek Goyal <vgoyal@redhat.com>
RH-Acked-by: Baoquan He <bhe@redhat.com>

Bugzilla: 1061368
https://bugzilla.redhat.com/show_bug.cgi?id=1061368

Brew build: 7113052
https://brewweb.devel.redhat.com/taskinfo?taskID=7113052

Cherry-picked with no merge conflicts.

Tested on HP hardware by Greg Pearson. Greg caused a crash
and was able to see the new message. Which means the patch
allowed crash dump to work even though corrupted records
were encountered.

Upstream commit IDs:
38dfac843cb6d7be1874888839817404a15a6b3c

Nigel Croxon
HP on-site partner

>From 38dfac843cb6d7be1874888839817404a15a6b3c Mon Sep 17 00:00:00 2001
From: Greg Pearson <greg.pearson@hp.com>
Date: Mon, 10 Feb 2014 14:25:36 -0800
Subject: [RHEL7.0 PATCH 1/1] vmcore: prevent PT_NOTE p_memsz overflow during
 header update

Currently, update_note_header_size_elf64() and
update_note_header_size_elf32() will add the size of a PT_NOTE entry to
real_sz even if that causes real_sz to exceeds max_sz.  This patch
corrects the while loop logic in those routines to ensure that does not
happen and prints a warning if a PT_NOTE entry is dropped.  If zero
PT_NOTE entries are found or this condition is encountered because the
only entry was dropped, a warning is printed and an error is returned.

One possible negative side effect of exceeding the max_sz limit is an
allocation failure in merge_note_headers_elf64() or
merge_note_headers_elf32() which would produce console output such as
the following while booting the crash kernel.

  vmalloc: allocation failure: 14076997632 bytes
  swapper/0: page allocation failure: order:0, mode:0x80d2
  CPU: 0 PID: 1 Comm: swapper/0 Not tainted 3.10.0-gbp1 #7
  Call Trace:
    dump_stack+0x19/0x1b

fs/proc/vmcore.c
@@ -468,17 +468,24 @@ static int __init update_note_header_size_elf64(const Elf64_Ehdr *ehdr_ptr)
 			return rc;
 		}
 		nhdr_ptr = notes_section;
-		while (real_sz < max_sz) {
-			if (nhdr_ptr->n_namesz == 0)
-				break;
+		while (nhdr_ptr->n_namesz != 0) {
 			sz = sizeof(Elf64_Nhdr) +
 				((nhdr_ptr->n_namesz + 3) & ~3) +
 				((nhdr_ptr->n_descsz + 3) & ~3);
+			if ((real_sz + sz) > max_sz) {
+				pr_warn("Warning: Exceeded p_memsz, dropping PT_NOTE entry n_namesz=0x%x, n_descsz=0x%x\n",
+					nhdr_ptr->n_namesz, nhdr_ptr->n_descsz);
+				break;
+			}
 			real_sz += sz;
 			nhdr_ptr = (Elf64_Nhdr*)((char*)nhdr_ptr + sz);
 		}
 		kfree(notes_section);
 		phdr_ptr->p_memsz = real_sz;
+		if (real_sz == 0) {
+			pr_warn("Warning: Zero PT_NOTE entries found\n");
+			return -EINVAL;
+		}
 	}
 
 	return 0;
@@ -648,17 +655,24 @@ static int __init update_note_header_size_elf32(const Elf32_Ehdr *ehdr_ptr)
 			return rc;
 		}
 		nhdr_ptr = notes_section;
-		while (real_sz < max_sz) {
-			if (nhdr_ptr->n_namesz == 0)
-				break;
+		while (nhdr_ptr->n_namesz != 0) {
 			sz = sizeof(Elf32_Nhdr) +
 				((nhdr_ptr->n_namesz + 3) & ~3) +
 				((nhdr_ptr->n_descsz + 3) & ~3);
+			if ((real_sz + sz) > max_sz) {
+				pr_warn("Warning: Exceeded p_memsz, dropping PT_NOTE entry n_namesz=0x%x, n_descsz=0x%x\n",
+					nhdr_ptr->n_namesz, nhdr_ptr->n_descsz);
+				break;
+			}
 			real_sz += sz;
 			nhdr_ptr = (Elf32_Nhdr*)((char*)nhdr_ptr + sz);
 		}
 		kfree(notes_section);
 		phdr_ptr->p_memsz = real_sz;
+		if (real_sz == 0) {
+			pr_warn("Warning: Zero PT_NOTE entries found\n");
+			return -EINVAL;
+		}
 	}
 
 	return 0;

commit fa9b6c3bcaa536bfc37c7845688b0ee3cedf4bb9

[fs] proc/vmcore: continue vmcore initialization if PT_NOTE is found empty
Message-id: <1395668072-13024-1-git-send-email-chaowang@redhat.com>
Patchwork-id: 78530
O-Subject: [RHEL7.0 PATCH] vmcore: continue vmcore initialization if PT_NOTE is found empty
Bugzilla: 1077535
RH-Acked-by: Dave Young <dyoung@redhat.com>
RH-Acked-by: Vivek Goyal <vgoyal@redhat.com>

BZ: bz1077535
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=7242824

Upstream:
This patch is queued in akpm's mmotm tree for linux-next.
http://ozlabs.org/~akpm/mmotm/broken-out/vmcore-continue-vmcore-initialization-if-pt_note-is-found-empty.patch

    From: WANG Chao <chaowang@redhat.com>
    Subject: vmcore: continue vmcore initialization if PT_NOTE is found empty

    vmcore: continue vmcore initialization if PT_NOTE is found empty

    Currently when an empty PT_NOTE is detected, vmcore initialization
    fails. It sounds too harsh. Because PT_NOTE could be empty, for example,
    one offlined a cpu but never restarted kdump service, and after crash,
    PT_NOTE program header is there but no data contains. It's better to
    warn about the empty PT_NOTE and continue to initialise vmcore.

    And ultimately the multiple PT_NOTE are merged into a single one, all
    empty PT_NOTE are discarded naturally during the merge. So empty PT_NOTE
    is not visible to user space and vmcore is as good as expected.

    Signed-off-by: WANG Chao <chaowang@redhat.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: HATAYAMA Daisuke <d.hatayama@jp.fujitsu.com>
    Cc: Greg Pearson <greg.pearson@hp.com>
    Cc: Baoquan He <bhe@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

On IBM Power p730, after crash, several PT_NOTE are empty and vmcore
refuse to initialize:

[    0.649103] Warning: Zero PT_NOTE entries found
[    0.649103] Warning: Zero PT_NOTE entries found
[    0.649134] Kdump: vmcore not initialized
[    0.649134] Kdump: vmcore not initialized

This is a regression introduced in commit c8fcd76

fs/proc/vmcore.c
@@ -484,7 +484,6 @@ static int __init update_note_header_size_elf64(const Elf64_Ehdr *ehdr_ptr)
 		phdr_ptr->p_memsz = real_sz;
 		if (real_sz == 0) {
 			pr_warn("Warning: Zero PT_NOTE entries found\n");
-			return -EINVAL;
 		}
 	}
 
@@ -671,7 +670,6 @@ static int __init update_note_header_size_elf32(const Elf32_Ehdr *ehdr_ptr)
 		phdr_ptr->p_memsz = real_sz;
 		if (real_sz == 0) {
 			pr_warn("Warning: Zero PT_NOTE entries found\n");
-			return -EINVAL;
 		}
 	}
 