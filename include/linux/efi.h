9aca7ca5d9f8645f904f05a7e8c53b87bc3ea1a5
Dave Young <dyoung@redhat.com>
2014-05-22 02:43:06 -0400

    [firmware] efi: Runtime services virtual mapping
    Message-id: <1400726607-13717-15-git-send-email-dyoung@redhat.com>
    Patchwork-id: 80115
    O-Subject: [RHEL7.1 PATCH 14/35] EFI: Runtime services virtual mapping
    Bugzilla: 1080109
    RH-Acked-by: Chao WANG <chaowang@redhat.com>
    RH-Acked-by: Vivek Goyal <vgoyal@redhat.com>
    
    Resolves: bz1080109
    
    Brew build:
    https://brewweb.devel.redhat.com/taskinfo?taskID=7480539
    
    Test: Tested kexec/kdump on HP workstation and qemu.
    
    Difference from upstream patch:
    efi.h: macro EFI_ARCH_1 is 7 instead of 6 because extra EFI_SECURE_BOOT
    
    Upstream commit:
    commit d2f7cbe7b26a74dbbbf8f325b2a6fd01bc34032c
    Author: Borislav Petkov <bp@suse.de>
    Date:   Thu Oct 31 17:25:08 2013 +0100
    
        x86/efi: Runtime services virtual mapping
    
        We map the EFI regions needed for runtime services non-contiguously,
        with preserved alignment on virtual addresses starting from -4G down
        for a total max space of 64G. This way, we provide for stable runtime
        services addresses across kernels so that a kexec'd kernel can still use
        them.
    
        Thus, they're mapped in a separate pagetable so that we don't pollute
        the kernel namespace.
    
        Add an efi= kernel command line parameter for passing miscellaneous
        options and chicken bits from the command line.
    
        While at it, add a chicken bit called "efi=old_map" which can be used as
        a fallback to the old runtime services mapping method in case there's
        some b0rkage with a particular EFI implementation (haha, it is hard to
        hold up the sarcasm here...).
    
        Also, add the UEFI RT VA space to Documentation/x86/x86_64/mm.txt.
    
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Signed-off-by: Matt Fleming <matt.fleming@intel.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -684,6 +684,7 @@ extern int __init efi_setup_pcdp_console(char *);
 #define EFI_MEMMAP		4	/* Can we use EFI memory map? */
 #define EFI_64BIT		5	/* Is the firmware 64-bit? */
 #define EFI_SECURE_BOOT		6	/* Area we in Secure Boot mode? */
+#define EFI_ARCH_1		7	/* First arch-specific bit */
 
 #ifdef CONFIG_EFI
 # ifdef CONFIG_X86
