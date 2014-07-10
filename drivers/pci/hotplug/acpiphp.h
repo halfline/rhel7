275c4173fba3413b69d48304ec2b9d01e96c6258
Myron Stowe <myron.stowe@redhat.com>
2014-07-10 01:00:20 -0400

    [acpi] dock/pci: Drop ACPI dock notifier chain
    Message-id: <20140710010020.22146.17958.stgit@gir.stowe>
    Patchwork-id: 85559
    O-Subject: [RHEL7.1 PATCH 020/105] ACPI / dock / PCI: Drop ACPI dock notifier chain
    Bugzilla: 1114228
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1114228
    Upstream Status: f09ce741a03ad7de591aa47e760fbeee28567b63
    
    commit f09ce741a03ad7de591aa47e760fbeee28567b63
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Fri Jul 5 03:03:25 2013 +0200
    
        ACPI / dock / PCI: Drop ACPI dock notifier chain
    
        The only user of the ACPI dock notifier chain is the ACPI-based PCI
        hotplug (acpiphp) driver that uses it to carry out post-dock fixups
        needed by some systems with broken _DCK.  However, it is not
        necessary to use a separate notifier chain for that, as it can be
        simply replaced with a new callback in struct acpi_dock_ops.
    
        For this reason, add a new .fixup() callback to struct acpi_dock_ops
        and make hotplug_dock_devices() execute it for all dock devices with
        hotplug operations registered.  Accordingly, make acpiphp point that
        callback to the function carrying out the post-dock fixups and
        do not register a separate dock notifier for each device
        registering dock operations.  Finally, drop the ACPI dock notifier
        chain that has no more users.
    
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/pci/hotplug/acpiphp.h
+++ drivers/pci/hotplug/acpiphp.h
@@ -112,7 +112,6 @@ struct acpiphp_func {
 	struct acpiphp_slot *slot;	/* parent */
 
 	struct list_head sibling;
-	struct notifier_block nb;
 	acpi_handle	handle;
 
 	u8		function;	/* pci function# */
