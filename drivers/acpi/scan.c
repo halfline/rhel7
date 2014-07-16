torvalds/linux

commit 6931007cc90ba94b3c2b29179d0a7cde194dabe8

ACPI / scan: Start matching drivers after trying scan handlers
ACPI scan handlers should always be attached to struct acpi_device
objects before any ACPI drivers, but there is a window during which
a driver may be attached to a struct acpi_device before checking if
there is a matching scan handler.  Namely, that will happen if an
ACPI driver module is loaded during acpi_bus_scan() right after
the first namespace walk is complete and before the given device
is processed by the second namespace walk.

To prevent that from happening, set the match_driver flags of
struct acpi_device objects right before running device_attach()
for them in acpi_bus_device_attach().

Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Acked-by: Toshi Kani <toshi.kani@hp.com>

drivers/acpi/scan.c
@@ -1677,7 +1677,6 @@ void acpi_init_device_object(struct acpi_device *device, acpi_handle handle,
 
 void acpi_device_add_finalize(struct acpi_device *device)
 {
-	device->flags.match_driver = true;
 	dev_set_uevent_suppress(&device->dev, false);
 	kobject_uevent(&device->dev.kobj, KOBJ_ADD);
 }
@@ -1916,8 +1915,12 @@ static acpi_status acpi_bus_device_attach(acpi_handle handle, u32 lvl_not_used,
 		return AE_OK;
 
 	ret = acpi_scan_attach_handler(device);
-	if (ret)
-		return ret > 0 ? AE_OK : AE_CTRL_DEPTH;
+	if (ret < 0)
+		return AE_CTRL_DEPTH;
+
+	device->flags.match_driver = true;
+	if (ret > 0)
+		return AE_OK;
 
 	ret = device_attach(&device->dev);
 	return ret >= 0 ? AE_OK : AE_CTRL_DEPTH;

halfline/rhel7

commit cc3d1c727d5b6444dfe14d10a1e18783612ba578

[acpi] scan: Set flags.match_driver in acpi_bus_scan_fixed()
Message-id: <20140710010904.22146.33345.stgit@gir.stowe>
Patchwork-id: 85642
O-Subject: [RHEL7.1 PATCH 103/105] ACPI / scan: Set flags.match_driver in acpi_bus_scan_fixed()
Bugzilla: 1114228
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1114228
Upstream Status: 883461679ad1ed6e09135adf8c02bb98d7962623

commit 883461679ad1ed6e09135adf8c02bb98d7962623
Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Date:   Mon Nov 18 14:18:47 2013 +0100

    ACPI / scan: Set flags.match_driver in acpi_bus_scan_fixed()

    Before commit 6931007cc90b (ACPI / scan: Start matching drivers
    after trying scan handlers) the match_driver flag for all devices
    was set in acpi_add_single_object(), but now it is set by
    acpi_bus_device_attach() which is not called for the "fixed"
    devices added by acpi_bus_scan_fixed().  This means that
    flags.match_driver is never set for those devices now, so make
    acpi_bus_scan_fixed() set it before calling device_attach().

    Fixes: 6931007cc90b (ACPI / scan: Start matching drivers after trying scan h
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
---

Signed-off-by: Jarod Wilson <jarod@redhat.com>

drivers/acpi/scan.c
@@ -1976,6 +1976,7 @@ static int acpi_bus_scan_fixed(void)
 		if (result)
 			return result;
 
+		device->flags.match_driver = true;
 		result = device_attach(&device->dev);
 		if (result < 0)
 			return result;
@@ -1992,6 +1993,7 @@ static int acpi_bus_scan_fixed(void)
 		if (result)
 			return result;
 
+		device->flags.match_driver = true;
 		result = device_attach(&device->dev);
 	}
 