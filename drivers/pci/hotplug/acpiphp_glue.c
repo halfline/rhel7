torvalds/linux

commit 2dc41281b1d1178befe4b76adf817570a7f45ec1

ACPI / hotplug / PCI: Avoid doing too much for spurious notifies
Sometimes we may get a spurious device check or bus check notify for
a hotplug device and in those cases we should avoid doing all of the
configuration work needed when something actually changes.  To that
end, check the return value of pci_scan_slot() in enable_slot() and
bail out early if it is 0.

This turns out to help reduce the amount of diagnostic output from
the ACPIPHP subsystem and speed up boot on at least one system that
generates multiple device check notifies for PCIe devices on the root
bus during boot.

Reported-and-tested-by: Alex Williamson <alex.williamson@redhat.com>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

drivers/pci/hotplug/acpiphp_glue.c
@@ -542,12 +542,12 @@ static void __ref enable_slot(struct acpiphp_slot *slot)
 	struct acpiphp_func *func;
 	int max, pass;
 	LIST_HEAD(add_list);
+	int nr_found;
 
 	list_for_each_entry(func, &slot->funcs, sibling)
 		acpiphp_bus_add(func_to_handle(func));
 
-	pci_scan_slot(bus, PCI_DEVFN(slot->device, 0));
-
+	nr_found = pci_scan_slot(bus, PCI_DEVFN(slot->device, 0));
 	max = acpiphp_max_busnr(bus);
 	for (pass = 0; pass < 2; pass++) {
 		list_for_each_entry(dev, &bus->devices, bus_list) {
@@ -566,8 +566,11 @@ static void __ref enable_slot(struct acpiphp_slot *slot)
 			}
 		}
 	}
-
 	__pci_bus_assign_resources(bus, &add_list, NULL);
+	/* Nothing more to do here if there are no new devices on this bus. */
+	if (!nr_found && (slot->flags & SLOT_ENABLED))
+		return;
+
 	acpiphp_sanitize_bus(bus);
 	acpiphp_set_hpp_values(bus);
 	acpiphp_set_acpi_region(slot);

halfline/rhel7

commit e74abb370194d2ee4859f0cef3d1d9f198ee0067

[pci] Revert: acpi / hotplug / pci: Avoid doing too much for spurious notifies
Message-id: <20140710010727.22146.68951.stgit@gir.stowe>
Patchwork-id: 85628
O-Subject: [RHEL7.1 PATCH 089/105] Revert "ACPI / hotplug / PCI: Avoid doing too much for spurious notifies"
Bugzilla: 1114228
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1114228
Upstream Status: ab1225901da2d4cd2dcbae6840e93abbef417064

commit ab1225901da2d4cd2dcbae6840e93abbef417064
Author: Mika Westerberg <mika.westerberg@linux.intel.com>
Date:   Wed Oct 30 14:40:36 2013 +0200

    Revert "ACPI / hotplug / PCI: Avoid doing too much for spurious notifies"

    Commit 2dc4128 (ACPI / hotplug / PCI: Avoid doing too much for
    spurious notifies) changed the enable_slot() to check return value of
    pci_scan_slot() and if it is zero return early from the function. It
    means that there were no new devices in this particular slot.

    However, if a device appeared deeper in the hierarchy the code now
    ignores it causing things like Thunderbolt chaining fail to recognize
    new devices.

    The problem with Alex Williamson's machine was solved with commit
    a47d8c8 (ACPI / hotplug / PCI: Avoid parent bus rescans on spurious
    device checks) and hence we should be able to restore the original
    functionality that we always rescan on bus check notification.

    On a device check notification we still check what acpiphp_rescan_slot()
    returns and on zero bail out early.

    Fixes: 2dc41281b1d1 (ACPI / hotplug / PCI: Avoid doing too much for spurious notifies)
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Tested-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
---

Signed-off-by: Jarod Wilson <jarod@redhat.com>

drivers/pci/hotplug/acpiphp_glue.c
@@ -552,9 +552,8 @@ static void __ref enable_slot(struct acpiphp_slot *slot)
 	struct acpiphp_func *func;
 	int max, pass;
 	LIST_HEAD(add_list);
-	int nr_found;
 
-	nr_found = acpiphp_rescan_slot(slot);
+	acpiphp_rescan_slot(slot);
 	max = acpiphp_max_busnr(bus);
 	for (pass = 0; pass < 2; pass++) {
 		list_for_each_entry(dev, &bus->devices, bus_list) {
@@ -574,9 +573,6 @@ static void __ref enable_slot(struct acpiphp_slot *slot)
 		}
 	}
 	__pci_bus_assign_resources(bus, &add_list, NULL);
-	/* Nothing more to do here if there are no new devices on this bus. */
-	if (!nr_found && (slot->flags & SLOT_ENABLED))
-		return;
 
 	acpiphp_sanitize_bus(bus);
 	acpiphp_set_hpp_values(bus);