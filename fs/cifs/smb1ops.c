a65d7556c0a4802e299f9db928c5ea95213570cf
Sachin Prabhu <sprabhu@redhat.com>
2014-07-28 12:16:39 -0400

    [fs] cifs: Fix cifsacl mounts over smb2 to not call cifs
    Message-id: <1406549822-10376-47-git-send-email-sprabhu@redhat.com>
    Patchwork-id: 86772
    O-Subject: [RHEL7.1 PATCH 46/69] [bz1082049][CIFS] Fix cifsacl mounts over smb2 to not call cifs
    Bugzilla: 1082049
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Steve French <smfrench@gmail.com>
    
    When mounting with smb2/smb3 (e.g. vers=2.1) and cifsacl mount option,
    it was trying to get the mode by querying the acl over the cifs
    rather than smb2 protocol.  This patch makes that protocol
    independent and makes cifsacl smb2 mounts return a more intuitive
    operation not supported error (until we add a worker function
    for smb2_get_acl).
    
    Note that a previous patch fixed getxattr/setxattr for the CIFSACL xattr
    which would unconditionally call cifs_get_acl and cifs_set_acl (even when
    mounted smb2). I made those protocol independent last week (new protocol
    version operations "get_acl" and "set_acl" but did not add an
    smb2_get_acl and smb2_set_acl yet so those now simply return EOPNOTSUPP
    which at least is better than sending cifs requests on smb2 mount)
    
    The previous patches did not fix the one remaining case though ie
    mounting with "cifsacl" when getting mode from acl would unconditionally
    end up calling "cifs_get_acl_from_fid" even for smb2 - so made that protocol
    independent but to make that protocol independent had to make sure that the callers
    were passing the protocol independent handle structure (cifs_fid) instead
    of cifs specific _u16 network file handle (ie cifs_fid instead of cifs_fid->fid)
    
    Now mount with smb2 and cifsacl mount options will return EOPNOTSUP (instead
    of timing out) and a future patch will add smb2 operations (e.g. get_smb2_acl)
    to enable this.
    
    Signed-off-by: Steve French <smfrench@gmail.com>
    (cherry picked from commit 42eacf9e57b65ffa768af72cb7fc86cc6f6af042)
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/cifs/smb1ops.c
+++ fs/cifs/smb1ops.c
@@ -1084,6 +1084,7 @@ struct smb_version_operations smb1_operations = {
 #endif /* CIFS_XATTR */
 #ifdef CONFIG_CIFS_ACL
 	.get_acl = get_cifs_acl,
+	.get_acl_by_fid = get_cifs_acl_by_fid,
 	.set_acl = set_cifs_acl,
 #endif /* CIFS_ACL */
 };
