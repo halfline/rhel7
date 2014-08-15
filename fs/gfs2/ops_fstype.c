b4657ffb9cf75f13afdd28875248b8fecae1b471
Abhijith Das <adas@redhat.com>
2014-08-15 19:56:40 -0400

    [fs] gfs2: Move quota bitmap operations under their own lock
    Message-id: <1408132603-52384-15-git-send-email-adas@redhat.com>
    Patchwork-id: 87815
    O-Subject: [RHEL7.1 PATCH 14/17] GFS2: Move quota bitmap operations under their own lock
    Bugzilla: 1026406
    RH-Acked-by: Robert S Peterson <rpeterso@redhat.com>
    RH-Acked-by: Steven Whitehouse <swhiteho@redhat.com>
    
    Gradually, the global qd_lock is being used for less and less.
    After this patch it will only be used for the per super block
    list whose purpose is to allow syncing of changes back to the
    master quota file from the local quota changes file. Fixing
    up that process to make it more efficient will be the subject
    of a later patch, however this patch removes another barrier
    to doing that.
    
    Resolves: rhbz#1026406
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>
    Cc: Abhijith Das <adas@redhat.com>
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- fs/gfs2/ops_fstype.c
+++ fs/gfs2/ops_fstype.c
@@ -98,6 +98,7 @@ static struct gfs2_sbd *init_sbd(struct super_block *sb)
 	init_waitqueue_head(&sdp->sd_quota_wait);
 	INIT_LIST_HEAD(&sdp->sd_trunc_list);
 	spin_lock_init(&sdp->sd_trunc_lock);
+	spin_lock_init(&sdp->sd_bitmap_lock);
 
 	spin_lock_init(&sdp->sd_log_lock);
 	atomic_set(&sdp->sd_log_pinned, 0);
