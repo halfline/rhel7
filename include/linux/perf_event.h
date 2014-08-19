6b3493348de6b1a45eb4c332eb0454621c5dd0ad
Jiri Olsa <jolsa@redhat.com>
2014-08-19 15:22:59 -0400

    [tools] perf: Add generic transaction flags
    Message-id: <1408462094-14194-11-git-send-email-jolsa@redhat.com>
    Patchwork-id: 87997
    O-Subject: [PATCH RHEL7.1 BZ1131394 010/325] perf: Add generic transaction flags
    Bugzilla: 1131394
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1131394
    https://bugzilla.redhat.com/show_bug.cgi?id=1131394
    
    upstream
    ========
    commit fdfbbd07e91f8fe387140776f3fd94605f0c89e5
    Author: Andi Kleen <ak@linux.intel.com>
    Date: Fri Sep 20 07:40:39 2013 -0700
    
    KABI
    ====
    The 'struct perf_sample_data' is internal perf structure, but it
    spread out (checksum-wise) into wild due to 'struct thread_struct'
    including 'struct perf_event' elements.
    
    Using __GENKSYMS__ to alter 'struct perf_sample_data' and not
    changing other dependent functions checksums.
    
    description
    ===========
    Add a generic qualifier for transaction events, as a new sample
    type that returns a flag word. This is particularly useful
    for qualifying aborts: to distinguish aborts which happen
    due to asynchronous events (like conflicts caused by another
    CPU) versus instructions that lead to an abort.
    
    The tuning strategies are very different for those cases,
    so it's important to distinguish them easily and early.
    
    Since it's inconvenient and inflexible to filter for this
    in the kernel we report all the events out and allow
    some post processing in user space.
    
    The flags are based on the Intel TSX events, but should be fairly
    generic and mostly applicable to other HTM architectures too. In addition
    to various flag words there's also reserved space to report an
    program supplied abort code. For TSX this is used to distinguish specific
    classes of aborts, like a lock busy abort when doing lock elision.
    
    Flags:
    
    Elision and generic transactions 		   (ELISION vs TRANSACTION)
    (HLE vs RTM on TSX; IBM etc.  would likely only use TRANSACTION)
    Aborts caused by current thread vs aborts caused by others (SYNC vs ASYNC)
    Retryable transaction				   (RETRY)
    Conflicts with other threads			   (CONFLICT)
    Transaction write capacity overflow		   (CAPACITY WRITE)
    Transaction read capacity overflow		   (CAPACITY READ)
    
    Transactions implicitely aborted can also return an abort code.
    This can be used to signal specific events to the profiler. A common
    case is abort on lock busy in a RTM eliding library (code 0xff)
    To handle this case we include the TSX abort code
    
    Common example aborts in TSX would be:
    
    - Data conflict with another thread on memory read.
                                          Flags: TRANSACTION|ASYNC|CONFLICT
    - executing a WRMSR in a transaction. Flags: TRANSACTION|SYNC
    - HLE transaction in user space is too large
                                          Flags: ELISION|SYNC|CAPACITY-WRITE
    
    The only flag that is somewhat TSX specific is ELISION.
    
    This adds the perf core glue needed for reporting the new flag word out.
    
    v2: Add MEM/MISC
    v3: Move transaction to the end
    v4: Separate capacity-read/write and remove misc
    v5: Remove _SAMPLE. Move abort flags to 32bit. Rename
        transaction to txn
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -582,6 +582,13 @@ struct perf_sample_data {
 	struct perf_regs_user		regs_user;
 	u64				stack_user_size;
 	u64				weight;
+
+#ifndef __GENKSYMS__
+	/*
+	 * Transaction flags for abort events:
+	 */
+	u64				txn;
+#endif
 };
 
 static inline void perf_sample_data_init(struct perf_sample_data *data,
@@ -597,6 +604,7 @@ static inline void perf_sample_data_init(struct perf_sample_data *data,
 	data->stack_user_size = 0;
 	data->weight = 0;
 	data->data_src.val = 0;
+	data->txn = 0;
 }
 
 extern void perf_output_sample(struct perf_output_handle *handle,
--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1198,6 +1198,9 @@ static void perf_event__header_size(struct perf_event *event)
 	if (sample_type & PERF_SAMPLE_DATA_SRC)
 		size += sizeof(data->data_src.val);
 
+	if (sample_type & PERF_SAMPLE_TRANSACTION)
+		size += sizeof(data->txn);
+
 	event->header_size = size;
 }
 
@@ -4565,6 +4568,9 @@ void perf_output_sample(struct perf_output_handle *handle,
 
 	if (sample_type & PERF_SAMPLE_DATA_SRC)
 		perf_output_put(handle, data->data_src.val);
+
+	if (sample_type & PERF_SAMPLE_TRANSACTION)
+		perf_output_put(handle, data->txn);
 
 	if (!event->attr.watermark) {
 		int wakeup_events = event->attr.wakeup_events;
