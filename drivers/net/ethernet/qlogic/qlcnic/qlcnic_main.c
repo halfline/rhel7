5c4f2774929f0c2810d0c68a8529f9e4a0795610
Chad Dupuis <cdupuis@redhat.com>
2014-08-21 13:05:35 -0400

    [ethernet] qlcnic: Issue INIT_NIC command only once
    Message-id: <1408626422-3941-52-git-send-email-cdupuis@redhat.com>
    Patchwork-id: 88586
    O-Subject: [PATCH RHEL 7.1 051/138] qlcnic: Issue INIT_NIC command only once.
    Bugzilla: 1089352
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Sucheta Chakraborty <sucheta.chakraborty@qlogic.com>
    
    Bug 1089352 (https://bugzilla.redhat.com/show_bug.cgi?id=1089352)
    
    net commit id 3720bf796b1826e8be804c3000b2c51492c4a10b
    
    o DCB AEN registration was reissuing INIT_NIC command. Instead, club
      all options of INIT NIC command and issue this command only once.
    
    Signed-off-by: Sucheta Chakraborty <sucheta.chakraborty@qlogic.com>
    Signed-off-by: Manish Chopra <manish.chopra@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/net/ethernet/qlogic/qlcnic/qlcnic_main.c
+++ drivers/net/ethernet/qlogic/qlcnic/qlcnic_main.c
@@ -2496,8 +2496,6 @@ static void qlcnic_remove(struct pci_dev *pdev)
 	qlcnic_cancel_idc_work(adapter);
 	ahw = adapter->ahw;
 
-	qlcnic_dcb_free(adapter->dcb);
-
 	unregister_netdev(netdev);
 	qlcnic_sriov_cleanup(adapter);
 
@@ -2509,6 +2507,8 @@ static void qlcnic_remove(struct pci_dev *pdev)
 		qlcnic_83xx_free_mailbox(ahw->mailbox);
 		kfree(ahw->fw_info);
 	}
+
+	qlcnic_dcb_free(adapter->dcb);
 
 	qlcnic_detach(adapter);
 
