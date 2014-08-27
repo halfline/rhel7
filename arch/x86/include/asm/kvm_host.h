c8e146e45cd42eda040b34c8afc64a2e72c1241a
Radim Krcmar <rkrcmar@redhat.com>
2014-08-27 15:09:59 -0400

    [virt] kvm: increase x86 VCPU limit to 240
    Message-id: <1409152199-31076-1-git-send-email-rkrcmar@redhat.com>
    Patchwork-id: 89266
    O-Subject: [RHEL7.1 kernel PATCH v2] kvm: x86: increase VCPU limit to 240
    Bugzilla: 1061403
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Eduardo Habkost <ehabkost@redhat.com>
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    
    bug:  https://bugzilla.redhat.com/show_bug.cgi?id=1061403
    brew: https://brewweb.devel.redhat.com/taskinfo?taskID=7877726
    
    255 VCPUs should work, but HP has only tested up to 240.
    
    RHEL imposes another limit though qemu.
    (RHEL_MAX_CPUS in include/hw/i386/pc.h)
    
    Signed-off-by: Radim Krcmar <rkrcmar@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -32,7 +32,7 @@
 #include <asm/asm.h>
 
 #define KVM_MAX_VCPUS 255
-#define KVM_SOFT_MAX_VCPUS 160
+#define KVM_SOFT_MAX_VCPUS 240
 #define KVM_USER_MEM_SLOTS 125
 /* memory slots that are not exposed to userspace */
 #define KVM_PRIVATE_MEM_SLOTS 3
