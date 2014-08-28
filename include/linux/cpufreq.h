6591b56132573dacf4196c2689e4b48e7a760f4c
Prarit Bhargava <prarit@redhat.com>
2014-08-28 14:24:33 -0400

    [cpufreq] Introduce new relation for freq selection
    Message-id: <1409235879-1283-180-git-send-email-prarit@redhat.com>
    Patchwork-id: 89470
    O-Subject: [RHEL7.1 PATCH BZ 1134639 179/185] cpufreq: Introduce new relation for freq selection
    Bugzilla: 1134369
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Mark Langsdorf <mlangsdo@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1134639
    
    commit 5b0c0b16d48d20e26859907df4dd449e3b3c7f4c
    Author: Stratos Karafotis <stratosk@semaphore.gr>
    Date:   Mon Jun 30 19:59:33 2014 +0300
    
        cpufreq: Introduce new relation for freq selection
    
        Introduce CPUFREQ_RELATION_C for frequency selection.
        It selects the frequency with the minimum euclidean distance to target.
        In case of equal distance between 2 frequencies, it will select the
        greater frequency.
    
        Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -256,6 +256,7 @@ static inline void cpufreq_resume(void) {}
 
 #define CPUFREQ_RELATION_L 0  /* lowest frequency at or above target */
 #define CPUFREQ_RELATION_H 1  /* highest frequency below or at target */
+#define CPUFREQ_RELATION_C 2  /* closest frequency to target */
 
 struct freq_attr;
 
