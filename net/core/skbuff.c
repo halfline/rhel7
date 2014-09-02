93656fa57f24bbb6577f838f869931e267004ad6
Florian Westphal <fwestpha@redhat.com>
2014-09-02 10:14:58 -0400

    [net] Fix save software checksum complete
    Message-id: <1409652920-431-36-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 90184
    O-Subject: [PATCH 35/57] [RHEL7.1 net] net: Fix save software checksum complete
    Bugzilla: 1131999
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    RH-Acked-by: Nikolay Aleksandrov <nikolay@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    BZ: 1131999
    
    commit 46fb51eb96cafb2c148b7b5119adb5e31a2bf3c4
    Author: Tom Herbert <therbert@google.com>
    Date:   Sat Jun 14 23:24:03 2014 -0700
    
        net: Fix save software checksum complete
    
        Geert reported issues regarding checksum complete and UDP.
        The logic introduced in commit 7e3cead5172927732f51fde
        ("net: Save software checksum complete") is not correct.
    
        This patch:
        1) Restores code in __skb_checksum_complete_header except for setting
           CHECKSUM_UNNECESSARY. This function may be calculating checksum on
           something less than skb->len.
        2) Adds saving checksum to __skb_checksum_complete. The full packet
           checksum 0..skb->len is calculated without adding in pseudo header.
           This value is saved in skb->csum and then the pseudo header is added
           to that to derive the checksum for validation.
        3) In both __skb_checksum_complete_header and __skb_checksum_complete,
           set skb->csum_valid to whether checksum of zero was computed. This
           allows skb_csum_unnecessary to return true without changing to
           CHECKSUM_UNNECESSARY which was done previously.
        4) Copy new csum related bits in __copy_skb_header.
    
        Reported-by: Geert Uytterhoeven <geert@linux-m68k.org>
        Signed-off-by: Tom Herbert <therbert@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -706,6 +706,9 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 	new->l4_rxhash		= old->l4_rxhash;
 	new->no_fcs		= old->no_fcs;
 	new->encapsulation	= old->encapsulation;
+	new->encap_hdr_csum	= old->encap_hdr_csum;
+	new->csum_valid		= old->csum_valid;
+	new->csum_complete_sw	= old->csum_complete_sw;
 #ifdef CONFIG_XFRM
 	new->sp			= secpath_get(old->sp);
 #endif
