9712f240a96357ef83353900f69396e5c6bbd927
Don Zickus <dzickus@redhat.com>
2014-09-12 18:06:14 -0400

    [powerpc] move debug registers in a structure
    Message-id: <1410545655-205645-146-git-send-email-dzickus@redhat.com>
    Patchwork-id: 94247
    O-Subject: [RHEL7 PATCH 145/626] powerpc: move debug registers in a structure
    Bugzilla: 1127366
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1127366
    
    commit 95791988fec645d196e746fcc0e329e19f7b1347
    Author: Bharat Bhushan <r65777@freescale.com>
    Date:   Wed Jun 26 11:12:22 2013 +0530
    
        powerpc: move debug registers in a structure
    
        This way we can use same data type struct with KVM and
        also help in using other debug related function.
    
        Signed-off-by: Bharat Bhushan <bharat.bhushan@freescale.com>
        Signed-off-by: Alexander Graf <agraf@suse.de>
    
    Backport notes: due to conflicts with three previously applied patches
    code had to move around compare to upstream.  This is what happens
    when we backport out of order.
    
    kabi notes:  This patch only changes how the elements in the struct are
    accessed.  It does not change the size or layout of the thread_struct.
    The reason is the code movement to the debug struct is just a copy and
    paste but the new struct is embedded in thread_struct in the same exact
    way it was originally.  So GENKSYMS works fine here.  Nothing is different,
    other than how things are accessed.
    
    ----
    v2 - add kabi notes to processor.h
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- arch/powerpc/include/asm/processor.h
+++ arch/powerpc/include/asm/processor.h
@@ -167,21 +167,14 @@ struct thread_vr_state {
 	vector128	vscr __attribute__((aligned(16)));
 };
 
-struct thread_struct {
-	unsigned long	ksp;		/* Kernel stack pointer */
-#ifdef CONFIG_PPC64
-	unsigned long	ksp_vsid;
-#endif
-	struct pt_regs	*regs;		/* Pointer to saved register state */
-	mm_segment_t	fs;		/* for get_fs() validation */
-#ifdef CONFIG_BOOKE
-	/* BookE base exception scratch space; align on cacheline */
-	unsigned long	normsave[8] ____cacheline_aligned;
-#endif
-#ifdef CONFIG_PPC32
-	void		*pgdir;		/* root of page-table tree */
-	unsigned long	ksp_limit;	/* if ksp <= ksp_limit stack overflow */
-#endif
+#ifndef __GENKSYMS__
+/*
+ * RHEL:
+ * This struct is embedded in thread_struct and matches the identical
+ * pre-change layout, hence why GENKSYMS works here.  Modifying this struct
+ * WILL break kabi.
+ */
+struct debug_reg {
 #ifdef CONFIG_PPC_ADV_DEBUG_REGS
 	/*
 	 * The following help to manage the use of Debug Control Registers
@@ -218,6 +211,67 @@ struct thread_struct {
 	unsigned long	dvc2;
 #endif
 #endif
+};
+#endif /* __GENKSYMS__ */
+
+struct thread_struct {
+	unsigned long	ksp;		/* Kernel stack pointer */
+
+#ifdef CONFIG_PPC64
+	unsigned long	ksp_vsid;
+#endif
+	struct pt_regs	*regs;		/* Pointer to saved register state */
+	mm_segment_t	fs;		/* for get_fs() validation */
+#ifdef CONFIG_BOOKE
+	/* BookE base exception scratch space; align on cacheline */
+	unsigned long	normsave[8] ____cacheline_aligned;
+#endif
+#ifdef CONFIG_PPC32
+	void		*pgdir;		/* root of page-table tree */
+	unsigned long	ksp_limit;	/* if ksp <= ksp_limit stack overflow */
+#endif
+#ifdef __GENKSYMS__
+#ifdef CONFIG_PPC_ADV_DEBUG_REGS
+	/*
+	 * The following help to manage the use of Debug Control Registers
+	 * om the BookE platforms.
+	 */
+	unsigned long	dbcr0;
+	unsigned long	dbcr1;
+#ifdef CONFIG_BOOKE
+	unsigned long	dbcr2;
+#endif
+	/*
+	 * The stored value of the DBSR register will be the value at the
+	 * last debug interrupt. This register can only be read from the
+	 * user (will never be written to) and has value while helping to
+	 * describe the reason for the last debug trap.  Torez
+	 */
+	unsigned long	dbsr;
+	/*
+	 * The following will contain addresses used by debug applications
+	 * to help trace and trap on particular address locations.
+	 * The bits in the Debug Control Registers above help define which
+	 * of the following registers will contain valid data and/or addresses.
+	 */
+	unsigned long	iac1;
+	unsigned long	iac2;
+#if CONFIG_PPC_ADV_DEBUG_IACS > 2
+	unsigned long	iac3;
+	unsigned long	iac4;
+#endif
+	unsigned long	dac1;
+	unsigned long	dac2;
+#if CONFIG_PPC_ADV_DEBUG_DVCS > 0
+	unsigned long	dvc1;
+	unsigned long	dvc2;
+#endif
+#endif
+#else /* __GENKSYMS__ */
+	/* Debug Registers */
+	struct debug_reg debug;
+#endif /* __GENKSYMS__ */
+
 	struct thread_fp_state	fp_state;
 	struct thread_fp_state	*fp_save_area;
 	int		fpexc_mode;	/* floating-point exception mode */
