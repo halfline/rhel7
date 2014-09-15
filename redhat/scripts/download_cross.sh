8512269fa476bf6f68eead1712a0050c9615d365
Prarit Bhargava <prarit@redhat.com>
2014-09-15 17:21:05 -0400

    [redhat] cross: Updated cross compile information as it is now implemented
    Message-id: <1410801665-5501-1-git-send-email-prarit@redhat.com>
    Patchwork-id: 94800
    O-Subject: [RHEL7.1 PATCH INTERNAL] Updated cross compile information as it is now implemented. [v4]
    Bugzilla: 1117819
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    From: Johnny Bieren <jbieren@redhat.com>
    
    redhat/docs/RHELPatchRules was changed to reflect that make rh-cross-all-builds is now implemented in RHEL-7.1.
    Also, redhat/Makefile.cross was edited to have variable EPEL_RELEASE.  Before the change, the following error occurred when running make rh-cross-all-builds:
    
    rpm -ivh http://download.bos.redhat.com/pub/epel/beta/7/x86_64/epel-release-7-0.1.noarch.rpm
    Retrieving http://download.bos.redhat.com/pub/epel/beta/7/x86_64/epel-release-7-0.1.noarch.rpm
    Creating linux-3.10.0-133.el7.test.tar.xz...
    curl: (22) The requested URL returned error: 404 Not Found
    error: skipping http://download.bos.redhat.com/pub/epel/beta/7/x86_64/epel-release-7-0.1.noarch.rpm - transfer failed
    
    In order to fix the error, the 7.0.1 in epel-release-7-0.1 needed to be variable to account for the latest updates to epel-release.
    
    [v3]: text change recommended by jbenc
    [v4]: update and change script name to download_cross.sh, dzickus
    
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Tony Camuso <tcamuso@redhat.com>
    Cc: Stefan Assmann <sassmann@redhat.com>
    Cc: Jes Sorensen <Jes.Sorensen@redhat.com>
    Cc: Mike Snitzer <snitzer@redhat.com>
    Cc: Jiri Benc <jbenc@redhat.com>
    Cc: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

---
+++ redhat/scripts/download_cross.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Download & install the latest EPEL7 repo information
+# Argument(s) to this script is a list of rpm names to install
+
+# if everything is installed then exit successfully
+rpm -q $@ && exit 0
+
+# install epel-release if necessary
+rpm -q epel-release >& /dev/null
+if [ $? -ne 0 ]; then
+	wget -nd -r -l1 --no-parent -A "epel-release*.rpm" http://download.bos.redhat.com/pub/epel/beta/7/x86_64
+	rpm -ivh epel-release*.rpm
+	# clean up
+	rm -f epel-release*.rpm
+fi
+
+# install list of rpms for cross compile
+yum -y install $@
+
+exit 0
