2de58a1dff31ee21990b9e0d7751d1f384aed9e9
Don Zickus <dzickus@redhat.com>
2014-09-16 17:09:46 -0400

    [usb] block suspension of superspeed port while hispeed peer is active
    Message-id: <1410887463-20674-515-git-send-email-dzickus@redhat.com>
    Patchwork-id: 95599
    O-Subject: [RHEL7 PATCH 514/591] usb: block suspension of superspeed port while hispeed peer is active
    Bugzilla: 1110939
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=821903
    
    commit 7ad3c47088f9faec463f5226e5e968a5c3b0e593
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Tue May 20 18:08:57 2014 -0700
    
        usb: block suspension of superspeed port while hispeed peer is active
    
        ClearPortFeature(PORT_POWER) on a usb3 port places the port in either a
        DSPORT.Powered-off-detect / DSPORT.Powered-off-reset loop, or the
        DSPORT.Powered-off state.  There is no way to ensure that RX
        terminations will persist in this state, so it is possible a device will
        degrade to its usb2 connection.  Prevent this by blocking power-off of a
        usb3 port while its usb2 peer is active, and powering on a usb3 port
        before its usb2 peer.
    
        By default the latency between peer power-on events is 0.  In order for
        the device to not see usb2 active while usb3 is still powering up inject
        the hub recommended power_on_good delay.  In support of satisfying the
        power_on_good delay outside of hub_power_on() refactor the places where
        the delay is consumed to call a new hub_power_on_good_delay() helper.
    
        Finally, because this introduces several new checks for whether a port
        is_superspeed, cache that disctinction at port creation so that we don't
        need to keep looking up the parent hub device.
    
        Acked-by: Alan Stern <stern@rowland.harvard.edu>
        [alan]: add a 'superspeed' flag to the port
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    ---
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -36,11 +36,6 @@
 #define USB_VENDOR_GENESYS_LOGIC		0x05e3
 #define HUB_QUIRK_CHECK_PORT_AUTOSUSPEND	0x01
 
-static inline int hub_is_superspeed(struct usb_device *hdev)
-{
-	return (hdev->descriptor.bDeviceProtocol == USB_HUB_PR_SS);
-}
-
 /* Protect struct usb_device->state and ->children members
  * Note: Both are also protected by ->dev.sem, except that ->state can
  * change to USB_STATE_NOTATTACHED even when the semaphore isn't held. */
@@ -821,14 +816,9 @@ int usb_hub_clear_tt_buffer(struct urb *urb)
 }
 EXPORT_SYMBOL_GPL(usb_hub_clear_tt_buffer);
 
-/* If do_delay is false, return the number of milliseconds the caller
- * needs to delay.
- */
-static unsigned hub_power_on(struct usb_hub *hub, bool do_delay)
+static void hub_power_on(struct usb_hub *hub, bool do_delay)
 {
 	int port1;
-	unsigned pgood_delay = hub->descriptor->bPwrOn2PwrGood * 2;
-	unsigned delay;
 
 	/* Enable power on each port.  Some hubs have reserved values
 	 * of LPSM (> 2) in their descriptors, even though they are
@@ -847,12 +837,8 @@ static unsigned hub_power_on(struct usb_hub *hub, bool do_delay)
 		else
 			usb_clear_port_feature(hub->hdev, port1,
 						USB_PORT_FEAT_POWER);
-
-	/* Wait at least 100 msec for power to become stable */
-	delay = max(pgood_delay, (unsigned) 100);
 	if (do_delay)
-		msleep(delay);
-	return delay;
+		msleep(hub_power_on_good_delay(hub));
 }
 
 static int hub_hub_status(struct usb_hub *hub,
@@ -1056,7 +1042,9 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 		 * for HUB_POST_RESET, but it's easier not to.
 		 */
 		if (type == HUB_INIT) {
-			delay = hub_power_on(hub, false);
+			unsigned delay = hub_power_on_good_delay(hub);
+
+			hub_power_on(hub, false);
 			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func2);
 			schedule_delayed_work(&hub->init_work,
 					msecs_to_jiffies(delay));
