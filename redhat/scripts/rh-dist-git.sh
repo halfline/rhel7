1d28e54b393d0a69883810ff6ffc29cdaad73d91
Jarod Wilson <jarod@redhat.com>
2014-09-23 18:15:07 -0400

    [redhat] infra: properly export kabi bits to dist-git
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- redhat/scripts/rh-dist-git.sh
+++ redhat/scripts/rh-dist-git.sh
@@ -5,12 +5,15 @@
 # $2: local pristine clone of dist-git
 # $3: alternate tmp directory (if you have faster storage)
 # $4: alternate dist-git server
+# $5: kernel source tarball
+# $6: kabi whitelists tarball
 
 rhdistgit_branch=$1;
 rhdistgit_cache=$2;
 rhdistgit_tmp=$3;
 rhdistgit_server=$4;
 rhdistgit_tarball=$5;
+rhdistgit_kabi_tarball=$6;
 
 redhat=$(dirname $0)/..;
 topdir=$redhat/..;
@@ -39,11 +42,14 @@ echo "Copying updated files"
 # copy the required files (redhat/git/files)
 $redhat/scripts/copy_files.sh "$topdir" "$tmpdir"
 
-echo "Uploading new tarball"
-# upload tarball
+echo "Uploading new tarballs"
+# upload tarballs
 sed -i "/linux-3.*.el7.tar.xz/d" $tmpdir/kernel/sources;
+sed -i "/kernel-abi-whitelists.tar.bz2/d" $tmpdir/kernel/sources;
 sed -i "/linux-3.*.el7.tar.xz/d" $tmpdir/kernel/.gitignore;
-rhpkg upload $rhdistgit_tarball >/dev/null || die "uploading tarball";
+sed -i "/kernel-abi-whitelists.tar.bz2/d" $tmpdir/kernel/.gitignore;
+rhpkg upload $rhdistgit_tarball >/dev/null || die "uploading kernel tarball";
+rhpkg upload $rhdistgit_kabi_tarball >/dev/null || die "uploading kabi tarball";
 
 echo "Creating diff for review ($tmpdir/diff) and changelog"
 # diff the result (redhat/cvs/dontdiff). note: diff reuturns 1 if
