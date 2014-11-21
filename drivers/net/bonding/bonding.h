ab9f4c2cefd721c2c03a53bd80af68b591f61689
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-21 12:52:19 -0500

    [net] bonding: add bond_tx_drop() helper
    Message-id: <1416574342-24493-334-git-send-email-nikolay@redhat.com>
    Patchwork-id: 100715
    O-Subject: [RHEL7.2 PATCH BZ 1166647 333/336] bonding: add bond_tx_drop() helper
    Bugzilla: 1166647
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    
    From: Eric Dumazet <edumazet@google.com>
    
    Because bonding stats are usually sum of slave stats, it was
    not easy to account for tx drops at bonding layer.
    
    We can use dev->tx_dropped for this, as this counter is later
    added to the device stats (in dev_get_stats())
    
    This extends the idea we had in commit ee6377147409a ("bonding: Simplify
    the xmit function for modes that use xmit_hash") for bond_3ad_xor_xmit()
    to other bonding modes.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Mahesh Bandewar <maheshb@google.com>
    Reviewed-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Acked-by: Mahesh Bandewar <maheshb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 31aa860e0aafd3a7c5a31c2aae67b6534115ea41)
    
    Nik: removed dev->tx_dropped increment.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -637,4 +637,9 @@ extern struct bond_parm_tbl ad_select_tbl[];
 /* exported from bond_netlink.c */
 extern struct rtnl_link_ops bond_link_ops;
 
+static inline void bond_tx_drop(struct net_device *dev, struct sk_buff *skb)
+{
+	dev_kfree_skb_any(skb);
+}
+
 #endif /* _LINUX_BONDING_H */
