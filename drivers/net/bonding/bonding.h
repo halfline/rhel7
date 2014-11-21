38b2580df1c86b2406b4c707341a47122eb33508
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-21 12:47:18 -0500

    [net] bonding: use netdev_upper list in bond_vlan_used
    Message-id: <1416574342-24493-33-git-send-email-nikolay@redhat.com>
    Patchwork-id: 100443
    O-Subject: [RHEL7.2 PATCH BZ 1166647 032/336] bonding: use netdev_upper list in bond_vlan_used
    Bugzilla: 1166647
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    
    From: Veaceslav Falico <vfalico@redhat.com>
    
    Convert bond_vlan_used() to traverse the upper device list to see if we
    have any vlans above us. It's protected by rcu, and in case we are holding
    rtnl_lock we should call vlan_uses_dev() instead - it's faster.
    
    CC: Jay Vosburgh <fubar@us.ibm.com>
    CC: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: Veaceslav Falico <vfalico@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c752af2c555617017dbb3a66f81e974b66ad5e2d)
    
    Nik: since we have older upper_dev_list handling, use
         list_for_each_entry_rcu directly. Also had to export struct
         netdev_upper, but kept it local to bonding.h.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -267,9 +267,29 @@ struct bonding {
 #endif /* CONFIG_DEBUG_FS */
 };
 
+struct netdev_upper {
+        struct net_device *dev;
+        bool master;
+        struct list_head list;
+        struct rcu_head rcu;
+        struct list_head search_list;
+};
+
+/* if we hold rtnl_lock() - call vlan_uses_dev() */
 static inline bool bond_vlan_used(struct bonding *bond)
 {
-	return !list_empty(&bond->vlan_list);
+	struct netdev_upper *upper;
+
+	rcu_read_lock();
+	list_for_each_entry_rcu(upper, &bond->dev->upper_dev_list, list) {
+		if (upper->dev->priv_flags & IFF_802_1Q_VLAN) {
+			rcu_read_unlock();
+			return true;
+		}
+	}
+	rcu_read_unlock();
+
+	return false;
 }
 
 #define bond_slave_get_rcu(dev) \
