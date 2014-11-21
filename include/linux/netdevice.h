fcdd59d3adcb1b1961481e36d88e7fb962f170e1
Nikolay Aleksandrov <nikolay@redhat.com>
2014-11-21 12:51:53 -0500

    [net] bonding: create netlink event when bonding option is changed
    Message-id: <1416574342-24493-308-git-send-email-nikolay@redhat.com>
    Patchwork-id: 100716
    O-Subject: [RHEL7.2 PATCH BZ 1166647 307/336] bonding: create netlink event when bonding option is changed
    Bugzilla: 1166647
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    
    From: Jiri Pirko <jiri@resnulli.us>
    
    Userspace needs to be notified if one changes some option.
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Acked-by: Veaceslav Falico <vfalico@gmail.com>
    Acked-by: Andy Gospodarek <gospo@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d4261e5650004d6d51137553ea5433d5828562dc)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	include/linux/netdevice.h
    	- missing 1 define, removed it from the patch.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1750,6 +1750,7 @@ struct udp_offload {
 #define NETDEV_NOTIFY_PEERS	0x0013
 #define NETDEV_JOIN		0x0014
 #define NETDEV_RESEND_IGMP	0x0016
+#define NETDEV_CHANGEINFODATA	0x0018
 
 extern int register_netdevice_notifier(struct notifier_block *nb);
 extern int unregister_netdevice_notifier(struct notifier_block *nb);
