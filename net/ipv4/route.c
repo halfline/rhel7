b1a097f749d8ad078df623a248ed084f6f151652
Hannes Frederic Sowa <hannes@redhat.com>
2014-12-08 17:32:59 -0500

    [net] ipv4: introduce new IP_MTU_DISCOVER mode IP_PMTUDISC_INTERFACE
    Message-id: <b78c24abb02d959462d8310496f8b6e25ae08049.1418058212.git.hannes@redhat.com>
    Patchwork-id: 101177
    O-Subject: [RHEL7.1 net 03/13] ipv4: introduce new IP_MTU_DISCOVER mode IP_PMTUDISC_INTERFACE
    Bugzilla: 1170116
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Flavio Leitner <fbl@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1170116
    Upstream Status: 482fc6094afad572a4ea1fd722e7b11ca72022a0
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=8344420
    
    Upstream Commit:
    commit 482fc6094afad572a4ea1fd722e7b11ca72022a0
    Author: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Date:   Tue Nov 5 02:24:17 2013 +0100
    
        ipv4: introduce new IP_MTU_DISCOVER mode IP_PMTUDISC_INTERFACE
    
        Sockets marked with IP_PMTUDISC_INTERFACE won't do path mtu discovery,
        their sockets won't accept and install new path mtu information and they
        will always use the interface mtu for outgoing packets. It is guaranteed
        that the packet is not fragmented locally. But we won't set the DF-Flag
        on the outgoing frames.
    
        Florian Weimer had the idea to use this flag to ensure DNS servers are
        never generating outgoing fragments. They may well be fragmented on the
        path, but the server never stores or usees path mtu values, which could
        well be forged in an attack.
    
        (The root of the problem with path MTU discovery is that there is
        no reliable way to authenticate ICMP Fragmentation Needed But DF Set
        messages because they are sent from intermediate routers with their
        source addresses, and the IMCP payload will not always contain sufficient
        information to identify a flow.)
    
        Recent research in the DNS community showed that it is possible to
        implement an attack where DNS cache poisoning is feasible by spoofing
        fragments. This work was done by Amir Herzberg and Haya Shulman:
        <https://sites.google.com/site/hayashulman/files/fragmentation-poisoning.pdf>
    
        This issue was previously discussed among the DNS community, e.g.
        <http://www.ietf.org/mail-archive/web/dnsext/current/msg01204.html>,
        without leading to fixes.
    
        This patch depends on the patch "ipv4: fix DO and PROBE pmtu mode
        regarding local fragmentation with UFO/CORK" for the enforcement of the
        non-fragmentable checks. If other users than ip_append_page/data should
        use this semantic too, we have to add a new flag to IPCB(skb)->flags to
        suppress local fragmentation and check for this in ip_finish_output.
    
        Many thanks to Florian Weimer for the idea and feedback while implementing
        this patch.
    
        Cc: David S. Miller <davem@davemloft.net>
        Suggested-by: Florian Weimer <fweimer@redhat.com>
        Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Hannes Frederic Sowa <hannes@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1013,6 +1013,9 @@ void ipv4_sk_update_pmtu(struct sk_buff *skb, struct sock *sk, u32 mtu)
 	bool new = false;
 
 	bh_lock_sock(sk);
+	if (!ip_sk_accept_pmtu(sk))
+		goto out;
+
 	odst = sk_dst_get(sk);
 
 	if (sock_owned_by_user(sk) || !odst) {
