9c2d0bbcad6d9bf1a3c41fdbf95e144feb987688
Hannes Frederic Sowa <hannes@redhat.com>
2014-12-08 17:33:04 -0500

    [net] ipv6: support IPV6_PMTU_INTERFACE on sockets
    Message-id: <e25cfbcf6c1694fc0699b7ff5c114d0ae3870b05.1418058212.git.hannes@redhat.com>
    Patchwork-id: 101182
    O-Subject: [RHEL7.1 net 08/13] ipv6: support IPV6_PMTU_INTERFACE on sockets
    Bugzilla: 1170116
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Flavio Leitner <fbl@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1170116
    Upstream Status: 93b36cf3425b9bd9c56df7680fb237686b9c82ae
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=8344420
    
    This part is missing, because it was fixed up by a later patch, which
    got into RHEL7 earlier than this one:
    
                                    ip6_append_data_mtu(&mtu, &maxfraglen,
                                                        fragheaderlen, skb, rt,
    -                                                   np->pmtudisc ==
    +                                                   np->pmtudisc >=
                                                        IPV6_PMTUDISC_PROBE);
    
    Upstream Commit:
    commit 93b36cf3425b9bd9c56df7680fb237686b9c82ae
    Author: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Date:   Sun Dec 15 03:41:14 2013 +0100
    
        ipv6: support IPV6_PMTU_INTERFACE on sockets
    
        IPV6_PMTU_INTERFACE is the same as IPV6_PMTU_PROBE for ipv6. Add it
        nontheless for symmetry with IPv4 sockets. Also drop incoming MTU
        information if this mode is enabled.
    
        The additional bit in ipv6_pinfo just eats in the padding behind the
        bitfield. There are no changes to the layout of the struct at all.
    
        Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Hannes Frederic Sowa <hannes@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1175,10 +1175,10 @@ int ip6_append_data(struct sock *sk, int getfrag(void *from, char *to,
 		np->cork.hop_limit = hlimit;
 		np->cork.tclass = tclass;
 		if (rt->dst.flags & DST_XFRM_TUNNEL)
-			mtu = np->pmtudisc == IPV6_PMTUDISC_PROBE ?
+			mtu = np->pmtudisc >= IPV6_PMTUDISC_PROBE ?
 			      rt->dst.dev->mtu : dst_mtu(&rt->dst);
 		else
-			mtu = np->pmtudisc == IPV6_PMTUDISC_PROBE ?
+			mtu = np->pmtudisc >= IPV6_PMTUDISC_PROBE ?
 			      rt->dst.dev->mtu : dst_mtu(rt->dst.path);
 		if (np->frag_size < mtu) {
 			if (np->frag_size)
