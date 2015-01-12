c33d54b83076f972083d0c5d50cdee821310cae8
Florian Westphal <fwestpha@redhat.com>
2015-01-12 12:04:27 -0500

    [net] udp: fix two sparse errors
    Message-id: <1421064408-18421-13-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 102649
    O-Subject: [PATCH 012/153] [RHEL7.2 net] udp: fix two sparse errors
    Bugzilla: 1151756
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    
    commit 7c0cadc69ca2ac8893aa162ee80d92a805840909
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Jun 12 14:31:39 2013 -0700
    
        udp: fix two sparse errors
    
        commit ba418fa357a7b3c ("soreuseport: UDP/IPv4 implementation")
        added following sparse errors :
    
        net/ipv4/udp.c:433:60: warning: cast from restricted __be16
        net/ipv4/udp.c:433:60: warning: incorrect type in argument 1 (different base types)
        net/ipv4/udp.c:433:60:    expected unsigned short [unsigned] [usertype] val
        net/ipv4/udp.c:433:60:    got restricted __be16 [usertype] sport
        net/ipv4/udp.c:433:60: warning: cast from restricted __be16
        net/ipv4/udp.c:433:60: warning: cast from restricted __be16
        net/ipv4/udp.c:514:60: warning: cast from restricted __be16
        net/ipv4/udp.c:514:60: warning: incorrect type in argument 1 (different base types)
        net/ipv4/udp.c:514:60:    expected unsigned short [unsigned] [usertype] val
        net/ipv4/udp.c:514:60:    got restricted __be16 [usertype] sport
        net/ipv4/udp.c:514:60: warning: cast from restricted __be16
        net/ipv4/udp.c:514:60: warning: cast from restricted __be16
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -431,7 +431,7 @@ begin:
 			reuseport = sk->sk_reuseport;
 			if (reuseport) {
 				hash = inet_ehashfn(net, daddr, hnum,
-						    saddr, htons(sport));
+						    saddr, sport);
 				matches = 1;
 			}
 		} else if (score == badness && reuseport) {
@@ -512,7 +512,7 @@ begin:
 			reuseport = sk->sk_reuseport;
 			if (reuseport) {
 				hash = inet_ehashfn(net, daddr, hnum,
-						    saddr, htons(sport));
+						    saddr, sport);
 				matches = 1;
 			}
 		} else if (score == badness && reuseport) {
