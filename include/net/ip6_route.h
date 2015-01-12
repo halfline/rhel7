dc4c2e547d6df2acb7e261089ba721cbbc75cbf6
Florian Westphal <fwestpha@redhat.com>
2015-01-12 12:05:54 -0500

    [net] ipv6: reuse rt6_need_strict
    Message-id: <1421064408-18421-100-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 102740
    O-Subject: [PATCH 099/153] [RHEL7.2 net] ipv6: reuse rt6_need_strict
    Bugzilla: 1151756
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Daniel Borkmann <dborkman@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    
    next patch of series depends on this.
    conflicts due to upstream 'extern' removal in ipv6 header files.
    
    commit 60ea37f7a5c776be4631c535c7cdd3ce62cf4dee
    Author: Wang Yufen <wangyufen@huawei.com>
    Date:   Sat Mar 29 09:27:30 2014 +0800
    
        ipv6: reuse rt6_need_strict
    
        Move the whole rt6_need_strict as static inline into ip6_route.h,
        so that it can be reused
    
        Signed-off-by: Wang Yufen <wangyufen@huawei.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/net/ip6_route.h
+++ include/net/ip6_route.h
@@ -51,6 +51,12 @@ static inline unsigned int rt6_flags2srcprefs(int flags)
 	return (flags >> 3) & 7;
 }
 
+static inline bool rt6_need_strict(const struct in6_addr *daddr)
+{
+	return ipv6_addr_type(daddr) &
+		(IPV6_ADDR_MULTICAST | IPV6_ADDR_LINKLOCAL | IPV6_ADDR_LOOPBACK);
+}
+
 extern void rt6_bind_peer(struct rt6_info *rt, int create);
 
 static inline struct inet_peer *__rt6_get_peer(struct rt6_info *rt, int create)
