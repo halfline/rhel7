bb823ec8bd27888ea6887e36ac3820379ed2f9a2
Florian Westphal <fwestpha@redhat.com>
2015-02-20 10:23:46 -0500

    [net] ip_tunnel: Do not use stale inner_iph pointer
    Message-id: <1424427831-26672-2-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 106266
    O-Subject: [PATCH 1/6] [RHEL7.2 net] ip_tunnel: Do not use stale inner_iph pointer.
    Bugzilla: 1187739
    RH-Acked-by: Jiri Pirko <jpirko@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    
    BZ: 1187739
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	net/ipv4/ip_tunnel.c
    
    Conflict due to lack of 963a88b31ddbbe ('tunnels: harmonize cleanup done
    on skb on xmit path') and upstream commit
    aad88724c9d54a ('ipv4: add a sock pointer to dst->output() path.')
    already being in rhel7, but that commit predates this one.
    
    commit d4a71b155c12d0d429c6b69d94076d6d57e2a7a7
    Author: Pravin B Shelar <pshelar@nicira.com>
    Date:   Wed Sep 25 09:57:47 2013 -0700
    
        ip_tunnel: Do not use stale inner_iph pointer.
    
        While sending packet skb_cow_head() can change skb header which
        invalidates inner_iph pointer to skb header. Following patch
        avoid using it. Found by code inspection.
    
        This bug was introduced by commit 0e6fbc5b6c6218 (ip_tunnels: extend
        iptunnel_xmit()).
    
        Signed-off-by: Pravin B Shelar <pshelar@nicira.com>
        Acked-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- net/ipv4/ip_tunnel.c
+++ net/ipv4/ip_tunnel.c
@@ -629,6 +629,7 @@ void ip_tunnel_xmit(struct sk_buff *skb, struct net_device *dev,
 			tunnel->err_count = 0;
 	}
 
+	tos = ip_tunnel_ecn_encap(tos, inner_iph, skb);
 	ttl = tnl_params->ttl;
 	if (ttl == 0) {
 		if (skb->protocol == htons(ETH_P_IP))
@@ -657,7 +658,8 @@ void ip_tunnel_xmit(struct sk_buff *skb, struct net_device *dev,
 	}
 
 	err = iptunnel_xmit(skb->sk, rt, skb, fl4.saddr, fl4.daddr, protocol,
-			    ip_tunnel_ecn_encap(tos, inner_iph, skb), ttl, df);
+			    tos, ttl, df);
+
 	iptunnel_xmit_stats(err, &dev->stats, dev->tstats);
 
 	return;
