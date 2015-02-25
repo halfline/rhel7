ff015c90b55faefb6f4c236603633adde7e45886
Alexander Gordeev <agordeev@redhat.com>
2015-02-25 09:14:36 -0500

    [redhat] scripts: Move release number generation to new_release.sh
    Message-id: <118ff320aaf6498717a03d0419315421b3bf81d1.1424855617.git.agordeev@redhat.com>
    Patchwork-id: 106445
    O-Subject: [PATCH RHEL7.2 v2 4/7] [redhat] Move release number generation to new_release.sh
    Bugzilla:
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Move a new release number generation to new_release.sh script
    entirely. That allows new_release.sh do what it is named after,
    avoid redundant Makefile scripting and passing unnecessary
    parameters around.
    
    Z-Stream could capitalize on this change and put its own logic
    into new_release.sh.
    
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- redhat/scripts/new_release.sh
+++ redhat/scripts/new_release.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
-	echo "$(basename $0) <kversion> <version> <redhat path>" >&2;
+if [ -z "$1" ]; then
+	echo "$(basename $0) <redhat path>" >&2;
 	exit 1;
 fi
 
-KVERSION="$1";
-RELEASE="$2";
-NEW_RELEASE="$[RELEASE + 1]";
-RHPATH="$3";
+RHPATH="$1";
 
 if [ -s "$RHPATH/linux-kernel-test.patch" ]; then
 	echo "linux-kernel-test.patch is not empty, aborting" >&2;
 	exit 1;
 fi
+
+RELEASE=$(sed -n -e 's/^BUILD:=\(.*\)/\1/p' $RHPATH/Makefile.common);
+NEW_RELEASE="$[RELEASE + 1]";
+
 sed -i -e "s/BUILD:=$RELEASE/BUILD:=$NEW_RELEASE/" $RHPATH/Makefile.common;
 
 # update top Makefile
