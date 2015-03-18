1d656bd4f24f2d087b4fb90dbb31c4874b80d1b5
Brian Foster <bfoster@redhat.com>
2015-03-18 15:05:51 -0400

    [fs] xfs: return is not a function
    Message-id: <1426691250-14369-85-git-send-email-bfoster@redhat.com>
    Patchwork-id: 107616
    O-Subject: [RHEL7.2 PATCH 084/183] xfs: return is not a function
    Bugzilla: 1184177
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    
    commit d99831ff393ff2e28d6110b41f24d9fecf986222
    Author: Eric Sandeen <sandeen@sandeen.net>
    Date:   Sun Jun 22 15:03:54 2014 +1000
    
        xfs: return is not a function
    
        return is not a function.  "return(EIO);" is silly;
        "return (EIO);" moreso.  return is not a function.
        Nuke the pointless parens.
    
        [dchinner: catch a couple of extra cases in xfs_attr_list.c,
        xfs_acl.c and xfs_linux.h.]
    
        Signed-off-by: Eric Sandeen <sandeen@redhat.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Signed-off-by: Dave Chinner <david@fromorbit.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/xfs/xfs_qm.c
+++ fs/xfs/xfs_qm.c
@@ -1326,7 +1326,7 @@ xfs_qm_quotacheck(
 		}
 	} else
 		xfs_notice(mp, "Quotacheck: Done.");
-	return (error);
+	return error;
 }
 
 /*
@@ -1886,7 +1886,7 @@ xfs_qm_vop_chown_reserve(
 				-((xfs_qcnt_t)delblks), 0, blkflags);
 	}
 
-	return (0);
+	return 0;
 }
 
 int
