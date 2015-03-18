c650ffa4c8ff4bf9825fbb3ea74fd78a73ecf9bf
Brian Foster <bfoster@redhat.com>
2015-03-18 15:04:54 -0400

    [fs] xfs: remove unused ail pointer arg from xfs_trans_ail_cursor_done()
    Message-id: <1426691250-14369-28-git-send-email-bfoster@redhat.com>
    Patchwork-id: 107673
    O-Subject: [RHEL7.2 PATCH 027/183] xfs: remove unused ail pointer arg from xfs_trans_ail_cursor_done()
    Bugzilla: 1184177
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    
    commit e4a1e29cb0ace3a322c5c07d33dd1f4ab50dbbb8
    Author: Eric Sandeen <sandeen@redhat.com>
    Date:   Mon Apr 14 19:06:05 2014 +1000
    
        xfs: remove unused ail pointer arg from xfs_trans_ail_cursor_done()
    
        Signed-off-by: Eric Sandeen <sandeen@redhat.com>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Signed-off-by: Dave Chinner <david@fromorbit.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -3158,7 +3158,7 @@ xlog_recover_efd_pass2(
 		}
 		lip = xfs_trans_ail_cursor_next(ailp, &cur);
 	}
-	xfs_trans_ail_cursor_done(ailp, &cur);
+	xfs_trans_ail_cursor_done(&cur);
 	spin_unlock(&ailp->xa_lock);
 
 	return 0;
@@ -3770,7 +3770,7 @@ xlog_recover_process_efis(
 		lip = xfs_trans_ail_cursor_next(ailp, &cur);
 	}
 out:
-	xfs_trans_ail_cursor_done(ailp, &cur);
+	xfs_trans_ail_cursor_done(&cur);
 	spin_unlock(&ailp->xa_lock);
 	return error;
 }
