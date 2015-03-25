74d2fc21d6d8a9f3e89bbbf444fb26de30187bea
George Beshers <gbeshers@redhat.com>
2015-03-25 15:08:33 -0400

    [kernel] timekeeping: Pass flags instead of multiple bools to timekeeping_update()
    Message-id: <20150325150833.21281.53445.sendpatchset@sgi-xe250-01.lab.bos.redhat.com>
    Patchwork-id: 108272
    O-Subject: [RHEL7.1 PATCH 03/27] timekeeping: Pass flags instead of multiple bools to timekeeping_update()
    Bugzilla: 1182693
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1182693
    Brew Build: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=8890410
    Upstream commit: 04397fe94ad65289884b9862b6a0c722ececaadf
    Testing: UV1000 4rack 8Tb system with up to 10 cores per socket (256 sockets)
    	 UV2000 4rack 32Tb system with up to 8 cores per socket (256 sockets)
             UV300  2rack 12Tb system with up to 15 cores (plus HT) per socket
    	 	(32 sockets).
    
    commit a573712b517176584e2d8d0cd621052a5892a587
    Author: David Vrabel <david.vrabel@citrix.com>
    Date:   Thu Jun 27 11:35:45 2013 +0100
    
        timekeeping: Pass flags instead of multiple bools to timekeeping_update()
    
        Instead of passing multiple bools to timekeeping_updated(), define
        flags and use a single 'action' parameter.  It is then more obvious
        what each timekeeping_update() call does.
    
        Signed-off-by: David Vrabel <david.vrabel@citrix.com>
        Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
        Cc: John Stultz <john.stultz@linaro.org>
        Cc: <xen-devel@lists.xen.org>
        Link: http://lkml.kernel.org/r/1372329348-20841-3-git-send-email-david.vrabel@citrix.com
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        (cherry picked from commit 04397fe94ad65289884b9862b6a0c722ececaadf)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -27,6 +27,9 @@
 #include "ntp_internal.h"
 #include "timekeeping_internal.h"
 
+#define TK_CLEAR_NTP		(1 << 0)
+#define TK_MIRROR		(1 << 1)
+
 static struct timekeeper timekeeper;
 static DEFINE_RAW_SPINLOCK(timekeeper_lock);
 static seqcount_t timekeeper_seq;
@@ -244,16 +247,16 @@ int pvclock_gtod_unregister_notifier(struct notifier_block *nb)
 EXPORT_SYMBOL_GPL(pvclock_gtod_unregister_notifier);
 
 /* must hold timekeeper_lock */
-static void timekeeping_update(struct timekeeper *tk, bool clearntp, bool mirror)
+static void timekeeping_update(struct timekeeper *tk, unsigned int action)
 {
-	if (clearntp) {
+	if (action & TK_CLEAR_NTP) {
 		tk->ntp_error = 0;
 		ntp_clear();
 	}
 	update_vsyscall(tk);
 	update_pvclock_gtod(tk);
 
-	if (mirror)
+	if (action & TK_MIRROR)
 		memcpy(&shadow_timekeeper, &timekeeper, sizeof(timekeeper));
 }
 
@@ -511,7 +514,7 @@ int do_settimeofday(const struct timespec *tv)
 
 	tk_set_xtime(tk, tv);
 
-	timekeeping_update(tk, true, true);
+	timekeeping_update(tk, TK_CLEAR_NTP | TK_MIRROR);
 
 	write_seqcount_end(&timekeeper_seq);
 	raw_spin_unlock_irqrestore(&timekeeper_lock, flags);
@@ -555,7 +558,7 @@ int timekeeping_inject_offset(struct timespec *ts)
 	tk_set_wall_to_mono(tk, timespec_sub(tk->wall_to_monotonic, *ts));
 
 error: /* even if we error out, we forwarded the time, so call update */
-	timekeeping_update(tk, true, true);
+	timekeeping_update(tk, TK_CLEAR_NTP | TK_MIRROR);
 
 	write_seqcount_end(&timekeeper_seq);
 	raw_spin_unlock_irqrestore(&timekeeper_lock, flags);
@@ -636,7 +639,7 @@ static int change_clocksource(void *data)
 		if (old->disable)
 			old->disable(old);
 	}
-	timekeeping_update(tk, true, true);
+	timekeeping_update(tk, TK_CLEAR_NTP | TK_MIRROR);
 
 	write_seqcount_end(&timekeeper_seq);
 	raw_spin_unlock_irqrestore(&timekeeper_lock, flags);
@@ -875,7 +878,7 @@ void timekeeping_inject_sleeptime(struct timespec *delta)
 
 	__timekeeping_inject_sleeptime(tk, delta);
 
-	timekeeping_update(tk, true, true);
+	timekeeping_update(tk, TK_CLEAR_NTP | TK_MIRROR);
 
 	write_seqcount_end(&timekeeper_seq);
 	raw_spin_unlock_irqrestore(&timekeeper_lock, flags);
@@ -958,7 +961,7 @@ static void timekeeping_resume(void)
 	tk->cycle_last = clock->cycle_last = cycle_now;
 	tk->ntp_error = 0;
 	timekeeping_suspended = 0;
-	timekeeping_update(tk, false, true);
+	timekeeping_update(tk, TK_MIRROR);
 	write_seqcount_end(&timekeeper_seq);
 	raw_spin_unlock_irqrestore(&timekeeper_lock, flags);
 
@@ -1384,7 +1387,7 @@ static void update_wall_time(void)
 	 * updating.
 	 */
 	memcpy(real_tk, tk, sizeof(*tk));
-	timekeeping_update(real_tk, false, false);
+	timekeeping_update(real_tk, 0);
 	write_seqcount_end(&timekeeper_seq);
 out:
 	raw_spin_unlock_irqrestore(&timekeeper_lock, flags);
