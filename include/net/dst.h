3bf2eff3afa0b45f5c67e9b2393dbef575f608b5
Florian Westphal <fwestpha@redhat.com>
2015-04-21 11:42:16 -0400

    [net] ipv6: do not overwrite inetpeer metrics prematurely
    Message-id: <1429616545-9897-7-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 111003
    O-Subject: [PATCH 06/15] [RHEL7.2 net] ipv6: do not overwrite inetpeer metrics prematurely
    Bugzilla: 1212624
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    
    BZ: 1212624
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	include/net/dst.h
    	include/net/ip6_fib.h
    	net/ipv6/ip6_fib.c
    
    Due to missing commits:
    5c3a0fd7d0fc298 ("ip*.h: Remove extern from function prototypes")
    a4023dd01a37f3a ("dst.h: Remove extern from function prototypes")
    and presence of commit
    6e9e16e6143b725 ("ipv6: replacing a rt6_info needs to purge possible propagated rt6_infos too")
    
    Also, folded fix 793c3b4000a ("net: ipv6: fib: don't sleep inside atomic lock")
    into this one, it was just s/GFP_KERNEL/GFP_ATOMIC/.
    
    commit e5fd387ad5b30ca3971fbccb0735c843cdebf967
    Author: Michal Kubecek <mkubecek@suse.cz>
    Date:   Thu Mar 27 13:04:08 2014 +0100
    
        ipv6: do not overwrite inetpeer metrics prematurely
    
        If an IPv6 host route with metrics exists, an attempt to add a
        new route for the same target with different metrics fails but
        rewrites the metrics anyway:
    
        12sp0:~ # ip route add fec0::1 dev eth0 rto_min 1000
        12sp0:~ # ip -6 route show
        fe80::/64 dev eth0  proto kernel  metric 256
        fec0::1 dev eth0  metric 1024  rto_min lock 1s
        12sp0:~ # ip route add fec0::1 dev eth0 rto_min 1500
        RTNETLINK answers: File exists
        12sp0:~ # ip -6 route show
        fe80::/64 dev eth0  proto kernel  metric 256
        fec0::1 dev eth0  metric 1024  rto_min lock 1.5s
    
        This is caused by all IPv6 host routes using the metrics in
        their inetpeer (or the shared default). This also holds for the
        new route created in ip6_route_add() which shares the metrics
        with the already existing route and thus ip6_route_add()
        rewrites the metrics even if the new route ends up not being
        used at all.
    
        Another problem is that old metrics in inetpeer can reappear
        unexpectedly for a new route, e.g.
    
        12sp0:~ # ip route add fec0::1 dev eth0 rto_min 1000
        12sp0:~ # ip route del fec0::1
        12sp0:~ # ip route add fec0::1 dev eth0
        12sp0:~ # ip route change fec0::1 dev eth0 hoplimit 10
        12sp0:~ # ip -6 route show
        fe80::/64 dev eth0  proto kernel  metric 256
        fec0::1 dev eth0  metric 1024  hoplimit 10 rto_min lock 1s
    
        Resolve the first problem by moving the setting of metrics down
        into fib6_add_rt2node() to the point we are sure we are
        inserting the new route into the tree. Second problem is
        addressed by introducing new flag DST_METRICS_FORCE_OVERWRITE
        which is set for a new host route in ip6_route_add() and makes
        ipv6_cow_metrics() always overwrite the metrics in inetpeer
        (even if they are not "new"); it is reset after that.
    
        v5: use a flag in _metrics member rather than one in flags
    
        v4: fix a typo making a condition always true (thanks to Hannes
        Frederic Sowa)
    
        v3: rewritten based on David Miller's idea to move setting the
        metrics (and allocation in non-host case) down to the point we
        already know the route is to be inserted. Also rebased to
        net-next as it is quite late in the cycle.
    
        Signed-off-by: Michal Kubecek <mkubecek@suse.cz>
        Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- include/net/dst.h
+++ include/net/dst.h
@@ -124,9 +124,11 @@ struct dst_entry {
 extern u32 *dst_cow_metrics_generic(struct dst_entry *dst, unsigned long old);
 extern const u32 dst_default_metrics[];
 
-#define DST_METRICS_READ_ONLY	0x1UL
+#define DST_METRICS_READ_ONLY		0x1UL
+#define DST_METRICS_FORCE_OVERWRITE	0x2UL
+#define DST_METRICS_FLAGS		0x3UL
 #define __DST_METRICS_PTR(Y)	\
-	((u32 *)((Y) & ~DST_METRICS_READ_ONLY))
+	((u32 *)((Y) & ~DST_METRICS_FLAGS))
 #define DST_METRICS_PTR(X)	__DST_METRICS_PTR((X)->_metrics)
 
 static inline bool dst_metrics_read_only(const struct dst_entry *dst)
@@ -134,7 +136,12 @@ static inline bool dst_metrics_read_only(const struct dst_entry *dst)
 	return dst->_metrics & DST_METRICS_READ_ONLY;
 }
 
-extern void __dst_destroy_metrics_generic(struct dst_entry *dst, unsigned long old);
+static inline void dst_metrics_set_force_overwrite(struct dst_entry *dst)
+{
+	dst->_metrics |= DST_METRICS_FORCE_OVERWRITE;
+}
+
+void __dst_destroy_metrics_generic(struct dst_entry *dst, unsigned long old);
 
 static inline void dst_destroy_metrics_generic(struct dst_entry *dst)
 {
--- include/net/ip6_fib.h
+++ include/net/ip6_fib.h
@@ -290,9 +290,8 @@ extern void			fib6_clean_all(struct net *net,
 					       int (*func)(struct rt6_info *, void *arg),
 					       void *arg);
 
-extern int			fib6_add(struct fib6_node *root,
-					 struct rt6_info *rt,
-					 struct nl_info *info);
+int fib6_add(struct fib6_node *root, struct rt6_info *rt, struct nl_info *info,
+	     struct nlattr *mx, int mx_len);
 
 extern int			fib6_del(struct rt6_info *rt,
 					 struct nl_info *info);
