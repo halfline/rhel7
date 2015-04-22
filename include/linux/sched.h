3721c987d1148ff4f4fa94acc336b26ff06e35c3
Johannes Weiner <jweiner@redhat.com>
2015-04-21 21:39:38 -0400

    [mm] memcg: handle non-error OOM situations more gracefully
    Message-id: <1429652382-5176-7-git-send-email-jweiner@redhat.com>
    Patchwork-id: 111125
    O-Subject: [PATCH RHEL7 06/10] mm: memcg: handle non-error OOM situations more gracefully
    Bugzilla: 1213903
    CVE: CVE-2014-8171
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    ommit 4942642080ea82d99ab5b653abb9a12b7ba31f4a
    Author: Johannes Weiner <hannes@cmpxchg.org>
    Date:   Wed Oct 16 13:46:59 2013 -0700
    
        mm: memcg: handle non-error OOM situations more gracefully
    
        Commit 3812c8c8f395 ("mm: memcg: do not trap chargers with full
        callstack on OOM") assumed that only a few places that can trigger a
        memcg OOM situation do not return VM_FAULT_OOM, like optional page cache
        readahead.  But there are many more and it's impractical to annotate
        them all.
    
        First of all, we don't want to invoke the OOM killer when the failed
        allocation is gracefully handled, so defer the actual kill to the end of
        the fault handling as well.  This simplifies the code quite a bit for
        added bonus.
    
        Second, since a failed allocation might not be the abrupt end of the
        fault, the memcg OOM handler needs to be re-entrant until the fault
        finishes for subsequent allocation attempts.  If an allocation is
        attempted after the task already OOMed, allow it to bypass the limit so
        that it can quickly finish the fault and invoke the OOM killer.
    
        Reported-by: azurIt <azurit@pobox.sk>
        Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
        Cc: Michal Hocko <mhocko@suse.cz>
        Cc: <stable@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Johannes Weiner <jweiner@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1562,11 +1562,10 @@ struct task_struct {
 #ifndef __GENKSYMS__
 #ifdef CONFIG_MEMCG
 	struct memcg_oom_info {
+		struct mem_cgroup *memcg;
+		gfp_t gfp_mask;
+		int order;
 		unsigned int may_oom:1;
-		unsigned int in_memcg_oom:1;
-		unsigned int oom_locked:1;
-		int wakeups;
-		struct mem_cgroup *wait_on_memcg;
 	} memcg_oom;
 #endif
 #endif /* __GENKSYMS__ */
