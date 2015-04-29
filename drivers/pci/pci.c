halfline/rhel7

commit c4f887cea66ee5062e0cb835f5ff8976b754475d

[pci] Notify driver before and after device reset
Message-id: <20150413153245.30909.74256.stgit@amt.stowe>
Patchwork-id: 110042
O-Subject: [RHEL7.2 PATCH 41/67] PCI: Notify driver before and after device reset
Bugzilla: 1110898
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Ivan Vecera <ivecera@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1110898

This upstream commit adds a new member to 'struct pci_error_handlers'.
'struct pci_error_handlers' is included within 'struct pci_driver' and
thus the additional member ends up breaking KABI.

Fortunately, 'struct pci_driver' has a Red Hat specific extension
structure - 'struct pci_driver_rh' - so that is where the new member is
added and all the references then adjusted to account for such.

commit 3ebe7f9f7e4a4fd1f6461ecd01ff2961317a483a
Author: Keith Busch <keith.busch@intel.com>
Date:   Fri May 2 10:40:42 2014 -0600

    PCI: Notify driver before and after device reset

    Notify a PCI device driver when its device's access is about to be disabled
    for an impending reset attempt, then after the attempt completes and device
    access is restored.  The notification is via the pci_error_handlers
    interface.

    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/pci/pci.c
@@ -3328,8 +3328,27 @@ static void pci_dev_unlock(struct pci_dev *dev)
 	pci_cfg_access_unlock(dev);
 }
 
+/**
+ * pci_reset_notify - notify device driver of reset
+ * @dev: device to be notified of reset
+ * @prepare: 'true' if device is about to be reset; 'false' if reset attempt
+ *           completed
+ *
+ * Must be called prior to device access being disabled and after device
+ * access is restored.
+ */
+static void pci_reset_notify(struct pci_dev *dev, bool prepare)
+{
+	const struct pci_error_handlers *err_handler =
+			dev->driver ? dev->driver->err_handler : NULL;
+	if (err_handler && dev->driver->pci_driver_rh->reset_notify)
+		dev->driver->pci_driver_rh->reset_notify(dev, prepare);
+}
+
 static void pci_dev_save_and_disable(struct pci_dev *dev)
 {
+	pci_reset_notify(dev, true);
+
 	/*
 	 * Wake-up device prior to save.  PM registers default to D0 after
 	 * reset and a simple register restore doesn't reliably return
@@ -3351,6 +3370,7 @@ static void pci_dev_save_and_disable(struct pci_dev *dev)
 static void pci_dev_restore(struct pci_dev *dev)
 {
 	pci_restore_state(dev);
+	pci_reset_notify(dev, false);
 }
 
 static int pci_dev_reset(struct pci_dev *dev, int probe)
@@ -3367,6 +3387,7 @@ static int pci_dev_reset(struct pci_dev *dev, int probe)
 
 	return rc;
 }
+
 /**
  * __pci_reset_function - reset a PCI device function
  * @dev: PCI device to reset

include/linux/pci.h
@@ -694,6 +694,8 @@ struct pci_driver {
 struct pci_driver_rh {
 	unsigned int  size;	/* Note: always outside of __GENKSYMS__ check */
 #ifndef __GENKSYMS__
+	/* PCI function reset prepare or completed */
+	void (*reset_notify)(struct pci_dev *dev, bool prepare);
 #endif
 };
 

commit 78ab59637d3dfc1cd4cbae2a25e5d13943b44f03

[pci] Fix RHEL7 specific possible Null pointer dereference
Message-id: <20150427185814.18509.43391.stgit@amt.stowe>
Patchwork-id: 111252
O-Subject: [RHEL7.2 PATCH] PCI: Fix RHEL7 specific possible Null pointer dereference
Bugzilla: 1215237
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
RH-Acked-by: Don Dutile <ddutile@redhat.com>
RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
RH-Acked-by: Rafael Aquini <aquini@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1215237
Upstream Status: RHEL7 specific (KABI related)
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9057433
Testing: Successfully tested on dell-per320-03.khw.lab.eng.bos.redhat.com
by applying this fix onto kernel-3.10.0-242.el7.

In 'pci_reset_notify()', the KABI related pointer to struct
'pci_driver_rh' ('dev->driver->pci_driver_rh') will be NULL unless the
driver has a need for extensions that have occurred after 7.0 and have
explicitly added such as part of their initialization.  For those drivers
that have not needed to address any extensions, an additional check is
needed so that we don't try to dereference a NULL pointer.

Fixes: c4f887cea66e ("[pci] Notify driver before and after device reset")
Found-by: Paul Bunyan <pbunyan@redhat.com>
Signed-off-by: Myron Stowe <myron.stowe@redhat.com>
CC: Paul Bunyan <pbunyan@redhat.com>
CC: Steve Best <sbest@redhat.com>
CC: Jan Stancek <jstancek@redhat.com>
CC: Rafael Aquini <aquini@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/pci/pci.c
@@ -3366,7 +3366,8 @@ static void pci_reset_notify(struct pci_dev *dev, bool prepare)
 {
 	const struct pci_error_handlers *err_handler =
 			dev->driver ? dev->driver->err_handler : NULL;
-	if (err_handler && dev->driver->pci_driver_rh->reset_notify)
+	if (err_handler && dev->driver->pci_driver_rh
+	    && dev->driver->pci_driver_rh->reset_notify)
 		dev->driver->pci_driver_rh->reset_notify(dev, prepare);
 }
 