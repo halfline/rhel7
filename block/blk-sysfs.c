0b765a6f4980c70addb198daa11f3fa4f90c5d2f
Jeff Moyer <jmoyer@redhat.com>
2015-05-06 20:25:04 -0400

    [block] remove blk_init_flush() and its pair
    Message-id: <1430943968-594-72-git-send-email-jmoyer@redhat.com>
    Patchwork-id: 111655
    O-Subject: [RHEL7 PATCH 071/135] block: remove blk_init_flush() and its pair
    Bugzilla: 1209624
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    
    BZ: 1209624
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9077774
    
      Upstream commit ba483388e3058b3e412632a84e6bf1f134beaf3d
      Author: Ming Lei <ming.lei@canonical.com>
      Date:   Thu Sep 25 23:23:44 2014 +0800
    
        block: remove blk_init_flush() and its pair
    
        Now mission of the two helpers is over, and just call
        blk_alloc_flush_queue() and blk_free_flush_queue() directly.
    
        Signed-off-by: Ming Lei <ming.lei@canonical.com>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -548,7 +548,7 @@ static void blk_release_queue(struct kobject *kobj)
 	if (q->queue_tags)
 		__blk_queue_free_tags(q);
 
-	blk_exit_flush(q);
+	blk_free_flush_queue(q->fq);
 
 	blk_trace_shutdown(q);
 
