ac0852c068e11858b17a35a3aba20a4600e501a6
Jarod Wilson <jarod@redhat.com>
2015-05-15 02:11:36 -0400

    [fs] proc: fix PAGE_SIZE limit of /proc/$PID/cmdline
    Message-id: <1431655897-53580-2-git-send-email-jarod@redhat.com>
    Patchwork-id: 112032
    O-Subject: [RHEL7 PATCH 1/2] proc: fix PAGE_SIZE limit of /proc/$PID/cmdline
    Bugzilla: 1193998
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1193998
    
    Upstream: https://git.kernel.org/cgit/linux/kernel/git/next/linux-next.git/patch/?id=136bb7281937092b58d5ca7c8841c16b7e7f8da5
    
    From: Alexey Dobriyan <adobriyan@gmail.com>
    Date: Thu, 14 May 2015 10:55:06 +1000
    Subject: proc: fix PAGE_SIZE limit of /proc/$PID/cmdline
    
        /proc/$PID/cmdline truncates output at PAGE_SIZE. It is easy to see with
    
        	$ cat /proc/self/cmdline $(seq 1037) 2>/dev/null
    
        However, command line size was never limited to PAGE_SIZE but to 128 KB and
        relatively recently limitation was removed altogether.
    
        People noticed and ask questions:
        http://stackoverflow.com/questions/199130/how-do-i-increase-the-proc-pid-cmdline-4096-byte-limit
    
        seq file interface is not OK, because it kmalloc's for whole output and
        open + read(, 1) + sleep will pin arbitrary amounts of kernel memory.
        To not do that, limit must be imposed which is incompatible with
        arbitrary sized command lines.
    
        I apologize for hairy code, but this it direct consequence of command line
        layout in memory and hacks to support things like "init [3]".
    
        The loops are "unrolled" otherwise it is either macros which hide
        control flow or functions with 7-8 arguments with equal line count.
    
        There should be real setproctitle(2) or something.
    
        Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
        Tested-by: Jarod Wilson <jarod@redhat.com>
        Acked-by: Jarod Wilson <jarod@redhat.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -200,42 +200,202 @@ static int proc_root_link(struct dentry *dentry, struct path *path)
 	return result;
 }
 
-static int proc_pid_cmdline(struct task_struct *task, char * buffer)
+static ssize_t proc_pid_cmdline_read(struct file *file, char __user *buf,
+				     size_t count, loff_t *pos)
 {
-	int res = 0;
-	unsigned int len;
-	struct mm_struct *mm = get_task_mm(task);
+	struct task_struct *tsk;
+	struct mm_struct *mm;
+	char *page;
+	unsigned long arg_start, arg_end, env_start, env_end;
+	unsigned long len1, len2, len;
+	unsigned long p;
+	char c;
+	ssize_t rv;
+
+	BUG_ON(*pos < 0);
+
+	tsk = get_proc_task(file_inode(file));
+	if (!tsk)
+		return -ESRCH;
+	mm = get_task_mm(tsk);
+	put_task_struct(tsk);
 	if (!mm)
-		goto out;
-	if (!mm->arg_end)
-		goto out_mm;	/* Shh! No looking before we're done */
-
- 	len = mm->arg_end - mm->arg_start;
- 
-	if (len > PAGE_SIZE)
-		len = PAGE_SIZE;
- 
-	res = access_process_vm(task, mm->arg_start, buffer, len, 0);
-
-	// If the nul at the end of args has been overwritten, then
-	// assume application is using setproctitle(3).
-	if (res > 0 && buffer[res-1] != '\0' && len < PAGE_SIZE) {
-		len = strnlen(buffer, res);
-		if (len < res) {
-		    res = len;
-		} else {
-			len = mm->env_end - mm->env_start;
-			if (len > PAGE_SIZE - res)
-				len = PAGE_SIZE - res;
-			res += access_process_vm(task, mm->env_start, buffer+res, len, 0);
-			res = strnlen(buffer, res);
-		}
+		return 0;
+	if (!mm->arg_end) {
+		rv = 0;
+		goto out_mmput;
 	}
-out_mm:
+
+	page = (char *)__get_free_page(GFP_TEMPORARY);
+	if (!page) {
+		rv = -ENOMEM;
+		goto out_mmput;
+	}
+
+	down_read(&mm->mmap_sem);
+	arg_start = mm->arg_start;
+	arg_end = mm->arg_end;
+	env_start = mm->env_start;
+	env_end = mm->env_end;
+	up_read(&mm->mmap_sem);
+
+	BUG_ON(arg_start > arg_end);
+	BUG_ON(env_start > env_end);
+
+	len1 = arg_end - arg_start;
+	len2 = env_end - env_start;
+
+	/*
+	 * Inherently racy -- command line shares address space
+	 * with code and data.
+	 */
+	rv = access_remote_vm(mm, arg_end - 1, &c, 1, 0);
+	if (rv <= 0)
+		goto out_free_page;
+
+	rv = 0;
+
+	if (c == '\0') {
+		/* Command line (set of strings) occupies whole ARGV. */
+		if (len1 <= *pos)
+			goto out_free_page;
+
+		p = arg_start + *pos;
+		len = len1 - *pos;
+		while (count > 0 && len > 0) {
+			unsigned int _count;
+			int nr_read;
+
+			_count = min3(count, len, PAGE_SIZE);
+			nr_read = access_remote_vm(mm, p, page, _count, 0);
+			if (nr_read < 0)
+				rv = nr_read;
+			if (nr_read <= 0)
+				goto out_free_page;
+
+			if (copy_to_user(buf, page, nr_read)) {
+				rv = -EFAULT;
+				goto out_free_page;
+			}
+
+			p       += nr_read;
+			len     -= nr_read;
+			buf     += nr_read;
+			count   -= nr_read;
+			rv      += nr_read;
+		}
+	} else {
+		/*
+		 * Command line (1 string) occupies ARGV and maybe
+		 * extends into ENVP.
+		 */
+		if (len1 + len2 <= *pos)
+			goto skip_argv_envp;
+		if (len1 <= *pos)
+			goto skip_argv;
+
+		p = arg_start + *pos;
+		len = len1 - *pos;
+		while (count > 0 && len > 0) {
+			unsigned int _count, l;
+			int nr_read;
+			bool final;
+
+			_count = min3(count, len, PAGE_SIZE);
+			nr_read = access_remote_vm(mm, p, page, _count, 0);
+			if (nr_read < 0)
+			        rv = nr_read;
+			if (nr_read <= 0)
+			        goto out_free_page;
+
+			/*
+			 * Command line can be shorter than whole ARGV
+			 * even if last "marker" byte says it is not.
+			 */
+			final = false;
+			l = strnlen(page, nr_read);
+			if (l < nr_read) {
+			        nr_read = l;
+			        final = true;
+			}
+
+			if (copy_to_user(buf, page, nr_read)) {
+			        rv = -EFAULT;
+			        goto out_free_page;
+			}
+
+			p       += nr_read;
+			len     -= nr_read;
+			buf     += nr_read;
+			count   -= nr_read;
+			rv      += nr_read;
+
+			if (final)
+			        goto out_free_page;
+		}
+skip_argv:
+		/*
+		 * Command line (1 string) occupies ARGV and
+		 * extends into ENVP.
+		 */
+		if (len1 <= *pos) {
+			p = env_start + *pos - len1;
+			len = len1 + len2 - *pos;
+		} else {
+			p = env_start;
+			len = len2;
+		}
+		while (count > 0 && len > 0) {
+			unsigned int _count, l;
+			int nr_read;
+			bool final;
+
+			_count = min3(count, len, PAGE_SIZE);
+			nr_read = access_remote_vm(mm, p, page, _count, 0);
+			if (nr_read < 0)
+			        rv = nr_read;
+			if (nr_read <= 0)
+			        goto out_free_page;
+
+			/* Find EOS. */
+			final = false;
+			l = strnlen(page, nr_read);
+			if (l < nr_read) {
+			        nr_read = l;
+			        final = true;
+			}
+
+			if (copy_to_user(buf, page, nr_read)) {
+			        rv = -EFAULT;
+			        goto out_free_page;
+			}
+
+			p       += nr_read;
+			len     -= nr_read;
+			buf     += nr_read;
+			count   -= nr_read;
+			rv      += nr_read;
+
+			if (final)
+			        goto out_free_page;
+		}
+skip_argv_envp:
+		;
+	}
+
+out_free_page:
+	free_page((unsigned long)page);
+out_mmput:
 	mmput(mm);
-out:
-	return res;
+	if (rv > 0)
+		*pos += rv;
+	return rv;
 }
+
+static const struct file_operations proc_pid_cmdline_ops = {
+	.read   = proc_pid_cmdline_read,
+	.llseek = generic_file_llseek,
+};
 
 static int proc_pid_auxv(struct task_struct *task, char *buffer)
 {
@@ -2662,7 +2822,7 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_HAVE_ARCH_TRACEHOOK
 	INF("syscall",    S_IRUGO, proc_pid_syscall),
 #endif
-	INF("cmdline",    S_IRUGO, proc_pid_cmdline),
+	REG("cmdline",    S_IRUGO, proc_pid_cmdline_ops),
 	ONE("stat",       S_IRUGO, proc_tgid_stat),
 	ONE("statm",      S_IRUGO, proc_pid_statm),
 	REG("maps",       S_IRUGO, proc_pid_maps_operations),
@@ -3016,7 +3176,7 @@ static const struct pid_entry tid_base_stuff[] = {
 #ifdef CONFIG_HAVE_ARCH_TRACEHOOK
 	INF("syscall",   S_IRUGO, proc_pid_syscall),
 #endif
-	INF("cmdline",   S_IRUGO, proc_pid_cmdline),
+	REG("cmdline",   S_IRUGO, proc_pid_cmdline_ops),
 	ONE("stat",      S_IRUGO, proc_tid_stat),
 	ONE("statm",     S_IRUGO, proc_pid_statm),
 	REG("maps",      S_IRUGO, proc_tid_maps_operations),
