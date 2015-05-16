7e7beb67b19a56dd899d408eab1627a90d1d9d56
Jiri Olsa <jolsa@redhat.com>
2015-05-16 10:57:17 -0400

    [perf] x86/intel: Support task events with Intel CQM
    Message-id: <1431774138-14330-27-git-send-email-jolsa@redhat.com>
    Patchwork-id: 112247
    O-Subject: [PATCH RHEL7.2 BZ1222189 026/327] perf/x86/intel: Support task events with Intel CQM
    Bugzilla: 1222189
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Bugzilla: 1222189
    https://bugzilla.redhat.com/show_bug.cgi?id=1222189
    
    upstream
    ========
    commit bfe1fcd2688f557a6b6a88f59ea7619228728bd7
    Author: Matt Fleming <matt.fleming@intel.com>
    Date: Fri Jan 23 18:45:46 2015 +0000
    
    description
    ===========
    Add support for task events as well as system-wide events. This change
    has a big impact on the way that we gather LLC occupancy values in
    intel_cqm_event_read().
    
    Currently, for system-wide (per-cpu) events we defer processing to
    userspace which knows how to discard all but one cpu result per package.
    
    Things aren't so simple for task events because we need to do the value
    aggregation ourselves. To do this, we defer updating the LLC occupancy
    value in event->count from intel_cqm_event_read() and do an SMP
    cross-call to read values for all packages in intel_cqm_event_count().
    We need to ensure that we only do this for one task event per cache
    group, otherwise we'll report duplicate values.
    
    If we're a system-wide event we want to fallback to the default
    perf_event_count() implementation. Refactor this into a common function
    so that we don't duplicate the code.
    
    Also, introduce PERF_TYPE_INTEL_CQM, since we need a way to track an
    event's task (if the event isn't per-cpu) inside of the Intel CQM PMU
    driver.  This task information is only availble in the upper layers of
    the perf infrastructure.
    
    Other perf backends stash the target task in event->hw.*target so we
    need to do something similar. The task is used to determine whether
    events should share a cache group and an RMID.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -156,6 +156,7 @@ struct hw_perf_event {
 			struct list_head	cqm_events_entry;
 			struct list_head	cqm_groups_entry;
 			struct list_head	cqm_group_entry;
+			struct task_struct	*cqm_target;
 		};
 #endif /* __GENKSYMS__ */
 #ifdef CONFIG_HAVE_HW_BREAKPOINT
