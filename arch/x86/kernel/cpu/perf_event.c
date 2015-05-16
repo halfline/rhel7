a2b140c6fedbbb71f9441f4a2badf3f155c7accb
Jiri Olsa <jolsa@redhat.com>
2015-05-16 10:57:19 -0400

    [perf] x86: Remove redundant calls to perf_pmu_{dis|en}able()
    Message-id: <1431774138-14330-29-git-send-email-jolsa@redhat.com>
    Patchwork-id: 112248
    O-Subject: [PATCH RHEL7.2 BZ1222189 028/327] perf/x86: Remove redundant calls to perf_pmu_{dis|en}able()
    Bugzilla: 1222189
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Bugzilla: 1222189
    https://bugzilla.redhat.com/show_bug.cgi?id=1222189
    
    upstream
    ========
    commit 9332d250b4b4f67c633894b311e022e3cf943bd5
    Author: David Ahern <dsahern@gmail.com>
    Date: Wed Feb 18 10:45:43 2015 -0700
    
    description
    ===========
    perf_pmu_disable() is called before pmu->add() and perf_pmu_enable() is called
    afterwards. No need to call these inside of x86_pmu_add() as well.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/perf_event.c
+++ arch/x86/kernel/cpu/perf_event.c
@@ -1040,7 +1040,6 @@ static int x86_pmu_add(struct perf_event *event, int flags)
 
 	hwc = &event->hw;
 
-	perf_pmu_disable(event->pmu);
 	n0 = cpuc->n_events;
 	ret = n = collect_events(cpuc, event, false);
 	if (ret < 0)
@@ -1078,7 +1077,6 @@ done_collect:
 
 	ret = 0;
 out:
-	perf_pmu_enable(event->pmu);
 	return ret;
 }
 
