3a44448a6949f3825e733ca455d696b4eb7107cb
Chad Dupuis <cdupuis@redhat.com>
2015-05-19 18:01:07 -0400

    [netdrv] qlcnic: Set skb->csum_level for encapsulated checksum
    Message-id: <1432058481-4062-10-git-send-email-cdupuis@redhat.com>
    Patchwork-id: 112924
    O-Subject: [PATCH RHEL 7.2 09/23] qlcnic: Set skb->csum_level for encapsulated checksum
    Bugzilla: 1187297
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Tom Herbert <therbert@google.com>
    
    Bug 1187297 (https://bugzilla.redhat.com/show_bug.cgi?id=1187297)
    
    net commit id 71d7a2772510a05a2cec4d005f984043827718e1
    
    Set skb->csum_level instead of skb->encapsulation when indicating
    CHECKSUM_UNNECESSARY for an encapsulated checksum.
    
    Signed-off-by: Tom Herbert <therbert@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/qlogic/qlcnic/qlcnic_io.c
+++ drivers/net/ethernet/qlogic/qlcnic/qlcnic_io.c
@@ -1752,7 +1752,7 @@ qlcnic_83xx_process_rcv(struct qlcnic_adapter *adapter,
 
 	if (qlcnic_encap_length(sts_data[1]) &&
 	    skb->ip_summed == CHECKSUM_UNNECESSARY) {
-		skb->encapsulation = 1;
+		skb->csum_level = 1;
 		adapter->stats.encap_rx_csummed++;
 	}
 
