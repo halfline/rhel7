02464f3a023da780efb585f76a0a428ceab0108b
Myron Stowe <myron.stowe@redhat.com>
2015-05-26 02:35:54 -0400

    [pci] acpiphp / radeon / nouveau: Remove acpi_bus_no_hotplug()
    Message-id: <20150526023554.3637.65146.stgit@amt.stowe>
    Patchwork-id: 113669
    O-Subject: [RHEL7.2 PATCH 31/34] ACPIPHP / radeon / nouveau: Remove acpi_bus_no_hotplug()
    Bugzilla: 1223472
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Rob Clark <rclark@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1223472
    Upstream Status: f91ce35e471ae17552ce7bfe355cfd997e3ad781
    
    commit f91ce35e471ae17552ce7bfe355cfd997e3ad781
    Author: Bjorn Helgaas <bhelgaas@google.com>
    Date:   Wed Sep 10 15:30:08 2014 -0600
    
        ACPIPHP / radeon / nouveau: Remove acpi_bus_no_hotplug()
    
        Revert parts of f244d8b623da ("ACPIPHP / radeon / nouveau: Fix VGA
        switcheroo problem related to hotplug").
    
        A previous commit 5493b31f0b55 ("PCI: Add pci_ignore_hotplug() to ignore
        hotplug events for a device") added equivalent functionality implemented in
        a different way for both acpiphp and pciehp.
    
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Acked-by: Alex Deucher <alexander.deucher@amd.com>
        Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
        Acked-by: Dave Airlie <airlied@redhat.com>
        Acked-by: Rajat Jain <rajatxjain@gmail.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -206,10 +206,9 @@ struct acpi_device_flags {
 	u32 match_driver:1;
 	u32 initialized:1;
 	u32 visited:1;
-	u32 no_hotplug:1;
 	u32 hotplug_notify:1;
 	u32 is_dock_station:1;
-	u32 reserved:22;
+	u32 reserved:23;
 };
 
 /* File System */
@@ -401,7 +400,6 @@ extern struct kobject *acpi_kobj;
 extern int acpi_bus_generate_netlink_event(const char*, const char*, u8, int);
 void acpi_bus_private_data_handler(acpi_handle, void *);
 int acpi_bus_get_private_data(acpi_handle, void **);
-void acpi_bus_no_hotplug(acpi_handle handle);
 extern int acpi_notifier_call_chain(struct acpi_device *, u32, u32);
 extern int register_acpi_notifier(struct notifier_block *);
 extern int unregister_acpi_notifier(struct notifier_block *);
