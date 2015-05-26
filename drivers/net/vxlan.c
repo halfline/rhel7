e76b3a994473ec0fc68541357947cfbc92bedb39
Marcelo Leitner <mleitner@redhat.com>
2015-05-26 16:44:44 -0400

    [net] vxlan: Convert uses of compare_ether_addr to ether_addr_equal
    Message-id: <8dc0e69ef84614f51acadc2297f6752a0080e9ea.1432658084.git.mleitner@redhat.com>
    Patchwork-id: 113718
    O-Subject: [RHEL7.2 net PATCH v2 005/137] drivers/net: Convert uses of compare_ether_addr to ether_addr_equal
    Bugzilla: 1206570
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1206570
    Backport of upstream commit: 7367d0b573d149550d2ae25c402984b98f8f422e
    
     commit 7367d0b573d149550d2ae25c402984b98f8f422e
     Author: Joe Perches <joe@perches.com>
     Date:   Sun Sep 1 11:51:23 2013 -0700
    
        drivers/net: Convert uses of compare_ether_addr to ether_addr_equal
    
        Use the new bool function ether_addr_equal to add
        some clarity and reduce the likelihood for misuse
        of compare_ether_addr for sorting.
    
        Done via cocci script: (and a little typing)
    
        $ cat compare_ether_addr.cocci
        @@
        expression a,b;
        @@
        -	!compare_ether_addr(a, b)
        +	ether_addr_equal(a, b)
    
        @@
        expression a,b;
        @@
        -	compare_ether_addr(a, b)
        +	!ether_addr_equal(a, b)
    
        @@
        expression a,b;
        @@
        -	!ether_addr_equal(a, b) == 0
        +	ether_addr_equal(a, b)
    
        @@
        expression a,b;
        @@
        -	!ether_addr_equal(a, b) != 0
        +	!ether_addr_equal(a, b)
    
        @@
        expression a,b;
        @@
        -	ether_addr_equal(a, b) == 0
        +	!ether_addr_equal(a, b)
    
        @@
        expression a,b;
        @@
        -	ether_addr_equal(a, b) != 0
        +	ether_addr_equal(a, b)
    
        @@
        expression a,b;
        @@
        -	!!ether_addr_equal(a, b)
        +	ether_addr_equal(a, b)
    
        Signed-off-by: Joe Perches <joe@perches.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Marcelo Ricardo Leitner <mleitner@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -470,7 +470,7 @@ static struct vxlan_fdb *__vxlan_find_mac(struct vxlan_dev *vxlan,
 	struct vxlan_fdb *f;
 
 	hlist_for_each_entry_rcu(f, head, hlist) {
-		if (compare_ether_addr(mac, f->eth_addr) == 0)
+		if (ether_addr_equal(mac, f->eth_addr))
 			return f;
 	}
 
@@ -1176,8 +1176,7 @@ static void vxlan_rcv(struct vxlan_sock *vs,
 	skb_postpull_rcsum(skb, eth_hdr(skb), ETH_HLEN);
 
 	/* Ignore packet loops (and multicast echo) */
-	if (compare_ether_addr(eth_hdr(skb)->h_source,
-			       vxlan->dev->dev_addr) == 0)
+	if (ether_addr_equal(eth_hdr(skb)->h_source, vxlan->dev->dev_addr))
 		goto drop;
 
 	/* Re-examine inner Ethernet packet */
@@ -1538,7 +1537,7 @@ static bool route_shortcircuit(struct net_device *dev, struct sk_buff *skb)
 	if (n) {
 		bool diff;
 
-		diff = compare_ether_addr(eth_hdr(skb)->h_dest, n->ha) != 0;
+		diff = !ether_addr_equal(eth_hdr(skb)->h_dest, n->ha);
 		if (diff) {
 			memcpy(eth_hdr(skb)->h_source, eth_hdr(skb)->h_dest,
 				dev->addr_len);
