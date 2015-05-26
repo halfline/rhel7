c570618ea001ab274fc0da41539c71e9f65a88ac
Marcelo Leitner <mleitner@redhat.com>
2015-05-26 16:44:49 -0400

    [net] Explicitly initialize u64_stats_sync structures for lockdep
    Message-id: <ed497f9d291975d8132ede759a62ee455d4eec4e.1432658084.git.mleitner@redhat.com>
    Patchwork-id: 113723
    O-Subject: [RHEL7.2 net PATCH v2 010/137] net: Explicitly initialize u64_stats_sync structures for lockdep
    Bugzilla: 1206570
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1206570
    Backport of upstream commit: 827da44c61419f29ae3be198c342e2147f1a10cb
    
     commit 827da44c61419f29ae3be198c342e2147f1a10cb
     Author: John Stultz <john.stultz@linaro.org>
     Date:   Mon Oct 7 15:51:58 2013 -0700
    
        net: Explicitly initialize u64_stats_sync structures for lockdep
    
        In order to enable lockdep on seqcount/seqlock structures, we
        must explicitly initialize any locks.
    
        The u64_stats_sync structure, uses a seqcount, and thus we need
        to introduce a u64_stats_init() function and use it to initialize
        the structure.
    
        This unfortunately adds a lot of fairly trivial initialization code
        to a number of drivers. But the benefit of ensuring correctness makes
        this worth while.
    
        Because these changes are required for lockdep to be enabled, and the
        changes are quite trivial, I've not yet split this patch out into 30-some
        separate patches, as I figured it would be better to get the various
        maintainers thoughts on how to best merge this change along with
        the seqcount lockdep enablement.
    
        Feedback would be appreciated!
    
        Signed-off-by: John Stultz <john.stultz@linaro.org>
        Acked-by: Julian Anastasov <ja@ssi.bg>
        Signed-off-by: Peter Zijlstra <peterz@infradead.org>
        Cc: Alexey Kuznetsov <kuznet@ms2.inr.ac.ru>
        Cc: "David S. Miller" <davem@davemloft.net>
        Cc: Eric Dumazet <eric.dumazet@gmail.com>
        Cc: Hideaki YOSHIFUJI <yoshfuji@linux-ipv6.org>
        Cc: James Morris <jmorris@namei.org>
        Cc: Jesse Gross <jesse@nicira.com>
        Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
        Cc: "Michael S. Tsirkin" <mst@redhat.com>
        Cc: Mirko Lindner <mlindner@marvell.com>
        Cc: Patrick McHardy <kaber@trash.net>
        Cc: Roger Luethi <rl@hellgate.ch>
        Cc: Rusty Russell <rusty@rustcorp.com.au>
        Cc: Simon Horman <horms@verge.net.au>
        Cc: Stephen Hemminger <stephen@networkplumber.org>
        Cc: Steven Rostedt <rostedt@goodmis.org>
        Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
        Cc: Wensong Zhang <wensong@linux-vs.org>
        Cc: netdev@vger.kernel.org
        Link: http://lkml.kernel.org/r/1381186321-4906-2-git-send-email-john.stultz@linaro.org
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Marcelo Ricardo Leitner <mleitner@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -2010,10 +2010,18 @@ static int vxlan_init(struct net_device *dev)
 	struct vxlan_net *vn = net_generic(dev_net(dev), vxlan_net_id);
 	struct vxlan_sock *vs;
 	bool ipv6 = vxlan->flags & VXLAN_F_IPV6;
+	int i;
 
 	dev->tstats = alloc_percpu(struct pcpu_tstats);
 	if (!dev->tstats)
 		return -ENOMEM;
+
+	for_each_possible_cpu(i) {
+		struct pcpu_tstats *vxlan_stats;
+		vxlan_stats = per_cpu_ptr(dev->tstats, i);
+		u64_stats_init(&vxlan_stats->syncp);
+	}
+
 
 	spin_lock(&vn->sock_lock);
 	vs = vxlan_find_sock(dev_net(dev), ipv6 ? AF_INET6 : AF_INET,
