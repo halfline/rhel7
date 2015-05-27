6fcfeba13fa46ca3912ba8c4f44ba87f7f7223a5
Bandan Das <bsd@redhat.com>
2015-05-27 03:57:26 -0400

    [kvm] x86: optimize out smp_mb after srcu_read_unlock
    Message-id: <1432699356-504-3-git-send-email-bsd@redhat.com>
    Patchwork-id: 114190
    O-Subject: [PATCH RHEL-7.2 v4 002/312] kvm: optimize out smp_mb after srcu_read_unlock
    Bugzilla: 1209995
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    
    From: "Michael S. Tsirkin" <mst@redhat.com>
    
    I noticed that srcu_read_lock/unlock both have a memory barrier,
    so just by moving srcu_read_unlock earlier we can get rid of
    one call to smp_mb() using smp_mb__after_srcu_read_unlock instead.
    
    Unsurprisingly, the gain is small but measureable using the unit test
    microbenchmark:
    before
            vmcall in the ballpark of 1410 cycles
    after
            vmcall in the ballpark of 1360 cycles
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Gleb Natapov <gleb@redhat.com>
    (cherry picked from commit 01b71917b55d28c09ade9fb8c683cf0d2aad1858)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6111,10 +6111,12 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 
 	vcpu->mode = IN_GUEST_MODE;
 
+	srcu_read_unlock(&vcpu->kvm->srcu, vcpu->srcu_idx);
+
 	/* We should set ->mode before check ->requests,
 	 * see the comment in make_all_cpus_request.
 	 */
-	smp_mb();
+	smp_mb__after_srcu_read_unlock();
 
 	local_irq_disable();
 
@@ -6124,11 +6126,10 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		smp_wmb();
 		local_irq_enable();
 		preempt_enable();
+		vcpu->srcu_idx = srcu_read_lock(&vcpu->kvm->srcu);
 		r = 1;
 		goto cancel_injection;
 	}
-
-	srcu_read_unlock(&vcpu->kvm->srcu, vcpu->srcu_idx);
 
 	if (req_immediate_exit)
 		smp_send_reschedule(vcpu->cpu);
