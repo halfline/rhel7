615006d84ff78496091e3bdc4f00e4ec70d4b5a1
Hannes Frederic Sowa <hannes@redhat.com>
2015-05-29 15:42:28 -0400

    [net] make skb_splice_bits more configureable
    Message-id: <70a979c9077958076d3ba6191e46897d787e5b73.1432913688.git.hannes@redhat.com>
    Patchwork-id: 114618
    O-Subject: [RHEL7.2 net 10/11] net: make skb_splice_bits more configureable
    Bugzilla: 1226230
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1226230
    Upstream Status: a60e3cc7c92973a31fad0fd04dc5cf4355d3d1ef
    
    Upstream Commit:
    commit a60e3cc7c92973a31fad0fd04dc5cf4355d3d1ef
    Author: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Date:   Thu May 21 17:00:00 2015 +0200
    
        net: make skb_splice_bits more configureable
    
        Prepare skb_splice_bits to be able to deal with AF_UNIX sockets.
    
        AF_UNIX sockets don't use lock_sock/release_sock and thus we have to
        use a callback to make the locking and unlocking configureable.
    
        Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Acked-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Hannes Frederic Sowa <hannes@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -2631,11 +2631,16 @@ extern int	       skb_store_bits(struct sk_buff *skb, int offset,
 extern __wsum	       skb_copy_and_csum_bits(const struct sk_buff *skb,
 					      int offset, u8 *to, int len,
 					      __wsum csum);
-extern int             skb_splice_bits(struct sk_buff *skb,
-						unsigned int offset,
-						struct pipe_inode_info *pipe,
-						unsigned int len,
-						unsigned int flags);
+struct splice_pipe_desc;
+ssize_t skb_socket_splice(struct sock *sk,
+			  struct pipe_inode_info *pipe,
+			  struct splice_pipe_desc *spd);
+extern int skb_splice_bits(struct sk_buff *skb, struct sock *sk,
+			   unsigned int offset, struct pipe_inode_info *pipe,
+			   unsigned int len, unsigned int flags,
+			   ssize_t (*splice_cb)(struct sock *,
+						struct pipe_inode_info *,
+						struct splice_pipe_desc *));
 extern void	       skb_copy_and_csum_dev(const struct sk_buff *skb, u8 *to);
 unsigned int skb_zerocopy_headlen(const struct sk_buff *from);
 int skb_zerocopy(struct sk_buff *to, struct sk_buff *from,
