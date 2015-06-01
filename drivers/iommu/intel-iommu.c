293d41b669f0e37fa42c6497697918e184053602
Myron Stowe <myron.stowe@redhat.com>
2015-06-01 19:34:05 -0400

    [iommu] vt-d: Remove unused variable
    Message-id: <20150601193405.6371.1970.stgit@amt.stowe>
    Patchwork-id: 114846
    O-Subject: [RHEL7.2 PATCH 42/42] iommu/vt-d: Remove unused variable
    Bugzilla: 1225193
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1225193
    Upstream Status: 509fca899d5682a6eee3d1fb70bba7c89439034b
    
    commit 509fca899d5682a6eee3d1fb70bba7c89439034b
    Author: Alex Williamson <alex.williamson@redhat.com>
    Date:   Mon Mar 23 19:50:21 2015 -0600
    
        iommu/vt-d: Remove unused variable
    
        Unused after commit 71684406905f ("iommu/vt-d: Detach domain *only*
        from attached iommus").  Reported by 0-day builder.
    
        Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -1744,7 +1744,6 @@ static int domain_init(struct dmar_domain *domain, int guest_width)
 
 static void domain_exit(struct dmar_domain *domain)
 {
-	struct dmar_drhd_unit *drhd;
 	struct page *freelist = NULL;
 	int i;
 
