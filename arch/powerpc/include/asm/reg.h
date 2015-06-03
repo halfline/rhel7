43143d7059d076d4d48c7831c213a2b3f2e369ac
Thomas Huth <thuth@redhat.com>
2015-06-03 16:41:07 -0400

    [kvm] ppc: book3s: pr: Emulate virtual timebase register
    Message-id: <1433349715-17265-3-git-send-email-thuth@redhat.com>
    Patchwork-id: 115258
    O-Subject: [RHEL7.2 kernel PATCH 02/50] KVM: PPC: BOOK3S: PR: Emulate virtual timebase register
    Bugzilla: 1226884 1227323
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    
    From: "Aneesh Kumar K.V" <aneesh.kumar@linux.vnet.ibm.com>
    
    virtual time base register is a per VM, per cpu register that needs
    to be saved and restored on vm exit and entry. Writing to VTB is not
    allowed in the privileged mode.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    [agraf: fix compile error]
    Signed-off-by: Alexander Graf <agraf@suse.de>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1226884
    (cherry picked from commit 8f42ab2749d00ea15157ab896cfbed73a247b3e1)
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -1196,6 +1196,15 @@
 				     : "r" ((unsigned long)(v)) \
 				     : "memory")
 
+static inline unsigned long mfvtb (void)
+{
+#ifdef CONFIG_PPC_BOOK3S_64
+	if (cpu_has_feature(CPU_FTR_ARCH_207S))
+		return mfspr(SPRN_VTB);
+#endif
+	return 0;
+}
+
 #ifdef __powerpc64__
 #ifdef CONFIG_PPC_CELL
 #define mftb()		({unsigned long rval;				\
