66969c0c2fa1f0447f4be8e44551c06fd93a0bb9
Tomoaki Nishimura <tonishim@redhat.com>
2015-06-04 14:48:39 -0400

    [mm] hwpoison: Fix wrong error recovery status
    Message-id: <1433429334-7489-8-git-send-email-tonishim@redhat.com>
    Patchwork-id: 115474
    O-Subject: [RHEL7.2 PATCH BZ1226196 07/22] RAS, HWPOISON: Fix wrong error recovery status
    Bugzilla: 1226196
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1226196
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9299977
    
    Backport of the following upstream commit:
    commit 6dc52cbe0b181818450fc1de4c0c850226ce0e68
    Author: Chen, Gong <gong.chen@linux.intel.com>
    Date:   Mon Jul 28 02:51:00 2014 -0400
    
        When Uncorrected error happens, if the poisoned page is referenced
        by more than one user after error recovery, the recovery is not
        successful. But currently the display result is wrong.
        Before this patch:
    
        MCE 0x44e336: dirty mlocked LRU page recovery: Recovered
        MCE 0x44e336: dirty mlocked LRU page still referenced by 1 users
        mce: Memory error not recovered
    
        After this patch:
    
        MCE 0x44e336: dirty mlocked LRU page recovery: Failed
        MCE 0x44e336: dirty mlocked LRU page still referenced by 1 users
        mce: Memory error not recovered
    
        Signed-off-by: Chen, Gong <gong.chen@linux.intel.com>
        Link: http://lkml.kernel.org/r/1406530260-26078-3-git-send-email-gong.chen@linux.intel.com
        Acked-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
        Acked-by: Tony Luck <tony.luck@intel.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -862,7 +862,6 @@ static int page_action(struct page_state *ps, struct page *p,
 	int count;
 
 	result = ps->action(p, pfn);
-	action_result(pfn, ps->msg, result);
 
 	count = page_count(p) - 1;
 	if (ps->action == me_swapcache_dirty && result == DELAYED)
@@ -873,6 +872,7 @@ static int page_action(struct page_state *ps, struct page *p,
 		       pfn, ps->msg, count);
 		result = FAILED;
 	}
+	action_result(pfn, ps->msg, result);
 
 	/* Could do more checks here if page looks ok */
 	/*
