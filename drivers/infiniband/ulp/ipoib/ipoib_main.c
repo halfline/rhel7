torvalds/linux

commit 5aa7add8f14bc29105bca841c053f574c8d232dd

infiniband/ipoib: implement ndo_get_iflink
Don't use dev->iflink anymore.

CC: Roland Dreier <roland@kernel.org>
Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -842,6 +842,13 @@ static void ipoib_set_mcast_list(struct net_device *dev)
 	queue_work(ipoib_workqueue, &priv->restart_task);
 }
 
+static int ipoib_get_iflink(const struct net_device *dev)
+{
+	struct ipoib_dev_priv *priv = netdev_priv(dev);
+
+	return priv->parent->ifindex;
+}
+
 static u32 ipoib_addr_hash(struct ipoib_neigh_hash *htbl, u8 *daddr)
 {
 	/*
@@ -1341,6 +1348,7 @@ static const struct net_device_ops ipoib_netdev_ops = {
 	.ndo_start_xmit	 	 = ipoib_start_xmit,
 	.ndo_tx_timeout		 = ipoib_timeout,
 	.ndo_set_rx_mode	 = ipoib_set_mcast_list,
+	.ndo_get_iflink		 = ipoib_get_iflink,
 };
 
 void ipoib_setup(struct net_device *dev)

drivers/infiniband/ulp/ipoib/ipoib_vlan.c
@@ -102,7 +102,6 @@ int __ipoib_vlan_add(struct ipoib_dev_priv *ppriv, struct ipoib_dev_priv *priv,
 	}
 
 	priv->child_type  = type;
-	priv->dev->iflink = ppriv->dev->ifindex;
 	list_add_tail(&priv->list, &ppriv->child_intfs);
 
 	return 0;

halfline/rhel7

commit c6575d4c2553d2232ced84c3a49ef8b342409862

[infiniband] ipoib: Fix ndo_get_iflink
Message-id: <d4744dc8d0dccfca19bce8f9db925767c390a158.1432060198.git.jbenc@redhat.com>
Patchwork-id: 112987
O-Subject: [RHEL7.2 net PATCH 35/52] IB/ipoib: Fix ndo_get_iflink
Bugzilla: 1210260
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: Alexander Duyck <alexander.h.duyck@redhat.com>
RH-Acked-by: Flavio Leitner <fbl@redhat.com>
RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1210260

commit 2c1539597419df6dfeb8ed774d60198b5830b8f7
Author: Erez Shitrit <erezsh@mellanox.com>
Date:   Thu Apr 16 16:34:34 2015 +0300

    IB/ipoib: Fix ndo_get_iflink

    Currently, iflink of the parent interface was always accessed, even
    when interface didn't have a parent and hence we crashed there.

    Handle the interface types properly: for a child interface, return
    the ifindex of the parent, for parent interface, return its ifindex.

    For child devices, make sure to set the parent pointer prior to
    invoking register_netdevice(), this allows the new ndo to be called
    by the stack immediately after the child device is registered.

    Fixes: 5aa7add8f14b ('infiniband/ipoib: implement ndo_get_iflink')
    Reported-by: Honggang Li <honli@redhat.com>
    Signed-off-by: Erez Shitrit <erezsh@mellanox.com>
    Signed-off-by: Honggang Li <honli@redhat.com>
    Reviewed-By: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>+
    Acked-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

Signed-off-by: Jiri Benc <jbenc@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -846,6 +846,11 @@ static int ipoib_get_iflink(const struct net_device *dev)
 {
 	struct ipoib_dev_priv *priv = netdev_priv(dev);
 
+	/* parent interface */
+	if (!test_bit(IPOIB_FLAG_SUBINTERFACE, &priv->flags))
+		return dev->ifindex;
+
+	/* child/vlan interface */
 	return priv->parent->ifindex;
 }
 

drivers/infiniband/ulp/ipoib/ipoib_vlan.c
@@ -58,6 +58,7 @@ int __ipoib_vlan_add(struct ipoib_dev_priv *ppriv, struct ipoib_dev_priv *priv,
 	/* MTU will be reset when mcast join happens */
 	priv->dev->mtu   = IPOIB_UD_MTU(priv->max_ib_mtu);
 	priv->mcast_mtu  = priv->admin_mtu = priv->dev->mtu;
+	priv->parent = ppriv->dev;
 	set_bit(IPOIB_FLAG_SUBINTERFACE, &priv->flags);
 
 	result = ipoib_set_dev_features(priv, ppriv->ca);
@@ -83,8 +84,6 @@ int __ipoib_vlan_add(struct ipoib_dev_priv *ppriv, struct ipoib_dev_priv *priv,
 		ipoib_warn(priv, "failed to initialize; error %i", result);
 		goto register_failed;
 	}
-
-	priv->parent = ppriv->dev;
 
 	ipoib_create_debug_files(priv->dev);
 