532b37dced3abf9bcf47bc37557a9eaa6ad3f9db
Ivan Vecera <ivecera@redhat.com>
2015-06-08 10:57:35 -0400

    [net] fix comment typo for __skb_alloc_pages()
    Message-id: <1433761072-10092-31-git-send-email-ivecera@redhat.com>
    Patchwork-id: 115738
    O-Subject: [RHEL7.2 PATCH 30/47] net: fix comment typo for __skb_alloc_pages()
    Bugzilla: 1200759
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: John Greene <jogreene@redhat.com>
    
    BZ: #1200759
    
    Upstream commit(s):
    commit bc6fc9fa0e1686d8a06aaa14005d3dbf7978d81f
    Author: Florian Fainelli <f.fainelli@gmail.com>
    Date:   Fri Aug 30 15:36:14 2013 +0100
    
        net: fix comment typo for __skb_alloc_pages()
    
        The name of the function in the comment is __skb_alloc_page() while we
        are actually commenting __skb_alloc_pages(). Fix this typo and make it
        a valid kernel doc comment.
    
        Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -2149,8 +2149,8 @@ static inline struct sk_buff *napi_alloc_skb(struct napi_struct *napi,
 	return __napi_alloc_skb(napi, length, GFP_ATOMIC);
 }
 
-/*
- *	__skb_alloc_page - allocate pages for ps-rx on a skb and preserve pfmemalloc data
+/**
+ *	__skb_alloc_pages - allocate pages for ps-rx on a skb and preserve pfmemalloc data
  *	@gfp_mask: alloc_pages_node mask. Set __GFP_NOMEMALLOC if not for network packet RX
  *	@skb: skb to set pfmemalloc on if __GFP_MEMALLOC is used
  *	@order: size of the allocation
