3c7c1f5ce93d18ff586fa35b417d7c29b6a35c4f
Paolo Bonzini <pbonzini@redhat.com>
2015-06-08 10:30:20 -0400

    [kvm] x86: clear hidden CPU state at reset time
    Message-id: <1433759435-30449-56-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 115699
    O-Subject: [RHEL7.2 PATCH 55/70] KVM: x86: clear hidden CPU state at reset time
    Bugzilla: 1202825
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Igor Mammedov <imammedo@redhat.com>
    
    This was noticed by Radim while reviewing the implementation of
    system management mode.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit e69fab5df45f993cb3b8cc0625a7791e86450a99)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -7122,6 +7122,8 @@ void kvm_arch_vcpu_destroy(struct kvm_vcpu *vcpu)
 
 void kvm_vcpu_reset(struct kvm_vcpu *vcpu)
 {
+	vcpu->arch.hflags = 0;
+
 	atomic_set(&vcpu->arch.nmi_queued, 0);
 	vcpu->arch.nmi_pending = 0;
 	vcpu->arch.nmi_injected = false;
