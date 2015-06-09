c3ec17a9698f46604f84b41aeadb6554011923f7
Sage Weil <sage@redhat.com>
2015-06-08 23:31:40 -0400

    [fs] ceph: fix atomic_open snapdir
    Message-id: <1433806338-11655-47-git-send-email-sage@redhat.com>
    Patchwork-id: 116029
    O-Subject: [RHEL 7.2 PATCH 46/84] ceph: fix atomic_open snapdir
    Bugzilla: 1229488
    RH-Acked-by: Zheng Yan <zyan@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Josh Durgin <jdurgin@redhat.com>
    RH-Acked-by: Mike Snitzer <snitzer@redhat.com>
    
    From: "Yan, Zheng" <zyan@redhat.com>
    
    ceph_handle_snapdir() checks ceph_mdsc_do_request()'s return value
    and creates snapdir inode if it's -ENOENT
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    (cherry picked from commit bf91c3150880ed6304f578cf00bd408d642fe6a0)
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/ceph/file.c
+++ fs/ceph/file.c
@@ -245,10 +245,10 @@ int ceph_atomic_open(struct inode *dir, struct dentry *dentry,
 	err = ceph_mdsc_do_request(mdsc,
 				   (flags & (O_CREAT|O_TRUNC)) ? dir : NULL,
 				   req);
+	err = ceph_handle_snapdir(req, dentry, err);
 	if (err)
 		goto out_err;
 
-	err = ceph_handle_snapdir(req, dentry, err);
 	if (err == 0 && (flags & O_CREAT) && !req->r_reply_info.head->is_dentry)
 		err = ceph_handle_notrace_create(dir, dentry);
 
