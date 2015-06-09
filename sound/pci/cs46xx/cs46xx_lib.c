c708fd8d519c81d951d8f4227b0bd093b0f3fa5a
Jaroslav Kysela <jkysela@redhat.com>
2015-06-09 15:12:18 -0400

    [alsa] Include linux/io.h instead of asm/io.h
    Message-id: <143386227049-408-patchset-jkysela@redhat.com>
    Patchwork-id: 116559
    O-Subject: [RHEL 7.2 PATCH 407/763] ALSA: Include linux/io.h instead of asm/io.h
    Bugzilla: 1197064
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Nowadays it's recommended.  Replace all in a shot.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    
    (picked from kernel: 6cbbfe1c8ddb6ac1322a87795dff9d8712c3e359)
    rhbz#1197064
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- sound/pci/cs46xx/cs46xx_lib.c
+++ sound/pci/cs46xx/cs46xx_lib.c
@@ -54,7 +54,7 @@
 #include <linux/gameport.h>
 #include <linux/mutex.h>
 #include <linux/export.h>
-
+#include <linux/io.h>
 
 #include <sound/core.h>
 #include <sound/control.h>
@@ -62,8 +62,6 @@
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include "cs46xx.h"
-
-#include <asm/io.h>
 
 #include "cs46xx_lib.h"
 #include "dsp_spos.h"
