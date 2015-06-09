ef9efc01b7c112bd0a9e4461fdefb0aa5020244a
Laurent Vivier <lvivier@redhat.com>
2015-06-09 10:15:32 -0400

    [powerpc] eeh: Create eeh_dev from pci_dn instead of device_node
    Message-id: <1433844943-12330-62-git-send-email-lvivier@redhat.com>
    Patchwork-id: 116134
    O-Subject: [RHEL-7.2 kernel PATCH v3 61/72] powerpc/eeh: Create eeh_dev from pci_dn instead of device_node
    Bugzilla: 1213675
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Gavin Shan <gwshan@linux.vnet.ibm.com>
    
    The patch adds function traverse_pci_dn(), which is similar to
    traverse_pci_devices() except it takes pci_dn, not device_node
    as parameter. The pci_dev.c has been reworked to create eeh_dev
    from pci_dn, instead of device_node.
    
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    (cherry picked from commit e8e9b34cef237d4d6fdc0d350cd8a95d1adb9ee9)
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1213675
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/include/asm/eeh.h
+++ arch/powerpc/include/asm/eeh.h
@@ -32,6 +32,7 @@
 struct pci_dev;
 struct pci_bus;
 struct device_node;
+struct pci_dn;
 
 #ifdef CONFIG_EEH
 
@@ -143,6 +144,7 @@ struct eeh_dev {
 	struct list_head list;		/* Form link list in the PE	*/
 	struct pci_controller *phb;	/* Associated PHB		*/
 	struct device_node *dn;		/* Associated device node	*/
+	struct pci_dn *pdn;		/* Associated PCI device node	*/
 	struct pci_dev *pdev;		/* Associated PCI device	*/
 	struct pci_bus *bus;		/* PCI bus for partial hotplug	*/
 	RH_KABI_EXTEND(int pcix_cap)	/* Saved PCIx capability	*/
@@ -153,6 +155,11 @@ struct eeh_dev {
 static inline struct device_node *eeh_dev_to_of_node(struct eeh_dev *edev)
 {
 	return edev ? edev->dn : NULL;
+}
+
+static inline struct pci_dn *eeh_dev_to_pdn(struct eeh_dev *edev)
+{
+	return edev ? edev->pdn : NULL;
 }
 
 static inline struct pci_dev *eeh_dev_to_pci_dev(struct eeh_dev *edev)
@@ -281,7 +288,7 @@ void eeh_pe_restore_bars(struct eeh_pe *pe);
 const char *eeh_pe_loc_get(struct eeh_pe *pe);
 struct pci_bus *eeh_pe_bus_get(struct eeh_pe *pe);
 
-void *eeh_dev_init(struct device_node *dn, void *data);
+void *eeh_dev_init(struct pci_dn *pdn, void *data);
 void eeh_dev_phb_init_dynamic(struct pci_controller *phb);
 int eeh_init(void);
 int __init eeh_ops_register(struct eeh_ops *ops);
@@ -332,7 +339,7 @@ static inline int eeh_init(void)
 	return 0;
 }
 
-static inline void *eeh_dev_init(struct device_node *dn, void *data)
+static inline void *eeh_dev_init(struct pci_dn *pdn, void *data)
 {
 	return NULL;
 }
