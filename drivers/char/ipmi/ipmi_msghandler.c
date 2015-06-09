0ce97f701a2fd1649108382a1b8f02de6cdf2e26
Tony Camuso <tcamuso@redhat.com>
2015-06-09 18:48:55 -0400

    [char] ipmi: Remove useless sysfs_name parameters
    Message-id: <1433875773-31976-6-git-send-email-tcamuso@redhat.com>
    Patchwork-id: 117038
    O-Subject: [PATCH RHEL7.2 05/43] ipmi: Remove useless sysfs_name parameters
    Bugzilla: 1229675
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1229675
    
    Cherry-picked from the following upstream commit.
    
    KABI: Removed hunks that deleted an arg from the arg list of the
          exported function ipmi_register_smi(). The arg is left in the arg
          list, for KABI compatibility, but is now unused. The other hunks
          that remove the references to this arg remain in the patch to keep
          as close as possible to upstream and to simplify future backports.
    
    commit 5a0e10ec4a82ec9e1ab9b85a0f2c2893f7ffda25
    Author: Corey Minyard <cminyard@mvista.com>
    Date:   Fri Oct 10 22:11:05 2014 -0500
    
        ipmi: Remove useless sysfs_name parameters
    
        It was always "bmc", so just hardcode it.  It makes no sense to
        pass that in.
    
        Signed-off-by: Corey Minyard <cminyard@mvista.com>
    
    Signed-off-by: Tony Camuso <tcamuso@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/char/ipmi/ipmi_msghandler.c
+++ drivers/char/ipmi/ipmi_msghandler.c
@@ -344,7 +344,6 @@ struct ipmi_smi {
 
 	struct bmc_device *bmc;
 	char *my_dev_name;
-	char *sysfs_name;
 
 	/*
 	 * This is the lower-layer's sender routine.  Note that you
@@ -2389,11 +2388,7 @@ static void ipmi_bmc_unregister(ipmi_smi_t intf)
 {
 	struct bmc_device *bmc = intf->bmc;
 
-	if (intf->sysfs_name) {
-		sysfs_remove_link(&intf->si_dev->kobj, intf->sysfs_name);
-		kfree(intf->sysfs_name);
-		intf->sysfs_name = NULL;
-	}
+	sysfs_remove_link(&intf->si_dev->kobj, "bmc");
 	if (intf->my_dev_name) {
 		sysfs_remove_link(&bmc->pdev.dev.kobj, intf->my_dev_name);
 		kfree(intf->my_dev_name);
@@ -2433,8 +2428,7 @@ out:
 	return err;
 }
 
-static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum,
-			     const char *sysfs_name)
+static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum)
 {
 	int               rv;
 	struct bmc_device *bmc = intf->bmc;
@@ -2505,6 +2499,7 @@ static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum,
 		bmc->pdev.id = bmc->id.device_id;
 		bmc->pdev.dev.release = release_bmc_device;
 		bmc->pdev.dev.type = &bmc_device_type;
+		kref_init(&bmc->usecount);
 
 		rv = platform_device_register(&bmc->pdev);
 		mutex_unlock(&ipmidriver_mutex);
@@ -2520,8 +2515,6 @@ static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum,
 			 */
 			return rv;
 		}
-
-		kref_init(&bmc->usecount);
 
 		rv = create_bmc_files(bmc);
 		if (rv) {
@@ -2543,20 +2536,8 @@ static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum,
 	 * create symlink from system interface device to bmc device
 	 * and back.
 	 */
-	intf->sysfs_name = kstrdup(sysfs_name, GFP_KERNEL);
-	if (!intf->sysfs_name) {
-		rv = -ENOMEM;
-		printk(KERN_ERR
-		       "ipmi_msghandler: allocate link to BMC: %d\n",
-		       rv);
-		goto out_err;
-	}
-
-	rv = sysfs_create_link(&intf->si_dev->kobj,
-			       &bmc->pdev.dev.kobj, intf->sysfs_name);
+	rv = sysfs_create_link(&intf->si_dev->kobj, &bmc->pdev.dev.kobj, "bmc");
 	if (rv) {
-		kfree(intf->sysfs_name);
-		intf->sysfs_name = NULL;
 		printk(KERN_ERR
 		       "ipmi_msghandler: Unable to create bmc symlink: %d\n",
 		       rv);
@@ -2565,8 +2546,6 @@ static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum,
 
 	intf->my_dev_name = kasprintf(GFP_KERNEL, "ipmi%d", ifnum);
 	if (!intf->my_dev_name) {
-		kfree(intf->sysfs_name);
-		intf->sysfs_name = NULL;
 		rv = -ENOMEM;
 		printk(KERN_ERR
 		       "ipmi_msghandler: allocate link from BMC: %d\n",
@@ -2577,8 +2556,6 @@ static int ipmi_bmc_register(ipmi_smi_t intf, int ifnum,
 	rv = sysfs_create_link(&bmc->pdev.dev.kobj, &intf->si_dev->kobj,
 			       intf->my_dev_name);
 	if (rv) {
-		kfree(intf->sysfs_name);
-		intf->sysfs_name = NULL;
 		kfree(intf->my_dev_name);
 		intf->my_dev_name = NULL;
 		printk(KERN_ERR
@@ -2908,7 +2885,7 @@ int ipmi_register_smi(struct ipmi_smi_handlers *handlers,
 	if (rv == 0)
 		rv = add_proc_entries(intf, i);
 
-	rv = ipmi_bmc_register(intf, i, sysfs_name);
+	rv = ipmi_bmc_register(intf, i);
 
  out:
 	if (rv) {
