f2e27a45f04b828e1d4e477511e54a843f661215
Gustavo Duarte <gduarte@redhat.com>
2015-06-09 14:09:40 -0400

    [powerpc] powernv: Add interfaces for flash device access
    Message-id: <1433858980-14457-1-git-send-email-gduarte@redhat.com>
    Patchwork-id: 116184
    O-Subject: [RHEL7.2 PATCH BZ 1229224] powerpc/powernv: Add interfaces for flash device access
    Bugzilla: 1221072 1229224
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    
    BZ 1229224
    Upstream Status: ed59190e41b725e1cfd79541f5fc66c20adb0671
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=9324913
    Tested: Sanity checks were done on ppc64 lpar and ppc64le kvm guest. Waiting
    for further test results from IBM.
    
    This patch depends on the patch set from BZ 1221072. Please apply those first.
    
    commit ed59190e41b725e1cfd79541f5fc66c20adb0671
    Author: Cyril Bur <cyrilbur@gmail.com>
    Date:   Wed Apr 1 14:05:30 2015 +0800
    
        powerpc/powernv: Add interfaces for flash device access
    
        This change adds the OPAL interface definitions to allow Linux to read,
        write and erase from system flash devices. We register platform devices
        for the flash devices exported by firmware.
    
        We clash with the existing opal_flash_init function, which is really for
        the FSP flash update functionality, so we rename that initcall to
        opal_flash_update_init().
    
        A future change will add an mtd driver that uses this interface.
    
        Changes from Joel Stanley and Jeremy Kerr.
    
        Signed-off-by: Cyril Bur <cyrilbur@gmail.com>
        Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
        Signed-off-by: Joel Stanley <joel@jms.id.au>
        Acked-by: Stewart Smith <stewart@linux.vnet.ibm.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/include/asm/opal.h
+++ arch/powerpc/include/asm/opal.h
@@ -168,6 +168,10 @@ struct opal_sg_list {
 #define OPAL_IPMI_SEND				107
 #define OPAL_IPMI_RECV				108
 #define OPAL_I2C_REQUEST			109
+#define OPAL_FLASH_READ				110
+#define OPAL_FLASH_WRITE			111
+#define OPAL_FLASH_ERASE			112
+#define OPAL_LAST				112
 
 /* Device tree flags */
 
@@ -1023,6 +1027,13 @@ int64_t opal_ipmi_recv(uint64_t interface, struct opal_ipmi_msg *msg,
 int64_t opal_i2c_request(uint64_t async_token, uint32_t bus_id,
 			 struct opal_i2c_request *oreq);
 
+int64_t opal_flash_read(uint64_t id, uint64_t offset, uint64_t buf,
+		uint64_t size, uint64_t token);
+int64_t opal_flash_write(uint64_t id, uint64_t offset, uint64_t buf,
+		uint64_t size, uint64_t token);
+int64_t opal_flash_erase(uint64_t id, uint64_t offset, uint64_t size,
+		uint64_t token);
+
 /* Internal functions */
 extern int early_init_dt_scan_opal(unsigned long node, const char *uname,
 				   int depth, void *data);
@@ -1055,7 +1066,7 @@ extern int opal_get_sensor_data(u32 sensor_hndl, u32 *sensor_data);
 struct rtc_time;
 extern unsigned long opal_get_boot_time(void);
 extern void opal_nvram_init(void);
-extern void opal_flash_init(void);
+extern void opal_flash_update_init(void);
 extern void opal_flash_term_callback(void);
 extern int opal_elog_init(void);
 extern void opal_platform_dump_init(void);
