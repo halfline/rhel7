82bfab3b939dd1abe409250937578dde68168153
Jason Wang <jasowang@redhat.com>
2015-06-12 09:40:33 -0400

    [virtio] virtio-scsi: enable VQs early on restore
    Message-id: <1434102153-8864-26-git-send-email-jasowang@redhat.com>
    Patchwork-id: 117931
    O-Subject: [RHEL7.2 PATCH V2 025/145] virtio_scsi: enable VQs early on restore
    Bugzilla: 1227339
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    From: "Michael S. Tsirkin" <mst@redhat.com>
    
    virtio spec requires drivers to set DRIVER_OK before using VQs.
    This is set automatically after restore returns, virtio scsi violated
    this rule on restore by kicking event vq within restore.
    
    To fix, call virtio_device_ready before using event queue.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    (cherry picked from commit 52c9cf1ac3d315995e9a65b900bc25e1d8a538b3)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/virtio_scsi.c
+++ drivers/scsi/virtio_scsi.c
@@ -1007,6 +1007,8 @@ static int virtscsi_restore(struct virtio_device *vdev)
 		return err;
 	}
 
+	virtio_device_ready(vdev);
+
 	if (virtio_has_feature(vdev, VIRTIO_SCSI_F_HOTPLUG))
 		virtscsi_kick_event_all(vscsi);
 
