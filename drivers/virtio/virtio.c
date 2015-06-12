aff02e6362f987ca3939121536181757101d7e6d
Jason Wang <jasowang@redhat.com>
2015-06-12 09:40:48 -0400

    [virtio] simplify feature bit handling
    Message-id: <1434102153-8864-41-git-send-email-jasowang@redhat.com>
    Patchwork-id: 117946
    O-Subject: [RHEL7.2 PATCH V2 040/145] virtio: simplify feature bit handling
    Bugzilla: 1227339
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    From: "Michael S. Tsirkin" <mst@redhat.com>
    
    Now that we use u64 for bits, we can simply & them together.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Reviewed-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    
    (cherry picked from commit c102659d690d382171bd2e40f35c5c811f0cdcac)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/virtio/virtio.c
+++ drivers/virtio/virtio.c
@@ -149,6 +149,7 @@ static int virtio_dev_probe(struct device *_d)
 	struct virtio_device *dev = dev_to_virtio(_d);
 	struct virtio_driver *drv = drv_to_virtio(dev->dev.driver);
 	u64 device_features;
+	u64 driver_features;
 	unsigned status;
 
 	/* We have a driver! */
@@ -157,14 +158,15 @@ static int virtio_dev_probe(struct device *_d)
 	/* Figure out what features the device supports. */
 	device_features = dev->config->get_features(dev);
 
-	/* Features supported by both device and driver into dev->features. */
-	dev->features = 0;
+	/* Figure out what features the driver supports. */
+	driver_features = 0;
 	for (i = 0; i < drv->feature_table_size; i++) {
 		unsigned int f = drv->feature_table[i];
 		BUG_ON(f >= 64);
-		if (device_features & (1ULL << f))
-			__virtio_set_bit(dev, f);
+		driver_features |= (1ULL << f);
 	}
+
+	dev->features = driver_features & device_features;
 
 	/* Transport features always preserved to pass to finalize_features. */
 	for (i = VIRTIO_TRANSPORT_F_START; i < VIRTIO_TRANSPORT_F_END; i++)
