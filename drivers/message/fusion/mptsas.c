9cf6bd1f6ccb0396040314ffa56a74dc485cf1fe
Tomas Henzl <thenzl@redhat.com>
2015-06-15 12:07:23 -0400

    [scsi] mptsas: fix depth param in scsi_track_queue_full
    Message-id: <1434370043-30377-1-git-send-email-thenzl@redhat.com>
    Patchwork-id: 118410
    O-Subject: [RHEL7.2 e-stor PATCH] mptsas: fix depth param in scsi_track_queue_full
    Bugzilla: 1117348
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Maurizio Lombardi <mlombard@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    
    A simple fix - in addition to the description from mainline patch
    there also is a change in the log level - to silence the
    unsurprising information.
    
    A user of scsi_track_queue_full should pass to the function a constant value
    untill the queue-depth changes, otherwise the internal logic in
    scsi_track_queue_full rejects the change.  Other users of this function use a
    'sdev->queue_depth - 1' as depth parameter, let's do the same.
    
    Signed-off-by: Tomas Henzl <thenzl@redhat.com>
    Acked-by: "Sreekanth Reddy" <Sreekanth.reddy@avagotech.com>
    Signed-off-by: James Bottomley <JBottomley@Odin.com>
    (cherry picked from commit 2865c073257eaf1ede043a4dbc77afc71b6de175)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/message/fusion/mptsas.c
+++ drivers/message/fusion/mptsas.c
@@ -4094,7 +4094,7 @@ mptsas_handle_queue_full_event(struct fw_event_work *fw_event)
 					continue;
 				}
 				depth = scsi_track_queue_full(sdev,
-				    current_depth - 1);
+					sdev->queue_depth - 1);
 				if (depth > 0)
 					sdev_printk(KERN_INFO, sdev,
 					"Queue depth reduced to (%d)\n",
@@ -4104,7 +4104,7 @@ mptsas_handle_queue_full_event(struct fw_event_work *fw_event)
 					"Tagged Command Queueing is being "
 					"disabled\n");
 				else if (depth == 0)
-					sdev_printk(KERN_INFO, sdev,
+					sdev_printk(KERN_DEBUG, sdev,
 					"Queue depth not changed yet\n");
 			}
 		}
