torvalds/linux

commit 886129a8eebebec260165741fe31421482371006

ACPI / video: change acpi-video brightness_switch_enabled default to 0
acpi-video is unique in that it not only generates brightness up/down
keypresses, but also (sometimes) actively changes the brightness itself.

This presents an inconsistent kernel interface to userspace, basically there
are 2 different scenarios, depending on the laptop model:

 1) On some laptops a brightness up/down keypress means: show a brightness osd
 with the current brightness, iow it is a brightness has changed notification.

 2) Where as on (a lot of) other laptops it means a brightness up/down key was
 pressed, deal with it.

Most of the desktop environments interpret any press as in scenario 2, and
change the brightness up / down as a response to the key events, causing it
to be changed twice, once by acpi-video and once by the DE.

With the new default for video.use_native_backlight we will be moving even
more laptops over to behaving as in scenario 2. Making the remaining laptops
even more of a weird exception. Also note that it is hard to detect scenario
1 properly in userspace, and AFAIK none of the DE-s deals with it.

Therefor this commit changes the default of brightness_switch_enabled to 0
making its behavior consistent with all the other backlight drivers.

Signed-off-by: Hans de Goede <hdegoede@redhat.com>
Reviewed-by: Aaron Lu <aaron.lu@intel.com>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

drivers/acpi/video.c
@@ -68,7 +68,7 @@ MODULE_AUTHOR("Bruno Ducrot");
 MODULE_DESCRIPTION("ACPI Video Driver");
 MODULE_LICENSE("GPL");
 
-static bool brightness_switch_enabled = 1;
+static bool brightness_switch_enabled;
 module_param(brightness_switch_enabled, bool, 0644);
 
 /*

halfline/rhel7

commit 614a7536f4691c60a2c9a518d30353457faded9f

[acpi] revert "acpi video: change acpi-video brightness_switch_enabled default to 0"
Message-id: <1433888934-1105-45-git-send-email-benjamin.tissoires@redhat.com>
Patchwork-id: 117225
O-Subject: [PATCH RHEL 7.2 44/67] Revert "ACPI / video: change acpi-video brightness_switch_enabled default to 0"
Bugzilla: 1218354
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1218354
Upstream: since v3.16
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9322964
Tested by me with a bunch of laptops (T400, T410, T440s, T540p, X1 carbon 3,
W541, T550s)

commit 2843768b701971ab10e62c77d5c75ad7c306f1bd
Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Date:   Mon Jul 14 19:35:45 2014 +0200

    Revert "ACPI / video: change acpi-video brightness_switch_enabled default to 0"

    This reverts commit 886129a8eebeb (ACPI / video: change acpi-video
    brightness_switch_enabled default to 0) as it is reported to cause
    problems to happen.

    Fixes: 886129a8eebeb (ACPI / video: change acpi-video brightness_switch_enabled default to 0)
    Link: http://marc.info/?l=linux-acpi&m=140534286826819&w=2
    Reported by: Bjrn Mork <bjorn@mork.no>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/acpi/video.c
@@ -69,7 +69,7 @@ MODULE_AUTHOR("Bruno Ducrot");
 MODULE_DESCRIPTION("ACPI Video Driver");
 MODULE_LICENSE("GPL");
 
-static bool brightness_switch_enabled;
+static bool brightness_switch_enabled = 1;
 module_param(brightness_switch_enabled, bool, 0644);
 
 /*