torvalds/linux

commit 0b8db271f1592e118feef7300f6da85bea9366da

ACPI / video: check _DOD list when creating backlight devices
The _DOD method lists which video output device is currently attached so
we should only care about them and ignore others. An user recently
reported that there are two acpi_video interfaces appeared on his system
and one of them doesn't work. From the acpidump, it is found that there
are more than one video output devices that have _BCM control method but
the _DOD lists only one of them. So this patch checks if the video output
device is in the _DOD list and will not create backlight device if it is
not in the list. Also, we consider the broken _DOD case(reflected by the
video->attached_count is 0) and do not change behaviour for those broken
_DOD systems.

Link: https://bugzilla.kernel.org/show_bug.cgi?id=84111
Reported-and-tested-by: ntrrgc@gmail.com
Signed-off-by: Aaron Lu <aaron.lu@intel.com>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

drivers/acpi/video.c
@@ -1146,6 +1146,23 @@ acpi_video_device_bind(struct acpi_video_bus *video,
 	}
 }
 
+static bool acpi_video_device_in_dod(struct acpi_video_device *device)
+{
+	struct acpi_video_bus *video = device->video;
+	int i;
+
+	/* If we have a broken _DOD, no need to test */
+	if (!video->attached_count)
+		return true;
+
+	for (i = 0; i < video->attached_count; i++) {
+		if (video->attached_array[i].bind_info == device)
+			return true;
+	}
+
+	return false;
+}
+
 /*
  *  Arg:
  *	video	: video bus device
@@ -1584,6 +1601,15 @@ static void acpi_video_dev_register_backlight(struct acpi_video_device *device)
 	int result;
 	static int count;
 	char *name;
+
+	/*
+	 * Do not create backlight device for video output
+	 * device that is not in the enumerated list.
+	 */
+	if (!acpi_video_device_in_dod(device)) {
+		dev_dbg(&device->dev->dev, "not in _DOD list, ignore\n");
+		return;
+	}
 
 	result = acpi_video_init_brightness(device);
 	if (result)

halfline/rhel7

commit 5749b526a79870dcdc787238fb5797c35f72ec13

[acpi] video: update condition to check if device is in _DOD list
Message-id: <1433888934-1105-60-git-send-email-benjamin.tissoires@redhat.com>
Patchwork-id: 117263
O-Subject: [PATCH RHEL 7.2 59/67] ACPI / video: update condition to check if device is in _DOD list
Bugzilla: 1218354
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1218354
Upstream: since v3.18
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9322964
Tested by me with a bunch of laptops (T400, T410, T440s, T540p, X1 carbon 3,
W541, T550s)

commit 35d0565b95547ec12d025dc9b1394f22968d113d
Author: Aaron Lu <aaron.lu@intel.com>
Date:   Mon Dec 1 02:09:18 2014 +0100

    ACPI / video: update condition to check if device is in _DOD list

    Commit 0b8db271f159 ("ACPI / video: check _DOD list when creating
    backlight devices") checks if the video device is in the bind devices
    list to decide if we should create backlight device for it, that causes
    problem for one Dell Latitude E6410, where none of the video output
    devices are properly bound due to the way how we did the comparing
    between its _ADR and the _DOD's values. Solve this problem by comparing
    the lower 12 bits of both the device's _ADR and the _DOD's values instead
    of relying on bind result.

    Fixes: 0b8db271f159 ("ACPI / video: check _DOD list when creating backlight devices")
    Reported-and-tested-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/acpi/video.c
@@ -1165,7 +1165,8 @@ static bool acpi_video_device_in_dod(struct acpi_video_device *device)
 		return true;
 
 	for (i = 0; i < video->attached_count; i++) {
-		if (video->attached_array[i].bind_info == device)
+		if ((video->attached_array[i].value.int_val & 0xfff) ==
+		    (device->device_id & 0xfff))
 			return true;
 	}
 