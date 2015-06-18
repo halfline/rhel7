9d3369d98a62bfd18b75cd096fc46b3c6029f017
Don Zickus <dzickus@redhat.com>
2015-06-17 21:19:20 -0400

    [usb] uas: Add US_FL_NO_REPORT_OPCODES for JMicron JMS566 with usb-id 0bc2:a013
    Message-id: <1434576081-13393-256-git-send-email-dzickus@redhat.com>
    Patchwork-id: 119794
    O-Subject: [RHEL7 PATCH 255/376] uas: Add US_FL_NO_REPORT_OPCODES for JMicron JMS566 with usb-id 0bc2:a013
    Bugzilla: 1232920
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1232920
    
    commit c6fa3945c8b5baf62f2e849104ecd6f3a1e5e407
    Author: Hans de Goede <hdegoede@redhat.com>
    Date:   Mon Dec 8 09:50:47 2014 +0100
    
        uas: Add US_FL_NO_REPORT_OPCODES for JMicron JMS566 with usb-id 0bc2:a013
    
        Like the JMicron JMS567 enclosures with the JMS566 choke on report-opcodes,
        so avoid it.
    
        Tested-and-reported-by: Takeo Nakayama <javhera@gmx.com>
        Cc: stable@vger.kernel.org # 3.16
        Signed-off-by: Hans de Goede <hdegoede@redhat.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/usb/storage/unusual_uas.h
+++ drivers/usb/storage/unusual_uas.h
@@ -104,6 +104,13 @@ UNUSUAL_DEV(0x174c, 0x5106, 0x0000, 0x9999,
 		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
 		US_FL_IGNORE_UAS),
 
+/* Reported-by: Takeo Nakayama <javhera@gmx.com> */
+UNUSUAL_DEV(0x357d, 0x7788, 0x0000, 0x9999,
+		"JMicron",
+		"JMS566",
+		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
+		US_FL_NO_REPORT_OPCODES),
+
 /* Reported-by: Hans de Goede <hdegoede@redhat.com> */
 UNUSUAL_DEV(0x2109, 0x0711, 0x0000, 0x9999,
 		"VIA",
