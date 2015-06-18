f0dd629ef707b36f154372936d315981078b2de6
Don Zickus <dzickus@redhat.com>
2015-06-17 21:17:48 -0400

    [usb] hub: rename khubd to hub_wq in documentation and comments
    Message-id: <1434576081-13393-164-git-send-email-dzickus@redhat.com>
    Patchwork-id: 119585
    O-Subject: [RHEL7 PATCH 163/376] usb: hub: rename khubd to hub_wq in documentation and comments
    Bugzilla: 1232920
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1232920
    
    commit 37ebb54915dc42944f6ae92fe53b9531c3903801
    Author: Petr Mladek <pmladek@suse.cz>
    Date:   Fri Sep 19 17:32:23 2014 +0200
    
        usb: hub: rename khubd to hub_wq in documentation and comments
    
        USB hub has started to use a workqueue instead of kthread. Let's update
        the documentation and comments here and there.
    
        This patch mostly just replaces "khubd" with "hub_wq". There are only few
        exceptions where the whole sentence was updated. These more complicated
        changes can be found in the following files:
    
        	   Documentation/usb/hotplug.txt
        	   drivers/net/usb/usbnet.c
        	   drivers/usb/core/hcd.c
        	   drivers/usb/host/ohci-hcd.c
        	   drivers/usb/host/xhci.c
    
        Signed-off-by: Petr Mladek <pmladek@suse.cz>
        Acked-by: Alan Stern <stern@rowland.harvard.edu>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -70,8 +70,9 @@
 // reawaken network queue this soon after stopping; else watchdog barks
 #define TX_TIMEOUT_JIFFIES	(5*HZ)
 
-// throttle rx/tx briefly after some faults, so khubd might disconnect()
-// us (it polls at HZ/4 usually) before we report too many false errors.
+/* throttle rx/tx briefly after some faults, so hub_wq might disconnect()
+ * us (it polls at HZ/4 usually) before we report too many false errors.
+ */
 #define THROTTLE_JIFFIES	(HZ/8)
 
 // between wakeups
@@ -592,9 +593,9 @@ static void rx_complete (struct urb *urb)
 			  "rx shutdown, code %d\n", urb_status);
 		goto block;
 
-	/* we get controller i/o faults during khubd disconnect() delays.
+	/* we get controller i/o faults during hub_wq disconnect() delays.
 	 * throttle down resubmits, to avoid log floods; just temporarily,
-	 * so we still recover when the fault isn't a khubd delay.
+	 * so we still recover when the fault isn't a hub_wq delay.
 	 */
 	case -EPROTO:
 	case -ETIME:
@@ -1182,8 +1183,9 @@ static void tx_complete (struct urb *urb)
 		case -ESHUTDOWN:		// hardware gone
 			break;
 
-		// like rx, tx gets controller i/o faults during khubd delays
-		// and so it uses the same throttling mechanism.
+		/* like rx, tx gets controller i/o faults during hub_wq
+		 * delays and so it uses the same throttling mechanism.
+		 */
 		case -EPROTO:
 		case -ETIME:
 		case -EILSEQ:
