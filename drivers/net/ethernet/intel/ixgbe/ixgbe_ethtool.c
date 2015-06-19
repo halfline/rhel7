0fad54bdb8e0e5ea20cc0aaceb006f8c27a8279a
Jiri Benc <jbenc@redhat.com>
2015-06-19 11:03:51 -0400

    [net] Replace u64_stats_fetch_begin_bh to u64_stats_fetch_begin_irq
    Message-id: <3ac247cd024d683d70f662f4b3406dbd7185e7d5.1434711656.git.jbenc@redhat.com>
    Patchwork-id: 120045
    O-Subject: [RHEL7.2 net PATCH 52/58] net: Replace u64_stats_fetch_begin_bh to u64_stats_fetch_begin_irq
    Bugzilla: 1200759
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1200759
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    - A bit different code at some places but this is a straightforward
      tree-wide conversion.
    
    commit 57a7744e09867ebcfa0ccf1d6d529caa7728d552
    Author: Eric W. Biederman <ebiederm@xmission.com>
    Date:   Thu Mar 13 21:26:42 2014 -0700
    
        net: Replace u64_stats_fetch_begin_bh to u64_stats_fetch_begin_irq
    
        Replace the bh safe variant with the hard irq safe variant.
    
        We need a hard irq safe variant to deal with netpoll transmitting
        packets from hard irq context, and we need it in most if not all of
        the places using the bh safe variant.
    
        Except on 32bit uni-processor the code is exactly the same so don't
        bother with a bh variant, just have a hard irq safe variant that
        everyone can use.
    
        Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_ethtool.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_ethtool.c
@@ -1096,10 +1096,10 @@ static void ixgbe_get_ethtool_stats(struct net_device *netdev,
 		}
 
 		do {
-			start = u64_stats_fetch_begin_bh(&ring->syncp);
+			start = u64_stats_fetch_begin_irq(&ring->syncp);
 			data[i]   = ring->stats.packets;
 			data[i+1] = ring->stats.bytes;
-		} while (u64_stats_fetch_retry_bh(&ring->syncp, start));
+		} while (u64_stats_fetch_retry_irq(&ring->syncp, start));
 		i += 2;
 #ifdef LL_EXTENDED_STATS
 		data[i] = ring->stats.yields;
@@ -1124,10 +1124,10 @@ static void ixgbe_get_ethtool_stats(struct net_device *netdev,
 		}
 
 		do {
-			start = u64_stats_fetch_begin_bh(&ring->syncp);
+			start = u64_stats_fetch_begin_irq(&ring->syncp);
 			data[i]   = ring->stats.packets;
 			data[i+1] = ring->stats.bytes;
-		} while (u64_stats_fetch_retry_bh(&ring->syncp, start));
+		} while (u64_stats_fetch_retry_irq(&ring->syncp, start));
 		i += 2;
 #ifdef LL_EXTENDED_STATS
 		data[i] = ring->stats.yields;
