torvalds/linux

commit 3df2f59f0aae2f5380a9d8037c91dcab75c4478d

hwmon: (ibmpowernv) pretty print labels
The new OPAL device tree adds a few properties which can be used to add
extra information on the sensor label.

In the case of a cpu core sensor, the firmware exposes the physical
identifier of the core in the "ibm,pir" property. The driver
translates this identifier in a linux cpu number and prints out a
range corresponding to the hardware threads of the core (as they
share the same sensor).

The numbering gives a hint on the localization of the core in the
system (which socket, which chip).

Signed-off-by: Cédric Le Goater <clg@fr.ibm.com>
Signed-off-by: Guenter Roeck <linux@roeck-us.net>

drivers/hwmon/ibmpowernv.c
@@ -30,6 +30,7 @@
 #include <linux/platform_device.h>
 #include <asm/opal.h>
 #include <linux/err.h>
+#include <asm/cputhreads.h>
 
 #define MAX_ATTR_LEN	32
 #define MAX_LABEL_LEN	64
@@ -112,13 +113,53 @@ static ssize_t show_label(struct device *dev, struct device_attribute *devattr,
 	return sprintf(buf, "%s\n", sdata->label);
 }
 
+static int __init get_logical_cpu(int hwcpu)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu)
+		if (get_hard_smp_processor_id(cpu) == hwcpu)
+			return cpu;
+
+	return -ENOENT;
+}
+
 static void __init make_sensor_label(struct device_node *np,
 				     struct sensor_data *sdata,
 				     const char *label)
 {
+	u32 id;
 	size_t n;
 
 	n = snprintf(sdata->label, sizeof(sdata->label), "%s", label);
+
+	/*
+	 * Core temp pretty print
+	 */
+	if (!of_property_read_u32(np, "ibm,pir", &id)) {
+		int cpuid = get_logical_cpu(id);
+
+		if (cpuid >= 0)
+			/*
+			 * The digital thermal sensors are associated
+			 * with a core. Let's print out the range of
+			 * cpu ids corresponding to the hardware
+			 * threads of the core.
+			 */
+			n += snprintf(sdata->label + n,
+				      sizeof(sdata->label) - n, " %d-%d",
+				      cpuid, cpuid + threads_per_core - 1);
+		else
+			n += snprintf(sdata->label + n,
+				      sizeof(sdata->label) - n, " phy%d", id);
+	}
+
+	/*
+	 * Membuffer pretty print
+	 */
+	if (!of_property_read_u32(np, "ibm,chip-id", &id))
+		n += snprintf(sdata->label + n, sizeof(sdata->label) - n,
+			      " %d", id & 0xffff);
 }
 
 static int get_sensor_index_attr(const char *name, u32 *index, char *attr)

halfline/rhel7

commit 490009de85bf126a1d97c1b0fcc4d2279c95fcf1

[hwmon] ibmpowernv: Fix build error seen for some configurations
Message-id: <1433369228-16410-17-git-send-email-gduarte@redhat.com>
Patchwork-id: 115434
O-Subject: [RHEL7.2 PATCH BZ 1224367 16/19] hwmon: (ibmpowernv) Fix build error seen for some configurations
Bugzilla: 1224367
RH-Acked-by: David Gibson <dgibson@redhat.com>
RH-Acked-by: Dean Nelson <dnelson@redhat.com>

BZ 1224367
Upstream Status: 8416915c16d4335980c5d5d6d39957adbfe40b55

commit 8416915c16d4335980c5d5d6d39957adbfe40b55
Author: Guenter Roeck <linux@roeck-us.net>
Date:   Wed Apr 8 21:02:47 2015 -0700

    hwmon: (ibmpowernv) Fix build error seen for some configurations

    Fix
    drivers/hwmon/ibmpowernv.c: In function 'get_logical_cpu':
    drivers/hwmon/ibmpowernv.c:121:3:
    	error: implicit declaration of function 'get_hard_smp_processor_id'

    seen for some configurations, possibly if SMP is not configured.

    Fixes: 3df2f59f0aae ("hwmon: (ibmpowernv) pretty print labels")
    Cc: Cdric Le Goater <clg@fr.ibm.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/hwmon/ibmpowernv.c
@@ -31,6 +31,7 @@
 #include <asm/opal.h>
 #include <linux/err.h>
 #include <asm/cputhreads.h>
+#include <asm/smp.h>
 
 #define MAX_ATTR_LEN	32
 #define MAX_LABEL_LEN	64