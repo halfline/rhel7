6da9dd5f2dd31479408be3646befa33d56a62139
Benjamin Coddington <bcodding@redhat.com>
2015-06-24 13:30:31 -0400

    [fs] nfs: fix dio deadlock when O_DIRECT flag is flipped
    Message-id: <c1482c44a2519714f8ebccc5f5e4b8076a663c97.1435150238.git.bcodding@redhat.com>
    Patchwork-id: 120701
    O-Subject: [RHEL7.2 PATCH fs BZ1234797 05/17] nfs: fix dio deadlock when O_DIRECT flag is flipped
    Bugzilla: 1234797
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Peng Tao <tao.peng@primarydata.com>
    
    We only support swap file calling nfs_direct_IO. However, application
    might be able to get to nfs_direct_IO if it toggles O_DIRECT flag
    during IO and it can deadlock because we grab inode->i_mutex in
    nfs_file_direct_write(). So return 0 for such case. Then the generic
    layer will fall back to buffer IO.
    
    Signed-off-by: Peng Tao <tao.peng@primarydata.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    
    Slight modifications to calling args and trailing dprintk since iov_itr
    work has not landed in RHEL7.
    Bugzilla: 1234797
    Upstream Id: ee8a1a8b160a87dc3a9c81a86796aa4db85ea815
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfs/direct.c
+++ fs/nfs/direct.c
@@ -256,6 +256,12 @@ static int nfs_direct_cmp_commit_data_verf(struct nfs_direct_req *dreq,
  */
 ssize_t nfs_direct_IO(int rw, struct kiocb *iocb, const struct iovec *iov, loff_t pos, unsigned long nr_segs)
 {
+	struct inode *inode = iocb->ki_filp->f_mapping->host;
+
+	/* we only support swap file calling nfs_direct_IO */
+	if (!IS_SWAPFILE(inode))
+		return 0;
+
 #ifndef CONFIG_NFS_SWAP
 	dprintk("NFS: nfs_direct_IO (%pD) off/no(%Ld/%lu) EINVAL\n",
 			iocb->ki_filp, (long long) pos, nr_segs);
