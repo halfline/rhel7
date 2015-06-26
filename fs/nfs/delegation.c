671e58df2e4c6e1f638a3158e90b60739ee148d7
Benjamin Coddington <bcodding@redhat.com>
2015-06-26 14:46:42 -0400

    [fs] nfsv4: Ensure we skip delegations that are already being returned
    Message-id: <9a9814f17fe010c2770216e33e447aea8d488019.1435329669.git.bcodding@redhat.com>
    Patchwork-id: 120786
    O-Subject: [RHEL7.2 PATCH fs BZ1206610 4/4] NFSv4: Ensure we skip delegations that are already being returned
    Bugzilla: 1206610
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Trond Myklebust <trond.myklebust@primarydata.com>
    
    In nfs_client_return_marked_delegations() and nfs_delegation_reap_unclaimed()
    we want to optimise the loop traversal by skipping delegations that are
    already in the process of being returned.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Upstream Id: ec3ca4e57e00d52ff724b0ae49f4489667a9c311
    Bugzilla: 1206610
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfs/delegation.c
+++ fs/nfs/delegation.c
@@ -421,6 +421,8 @@ static bool nfs_delegation_need_return(struct nfs_delegation *delegation)
 {
 	bool ret = false;
 
+	if (test_bit(NFS_DELEGATION_RETURNING, &delegation->flags))
+		goto out;
 	if (test_and_clear_bit(NFS_DELEGATION_RETURN, &delegation->flags))
 		ret = true;
 	if (test_and_clear_bit(NFS_DELEGATION_RETURN_IF_CLOSED, &delegation->flags) && !ret) {
@@ -432,6 +434,7 @@ static bool nfs_delegation_need_return(struct nfs_delegation *delegation)
 			ret = true;
 		spin_unlock(&delegation->lock);
 	}
+out:
 	return ret;
 }
 
@@ -803,6 +806,9 @@ restart:
 	list_for_each_entry_rcu(server, &clp->cl_superblocks, client_link) {
 		list_for_each_entry_rcu(delegation, &server->delegations,
 								super_list) {
+			if (test_bit(NFS_DELEGATION_RETURNING,
+						&delegation->flags))
+				continue;
 			if (test_bit(NFS_DELEGATION_NEED_RECLAIM,
 						&delegation->flags) == 0)
 				continue;
