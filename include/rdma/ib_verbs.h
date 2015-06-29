29f59a1b2b5cf7dc82e6e1ad64a1f964d248b761
Honggang Li <honli@redhat.com>
2015-06-29 12:03:37 -0400

    [infiniband] core: Change rdma_protocol_iboe to roce
    Message-id: <1435579444-27641-39-git-send-email-honli@redhat.com>
    Patchwork-id: 120840
    O-Subject: [RHEL-7.2 PATCH 38/65] IB/core: Change rdma_protocol_iboe to roce
    Bugzilla: 1229265
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    From: Ira Weiny <ira.weiny@intel.com>
    
    After discussion upstream, it was agreed to transition the usage of iboe
    in the kernel to roce.  This keeps our terminology consistent with what
    was finalized in the IBTA Annex 16 and IBTA Annex 17 publications.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit 5d9fb0440698a8b9e8595353d60cfac7ab30efae)
    Signed-off-by: Honggang Li <honli@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -1840,7 +1840,7 @@ static inline bool rdma_protocol_ib(struct ib_device *device, u8 port_num)
 	return device->port_immutable[port_num].core_cap_flags & RDMA_CORE_CAP_PROT_IB;
 }
 
-static inline bool rdma_protocol_iboe(struct ib_device *device, u8 port_num)
+static inline bool rdma_protocol_roce(struct ib_device *device, u8 port_num)
 {
 	return device->port_immutable[port_num].core_cap_flags & RDMA_CORE_CAP_PROT_ROCE;
 }
@@ -1850,7 +1850,7 @@ static inline bool rdma_protocol_iwarp(struct ib_device *device, u8 port_num)
 	return device->port_immutable[port_num].core_cap_flags & RDMA_CORE_CAP_PROT_IWARP;
 }
 
-static inline bool rdma_ib_or_iboe(struct ib_device *device, u8 port_num)
+static inline bool rdma_ib_or_roce(struct ib_device *device, u8 port_num)
 {
 	return device->port_immutable[port_num].core_cap_flags &
 		(RDMA_CORE_CAP_PROT_IB | RDMA_CORE_CAP_PROT_ROCE);
