ca08df94667819704c1411604be0d59a5d957941
Amir Vadai <avadai@redhat.com>
2015-07-01 11:46:03 -0400

    [netdrv] mlx4_core: Fix build failure introduced by the EQ pool changes
    Message-id: <3bf3f45b3e86d86b8bbc06a33550f4fd8dfb1a23.1435751116.git.avadai@redhat.com>
    Patchwork-id: 121310
    O-Subject: [Patch RHEL7.2 V2 245/317] net/mlx4_core: Fix build failure introduced by the EQ pool changes
    Bugzilla: 1164527 1164530 1164531 1164536 1164537
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Honggang Li <honli@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Doug Ledford <dledford@redhat.com>
    
    From: Or Gerlitz <ogerlitz@mellanox.com>
    
    When CONFIG_RFS_ACCEL or SMP aren't set, we fail to build, fix it.
    
    Also, avoid build warning as of unused function on that setup.
    
    Fixes: c66fa19c405a ('net/mlx4: Add EQ pool')
    Reported-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit db9777e376b0e038fc01d1361c7ea3eaf801c718)
    
    Signed-off-by: Amir Vadai <avadai@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/mellanox/mlx4/eq.c
+++ drivers/net/ethernet/mellanox/mlx4/eq.c
@@ -222,6 +222,7 @@ static void mlx4_slave_event(struct mlx4_dev *dev, int slave,
 	slave_event(dev, slave, eqe);
 }
 
+#if defined(CONFIG_SMP)
 static void mlx4_set_eq_affinity_hint(struct mlx4_priv *priv, int vec)
 {
 	int hint_err;
@@ -235,6 +236,7 @@ static void mlx4_set_eq_affinity_hint(struct mlx4_priv *priv, int vec)
 	if (hint_err)
 		mlx4_warn(dev, "irq_set_affinity_hint failed, err %d\n", hint_err);
 }
+#endif
 
 int mlx4_gen_pkey_eqe(struct mlx4_dev *dev, int slave, u8 port)
 {
@@ -1208,8 +1210,8 @@ int mlx4_init_eq_table(struct mlx4_dev *dev)
 					     MLX4_NUM_ASYNC_EQE + MLX4_NUM_SPARE_EQE,
 					     0, &priv->eq_table.eq[MLX4_EQ_ASYNC]);
 		} else {
-#ifdef CONFIG_RFS_ACCEL
 			struct mlx4_eq	*eq = &priv->eq_table.eq[i];
+#ifdef CONFIG_RFS_ACCEL
 			int port = find_first_bit(eq->actv_ports.ports,
 						  dev->caps.num_ports) + 1;
 
