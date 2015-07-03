8c67a20982b4c4daaa83bc6175a3207123d04332
Sabrina Dubroca <sdubroca@redhat.com>
2015-07-03 13:29:26 -0400

    [net] net_sched: return nla_nest_end() instead of skb->len
    Message-id: <1435930181-19698-14-git-send-email-sdubroca@redhat.com>
    Patchwork-id: 121529
    O-Subject: [RHEL7.2 net 13/28] net_sched: return nla_nest_end() instead of skb->len
    Bugzilla: 1235102
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    RH-Acked-by: Florian Westphal <fwestpha@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1235102
    Upstream Status: d59b7d8059ddc4f9ac1f0904d28ea62a252e8de7
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=9450079
    Tested: on beaker
    
    Change from upstream: we don't have net/sched/sch_hhf.c
    
    Upstream Commit:
    commit d59b7d8059ddc4f9ac1f0904d28ea62a252e8de7
    Author: Yang Yingliang <yangyingliang@huawei.com>
    Date:   Wed Mar 12 10:20:32 2014 +0800
    
        net_sched: return nla_nest_end() instead of skb->len
    
        nla_nest_end() already has return skb->len, so replace
        return skb->len with return nla_nest_end instead().
    
        Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sched/sch_cbq.c
+++ net/sched/sch_cbq.c
@@ -1562,8 +1562,7 @@ static int cbq_dump(struct Qdisc *sch, struct sk_buff *skb)
 		goto nla_put_failure;
 	if (cbq_dump_attr(skb, &q->link) < 0)
 		goto nla_put_failure;
-	nla_nest_end(skb, nest);
-	return skb->len;
+	return nla_nest_end(skb, nest);
 
 nla_put_failure:
 	nla_nest_cancel(skb, nest);
@@ -1598,8 +1597,7 @@ cbq_dump_class(struct Qdisc *sch, unsigned long arg,
 		goto nla_put_failure;
 	if (cbq_dump_attr(skb, cl) < 0)
 		goto nla_put_failure;
-	nla_nest_end(skb, nest);
-	return skb->len;
+	return nla_nest_end(skb, nest);
 
 nla_put_failure:
 	nla_nest_cancel(skb, nest);
