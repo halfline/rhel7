71bc66dec692f75f3857a67ca89f3166b0c819b3
Sabrina Dubroca <sdubroca@redhat.com>
2015-07-03 13:38:07 -0400

    [net] ipv4: rename ip_options_echo to __ip_options_echo()
    Message-id: <1435930695-19956-6-git-send-email-sdubroca@redhat.com>
    Patchwork-id: 121547
    O-Subject: [RHEL7.2 net 05/13] ipv4: rename ip_options_echo to __ip_options_echo()
    Bugzilla: 1235100
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    RH-Acked-by: Florian Westphal <fwestpha@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1235100
    Upstream Status: 24a2d43d8886f5a29c3cf108927f630c545a9a38
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=9454142
    Tested: on beaker
    
    Changes from upstream: conflict in include/net/ip.h because we don't
    have 5c3a0fd7d0fc ("ip*.h: Remove extern from function prototypes").
    
    Upstream Commit:
    commit 24a2d43d8886f5a29c3cf108927f630c545a9a38
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Sat Sep 27 09:50:55 2014 -0700
    
        ipv4: rename ip_options_echo to __ip_options_echo()
    
        ip_options_echo() assumes struct ip_options is provided in &IPCB(skb)->opt
        Lets break this assumption, but provide a helper to not change all call points.
    
        ip_send_unicast_reply() gets a new struct ip_options pointer.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -688,8 +688,9 @@ static void tcp_v4_send_reset(struct sock *sk, struct sk_buff *skb)
 		arg.bound_dev_if = sk->sk_bound_dev_if;
 
 	arg.tos = ip_hdr(skb)->tos;
-	ip_send_unicast_reply(net, skb, ip_hdr(skb)->saddr,
-			      ip_hdr(skb)->daddr, &arg, arg.iov[0].iov_len);
+	ip_send_unicast_reply(net, skb, &TCP_SKB_CB(skb)->header.h4.opt,
+			      ip_hdr(skb)->saddr, ip_hdr(skb)->daddr,
+			      &arg, arg.iov[0].iov_len);
 
 	TCP_INC_STATS_BH(net, TCP_MIB_OUTSEGS);
 	TCP_INC_STATS_BH(net, TCP_MIB_OUTRSTS);
@@ -771,8 +772,9 @@ static void tcp_v4_send_ack(struct sk_buff *skb, u32 seq, u32 ack,
 	if (oif)
 		arg.bound_dev_if = oif;
 	arg.tos = tos;
-	ip_send_unicast_reply(net, skb, ip_hdr(skb)->saddr,
-			      ip_hdr(skb)->daddr, &arg, arg.iov[0].iov_len);
+	ip_send_unicast_reply(net, skb, &TCP_SKB_CB(skb)->header.h4.opt,
+			      ip_hdr(skb)->saddr, ip_hdr(skb)->daddr,
+			      &arg, arg.iov[0].iov_len);
 
 	TCP_INC_STATS_BH(net, TCP_MIB_OUTSEGS);
 }
