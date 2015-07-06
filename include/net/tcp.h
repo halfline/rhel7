cb3930645b9527ee7f2f7a13564661a0b94c1614
Sabrina Dubroca <sdubroca@redhat.com>
2015-07-06 15:53:30 -0400

    [net] tcp: do not pace pure ack packets
    Message-id: <1436198010-18303-1-git-send-email-sdubroca@redhat.com>
    Patchwork-id: 121625
    O-Subject: [RHEL7.2 net v2 11/13] tcp: do not pace pure ack packets
    Bugzilla: 1235100
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    RH-Acked-by: Florian Westphal <fwestpha@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1235100
    Upstream Status: 9878196578286c5ed494778ada01da094377a686
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=9470633
    Tested: on beaker
    
    Changes from upstream: context conflict in include/net/tcp.h because
    we haven't moved tcp_v4_save_options there (upstream e25f866fbc8a).
    
    Upstream Commit:
    commit 9878196578286c5ed494778ada01da094377a686
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Tue Feb 3 18:31:53 2015 -0800
    
        tcp: do not pace pure ack packets
    
        When we added pacing to TCP, we decided to let sch_fq take care
        of actual pacing.
    
        All TCP had to do was to compute sk->pacing_rate using simple formula:
    
        sk->pacing_rate = 2 * cwnd * mss / rtt
    
        It works well for senders (bulk flows), but not very well for receivers
        or even RPC :
    
        cwnd on the receiver can be less than 10, rtt can be around 100ms, so we
        can end up pacing ACK packets, slowing down the sender.
    
        Really, only the sender should pace, according to its own logic.
    
        Instead of adding a new bit in skb, or call yet another flow
        dissection, we tweak skb->truesize to a small value (2), and
        we instruct sch_fq to use new helper and not pace pure ack.
    
        Note this also helps TCP small queue, as ack packets present
        in qdisc/NIC do not prevent sending a data packet (RPC workload)
    
        This helps to reduce tx completion overhead, ack packets can use regular
        sock_wfree() instead of tcp_wfree() which is a bit more expensive.
    
        This has no impact in the case packets are sent to loopback interface,
        as we do not coalesce ack packets (were we would detect skb->truesize
        lie)
    
        In case netem (with a delay) is used, skb_orphan_partial() also sets
        skb->truesize to 1.
    
        This patch is a combination of two patches we used for about one year at
        Google.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1701,4 +1701,19 @@ int tcpv4_offload_init(void);
 void tcp_v4_init(void);
 void tcp_init(void);
 
+/* locally generated TCP pure ACKs have skb->truesize == 2
+ * (check tcp_send_ack() in net/ipv4/tcp_output.c )
+ * This is much faster than dissecting the packet to find out.
+ * (Think of GRE encapsulations, IPv4, IPv6, ...)
+ */
+static inline bool skb_is_tcp_pure_ack(const struct sk_buff *skb)
+{
+	return skb->truesize == 2;
+}
+
+static inline void skb_set_tcp_pure_ack(struct sk_buff *skb)
+{
+	skb->truesize = 2;
+}
+
 #endif	/* _TCP_H */
