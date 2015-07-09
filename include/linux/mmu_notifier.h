febfacc49d8177cd01424717e47b54817174c1e2
Jerome Glisse <jglisse@redhat.com>
2015-07-09 18:27:01 -0400

    [mm] mmu_notifier: add new callback for mmu_notifier without breaking kabi
    Message-id: <1436466422-7367-4-git-send-email-jglisse@redhat.com>
    Patchwork-id: 121846
    O-Subject: [RHEL 7.2 PATCH 3/4 v3] mmu_notifier: add new callback for mmu_notifier without breaking kabi v2
    Bugzilla: 1210492
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    From: Jrme Glisse <jglisse@redhat.com>
    
    rhbz#1210492
    
    This differ from upstream because i can not grow struct mmu_notifier_ops
    thus i add a new rhel7 version of this struct with the new fields. This
    also require a new field to struct mmu_notifier_mm which is wraped inside
    a new struct mmu_notifier_mm_rhel7 and hidden inside mmu_notifier.c.
    
    I think this is the cleanest/simplest solution.
    
    Changed since v1:
      - Fix a cut and paste issue that caused mmu_notifier_release() to
        enter infinite loop.
    
    Signed-of-by: Jrme Glisse <jglisse@redhat.com>
    
    ----------------------------------------------------------------------
    commit 0f0a327fa12cd55de5e7f8c05a70ac3d047f405e
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Thu Nov 13 13:46:09 2014 +1100
    
        mmu_notifier: add the callback for mmu_notifier_invalidate_range()
    
        Now that the mmu_notifier_invalidate_range() calls are in place,
        add the callback to allow subsystems to register against it.
    
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
        Reviewed-by: Andrea Arcangeli <aarcange@redhat.com>
        Reviewed-by: Jrme Glisse <jglisse@redhat.com>
        Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
        Cc: Rik van Riel <riel@redhat.com>
        Cc: Hugh Dickins <hughd@google.com>
        Cc: Mel Gorman <mgorman@suse.de>
        Cc: Johannes Weiner <jweiner@redhat.com>
        Cc: Jay Cornwall <Jay.Cornwall@amd.com>
        Cc: Oded Gabbay <Oded.Gabbay@amd.com>
        Cc: Suravee Suthikulpanit <Suravee.Suthikulpanit@amd.com>
        Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
        Cc: David Woodhouse <dwmw2@infradead.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/mmu_notifier.h
+++ include/linux/mmu_notifier.h
@@ -7,7 +7,9 @@
 #include <linux/srcu.h>
 
 struct mmu_notifier;
+struct mmu_notifier_rhel7;
 struct mmu_notifier_ops;
+struct mmu_notifier_ops_rhel7;
 
 #ifdef CONFIG_MMU_NOTIFIER
 
@@ -95,11 +97,13 @@ struct mmu_notifier_ops {
 	/*
 	 * invalidate_range_start() and invalidate_range_end() must be
 	 * paired and are called only when the mmap_sem and/or the
-	 * locks protecting the reverse maps are held. The subsystem
-	 * must guarantee that no additional references are taken to
-	 * the pages in the range established between the call to
-	 * invalidate_range_start() and the matching call to
-	 * invalidate_range_end().
+	 * locks protecting the reverse maps are held. If the subsystem
+	 * can't guarantee that no additional references are taken to
+	 * the pages in the range, it has to implement the
+	 * invalidate_range() notifier from rhel7 interface (see below
+	 * for updated mmu_notifier_ops structure) to remove any
+	 * references taken after invalidate_range_start().
+	 *
 	 *
 	 * Invalidation of multiple concurrent ranges may be
 	 * optionally permitted by the driver. Either way the
@@ -143,6 +147,60 @@ struct mmu_notifier_ops {
 				     unsigned long start, unsigned long end);
 };
 
+struct mmu_notifier_ops_rhel7 {
+	void (*release)(struct mmu_notifier_rhel7 *mn,
+			struct mm_struct *mm);
+	int (*clear_flush_young)(struct mmu_notifier_rhel7 *mn,
+				 struct mm_struct *mm,
+				 unsigned long address);
+	int (*test_young)(struct mmu_notifier_rhel7 *mn,
+			  struct mm_struct *mm,
+			  unsigned long address);
+	void (*change_pte)(struct mmu_notifier_rhel7 *mn,
+			   struct mm_struct *mm,
+			   unsigned long address,
+			   pte_t pte);
+	void (*invalidate_page)(struct mmu_notifier_rhel7 *mn,
+				struct mm_struct *mm,
+				unsigned long address);
+	void (*invalidate_range_start)(struct mmu_notifier_rhel7 *mn,
+				       struct mm_struct *mm,
+				       unsigned long start, unsigned long end);
+	void (*invalidate_range_end)(struct mmu_notifier_rhel7 *mn,
+				     struct mm_struct *mm,
+				     unsigned long start, unsigned long end);
+	/*
+	 * invalidate_range() is either called between
+	 * invalidate_range_start() and invalidate_range_end() when the
+	 * VM has to free pages that where unmapped, but before the
+	 * pages are actually freed, or outside of _start()/_end() when
+	 * a (remote) TLB is necessary.
+	 *
+	 * If invalidate_range() is used to manage a non-CPU TLB with
+	 * shared page-tables, it not necessary to implement the
+	 * invalidate_range_start()/end() notifiers, as
+	 * invalidate_range() alread catches the points in time when an
+	 * external TLB range needs to be flushed.
+	 *
+	 * The invalidate_range() function is called under the ptl
+	 * spin-lock and not allowed to sleep.
+	 *
+	 * Note that this function might be called with just a sub-range
+	 * of what was passed to invalidate_range_start()/end(), if
+	 * called between those functions.
+	 */
+	void (*invalidate_range)(struct mmu_notifier_rhel7 *mn,
+				 struct mm_struct *mm,
+				 unsigned long start,
+				 unsigned long end);
+};
+
+struct mmu_notifier_rhel7 {
+	struct hlist_node hlist;
+	const struct mmu_notifier_ops_rhel7 *ops;
+};
+
+
 /*
  * The notifier chains are protected by mmap_sem and/or the reverse map
  * semaphores. Notifier chains are only changed when all reverse maps and
@@ -172,6 +230,12 @@ extern void mmu_notifier_unregister(struct mmu_notifier *mn,
 				    struct mm_struct *mm);
 extern void mmu_notifier_unregister_no_release(struct mmu_notifier *mn,
 					       struct mm_struct *mm);
+extern int mmu_notifier_register_rhel7(struct mmu_notifier_rhel7 *mn,
+				 struct mm_struct *mm);
+extern int __mmu_notifier_register_rhel7(struct mmu_notifier_rhel7 *mn,
+				   struct mm_struct *mm);
+extern void mmu_notifier_unregister_rhel7(struct mmu_notifier_rhel7 *mn,
+					  struct mm_struct *mm);
 extern void __mmu_notifier_mm_destroy(struct mm_struct *mm);
 extern void __mmu_notifier_release(struct mm_struct *mm);
 extern int __mmu_notifier_clear_flush_young(struct mm_struct *mm,
@@ -186,6 +250,9 @@ extern void __mmu_notifier_invalidate_range_start(struct mm_struct *mm,
 				  unsigned long start, unsigned long end);
 extern void __mmu_notifier_invalidate_range_end(struct mm_struct *mm,
 				  unsigned long start, unsigned long end);
+extern void __mmu_notifier_invalidate_range(struct mm_struct *mm,
+					    unsigned long start,
+					    unsigned long end);
 
 static inline void mmu_notifier_release(struct mm_struct *mm)
 {
@@ -240,6 +307,8 @@ static inline void mmu_notifier_invalidate_range_end(struct mm_struct *mm,
 static inline void mmu_notifier_invalidate_range(struct mm_struct *mm,
 				  unsigned long start, unsigned long end)
 {
+	if (mm_has_notifiers(mm))
+		__mmu_notifier_invalidate_range(mm, start, end);
 }
 
 static inline void mmu_notifier_mm_init(struct mm_struct *mm)
