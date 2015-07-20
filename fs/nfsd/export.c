5f8f4ab7989791c951532c4f10cc9a8b80f808eb
Steve Dickson <SteveD@redhat.com>
2015-07-20 16:16:16 -0400

    [fs] nfsd: add NFSEXP_PNFS to the exflags array
    Message-id: <1437408981-4459-22-git-send-email-steved@redhat.com>
    Patchwork-id: 122207
    O-Subject: [RHEL7.2 PATCH 21/26] nfsd: add NFSEXP_PNFS to the exflags array
    Bugzilla: 1195496
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    RH-Acked-by: Andreas Grünbacher <agruenba@redhat.com>
    
    From: Christoph Hellwig <hch@infradead.org>
    
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Upstream id: 9b3075c59f858d64478f46a15daa6ecda3cf2318
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -1159,6 +1159,7 @@ static struct flags {
 	{ NFSEXP_NOSUBTREECHECK, {"no_subtree_check", ""}},
 	{ NFSEXP_NOAUTHNLM, {"insecure_locks", ""}},
 	{ NFSEXP_V4ROOT, {"v4root", ""}},
+	{ NFSEXP_PNFS, {"pnfs", ""}},
 	{ 0, {"", ""}}
 };
 
