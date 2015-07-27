3d7d57af7940bfff699fac7f4293b2227062fd76
Steve Dickson <SteveD@redhat.com>
2015-07-27 00:48:28 -0400

    [fs] flexfilelayout: Mark the Flexfile layout driver as a tech preview
    Message-id: <1437958108-7708-2-git-send-email-steved@redhat.com>
    Patchwork-id: 122410
    O-Subject: [RHEL7.2 PATCH][BZ1246230] flexfilelayout: Mark the Flexfile layout driver as a tech preview
    Bugzilla: 1246230
    RH-Acked-by: Steven Whitehouse <swhiteho@redhat.com>
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    
    Signed-off-by: Steve Dickson <steved@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -1514,6 +1514,7 @@ static int __init nfs4flexfilelayout_init(void)
 {
 	printk(KERN_INFO "%s: NFSv4 Flexfile Layout Driver Registering...\n",
 	       __func__);
+	mark_tech_preview("NFSv4 Flexfile Layout Driver", NULL);
 	return pnfs_register_layoutdriver(&flexfilelayout_type);
 }
 
