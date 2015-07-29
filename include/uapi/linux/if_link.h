d1814e9ad6255e36069806c92f18fdfdacb8d1c0
John Greene <jogreene@redhat.com>
2015-07-29 16:05:35 -0400

    [net] bridge: add new brport flag LEARNING_SYNC
    Message-id: <1438185945-7065-4-git-send-email-jogreene@redhat.com>
    Patchwork-id: 122680
    O-Subject: [RHEL7.2 net bz1236709 v2 03/13] bridge: add new brport flag LEARNING_SYNC
    Bugzilla: 1236709
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    [redhat] IFLA_BRPORT_PROXYARP definitions added for other ixgbe features.
    These are added from net-next 958501163ddd6ea22a98f94fa0e7ce6d4734e5c4
    but doesn't fully implement that feature. Commented as such, and
    the name is flagged as RESERVED_IFLA_BRPORT_PROXYARP to note it's issue.
    
    Bugzilla: 1236709
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9415509
    Tested: In beaker. See BZ for details
    
    commit efacacdaf7cb5a0592ed772e3731636b2742e34a
    Author: Scott Feldman <sfeldma@gmail.com>
    Date:   Fri Nov 28 14:34:23 2014 +0100
    
        bridge: add new brport flag LEARNING_SYNC
    
        This policy flag controls syncing of learned FDB entries to bridge's FDB.  If
        on, FDB entries learned on bridge port device will be synced.  If off, device
        may still learn new FDB entries but they will not be synced with bridge's FDB.
    
        Signed-off-by: Scott Feldman <sfeldma@gmail.com>
        Signed-off-by: Jiri Pirko <jiri@resnulli.us>
        Acked-by: Roopa Prabhu <roopa@cumulusnetworks.com>
        Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
        Acked-by: Andy Gospodarek <gospo@cumulusnetworks.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/uapi/linux/if_link.h
+++ include/uapi/linux/if_link.h
@@ -233,6 +233,9 @@ enum {
 	IFLA_BRPORT_FAST_LEAVE,	/* multicast fast leave    */
 	IFLA_BRPORT_LEARNING,	/* mac learning */
 	IFLA_BRPORT_UNICAST_FLOOD, /* flood unicast traffic */
+	/* RHEL: IFLA_BRPORT_PROXYARP not fully supported yet */
+	RESERVED_IFLA_BRPORT_PROXYARP,	/* proxy ARP */
+	IFLA_BRPORT_LEARNING_SYNC, /* mac learning sync from device */
 	__IFLA_BRPORT_MAX
 };
 #define IFLA_BRPORT_MAX (__IFLA_BRPORT_MAX - 1)
