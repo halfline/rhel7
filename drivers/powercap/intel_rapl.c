0dc70fe85132083230bd4a059563255e5b8b7f9b
Steve Best <sbest@redhat.com>
2015-08-05 12:18:59 -0400

    [powercap] rapl: Add support for Broadwell-H
    Message-id: <20150805121859.21163.55533.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 123089
    O-Subject: [PATCH RHEL7.2 BZ1249855] powercap / RAPL: Add support for Broadwell-H
    Bugzilla: 1249855
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1249855
    
    Description:
    ------------
    
    This patch enables RAPL to support Broadwell-H
    
    Signed-off-by: Radivoje Jovanovic <radivoje.jovanovic@intel.com>
    
    Brew:
    -----
    http://brewweb.devel.redhat.com/brew/taskinfo?taskID=9638323
    
    Upstream:
    ---------
    posted here
    http://marc.info/?l=linux-kernel&m=143835547404833&w=2
    and should be accepted soon, posting now to try to make the beta
    
    Rafael: This is built on top of the patches post here
    http://post-office.corp.redhat.com/archives/rhkernel-list/2015-July/msg01894.html
    http://post-office.corp.redhat.com/archives/rhkernel-list/2015-August/msg00118.html
    
    Test Status:
    ------------
    Tested successfully by me using intel-denlow-r-01.khw which is cpu family 6 model 71.
    
    before the patch the driver doesn't load on the denlow system
    
    [root@intel-denlow-r-01 powercap]# insmod ./intel_rapl.ko
    [81633.430747] intel_rapl: driver does not support CPU family 6 model 71
    insmod: ERROR: could not insert module ./intel_rapl.ko: No such device
    
    after the patch the driver loads and works.
    
    ---------------------------------------------------------------
    Steve Best
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/powercap/intel_rapl.c
+++ drivers/powercap/intel_rapl.c
@@ -956,6 +956,7 @@ static const struct x86_cpu_id rapl_ids[] = {
 	{ X86_VENDOR_INTEL, 6, 0x3f},/* Haswell */
 	{ X86_VENDOR_INTEL, 6, 0x4f},/* Broadwell servers */
 	{ X86_VENDOR_INTEL, 6, 0x45},/* Haswell ULT */
+	{ X86_VENDOR_INTEL, 6, 0x47},/* Broadwell-H */
 	{ X86_VENDOR_INTEL, 6, 0x4E},/* Skylake */
 	{ X86_VENDOR_INTEL, 6, 0x56},/* Future Xeon */
 	{ X86_VENDOR_INTEL, 6, 0x57},/* Knights Landing */
