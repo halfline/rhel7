eb3cc61e5e656acc64e5f4d196eeb187b6f33772
John Greene <jogreene@redhat.com>
2015-08-20 19:57:11 -0400

    [netdrv] ixgbe: Remove NETIF_F_HW_VLAN_CTAG_FILTER from hw_features
    Message-id: <1440100662-9783-118-git-send-email-jogreene@redhat.com>
    Patchwork-id: 124412
    O-Subject: [RHEL7.2 bz1173786 v3 117/148] ixgbe: Remove NETIF_F_HW_VLAN_CTAG_FILTER from hw_features
    Bugzilla: 1173786
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Bugzilla: 1173786
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9415509
    Tested: In beaker. See BZ for details
    
    This change makes it so that the HW_VLAN_CTAG_FILTER bit is not falsely
    advertised as being a feature that can be toggled on ixgbe parts.  The
    driver was setting the bit in features and letting it be inherited by
    hw_features, however the driver was actually ignoring the value of the bit
    and just assuming it was always set.  As a result VLAN filtering was always
    enabled which is a requirement for SR-IOV, VMDq, DCB, FCoE, and possibly
    other features within the adapters.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@redhat.com>
    Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 87031c0df521d80af3b168bb2425a8faa999b629)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -7996,7 +7996,6 @@ skip_sriov:
 			   NETIF_F_IPV6_CSUM |
 			   NETIF_F_HW_VLAN_CTAG_TX |
 			   NETIF_F_HW_VLAN_CTAG_RX |
-			   NETIF_F_HW_VLAN_CTAG_FILTER |
 			   NETIF_F_TSO |
 			   NETIF_F_TSO6 |
 			   NETIF_F_RXHASH |
@@ -8018,6 +8017,7 @@ skip_sriov:
 	}
 
 	netdev->hw_features |= NETIF_F_RXALL;
+	netdev->features |= NETIF_F_HW_VLAN_CTAG_FILTER;
 
 	netdev->vlan_features |= NETIF_F_TSO;
 	netdev->vlan_features |= NETIF_F_TSO6;
