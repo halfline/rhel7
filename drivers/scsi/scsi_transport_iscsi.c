ff3707017123651c100b09d75f72feb17a38f96b
Chris Leech <cleech@redhat.com>
2015-09-21 21:24:51 -0400

    [scsi] iscsi: make mutex for target scanning and unbinding per-session
    Message-id: <1442870691-14816-1-git-send-email-cleech@redhat.com>
    Patchwork-id: 124979
    O-Subject: [RHEL-7.2 e-stor PATCH] iscsi: make mutex for target scanning and unbinding per-session
    Bugzilla: 1253032
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    Passsed directed testing for the BZ, regression testing began last week
    and I havn't heard anything bad.
    
    Upstream: posted http://marc.info/?t=144251383000001
    
    	From: Chris Leech <cleech@redhat.com>
    	Date: Tue, 15 Sep 2015 13:15:43 -0700
    	Subject: [PATCH] iscsi: make mutex for target scanning and unbinding
    	 per-session
    
    	Currently the iSCSI transport class synchronises target scanning and
    	unbinding with a host level mutex.  For multi-session hosts (offloading
    	iSCSI HBAs) connecting to storage arrays that may implement one
    	target-per-lun, this can result in the target scan work for hundreds of
    	sessions being serialized behind a single mutex.  With slow enough
    	response times, this can cause scan requests initiated from userspace to
    	block on the mutex long enough to trigger 120 sec hung task warnings.
    
    	I can't see any reason not to move this to a session level mutex and let
    	the target scans run in parallel, speeding up connecting to a large
    	number of targets.  Note that as iscsi_tcp creates a virtual host for
    	each session, software iSCSI is effectively doing this already.
    
    	Signed-off-by: Chris Leech <cleech@redhat.com>
    
    Signed-off-by: Chris Leech <cleech@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/scsi_transport_iscsi.c
+++ drivers/scsi/scsi_transport_iscsi.c
@@ -1569,7 +1569,6 @@ static int iscsi_setup_host(struct transport_container *tc, struct device *dev,
 
 	memset(ihost, 0, sizeof(*ihost));
 	atomic_set(&ihost->nr_scans, 0);
-	mutex_init(&ihost->mutex);
 
 	iscsi_bsg_host_add(shost, ihost);
 	/* ignore any bsg add error - we just can't do sgio */
@@ -1789,8 +1788,6 @@ static int iscsi_user_scan_session(struct device *dev, void *data)
 {
 	struct iscsi_scan_data *scan_data = data;
 	struct iscsi_cls_session *session;
-	struct Scsi_Host *shost;
-	struct iscsi_cls_host *ihost;
 	unsigned long flags;
 	unsigned int id;
 
@@ -1801,10 +1798,7 @@ static int iscsi_user_scan_session(struct device *dev, void *data)
 
 	ISCSI_DBG_TRANS_SESSION(session, "Scanning session\n");
 
-	shost = iscsi_session_to_shost(session);
-	ihost = shost->shost_data;
-
-	mutex_lock(&ihost->mutex);
+	mutex_lock(&session->mutex);
 	spin_lock_irqsave(&session->lock, flags);
 	if (session->state != ISCSI_SESSION_LOGGED_IN) {
 		spin_unlock_irqrestore(&session->lock, flags);
@@ -1823,7 +1817,7 @@ static int iscsi_user_scan_session(struct device *dev, void *data)
 	}
 
 user_scan_exit:
-	mutex_unlock(&ihost->mutex);
+	mutex_unlock(&session->mutex);
 	ISCSI_DBG_TRANS_SESSION(session, "Completed session scan\n");
 	return 0;
 }
@@ -1999,26 +1993,24 @@ static void __iscsi_unbind_session(struct work_struct *work)
 	struct iscsi_cls_session *session =
 			container_of(work, struct iscsi_cls_session,
 				     unbind_work);
-	struct Scsi_Host *shost = iscsi_session_to_shost(session);
-	struct iscsi_cls_host *ihost = shost->shost_data;
 	unsigned long flags;
 	unsigned int target_id;
 
 	ISCSI_DBG_TRANS_SESSION(session, "Unbinding session\n");
 
 	/* Prevent new scans and make sure scanning is not in progress */
-	mutex_lock(&ihost->mutex);
+	mutex_lock(&session->mutex);
 	spin_lock_irqsave(&session->lock, flags);
 	if (session->target_id == ISCSI_MAX_TARGET) {
 		spin_unlock_irqrestore(&session->lock, flags);
-		mutex_unlock(&ihost->mutex);
+		mutex_unlock(&session->mutex);
 		return;
 	}
 
 	target_id = session->target_id;
 	session->target_id = ISCSI_MAX_TARGET;
 	spin_unlock_irqrestore(&session->lock, flags);
-	mutex_unlock(&ihost->mutex);
+	mutex_unlock(&session->mutex);
 
 	if (session->ida_used)
 		ida_simple_remove(&iscsi_sess_ida, target_id);
@@ -2051,6 +2043,7 @@ iscsi_alloc_session(struct Scsi_Host *shost, struct iscsi_transport *transport,
 	INIT_WORK(&session->unbind_work, __iscsi_unbind_session);
 	INIT_WORK(&session->scan_work, iscsi_scan_session);
 	spin_lock_init(&session->lock);
+	mutex_init(&session->mutex);
 
 	/* this is released in the dev's release function */
 	scsi_host_get(shost);
