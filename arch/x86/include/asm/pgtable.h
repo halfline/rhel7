5679bb5cc9ad689d7f8613876231b949725dc5e7
Thomas Huth <thuth@redhat.com>
2015-10-08 10:45:05 -0400

    [mm] revert "remove remaining references to NUMA hinting bits and helpers"
    Message-id: <1444301110-1513-6-git-send-email-thuth@redhat.com>
    Patchwork-id: 125497
    O-Subject: [RHEL7.2 PATCH 05/10] Revert "[mm] remove remaining references to NUMA hinting bits and helpers"
    Bugzilla: 1256718
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    From: David Gibson <dgibson@redhat.com>
    
    This reverts commit 433d07b74b8e41aa1fcf77133dd447acbe058db1.
    
    This is part of NUMA balancing series from Mel Gorman.  It was backported
    as a prerequisite for the backport of e148315 for BZ 1217743.  However it
    causes other problems, so we've instead altered the backport of e148315 not
    to require this series.
    
    Upstream: Downstream only (reverted backport)
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1256718
    
    Signed-off-by: David Gibson <dgibson@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -292,7 +292,7 @@ static inline pmd_t pmd_mkwrite(pmd_t pmd)
 
 static inline pmd_t pmd_mknotpresent(pmd_t pmd)
 {
-	return pmd_clear_flags(pmd, _PAGE_PRESENT | _PAGE_PROTNONE);
+	return pmd_clear_flags(pmd, _PAGE_PRESENT);
 }
 
 /*
@@ -411,7 +411,8 @@ static inline int pte_same(pte_t a, pte_t b)
 
 static inline int pte_present(pte_t a)
 {
-	return pte_flags(a) & (_PAGE_PRESENT | _PAGE_PROTNONE);
+	return pte_flags(a) & (_PAGE_PRESENT | _PAGE_PROTNONE |
+			       _PAGE_NUMA);
 }
 
 #define pte_accessible pte_accessible
@@ -420,7 +421,7 @@ static inline bool pte_accessible(struct mm_struct *mm, pte_t a)
 	if (pte_flags(a) & _PAGE_PRESENT)
 		return true;
 
-	if ((pte_flags(a) & _PAGE_PROTNONE) &&
+	if ((pte_flags(a) & (_PAGE_PROTNONE | _PAGE_NUMA)) &&
 			tlb_flush_pending(mm))
 		return true;
 
@@ -440,7 +441,8 @@ static inline int pmd_present(pmd_t pmd)
 	 * the _PAGE_PSE flag will remain set at all times while the
 	 * _PAGE_PRESENT bit is clear).
 	 */
-	return pmd_flags(pmd) & (_PAGE_PRESENT | _PAGE_PROTNONE | _PAGE_PSE);
+	return pmd_flags(pmd) & (_PAGE_PRESENT | _PAGE_PROTNONE | _PAGE_PSE |
+				 _PAGE_NUMA);
 }
 
 #ifdef CONFIG_NUMA_BALANCING
@@ -515,6 +517,11 @@ static inline pte_t *pte_offset_kernel(pmd_t *pmd, unsigned long address)
 
 static inline int pmd_bad(pmd_t pmd)
 {
+#ifdef CONFIG_NUMA_BALANCING
+	/* pmd_numa check */
+	if ((pmd_flags(pmd) & (_PAGE_NUMA|_PAGE_PRESENT)) == _PAGE_NUMA)
+		return 0;
+#endif
 	return (pmd_flags(pmd) & ~_PAGE_USER) != _KERNPG_TABLE;
 }
 
