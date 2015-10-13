868f8123a179274b0dcdf1dc06f4b23aee36cd01
Radim Krcmar <rkrcmar@redhat.com>
2015-10-13 18:21:51 -0400

    [x86] kvm: fix edge EOI and IOAPIC reconfig race
    Message-id: <1444760511-32430-3-git-send-email-rkrcmar@redhat.com>
    Patchwork-id: 125659
    O-Subject: [RHEL7.2 kernel PATCH 2/2] KVM: x86: fix edge EOI and IOAPIC reconfig race
    Bugzilla: 1271333
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    
    KVM uses eoi_exit_bitmap to track vectors that need an action on EOI.
    The problem is that IOAPIC can be reconfigured while an interrupt with
    old configuration is pending and eoi_exit_bitmap only remembers the
    newest configuration;  thus EOI from the pending interrupt is not
    recognized.
    
    (Reconfiguration is not a problem for level interrupts, because IOAPIC
     sends interrupt with the new configuration.)
    
    For an edge interrupt with ACK notifiers, like i8254 timer; things can
    happen in this order
     1) IOAPIC inject a vector from i8254
     2) guest reconfigures that vector's VCPU and therefore eoi_exit_bitmap
        on original VCPU gets cleared
     3) guest's handler for the vector does EOI
     4) KVM's EOI handler doesn't pass that vector to IOAPIC because it is
        not in that VCPU's eoi_exit_bitmap
     5) i8254 stops working
    
    A simple solution is to set the IOAPIC vector in eoi_exit_bitmap if the
    vector is in PIR/IRR/ISR.
    
    This creates an unwanted situation if the vector is reused by a
    non-IOAPIC source, but I think it is so rare that we don't want to make
    the solution more sophisticated.  The simple solution also doesn't work
    if we are reconfiguring the vector.  (Shouldn't happen in the wild and
    I'd rather fix users of ACK notifiers instead of working around that.)
    
    The are no races because ioapic injection and reconfig are locked.
    
    Fixes: b053b2aef25d ("KVM: x86: Add EOI exit bitmap inference")
    [Before b053b2aef25d, this bug happened only with APICv.]
    Fixes: c7c9c56ca26f ("x86, apicv: add virtual interrupt delivery support")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Radim Krcmar <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit 7cc1abc51da42702e5ce9e616ecf55bdd7789ee6)
    
    Unlike upstream, we don't hit the bug without APICv because
    eoi_exit_bitmap only controls guest's exits.
    (Upstream also uses eoi_exit_bitmap in kvm_ioapic_handles_vector.)
    
    There are two context conflicts due to functionally unrelated patches,
    arch/x86/kvm/ioapic.c: upstream doesn't set TMR in kvm_ioapic_scan_entry
     since bdaffe1d93e7 ("KVM: x86: set TMR when the interrupt is accepted")
    arch/x86/kvm/x86.c: RHEL7 doesn't have an extra branch from b053b2aef25d
     ("KVM: x86: Add EOI exit bitmap inference")
    
    Signed-off-by: Radim Krcmar <rkrcmar@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6491,6 +6491,7 @@ static void vcpu_scan_ioapic(struct kvm_vcpu *vcpu)
 	memset(eoi_exit_bitmap, 0, 32);
 	memset(tmr, 0, 32);
 
+	kvm_x86_ops->sync_pir_to_irr(vcpu);
 	kvm_ioapic_scan_entry(vcpu, eoi_exit_bitmap, tmr);
 	kvm_x86_ops->load_eoi_exitmap(vcpu, eoi_exit_bitmap);
 	kvm_apic_update_tmr(vcpu, tmr);
