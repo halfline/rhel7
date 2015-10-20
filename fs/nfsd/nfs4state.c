e70d315c7fc617577e06582d35fd0ed68f096511
J. Bruce Fields <bfields@redhat.com>
2015-10-20 14:09:18 -0400

    [fs] nfsd: fix duplicated destroy_delegation code introduced by backport
    Message-id: <1445350158-26423-1-git-send-email-bfields@redhat.com>
    Patchwork-id: 125795
    O-Subject: [Patch RHEL7 fs] nfsd: fix duplicated destroy_delegation code introduced by backport
    Bugzilla: 1273228
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    
    Thanks for Zorro Lang <lang@redhat.com> for catching my backporting
    error here.
    
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Bugzilla: 1273228
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -726,8 +726,6 @@ static void destroy_delegation(struct nfs4_delegation *dp)
 	spin_lock(&state_lock);
 	unhashed = unhash_delegation_locked(dp);
 	spin_unlock(&state_lock);
-	nfs4_put_deleg_lease(dp->dl_stid.sc_file);
-	nfs4_put_stid(&dp->dl_stid);
 	if (unhashed) {
 		nfs4_put_deleg_lease(dp->dl_stid.sc_file);
 		nfs4_put_stid(&dp->dl_stid);
