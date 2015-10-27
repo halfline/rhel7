75f43714ae95a51bcc41992edcda7da83552ad95
Stanislav Kozina <skozina@redhat.com>
2015-10-27 14:17:50 -0400

    [x86] cpu: Fix trivial printk formatting issues with dmesg
    Message-id: <1445955470-17475-1-git-send-email-skozina@redhat.com>
    Patchwork-id: 125918
    O-Subject: [RHEL7.3 PATCH] x86, CPU: Fix trivial printk formatting issues with dmesg
    Bugzilla: 1232441
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1232441
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10017419
    Testing: Booting in a VM shows dmesg output separated for each line:
    [    0.140329] Last level iTLB entries: 4KB 0, 2MB 0, 4MB 0
    [    0.140756] Last level dTLB entries: 4KB 0, 2MB 0, 4MB 0
    [    0.141177] tlb_flushall_shift: 6
    
    Upstream commit:
    commit f94fe119f2e53362a3038ee856fa58412f728bc9
    Author: Steven Honeyman <stevenhoneyman@gmail.com>
    Date:   Wed Nov 5 22:52:18 2014 +0000
    
        x86, CPU: Fix trivial printk formatting issues with dmesg
    
        dmesg (from util-linux) currently has two methods for reading the kernel
        message ring buffer: /dev/kmsg and syslog(2). Since kernel 3.5.0 kmsg
        has been the default, which escapes control characters (e.g. new lines)
        before they are shown.
    
        This change means that when dmesg is using /dev/kmsg, a 2 line printk
        makes the output messy, because the second line does not get a
        timestamp.
    
        For example:
    
        [    0.012863] CPU0: Thermal monitoring enabled (TM1)
        [    0.012869] Last level iTLB entries: 4KB 1024, 2MB 1024, 4MB 1024
        Last level dTLB entries: 4KB 1024, 2MB 1024, 4MB 1024, 1GB 4
        [    0.012958] Freeing SMP alternatives memory: 28K (ffffffff81d86000 - ffffffff81d8d000)
        [    0.014961] dmar: Host address width 39
    
        Because printk.c intentionally escapes control characters, they should
        not be there in the first place. This patch fixes two occurrences of
        this.
    
        Signed-off-by: Steven Honeyman <stevenhoneyman@gmail.com>
        Link: https://lkml.kernel.org/r/1414856696-8094-1-git-send-email-stevenhoneyman@gmail.com
        [ Boris: make cpu_detect_tlb() static, while at it. ]
        Signed-off-by: Borislav Petkov <bp@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -506,18 +506,20 @@ u16 __read_mostly tlb_lld_4m[NR_INFO];
  */
 s8  __read_mostly tlb_flushall_shift = -1;
 
-void cpu_detect_tlb(struct cpuinfo_x86 *c)
+static void cpu_detect_tlb(struct cpuinfo_x86 *c)
 {
 	if (this_cpu->c_detect_tlb)
 		this_cpu->c_detect_tlb(c);
 
-	printk(KERN_INFO "Last level iTLB entries: 4KB %d, 2MB %d, 4MB %d\n" \
-		"Last level dTLB entries: 4KB %d, 2MB %d, 4MB %d\n"	     \
-		"tlb_flushall_shift: %d\n",
+	pr_info("Last level iTLB entries: 4KB %d, 2MB %d, 4MB %d\n",
 		tlb_lli_4k[ENTRIES], tlb_lli_2m[ENTRIES],
-		tlb_lli_4m[ENTRIES], tlb_lld_4k[ENTRIES],
-		tlb_lld_2m[ENTRIES], tlb_lld_4m[ENTRIES],
-		tlb_flushall_shift);
+		tlb_lli_4m[ENTRIES]);
+
+	pr_info("Last level dTLB entries: 4KB %d, 2MB %d, 4MB %d\n",
+		tlb_lld_4k[ENTRIES], tlb_lld_2m[ENTRIES],
+		tlb_lld_4m[ENTRIES]);
+
+	pr_info("tlb_flushall_shift: %d\n", tlb_flushall_shift);
 }
 
 void detect_ht(struct cpuinfo_x86 *c)
