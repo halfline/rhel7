3d729c7b68ac84552b8a3dfc9d39aa67ed1b0a8d
Paolo Abeni <pabeni@redhat.com>
2015-11-09 21:15:20 -0500

    [net] bridge: Pass net into br_nf_push_frag_xmit
    Message-id: <d873fdd7db4f1a0984ee7b74faa7da17efb50da6.1447088666.git.pabeni@redhat.com>
    Patchwork-id: 126499
    O-Subject: [RHEL7.3 net PATCH 62/64] bridge: Pass net into br_nf_push_frag_xmit
    Bugzilla: 1265259
    RH-Acked-by: Florian Westphal <fwestpha@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1265259
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10081936
    Tested on: local VM
    
    Upstream linux-net commit:
    commit 6532948b2e7b75895c1264967342ae61dc97b4a6
    Author: Eric W. Biederman <ebiederm@xmission.com>
    Date:   Tue Sep 15 20:04:13 2015 -0500
    
        bridge: Pass net into br_nf_push_frag_xmit
    
        When struct net starts being passed through the ipv4 and ipv6 fragment
        routines br_nf_push_frag_xmit will need to take a net parameter.
        Prepare br_nf_push_frag_xmit before that is needed and introduce
        br_nf_push_frag_xmit_sk for the call sites that still need the old
        calling conventions.
    
        Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/bridge/br_netfilter_hooks.c
+++ net/bridge/br_netfilter_hooks.c
@@ -676,7 +676,7 @@ static unsigned int br_nf_forward_arp(const struct nf_hook_ops *ops,
 }
 
 #if IS_ENABLED(CONFIG_NF_DEFRAG_IPV4) || IS_ENABLED(CONFIG_NF_DEFRAG_IPV6)
-static int br_nf_push_frag_xmit(struct sock *sk, struct sk_buff *skb)
+static int br_nf_push_frag_xmit(struct net *net, struct sock *sk, struct sk_buff *skb)
 {
 	struct brnf_frag_data *data;
 	int err;
@@ -699,6 +699,11 @@ static int br_nf_push_frag_xmit(struct sock *sk, struct sk_buff *skb)
 
 	nf_bridge_info_free(skb);
 	return br_dev_queue_push_xmit(sk, skb);
+}
+static int br_nf_push_frag_xmit_sk(struct sock *sk, struct sk_buff *skb)
+{
+	struct net *net = dev_net(skb_dst(skb)->dev);
+	return br_nf_push_frag_xmit(net, sk, skb);
 }
 #endif
 
@@ -768,7 +773,7 @@ static int br_nf_dev_queue_xmit(struct sock *sk, struct sk_buff *skb)
 		skb_copy_from_linear_data_offset(skb, -data->size, data->mac,
 						 data->size);
 
-		return br_nf_ip_fragment(net, sk, skb, br_nf_push_frag_xmit);
+		return br_nf_ip_fragment(net, sk, skb, br_nf_push_frag_xmit_sk);
 	}
 #endif
 #if IS_ENABLED(CONFIG_NF_DEFRAG_IPV6)
@@ -791,7 +796,7 @@ static int br_nf_dev_queue_xmit(struct sock *sk, struct sk_buff *skb)
 						 data->size);
 
 		if (v6ops)
-			return v6ops->fragment(sk, skb, br_nf_push_frag_xmit);
+			return v6ops->fragment(sk, skb, br_nf_push_frag_xmit_sk);
 
 		kfree_skb(skb);
 		return -EMSGSIZE;
