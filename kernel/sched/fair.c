4158e0919db960dbba7eb0a8aff9acb83a731ba5
Jiri Olsa <jolsa@redhat.com>
2015-11-25 14:41:16 -0500

    [kernel] sched: Shrink sg_lb_stats and play memset games
    Message-id: <1448462497-23895-8-git-send-email-jolsa@redhat.com>
    Patchwork-id: 127608
    O-Subject: [PATCH RHEL7 BZ1211784 07/28] sched/fair: Shrink sg_lb_stats and play memset games
    Bugzilla: 1211784
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1211784
    https://bugzilla.redhat.com/show_bug.cgi?id=1211784
    
    upstream
    ========
    commit 147c5fc2bad780d8093b547f2baa204e78107faf
    Author: Peter Zijlstra <peterz@infradead.org>
    Date: Mon Aug 19 15:22:57 2013 +0200
    
    RHEL7
    =====
    Only single leftover init_sd_lb_stats hunk. The rest of
    the changes was already picked up via:
      f851290a0cab [kernel] sched: monolithic code dump of what is being pushed
    
    description
    ===========
    We can shrink sg_lb_stats because rq::nr_running is an unsigned int
    and cpu numbers are 'int'
    
    Before:
      sgs:        /* size: 72, cachelines: 2, members: 10 */
      sds:        /* size: 184, cachelines: 3, members: 7 */
    
    After:
      sgs:        /* size: 56, cachelines: 1, members: 10 */
      sds:        /* size: 152, cachelines: 3, members: 7 */
    
    Further we can avoid clearing all of sds since we do a total
    clear/assignment of sg_stats in update_sg_lb_stats() with exception of
    busiest_stat.avg_load which is referenced in update_sd_pick_busiest().
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5385,11 +5385,19 @@ static inline void init_sd_lb_stats(struct sd_lb_stats *sds)
 	 * };
 	 *
 	 * Skimp on the clearing to avoid duplicate work. We can avoid clearing
-	 * this_stat because update_sg_lb_stats() does a full clear/assignment.
+	 * local_stat because update_sg_lb_stats() does a full clear/assignment.
 	 * We must however clear busiest_stat::avg_load because
 	 * update_sd_pick_busiest() reads this before assignment.
 	 */
-	memset(sds, 0, offsetof(struct sd_lb_stats, busiest_stat.group_load));
+	*sds = (struct sd_lb_stats){
+		.busiest = NULL,
+		.local = NULL,
+		.total_load = 0UL,
+		.total_pwr = 0UL,
+		.busiest_stat = {
+			.avg_load = 0UL,
+		},
+	};
 }
 
 /**
