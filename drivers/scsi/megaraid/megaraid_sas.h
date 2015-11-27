637980b4535f1c50d7a1191a29fde361b59b3926
Tomas Henzl <thenzl@redhat.com>
2015-11-27 13:51:58 -0500

    [scsi] megaraid_sas: touch version string
    Message-id: <1448632318-4331-3-git-send-email-thenzl@redhat.com>
    Patchwork-id: 127658
    O-Subject: [RHEL7.3 e-stor PATCH 2/2] megaraid_sas: touch version string
    Bugzilla: 1267911
    RH-Acked-by: Mikulas Patocka <mpatocka@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    RHEL specific
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -35,7 +35,7 @@
 /*
  * MegaRAID SAS Driver meta data
  */
-#define MEGASAS_VERSION				"06.807.10.00-rh1"
+#define MEGASAS_VERSION				"06.807.10.00-rh2"
 #define MEGASAS_RELDATE				"March 6, 2015"
 
 /*
