e3dce4df63aa8189e18a550282cd3b88bfb8aec5
Hendrik Brueckner <brueckner@redhat.com>
2015-12-02 13:23:27 -0500

    [s390] dasd: fix invalid PAV assignment after suspend/resume
    Message-id: <1449062609-21249-1-git-send-email-brueckner@redhat.com>
    Patchwork-id: 127955
    O-Subject: [RHEL7.3 PATCH] [s390] s390/dasd: fix invalid PAV assignment after suspend/resume
    Bugzilla: 1284022
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Description
    -----------
    commit 251afd69e3328653bae4a8ca09700971ca4000c5
    Author: Stefan Haberland <stefan.haberland@de.ibm.com>
    Commit: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
        s390/dasd: fix invalid PAV assignment after suspend/resume
    
        For a valid PAV assignment the DASD driver needs to notice possibly
        changed configuration data. Thus the failing of read configuration
        data should also fail the device restore to prevent invalid PAV
        assignment. The failed device may get restored after additional paths
        get available later on.
        If the restore fails after the device was added to the lcu alias
        handling it needs to be removed from the alias handling before exiting
        the restore function.
    
        Reviewed-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
        Signed-off-by: Stefan Haberland <stefan.haberland@de.ibm.com>
        Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Bugzilla
    --------
    BZ 1284022
    https://bugzilla.redhat.com/show_bug.cgi?id=1284022
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version v4.4-rc1
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=commitdiff;h=251afd69e3328653bae4a8ca09700971ca4000c5
    
    Brew
    ----
    https://brewweb.devel.redhat.com/taskinfo?taskID=10148039
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/s390/block/dasd_eckd.c
+++ drivers/s390/block/dasd_eckd.c
@@ -4347,7 +4347,12 @@ static int dasd_eckd_restore_device(struct dasd_device *device)
 	private = (struct dasd_eckd_private *) device->private;
 
 	/* Read Configuration Data */
-	dasd_eckd_read_conf(device);
+	rc = dasd_eckd_read_conf(device);
+	if (rc) {
+		DBF_EVENT_DEVID(DBF_WARNING, device->cdev,
+				"Read configuration data failed, rc=%d", rc);
+		goto out_err;
+	}
 
 	dasd_eckd_get_uid(device, &temp_uid);
 	/* Generate device unique id */
@@ -4363,13 +4368,18 @@ static int dasd_eckd_restore_device(struct dasd_device *device)
 	/* register lcu with alias handling, enable PAV if this is a new lcu */
 	rc = dasd_alias_make_device_known_to_lcu(device);
 	if (rc)
-		return rc;
+		goto out_err;
 
 	set_bit(DASD_CQR_FLAGS_FAILFAST, &cqr_flags);
 	dasd_eckd_validate_server(device, cqr_flags);
 
 	/* RE-Read Configuration Data */
-	dasd_eckd_read_conf(device);
+	rc = dasd_eckd_read_conf(device);
+	if (rc) {
+		DBF_EVENT_DEVID(DBF_WARNING, device->cdev,
+			"Read configuration data failed, rc=%d", rc);
+		goto out_err2;
+	}
 
 	/* Read Feature Codes */
 	dasd_eckd_read_features(device);
@@ -4380,7 +4390,7 @@ static int dasd_eckd_restore_device(struct dasd_device *device)
 	if (rc) {
 		DBF_EVENT_DEVID(DBF_WARNING, device->cdev,
 				"Read device characteristic failed, rc=%d", rc);
-		goto out_err;
+		goto out_err2;
 	}
 	spin_lock_irqsave(get_ccwdev_lock(device->cdev), flags);
 	memcpy(&private->rdc_data, &temp_rdc_data, sizeof(temp_rdc_data));
@@ -4391,6 +4401,8 @@ static int dasd_eckd_restore_device(struct dasd_device *device)
 
 	return 0;
 
+out_err2:
+	dasd_alias_disconnect_device_from_lcu(device);
 out_err:
 	return -1;
 }
