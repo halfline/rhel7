5e5bb5262d479e3120da0187d060bae9598770a1
Tomoaki Nishimura <tonishim@redhat.com>
2015-12-04 04:27:42 -0500

    [mm] mbind: add hugepage migration code to mbind()
    Message-id: <1449203289-10006-11-git-send-email-tonishim@redhat.com>
    Patchwork-id: 128356
    O-Subject: [RHEL7.3 PATCHv2 BZ1287322 10/37] mm: mbind: add hugepage migration code to mbind()
    Bugzilla: 1287322
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Jerome Marchand <jmarchan@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1287322
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10191956
    
    commit 74060e4d78795c7c43805133cb717d82533d4e0d
    Author: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Date:   Wed Sep 11 14:22:06 2013 -0700
    
        mm: mbind: add hugepage migration code to mbind()
    
        Extend do_mbind() to handle vma with VM_HUGETLB set.  We will be able to
        migrate hugepage with mbind(2) after applying the enablement patch which
        comes later in this series.
    
        Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
        Acked-by: Andi Kleen <ak@linux.intel.com>
        Reviewed-by: Wanpeng Li <liwanp@linux.vnet.ibm.com>
        Acked-by: Hillf Danton <dhillf@gmail.com>
        Cc: Mel Gorman <mgorman@suse.de>
        Cc: Hugh Dickins <hughd@google.com>
        Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
        Cc: Michal Hocko <mhocko@suse.cz>
        Cc: Rik van Riel <riel@redhat.com>
        Cc: "Aneesh Kumar K.V" <aneesh.kumar@linux.vnet.ibm.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Tomoaki Nishimura <t-nishimura@hf.jp.nec.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -280,6 +280,8 @@ struct huge_bootmem_page {
 };
 
 struct page *alloc_huge_page_node(struct hstate *h, int nid);
+struct page *alloc_huge_page_noerr(struct vm_area_struct *vma,
+				unsigned long addr, int avoid_reserve);
 
 /* arch callback */
 int __init alloc_bootmem_huge_page(struct hstate *h);
@@ -418,6 +420,7 @@ static inline bool hugepages_supported(void)
 #else	/* CONFIG_HUGETLB_PAGE */
 struct hstate {};
 #define alloc_huge_page_node(h, nid) NULL
+#define alloc_huge_page_noerr(v, a, r) NULL
 #define alloc_bootmem_huge_page(h) NULL
 #define hstate_file(f) NULL
 #define hstate_sizelog(s) NULL
