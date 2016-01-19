torvalds/linux

commit 0c3f061c195ceb891067b6de9e4ecc347c4dea31

of: implement of_node_to_nid as a weak function
Implement of_node_to_nid as weak function to remove the dependency on
asm/prom.h. This is in preparation to make prom.h optional.

Signed-off-by: Rob Herring <rob.herring@calxeda.com>
Acked-by: Grant Likely <grant.likely@linaro.org>

drivers/of/base.c
@@ -74,6 +74,13 @@ int of_n_size_cells(struct device_node *np)
 }
 EXPORT_SYMBOL(of_n_size_cells);
 
+#ifdef CONFIG_NUMA
+int __weak of_node_to_nid(struct device_node *np)
+{
+	return numa_node_id();
+}
+#endif
+
 #if defined(CONFIG_OF_DYNAMIC)
 /**
  *	of_node_get - Increment refcount of a node

include/linux/of.h
@@ -534,13 +534,10 @@ static inline const char *of_prop_next_string(struct property *prop,
 #define of_match_node(_matches, _node)	NULL
 #endif /* CONFIG_OF */
 
-#ifndef of_node_to_nid
-static inline int of_node_to_nid(struct device_node *np)
-{
-	return numa_node_id();
-}
-
-#define of_node_to_nid of_node_to_nid
+#if defined(CONFIG_OF) && defined(CONFIG_NUMA)
+extern int of_node_to_nid(struct device_node *np);
+#else
+static inline int of_node_to_nid(struct device_node *device) { return 0; }
 #endif
 
 /**

arch/powerpc/include/asm/prom.h
@@ -42,13 +42,6 @@ extern void kdump_move_device_tree(void);
 /* cache lookup */
 struct device_node *of_find_next_cache_node(struct device_node *np);
 
-#ifdef CONFIG_NUMA
-extern int of_node_to_nid(struct device_node *device);
-#else
-static inline int of_node_to_nid(struct device_node *device) { return 0; }
-#endif
-#define of_node_to_nid of_node_to_nid
-
 extern void of_instantiate_rtc(void);
 
 extern int of_get_ibm_chip_id(struct device_node *np);

arch/sparc/include/asm/prom.h
@@ -43,10 +43,6 @@ extern int of_getintprop_default(struct device_node *np,
 				 const char *name,
 				 int def);
 extern int of_find_in_proplist(const char *list, const char *match, int len);
-#ifdef CONFIG_NUMA
-extern int of_node_to_nid(struct device_node *dp);
-#define of_node_to_nid of_node_to_nid
-#endif
 
 extern void prom_build_devicetree(void);
 extern void of_populate_present_mask(void);

halfline/rhel7

commit 9d61b249b390071d2c82380f1d1514802bc84259

[of] return NUMA_NO_NODE from fallback of_node_to_nid()
Message-id: <1452016763-11528-1-git-send-email-cascardo@redhat.com>
Patchwork-id: 131666
O-Subject: [RHEL7.3 PATCH] of: return NUMA_NO_NODE from fallback of_node_to_nid()
Bugzilla: 1294398
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Aaron Conole <aconole@redhat.com>
RH-Acked-by: Gustavo Duarte <gduarte@redhat.com>

From: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1294398
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10277115

Tested on a patched qemu with a memoryless node 0.

    commit c8fff7bc5bba6bd59cad40441c189c4efe7190f6
    Author: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Date:   Wed Apr 8 19:59:20 2015 +0300

        of: return NUMA_NO_NODE from fallback of_node_to_nid()

        Node 0 might be offline as well as any other numa node,
        in this case kernel cannot handle memory allocation and crashes.

        Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
        Fixes: 0c3f061c195c ("of: implement of_node_to_nid as a weak function")
        Signed-off-by: Grant Likely <grant.likely@linaro.org>

Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@redhat.com>
Signed-off-by: Jiri Benc <jbenc@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/of/base.c
@@ -82,7 +82,7 @@ EXPORT_SYMBOL(of_n_size_cells);
 #ifdef CONFIG_NUMA
 int __weak of_node_to_nid(struct device_node *np)
 {
-	return numa_node_id();
+	return NUMA_NO_NODE;
 }
 #endif
 

include/linux/of.h
@@ -545,7 +545,10 @@ static inline int of_machine_is_compatible(const char *compat)
 #if defined(CONFIG_OF) && defined(CONFIG_NUMA)
 extern int of_node_to_nid(struct device_node *np);
 #else
-static inline int of_node_to_nid(struct device_node *device) { return 0; }
+static inline int of_node_to_nid(struct device_node *device)
+{
+	return NUMA_NO_NODE;
+}
 #endif
 
 /**