a3233a11101f960b49d082a4ea9b8dff4d52d188
Yasuaki Ishimatsu <yishimat@redhat.com>
2016-02-05 15:46:06 -0500

    [mm] memblock: allocate boot time data structures from mirrored memory
    Message-id: <56B4C3BE.20003@redhat.com>
    Patchwork-id: 133745
    O-Subject: [RHEL7.3 PATCH 15/20] mm/memblock: allocate boot time data structures from mirrored memory
    Bugzilla: 1270209
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1270209
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
      include/linux/memblock.h
        Due to missing e76b63f80d "memblock, numa: binary search node id",
        memblock_search_pfn_nid() is not defined. But the commit is not related
        to the feature. So I have not backported it and modified the patch
        for applying to RHEL7.
    
      mm/memblock.c:
        Due to missing 0a313a998a "mem-hotplug: let memblock skip the hotpluggable
        memory", there is no skip code in __next_mem_range() when memblock is
        hotpluggable. But the commit is not related to the feature. So I have not
        backported it and modified the patch for applying to RHEL7.
    
        Due to missing the commit 26f09e9b3a "mm/memblock: add memblock memory
        allocation apis", memblock_virt_alloc_internal() is not defined. But the
        commit is not related to the feature. So I have not backported it and
        skipped the change here.
    
    commit a3f5bafcc04aaf62990e0cf3ced1cc6d8dc6fe95
    Author: Tony Luck <tony.luck@intel.com>
    Date:   Wed Jun 24 16:58:12 2015 -0700
    
        mm/memblock: allocate boot time data structures from mirrored memory
    
        Try to allocate all boot time kernel data structures from mirrored
        memory.
    
        If we run out of mirrored memory print warnings, but fall back to using
        non-mirrored memory to make sure that we still boot.
    
        By number of bytes, most of what we allocate at boot time is the page
        structures.  64 bytes per 4K page on x86_64 ...  or about 1.5% of total
        system memory.  For workloads where the bulk of memory is allocated to
        applications this may represent a useful improvement to system
        availability since 1.5% of total memory might be a third of the memory
        allocated to the kernel.
    
        Signed-off-by: Tony Luck <tony.luck@intel.com>
        Cc: Xishi Qiu <qiuxishi@huawei.com>
        Cc: Hanjun Guo <guohanjun@huawei.com>
        Cc: Xiexiuqi <xiexiuqi@huawei.com>
        Cc: Ingo Molnar <mingo@elte.hu>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: "H. Peter Anvin" <hpa@zytor.com>
        Cc: Yinghai Lu <yinghai@kernel.org>
        Cc: Naoya Horiguchi <nao.horiguchi@gmail.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Yasuaki Ishimatsu <yishimat@redhat.com>

--- include/linux/memblock.h
+++ include/linux/memblock.h
@@ -23,6 +23,7 @@
 enum {
 	MEMBLOCK_NONE		= 0x0,	/* No special request */
 	MEMBLOCK_HOTPLUG	= 0x1,	/* hotpluggable region */
+	MEMBLOCK_MIRROR		= 0x2,	/* mirrored region */
 };
 
 struct memblock_region {
@@ -73,6 +74,8 @@ int memblock_reserve(phys_addr_t base, phys_addr_t size);
 void memblock_trim_memory(phys_addr_t align);
 int memblock_mark_hotplug(phys_addr_t base, phys_addr_t size);
 int memblock_clear_hotplug(phys_addr_t base, phys_addr_t size);
+int memblock_mark_mirror(phys_addr_t base, phys_addr_t size);
+ulong choose_memblock_flags(void);
 
 /* Low level functions */
 int memblock_add_range(struct memblock_type *type,
@@ -154,6 +157,11 @@ static inline bool movable_node_is_enabled(void)
 	return false;
 }
 #endif
+
+static inline bool memblock_is_mirror(struct memblock_region *m)
+{
+	return m->flags & MEMBLOCK_MIRROR;
+}
 
 #ifdef CONFIG_HAVE_MEMBLOCK_NODE_MAP
 void __next_mem_pfn_range(int *idx, int nid, unsigned long *out_start_pfn,
