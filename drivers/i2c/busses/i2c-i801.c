e99dc010fd2d7b84cad649c2df7adb880aaeea8d
Steve Best <sbest@redhat.com>
2016-02-10 15:46:07 -0500

    [i2c] Adding Intel Lewisburg support for iTCO
    Message-id: <20160210154607.757.37172.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 134161
    O-Subject: [PATCH RHEL7.3 BZ1273640 5/5] Adding Intel Lewisburg support for iTCO
    Bugzilla: 1273640
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1273640
    
    Description:
    ------------
    Starting from Intel Sunrisepoint (Skylake PCH) the iTCO watchdog
    resources have been moved to reside under the i801 SMBus host
    controller whereas previously they were under the LPC device.
    
    This patch adds Intel lewisburg SMBus support for iTCO device.
    
    Signed-off-by: Alexandra Yates <alexandra.yates@linux.intel.com>
    
    Upstream Status:
    ----------------
    posted here
    https://lkml.org/lkml/2016/2/4/851
    and should be accepted soon
    
    ----------------
    Steve Best
    
    Proposed Patch:
    ---------------
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/i2c/busses/i2c-i801.c
+++ drivers/i2c/busses/i2c-i801.c
@@ -1263,6 +1263,8 @@ static int i801_probe(struct pci_dev *dev, const struct pci_device_id *id)
 	switch (dev->device) {
 	case PCI_DEVICE_ID_INTEL_SUNRISEPOINT_H_SMBUS:
 	case PCI_DEVICE_ID_INTEL_SUNRISEPOINT_LP_SMBUS:
+	case PCI_DEVICE_ID_INTEL_LEWISBURG_SMBUS:
+	case PCI_DEVICE_ID_INTEL_LEWISBURG_SSKU_SMBUS:
 		priv->features |= FEATURE_I2C_BLOCK_READ;
 		priv->features |= FEATURE_IRQ;
 		priv->features |= FEATURE_SMBUS_PEC;
