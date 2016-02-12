add35a452e035f26801c6bf0390e159b1612265d
Vitaly Kuznetsov <vkuznets@redhat.com>
2016-02-12 15:58:58 -0500

    [x86] xen: Support kexec/kdump in HVM guests by doing a soft reset
    Message-id: <1455292738-17157-1-git-send-email-vkuznets@redhat.com>
    Patchwork-id: 134241
    O-Subject: [RHEL7.3 KERNEL PATCH] x86/xen: Support kexec/kdump in HVM guests by doing a soft reset
    Bugzilla: 1007328
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    RH-Acked-by: Igor Mammedov <imammedo@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1007328
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10473013
    Tested: RHEL7 guest over Xen-4.7(unstable) and RHEL5/Xen
    
    Upstream commit 0b34a166f291d255755be46e43ed5497cdd194f2
    Author: Vitaly Kuznetsov <vkuznets@redhat.com>
    Date:   Fri Sep 25 11:59:52 2015 +0200
    
        x86/xen: Support kexec/kdump in HVM guests by doing a soft reset
    
        Currently there is a number of issues preventing PVHVM Xen guests from
        doing successful kexec/kdump:
    
          - Bound event channels.
          - Registered vcpu_info.
          - PIRQ/emuirq mappings.
          - shared_info frame after XENMAPSPACE_shared_info operation.
          - Active grant mappings.
    
        Basically, newly booted kernel stumbles upon already set up Xen
        interfaces and there is no way to reestablish them. In Xen-4.7 a new
        feature called 'soft reset' is coming. A guest performing kexec/kdump
        operation is supposed to call SCHEDOP_shutdown hypercall with
        SHUTDOWN_soft_reset reason before jumping to new kernel. Hypervisor
        (with some help from toolstack) will do full domain cleanup (but
        keeping its memory and vCPU contexts intact) returning the guest to
        the state it had when it was first booted and thus allowing it to
        start over.
    
        Doing SHUTDOWN_soft_reset on Xen hypervisors which don't support it is
        probably OK as by default all unknown shutdown reasons cause domain
        destroy with a message in toolstack log: 'Unknown shutdown reason code
        5. Destroying domain.'  which gives a clue to what the problem is and
        eliminates false expectations.
    
        Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
        Cc: <stable@vger.kernel.org>
        Signed-off-by: David Vrabel <david.vrabel@citrix.com>
    
    RHEL-only part:
    
    1) CONFIG_KEXEC_CORE -> CONFIG_KEXEC (matches stable trees)
    2) Keep RHEL7 over RHEL5 Xen guests doing kdump the old way: don't do the
       SHUTDOWN_soft_reset call (as xend kills the guest) and fail detecting
       Xen after kdump. This will work only in case emulated devices were not
       ejected.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -34,6 +34,10 @@
 #include <linux/edd.h>
 #include <linux/crash_dump.h>
 
+#ifdef CONFIG_KEXEC
+#include <linux/kexec.h>
+#endif
+
 #include <xen/xen.h>
 #include <xen/events.h>
 #include <xen/interface/xen.h>
@@ -1704,6 +1708,21 @@ static struct notifier_block xen_hvm_cpu_notifier = {
 	.notifier_call	= xen_hvm_cpu_notify,
 };
 
+#ifdef CONFIG_KEXEC
+static void xen_hvm_shutdown(void)
+{
+	native_machine_shutdown();
+	if (kexec_in_progress)
+		xen_reboot(SHUTDOWN_soft_reset);
+}
+
+static void xen_hvm_crash_shutdown(struct pt_regs *regs)
+{
+	native_machine_crash_shutdown(regs);
+	xen_reboot(SHUTDOWN_soft_reset);
+}
+#endif
+
 static void __init xen_hvm_guest_init(void)
 {
 	init_hvm_pv_info();
@@ -1720,17 +1739,41 @@ static void __init xen_hvm_guest_init(void)
 	x86_init.irqs.intr_init = xen_init_IRQ;
 	xen_hvm_init_time_ops();
 	xen_hvm_init_mmu_ops();
+#ifdef CONFIG_KEXEC
+	if (xen_running_on_version_or_later(3, 2)) {
+		machine_ops.shutdown = xen_hvm_shutdown;
+		machine_ops.crash_shutdown = xen_hvm_crash_shutdown;
+	}
+#endif
 }
 
 static uint32_t __init xen_hvm_platform(void)
 {
+	uint32_t eax, ebx, ecx, edx, base;
+	int major, minor;
+
 	if (xen_pv_domain())
 		return 0;
 
-	if (is_kdump_kernel())
-		return 0;
+	/*
+	 * RHEL-only: old Xen versions (e.g. RHEL5 Xen) allow booting pure HVM
+	 * kernel after kdump but for newer versions this trick won't work and
+	 * we must reestablish PV interfaces if we ever used them. We hope that
+	 * crashing kernel did SHUTDOWN_soft_reset call and that this call is
+	 * supported by the hypervisor.
+	 */
 
-	return xen_cpuid_base();
+	base = xen_cpuid_base();
+
+	if (base && is_kdump_kernel()) {
+		cpuid(base + 1, &eax, &ebx, &ecx, &edx);
+		major = eax >> 16;
+		minor = eax & 0xffff;
+		if (major == 3 && minor < 2)
+			return 0;
+	}
+
+	return base;
 }
 
 bool xen_hvm_need_lapic(void)
