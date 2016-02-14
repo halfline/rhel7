e37d045436674dc0db68e0ab48483fb036e534be
Myron Stowe <myron.stowe@redhat.com>
2016-02-14 18:27:52 -0500

    [pci] Use pci_physfn() rather than looking up physfn by hand
    Message-id: <20160214182752.8391.3376.stgit@amt.stowe>
    Patchwork-id: 134329
    O-Subject: [RHEL7.3 PATCH 25/66] PCI: Use pci_physfn() rather than looking up physfn by hand
    Bugzilla: 1050021
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Laine Stump <laine@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1050021
    Upstream Status: c39127dbaf6c267f18d1cac14e2b0ecbfaa52d63
    
    commit c39127dbaf6c267f18d1cac14e2b0ecbfaa52d63
    Author: Bjorn Helgaas <bhelgaas@google.com>
    Date:   Fri Jul 17 15:38:13 2015 -0500
    
        PCI: Use pci_physfn() rather than looking up physfn by hand
    
        Use the pci_physfn() helper rather than looking up physfn by hand.
        No functional change.
    
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Reviewed-by: Joerg Roedel <jroedel@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/pci/ats.c
+++ drivers/pci/ats.c
@@ -42,6 +42,7 @@ void pci_ats_init(struct pci_dev *dev)
 int pci_enable_ats(struct pci_dev *dev, int ps)
 {
 	u16 ctrl;
+	struct pci_dev *pdev;
 
 	BUG_ON(dev->pci_dev_rh->ats_cap && dev->pci_dev_rh->ats_enabled);
 
@@ -57,8 +58,7 @@ int pci_enable_ats(struct pci_dev *dev, int ps)
 	 */
 	ctrl = PCI_ATS_CTRL_ENABLE;
 	if (dev->is_virtfn) {
-		struct pci_dev *pdev = dev->physfn;
-
+		pdev = pci_physfn(dev);
 		if (pdev->pci_dev_rh->ats_stu != ps)
 			return -EINVAL;
 
@@ -80,6 +80,7 @@ EXPORT_SYMBOL_GPL(pci_enable_ats);
  */
 void pci_disable_ats(struct pci_dev *dev)
 {
+	struct pci_dev *pdev;
 	u16 ctrl;
 
 	BUG_ON(!dev->pci_dev_rh->ats_cap || !dev->pci_dev_rh->ats_enabled);
@@ -88,8 +89,7 @@ void pci_disable_ats(struct pci_dev *dev)
 		return;		/* VFs still enabled */
 
 	if (dev->is_virtfn) {
-		struct pci_dev *pdev = dev->physfn;
-
+		pdev = pci_physfn(dev);
 		atomic_dec(&pdev->pci_dev_rh->ats_ref_cnt);
 	}
 
