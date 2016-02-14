47c309b3b7b28fcb84fd7173e68de8605cb2be0c
Myron Stowe <myron.stowe@redhat.com>
2016-02-14 18:28:22 -0500

    [iommu] amd: Implement add_device and remove_device
    Message-id: <20160214182822.8391.26064.stgit@amt.stowe>
    Patchwork-id: 134334
    O-Subject: [RHEL7.3 PATCH 30/66] iommu/amd: Implement add_device and remove_device
    Bugzilla: 1050021
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Laine Stump <laine@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1050021
    Upstream Status: aafd8ba0ca74894b9397e412bbd7f8ea2662ead8
    
    commit aafd8ba0ca74894b9397e412bbd7f8ea2662ead8
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Thu May 28 18:41:39 2015 +0200
    
        iommu/amd: Implement add_device and remove_device
    
        Implement these two iommu-ops call-backs to make use of the
        initialization and notifier features of the iommu core.
    
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/iommu/amd_iommu_init.c
+++ drivers/iommu/amd_iommu_init.c
@@ -225,6 +225,7 @@ static enum iommu_init_state init_state = IOMMU_START_STATE;
 
 static int amd_iommu_enable_interrupts(void);
 static int __init iommu_go_to_state(enum iommu_init_state state);
+static void init_device_table_dma(void);
 
 static inline void update_last_devid(u16 devid)
 {
@@ -1345,7 +1346,12 @@ static int __init amd_iommu_init_pci(void)
 			break;
 	}
 
-	ret = amd_iommu_init_devices();
+	init_device_table_dma();
+
+	for_each_iommu(iommu)
+		iommu_flush_all_caches(iommu);
+
+	amd_iommu_init_api();
 
 	print_iommu_info();
 
@@ -1785,8 +1791,6 @@ static bool __init check_ioapic_information(void)
 
 static void __init free_dma_resources(void)
 {
-	amd_iommu_uninit_devices();
-
 	free_pages((unsigned long)amd_iommu_pd_alloc_bitmap,
 		   get_order(MAX_DOMAIN_ID/8));
 
@@ -1979,27 +1983,10 @@ static bool detect_ivrs(void)
 
 static int amd_iommu_init_dma(void)
 {
-	struct amd_iommu *iommu;
-	int ret;
-
 	if (iommu_pass_through)
-		ret = amd_iommu_init_passthrough();
+		return amd_iommu_init_passthrough();
 	else
-		ret = amd_iommu_init_dma_ops();
-
-	if (ret)
-		return ret;
-
-	init_device_table_dma();
-
-	for_each_iommu(iommu)
-		iommu_flush_all_caches(iommu);
-
-	amd_iommu_init_api();
-
-	amd_iommu_init_notifier();
-
-	return 0;
+		return amd_iommu_init_dma_ops();
 }
 
 /****************************************************************************
