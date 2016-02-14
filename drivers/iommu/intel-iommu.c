04c425bc42b7ff39ad79588b3ff52ec2200c61c6
Myron Stowe <myron.stowe@redhat.com>
2016-02-14 18:08:26 -0500

    [iommu] Make IOVA domain low limit flexible
    Message-id: <20160214180826.7579.23304.stgit@amt.stowe>
    Patchwork-id: 134269
    O-Subject: [RHEL7.3 PATCH 21/29] iommu: Make IOVA domain low limit flexible
    Bugzilla: 1299036
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1299036
    Upstream Status: 1b72250076dde4276acecf3a7da722b185703e78
    
    commit 1b72250076dde4276acecf3a7da722b185703e78
    Author: Robin Murphy <robin.murphy@arm.com>
    Date:   Mon Jan 12 17:51:15 2015 +0000
    
        iommu: Make IOVA domain low limit flexible
    
        To share the IOVA allocator with other architectures, it needs to
        accommodate more general aperture restrictions; move the lower limit
        from a compile-time constant to a runtime domain property to allow
        IOVA domains with different requirements to co-exist.
    
        Also reword the slightly unclear description of alloc_iova since we're
        touching it anyway.
    
        Signed-off-by: Robin Murphy <robin.murphy@arm.com>
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -70,6 +70,9 @@
 				__DOMAIN_MAX_PFN(gaw), (unsigned long)-1))
 #define DOMAIN_MAX_ADDR(gaw)	(((uint64_t)__DOMAIN_MAX_PFN(gaw)) << VTD_PAGE_SHIFT)
 
+/* IO virtual address start page frame number */
+#define IOVA_START_PFN		(1)
+
 #define IOVA_PFN(addr)		((addr) >> PAGE_SHIFT)
 #define DMA_32BIT_PFN		IOVA_PFN(DMA_BIT_MASK(32))
 #define DMA_64BIT_PFN		IOVA_PFN(DMA_BIT_MASK(64))
@@ -1631,7 +1634,7 @@ static int dmar_init_reserved_ranges(void)
 	struct iova *iova;
 	int i;
 
-	init_iova_domain(&reserved_iova_list, DMA_32BIT_PFN);
+	init_iova_domain(&reserved_iova_list, IOVA_START_PFN, DMA_32BIT_PFN);
 
 	lockdep_set_class(&reserved_iova_list.iova_rbtree_lock,
 		&reserved_rbtree_key);
@@ -1689,7 +1692,7 @@ static int domain_init(struct dmar_domain *domain, int guest_width)
 	int adjust_width, agaw;
 	unsigned long sagaw;
 
-	init_iova_domain(&domain->iovad, DMA_32BIT_PFN);
+	init_iova_domain(&domain->iovad, IOVA_START_PFN, DMA_32BIT_PFN);
 	domain_reserve_special_ranges(domain);
 
 	/* calculate AGAW */
@@ -4298,7 +4301,7 @@ static int md_domain_init(struct dmar_domain *domain, int guest_width)
 {
 	int adjust_width;
 
-	init_iova_domain(&domain->iovad, DMA_32BIT_PFN);
+	init_iova_domain(&domain->iovad, IOVA_START_PFN, DMA_32BIT_PFN);
 	domain_reserve_special_ranges(domain);
 
 	/* calculate AGAW */
