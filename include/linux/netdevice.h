ed10732fdb9663d5ccdce7699f2a968cc6c5cd24
Phil Sutter <psutter@redhat.com>
2016-02-16 16:16:13 -0500

    [net] declare new net_device priv_flag IFF_NO_QUEUE
    Message-id: <1455639392-23267-2-git-send-email-psutter@redhat.com>
    Patchwork-id: 134782
    O-Subject: [RHEL7.3 net 01/20] net: declare new net_device priv_flag IFF_NO_QUEUE
    Bugzilla: 1152231
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Jakub Sitnicki <jkbs@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1152231
    Upstream Status: commit fa8187c96471c
    Conflicts: Context changes due to missing commits:
      - 2ad7bf3638411 ("ipvlan: Initial check-in of the IPVLAN driver.")
      - 4e3c89920cd3a ("net: Introduce VRF related flags and helpers")
    
    commit fa8187c96471c49419c25d4ec3299d17d3f274b2
    Author: Phil Sutter <phil@nwl.cc>
    Date:   Thu Aug 13 19:01:06 2015 +0200
    
        net: declare new net_device priv_flag IFF_NO_QUEUE
    
        This private net_device flag can be set by drivers to inform that a
        device runs fine without a qdisc attached. This was formerly done by
        setting tx_queue_len to zero.
    
        Signed-off-by: Phil Sutter <phil@nwl.cc>
        Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Phil Sutter <psutter@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1295,6 +1295,7 @@ struct net_device_ops {
  * @IFF_LIVE_ADDR_CHANGE: device supports hardware address
  *	change when it's running
  * @IFF_MACVLAN: Macvlan device
+ * @IFF_NO_QUEUE: device can run without qdisc attached
  * @IFF_RXFH_CONFIGURED: device has had Rx Flow indirection table configured
  */
 enum netdev_priv_flags {
@@ -1322,6 +1323,7 @@ enum netdev_priv_flags {
 	IFF_MACVLAN			= 1<<21,
 	IFF_XMIT_DST_RELEASE_PERM	= 1<<22,
 	IFF_RXFH_CONFIGURED		= 1<<25,
+	IFF_NO_QUEUE			= 1<<26,
 };
 
 #define IFF_802_1Q_VLAN			IFF_802_1Q_VLAN
@@ -1347,6 +1349,7 @@ enum netdev_priv_flags {
 #define IFF_LIVE_ADDR_CHANGE		IFF_LIVE_ADDR_CHANGE
 #define IFF_MACVLAN			IFF_MACVLAN
 #define IFF_XMIT_DST_RELEASE_PERM	IFF_XMIT_DST_RELEASE_PERM
+#define IFF_NO_QUEUE			IFF_NO_QUEUE
 #define IFF_RXFH_CONFIGURED		IFF_RXFH_CONFIGURED
 
 /*
