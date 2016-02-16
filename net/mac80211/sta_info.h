c4816291d381d24ac02204d42904c982cc3f47c9
Phil Sutter <psutter@redhat.com>
2016-02-15 21:44:55 -0500

    [lib] rhashtable: Do hashing inside of rhashtable_lookup_compare()
    Message-id: <1455572815-13933-10-git-send-email-psutter@redhat.com>
    Patchwork-id: 134749
    O-Subject: [RHEL7.3 net v2 009/129] rhashtable: Do hashing inside of rhashtable_lookup_compare()
    Bugzilla: 1238749
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    RH-Acked-by: Robert S Peterson <rpeterso@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1238749
    Upstream Status: commit 8d24c0b43125e
    MAC80211: Adjust compat code so it compiles. This probably breaks
              MAC80211 until the compat code is finally dropped.
    
    commit 8d24c0b43125ec26cc80e04588477a9a2afc025c
    Author: Thomas Graf <tgraf@suug.ch>
    Date:   Fri Jan 2 23:00:14 2015 +0100
    
        rhashtable: Do hashing inside of rhashtable_lookup_compare()
    
        Hash the key inside of rhashtable_lookup_compare() like
        rhashtable_lookup() does. This allows to simplify the hashing
        functions and keep them private.
    
        Signed-off-by: Thomas Graf <tgraf@suug.ch>
        Cc: netfilter-devel@vger.kernel.org
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Phil Sutter <psutter@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/mac80211/sta_info.h
+++ net/mac80211/sta_info.h
@@ -573,8 +573,9 @@ u32 sta_addr_hash(const void *key, u32 length, u32 seed);
 	/* compare address and run code only if it matches */		\
 	if (ether_addr_equal(_sta->sta.addr, (_addr)))
 #else
+/* XXX: This is broken, just compiles so rhashtable can be bisected */
 #define _sta_bucket_idx(_l, _tbl, _a)					\
-	_tbl->buckets[rhashtable_hashfn(&_l->sta_hash, _a, ETH_ALEN)]
+	_tbl->buckets[0]
 
 #define for_each_sta_info(local, tbl, _addr, _sta, _tmp)		\
 	(void) _tmp;  /* workaround unused variable error */		\
