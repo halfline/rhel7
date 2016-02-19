b82a4a682c6232b03873b8315c65f1e98d6dc9c0
Gustavo Duarte <gduarte@redhat.com>
2016-02-19 14:59:07 -0500

    [powerpc] pasemi: Only the build the pasemi MSI code for PASEMI=y
    Message-id: <1455893954-28433-40-git-send-email-gduarte@redhat.com>
    Patchwork-id: 135019
    O-Subject: [RHEL7.3 PATCH BZ 1275657 39/46] powerpc/pasemi: Only the build the pasemi MSI code for PASEMI=y
    Bugzilla: 1275657
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ 1275657
    Upstream Status: 5af7a6f3e2d015dcaaeffa48c6d47238415cbe66
    
    Backport note: the removed file arch/powerpc/sysdev/mpic_pasemi_msi.c is
      slightly different. This is because commits :
    
      280510f1060b "PCI/MSI: Rename mask/unmask_msi_irq treewide"
      83a18912b0e8 "PCI/MSI: Rename write_msi_msg() to pci_write_msi_msg()"
    
      modify calls to :
    
    	pci_msi_mask_irq(data);
    	pci_write_msi_msg(virq, &msg);
    
       and they are not part of the backport. However, the added file is
       the same as upstream.
    Signed-off-by: Cdric Le Goater <clg@fr.ibm.com>
    
    commit 5af7a6f3e2d015dcaaeffa48c6d47238415cbe66
    Author: Michael Ellerman <mpe@ellerman.id.au>
    Date:   Fri Apr 10 11:52:06 2015 +1000
    
        powerpc/pasemi: Only the build the pasemi MSI code for PASEMI=y
    
        The pasemi MSI code is currently always built when MPIC=y && PCI_MSI=y.
        It should not have any effect on other platforms, because it immediately
        checks the MPIC's compatible property for "pasemi,pwrficient-openpic".
    
        However it's odd that it's still built even when PASEMI=n. It also
        needn't be in sysdev, as it's only used by pasemi. So move it into
        platforms/pasemi, whereby it will only be built for PASEMI=y.
    
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/platforms/pasemi/msi.c
+++ arch/powerpc/platforms/pasemi/msi.c
@@ -13,10 +13,7 @@
  *
  */
 
-#undef DEBUG
-
 #include <linux/irq.h>
-#include <linux/bootmem.h>
 #include <linux/msi.h>
 #include <asm/mpic.h>
 #include <asm/prom.h>
@@ -24,7 +21,7 @@
 #include <asm/ppc-pci.h>
 #include <asm/msi_bitmap.h>
 
-#include "mpic.h"
+#include <sysdev/mpic.h>
 
 /* Allocate 16 interrupts per device, to give an alignment of 16,
  * since that's the size of the grouping w.r.t. affinity. If someone
@@ -42,7 +39,7 @@ static struct mpic *msi_mpic;
 static void mpic_pasemi_msi_mask_irq(struct irq_data *data)
 {
 	pr_debug("mpic_pasemi_msi_mask_irq %d\n", data->irq);
-	mask_msi_irq(data);
+	pci_msi_mask_irq(data);
 	mpic_mask_irq(data);
 }
 
@@ -50,7 +47,7 @@ static void mpic_pasemi_msi_unmask_irq(struct irq_data *data)
 {
 	pr_debug("mpic_pasemi_msi_unmask_irq %d\n", data->irq);
 	mpic_unmask_irq(data);
-	unmask_msi_irq(data);
+	pci_msi_unmask_irq(data);
 }
 
 static struct irq_chip mpic_pasemi_msi_chip = {
@@ -136,7 +133,7 @@ static int pasemi_msi_setup_msi_irqs(struct pci_dev *pdev, int nvec, int type)
 		 * register to generate MSI [512...1023]
 		 */
 		msg.data = hwirq-0x200;
-		write_msi_msg(virq, &msg);
+		pci_write_msi_msg(virq, &msg);
 	}
 
 	return 0;
