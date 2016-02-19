716d0fb29a9b36cba23b525c05d621a04fa66cbb
Gustavo Duarte <gduarte@redhat.com>
2016-02-19 14:58:55 -0500

    [powerpc] powernv: Fix early pci_controller_ops loading
    Message-id: <1455893954-28433-28-git-send-email-gduarte@redhat.com>
    Patchwork-id: 134993
    O-Subject: [RHEL7.3 PATCH BZ 1275657 27/46] powerpc/powernv: Fix early pci_controller_ops loading.
    Bugzilla: 1275657
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ 1275657
    Upstream Status: d33047fd7e7d93662622888681861ba84d43c506
    
    commit d33047fd7e7d93662622888681861ba84d43c506
    Author: Daniel Axtens <dja@axtens.net>
    Date:   Mon Apr 27 12:33:51 2015 +1000
    
        powerpc/powernv: Fix early pci_controller_ops loading.
    
        Load the PowerNV platform pci controller ops into pci controllers
        after all the operations are loaded into the platform ops struct, not
        before.
    
        Otherwise we aren't actually setting the ops properly which can break
        IO for some devices.
    
        Fixes: 65ebf4b63 ("powerpc/powernv: Move controller ops from ppc_md to controller_ops")
        Reported-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
        Reviewed-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
        Signed-off-by: Daniel Axtens <dja@axtens.net>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/platforms/powernv/pci-ioda.c
+++ arch/powerpc/platforms/powernv/pci-ioda.c
@@ -2944,7 +2944,6 @@ void __init pnv_pci_init_ioda_phb(struct device_node *np,
 		hose->last_busno = 0xff;
 	}
 	hose->private_data = phb;
-	hose->controller_ops = pnv_pci_controller_ops;
 	phb->hub_id = hub_id;
 	phb->opal_id = phb_id;
 	phb->type = ioda_type;
@@ -3064,6 +3063,7 @@ void __init pnv_pci_init_ioda_phb(struct device_node *np,
 	pnv_pci_controller_ops.enable_device_hook = pnv_pci_enable_device_hook;
 	pnv_pci_controller_ops.window_alignment = pnv_pci_window_alignment;
 	pnv_pci_controller_ops.reset_secondary_bus = pnv_pci_reset_secondary_bus;
+	hose->controller_ops = pnv_pci_controller_ops;
 
 #ifdef CONFIG_PCI_IOV
 	ppc_md.pcibios_fixup_sriov = pnv_pci_ioda_fixup_iov_resources;
