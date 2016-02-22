812fe38ed9a1a69ecb08ef74b893adc58f643475
Brian Foster <bfoster@redhat.com>
2016-02-22 17:32:17 -0500

    [fs] xfs: add tracepoints to readpage calls
    Message-id: <1456162346-7841-120-git-send-email-bfoster@redhat.com>
    Patchwork-id: 135634
    O-Subject: [RHEL7 PATCH 119/128] xfs: add tracepoints to readpage calls
    Bugzilla: 1298684
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    commit 121e213eabad66c0453904d76e3eda193958acbd
    Author: Dave Chinner <dchinner@redhat.com>
    Date:   Fri Jan 8 11:28:35 2016 +1100
    
        xfs: add tracepoints to readpage calls
    
        This allows us to see page cache driven readahead in action as it
        passes through XFS. This helps to understand buffered read
        throughput problems such as readahead IO IO sizes being too small
        for the underlying device to reach max throughput.
    
        Signed-off-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: Brian Foster <bfoster@redhat.com>
        Signed-off-by: Dave Chinner <david@fromorbit.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/xfs/xfs_aops.c
+++ fs/xfs/xfs_aops.c
@@ -1858,6 +1858,7 @@ xfs_vm_readpage(
 	struct file		*unused,
 	struct page		*page)
 {
+	trace_xfs_vm_readpage(page->mapping->host, 1);
 	return mpage_readpage(page, xfs_get_blocks);
 }
 
@@ -1868,6 +1869,7 @@ xfs_vm_readpages(
 	struct list_head	*pages,
 	unsigned		nr_pages)
 {
+	trace_xfs_vm_readpages(mapping->host, nr_pages);
 	return mpage_readpages(mapping, pages, nr_pages, xfs_get_blocks);
 }
 
