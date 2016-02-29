torvalds/linux

commit 9e65dc371b5c8d7476c81353137efc13cc1bdabd

IB/mlx5: Fix RC transport send queue overhead computation
Fix the RC QPs send queue overhead computation to take into account
two additional segments in the WQE which are needed for registration
operations.

Signed-off-by: Eli Cohen <eli@mellanox.com>
Signed-off-by: Roland Dreier <roland@purestorage.com>

drivers/infiniband/hw/mlx5/qp.c
@@ -216,7 +216,9 @@ static int sq_overhead(enum ib_qp_type qp_type)
 
 	case IB_QPT_UC:
 		size += sizeof(struct mlx5_wqe_ctrl_seg) +
-			sizeof(struct mlx5_wqe_raddr_seg);
+			sizeof(struct mlx5_wqe_raddr_seg) +
+			sizeof(struct mlx5_wqe_umr_ctrl_seg) +
+			sizeof(struct mlx5_mkey_seg);
 		break;
 
 	case IB_QPT_UD:

halfline/rhel7

commit 7f8cc6ac09e51d95468661a8bc0a7d9f47295b94

[ib] mlx5: Fix RC transport send queue overhead computation
Message-id: <1456433078-62256-1-git-send-email-ddutile@redhat.com>
Patchwork-id: 136331
O-Subject: [PATCH RHEL-7.3] IB/mlx5: Fix RC transport send queue overhead computation
Bugzilla: 1293336
RH-Acked-by: Doug Ledford <dledford@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: David S. Miller <davem@redhat.com>
RH-Acked-by: Ivan Vecera <ivecera@redhat.com>

commit 75c1657e1d50730dc0130a67977f7831a4e241f4
 Author: Leon Romanovsky <leon@leon.nu>
 Date:   Thu Feb 11 21:09:57 2016 +0200

     IB/mlx5: Fix RC transport send queue overhead computation

 Fix the RC QPs send queue overhead computation to take into account
 two additional segments in the WQE which are needed for registration
 operations.

 The ATOMIC and UMR segments can't coexist together, so chose maximum out
 of them.

 The commit 9e65dc371b5c ("IB/mlx5: Fix RC transport send queue overhead
 computation") was intended to update RC transport as commit messages
 states, but added the code to UC transport.

 Fixes: 9e65dc371b5c ("IB/mlx5: Fix RC transport send queue overhead computation")
 Signed-off-by: Kamal Heib <kamalh@mellanox.com>
 Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
 Reviewed-by: Sagi Grimberg <sagig@mellanox.com>
 Signed-off-by: Doug Ledford <dledford@redhat.com>
 (cherry picked from commit 75c1657e1d50730dc0130a67977f7831a4e241f4)

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1293336
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10557827

Testing:
Test kernel run against load by CEE that is tracking
this bz as well as its ensuing bz 1309411.  Posting this
update now, as it is common code to other IB-ULP's beside
the original SRP-based issue.

Signed-off-by: Donald Dutile <ddutile@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/infiniband/hw/mlx5/qp.c
@@ -270,8 +270,10 @@ static int sq_overhead(enum ib_qp_type qp_type)
 		/* fall through */
 	case IB_QPT_RC:
 		size += sizeof(struct mlx5_wqe_ctrl_seg) +
-			sizeof(struct mlx5_wqe_atomic_seg) +
-			sizeof(struct mlx5_wqe_raddr_seg);
+			max(sizeof(struct mlx5_wqe_atomic_seg) +
+			    sizeof(struct mlx5_wqe_raddr_seg),
+			    sizeof(struct mlx5_wqe_umr_ctrl_seg) +
+			    sizeof(struct mlx5_mkey_seg));
 		break;
 
 	case IB_QPT_XRC_TGT:
@@ -279,9 +281,9 @@ static int sq_overhead(enum ib_qp_type qp_type)
 
 	case IB_QPT_UC:
 		size += sizeof(struct mlx5_wqe_ctrl_seg) +
-			sizeof(struct mlx5_wqe_raddr_seg) +
-			sizeof(struct mlx5_wqe_umr_ctrl_seg) +
-			sizeof(struct mlx5_mkey_seg);
+			max(sizeof(struct mlx5_wqe_raddr_seg),
+			    sizeof(struct mlx5_wqe_umr_ctrl_seg) +
+			    sizeof(struct mlx5_mkey_seg));
 		break;
 
 	case IB_QPT_UD: