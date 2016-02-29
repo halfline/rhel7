6b0a76349a52dcf4dd2eaffc98c23c383cfd3b59
David Arcari <darcari@redhat.com>
2016-02-29 17:52:35 -0500

    [cpufreq] intel_pstate: decrease number of "HWP enabled" messages
    Message-id: <1456768355-73103-1-git-send-email-darcari@redhat.com>
    Patchwork-id: 136406
    O-Subject: [RHEL7.3 PATCH BZ 1310927 v2 2/2] intel_pstate: decrease number of "HWP enabled" messages
    Bugzilla: 1310927
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Lenny Szubowicz <lszubowi@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1310927
    
    commit 539342f60b93c9f98c47f75b63fe5b8b13c1d226
    Author: Prarit Bhargava <prarit@redhat.com>
    Date:   Thu Oct 22 09:43:31 2015 -0400
    
        intel_pstate: decrease number of "HWP enabled" messages
    
        When booting an HWP enabled system the kernel displays one "HWP enabled"
        message for each cpu.  The messages are superfluous since HWP is globally
        enabled across all CPUs. This patch also adds an informational message
        when HWP is disabled via intel_pstate=no_hwp.
    
        Signed-off-by: Prarit Bhargava <prarit@redhat.com>
        Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Cc: Steve Best <sbest@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -483,8 +483,6 @@ static void __init intel_pstate_sysfs_expose_params(void)
 
 static void intel_pstate_hwp_enable(struct cpudata *cpudata)
 {
-	pr_info("intel_pstate HWP enabled\n");
-
 	wrmsrl_on_cpu(cpudata->cpu, MSR_PM_ENABLE, 0x1);
 }
 
@@ -1241,8 +1239,10 @@ static int __init intel_pstate_init(void)
 	if (!all_cpu_data)
 		return -ENOMEM;
 
-	if (cpu_has(c,X86_FEATURE_HWP) && !no_hwp)
+	if (cpu_has(c,X86_FEATURE_HWP) && !no_hwp) {
+		pr_info("intel_pstate: HWP enabled\n");
 		hwp_active++;
+	}
 
 	if (!hwp_active && hwp_only)
 		goto out;
@@ -1277,8 +1277,10 @@ static int __init intel_pstate_setup(char *str)
 
 	if (!strcmp(str, "disable"))
 		no_load = 1;
-	if (!strcmp(str, "no_hwp"))
+	if (!strcmp(str, "no_hwp")) {
+		pr_info("intel_pstate: HWP disabled\n");
 		no_hwp = 1;
+	}
 	if (!strcmp(str, "force"))
 		force_load = 1;
 	if (!strcmp(str, "hwp_only"))
