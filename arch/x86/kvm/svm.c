ba390e152f83a572e362a3a7b79867326ecc5d2f
Paolo Bonzini <pbonzini@redhat.com>
2016-03-09 18:07:03 -0500

    [x86] kvm: x86: fix initial PAT value
    Message-id: <1457546986-23318-38-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 138097
    O-Subject: [RHEL7.3 PATCH 037/200] KVM: x86: fix initial PAT value
    Bugzilla: 1301888
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    From: Radim Krm <rkrcmar@redhat.com>
    
    PAT should be 0007_0406_0007_0406h on RESET and not modified on INIT.
    VMX used a wrong value (host's PAT) and while SVM used the right one,
    it never got to arch.pat.
    
    This is not an issue with QEMU as it will force the correct value.
    
    Signed-off-by: Radim Krm <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 74545705cb2e398efc3cd751628c58f021323434)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -1181,7 +1181,7 @@ static void init_vmcb(struct vcpu_svm *svm)
 		clr_exception_intercept(svm, PF_VECTOR);
 		clr_cr_intercept(svm, INTERCEPT_CR3_READ);
 		clr_cr_intercept(svm, INTERCEPT_CR3_WRITE);
-		save->g_pat = 0x0007040600070406ULL;
+		save->g_pat = svm->vcpu.arch.pat;
 		save->cr3 = 0;
 		save->cr4 = 0;
 	}
