2678d830d22d1af0a4c5573105f22f1dce3eaa38
Paolo Bonzini <pbonzini@redhat.com>
2016-03-09 18:09:25 -0500

    [include] kvm: x86: Add a common TSC scaling function
    Message-id: <1457546986-23318-180-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 138256
    O-Subject: [RHEL7.3 PATCH 179/200] KVM: x86: Add a common TSC scaling function
    Bugzilla: 1301888
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    VMX and SVM calculate the TSC scaling ratio in a similar logic, so this
    patch generalizes it to a common TSC scaling function.
    
    Signed-off-by: Haozhong Zhang <haozhong.zhang@intel.com>
    [Inline the multiplication and shift steps into mul_u64_u64_shr.  Remove
     BUG_ON.  - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 35181e86df97e4223f4a28fb33e2bcf3b73de141)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1133,5 +1133,6 @@ void kvm_arch_irq_bypass_start(struct irq_bypass_consumer *);
 int kvm_arch_update_irqfd_routing(struct kvm *kvm, unsigned int host_irq,
 				  uint32_t guest_irq, bool set);
 #endif /* CONFIG_HAVE_KVM_IRQ_BYPASS */
+
 #endif
 
