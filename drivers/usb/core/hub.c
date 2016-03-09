687d415875088be6cc31c928c15460221393a830
Don Zickus <dzickus@redhat.com>
2016-03-08 20:46:27 -0500

    [usb] don't use PREPARE_DELAYED_WORK
    Message-id: <1457470219-37147-192-git-send-email-dzickus@redhat.com>
    Patchwork-id: 137716
    O-Subject: [RHEL7 PATCH 191/423] usb: don't use PREPARE_DELAYED_WORK
    Bugzilla: 1303209
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Torez Smith <torez@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1303209
    
    commit 77fa83cf7478202fac1520ca082ab8f9658d63b4
    Author: Tejun Heo <tj@kernel.org>
    Date:   Fri Mar 7 10:24:48 2014 -0500
    
        usb: don't use PREPARE_DELAYED_WORK
    
        PREPARE_[DELAYED_]WORK() are being phased out.  They have few users
        and a nasty surprise in terms of reentrancy guarantee as workqueue
        considers work items to be different if they don't have the same work
        function.
    
        usb_hub->init_work is multiplexed with multiple work functions;
        however, the work item is never queued while in-flight, so we can
        simply use INIT_DELAYED_WORK() before each queueing.
    
        It would probably be best to route this with other related updates
        through the workqueue tree.
    
        Lightly tested.
    
        v2: Greg and Alan confirm that the work item is never queued while
            in-flight.  Simply use INIT_DELAYED_WORK().
    
        Signed-off-by: Tejun Heo <tj@kernel.org>
        Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Cc: Alan Stern <stern@rowland.harvard.edu>
        Cc: linux-usb@vger.kernel.org
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1072,7 +1072,7 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 			unsigned delay = hub_power_on_good_delay(hub);
 
 			hub_power_on(hub, false);
-			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func2);
+			INIT_DELAYED_WORK(&hub->init_work, hub_init_func2);
 			queue_delayed_work(system_power_efficient_wq,
 					&hub->init_work,
 					msecs_to_jiffies(delay));
@@ -1227,7 +1227,7 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 
 		/* Don't do a long sleep inside a workqueue routine */
 		if (type == HUB_INIT2) {
-			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func3);
+			INIT_DELAYED_WORK(&hub->init_work, hub_init_func3);
 			queue_delayed_work(system_power_efficient_wq,
 					&hub->init_work,
 					msecs_to_jiffies(delay));
