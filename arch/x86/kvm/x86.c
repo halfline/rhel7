88387d8c2dcd4751d20e2fc7024360fed7461323
Paolo Bonzini <pbonzini@redhat.com>
2016-03-09 18:07:16 -0500

    [x86] kvm: x86: Avoid using plain integer as NULL pointer warning
    Message-id: <1457546986-23318-51-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 138106
    O-Subject: [RHEL7.3 PATCH 050/200] KVM: x86: Avoid using plain integer as NULL pointer warning
    Bugzilla: 1301888
    RH-Acked-by: Alex Williamson <alex.williamson@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    From: Xiubo Li <lixiubo@cmss.chinamobile.com>
    
    This patch fix the following sparse warning:
    
    for file arch/x86/kvm/x86.c:
    warning: Using plain integer as NULL pointer
    
    Signed-off-by: Xiubo Li <lixiubo@cmss.chinamobile.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    (cherry picked from commit 795a149e78f49c0e260c56cee9978c5d001a84f1)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6133,7 +6133,7 @@ static void kvm_pv_kick_cpu_op(struct kvm *kvm, unsigned long flags, int apicid)
 	lapic_irq.msi_redir_hint = false;
 
 	lapic_irq.delivery_mode = APIC_DM_REMRD;
-	kvm_irq_delivery_to_apic(kvm, 0, &lapic_irq, NULL);
+	kvm_irq_delivery_to_apic(kvm, NULL, &lapic_irq, NULL);
 }
 
 int kvm_emulate_hypercall(struct kvm_vcpu *vcpu)
