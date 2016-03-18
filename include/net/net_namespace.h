4854251c200a3b93826c7f35400abe3ef58daeab
Sabrina Dubroca <sdubroca@redhat.com>
2016-03-18 13:03:32 -0400

    [net] ipv4: bind ip_nonlocal_bind to current netns
    Message-id: <9190c8a27a6f4f9032ae883eadddfdf34b63019d.1458297184.git.sdubroca@redhat.com>
    Patchwork-id: 139143
    O-Subject: [PATCH RHEL7.3 net 1/2] net/ipv4: bind ip_nonlocal_bind to current netns
    Bugzilla: 1315968
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1315968
    Upstream Status: 49a601589caaf0e93194c0cc9b4ecddbe75dd2d5
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=10687471
    Tested: VM
    
    Changes from upstream:
    - context changes
    - we don't have 6d0bfe226116 ("net: ipv6: Add IPv6 support to the ping
      socket"), which introduced ping_check_bind_addr, so apply the
      namespace-ification directly to the identical condition in ping_bind()
    - add the sysctl to struct net, since struct netns_ipv4 is kabi-protected
    
    Upstream Commit:
    commit 49a601589caaf0e93194c0cc9b4ecddbe75dd2d5
    Author: Vincent Bernat <vincent@bernat.im>
    Date:   Fri Sep 5 15:09:03 2014 +0200
    
        net/ipv4: bind ip_nonlocal_bind to current netns
    
        net.ipv4.ip_nonlocal_bind sysctl was global to all network
        namespaces. This patch allows to set a different value for each
        network namespace.
    
        Signed-off-by: Vincent Bernat <vincent@bernat.im>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -149,6 +149,7 @@ struct net {
 	RH_KABI_EXTEND(struct xfrm_policy_hthresh policy_hthresh)
 #endif
 	RH_KABI_EXTEND(bool ip_local_ports_warned)
+	RH_KABI_EXTEND(int ipv4_sysctl_ip_nonlocal_bind)
 };
 
 /*
