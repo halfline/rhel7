torvalds/linux

commit 785ee27881411bd971d5e37a7fd72a5063fc1237

cpufreq: intel_pstate: Fix limits->max_perf rounding error
A rounding error was found in the calculation of limits->max_perf
in intel_pstate_set_policy(), which is used to calculate the max and min
pstate values in intel_pstate_get_min_max().  In that code,
limits->max_perf is truncated to 2 hex digits such that, for example,
0x169 was incorrectly calculated to 0x16 instead of 0x17.  This resulted in
the pstate being set one level too low.  This patch rounds the value of
limits->max_perf up instead of down so that the correct max pstate can
be reached.

Signed-off-by: Prarit Bhargava <prarit@redhat.com>
Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

drivers/cpufreq/intel_pstate.c
@@ -1121,6 +1121,7 @@ static int intel_pstate_set_policy(struct cpufreq_policy *policy)
 				   limits->max_sysfs_pct);
 	limits->max_perf_pct = max(limits->min_policy_pct,
 				   limits->max_perf_pct);
+	limits->max_perf = round_up(limits->max_perf, 8);
 
 	/* Make sure min_perf_pct <= max_perf_pct */
 	limits->min_perf_pct = min(limits->max_perf_pct, limits->min_perf_pct);

halfline/rhel7

commit ed8b86578bde9ccfbb6043efb6762f0069d656b1

[cpufreq] intel_pstate: Minor cleanup for FRAC_BITS
Message-id: <1457363691-16370-14-git-send-email-prarit@redhat.com>
Patchwork-id: 137118
O-Subject: [RHEL7.3 PATCH BZ 1283337 13/13] cpufreq: intel_pstate: Minor cleanup for FRAC_BITS
Bugzilla: 1283337
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
RH-Acked-by: Steve Best <sbest@redhat.com>
RH-Acked-by: David Arcari <darcari@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1283337

commit 88b7b7c0c2ba2c1f2c589ee883050717fe91af22
Author: Prarit Bhargava <prarit@redhat.com>
Date:   Tue Dec 8 13:44:59 2015 -0500

    cpufreq: intel_pstate: Minor cleanup for FRAC_BITS

    785ee27 ("cpufreq: intel_pstate: Fix limits->max_perf rounding error")
    hardcodes the value of FRAC_BITS.  This patch fixes that minor issue.

    Fixes: 785ee2788141 (cpufreq: intel_pstate: Fix limits->max_perf rounding error)
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

Cc: Lenny Szubowicz <lszubowi@redhat.com>
Cc: Steve Best <sbest@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/cpufreq/intel_pstate.c
@@ -1105,7 +1105,7 @@ static int intel_pstate_set_policy(struct cpufreq_policy *policy)
 				   limits->max_sysfs_pct);
 	limits->max_perf_pct = max(limits->min_policy_pct,
 				   limits->max_perf_pct);
-	limits->max_perf = round_up(limits->max_perf, 8);
+	limits->max_perf = round_up(limits->max_perf, FRAC_BITS);
 
 	/* Make sure min_perf_pct <= max_perf_pct */
 	limits->min_perf_pct = min(limits->max_perf_pct, limits->min_perf_pct);