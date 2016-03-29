ba0b7a1ac603d8c6cac902e8f5230b4a3e43f28d
Ivan Vecera <ivecera@redhat.com>
2016-03-29 15:56:14 -0400

    [net] ethtool: correctly ensure {GS}CHANNELS doesn't conflict with GS{RXFH}
    Message-id: <1459266986-31005-102-git-send-email-ivecera@redhat.com>
    Patchwork-id: 139397
    O-Subject: [RHEL7.3 net PATCH 101/113] ethtool: correctly ensure {GS}CHANNELS doesn't conflict with GS{RXFH}
    Bugzilla: 1268334
    RH-Acked-by: Florian Westphal <fwestpha@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: John Greene <jogreene@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ: #1268334
    
    Upstream commit(s):
    commit d4ab4286276fcd6c155bafdf4422b712068d2516
    Author: Keller, Jacob E <jacob.e.keller@intel.com>
    Date:   Mon Feb 8 16:05:03 2016 -0800
    
        ethtool: correctly ensure {GS}CHANNELS doesn't conflict with GS{RXFH}
    
        Ethernet drivers implementing both {GS}RXFH and {GS}CHANNELS ethtool ops
        incorrectly allow SCHANNELS when it would conflict with the settings
        from SRXFH. This occurs because it is not possible for drivers to
        understand whether their Rx flow indirection table has been configured
        or is in the default state. In addition, drivers currently behave in
        various ways when increasing the number of Rx channels.
    
        Some drivers will always destroy the Rx flow indirection table when this
        occurs, whether it has been set by the user or not. Other drivers will
        attempt to preserve the table even if the user has never modified it
        from the default driver settings. Neither of these situation is
        desirable because it leads to unexpected behavior or loss of user
        configuration.
    
        The correct behavior is to simply return -EINVAL when SCHANNELS would
        conflict with the current Rx flow table settings. However, it should
        only do so if the current settings were modified by the user. If we
        required that the new settings never conflict with the current (default)
        Rx flow settings, we would force users to first reduce their Rx flow
        settings and then reduce the number of Rx channels.
    
        This patch proposes a solution implemented in net/core/ethtool.c which
        ensures that all drivers behave correctly. It checks whether the RXFH
        table has been configured to non-default settings, and stores this
        information in a private netdev flag. When the number of channels is
        requested to change, it first ensures that the current Rx flow table is
        not going to assign flows to now disabled channels.
    
        Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1273,6 +1273,7 @@ struct net_device_ops {
  * @IFF_LIVE_ADDR_CHANGE: device supports hardware address
  *	change when it's running
  * @IFF_MACVLAN: Macvlan device
+ * @IFF_RXFH_CONFIGURED: device has had Rx Flow indirection table configured
  */
 enum netdev_priv_flags {
 	IFF_802_1Q_VLAN			= 1<<0,
@@ -1298,6 +1299,7 @@ enum netdev_priv_flags {
 	IFF_LIVE_ADDR_CHANGE		= 1<<20,
 	IFF_MACVLAN			= 1<<21,
 	IFF_XMIT_DST_RELEASE_PERM	= 1<<22,
+	IFF_RXFH_CONFIGURED		= 1<<25,
 };
 
 #define IFF_802_1Q_VLAN			IFF_802_1Q_VLAN
@@ -1323,6 +1325,7 @@ enum netdev_priv_flags {
 #define IFF_LIVE_ADDR_CHANGE		IFF_LIVE_ADDR_CHANGE
 #define IFF_MACVLAN			IFF_MACVLAN
 #define IFF_XMIT_DST_RELEASE_PERM	IFF_XMIT_DST_RELEASE_PERM
+#define IFF_RXFH_CONFIGURED		IFF_RXFH_CONFIGURED
 
 /*
  *	The DEVICE structure.
@@ -3675,6 +3678,11 @@ static inline bool netif_is_bridge_port(const struct net_device *dev)
 static inline bool netif_supports_nofcs(struct net_device *dev)
 {
 	return dev->priv_flags & IFF_SUPP_NOFCS;
+}
+
+static inline bool netif_is_rxfh_configured(const struct net_device *dev)
+{
+	return dev->priv_flags & IFF_RXFH_CONFIGURED;
 }
 
 /* This device needs to keep skb dst for qdisc enqueue or ndo_start_xmit() */
