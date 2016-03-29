03bbcd2c79ee6e51edec4e28cec768c1b31ea425
Ivan Vecera <ivecera@redhat.com>
2016-03-29 15:55:25 -0400

    [net] un-inline sk_busy_loop()
    Message-id: <1459266986-31005-53-git-send-email-ivecera@redhat.com>
    Patchwork-id: 139417
    O-Subject: [RHEL7.3 net PATCH 052/113] net: un-inline sk_busy_loop()
    Bugzilla: 1268334
    RH-Acked-by: Florian Westphal <fwestpha@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: John Greene <jogreene@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ: #1268334
    
    Upstream commit(s):
    commit 02d62e86fe892c59a1259d089d4d16ac76977a37
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Nov 18 06:30:52 2015 -0800
    
        net: un-inline sk_busy_loop()
    
        There is really little gain from inlining this big function.
        We'll soon make it even bigger in following patches.
    
        This means we no longer need to export napi_by_id()
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -482,15 +482,6 @@ void napi_complete(struct napi_struct *n);
 #define napi_complete _napi_complete
 
 /**
- *	napi_by_id - lookup a NAPI by napi_id
- *	@napi_id: hashed napi_id
- *
- * lookup @napi_id in napi_hash table
- * must be called under rcu_read_lock()
- */
-struct napi_struct *napi_by_id(unsigned int napi_id);
-
-/**
  *	napi_hash_add - add a NAPI to global hashtable
  *	@napi: napi context
  *
