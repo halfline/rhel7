30096b5cb36567c904a0a70d77dd360d192b4281
Bill O'Donnell <billodo@redhat.com>
2016-03-31 20:31:13 -0400

    [fs] btrfs: fix hang when failing to submit bio of directIO
    Message-id: <1459456505-5012-294-git-send-email-billodo@redhat.com>
    Patchwork-id: 139771
    O-Subject: [RHEL7.3 PATCH V2 BZ 1298680 293/525] [fs] Btrfs: fix hang when failing to submit bio of directIO
    Bugzilla: 1298680
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    Bugzilla: 1298680
    Upstream Status: Committed upstream
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=10759031
    Tested: Locally with fstests
    
    Backport of the following upstream commit:
    
        commit ad9ee2053f3f2babebc09ebc4970daa66c56c7ee
        Author: Liu Bo <bo.li.liu@oracle.com>
        Date:   Wed Jun 17 16:59:57 2015 +0800
    
        Btrfs: fix hang when failing to submit bio of directIO
    
        The hang is uncoverd by generic/019.
    
        btrfs_endio_direct_write() skips the "finish_ordered_fn" part when it hits
        an error, thus those added ordered extents will never get processed, which
        block processes that waiting for them via btrfs_start_ordered_extent().
    
        This fixes the above, and meanwhile finish_ordered_fn will do the space
        accounting work.
    
        Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
        Reviewed-by: Filipe Manana <fdmanana@suse.com>
        Tested-by: Filipe Manana <fdmanana@suse.com>
        Signed-off-by: Chris Mason <clm@fb.com>
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -7866,8 +7866,6 @@ static void btrfs_endio_direct_write(struct bio *bio, int err)
 	struct bio *dio_bio;
 	int ret;
 
-	if (err)
-		goto out_done;
 again:
 	ret = btrfs_dec_test_first_ordered_pending(inode, &ordered,
 						   &ordered_offset,
@@ -7890,7 +7888,6 @@ out_test:
 		ordered = NULL;
 		goto again;
 	}
-out_done:
 	dio_bio = dip->dio_bio;
 
 	kfree(dip);
