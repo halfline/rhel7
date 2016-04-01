de7d1432e918326129de20a42c77ac09ebfd0d1b
Bill O'Donnell <billodo@redhat.com>
2016-03-31 20:31:17 -0400

    [fs] btrfs: fix memory leak in the extent_same ioctl
    Message-id: <1459456505-5012-298-git-send-email-billodo@redhat.com>
    Patchwork-id: 139732
    O-Subject: [RHEL7.3 PATCH V2 BZ 1298680 297/525] [fs] Btrfs: fix memory leak in the extent_same ioctl
    Bugzilla: 1298680
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    Bugzilla: 1298680
    Upstream Status: Committed upstream
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=10759031
    Tested: Locally with fstests
    
    Backport of the following upstream commit:
    
        commit 497b4050e0eacd4c746dd396d14916b1e669849d
        Author: Filipe Manana <fdmanana@suse.com>
        Date:   Fri Jul 3 08:36:11 2015 +0100
    
        Btrfs: fix memory leak in the extent_same ioctl
    
        We were allocating memory with memdup_user() but we were never releasing
        that memory. This affected pretty much every call to the ioctl, whether
        it deduplicated extents or not.
    
        This issue was reported on IRC by Julian Taylor and on the mailing list
        by Marcel Ritter, credit goes to them for finding the issue.
    
        Reported-by: Julian Taylor <jtaylor.debian@googlemail.com>
        Reported-by: Marcel Ritter <ritter.marcel@gmail.com>
        Cc: stable@vger.kernel.org
        Signed-off-by: Filipe Manana <fdmanana@suse.com>
        Reviewed-by: Mark Fasheh <mfasheh@suse.de>
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -3092,7 +3092,7 @@ out_unlock:
 static long btrfs_ioctl_file_extent_same(struct file *file,
 			struct btrfs_ioctl_same_args __user *argp)
 {
-	struct btrfs_ioctl_same_args *same;
+	struct btrfs_ioctl_same_args *same = NULL;
 	struct btrfs_ioctl_same_extent_info *info;
 	struct inode *src = file_inode(file);
 	u64 off;
@@ -3122,6 +3122,7 @@ static long btrfs_ioctl_file_extent_same(struct file *file,
 
 	if (IS_ERR(same)) {
 		ret = PTR_ERR(same);
+		same = NULL;
 		goto out;
 	}
 
@@ -3192,6 +3193,7 @@ static long btrfs_ioctl_file_extent_same(struct file *file,
 
 out:
 	mnt_drop_write_file(file);
+	kfree(same);
 	return ret;
 }
 
