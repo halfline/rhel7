71775a39523e1772bb0617e55a86bc3f3b7be60b
Jiri Olsa <jolsa@redhat.com>
2016-04-07 14:13:56 -0400

    [tools] perf tools: Add stat config event synthesize function
    Message-id: <1460038562-19641-186-git-send-email-jolsa@redhat.com>
    Patchwork-id: 140504
    O-Subject: [PATCH RHEL7.3 BZ1323852 185/311] perf tools: Add stat config event synthesize function
    Bugzilla: 1323852
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1323852
    https://bugzilla.redhat.com/show_bug.cgi?id=1323852
    
    upstream
    ========
    commit 6742434261158ad9678bf15b165304e0200cc324
    Author: Jiri Olsa <jolsa@kernel.org>
    Date: Sun Oct 25 15:51:28 2015 +0100
    
    description
    ===========
    Introduce the perf_event__synthesize_stat_config to synthesize a 'struct
    perf_stat_config'.
    
    Storing the stat config in the form of tag-value pairs will, I believe,
    sort out future version extensibility issues.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -68,6 +68,7 @@ int test__thread_map(int subtest);
 int test_session_topology(int subtest);
 int test__thread_map_synthesize(int subtest);
 int test__cpu_map_synthesize(int subtest);
+int test__synthesize_stat_config(int subtest);
 
 #if defined(__arm__) || defined(__aarch64__)
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
