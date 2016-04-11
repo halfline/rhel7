4b82c806abed6e8cda5ad986f48517d096c5b159
Vitaly Kuznetsov <vkuznets@redhat.com>
2016-04-11 13:25:42 -0400

    [netdrv] hv_netvsc: Eliminate status from struct hv_netvsc_packet
    Message-id: <1460381154-24603-52-git-send-email-vkuznets@redhat.com>
    Patchwork-id: 140949
    O-Subject: [RHEL7.3 KERNEL PATCH 51/63] hv_netvsc: Eliminate status from struct hv_netvsc_packet
    Bugzilla: 1257293
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Cathy Avery <cavery@redhat.com>
    
    Upstream commit 10082f98878a9dff1563745f9f1dd9d1ff142700
    Author: KY Srinivasan <kys@microsoft.com>
    Date:   Tue Dec 1 16:43:18 2015 -0800
    
        hv_netvsc: Eliminate status from struct hv_netvsc_packet
    
        Eliminate status from struct hv_netvsc_packet.
    
        Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -472,8 +472,6 @@ check_size:
 			FIELD_SIZEOF(struct sk_buff, cb));
 	packet = (struct hv_netvsc_packet *)skb->cb;
 
-	packet->status = 0;
-
 	packet->vlan_tci = skb->vlan_tci;
 
 	packet->q_idx = skb_get_queue_mapping(skb);
@@ -685,8 +683,7 @@ int netvsc_recv_callback(struct hv_device *device_obj,
 
 	net = ((struct netvsc_device *)hv_get_drvdata(device_obj))->ndev;
 	if (!net || net->reg_state != NETREG_REGISTERED) {
-		packet->status = NVSP_STAT_FAIL;
-		return 0;
+		return NVSP_STAT_FAIL;
 	}
 	net_device_ctx = netdev_priv(net);
 	rx_stats = this_cpu_ptr(net_device_ctx->rx_stats);
@@ -695,8 +692,7 @@ int netvsc_recv_callback(struct hv_device *device_obj,
 	skb = netdev_alloc_skb_ip_align(net, packet->total_data_buflen);
 	if (unlikely(!skb)) {
 		++net->stats.rx_dropped;
-		packet->status = NVSP_STAT_FAIL;
-		return 0;
+		return NVSP_STAT_FAIL;
 	}
 
 	/*
