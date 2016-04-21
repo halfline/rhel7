4679a25653c4efe46f59cc6324400fe8e8a1392d
David Milburn <dmilburn@redhat.com>
2016-04-21 14:10:54 -0400

    [block] nvme: split a new struct nvme_ctrl out of struct nvme_dev
    Message-id: <1461247929-26697-84-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 141633
    O-Subject: [RHEL7.3 PATCH BZ 1288601 v2 083/158] nvme: split a new struct nvme_ctrl out of struct nvme_dev
    Bugzilla: 1288601
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    commit 1c63dc66580d4bbb6d2b75bf184b5aa105ba5bdb
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Thu Nov 26 10:06:56 2015 +0100
    
        nvme: split a new struct nvme_ctrl out of struct nvme_dev
    
        The new struct nvme_ctrl will be used by the common NVMe code that sits
        on top of struct request_queue and the new nvme_ctrl_ops abstraction.
        It only contains the bare minimum required, which consists of values
        sampled during controller probe, the admin queue pointer and a second
        struct device pointer at the moment, but more will follow later.  Only
        values that are not used in the I/O fast path should be moved to
        struct nvme_ctrl so that drivers can optimize their cache line usage
        easily.  That's also the reason why we have two device pointers as
        the struct device is used for DMA mapping purposes.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Acked-by: Keith Busch <keith.busch@intel.com>
        Signed-off-by: Keith Busch <keith.busch@intel.com>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    RHEL notes:
    
    RHEL7 is missing upstream commit ca0640850e4 results in diff to nvme.h,
    6f3b0e8bcf3 (diff using blk_mq_alloc_request), and 03100aada96f0 use of
    SG_GAPS.
    
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -78,7 +78,7 @@ int nvme_submit_sync_cmd(struct request_queue *q, struct nvme_command *cmd,
 	return __nvme_submit_sync_cmd(q, cmd, buffer, NULL, bufflen, NULL, 0);
 }
 
-int nvme_identify_ctrl(struct nvme_dev *dev, struct nvme_id_ctrl **id)
+int nvme_identify_ctrl(struct nvme_ctrl *dev, struct nvme_id_ctrl **id)
 {
 	struct nvme_command c = {
 		.identify.opcode = nvme_admin_identify,
@@ -97,7 +97,7 @@ int nvme_identify_ctrl(struct nvme_dev *dev, struct nvme_id_ctrl **id)
 	return error;
 }
 
-int nvme_identify_ns(struct nvme_dev *dev, unsigned nsid,
+int nvme_identify_ns(struct nvme_ctrl *dev, unsigned nsid,
 		struct nvme_id_ns **id)
 {
 	struct nvme_command c = {
@@ -117,7 +117,7 @@ int nvme_identify_ns(struct nvme_dev *dev, unsigned nsid,
 	return error;
 }
 
-int nvme_get_features(struct nvme_dev *dev, unsigned fid, unsigned nsid,
+int nvme_get_features(struct nvme_ctrl *dev, unsigned fid, unsigned nsid,
 					dma_addr_t dma_addr, u32 *result)
 {
 	struct nvme_command c;
@@ -132,7 +132,7 @@ int nvme_get_features(struct nvme_dev *dev, unsigned fid, unsigned nsid,
 			result, 0);
 }
 
-int nvme_set_features(struct nvme_dev *dev, unsigned fid, unsigned dword11,
+int nvme_set_features(struct nvme_ctrl *dev, unsigned fid, unsigned dword11,
 					dma_addr_t dma_addr, u32 *result)
 {
 	struct nvme_command c;
@@ -147,7 +147,7 @@ int nvme_set_features(struct nvme_dev *dev, unsigned fid, unsigned dword11,
 			result, 0);
 }
 
-int nvme_get_log_page(struct nvme_dev *dev, struct nvme_smart_log **log)
+int nvme_get_log_page(struct nvme_ctrl *dev, struct nvme_smart_log **log)
 {
 	struct nvme_command c = {
 		.common.opcode = nvme_admin_get_log_page,
