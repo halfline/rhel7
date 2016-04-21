f2a180636b2d9b8417540297f0d0d5939e847f33
Jiri Olsa <jolsa@redhat.com>
2016-04-21 12:47:33 -0400

    [x86] perf: Add model number for Skylake Server to perf
    Message-id: <1461242854-8055-2-git-send-email-jolsa@redhat.com>
    Patchwork-id: 141519
    O-Subject: [PATCH RHEL7.3 BZ1273753 1/2] x86, perf: Add model number for Skylake Server to perf
    Bugzilla: 1273753
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: 1273753
    https://bugzilla.redhat.com/show_bug.cgi?id=1273753
    
    upstream
    ========
    commit 3dd94a3d83882ee6f42a40164a65d3304009899e
    Author: Andi Kleen <ak@linux.intel.com>
    Date:   Fri Apr 15 13:25:33 2016 -0700
    
    peterz's git tree:
      git://git.kernel.org/pub/scm/linux/kernel/git/peterz/queue.git
    
    description
    ===========
    Everything the same as base Skylake, just a new model number.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -3641,6 +3641,7 @@ __init int intel_pmu_init(void)
 	case 158: /* 14nm Kabylake Desktop */
 	case 78: /* 14nm Skylake Mobile */
 	case 94: /* 14nm Skylake Desktop */
+	case 85: /* 14nm Skylake Server */
 		x86_pmu.late_ack = true;
 		memcpy(hw_cache_event_ids, skl_hw_cache_event_ids, sizeof(hw_cache_event_ids));
 		memcpy(hw_cache_extra_regs, skl_hw_cache_extra_regs, sizeof(hw_cache_extra_regs));
