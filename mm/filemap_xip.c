6ced069bc9e840f7d98dbdc9e561eec0002d6e68
Jerome Marchand <jmarchan@redhat.com>
2016-04-21 15:37:21 -0400

    [mm] shmem: add internal shmem resident memory accounting
    Message-id: <1461253043-17496-6-git-send-email-jmarchan@redhat.com>
    Patchwork-id: 141685
    O-Subject: [RHEL7 PATCH V2 5/7] mm, shmem: add internal shmem resident memory accounting
    Bugzilla: 838926
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    RH-Acked-by: Aristeu Rozanski <aris@redhat.com>
    
    Bugzilla:
    https://bugzilla.redhat.com/show_bug.cgi?id=838926
    
    This is a backport of:
    commit 2e93830e061666070ba04d34d317dff42bef7cad
    Author: Jerome Marchand <jmarchan@redhat.com>
    Date:   Wed Jan 13 10:56:38 2016 +0100
    
        mm, shmem: add internal shmem resident memory accounting
    
        Currently looking at /proc/<pid>/status or statm, there is no way to
        distinguish shmem pages from pages mapped to a regular file (shmem pages
        are mapped to /dev/zero), even though their implication in actual memory
        use is quite different.
    
        The internal accounting currently counts shmem pages together with regular
        files.  As a preparation to extend the userspace interfaces, this patch
        adds MM_SHMEMPAGES counter to mm_rss_stat to account for shmem pages
        separately from MM_FILEPAGES.  The next patch will expose it to userspace
        - this patch doesn't change the exported values yet, by adding up
        MM_SHMEMPAGES to MM_FILEPAGES at places where MM_FILEPAGES was used
        before.  The only user-visible change after this patch is the OOM killer
        message that separates the reported "shmem-rss" from "file-rss".
    
        [vbabka@suse.cz: forward-porting, tweak changelog]
        Signed-off-by: Jerome Marchand <jmarchan@redhat.com>
        Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
        Acked-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
        Acked-by: Michal Hocko <mhocko@suse.com>
        Acked-by: Hugh Dickins <hughd@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/filemap_xip.c
+++ mm/filemap_xip.c
@@ -194,7 +194,7 @@ retry:
 			flush_cache_page(vma, address, pte_pfn(*pte));
 			pteval = ptep_clear_flush(vma, address, pte);
 			page_remove_rmap(page);
-			dec_mm_counter(mm, MM_FILEPAGES);
+			dec_mm_counter(mm, mm_counter_file(page));
 			BUG_ON(pte_dirty(pteval));
 			pte_unmap_unlock(pte, ptl);
 			/* must invalidate_page _before_ freeing the page */
