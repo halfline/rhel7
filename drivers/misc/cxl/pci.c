9473e2ef63c23c5a12bb44d211a946740742a8ff
Gustavo Duarte <gduarte@redhat.com>
2016-04-22 19:50:46 -0400

    [misc] cxl: Test the correct mmio space before unmapping
    Message-id: <1461354698-1483-33-git-send-email-gduarte@redhat.com>
    Patchwork-id: 141831
    O-Subject: [RHEL7.3 PATCH BZ 1275968 32/84] cxl: Test the correct mmio space before unmapping
    Bugzilla: 1275968
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    
    BZ 1275968
    Upstream Status: 8c00d5c9d3e7452658dda55024485d52919ebfdd
    
    commit 8c00d5c9d3e7452658dda55024485d52919ebfdd
    Author: Daniel Axtens <dja@axtens.net>
    Date:   Thu Jul 2 15:55:21 2015 +1000
    
        cxl: Test the correct mmio space before unmapping
    
        Before freeing p2n, test p2n, not p1n.
    
        Signed-off-by: Daniel Axtens <dja@axtens.net>
        Acked-by: Ian Munsie <imunsie@au1.ibm.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/misc/cxl/pci.c
+++ drivers/misc/cxl/pci.c
@@ -578,7 +578,7 @@ err:
 
 static void cxl_unmap_slice_regs(struct cxl_afu *afu)
 {
-	if (afu->p1n_mmio)
+	if (afu->p2n_mmio)
 		iounmap(afu->p2n_mmio);
 	if (afu->p1n_mmio)
 		iounmap(afu->p1n_mmio);
