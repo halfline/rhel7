b4c002059c5642b953152242b7938d231449069b
Jarod Wilson <jarod@redhat.com>
2016-04-28 16:37:33 -0400

    [netdrv] e1000e: Enable TSO for stacked VLAN
    Message-id: <1461861470-32275-32-git-send-email-jarod@redhat.com>
    Patchwork-id: 142526
    O-Subject: [RHEL7 PATCH 31/48] e1000e: Enable TSO for stacked VLAN
    Bugzilla: 1274171
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1274171
    
    commit f2701b185e05d0897a47f6a14da40a068b0644ff
    Author: Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
    Date:   Thu Aug 6 17:57:29 2015 +0900
    
        e1000e: Enable TSO for stacked VLAN
    
        Setting ndo_features_check to passthru_features_check allows the driver
        to skip the check for multiple tagged TSO packets and enables stacked
        VLAN TSO.
        Tested with I217-LM.
    
        Signed-off-by: Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
        Tested-by: Aaron Brown <aaron.f.brown@intel.com>
        Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/intel/e1000e/netdev.c
+++ drivers/net/ethernet/intel/e1000e/netdev.c
@@ -6944,6 +6944,7 @@ static const struct net_device_ops e1000e_netdev_ops = {
 #endif
 	.ndo_set_features = e1000_set_features,
 	.ndo_fix_features = e1000_fix_features,
+	.ndo_features_check	= passthru_features_check,
 };
 
 /**
