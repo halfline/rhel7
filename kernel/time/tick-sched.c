5e180828119ad9899fa51537bdf8ff690f28d064
Jiri Olsa <jolsa@redhat.com>
2016-04-28 13:58:10 -0400

    [kernel] param: convert some "on"/"off" users to strtobool
    Message-id: <1461851890-19160-4-git-send-email-jolsa@redhat.com>
    Patchwork-id: 142469
    O-Subject: [PATCH RHEL7.3 BZ1331008 3/3] param: convert some "on"/"off" users to strtobool
    Bugzilla: 1331008
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    Bugzilla: 1331008
    https://bugzilla.redhat.com/show_bug.cgi?id=1331008
    
    upstream
    ========
    commit 4cc7ecb7f2a60e8deb783b8fbf7c1ae467acb920
    Author: Kees Cook <keescook@chromium.org>
    Date:   Thu Mar 17 14:23:00 2016 -0700
    
    RHEL7
    =====
    Took only the tick_nohz_enabled hunk.
    
    description
    ===========
    This changes several users of manual "on"/"off" parsing to use
    strtobool.
    
    Some side-effects:
    - these uses will now parse y/n/1/0 meaningfully too
    - the early_param uses will now bubble up parse errors
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -381,20 +381,14 @@ void __init tick_nohz_init(void)
 /*
  * NO HZ enabled ?
  */
-static int tick_nohz_enabled __read_mostly  = 1;
+static bool tick_nohz_enabled __read_mostly  = true;
 int tick_nohz_active  __read_mostly;
 /*
  * Enable / Disable tickless mode
  */
 static int __init setup_tick_nohz(char *str)
 {
-	if (!strcmp(str, "off"))
-		tick_nohz_enabled = 0;
-	else if (!strcmp(str, "on"))
-		tick_nohz_enabled = 1;
-	else
-		return 0;
-	return 1;
+	return (strtobool(str, &tick_nohz_enabled) == 0);
 }
 
 __setup("nohz=", setup_tick_nohz);
