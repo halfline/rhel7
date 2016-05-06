8fc81377aa5bba8828e54eb893e0e7595b504612
Benjamin Coddington <bcodding@redhat.com>
2016-05-05 23:53:04 -0400

    [fs] locks: new helpers - flock_lock_inode_wait and posix_lock_inode_wait
    Message-id: <b9a389d378f436de3e19128cea0f8334a5ab85fc.1462491372.git.bcodding@redhat.com>
    Patchwork-id: 143060
    O-Subject: [RHEL7 PATCH fs 3/5] locks: new helpers - flock_lock_inode_wait and posix_lock_inode_wait
    Bugzilla: 1329488
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Jeff Layton <jeff.layton@primarydata.com>
    
    Allow callers to pass in an inode instead of a filp.
    
    Signed-off-by: Jeff Layton <jeff.layton@primarydata.com>
    Reviewed-by: "J. Bruce Fields" <bfields@fieldses.org>
    Tested-by: "J. Bruce Fields" <bfields@fieldses.org>
    Upstream-Id: 29d01b22eaa18d8b46091d3c98c6001c49f78e4a
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1329488
    Tested: cthon
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=10976042
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1098,11 +1098,13 @@ extern void locks_remove_flock(struct file *);
 extern void locks_release_private(struct file_lock *);
 extern void posix_test_lock(struct file *, struct file_lock *);
 extern int posix_lock_file(struct file *, struct file_lock *, struct file_lock *);
+extern int posix_lock_inode_wait(struct inode *, struct file_lock *);
 extern int posix_lock_file_wait(struct file *, struct file_lock *);
 extern int posix_unblock_lock(struct file_lock *);
 extern int vfs_test_lock(struct file *, struct file_lock *);
 extern int vfs_lock_file(struct file *, unsigned int, struct file_lock *, struct file_lock *);
 extern int vfs_cancel_lock(struct file *filp, struct file_lock *fl);
+extern int flock_lock_inode_wait(struct inode *inode, struct file_lock *fl);
 extern int flock_lock_file_wait(struct file *filp, struct file_lock *fl);
 extern int __break_lease(struct inode *inode, unsigned int flags, unsigned int type);
 extern void lease_get_mtime(struct inode *, struct timespec *time);
@@ -1184,6 +1186,12 @@ static inline int posix_lock_file(struct file *filp, struct file_lock *fl,
 	return -ENOLCK;
 }
 
+static inline int posix_lock_inode_wait(struct inode *inode,
+					struct file_lock *fl)
+{
+	return -ENOLCK;
+}
+
 static inline int posix_lock_file_wait(struct file *filp, struct file_lock *fl)
 {
 	return -ENOLCK;
@@ -1208,6 +1216,12 @@ static inline int vfs_lock_file(struct file *filp, unsigned int cmd,
 static inline int vfs_cancel_lock(struct file *filp, struct file_lock *fl)
 {
 	return 0;
+}
+
+static inline int flock_lock_inode_wait(struct inode *inode,
+					struct file_lock *request)
+{
+	return -ENOLCK;
 }
 
 static inline int flock_lock_file_wait(struct file *filp,
