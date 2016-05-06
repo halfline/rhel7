4ac04369f3cd74b7bb95f6676ef28e90833e5bec
Ivan Vecera <ivecera@redhat.com>
2016-05-06 16:02:10 -0400

    [net] ensure features get disabled on new lower devs
    Message-id: <1462550559-3336-16-git-send-email-ivecera@redhat.com>
    Patchwork-id: 143176
    O-Subject: [RHEL7.3 net PATCH 15/44] net/core: ensure features get disabled on new lower devs
    Bugzilla: 1268334
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    BZ: #1268334
    
    Upstream commit(s):
    commit e7868a85e1b26bcb2e71088841eec1d310a97ac9
    Author: Jarod Wilson <jarod@redhat.com>
    Date:   Tue Nov 3 23:09:32 2015 -0500
    
        net/core: ensure features get disabled on new lower devs
    
        With moving netdev_sync_lower_features() after the .ndo_set_features
        calls, I neglected to verify that devices added *after* a flag had been
        disabled on an upper device were properly added with that flag disabled as
        well. This currently happens, because we exit __netdev_update_features()
        when we see dev->features == features for the upper dev. We can retain the
        optimization of leaving without calling .ndo_set_features with a bit of
        tweaking and a goto here.
    
        Fixes: fd867d51f889 ("net/core: generic support for disabling netdev features down stack")
        Reported-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
        Signed-off-by: Jarod Wilson <jarod@redhat.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -6240,7 +6240,7 @@ int __netdev_update_features(struct net_device *dev)
 	struct net_device *upper, *lower;
 	netdev_features_t features;
 	struct list_head *iter;
-	int err = 0;
+	int err = -1;
 
 	ASSERT_RTNL();
 
@@ -6257,7 +6257,7 @@ int __netdev_update_features(struct net_device *dev)
 		features = netdev_sync_upper_features(dev, upper, features);
 
 	if (dev->features == features)
-		return 0;
+		goto sync_lower;
 
 	netdev_dbg(dev, "Features changed: %pNF -> %pNF\n",
 		&dev->features, &features);
@@ -6272,6 +6272,7 @@ int __netdev_update_features(struct net_device *dev)
 		return -1;
 	}
 
+sync_lower:
 	/* some features must be disabled on lower devices when disabled
 	 * on an upper device (think: bonding master or bridge)
 	 */
@@ -6281,7 +6282,7 @@ int __netdev_update_features(struct net_device *dev)
 	if (!err)
 		dev->features = features;
 
-	return 1;
+	return err < 0 ? 0 : 1;
 }
 
 /**
