f2d52fe030ebacf65ba645c59de3c96847af4bad
Chad Dupuis <cdupuis@redhat.com>
2016-05-10 22:46:55 -0400

    [scsi] qla2xxx: Add support for buffer to buffer credit value for ISP27XX
    Message-id: <1462920421-3973-36-git-send-email-cdupuis@redhat.com>
    Patchwork-id: 143818
    O-Subject: [PATCH RHEL 7.3 e-stor 35/41] qla2xxx: Add support for buffer to buffer credit value for ISP27XX.
    Bugzilla: 1273080
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Sawan Chandak <sawan.chandak@qlogic.com>
    
    linux commit 969a619966d84ec0d2b8392df83cd2693411e46e
    
    Signed-off-by: Sawan Chandak <sawan.chandak@qlogic.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@qlogic.com>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -3483,6 +3483,7 @@ typedef struct scsi_qla_host {
 		uint32_t	delete_progress:1;
 
 		uint32_t	fw_tgt_reported:1;
+		uint32_t	bbcr_enable:1;
 	} flags;
 
 	atomic_t	loop_state;
@@ -3599,6 +3600,7 @@ typedef struct scsi_qla_host {
 
 	atomic_t	vref_count;
 	struct qla8044_reset_template reset_tmplt;
+	uint16_t	bbcr;
 } scsi_qla_host_t;
 
 struct qla27xx_image_status {
