e5a5c410b2978fc90c20a314ad661f4742dc4a4a
Lance Richardson <lrichard@redhat.com>
2016-05-17 18:23:22 -0400

    [net] geneve: Support outer IPv4 Tx checksums by default
    Message-id: <1463509420-12020-215-git-send-email-lrichard@redhat.com>
    Patchwork-id: 145533
    O-Subject: [RHEL7.3 net PATCH v2 214/232] GENEVE: Support outer IPv4 Tx checksums by default
    Bugzilla: 1283886
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1283886
    Upstream Status: linus commit 14f1f724355206dea1cf3f23ee87993bfd47c70c
    
    commit 14f1f724355206dea1cf3f23ee87993bfd47c70c
    Author: Alexander Duyck <aduyck@mirantis.com>
    Date:   Fri Feb 19 11:26:24 2016 -0800
    
        GENEVE: Support outer IPv4 Tx checksums by default
    
        This change makes it so that if UDP CSUM is not specified we will default
        to enabling it.  The main motivation behind this is the fact that with the
        use of outer checksum we can greatly improve the performance for GENEVE
        tunnels on hardware that doesn't know how to parse them.
    
        Signed-off-by: Alexander Duyck <aduyck@mirantis.com>
        Acked-by: Tom Herbert <tom@herbertland.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Lance Richardson <lrichard@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/geneve.c
+++ drivers/net/geneve.c
@@ -75,7 +75,7 @@ struct geneve_dev {
 };
 
 /* Geneve device flags */
-#define GENEVE_F_UDP_CSUM		BIT(0)
+#define GENEVE_F_UDP_ZERO_CSUM_TX	BIT(0)
 #define GENEVE_F_UDP_ZERO_CSUM6_TX	BIT(1)
 #define GENEVE_F_UDP_ZERO_CSUM6_RX	BIT(2)
 
@@ -694,7 +694,7 @@ static int geneve_build_skb(struct rtable *rt, struct sk_buff *skb,
 	struct genevehdr *gnvh;
 	int min_headroom;
 	int err;
-	bool udp_sum = !!(flags & GENEVE_F_UDP_CSUM);
+	bool udp_sum = !(flags & GENEVE_F_UDP_ZERO_CSUM_TX);
 
 	skb_scrub_packet(skb, xnet);
 
@@ -907,9 +907,9 @@ static netdev_tx_t geneve_xmit_skb(struct sk_buff *skb, struct net_device *dev,
 			opts = ip_tunnel_info_opts(info);
 
 		if (key->tun_flags & TUNNEL_CSUM)
-			flags |= GENEVE_F_UDP_CSUM;
+			flags &= ~GENEVE_F_UDP_ZERO_CSUM_TX;
 		else
-			flags &= ~GENEVE_F_UDP_CSUM;
+			flags |= GENEVE_F_UDP_ZERO_CSUM_TX;
 
 		err = geneve_build_skb(rt, skb, key->tun_flags, vni,
 				       info->options_len, opts, flags, xnet);
@@ -935,7 +935,7 @@ static netdev_tx_t geneve_xmit_skb(struct sk_buff *skb, struct net_device *dev,
 	udp_tunnel_xmit_skb(rt, gs4->sock->sk, skb, fl4.saddr, fl4.daddr,
 			    tos, ttl, df, sport, geneve->dst_port,
 			    !net_eq(geneve->net, dev_net(geneve->dev)),
-			    !(flags & GENEVE_F_UDP_CSUM));
+			    !!(flags & GENEVE_F_UDP_ZERO_CSUM_TX));
 
 	return NETDEV_TX_OK;
 
@@ -1373,8 +1373,8 @@ static int geneve_newlink(struct net *net, struct net_device *dev,
 		metadata = true;
 
 	if (data[IFLA_GENEVE_UDP_CSUM] &&
-	    nla_get_u8(data[IFLA_GENEVE_UDP_CSUM]))
-		flags |= GENEVE_F_UDP_CSUM;
+	    !nla_get_u8(data[IFLA_GENEVE_UDP_CSUM]))
+		flags |= GENEVE_F_UDP_ZERO_CSUM_TX;
 
 	if (data[IFLA_GENEVE_UDP_ZERO_CSUM6_TX] &&
 	    nla_get_u8(data[IFLA_GENEVE_UDP_ZERO_CSUM6_TX]))
@@ -1444,7 +1444,7 @@ static int geneve_fill_info(struct sk_buff *skb, const struct net_device *dev)
 	}
 
 	if (nla_put_u8(skb, IFLA_GENEVE_UDP_CSUM,
-		       !!(geneve->flags & GENEVE_F_UDP_CSUM)) ||
+		       !(geneve->flags & GENEVE_F_UDP_ZERO_CSUM_TX)) ||
 	    nla_put_u8(skb, IFLA_GENEVE_UDP_ZERO_CSUM6_TX,
 		       !!(geneve->flags & GENEVE_F_UDP_ZERO_CSUM6_TX)) ||
 	    nla_put_u8(skb, IFLA_GENEVE_UDP_ZERO_CSUM6_RX,
