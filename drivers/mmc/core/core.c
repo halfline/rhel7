ef22973be8942660a17e58f6f24dc55b2b0b0365
Don Zickus <dzickus@redhat.com>
2016-05-17 16:43:59 -0400

    [mmc] core: Invoke ->card_event() callback only when needed
    Message-id: <1463503488-149391-677-git-send-email-dzickus@redhat.com>
    Patchwork-id: 145267
    O-Subject: [RHEL7 PATCH 676/725] mmc: core: Invoke ->card_event() callback only when needed
    Bugzilla: 1127975 1277866 1280133 1286932 1297039
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    commit 86236813ff23e0e8afc6844d307fb84df98f6723
    Author: Ulf Hansson <ulf.hansson@linaro.org>
    Date:   Thu Nov 5 16:08:07 2015 +0100
    
        mmc: core: Invoke ->card_event() callback only when needed
    
        The ->card_event() callback may be called when re-scan is disabled and for
        non-removable cards, which both cases are unnecessary.
    
        Instead let's move the call later in mmc_rescan() where these constraints
        have been validated.
    
        Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2560,11 +2560,6 @@ void mmc_rescan(struct work_struct *work)
 		container_of(work, struct mmc_host, detect.work);
 	int i;
 
-	if (host->trigger_card_event && host->ops->card_event) {
-		host->ops->card_event(host);
-		host->trigger_card_event = false;
-	}
-
 	if (host->rescan_disable)
 		return;
 
@@ -2572,6 +2567,11 @@ void mmc_rescan(struct work_struct *work)
 	if ((host->caps & MMC_CAP_NONREMOVABLE) && host->rescan_entered)
 		return;
 	host->rescan_entered = 1;
+
+	if (host->trigger_card_event && host->ops->card_event) {
+		host->ops->card_event(host);
+		host->trigger_card_event = false;
+	}
 
 	mmc_bus_get(host);
 
