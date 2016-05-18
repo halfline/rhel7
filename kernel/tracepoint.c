a4acaa6c401eb460ad88428337a950819cbb3785
Herton R. Krzesinski <herton@redhat.com>
2016-05-18 06:41:22 -0400

    [kernel] tracing: Do not add event files for modules that fail tracepoints
    Message-id: <1463553686-8136-22-git-send-email-herton@redhat.com>
    Patchwork-id: 145587
    O-Subject: [RHEL7 PATCH 21/25] tracing: Do not add event files for modules that fail tracepoints
    Bugzilla: 1336863
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1336863
    Upstream Status: commit 45ab281, linux-stable commit d6a6d1f
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11035443
    Tested: forced modprobe of mac80211, then verifying its trace event list
    with perf list in addition to dmesg output
    Conflicts: this change conflicts with a previous backport that adds new
    TAINT_UNSIGNED_MODULE (in commit "fix module signature vs tracepoints add new
    TAINT_UNSIGNED_MODULE"). Thus in this commit I adjusted trace_module_has_bad_taint
    function to account for this.
    
    commit d6a6d1f38ce55aa5a7d8aab972176660b19fd7ab
    Author: Steven Rostedt (Red Hat) <rostedt@goodmis.org>
    Date:   Wed Feb 26 13:37:38 2014 -0500
    
        tracing: Do not add event files for modules that fail tracepoints
    
        commit 45ab2813d40d88fc575e753c38478de242d03f88 upstream.
    
        If a module fails to add its tracepoints due to module tainting, do not
        create the module event infrastructure in the debugfs directory. As the events
        will not work and worse yet, they will silently fail, making the user wonder
        why the events they enable do not display anything.
    
        Having a warning on module load and the events not visible to the users
        will make the cause of the problem much clearer.
    
        Link: http://lkml.kernel.org/r/20140227154923.265882695@goodmis.org
    
        Fixes: 6d723736e472 "tracing/events: add support for modules to TRACE_EVENT"
        Acked-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
        Cc: Rusty Russell <rusty@rustcorp.com.au>
        Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/tracepoint.c
+++ kernel/tracepoint.c
@@ -631,6 +631,12 @@ void tracepoint_iter_reset(struct tracepoint_iter *iter)
 EXPORT_SYMBOL_GPL(tracepoint_iter_reset);
 
 #ifdef CONFIG_MODULES
+bool trace_module_has_bad_taint(struct module *mod)
+{
+	return mod->taints & ~((1 << TAINT_OOT_MODULE) | (1 << TAINT_CRAP) |
+			       (1 << TAINT_UNSIGNED_MODULE));
+}
+
 static int tracepoint_module_coming(struct module *mod)
 {
 	struct tp_module *tp_mod, *iter;
@@ -641,8 +647,7 @@ static int tracepoint_module_coming(struct module *mod)
 	 * module headers (for forced load), to make sure we don't cause a crash.
 	 * Staging, out-of-tree, and unsigned GPL modules are fine.
 	 */
-	if (mod->taints & ~((1 << TAINT_OOT_MODULE) | (1 << TAINT_CRAP) |
-			    (1 << TAINT_UNSIGNED_MODULE)))
+	if (trace_module_has_bad_taint(mod))
 		return 0;
 	mutex_lock(&tracepoints_mutex);
 	tp_mod = kmalloc(sizeof(struct tp_module), GFP_KERNEL);
