3d5e15afac3872c0b64758eb475ed0c45a6f9397
Jiri Olsa <jolsa@redhat.com>
2016-05-18 11:09:40 -0400

    [tools] perf tools: Introduce parse_events_terms__purge()
    Message-id: <1463569977-29479-128-git-send-email-jolsa@redhat.com>
    Patchwork-id: 145700
    O-Subject: [PATCH RHEL7.3 BZ1336447 127/324] perf tools: Introduce parse_events_terms__purge()
    Bugzilla: 1336447
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1336447
    https://bugzilla.redhat.com/show_bug.cgi?id=1336447
    
    upstream
    ========
    commit fc0a2c1d59beac70b8738f4ce14431b798837374
    Author: Arnaldo Carvalho de Melo <acme@redhat.com>
    Date: Fri Feb 12 16:43:02 2016 -0300
    
    description
    ===========
    Purges 'struct parse_event_term' entries from a list_head.
    
    Some users need this because they don't allocate space for the list
    head, it maybe on the stack or embedded into some other struct.
    
    Next patch will convert users that need just purging and then the
    perf_events__free_terms() routine will free the list head as well,
    finally being renamed to perf_events_terms__delete().
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -1938,7 +1938,7 @@ int parse_events_term__clone(struct parse_events_term **new,
 			term->err_term, term->err_val);
 }
 
-void parse_events__free_terms(struct list_head *terms)
+void parse_events_terms__purge(struct list_head *terms)
 {
 	struct parse_events_term *term, *h;
 
@@ -1946,6 +1946,11 @@ void parse_events__free_terms(struct list_head *terms)
 		list_del_init(&term->list);
 		free(term);
 	}
+}
+
+void parse_events__free_terms(struct list_head *terms)
+{
+	parse_events_terms__purge(terms);
 }
 
 void parse_events_evlist_error(struct parse_events_evlist *data,
