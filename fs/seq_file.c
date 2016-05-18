8d37305b9084671efc6800062651794920a2c434
Neil Horman <nhorman@redhat.com>
2016-05-18 15:52:35 -0400

    [include] seq_file: provide an analogue of print_hex_dump()
    Message-id: <1463586790-20564-53-git-send-email-nhorman@redhat.com>
    Patchwork-id: 146000
    O-Subject: [RHEL7.3 PATCH 52/87] seq_file: provide an analogue of print_hex_dump()
    Bugzilla: 1274179
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    This introduces a new helper and switches current users to use it.  All
    patches are compiled tested. kmemleak is tested via its own test suite.
    
    This patch (of 6):
    
    The new seq_hex_dump() is a complete analogue of print_hex_dump().
    
    We have few users of this functionality already. It allows to reduce their
    codebase.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Joe Perches <joe@perches.com>
    Cc: Tadeusz Struk <tadeusz.struk@intel.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Ingo Tuchscherer <ingo.tuchscherer@de.ibm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 37607102c4426cf92aeb5da1b1d9a79ba6d95e3f)
    
    BZ: 1274179
    Testing: Tested successfully by Intel
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11041914
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -14,6 +14,7 @@
 #ifndef __GENKSYMS__
 #include <linux/mm.h>
 #endif
+#include <linux/printk.h>
 
 #include <asm/uaccess.h>
 #include <asm/page.h>
@@ -778,6 +779,47 @@ int seq_write(struct seq_file *seq, const void *data, size_t len)
 	return -1;
 }
 EXPORT_SYMBOL(seq_write);
+
+/* A complete analogue of print_hex_dump() */
+void seq_hex_dump(struct seq_file *m, const char *prefix_str, int prefix_type,
+		  int rowsize, int groupsize, const void *buf, size_t len,
+		  bool ascii)
+{
+	const u8 *ptr = buf;
+	int i, linelen, remaining = len;
+	int ret;
+
+	if (rowsize != 16 && rowsize != 32)
+		rowsize = 16;
+
+	for (i = 0; i < len && !seq_has_overflowed(m); i += rowsize) {
+		linelen = min(remaining, rowsize);
+		remaining -= rowsize;
+
+		switch (prefix_type) {
+		case DUMP_PREFIX_ADDRESS:
+			seq_printf(m, "%s%p: ", prefix_str, ptr + i);
+			break;
+		case DUMP_PREFIX_OFFSET:
+			seq_printf(m, "%s%.8x: ", prefix_str, i);
+			break;
+		default:
+			seq_printf(m, "%s", prefix_str);
+			break;
+		}
+
+		ret = hex_dump_to_buffer(ptr + i, linelen, rowsize, groupsize,
+					 m->buf + m->count, m->size - m->count,
+					 ascii);
+		if (ret >= m->size - m->count) {
+			seq_set_overflow(m);
+		} else {
+			m->count += ret;
+			seq_putc(m, '\n');
+		}
+	}
+}
+EXPORT_SYMBOL(seq_hex_dump);
 
 struct list_head *seq_list_start(struct list_head *head, loff_t pos)
 {
