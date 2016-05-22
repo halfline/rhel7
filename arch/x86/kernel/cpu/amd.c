0428904641b52564f3c620650f2879764d95eef7
Jiri Olsa <jolsa@redhat.com>
2016-05-22 15:22:08 -0400

    [x86] cpu/amd: Give access to the number of nodes in a physical package
    Message-id: <1463930531-23986-2-git-send-email-jolsa@redhat.com>
    Patchwork-id: 147258
    O-Subject: [PATCH RHEL7.3 BZ1337866 1/4] x86/cpu/amd: Give access to the number of nodes in a physical package
    Bugzilla: 1337866
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: 1337866
    https://bugzilla.redhat.com/show_bug.cgi?id=1337866
    
    upstream
    ========
    commit cc2749e4095cbbcb35518fb2db5e926b85c3f25f
    Author: Aravind Gopalakrishnan <Aravind.Gopalakrishnan@amd.com>
    Date:   Mon Jun 15 10:28:15 2015 +0200
    
    RHEL7
    =====
    Using this patch as a base for following change to keep the
    history straight, so we don't need the amd_get_nodes_per_socket
    function.
    
    description
    ===========
    Stash the number of nodes in a physical processor package
    locally and add an accessor to be called by interested parties.
    The first user is the MCE injection module which uses it to find
    the node base core in a package for injecting a certain type of
    errors.
    
    [ Rewrote the commit message, merged it with the accessor patch and unified naming. ]
    Link: http://lkml.kernel.org/r/1433868317-18417-2-git-send-email-Aravind.Gopalakrishnan@amd.com
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -18,6 +18,13 @@
 
 #include "cpu.h"
 
+/*
+ * nodes_per_socket: Stores the number of nodes per socket.
+ * Refer to Fam15h Models 00-0fh BKDG - CPUID Fn8000_001E_ECX
+ * Node Identifiers[10:8]
+ */
+static u32 nodes_per_socket = 1;
+
 static inline int rdmsrl_amd_safe(unsigned msr, unsigned long long *p)
 {
 	u32 gprs[8] = { 0 };
@@ -294,7 +301,7 @@ static int nearby_node(int apicid)
 #ifdef CONFIG_X86_HT
 static void amd_get_topology(struct cpuinfo_x86 *c)
 {
-	u32 nodes, cores_per_cu = 1;
+	u32 cores_per_cu = 1;
 	u8 node_id;
 	int cpu = smp_processor_id();
 
@@ -303,7 +310,7 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 		u32 eax, ebx, ecx, edx;
 
 		cpuid(0x8000001e, &eax, &ebx, &ecx, &edx);
-		nodes = ((ecx >> 8) & 7) + 1;
+		nodes_per_socket = ((ecx >> 8) & 7) + 1;
 		node_id = ecx & 7;
 
 		/* get compute unit information */
@@ -314,18 +321,18 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 		u64 value;
 
 		rdmsrl(MSR_FAM10H_NODE_ID, value);
-		nodes = ((value >> 3) & 7) + 1;
+		nodes_per_socket = ((value >> 3) & 7) + 1;
 		node_id = value & 7;
 	} else
 		return;
 
 	/* fixup multi-node processor information */
-	if (nodes > 1) {
+	if (nodes_per_socket > 1) {
 		u32 cores_per_node;
 		u32 cus_per_node;
 
 		set_cpu_cap(c, X86_FEATURE_AMD_DCM);
-		cores_per_node = c->x86_max_cores / nodes;
+		cores_per_node = c->x86_max_cores / nodes_per_socket;
 		cus_per_node = cores_per_node / cores_per_cu;
 
 		/* store NodeID, use llc_shared_map to store sibling info */
