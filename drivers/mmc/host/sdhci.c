torvalds/linux

commit bb8175a8aa42d731a840cd474e348ac3367eb5a0

mmc: sdhci: clarify DDR timing mode between SD-UHS and eMMC
Added MMC_DDR52 as eMMC's DDR mode is distinguished from SD-UHS.

Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>
Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
Signed-off-by: Chris Ball <chris@printf.net>

drivers/mmc/host/sdhci.c
@@ -1510,6 +1510,7 @@ static void sdhci_do_set_ios(struct sdhci_host *host, struct mmc_ios *ios)
 
 		/* In case of UHS-I modes, set High Speed Enable */
 		if ((ios->timing == MMC_TIMING_MMC_HS200) ||
+		    (ios->timing == MMC_TIMING_MMC_DDR52) ||
 		    (ios->timing == MMC_TIMING_UHS_SDR50) ||
 		    (ios->timing == MMC_TIMING_UHS_SDR104) ||
 		    (ios->timing == MMC_TIMING_UHS_DDR50) ||
@@ -1570,7 +1571,8 @@ static void sdhci_do_set_ios(struct sdhci_host *host, struct mmc_ios *ios)
 				ctrl_2 |= SDHCI_CTRL_UHS_SDR25;
 			else if (ios->timing == MMC_TIMING_UHS_SDR50)
 				ctrl_2 |= SDHCI_CTRL_UHS_SDR50;
-			else if (ios->timing == MMC_TIMING_UHS_DDR50)
+			else if ((ios->timing == MMC_TIMING_UHS_DDR50) ||
+				 (ios->timing == MMC_TIMING_MMC_DDR52))
 				ctrl_2 |= SDHCI_CTRL_UHS_DDR50;
 			sdhci_writew(host, ctrl_2, SDHCI_HOST_CONTROL2);
 		}

halfline/rhel7

commit 1d6c4662cd3fe5601c0425d4f3f9049dce89a5ee

[mmc] sdhci: also get preset value and driver type for MMC_DDR52
Message-id: <1463503488-149391-632-git-send-email-dzickus@redhat.com>
Patchwork-id: 145130
O-Subject: [RHEL7 PATCH 631/725] mmc: sdhci: also get preset value and driver type for MMC_DDR52
Bugzilla: 1127975 1277866 1280133 1286932 1297039
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>

commit 0dafa60eb2506617e6968b97cc5a44914a7fb1a6
Author: Jisheng Zhang <jszhang@marvell.com>
Date:   Tue Aug 18 16:21:39 2015 +0800

    mmc: sdhci: also get preset value and driver type for MMC_DDR52

    commit bb8175a8aa42 ("mmc: sdhci: clarify DDR timing mode between
    SD-UHS and eMMC") added MMC_DDR52 as eMMC's DDR mode to be
    distinguished from SD-UHS, but it missed setting driver type for
    MMC_DDR52 timing mode.

    So sometimes we get the following error on Marvell BG2Q DMP board:

    [    1.559598] mmcblk0: error -84 transferring data, sector 0, nr 8, cmd
    response 0x900, card status 0xb00
    [    1.569314] mmcblk0: retrying using single block read
    [    1.575676] mmcblk0: error -84 transferring data, sector 2, nr 6, cmd
    response 0x900, card status 0x0
    [    1.585202] blk_update_request: I/O error, dev mmcblk0, sector 2
    [    1.591818] mmcblk0: error -84 transferring data, sector 3, nr 5, cmd
    response 0x900, card status 0x0
    [    1.601341] blk_update_request: I/O error, dev mmcblk0, sector 3

    This patches fixes this by adding the missing driver type setting.

    Fixes: bb8175a8aa42 ("mmc: sdhci: clarify DDR timing mode ...")
    Signed-off-by: Jisheng Zhang <jszhang@marvell.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/mmc/host/sdhci.c
@@ -1134,6 +1134,7 @@ static u16 sdhci_get_preset_value(struct sdhci_host *host)
 		preset = sdhci_readw(host, SDHCI_PRESET_FOR_SDR104);
 		break;
 	case MMC_TIMING_UHS_DDR50:
+	case MMC_TIMING_MMC_DDR52:
 		preset = sdhci_readw(host, SDHCI_PRESET_FOR_DDR50);
 		break;
 	case MMC_TIMING_MMC_HS400:
@@ -1575,7 +1576,8 @@ static void sdhci_do_set_ios(struct sdhci_host *host, struct mmc_ios *ios)
 				 (ios->timing == MMC_TIMING_UHS_SDR25) ||
 				 (ios->timing == MMC_TIMING_UHS_SDR50) ||
 				 (ios->timing == MMC_TIMING_UHS_SDR104) ||
-				 (ios->timing == MMC_TIMING_UHS_DDR50))) {
+				 (ios->timing == MMC_TIMING_UHS_DDR50) ||
+				 (ios->timing == MMC_TIMING_MMC_DDR52))) {
 			u16 preset;
 
 			sdhci_enable_preset_value(host, true);