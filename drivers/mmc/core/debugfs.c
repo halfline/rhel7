torvalds/linux

commit 42cd95a0603e0497e7221816bb5ddfe861ee9325

mmc: core: debugfs: Add signal_voltage to ios dump
Signed-off-by: Johan Rudholm <johan.rudholm@stericsson.com>
Reviewed-by: Philip Rakity <prakity@nvidia.com>
Signed-off-by: Chris Ball <cjb@laptop.org>

drivers/mmc/core/debugfs.c
@@ -144,6 +144,22 @@ static int mmc_ios_show(struct seq_file *s, void *data)
 	}
 	seq_printf(s, "timing spec:\t%u (%s)\n", ios->timing, str);
 
+	switch (ios->signal_voltage) {
+	case MMC_SIGNAL_VOLTAGE_330:
+		str = "3.30 V";
+		break;
+	case MMC_SIGNAL_VOLTAGE_180:
+		str = "1.80 V";
+		break;
+	case MMC_SIGNAL_VOLTAGE_120:
+		str = "1.20 V";
+		break;
+	default:
+		str = "invalid";
+		break;
+	}
+	seq_printf(s, "signal voltage:\t%u (%s)\n", ios->chip_select, str);
+
 	return 0;
 }
 

halfline/rhel7

commit fde1f1d872ac6dc24e898a25cda3b99c0a4a9b22

[mmc] debugfs: correct wrong voltage value
Message-id: <1463503488-149391-707-git-send-email-dzickus@redhat.com>
Patchwork-id: 145082
O-Subject: [RHEL7 PATCH 706/725] mmc: debugfs: correct wrong voltage value
Bugzilla: 1127975 1277866 1280133 1286932 1297039
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>

commit 0036e74686344f1051afc3107740140abfd03616
Author: Chuanxiao Dong <chuanxiao.dong@intel.com>
Date:   Mon Jan 18 10:35:19 2016 +0100

    mmc: debugfs: correct wrong voltage value

    Correct the wrong voltage value shown in debugfs for mmc/sd/sdio.

    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Pawel Wodkowski <pawelx.wodkowski@intel.com>
    Fixes: 42cd95a0603e ("mmc: core: debugfs: Add signal_voltage to ios dump")
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/mmc/core/debugfs.c
@@ -170,7 +170,7 @@ static int mmc_ios_show(struct seq_file *s, void *data)
 		str = "invalid";
 		break;
 	}
-	seq_printf(s, "signal voltage:\t%u (%s)\n", ios->chip_select, str);
+	seq_printf(s, "signal voltage:\t%u (%s)\n", ios->signal_voltage, str);
 
 	switch (ios->drv_type) {
 	case MMC_SET_DRIVER_TYPE_A: