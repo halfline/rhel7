torvalds/linux

commit 52221610dd84dc3e9196554f0292ca9e8ab3541d

mmc: sdhci: Improve external VDD regulator support
A standard compliant SDHCI can itself supply VDD at 1.8, 3.0, or 3.3v.
Several vendors ignore this and instead rely upon external regulators
to supply VDD.  While the external regulators typically can supply one
of the standard SDHCI voltage levels, there is no real reason for this
to be a hard requirement.

This patch alters the SDHCI driver such that external VDD regulators
that provide voltages other than the three mentioned above may be used
so long as they can supply a voltage that meets the needs of the card.

In the case that an external VDD regulator is provided, it is reasonable
to ignore the voltage capabilities of the host controller and allow the
external regulator to set the OCR mask.  Additionally, there is no need
to convert a VDD voltage request into one of the standard SDHCI voltage
levels or program it in the host controller's power control register.

Signed-off-by: Tim Kryger <tim.kryger@gmail.com>
Tested-by: Sachin Kamat <spk.linux@gmail.com>
Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

drivers/mmc/host/sdhci.c
@@ -1226,6 +1226,13 @@ static void sdhci_set_power(struct sdhci_host *host, unsigned char mode,
 	struct mmc_host *mmc = host->mmc;
 	u8 pwr = 0;
 
+	if (!IS_ERR(mmc->supply.vmmc)) {
+		spin_unlock_irq(&host->lock);
+		mmc_regulator_set_ocr(host->mmc, mmc->supply.vmmc, vdd);
+		spin_lock_irq(&host->lock);
+		return;
+	}
+
 	if (mode != MMC_POWER_OFF) {
 		switch (1 << vdd) {
 		case MMC_VDD_165_195:
@@ -1283,12 +1290,6 @@ static void sdhci_set_power(struct sdhci_host *host, unsigned char mode,
 		 */
 		if (host->quirks & SDHCI_QUIRK_DELAY_AFTER_POWER)
 			mdelay(10);
-	}
-
-	if (!IS_ERR(mmc->supply.vmmc)) {
-		spin_unlock_irq(&host->lock);
-		mmc_regulator_set_ocr(host->mmc, mmc->supply.vmmc, vdd);
-		spin_lock_irq(&host->lock);
 	}
 }
 
@@ -3092,8 +3093,9 @@ int sdhci_add_host(struct sdhci_host *host)
 				   SDHCI_MAX_CURRENT_MULTIPLIER;
 	}
 
+	/* If OCR set by external regulators, use it instead */
 	if (mmc->ocr_avail)
-		ocr_avail &= mmc->ocr_avail;
+		ocr_avail = mmc->ocr_avail;
 
 	if (host->ocr_mask)
 		ocr_avail &= host->ocr_mask;

halfline/rhel7

commit 28980a66fc3a89ada6fbbf1d5587ab35901ce645

[mmc] sdhci: restore behavior when setting VDD via external regulator
Message-id: <1463503488-149391-701-git-send-email-dzickus@redhat.com>
Patchwork-id: 145293
O-Subject: [RHEL7 PATCH 700/725] mmc: sdhci: restore behavior when setting VDD via external regulator
Bugzilla: 1127975 1277866 1280133 1286932 1297039
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>

commit 918f4cbd4340ddd1eb389cd8efa3b07ac74ec4c0
Author: Jisheng Zhang <jszhang@marvell.com>
Date:   Fri Dec 11 21:36:29 2015 +0800

    mmc: sdhci: restore behavior when setting VDD via external regulator

    After commit 52221610dd84 ("mmc: sdhci: Improve external VDD regulator
    support"), for the VDD is supplied via external regulators, we ignore
    the code to convert a VDD voltage request into one of the standard
    SDHCI voltage levels, then program it in the SDHCI_POWER_CONTROL. This
    brings two issues:

    1. SDHCI_QUIRK2_CARD_ON_NEEDS_BUS_ON quirk isn't handled properly any
    more.

    2. What's more, once SDHCI_POWER_ON bit is set, some controllers such
    as the sdhci-pxav3 used in marvell berlin SoCs require the voltage
    levels programming in the SDHCI_POWER_CONTROL register, even the VDD
    is supplied by external regulator. So the host in marvell berlin SoCs
    still works fine after the commit. However, commit 3cbc6123a93d ("mmc:
    sdhci: Set SDHCI_POWER_ON with external vmmc") sets the SDHCI_POWER_ON
    bit, this would make the host in marvell berlin SoCs won't work any
    more with external vmmc.

    This patch restores the behavior when setting VDD through external
    regulator by moving the call of mmc_regulator_set_ocr() to the end
    of sdhci_set_power() function.

    After this patch, the sdcard on Marvell Berlin SoC boards work again.

    Signed-off-by: Jisheng Zhang <jszhang@marvell.com>
    Fixes: 52221610dd84 ("mmc: sdhci: Improve external VDD ...")

drivers/mmc/host/sdhci.c
@@ -1275,19 +1275,6 @@ static void sdhci_set_power(struct sdhci_host *host, unsigned char mode,
 	struct mmc_host *mmc = host->mmc;
 	u8 pwr = 0;
 
-	if (!IS_ERR(mmc->supply.vmmc)) {
-		spin_unlock_irq(&host->lock);
-		mmc_regulator_set_ocr(mmc, mmc->supply.vmmc, vdd);
-		spin_lock_irq(&host->lock);
-
-		if (mode != MMC_POWER_OFF)
-			sdhci_writeb(host, SDHCI_POWER_ON, SDHCI_POWER_CONTROL);
-		else
-			sdhci_writeb(host, 0, SDHCI_POWER_CONTROL);
-
-		return;
-	}
-
 	if (mode != MMC_POWER_OFF) {
 		switch (1 << vdd) {
 		case MMC_VDD_165_195:
@@ -1347,6 +1334,12 @@ static void sdhci_set_power(struct sdhci_host *host, unsigned char mode,
 		 */
 		if (host->quirks & SDHCI_QUIRK_DELAY_AFTER_POWER)
 			mdelay(10);
+	}
+
+	if (!IS_ERR(mmc->supply.vmmc)) {
+		spin_unlock_irq(&host->lock);
+		mmc_regulator_set_ocr(mmc, mmc->supply.vmmc, vdd);
+		spin_lock_irq(&host->lock);
 	}
 }
 