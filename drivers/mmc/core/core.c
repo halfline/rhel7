torvalds/linux

commit bd11e8bd03cae9e0499c34f67c55408566f6a089

mmc: core: Flag re-tuning is needed on CRC errors
CRC errors could possibly be alleviated by
re-tuning so flag re-tuning needed in those cases.
Note this has no effect if re-tuning has not been
enabled.

Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

drivers/mmc/core/core.c
@@ -133,6 +133,12 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 	struct mmc_command *cmd = mrq->cmd;
 	int err = cmd->error;
 
+	/* Flag re-tuning needed on CRC errors */
+	if (err == -EILSEQ || (mrq->sbc && mrq->sbc->error == -EILSEQ) ||
+	    (mrq->data && mrq->data->error == -EILSEQ) ||
+	    (mrq->stop && mrq->stop->error == -EILSEQ))
+		mmc_retune_needed(host);
+
 	if (err && cmd->retries && mmc_host_is_spi(host)) {
 		if (cmd->resp[0] & R1_SPI_ILLEGAL_COMMAND)
 			cmd->retries = 0;

halfline/rhel7

commit 0c652865dcfb72b0cc443aeabed29f5439e98aff

[mmc] core: fix dead loop of mmc_retune
Message-id: <1463503488-149391-635-git-send-email-dzickus@redhat.com>
Patchwork-id: 145132
O-Subject: [RHEL7 PATCH 634/725] mmc: core: fix dead loop of mmc_retune
Bugzilla: 1127975 1277866 1280133 1286932 1297039
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>

commit 031277d4d33d33f0174fbb569ca8f68238175617
Author: Chaotian Jing <chaotian.jing@mediatek.com>
Date:   Wed Sep 30 17:37:18 2015 +0800

    mmc: core: fix dead loop of mmc_retune

    When get a CRC error, start the mmc_retune, it will issue CMD19/CMD21
    to do tune, assume there were 10 clock phase need to try, phase 0 to
    phase 6 is ok, phase 7 to phase 9 is NG, we try it from 0 to 9, so
    the last CMD19/CMD21 will get CRC error, host->need_retune was set and
    cause mmc_retune was called, then dead loop of mmc_retune

    Signed-off-by: Chaotian Jing <chaotian.jing@mediatek.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Fixes: bd11e8bd03ca ("mmc: core: Flag re-tuning is needed on CRC errors")
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/mmc/core/core.c
@@ -133,9 +133,11 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 	int err = cmd->error;
 
 	/* Flag re-tuning needed on CRC errors */
-	if (err == -EILSEQ || (mrq->sbc && mrq->sbc->error == -EILSEQ) ||
+	if ((cmd->opcode != MMC_SEND_TUNING_BLOCK &&
+	    cmd->opcode != MMC_SEND_TUNING_BLOCK_HS200) &&
+	    (err == -EILSEQ || (mrq->sbc && mrq->sbc->error == -EILSEQ) ||
 	    (mrq->data && mrq->data->error == -EILSEQ) ||
-	    (mrq->stop && mrq->stop->error == -EILSEQ))
+	    (mrq->stop && mrq->stop->error == -EILSEQ)))
 		mmc_retune_needed(host);
 
 	if (err && cmd->retries && mmc_host_is_spi(host)) {