94d23c6108295aa939a4a11249967fa384960cd4
Sachin Prabhu <sprabhu@redhat.com>
2016-05-26 10:39:45 -0400

    [fs] Allow copy offload (CopyChunk) across shares
    Message-id: <1464259193-1648-41-git-send-email-sprabhu@redhat.com>
    Patchwork-id: 147717
    O-Subject: [RHEL7 PATCH fs 40/48] [bz 1337587]Allow copy offload (CopyChunk) across shares
    Bugzilla: 1337587
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Steve French <smfrench@gmail.com>
    
    Fixes bz: 1337587
    
    FSCTL_SRV_COPYCHUNK_WRITE only requires that the source and target
    be on the same server (not the same volume or same share),
    so relax the existing check (which required them to be on
    the same share). Note that this works to Windows (and presumably
    most other NAS) but Samba requires that the source
    and target be on the same share.  Moving a file across
    shares is a common use case and can be very heplful (100x faster).
    
    Signed-off-by: Steve French <steve.french@primarydata.com>
    Reviewed-by: David Disseldorp <ddiss@samba.org>
    (cherry picked from commit 7b52e2793a58af61b5d349c2c080437a437a4edb)
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -937,9 +937,7 @@ const struct file_operations cifs_file_ops = {
 	.mmap  = cifs_file_mmap,
 	.splice_read = generic_file_splice_read,
 	.llseek = cifs_llseek,
-#ifdef CONFIG_CIFS_POSIX
 	.unlocked_ioctl	= cifs_ioctl,
-#endif /* CONFIG_CIFS_POSIX */
 	.setlease = cifs_setlease,
 	.fallocate = cifs_fallocate,
 };
@@ -957,9 +955,7 @@ const struct file_operations cifs_file_strict_ops = {
 	.mmap = cifs_file_strict_mmap,
 	.splice_read = generic_file_splice_read,
 	.llseek = cifs_llseek,
-#ifdef CONFIG_CIFS_POSIX
 	.unlocked_ioctl	= cifs_ioctl,
-#endif /* CONFIG_CIFS_POSIX */
 	.setlease = cifs_setlease,
 	.fallocate = cifs_fallocate,
 };
@@ -977,9 +973,7 @@ const struct file_operations cifs_file_direct_ops = {
 	.flush = cifs_flush,
 	.mmap = cifs_file_mmap,
 	.splice_read = generic_file_splice_read,
-#ifdef CONFIG_CIFS_POSIX
 	.unlocked_ioctl  = cifs_ioctl,
-#endif /* CONFIG_CIFS_POSIX */
 	.llseek = cifs_llseek,
 	.setlease = cifs_setlease,
 	.fallocate = cifs_fallocate,
@@ -997,9 +991,7 @@ const struct file_operations cifs_file_nobrl_ops = {
 	.mmap  = cifs_file_mmap,
 	.splice_read = generic_file_splice_read,
 	.llseek = cifs_llseek,
-#ifdef CONFIG_CIFS_POSIX
 	.unlocked_ioctl	= cifs_ioctl,
-#endif /* CONFIG_CIFS_POSIX */
 	.setlease = cifs_setlease,
 	.fallocate = cifs_fallocate,
 };
@@ -1016,9 +1008,7 @@ const struct file_operations cifs_file_strict_nobrl_ops = {
 	.mmap = cifs_file_strict_mmap,
 	.splice_read = generic_file_splice_read,
 	.llseek = cifs_llseek,
-#ifdef CONFIG_CIFS_POSIX
 	.unlocked_ioctl	= cifs_ioctl,
-#endif /* CONFIG_CIFS_POSIX */
 	.setlease = cifs_setlease,
 	.fallocate = cifs_fallocate,
 };
@@ -1035,9 +1025,7 @@ const struct file_operations cifs_file_direct_nobrl_ops = {
 	.flush = cifs_flush,
 	.mmap = cifs_file_mmap,
 	.splice_read = generic_file_splice_read,
-#ifdef CONFIG_CIFS_POSIX
 	.unlocked_ioctl  = cifs_ioctl,
-#endif /* CONFIG_CIFS_POSIX */
 	.llseek = cifs_llseek,
 	.setlease = cifs_setlease,
 	.fallocate = cifs_fallocate,
