07087803e455a441443b3eab956af1556b7851e1
Ken Cox <jkc@redhat.com>
2016-05-26 18:12:30 -0400

    [netdrv] ixgbe: Bump version number
    Message-id: <fe85fe2618db8df824111509587a7b5385a3f8f7.1464282996.git.jkc@redhat.com>
    Patchwork-id: 147998
    O-Subject: [RHEL7.3 BZ 1274174 141/161] ixgbe: Bump version number
    Bugzilla: 1274174
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1274174
    
    Delta: RHEL DRV_VERSION is not the same as upstream
    
    commit 10ef00fe539a387ded9e0d710012500896589dbb
    Author: Mark Rustad <mark.d.rustad@intel.com>
    Date:   Fri Apr 1 12:18:51 2016 -0700
    
        ixgbe: Bump version number
    
        Update ixgbe version number.
    
        Signed-off-by: Mark Rustad <mark.d.rustad@intel.com>
        Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
        Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    
    Signed-off-by: Ken Cox <jkc@redhat.com>
    
    Cc: Ken Cox <jkc@redhat.com>
    Cc: John Greene <jogreene@redhat.com>
    Cc: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -76,7 +76,7 @@ char ixgbe_default_device_descr[] =
 static char ixgbe_default_device_descr[] =
 			      "Intel(R) 10 Gigabit Network Connection";
 #endif
-#define DRV_VERSION "4.2.1-k-rh7.2"
+#define DRV_VERSION "4.4.0-k"
 const char ixgbe_driver_version[] = DRV_VERSION;
 static const char ixgbe_copyright[] =
 				"Copyright (c) 1999-2016 Intel Corporation.";
