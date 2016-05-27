564fbbb5421eb931d7bcec7b5daed633660b7d52
Jerome Marchand <jmarchan@redhat.com>
2016-05-27 12:26:15 -0400

    [crypto] testmgr - don't copy from source IV too much
    Message-id: <1464351975-7831-8-git-send-email-jmarchan@redhat.com>
    Patchwork-id: 148073
    O-Subject: [RHEL7 PATCH 7/7] crypto: testmgr - don't copy from source IV too much
    Bugzilla: 1340073
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Herton R. Krzesinski <herton@redhat.com>
    
    Bugzilla:
    https://bugzilla.redhat.com/show_bug.cgi?id=1340073
    
    This is a backport of:
    commit 84cba178a3b88efe2668a9039f70abda072faa21
    Author: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Date:   Thu Sep 10 13:11:55 2015 +0300
    
        crypto: testmgr - don't copy from source IV too much
    
        While the destination buffer 'iv' is MAX_IVLEN size,
        the source 'template[i].iv' could be smaller, thus
        memcpy may read read invalid memory.
        Use crypto_skcipher_ivsize() to get real ivsize
        and pass it to memcpy.
    
        Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
        Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    On RHEL7, there is no crypto_skcipher_ivsize() function, so this patch
    uses crypto_ablkcipher_ivsize().
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -846,6 +846,7 @@ static int __test_skcipher(struct crypto_ablkcipher *tfm, int enc,
 	char *xbuf[XBUFSIZE];
 	char *xoutbuf[XBUFSIZE];
 	int ret = -ENOMEM;
+	unsigned int ivsize = crypto_ablkcipher_ivsize(tfm);
 
 	if (testmgr_alloc_buf(xbuf))
 		goto out_nobuf;
@@ -878,7 +879,7 @@ static int __test_skcipher(struct crypto_ablkcipher *tfm, int enc,
 	j = 0;
 	for (i = 0; i < tcount; i++) {
 		if (template[i].iv)
-			memcpy(iv, template[i].iv, MAX_IVLEN);
+			memcpy(iv, template[i].iv, ivsize);
 		else
 			memset(iv, 0, MAX_IVLEN);
 
@@ -953,7 +954,7 @@ static int __test_skcipher(struct crypto_ablkcipher *tfm, int enc,
 	for (i = 0; i < tcount; i++) {
 
 		if (template[i].iv)
-			memcpy(iv, template[i].iv, MAX_IVLEN);
+			memcpy(iv, template[i].iv, ivsize);
 		else
 			memset(iv, 0, MAX_IVLEN);
 
