daedc0d0537f1bb1cfd26c942f418d143887028d
Jakub Sitnicki <jkbs@redhat.com>
2016-05-31 12:19:11 -0400

    [net] veth: implement ndo_set_rx_headroom
    Message-id: <2048fece0047c70e7dc3947682c1fb3e56740614.1464689230.git.jkbs@redhat.com>
    Patchwork-id: 148138
    O-Subject: [RHEL7.3 net PATCH v2 6/7] veth: implement ndo_set_rx_headroom
    Bugzilla: 1322337
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Lance Richardson <lrichard@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1322337
    Upstream Status: commit 163e529
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11111896
    Tested: on wsfd-netdev1.lab.bos.redhat.com
    Conflicts: \
    * ndo_set_rx_headroom is a member of extended net_device_ops
    
    * in context in veth_netdev_ops structure because we are missing
      upstream commit 1a04a82156ec ("veth: Don't segment multiple tagged
      packets on veth device")
    
    commit 163e529200af7b7521fbde5dbcc653cf3ce597df
    Author: Paolo Abeni <pabeni@redhat.com>
    Date:   Fri Feb 26 10:45:41 2016 +0100
    
        veth: implement ndo_set_rx_headroom
    
        The rx headroom for veth dev is the peer device needed_headroom.
        Avoid ping-pong updates setting the private flag IFF_PHONY_HEADROOM.
    
        This avoids skb head reallocation when forwarding from a veth dev
        towards a device adding some kind of encapsulation.
    
        When transmitting frames below the MTU size towards a vxlan device,
        this gives about 10% performance speed-up when OVS is used to connect
        the veth and the vxlan device and a little more when using a
        plain Linux bridge.
    
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jakub Sitnicki <jkbs@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/veth.c
+++ drivers/net/veth.c
@@ -35,6 +35,7 @@ struct pcpu_vstats {
 struct veth_priv {
 	struct net_device __rcu	*peer;
 	atomic64_t		dropped;
+	unsigned		requested_headroom;
 };
 
 /*
@@ -252,6 +253,29 @@ static int veth_get_iflink(const struct net_device *dev)
 	return iflink;
 }
 
+static void veth_set_rx_headroom(struct net_device *dev, int new_hr)
+{
+	struct veth_priv *peer_priv, *priv = netdev_priv(dev);
+	struct net_device *peer;
+
+	if (new_hr < 0)
+		new_hr = 0;
+
+	rcu_read_lock();
+	peer = rcu_dereference(priv->peer);
+	if (unlikely(!peer))
+		goto out;
+
+	peer_priv = netdev_priv(peer);
+	priv->requested_headroom = new_hr;
+	new_hr = max(priv->requested_headroom, peer_priv->requested_headroom);
+	dev->needed_headroom = new_hr;
+	peer->needed_headroom = new_hr;
+
+out:
+	rcu_read_unlock();
+}
+
 static const struct net_device_ops veth_netdev_ops = {
 	.ndo_init            = veth_dev_init,
 	.ndo_open            = veth_open,
@@ -261,6 +285,8 @@ static const struct net_device_ops veth_netdev_ops = {
 	.ndo_get_stats64     = veth_get_stats64,
 	.ndo_set_mac_address = eth_mac_addr,
 	.ndo_get_iflink		= veth_get_iflink,
+	.ndo_size		= sizeof(struct net_device_ops),
+	.extended.ndo_set_rx_headroom	= veth_set_rx_headroom,
 };
 
 #define VETH_FEATURES (NETIF_F_SG | NETIF_F_FRAGLIST | NETIF_F_ALL_TSO |    \
@@ -277,6 +303,7 @@ static void veth_setup(struct net_device *dev)
 	dev->priv_flags &= ~IFF_TX_SKB_SHARING;
 	dev->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	dev->priv_flags |= IFF_NO_QUEUE;
+	dev->priv_flags |= IFF_PHONY_HEADROOM;
 
 	dev->netdev_ops = &veth_netdev_ops;
 	dev->ethtool_ops = &veth_ethtool_ops;
