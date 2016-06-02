93662f037dd22c10162e5c8c18f658f6375da6c0
Xunlei Pang <xlpang@redhat.com>
2016-06-02 07:57:42 -0400

    [kernel] rtmutex: Turn the plist into an rb-tree
    Message-id: <1464854352-12026-11-git-send-email-xlpang@redhat.com>
    Patchwork-id: 148459
    O-Subject: [RHEL7.3 PATCH v2 010/100] rtmutex: Turn the plist into an rb-tree
    Bugzilla: 1298387
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Clark Williams <williams@redhat.com>
    RH-Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    Resolves:
    https://bugzilla.redhat.com/show_bug.cgi?id=1298387
    
    Brew build:
    https://brewweb.devel.redhat.com/taskinfo?taskID=11123613
    
    Tested:
    By me on my x86_64 laptop.
    
    This is back ported from upstream. This upstream patch modified
    "struct rt_mutex" and "struct task_struct" which seems cause KABI
    breakage. Worse still, "struct rt_mutex" can be used as memembers
    (not pointers) embedded in other structures(as an example in drivers,
    "struct i2c_adapter::bus_lock"), but fortunately the size of "struct
    rt_mutex" before and after modification stays invariant and these
    modified members are only used internally by the logic of rtmutex
    module. So, we can safely resolve the KABI breakage by applying the
    same logic of RH_KABI_REPLACE():
      #ifdef __GENKSYMS__
    	  struct plist_head	zzz;
      #else
    	  struct rb_root        xxx;
    	  struct rb_node       *yyy;
      #endif
    
    "struct rt_mutex_waiter" was also modified, it doesn't matter for code
    function as it is only an internal structure and referenced via pointers
    by other sturctures like "struct task_struct". But kernel's CRC algorithm
    will change the CRC value of following exported interfaces:
        __rt_mutex_init
        rt_mutex_unlock
        rt_mutex_destroy
        rt_mutex_lock_interruptible
        rt_mutex_lock
        rt_mutex_timed_lock
        rt_mutex_trylock
    
    so also applying the following logic to "struct rt_mutex_waiter":
        #ifdef __GENKSYMS__
            struct plist_node   list_entry;
            struct plist_node   pi_list_entry;
        #else
            struct rb_node          tree_entry;
            struct rb_node          pi_tree_entry;
        #endif
    
    commit fb00aca474405f4fa8a8519c3179fed722eabd83
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Thu Nov 7 14:43:43 2013 +0100
    
        rtmutex: Turn the plist into an rb-tree
    
    	Turn the pi-chains from plist to rb-tree, in the rt_mutex code,
    	and provide a proper comparison function for -deadline and
    	-priority tasks.
    
    	This is done mainly because:
    	 - classical prio field of the plist is just an int, which might
    	   not be enough for representing a deadline;
    	 - manipulating such a list would become O(nr_deadline_tasks),
    	   which might be to much, as the number of -deadline task increases.
    
    	Therefore, an rb-tree is used, and tasks are queued in it according
    	to the following logic:
    	 - among two -priority (i.e., SCHED_BATCH/OTHER/RR/FIFO) tasks, the
    	   one with the higher (lower, actually!) prio wins;
    	 - among a -priority and a -deadline task, the latter always wins;
    	 - among two -deadline tasks, the one with the earliest deadline
    	   wins.
    
    	Queueing and dequeueing functions are changed accordingly, for both
    	the list of a task's pi-waiters and the list of tasks blocked on
    	a pi-lock.
    
    	Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    	Signed-off-by: Dario Faggioli <raistlin@linux.it>
    	Signed-off-by: Juri Lelli <juri.lelli@gmail.com>
    	Signed-off-again-by: Peter Zijlstra <peterz@infradead.org>
    	Link: http://lkml.kernel.org/r/1383831828-15501-10-git-send-email-juri.lelli@gmail.com
    	Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Xunlei Pang <xlpang@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -16,6 +16,7 @@ struct sched_param {
 #include <linux/types.h>
 #include <linux/timex.h>
 #include <linux/jiffies.h>
+#include <linux/plist.h>
 #include <linux/rbtree.h>
 #include <linux/thread_info.h>
 #include <linux/cpumask.h>
@@ -1456,7 +1457,12 @@ struct task_struct {
 
 #ifdef CONFIG_RT_MUTEXES
 	/* PI waiters blocked on a rt_mutex held by this task */
+#ifndef __GENKSYMS__
+	struct rb_root pi_waiters;
+	struct rb_node *pi_waiters_leftmost;
+#else
 	struct plist_head pi_waiters;
+#endif
 	/* Deadlock detection and priority inheritance handling */
 	struct rt_mutex_waiter *pi_blocked_on;
 #endif
