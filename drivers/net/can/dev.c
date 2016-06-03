c6ae5e6103a7548fa8254b99f4432337d46ffe43
John Linville <linville@redhat.com>
2016-06-03 18:02:23 -0400

    [net] can: fix loss of CAN frames in raw_rcv
    Message-id: <a3e9549827ce527add54a8eef4c8245cd1febf71.1464976163.git.linville@tuxdriver.com>
    Patchwork-id: 149034
    O-Subject: [RHEL7 PATCH 31/33] can: fix loss of CAN frames in raw_rcv
    Bugzilla: 1333130
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    As reported by Manfred Schlaegl here
    
       http://marc.info/?l=linux-netdev&m=143482089824232&w=2
    
    commit 514ac99c64b "can: fix multiple delivery of a single CAN frame for
    overlapping CAN filters" requires the skb->tstamp to be set to check for
    identical CAN skbs.
    
    As net timestamping is influenced by several players (netstamp_needed and
    netdev_tstamp_prequeue) Manfred missed a proper timestamp which leads to
    CAN frame loss.
    
    As skb timestamping became now mandatory for CAN related skbs this patch
    makes sure that received CAN skbs always have a proper timestamp set.
    Maybe there's a better solution in the future but this patch fixes the
    CAN frame loss so far.
    
    alloc_canfd_skb is not used or even defined in RHEL7, so those bits
    have been excluded from the cherry-picked patch. -- JWL
    
    Reported-by: Manfred Schlaegl <manfred.schlaegl@gmx.at>
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>
    Cc: linux-stable <stable@vger.kernel.org>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    (cherry picked from commit 36c01245eb8046c16eee6431e7dbfbb302635fa8)
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/can/dev.c
+++ drivers/net/can/dev.c
@@ -376,6 +376,9 @@ unsigned int can_get_echo_skb(struct net_device *dev, unsigned int idx)
 		struct can_frame *cf = (struct can_frame *)skb->data;
 		u8 dlc = cf->can_dlc;
 
+		if (!(skb->tstamp.tv64))
+			__net_timestamp(skb);
+
 		netif_rx(priv->echo_skb[idx]);
 		priv->echo_skb[idx] = NULL;
 
@@ -512,6 +515,7 @@ struct sk_buff *alloc_can_skb(struct net_device *dev, struct can_frame **cf)
 	if (unlikely(!skb))
 		return NULL;
 
+	__net_timestamp(skb);
 	skb->protocol = htons(ETH_P_CAN);
 	skb->pkt_type = PACKET_BROADCAST;
 	skb->ip_summed = CHECKSUM_UNNECESSARY;
