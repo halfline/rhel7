4184a06fe0f6f70dec9bee2e78b6175e82542f6b
Mike Snitzer <snitzer@redhat.com>
2016-06-09 18:06:49 -0400

    [md] dm raid: make sure no feature flags are set in metadata
    Message-id: <1465495611-23310-11-git-send-email-snitzer@redhat.com>
    Patchwork-id: 149900
    O-Subject: [RHEL7.3 PATCH 10/12] dm raid: make sure no feature flags are set in metadata
    Bugzilla: 1337254
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    BZ: 1337254
    
    Upstream commit 4c9971ca6a172e70f52a7f9b6796e843c3f70293
    Author: Heinz Mauelshagen <heinzm@redhat.com>
    Date:   Fri Apr 29 18:59:56 2016 +0200
    
        dm raid: make sure no feature flags are set in metadata
    
        Given we don't yet support any feature flags in the dm-raid ondisk
        metadata (see: 'features' member of 'struct dm_raid_superblock'),
        add a check to ensure no flags are actually set, if any features are
        set reject the activation of the RAID mapping.
    
        This is to prevent possible data corruption in case of a kernel
        downgrade when there'll potentially be feature flags set by a future
        dm-raid target.
    
        Signed-off-by: Heinz Mauelshagen <heinzm@redhat.com>
        Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/dm-raid.c
+++ drivers/md/dm-raid.c
@@ -1037,6 +1037,11 @@ static int super_validate(struct raid_set *rs, struct md_rdev *rdev)
 	if (!mddev->events && super_init_validation(mddev, rdev))
 		return -EINVAL;
 
+	if (le32_to_cpu(sb->features)) {
+		rs->ti->error = "Unable to assemble array: No feature flags supported yet";
+		return -EINVAL;
+	}
+
 	/* Enable bitmap creation for RAID levels != 0 */
 	mddev->bitmap_info.offset = (rs->raid_type->level) ? to_sector(4096) : 0;
 	rdev->mddev->bitmap_info.default_offset = mddev->bitmap_info.offset;
@@ -1736,7 +1741,7 @@ static int raid_merge(struct dm_target *ti, struct bvec_merge_data *bvm,
 
 static struct target_type raid_target = {
 	.name = "raid",
-	.version = {1, 7, 0},
+	.version = {1, 8, 0},
 	.module = THIS_MODULE,
 	.ctr = raid_ctr,
 	.dtr = raid_dtr,
