42675c2cc6b8acf9add6e31cb9fbc4b35d173e62
Marcelo Leitner <mleitner@redhat.com>
2016-06-09 17:52:23 -0400

    [net] sctp: move skb_dst_set() a bit downwards in sctp_packet_transmit()
    Message-id: <6a030bbdcb837c2c500a95bbf7f18660815c7b7b.1465482606.git.mleitner@redhat.com>
    Patchwork-id: 149876
    O-Subject: [PATCH 02/16] sctp: move skb_dst_set() a bit downwards in sctp_packet_transmit()
    Bugzilla: 1278912
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1278912
    Backport of upstream commit: 0438816efd86c764d5544bf52e769604062316b3
    
     commit 0438816efd86c764d5544bf52e769604062316b3
     Author: wangweidong <wangweidong1@huawei.com>
     Date:   Thu Dec 26 13:55:56 2013 +0800
    
        sctp: move skb_dst_set() a bit downwards in sctp_packet_transmit()
    
        skb_dst_set will use dst, if dst is NULL although is not a problem,
        then goto the 'no_route' and free nskb, so do the skb_dst_set is pointless.
        so move the skb_dst_set after dst check.
        Remove the unnecessary initialization as well.
    
        v2: fix the subject line because it would confuse people,
            as pointed out by Daniel.
    
        Signed-off-by: Wang Weidong <wangweidong1@huawei.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Marcelo Ricardo Leitner <mleitner@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sctp/output.c
+++ net/sctp/output.c
@@ -398,7 +398,7 @@ int sctp_packet_transmit(struct sctp_packet *packet, gfp_t gfp)
 	int err = 0;
 	int padding;		/* How much padding do we need?  */
 	__u8 has_data = 0;
-	struct dst_entry *dst = tp->dst;
+	struct dst_entry *dst;
 	unsigned char *auth = NULL;	/* pointer to auth in skb data */
 
 	SCTP_DEBUG_PRINTK("%s: packet:%p\n", __func__, packet);
@@ -431,9 +431,9 @@ int sctp_packet_transmit(struct sctp_packet *packet, gfp_t gfp)
 		}
 	}
 	dst = dst_clone(tp->dst);
-	skb_dst_set(nskb, dst);
 	if (!dst)
 		goto no_route;
+	skb_dst_set(nskb, dst);
 
 	/* Build the SCTP header.  */
 	sh = (struct sctphdr *)skb_push(nskb, sizeof(struct sctphdr));
