4f0594fa75e1dbb876720e6b51f900929d6c56ae
Gustavo Duarte <gduarte@redhat.com>
2016-06-10 18:35:27 -0400

    [tools] perf kvm/{x86, s390}: Remove const from kvm_events_tp
    Message-id: <1465583729-27245-3-git-send-email-gduarte@redhat.com>
    Patchwork-id: 150321
    O-Subject: [RHEL7.3 PATCH BZ 1223849 2/4] perf kvm/{x86, s390}: Remove const from kvm_events_tp
    Bugzilla: 1223849
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    BZ 1223849
    Upstream Status: 48deaa74fcdad516a94fe38a4af706747d9e4745
    
    commit 48deaa74fcdad516a94fe38a4af706747d9e4745
    Author: Hemant Kumar <hemant@linux.vnet.ibm.com>
    Date:   Thu Jan 28 12:03:05 2016 +0530
    
        perf kvm/{x86,s390}: Remove const from kvm_events_tp
    
        This patch removes the "const" qualifier from kvm_events_tp declaration
        to account for the fact that some architectures may need to update this
        variable dynamically. For instance, powerpc will need to update this
        variable dynamically depending on the machine type.
    
        Signed-off-by: Hemant Kumar <hemant@linux.vnet.ibm.com>
        Acked-by: David Ahern <dsahern@gmail.com>
        Cc: Alexander Yarygin <yarygin@linux.vnet.ibm.com>
        Cc: Michael Ellerman <mpe@ellerman.id.au>
        Cc: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
        Cc: Paul Mackerras <paulus@samba.org>
        Cc: Scott  Wood <scottwood@freescale.com>
        Cc: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
        Cc: linuxppc-dev@lists.ozlabs.org
        Link: http://lkml.kernel.org/r/1453962787-15376-2-git-send-email-hemant@linux.vnet.ibm.com
        Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	tools/perf/arch/s390/util/kvm-stat.c (this file is not present
    in RHEL7)

--- tools/perf/util/kvm-stat.h
+++ tools/perf/util/kvm-stat.h
@@ -125,7 +125,7 @@ int setup_kvm_events_tp(struct perf_kvm_stat *kvm);
  */
 int cpu_isa_init(struct perf_kvm_stat *kvm, const char *cpuid);
 
-extern const char * const kvm_events_tp[];
+extern const char *kvm_events_tp[];
 extern struct kvm_reg_events_ops kvm_reg_events_ops[];
 extern const char * const kvm_skip_events[];
 extern const char *vcpu_id_str;
