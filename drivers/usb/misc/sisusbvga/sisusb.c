e71c6e98393a2917c5dc08066928db05cf375608
Torez Smith <torez@redhat.com>
2016-06-10 02:26:11 -0400

    [usb] usb-misc: sisusbvga: Remove memory allocation logs
    Message-id: <1465525749-9195-19-git-send-email-torez@redhat.com>
    Patchwork-id: 149963
    O-Subject: [RHEL7 BZ1332712 v2 018/196] usb-misc: sisusbvga: Remove memory allocation logs
    Bugzilla: 1332712
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1332712
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11164417
    Testing: on local desktop, Dell Berlinetta-P laptop, and, on some customer
        platforms.
    
    commit f40849b86862c00f24db10bc11fe2e972c029ec4
    Author: Peter Senna Tschudin <peter.senna@collabora.com>
    Date:   Fri Jan 15 18:41:33 2016 +0100
    
        usb-misc: sisusbvga: Remove memory allocation logs
    
        This patch remove three calls to dev_err() from sisusb_probe() as
        reporting memory allocation failures is redundant:
    
         - Remove a call to dev_err() that was reporting unsuccesful call to
           kzalloc().
    
         - Remove two calls to dev_err() that were reporting unsuccesful calls
           to kmalloc().
    
        One call to dev_err() reporting memory allocation is left unchanged as
        the last patch of the series removes it.
    
        Signed-off-by: Peter Senna Tschudin <peter.senna@collabora.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/usb/misc/sisusbvga/sisusb.c
+++ drivers/usb/misc/sisusbvga/sisusb.c
@@ -3050,10 +3050,9 @@ static int sisusb_probe(struct usb_interface *intf,
 
 	/* Allocate memory for our private */
 	sisusb = kzalloc(sizeof(*sisusb), GFP_KERNEL);
-	if (!sisusb) {
-		dev_err(&dev->dev, "Failed to allocate memory for private data\n");
+	if (!sisusb)
 		return -ENOMEM;
-	}
+
 	kref_init(&sisusb->kref);
 
 	mutex_init(&(sisusb->lock));
@@ -3080,7 +3079,6 @@ static int sisusb_probe(struct usb_interface *intf,
 	sisusb->ibufsize = SISUSB_IBUF_SIZE;
 	sisusb->ibuf = kmalloc(SISUSB_IBUF_SIZE, GFP_KERNEL);
 	if (!sisusb->ibuf) {
-		dev_err(&sisusb->sisusb_dev->dev, "Failed to allocate memory for input buffer");
 		retval = -ENOMEM;
 		goto error_2;
 	}
@@ -3091,7 +3089,6 @@ static int sisusb_probe(struct usb_interface *intf,
 		sisusb->obuf[i] = kmalloc(SISUSB_OBUF_SIZE, GFP_KERNEL);
 		if (!sisusb->obuf[i]) {
 			if (i == 0) {
-				dev_err(&sisusb->sisusb_dev->dev, "Failed to allocate memory for output buffer\n");
 				retval = -ENOMEM;
 				goto error_3;
 			}
