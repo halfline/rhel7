472048bf8e5d6899893fec596e617697d2299937
Rob Clark <rclark@redhat.com>
2016-06-10 00:16:07 -0400

    [gpu] drm/atomic: Verify connector->funcs != NULL when clearing states
    Message-id: <1465517793-14679-9-git-send-email-rclark@redhat.com>
    Patchwork-id: 149919
    O-Subject: [RHEL7 drm 08/34] drm/atomic: Verify connector->funcs != NULL when clearing states
    Bugzilla: 1021582 1171268 1171270 1238635 1243336 1243398 1255760 1269009 1273363 1287757 1293302 1300583 1310232 1328491 1336544 1344498 1344510
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Lyude Paul <cpaul@redhat.com>
    RH-Acked-by: Dave Airlie <airlied@redhat.com>
    
    Upstream: since v4.6.2
    RHBZ: 1344510
    
    commit 5d82a4b5bc708ab30cf41cb024f8d45b31987374
    
    Author:     Lyude <cpaul@redhat.com>
    AuthorDate: Tue May 31 12:49:07 2016 -0400
    Commit:     Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CommitDate: Tue Jun 7 18:23:40 2016 -0700
    
        drm/atomic: Verify connector->funcs != NULL when clearing states
    
        Unfortunately since we don't have Dave's connector refcounting patch
        here yet, it's very possible that drm_atomic_state_default_clear() could
        get called by intel_display_resume() when
        intel_dp_mst_destroy_connector() isn't completely finished destroying an
        mst connector, but has already finished setting connector->funcs to
        NULL. As such, we need to treat the connector like it's already been
        destroyed and just skip it, otherwise we'll end up dereferencing a NULL
        pointer.
    
        This fix is only required for 4.6 and below. David Airlie's patchseries
        for 4.7 to add connector reference counting provides a more proper fix
        for this.
    
        Changes since v1:
         - Fix leftover whitespace
    
        Upstream fix: 0552f7651bc2 ("drm/i915/mst: use reference counted
        connectors. (v3)")
        Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
        Signed-off-by: Lyude <cpaul@redhat.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Rob Clark <rclark@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -139,7 +139,7 @@ void drm_atomic_state_default_clear(struct drm_atomic_state *state)
 	for (i = 0; i < state->num_connector; i++) {
 		struct drm_connector *connector = state->connectors[i];
 
-		if (!connector)
+		if (!connector || !connector->funcs)
 			continue;
 
 		/*
@@ -150,6 +150,7 @@ void drm_atomic_state_default_clear(struct drm_atomic_state *state)
 		 * case by setting all connector pointers to NULL.
 		 */
 		state->connector_states[i]->connector = NULL;
+
 		connector->funcs->atomic_destroy_state(NULL,
 						       state->connector_states[i]);
 		state->connectors[i] = NULL;
