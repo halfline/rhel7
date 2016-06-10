0b36bed232396f6b0aaa69006097ed3ebac8e199
Torez Smith <torez@redhat.com>
2016-06-10 02:28:48 -0400

    [include] usb: define USB_SPEED_SUPER_PLUS speed for SuperSpeedPlus USB3.1 devices
    Message-id: <1465525749-9195-176-git-send-email-torez@redhat.com>
    Patchwork-id: 150119
    O-Subject: [RHEL7 BZ1332712 v2 175/196] usb: define USB_SPEED_SUPER_PLUS speed for SuperSpeedPlus USB3.1 devices
    Bugzilla: 1332712
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1332712
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11164417
    Testing: on local desktop, Dell Berlinetta-P laptop, and, on some customer
        platforms.
    
    commit 8a1b2725a60d3267135c15e80984b4406054f650
    Author: Mathias Nyman <mathias.nyman@linux.intel.com>
    Date:   Thu Dec 10 09:59:25 2015 +0200
    
        usb: define USB_SPEED_SUPER_PLUS speed for SuperSpeedPlus USB3.1 devices
    
        Add a new USB_SPEED_SUPER_PLUS device speed, and make sure usb core can
        handle the new speed.
        In most cases the behaviour is the same as with USB_SPEED_SUPER SuperSpeed
        devices. In a few places we add a "Plus" string to inform the user of the
        new speed.
    
        Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/usb/core/devices.c
+++ drivers/usb/core/devices.c
@@ -214,7 +214,7 @@ static char *usb_dump_endpoint_descriptor(int speed, char *start, char *end,
 		break;
 	case USB_ENDPOINT_XFER_INT:
 		type = "Int.";
-		if (speed == USB_SPEED_HIGH || speed == USB_SPEED_SUPER)
+		if (speed == USB_SPEED_HIGH || speed >= USB_SPEED_SUPER)
 			interval = 1 << (desc->bInterval - 1);
 		else
 			interval = desc->bInterval;
@@ -223,7 +223,7 @@ static char *usb_dump_endpoint_descriptor(int speed, char *start, char *end,
 		return start;
 	}
 	interval *= (speed == USB_SPEED_HIGH ||
-		     speed == USB_SPEED_SUPER) ? 125 : 1000;
+		     speed >= USB_SPEED_SUPER) ? 125 : 1000;
 	if (interval % 1000)
 		unit = 'u';
 	else {
@@ -315,7 +315,7 @@ static char *usb_dump_config_descriptor(char *start, char *end,
 
 	if (start > end)
 		return start;
-	if (speed == USB_SPEED_SUPER)
+	if (speed >= USB_SPEED_SUPER)
 		mul = 8;
 	else
 		mul = 2;
@@ -527,6 +527,8 @@ static ssize_t usb_device_dump(char __user **buffer, size_t *nbytes,
 		speed = "480"; break;
 	case USB_SPEED_SUPER:
 		speed = "5000"; break;
+	case USB_SPEED_SUPER_PLUS:
+		speed = "10000"; break;
 	default:
 		speed = "??";
 	}
@@ -546,7 +548,7 @@ static ssize_t usb_device_dump(char __user **buffer, size_t *nbytes,
 
 		/* super/high speed reserves 80%, full/low reserves 90% */
 		if (usbdev->speed == USB_SPEED_HIGH ||
-		    usbdev->speed == USB_SPEED_SUPER)
+		    usbdev->speed >= USB_SPEED_SUPER)
 			max = 800;
 		else
 			max = FRAME_TIME_MAX_USECS_ALLOC;
