be519d23bf8603f54ee6674f64605e2c76632936
Don Dutile <ddutile@redhat.com>
2016-06-10 22:24:40 -0400

    [netdrv] cxgb4: Add FL DMA mapping error and low counter
    Message-id: <1465597803-11831-681-git-send-email-ddutile@redhat.com>
    Patchwork-id: 151177
    O-Subject: [PATCH RHEL-7.3 0680/1003] cxgb4: Add FL DMA mapping error and low counter
    Bugzilla: 1169955 1259940 1262728 1275187 1275209 1275423 1275425 1289615 1291874 1292284 1292872 1296195 1296269 1296338 1296344 1298707 1302166 1310156
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Laurence Oberman <loberman@redhat.com>
    
    From: Hariprasad Shenai <hariprasad@chelsio.com>
    
    Add Free List DMA Mapping Errors to SGE Queue info for
    Free Lists. Add Free List "Low" counter to count the number of times we
    see the number of pointers that we _think_ the hardware sees in the
    Free List below the Egress Threshold.
    
    Signed-off-by: Hariprasad Shenai <hariprasad@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 70055dd0640666a8ffe7c98e91f0b36fd380fe2a)
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/chelsio/cxgb4/sge.c
+++ drivers/net/ethernet/chelsio/cxgb4/sge.c
@@ -610,6 +610,7 @@ static unsigned int refill_fl(struct adapter *adap, struct sge_fl *q, int n,
 				       PCI_DMA_FROMDEVICE);
 		if (unlikely(dma_mapping_error(adap->pdev_dev, mapping))) {
 			__free_pages(pg, s->fl_pg_order);
+			q->mapping_err++;
 			goto out;   /* do not try small pages for this error */
 		}
 		mapping |= RX_LARGE_PG_BUF;
@@ -639,6 +640,7 @@ alloc_small_pages:
 				       PCI_DMA_FROMDEVICE);
 		if (unlikely(dma_mapping_error(adap->pdev_dev, mapping))) {
 			put_page(pg);
+			q->mapping_err++;
 			goto out;
 		}
 		*d++ = cpu_to_be64(mapping);
@@ -660,6 +662,7 @@ out:	cred = q->avail - cred;
 
 	if (unlikely(fl_starving(adap, q))) {
 		smp_wmb();
+		q->low++;
 		set_bit(q->cntxt_id - adap->sge.egr_start,
 			adap->sge.starving_fl);
 	}
