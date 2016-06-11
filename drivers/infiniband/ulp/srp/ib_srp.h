f5ff1e15b05932b20cd25554507208de98252e5d
Don Dutile <ddutile@redhat.com>
2016-06-10 22:24:44 -0400

    [infiniband] ib/srp: use the new CQ API
    Message-id: <1465597803-11831-685-git-send-email-ddutile@redhat.com>
    Patchwork-id: 151232
    O-Subject: [PATCH RHEL-7.3 0684/1003] IB/srp: use the new CQ API
    Bugzilla: 1169955 1259940 1262728 1275187 1275209 1275423 1275425 1289615 1291874 1292284 1292872 1296195 1296269 1296338 1296344 1298707 1302166 1310156
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Laurence Oberman <loberman@redhat.com>
    
    From: Christoph Hellwig <hch@lst.de>
    
    This also moves recv completion handling from hardirq context into
    softirq context.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 1dc7b1f10dcbbaf875a0770b2b26d63bc326d92a)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	drivers/infiniband/ulp/srp/ib_srp.c
    Interface diff with btwn rhel7 using sg_nents and upstream patch.
    	drivers/infiniband/ulp/srp/ib_srp.h
    Minor diffs wrt RHEL7 source; applied upstream patches.
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>

--- drivers/infiniband/ulp/srp/ib_srp.h
+++ drivers/infiniband/ulp/srp/ib_srp.h
@@ -67,11 +67,6 @@ enum {
 	SRP_TAG_TSK_MGMT	= 1U << 31,
 
 	SRP_MAX_PAGES_PER_MR	= 512,
-
-	LOCAL_INV_WR_ID_MASK	= 1,
-	FAST_REG_WR_ID_MASK	= 2,
-
-	SRP_LAST_WR_ID		= 0xfffffffcU,
 };
 
 enum srp_target_state {
@@ -129,6 +124,7 @@ struct srp_request {
 	struct srp_direct_buf  *indirect_desc;
 	dma_addr_t		indirect_dma_addr;
 	short			nmdesc;
+	struct ib_cqe		reg_cqe;
 };
 
 /**
@@ -232,6 +228,7 @@ struct srp_iu {
 	void		       *buf;
 	size_t			size;
 	enum dma_data_direction	direction;
+	struct ib_cqe		cqe;
 };
 
 /**
