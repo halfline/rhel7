edd4c7e9b8942a91ceed3ff7a6f0197aa14433ab
Ilya Dryomov <idryomov@redhat.com>
2016-06-13 14:56:20 -0400

    [fs] ceph: don't enable rbytes mount option by default
    Message-id: <1465829862-17784-92-git-send-email-idryomov@redhat.com>
    Patchwork-id: 151520
    O-Subject: [RHEL7.3 PATCH 091/173] ceph: don't enable rbytes mount option by default
    Bugzilla: 1344930
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    From: "Yan, Zheng" <zyan@redhat.com>
    
    When rbytes mount option is enabled, directory size is recursive
    size. Recursive size is not updated instantly. This can cause
    directory size to change between successive stat(1)
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    (cherry picked from commit 133e91566ced3e7154db9b68bb3ceb3b30f6900f)
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/ceph/super.h
+++ fs/ceph/super.h
@@ -31,8 +31,7 @@
 #define CEPH_MOUNT_OPT_DCACHE          (1<<9) /* use dcache for readdir etc */
 #define CEPH_MOUNT_OPT_NOPOOLPERM      (1<<11) /* no pool permission check */
 
-#define CEPH_MOUNT_OPT_DEFAULT    (CEPH_MOUNT_OPT_RBYTES | \
-				   CEPH_MOUNT_OPT_DCACHE)
+#define CEPH_MOUNT_OPT_DEFAULT    CEPH_MOUNT_OPT_DCACHE
 
 #define ceph_set_mount_opt(fsc, opt) \
 	(fsc)->mount_options->flags |= CEPH_MOUNT_OPT_##opt;
