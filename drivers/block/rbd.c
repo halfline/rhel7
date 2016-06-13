67c342d1020e145261a72083464b6916de0fd96d
Ilya Dryomov <idryomov@redhat.com>
2016-06-13 15:12:01 -0400

    [fs] rbd: rbd_dev_header_unwatch_sync() variant
    Message-id: <1465830735-18344-62-git-send-email-idryomov@redhat.com>
    Patchwork-id: 151615
    O-Subject: [RHEL7.3 PATCH 159/173] rbd: rbd_dev_header_unwatch_sync() variant
    Bugzilla: 1344930
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    From: Ilya Dryomov <idryomov@gmail.com>
    
    Introduce __rbd_dev_header_unwatch_sync(), which doesn't flush notify
    callbacks.  This is for the new rados_watcherrcb_t, which would be
    called from a notify callback.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit c525f03601f52c83ded046624138f2a45e0ba56c)
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -3289,10 +3289,7 @@ static int rbd_dev_header_watch_sync(struct rbd_device *rbd_dev)
 	return 0;
 }
 
-/*
- * Tear down a watch request, synchronously.
- */
-static void rbd_dev_header_unwatch_sync(struct rbd_device *rbd_dev)
+static void __rbd_dev_header_unwatch_sync(struct rbd_device *rbd_dev)
 {
 	struct rbd_obj_request *obj_request;
 
@@ -3312,6 +3309,14 @@ static void rbd_dev_header_unwatch_sync(struct rbd_device *rbd_dev)
 
 	ceph_osdc_cancel_event(rbd_dev->watch_event);
 	rbd_dev->watch_event = NULL;
+}
+
+/*
+ * Tear down a watch request, synchronously.
+ */
+static void rbd_dev_header_unwatch_sync(struct rbd_device *rbd_dev)
+{
+	__rbd_dev_header_unwatch_sync(rbd_dev);
 
 	dout("%s flushing notifies\n", __func__);
 	ceph_osdc_flush_notifies(&rbd_dev->rbd_client->client->osdc);
