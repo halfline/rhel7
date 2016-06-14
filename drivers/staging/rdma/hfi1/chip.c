1856e0ae239bb38aa441f36f48c4c09f511b2a74
Alex Estrin <aestrin@redhat.com>
2016-06-14 19:41:24 -0400

    [infiniband] ib/hfi1: Change SBus handling to use resource reservation
    Message-id: <20160614194124.23159.60706.stgit@dhcp187-101.khw.lab.eng.bos.redhat.com>
    Patchwork-id: 152908
    O-Subject: [RHEL-7.3 BZs 1273170 1272062 PATCH 309/431] IB/hfi1: Change SBus handling to use resource reservation
    Bugzilla: 1272062 1273170
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Dean Luick <dean.luick@intel.com>
    
    Bugzilla: 1273170
    Upstream: (imported from commit 576531fde8473333322905ea09fd5cfd14ce91ef)
    
    The SBus resource includes SBUS, PCIE, and THERM registers.
    Change SBus handling to use the new ASIC resource reservation system.
    
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Reviewed-by: Easwar Hariharan <easwar.hariharan@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/staging/rdma/hfi1/chip.c
+++ drivers/staging/rdma/hfi1/chip.c
@@ -14321,7 +14321,12 @@ static int thermal_init(struct hfi1_devdata *dd)
 	    !(dd->flags & HFI1_DO_INIT_ASIC))
 		return ret;
 
-	acquire_hw_mutex(dd);
+	ret = acquire_chip_resource(dd, CR_SBUS, SBUS_TIMEOUT);
+	if (ret) {
+		THERM_FAILURE(dd, ret, "Acquire SBus");
+		return ret;
+	}
+
 	dd_dev_info(dd, "Initializing thermal sensor\n");
 	/* Disable polling of thermal readings */
 	write_csr(dd, ASIC_CFG_THERM_POLL_EN, 0x0);
@@ -14369,7 +14374,7 @@ static int thermal_init(struct hfi1_devdata *dd)
 	/* Enable polling of thermal readings */
 	write_csr(dd, ASIC_CFG_THERM_POLL_EN, 0x1);
 done:
-	release_hw_mutex(dd);
+	release_chip_resource(dd, CR_SBUS);
 	return ret;
 }
 
