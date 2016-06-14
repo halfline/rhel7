5966c14aa10f80b15c995bf54a33fcb86bb8bcb1
kamal heib <kheib@redhat.com>
2016-06-14 15:24:56 -0400

    [include] ib/core: Add interfaces to control VF attributes
    Message-id: <1465917990-1132-88-git-send-email-kheib@redhat.com>
    Patchwork-id: 151994
    O-Subject: [RHEL7.3 PATCH 087/181] IB/core: Add interfaces to control VF attributes
    Bugzilla: 1275159 1296272 1296405 1298421 1298422 1298423 1298424 1298425
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    From: Eli Cohen <eli@mellanox.com>
    
    Following the practice exercised for network devices which allow the PF
    net device to configure attributes of its virtual functions, we
    introduce the following functions to be used by IPoIB which is the
    network driver implementation for IB devices.
    
    ib_set_vf_link_state - set the policy for a VF link. More below.
    ib_get_vf_config - read configuration information of a VF
    ib_get_vf_stats - read VF statistics
    ib_set_vf_guid - set the node or port GUID of a VF
    
    Also add an indication in the device cap flags that indicates that this
    IB devices is based on a virtual function.
    
    A VF shares the physical port with the PF and other VFs. When setting
    the link state we have three options:
    
    1. Auto - in this mode, the virtual port follows the state of the
       physical port and becomes active only if the physical port's state is
       active. In all other cases it remains in a Down state.
    2. Down - sets the state of the virtual port to Down
    3. Up - causes the virtual port to transition into Initialize state if
       it was not already in this state. A virtualization aware subnet manager
       can then bring the state of the port into the Active state.
    
    Signed-off-by: Eli Cohen <eli@mellanox.com>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from commit 50174a7f2c24d13cdeec435ee1ba70b1e0b1318f)
    Signed-off-by: Kamal Heib <kheib@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	include/rdma/ib_verbs.h
    Due to commit ("765d677 IB: new common API for draining queues") that
    wasn't backported yet into RHEL7.

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -56,6 +56,7 @@
 #include <linux/string.h>
 #include <linux/slab.h>
 
+#include <linux/if_link.h>
 #include <linux/atomic.h>
 #include <linux/mmu_notifier.h>
 #include <asm/uaccess.h>
@@ -218,6 +219,7 @@ enum ib_device_cap_flags {
 	IB_DEVICE_SIGNATURE_HANDOVER		= (1 << 30),
 	IB_DEVICE_ON_DEMAND_PAGING		= (1 << 31),
 	IB_DEVICE_SG_GAPS_REG			= (1ULL << 32),
+	IB_DEVICE_VIRTUAL_FUNCTION		= ((u64)1 << 33),
 };
 
 enum ib_signature_prot_cap {
@@ -1865,6 +1867,14 @@ struct ib_device {
 	int			   (*check_mr_status)(struct ib_mr *mr, u32 check_mask,
 						      struct ib_mr_status *mr_status);
 	void			   (*disassociate_ucontext)(struct ib_ucontext *ibcontext);
+	int			   (*set_vf_link_state)(struct ib_device *device, int vf, u8 port,
+							int state);
+	int			   (*get_vf_config)(struct ib_device *device, int vf, u8 port,
+						   struct ifla_vf_info *ivf);
+	int			   (*get_vf_stats)(struct ib_device *device, int vf, u8 port,
+						   struct ifla_vf_stats *stats);
+	int			   (*set_vf_guid)(struct ib_device *device, int vf, u8 port, u64 guid,
+						  int type);
 
 	struct ib_dma_mapping_ops   *dma_ops;
 
@@ -2307,6 +2317,15 @@ static inline bool rdma_cap_roce_gid_table(const struct ib_device *device,
 int ib_query_gid(struct ib_device *device,
 		 u8 port_num, int index, union ib_gid *gid,
 		 struct ib_gid_attr *attr);
+
+int ib_set_vf_link_state(struct ib_device *device, int vf, u8 port,
+			 int state);
+int ib_get_vf_config(struct ib_device *device, int vf, u8 port,
+		     struct ifla_vf_info *info);
+int ib_get_vf_stats(struct ib_device *device, int vf, u8 port,
+		    struct ifla_vf_stats *stats);
+int ib_set_vf_guid(struct ib_device *device, int vf, u8 port, u64 guid,
+		   int type);
 
 int ib_query_pkey(struct ib_device *device,
 		  u8 port_num, u16 index, u16 *pkey);
