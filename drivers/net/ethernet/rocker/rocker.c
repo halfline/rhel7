a4dd44e45346f710a3b17a8858b46496dd3c60b4
Ivan Vecera <ivecera@redhat.com>
2016-06-14 17:10:48 -0400

    [netdrv] rocker: print switch ID consistent with phys_switch_id sysfs node
    Message-id: <1465924603-6119-216-git-send-email-ivecera@redhat.com>
    Patchwork-id: 152378
    O-Subject: [RHEL7.3 net PATCH 215/570] rocker: print switch ID consistent with phys_switch_id sysfs node
    Bugzilla: 1275772 1297841 1331748
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Lance Richardson <lrichard@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    BZ#1331748
    
    Upstream commit(s):
    commit c8beb5b2613418e7ac1ffaee1f0796e77a4000be
    Author: Scott Feldman <sfeldma@gmail.com>
    Date:   Wed Aug 12 18:44:13 2015 -0700
    
        rocker: print switch ID consistent with phys_switch_id sysfs node
    
        On sucessful probe, driver prints the switch ID.  This patch changes the
        format of the printed ID to match what's used in sysfs phys_switch_id node.
    
        Signed-off-by: Scott Feldman <sfeldma@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/rocker/rocker.c
+++ drivers/net/ethernet/rocker/rocker.c
@@ -5184,7 +5184,8 @@ static int rocker_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		goto err_probe_ports;
 	}
 
-	dev_info(&pdev->dev, "Rocker switch with id %016llx\n", rocker->hw.id);
+	dev_info(&pdev->dev, "Rocker switch with id %*phN\n",
+		 (int)sizeof(rocker->hw.id), &rocker->hw.id);
 
 	return 0;
 
