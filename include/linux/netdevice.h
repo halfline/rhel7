1caff9011b6f2ffe808b272496ea89b62fb7a5af
Ivan Vecera <ivecera@redhat.com>
2016-06-14 17:13:44 -0400

    [net] add possibility to pass information about upper device via notifier
    Message-id: <1465924603-6119-392-git-send-email-ivecera@redhat.com>
    Patchwork-id: 152492
    O-Subject: [RHEL7.3 net PATCH 391/570] net: add possibility to pass information about upper device via notifier
    Bugzilla: 1275772 1297841 1331748
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Lance Richardson <lrichard@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    BZ#1297841
    
    Backport note: drivers/net/vrf.c removed as it is not present in RHEL7.
    
    Upstream commit(s):
    commit 29bf24afb29042f568fa67b1b0eee46796725ed2
    Author: Jiri Pirko <jiri@mellanox.com>
    Date:   Thu Dec 3 12:12:11 2015 +0100
    
        net: add possibility to pass information about upper device via notifier
    
        Sometimes the drivers and other code would find it handy to know some
        internal information about upper device being changed. So allow upper-code
        to pass information down to notifier listeners during linking.
    
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2274,6 +2274,7 @@ struct netdev_notifier_changeupper_info {
 	struct net_device *upper_dev; /* new upper dev */
 	bool master; /* is upper dev master */
 	bool linking; /* is the nofication for link or unlink */
+	void *upper_info; /* upper dev info */
 };
 
 static inline void netdev_notifier_info_init(struct netdev_notifier_info *info,
@@ -3742,7 +3743,7 @@ int netdev_master_upper_dev_link(struct net_device *dev,
 				 struct net_device *upper_dev);
 int netdev_master_upper_dev_link_rh(struct net_device *dev,
 				    struct net_device *upper_dev,
-				    void *upper_priv);
+				    void *upper_priv, void *upper_info);
 #define netdev_master_upper_dev_link netdev_master_upper_dev_link_rh
 void netdev_upper_dev_unlink(struct net_device *dev,
 			     struct net_device *upper_dev);
