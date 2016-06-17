7b3c79c18a4d515b14db7b8c0c06a0c30497ca69
Jiri Benc <jbenc@redhat.com>
2016-06-17 15:16:17 -0400

    [netdrv] fm10k: protect fm10k_open in fm10k_io_resume with rtnl_lock
    Message-id: <8c4e330bc563937831879a881f617e7b96660c9a.1466168420.git.jbenc@redhat.com>
    Patchwork-id: 153583
    O-Subject: [RHEL7.3 net 42/53] fm10k: protect fm10k_open in fm10k_io_resume with rtnl_lock
    Bugzilla: 1297504
    RH-Acked-by: Jakub Sitnicki <jkbs@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1297504
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11200350
    
    commit 41419b9303f085e8912406140355e45230fed22f
    Author: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Date:   Mon Apr 18 21:19:43 2016 +0200
    
        fm10k: protect fm10k_open in fm10k_io_resume with rtnl_lock
    
        fm10k_open requires rtnl_lock to be held.
    
        Cc: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
        Cc: Jesse Brandeburg <jesse.brandeburg@intel.com>
        Cc: Shannon Nelson <shannon.nelson@intel.com>
        Cc: Carolyn Wyborny <carolyn.wyborny@intel.com>
        Cc: Don Skidmore <donald.c.skidmore@intel.com>
        Cc: Bruce Allan <bruce.w.allan@intel.com>
        Cc: John Ronciak <john.ronciak@intel.com>
        Cc: Mitch Williams <mitch.a.williams@intel.com>
        Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_pci.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_pci.c
@@ -2264,8 +2264,10 @@ static void fm10k_io_resume(struct pci_dev *pdev)
 	/* reset clock */
 	fm10k_ts_reset(interface);
 
+	rtnl_lock();
 	if (netif_running(netdev))
 		err = fm10k_open(netdev);
+	rtnl_unlock();
 
 	/* final check of hardware state before registering the interface */
 	err = err ? : fm10k_hw_ready(interface);
