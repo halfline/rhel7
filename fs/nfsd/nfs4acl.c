d812d70626c36f65809bd9b6069ec414b49eb267
J. Bruce Fields <bfields@redhat.com>
2016-06-20 20:14:52 -0400

    [fs] nfsd: use swap() in sort_pacl_range()
    Message-id: <1466453769-4933-67-git-send-email-bfields@redhat.com>
    Patchwork-id: 153747
    O-Subject: [Patch RHEL7 fs 066/143] nfsd: use swap() in sort_pacl_range()
    Bugzilla: 1344797
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: Fabian Frederick <fabf@skynet.be>
    
    Use kernel.h macro definition.
    
    Thanks to Julia Lawall for Coccinelle scripting support.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    (cherry picked from commit 97b1f9aae963cc0b229ef8147db4782170564d4f)
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfsd/nfs4acl.c
+++ fs/nfsd/nfs4acl.c
@@ -349,7 +349,6 @@ pace_gt(struct posix_acl_entry *pace1, struct posix_acl_entry *pace2)
 static void
 sort_pacl_range(struct posix_acl *pacl, int start, int end) {
 	int sorted = 0, i;
-	struct posix_acl_entry tmp;
 
 	/* We just do a bubble sort; easy to do in place, and we're not
 	 * expecting acl's to be long enough to justify anything more. */
@@ -359,9 +358,8 @@ sort_pacl_range(struct posix_acl *pacl, int start, int end) {
 			if (pace_gt(&pacl->a_entries[i],
 				    &pacl->a_entries[i+1])) {
 				sorted = 0;
-				tmp = pacl->a_entries[i];
-				pacl->a_entries[i] = pacl->a_entries[i+1];
-				pacl->a_entries[i+1] = tmp;
+				swap(pacl->a_entries[i],
+				     pacl->a_entries[i + 1]);
 			}
 		}
 	}
