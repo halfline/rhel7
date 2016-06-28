halfline/rhel7

commit 49839caaed14f69fa083bc4be6167f86569c6378

[fs] userns: Only allow privileged creation of the mount namespace
Message-id: <gvsoakrv1pk.fsf_-_@ebiederm.csb>
Patchwork-id: 115635
O-Subject: [RHEL7.2 PATCH BZ 1138782 16/17] userns: Only allow privileged creation of the mount namespace.
Bugzilla: 1138782
RH-Acked-by: Aristeu Rozanski <aris@redhat.com>

Bugzilla: https://bugzilla.redhat.com/bugzilla/show_bug.cgi?id=1138782
Upstream Status: N/A
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=9320006
Testing: https://beaker.engineering.redhat.com/jobs/975954
Testing: Manually verified the global root can create mount namespaces
         but the user namespace root can not.

Until all of the bugs have been fixed and backported don't try and
support unprivileged creation of the mount namespaces.

Signed-off-by: "Eric W. Biederman" <ebiederm@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

fs/namespace.c
@@ -2511,6 +2511,10 @@ struct mnt_namespace *copy_mnt_ns(unsigned long flags, struct mnt_namespace *ns,
 	if (!(flags & CLONE_NEWNS))
 		return ns;
 
+	/* Unprivileged creation currently disabled in RHEL7  */
+	if (!capable(CAP_SYS_ADMIN))
+		return ERR_PTR(-EPERM);
+
 	new_ns = dup_mnt_ns(ns, user_ns, new_fs);
 
 	put_mnt_ns(ns);

commit 0c6b35f26d8cc81798c92c1017ddec1b12ac4986

[fs] mntns: drop namespace reference if !CAP_SYS_ADMIN
Message-id: <20160203211405.GJ14249@redhat.com>
Patchwork-id: 133372
O-Subject: [RHEL7.3 PATCH] mntns: drop namespace reference if !CAP_SYS_ADMIN
Bugzilla: 1297446
RH-Acked-by: Herton R. Krzesinski <herton@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>

BZ#1297446
brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10437564
upstream: RHEL7 only

This was reported on the referenced BZ and it's not present upstream: the
problem was introduced by
	49839caaed14f69fa083bc4be6167f86569c6378 [fs] userns: Only allow privileged creation of the mount namespace
which is a measure to prevent users to create their own namespaces using
user namespace.

Signed-off-by: Rafael Aquini <aquini@redhat.com>

fs/namespace.c
@@ -2539,8 +2539,10 @@ struct mnt_namespace *copy_mnt_ns(unsigned long flags, struct mnt_namespace *ns,
 		return ns;
 
 	/* Unprivileged creation currently disabled in RHEL7  */
-	if (!capable(CAP_SYS_ADMIN))
+	if (!capable(CAP_SYS_ADMIN)) {
+		put_mnt_ns(ns);
 		return ERR_PTR(-EPERM);
+	}
 
 	new_ns = dup_mnt_ns(ns, user_ns, new_fs);
 