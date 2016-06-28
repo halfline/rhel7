bcf3174c8891269ff288dd0055e1c34285c5a305
Lenny Szubowicz <lszubowi@redhat.com>
2016-06-28 15:44:18 -0400

    [x86] efi: Remove unused efi_call* macros
    Message-id: <1467128659-24486-40-git-send-email-lszubowi@redhat.com>
    Patchwork-id: 154199
    O-Subject: [RHEL 7.3 BZ 1310154 39/40] x86/efi: Remove unused efi_call* macros
    Bugzilla: 1310154
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Mathias Krause <minipli@googlemail.com>
    
    Complement commit 62fa6e69a436 ("x86/efi: Delete most of the efi_call*
    macros") and delete the stub macros for the !CONFIG_EFI case, too. In
    fact, there are no EFI calls in this case so we don't need a dummy for
    efi_call() even.
    
    Signed-off-by: Mathias Krause <minipli@googlemail.com>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>
    
    (cherry picked from commit 24ffd84b606dafa93a5f8f6e6dbaa06fbde11632)
    For bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1310154
    Signed-off-by: Lenny Szubowicz <lszubowi@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/include/asm/efi.h
+++ arch/x86/include/asm/efi.h
@@ -141,16 +141,6 @@ static inline efi_status_t efi_thunk_set_virtual_address_map(
 }
 #endif /* CONFIG_EFI_MIXED */
 #else
-/*
- * IF EFI is not configured, have the EFI calls return -ENOSYS.
- */
-#define efi_call0(_f)					(-ENOSYS)
-#define efi_call1(_f, _a1)				(-ENOSYS)
-#define efi_call2(_f, _a1, _a2)				(-ENOSYS)
-#define efi_call3(_f, _a1, _a2, _a3)			(-ENOSYS)
-#define efi_call4(_f, _a1, _a2, _a3, _a4)		(-ENOSYS)
-#define efi_call5(_f, _a1, _a2, _a3, _a4, _a5)		(-ENOSYS)
-#define efi_call6(_f, _a1, _a2, _a3, _a4, _a5, _a6)	(-ENOSYS)
 static inline void parse_efi_setup(u64 phys_addr, u32 data_len) {}
 #endif /* CONFIG_EFI */
 
