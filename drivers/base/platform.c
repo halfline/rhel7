00858337b6872b377b5f9dc50e89742697a6682f
Prarit Bhargava <prarit@redhat.com>
2016-06-30 14:02:43 -0400

    [x86] driver core: platform: Add support for built-in device properties
    Message-id: <1467295392-9015-76-git-send-email-prarit@redhat.com>
    Patchwork-id: 154366
    O-Subject: [RHEL7.3 PATCH BZ 1331018 075/104] driver core: platform: Add support for built-in device properties
    Bugzilla: 1331018
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1331018
    
    commit 00bbc1d8e46a92ce7bd80622cf4b09c3b727a741
    Author: Mika Westerberg <mika.westerberg@linux.intel.com>
    Date:   Mon Nov 30 17:11:38 2015 +0200
    
        driver core: platform: Add support for built-in device properties
    
        Make it possible to pass built-in device properties to platform device
        drivers. This is useful if the system does not have any firmware interface
        like Device Tree or ACPI which provides these.
    
        Properties associated with the platform device will be automatically
        released when the corresponding device is removed.
    
        Suggested-by: Arnd Bergmann <arnd@arndb.de>
        Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
        Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -22,6 +22,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/idr.h>
 #include <linux/acpi.h>
+#include <linux/property.h>
 
 #include "base.h"
 #include "power/power.h"
@@ -272,6 +273,22 @@ int platform_device_add_data(struct platform_device *pdev, const void *data,
 EXPORT_SYMBOL_GPL(platform_device_add_data);
 
 /**
+ * platform_device_add_properties - add built-in properties to a platform device
+ * @pdev: platform device to add properties to
+ * @pset: properties to add
+ *
+ * The function will take deep copy of the properties in @pset and attach
+ * the copy to the platform device. The memory associated with properties
+ * will be freed when the platform device is released.
+ */
+int platform_device_add_properties(struct platform_device *pdev,
+				   const struct property_set *pset)
+{
+	return device_add_property_set(&pdev->dev, pset);
+}
+EXPORT_SYMBOL_GPL(platform_device_add_properties);
+
+/**
  * platform_device_add - add a platform device to device hierarchy
  * @pdev: platform device we're adding
  *
@@ -386,6 +403,8 @@ void platform_device_del(struct platform_device *pdev)
 			if (type == IORESOURCE_MEM || type == IORESOURCE_IO)
 				release_resource(r);
 		}
+
+		device_remove_property_set(&pdev->dev);
 	}
 }
 EXPORT_SYMBOL_GPL(platform_device_del);
@@ -463,6 +482,12 @@ struct platform_device *platform_device_register_full(
 			pdevinfo->data, pdevinfo->size_data);
 	if (ret)
 		goto err;
+
+	if (pdevinfo->pset) {
+		ret = platform_device_add_properties(pdev, pdevinfo->pset);
+		if (ret)
+			goto err;
+	}
 
 	ret = platform_device_add(pdev);
 	if (ret) {
