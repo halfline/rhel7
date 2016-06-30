8eb50c6e4dc2b81ea4e37c8b5cd0a2f36714679f
Robert S Peterson <rpeterso@redhat.com>
2016-06-29 20:05:32 -0400

    [fs] gfs2: Eliminate parameter non_block on gfs2_inode_lookup
    Message-id: <1467230737-19262-10-git-send-email-rpeterso@redhat.com>
    Patchwork-id: 154289
    O-Subject: [RHEL7.3 PATCH 09/14] GFS2: Eliminate parameter non_block on gfs2_inode_lookup
    Bugzilla: 1336011
    RH-Acked-by: Andreas Gruenbacher <agruenba@redhat.com>
    RH-Acked-by: Steven Whitehouse <swhiteho@redhat.com>
    RH-Acked-by: Abhijith Das <adas@redhat.com>
    
    Now that we're not filtering out I_FREEING inodes from our lookups
    anymore, we can eliminate the non_block parameter from the lookup
    function.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    
    This is a RHEL7 crosswrite of the following upstream patch:
    https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/fs/gfs2?id=73b462d2808d7cbca4d7886cf6aaed850640e6cd
    
    rhbz#1336011
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/gfs2/dir.c
+++ fs/gfs2/dir.c
@@ -1678,7 +1678,7 @@ struct inode *gfs2_dir_search(struct inode *dir, const struct qstr *name,
 		brelse(bh);
 		if (fail_on_exist)
 			return ERR_PTR(-EEXIST);
-		return gfs2_inode_lookup(dir->i_sb, dtype, addr, formal_ino, 0);
+		return gfs2_inode_lookup(dir->i_sb, dtype, addr, formal_ino);
 	}
 	return ERR_PTR(-ENOENT);
 }
