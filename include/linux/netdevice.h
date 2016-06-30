torvalds/linux

commit aabc92bbe3cfe4c545f8ccdaaeeea012a46f0abf

net: add __netdev_alloc_pcpu_stats() to indicate gfp flags
nf_tables may create percpu counters from the packet path through its
dynamic set instantiation infrastructure, so we need a way to allocate
this through GFP_ATOMIC.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
Acked-by: David S. Miller <davem@davemloft.net>

include/linux/netdevice.h
@@ -2068,19 +2068,22 @@ struct pcpu_sw_netstats {
 	struct u64_stats_sync   syncp;
 };
 
-#define netdev_alloc_pcpu_stats(type)				\
-({								\
-	typeof(type) __percpu *pcpu_stats = alloc_percpu(type); \
-	if (pcpu_stats)	{					\
-		int __cpu;					\
-		for_each_possible_cpu(__cpu) {			\
-			typeof(type) *stat;			\
-			stat = per_cpu_ptr(pcpu_stats, __cpu);	\
-			u64_stats_init(&stat->syncp);		\
-		}						\
-	}							\
-	pcpu_stats;						\
+#define __netdev_alloc_pcpu_stats(type, gfp)				\
+({									\
+	typeof(type) __percpu *pcpu_stats = alloc_percpu_gfp(type, gfp);\
+	if (pcpu_stats)	{						\
+		int __cpu;						\
+		for_each_possible_cpu(__cpu) {				\
+			typeof(type) *stat;				\
+			stat = per_cpu_ptr(pcpu_stats, __cpu);		\
+			u64_stats_init(&stat->syncp);			\
+		}							\
+	}								\
+	pcpu_stats;							\
 })
+
+#define netdev_alloc_pcpu_stats(type)					\
+	__netdev_alloc_pcpu_stats(type, GFP_KERNEL);
 
 #include <linux/notifier.h>
 

halfline/rhel7

commit b03144398ba248ef1c99da09355e6c30e84150ab

[net] remove unnecessary semicolon in netdev_alloc_pcpu_stats()
Message-id: <831082680f7c66dd138d044aa4c42babafdfcc9f.1465290494.git.pabeni@redhat.com>
Patchwork-id: 149306
O-Subject: [RHEL7.3 net PATCH 143/165] net: remove unnecessary semicolon in netdev_alloc_pcpu_stats()
Bugzilla: 1331757
RH-Acked-by: Aaron Conole <aconole@redhat.com>
RH-Acked-by: Jakub Sitnicki <jkbs@redhat.com>
RH-Acked-by: Xin Long <lxin@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1331757
Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11141575
Tested: nftables test-suite and basic functional tests

Upstream commit:
commit 326fcfa5acca446b3f71e99f6d19881145556e5c
Author: Felix Fietkau <nbd@openwrt.org>
Date:   Sat Dec 5 13:58:11 2015 +0100

    net: remove unnecessary semicolon in netdev_alloc_pcpu_stats()

    This semicolon causes a build error if the function call is wrapped in
    parentheses.

    Fixes: aabc92bbe3cf ("net: add __netdev_alloc_pcpu_stats() to indicate gfp flags")
    Reported-by: Imre Kaloz <kaloz@openwrt.org>
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Acked-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

Signed-off-by: Paolo Abeni <pabeni@redhat.com>
Signed-off-by: Jiri Benc <jbenc@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

include/linux/netdevice.h
@@ -2148,7 +2148,7 @@ struct pcpu_sw_netstats {
 })
 
 #define netdev_alloc_pcpu_stats(type)					\
-	__netdev_alloc_pcpu_stats(type, GFP_KERNEL);
+	__netdev_alloc_pcpu_stats(type, GFP_KERNEL)
 
 #include <linux/notifier.h>
 