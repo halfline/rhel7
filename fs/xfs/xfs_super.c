699b410ed5b15e2d7a0f11961fbc73159519f384
Brian Foster <bfoster@redhat.com>
2016-07-06 13:42:45 -0400

    [fs] xfs: cancel eofblocks background trimming on remount read-only
    Message-id: <1467812565-39801-1-git-send-email-bfoster@redhat.com>
    Patchwork-id: 154688
    O-Subject: [RHEL7 PATCH] xfs: cancel eofblocks background trimming on remount read-only
    Bugzilla: 1339414
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    commit fa5a4f57ddbece604e07abfe98e01f520635411d
    Author: Brian Foster <bfoster@redhat.com>
    Date:   Tue Jun 21 11:53:28 2016 +1000
    
        xfs: cancel eofblocks background trimming on remount read-only
    
        The filesystem quiesce sequence performs the operations necessary to
        drain all background work, push pending transactions through the log
        infrastructure and wait on I/O resulting from the final AIL push. We
        have had reports of remount,ro hangs in xfs_log_quiesce() ->
        xfs_wait_buftarg(), however, and some instrumentation code to detect
        transaction commits at this point in the quiesce sequence has inculpated
        the eofblocks background scanner as a cause.
    
        While higher level remount code generally prevents user modifications by
        the time the filesystem has made it to xfs_log_quiesce(), the background
        scanner may still be alive and can perform pending work at any time. If
        this occurs between the xfs_log_force() and xfs_wait_buftarg() calls
        within xfs_log_quiesce(), this can lead to an indefinite lockup in
        xfs_wait_buftarg().
    
        To prevent this problem, cancel the background eofblocks scan worker
        during the remount read-only quiesce sequence. This suspends background
        trimming when a filesystem is remounted read-only. This is only done in
        the remount path because the freeze codepath has already locked out new
        transactions by the time the filesystem attempts to quiesce (and thus
        waiting on an active work item could deadlock). Kick the eofblocks
        worker to pick up where it left off once an fs is remounted back to
        read-write.
    
        Signed-off-by: Brian Foster <bfoster@redhat.com>
        Reviewed-by: Eric Sandeen <sandeen@redhat.com>
        Signed-off-by: Dave Chinner <david@fromorbit.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    ---
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1339414
    Upstream Status: Merged (xfs repo)
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11313447
    Tested: via xfstests
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1286,6 +1286,7 @@ xfs_fs_remount(
 		 */
 		xfs_restore_resvblks(mp);
 		xfs_log_work_queue(mp);
+		xfs_queue_eofblocks(mp);
 	}
 
 	/* rw -> ro */
@@ -1298,6 +1299,13 @@ xfs_fs_remount(
 		 * return it to the same size.
 		 */
 		xfs_save_resvblks(mp);
+
+		/*
+		 * Cancel background eofb scanning so it cannot race with the
+		 * final log force+buftarg wait and deadlock the remount.
+		 */
+		cancel_delayed_work_sync(&mp->m_eofblocks_work);
+
 		xfs_quiesce_attr(mp);
 		mp->m_flags |= XFS_MOUNT_RDONLY;
 	}
