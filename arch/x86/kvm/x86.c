90bfb6a1cc26a48b3c913d0148daebacfbf35332
Paolo Bonzini <pbonzini@redhat.com>
2016-07-06 15:13:17 -0400

    [virt] kvm: x86: 32-bit wraparound read/write not emulated correctly
    Message-id: <1467818058-27383-17-git-send-email-pbonzini@redhat.com>
    Patchwork-id: 154705
    O-Subject: [RHEL7.3 PATCH v3 16/77] KVM: x86: 32-bit wraparound read/write not emulated correctly
    Bugzilla: 1347370
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Nadav Amit <namit@cs.technion.ac.il>
    
    If we got a wraparound of 32-bit operand, and the limit is 0xffffffff, read and
    writes should be successful. It just needs to be done in two segments.
    
    Signed-off-by: Nadav Amit <namit@cs.technion.ac.il>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit bac155310be35e0fa64b066d47625d2a12a75122)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4780,6 +4780,8 @@ static int emulator_read_write(struct x86_emulate_ctxt *ctxt,
 		if (rc != X86EMUL_CONTINUE)
 			return rc;
 		addr += now;
+		if (ctxt->mode != X86EMUL_MODE_PROT64)
+			addr = (u32)addr;
 		val += now;
 		bytes -= now;
 	}
