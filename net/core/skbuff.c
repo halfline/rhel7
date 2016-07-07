4ebeb77267050425e9eb97d7c232f121ce036881
Ivan Vecera <ivecera@redhat.com>
2016-07-07 17:11:25 -0400

    [net] remove incorrect assignment to skb->sender_cpu
    Message-id: <1467911485-3622-1-git-send-email-ivecera@redhat.com>
    Patchwork-id: 154863
    O-Subject: [RHEL7.3 net PATCH v2 35/38] net: remove incorrect assignment to skb->sender_cpu
    Bugzilla: 1268334
    RH-Acked-by: Lance Richardson <lrichard@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    
    BZ#1268334
    
    Backport note:
    This simple patch is result of the follow-up fixes mentioned below. The commit
    52bd2d62ce67 ("net: better skb->sender_cpu and skb->napi_id cohabitation")
    resolved all issues originally fixed by these follow-ups and made them
    unnecessary. There was one remaining incorrect assignment to skb->sender_cpu
    in RHEL code that is removed by this patch.
    
    Upstream commit(s):
    commit c29390c6dfeee0944ac6b5610ebbe403944378fc
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Mar 11 18:42:02 2015 -0700
    
        xps: must clear sender_cpu before forwarding
    
        John reported that my previous commit added a regression
        on his router.
    
        This is because sender_cpu & napi_id share a common location,
        so get_xps_queue() can see garbage and perform an out of bound access.
    
        We need to make sure sender_cpu is cleared before doing the transmit,
        otherwise any NIC busy poll enabled (skb_mark_napi_id()) can trigger
        this bug.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reported-by: John <jw@nuclearfallout.net>
        Bisected-by: John <jw@nuclearfallout.net>
        Fixes: 2bd82484bb4c ("xps: fix xps for stacked devices")
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    commit a7d35f9d73e9ffa74a02304b817e579eec632f67
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Thu Jul 9 18:56:07 2015 +0200
    
        bridge: fix potential crash in __netdev_pick_tx()
    
        Commit c29390c6dfee ("xps: must clear sender_cpu before forwarding")
        fixed an issue in normal forward path, caused by sender_cpu & napi_id
        skb fields being an union.
    
        Bridge is another point where skb can be forwarded, so we need
        the same cure.
    
        Bug triggers if packet was received on a NIC using skb_mark_napi_id()
    
        Fixes: 2bd82484bb4c ("xps: fix xps for stacked devices")
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reported-by: Bob Liu <bob.liu@oracle.com>
        Tested-by: Bob Liu <bob.liu@oracle.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    commit d40496a56430eac0d330378816954619899fe303
    Author: WANG Cong <xiyou.wangcong@gmail.com>
    Date:   Tue Oct 6 17:23:47 2015 -0700
    
        act_mirred: clear sender cpu before sending to tx
    
        Similar to commit c29390c6dfee ("xps: must clear sender_cpu before forwarding")
        the skb->sender_cpu needs to be cleared when moving from Rx
        Tx, otherwise kernel could crash.
    
        Fixes: 2bd82484bb4c ("xps: fix xps for stacked devices")
        Cc: Eric Dumazet <edumazet@google.com>
        Cc: Jamal Hadi Salim <jhs@mojatatu.com>
        Signed-off-by: Cong Wang <cwang@twopensource.com>
        Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
        Acked-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    commit 64d4e3431e686dc37ce388ba531c4c4e866fb141
    Author: WANG Cong <xiyou.wangcong@gmail.com>
    Date:   Sat Feb 27 20:19:54 2016 -0800
    
        net: remove skb_sender_cpu_clear()
    
        After commit 52bd2d62ce67 ("net: better skb->sender_cpu and skb->napi_id cohabitation")
        skb_sender_cpu_clear() becomes empty and can be removed.
    
        Cc: Eric Dumazet <edumazet@google.com>
        Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3866,7 +3866,6 @@ void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 	skb->skb_iif = 0;
 	skb->ignore_df = 0;
 	skb_dst_drop(skb);
-	skb->sender_cpu = 0;
 	secpath_reset(skb);
 	nf_reset(skb);
 	nf_reset_trace(skb);
