3e62d688e599a50b820840dfba305e61ffc09be2
Jeff Moyer <jmoyer@redhat.com>
2016-07-07 17:46:22 -0400

    [mm] add vmf_insert_pfn_pmd()
    Message-id: <20160707174648.1995-42-jmoyer@redhat.com>
    Patchwork-id: 154895
    O-Subject: [RHEL7 PATCH 41/67] mm: add vmf_insert_pfn_pmd()
    Bugzilla: 1345801 1348502 1348836 1350149 1350156
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    From: Matthew Wilcox <willy@linux.intel.com>
    
    BZ: 1345801, 1348502, 1348836, 1350149, 1350156
    
    commit 5cad465d7fa646bad3d677df276bfc8e2ad709e3
    Author: Matthew Wilcox <willy@linux.intel.com>
    Date:   Tue Sep 8 14:58:54 2015 -0700
    
        mm: add vmf_insert_pfn_pmd()
    
        Similar to vm_insert_pfn(), but for PMDs rather than PTEs.  The 'vmf_'
        prefix instead of 'vm_' prefix is intended to indicate that it returns a
        VMF_ value rather than an errno (which would only have to be converted
        into a VMF_ value anyway).
    
        Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
        Cc: Hillf Danton <dhillf@gmail.com>
        Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
        Cc: Theodore Ts'o <tytso@mit.edu>
        Cc: Jan Kara <jack@suse.cz>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -33,6 +33,8 @@ extern int move_huge_pmd(struct vm_area_struct *vma,
 extern int change_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
 			unsigned long addr, pgprot_t newprot,
 			int prot_numa);
+int vmf_insert_pfn_pmd(struct vm_area_struct *, unsigned long addr, pmd_t *,
+			unsigned long pfn, bool write);
 extern void put_huge_zero_page(void);
 
 enum transparent_hugepage_flag {
