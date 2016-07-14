halfline/rhel7

commit dbe9b2c4beb2fff10df0b17c9cf8947deba5f17f

[net] fix __copy_skb_header()
Message-id: <edbdd065d2844467361ec7790b2c364a731e8317.1453222903.git.pabeni@redhat.com>
Patchwork-id: 132454
O-Subject: [RHEL7.3 net PATCH v2] net: fix __copy_skb_header()
Bugzilla: 1298447
RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
RH-Acked-by: Jakub Sitnicki <jsitnick@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1298447
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=10357566
Tested: on local VM

Some skb header fields are not currently copied by
__copy_skb_header() from the old skb into the new one.

The missing propagation of csum_bad was the root cause of
bz#1298447.

The referenced upstream commit fix it as a side effect.

This patch is only loosely based on the upstream commit, since we
can't move around skb fields and we can copy with a single memcpy
only part of the skb header.

The upstream __nf_copy() changes do not apply here for the above
reason.

To max the memcpy() length, we allow the header_start/ header_end
location to cross a couple of fields that must not be copied, and thus
we have to reset them afterwards. We also include into the header
section the queue_mapping field, since our different skb header
layout makes this change beneficial.

After this commits __copy_skb_header() is functionally equivalent
with upstream and copies all the skb fields if copied before,
plus: wifi_acked_valid, wifi_acked, csum_level, csum_bad,
ndisc_nodetype, inner_protocol_type, remcsum_offload.

Upstream commit:
commit b1937227316417aa7568d01e6fa1f272e98fb890
Author: Eric Dumazet <edumazet@google.com>
Date:   Sun Sep 28 22:18:47 2014 -0700

    net: reorganize sk_buff for faster __copy_skb_header()

    With proliferation of bit fields in sk_buff, __copy_skb_header() became
    quite expensive, showing as the most expensive function in a GSO
    workload.

    __copy_skb_header() performance is also critical for non GSO TCP

net/core/skbuff.c
@@ -798,60 +798,77 @@ void consume_skb(struct sk_buff *skb)
 }
 EXPORT_SYMBOL(consume_skb);
 
+/* Make sure a field is enclosed inside headers_start/headers_end section */
+#define CHECK_SKB_FIELD(field) \
+	BUILD_BUG_ON(offsetof(struct sk_buff, field) <          \
+		     offsetof(struct sk_buff, headers_start));  \
+	BUILD_BUG_ON(offsetof(struct sk_buff, field) >          \
+		     offsetof(struct sk_buff, headers_end));    \
+
 static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 {
 	new->tstamp		= old->tstamp;
+	/* We do not copy old->sk */
 	new->dev		= old->dev;
-	new->transport_header	= old->transport_header;
-	new->network_header	= old->network_header;
-	new->mac_header		= old->mac_header;
-	new->inner_protocol	= old->inner_protocol;
-	new->inner_transport_header = old->inner_transport_header;
-	new->inner_network_header = old->inner_network_header;
-	new->inner_mac_header = old->inner_mac_header;
+	memcpy(new->cb, old->cb, sizeof(old->cb));
 	skb_dst_copy(new, old);
-	skb_copy_hash(new, old);
-	new->ooo_okay		= old->ooo_okay;
-	new->no_fcs		= old->no_fcs;
-	new->encapsulation	= old->encapsulation;
-	new->encap_hdr_csum	= old->encap_hdr_csum;
-	new->csum_valid		= old->csum_valid;
-	new->csum_complete_sw	= old->csum_complete_sw;
 #ifdef CONFIG_XFRM
 	new->sp			= secpath_get(old->sp);
 #endif
-	memcpy(new->cb, old->cb, sizeof(old->cb));
+	__nf_copy(new, old);
+
+	/* RHEL: the below fields are outside headers_start/headers_end, due to
+	 * difference in skb layout between upstream and RHEL, so we need
+	 * to explicitly copy them here.
+	 */
 	new->csum		= old->csum;
-	new->ignore_df		= old->ignore_df;
-	new->pkt_type		= old->pkt_type;
-	new->ip_summed		= old->ip_summed;
-	skb_copy_queue_mapping(new, old);
 	new->priority		= old->priority;
+	new->ignore_df		= old->ignore_df;
+	new->ip_summed		= old->ip_summed;
+	new->pkt_type		= old->pkt_type;
 #if IS_ENABLED(CONFIG_IP_VS)
 	new->ipvs_property	= old->ipvs_property;
 #endif
-	new->pfmemalloc		= old->pfmemalloc;
-	new->protocol		= old->protocol;
-	new->mark		= old->mark;
-	new->skb_iif		= old->skb_iif;
-	__nf_copy(new, old);
 #if IS_ENABLED(CONFIG_NETFILTER_XT_TARGET_TRACE)
 	new->nf_trace		= old->nf_trace;
 #endif
-#ifdef CONFIG_NET_SCHED
-	new->tc_index		= old->tc_index;
-#ifdef CONFIG_NET_CLS_ACT
-	new->tc_verd		= old->tc_verd;
-#endif
-#endif
-	new->vlan_proto		= old->vlan_proto;
-	new->vlan_tci		= old->vlan_tci;
+	new->protocol		= old->protocol;
 
-	skb_copy_secmark(new, old);
-
+	memcpy(&new->headers_start, &old->headers_start,
+	       offsetof(struct sk_buff, headers_end) -
+	       offsetof(struct sk_buff, headers_start));
+	CHECK_SKB_FIELD(queue_mapping);
+	CHECK_SKB_FIELD(hash);
+	CHECK_SKB_FIELD(skb_iif);
+	CHECK_SKB_FIELD(vlan_proto);
+	CHECK_SKB_FIELD(vlan_tci);
+	CHECK_SKB_FIELD(transport_header);
+	CHECK_SKB_FIELD(network_header);
+	CHECK_SKB_FIELD(mac_header);
+	CHECK_SKB_FIELD(inner_protocol);
+	CHECK_SKB_FIELD(inner_transport_header);
+	CHECK_SKB_FIELD(inner_network_header);
+	CHECK_SKB_FIELD(inner_mac_header);
+	CHECK_SKB_FIELD(mark);
+#ifdef CONFIG_NETWORK_SECMARK
+	CHECK_SKB_FIELD(secmark);
+#endif
 #ifdef CONFIG_NET_RX_BUSY_POLL
-	new->napi_id	= old->napi_id;
+	CHECK_SKB_FIELD(napi_id);
 #endif
+#ifdef CONFIG_NET_SCHED
+	CHECK_SKB_FIELD(tc_index);
+#ifdef CONFIG_NET_CLS_ACT
+	CHECK_SKB_FIELD(tc_verd);
+#endif
+#endif
+
+	/* RHEL: the following fields are placed between headers_start and
+	 * headers_end on RHEL skb layout, but must not be copied; reset them
+	 * here allow us to use a single memcpy().
+	 */
+	new->head_frag = 0;
+	new->xmit_more = 0;
 }
 
 /*

include/linux/skbuff.h
@@ -622,6 +622,13 @@ struct sk_buff {
 	struct nf_bridge_info	*nf_bridge;
 #endif
 
+	/* fields enclosed in headers_start/headers_end are copied
+	 * using a single memcpy() in __copy_skb_header()
+	 */
+	/* private: */
+	RH_KABI_EXTEND(__u32	headers_start[0])
+	/* public: */
+
 	int			skb_iif;
 
 	RH_KABI_REPLACE(__u32	rxhash,
@@ -698,12 +705,17 @@ struct sk_buff {
 	/* 12 bit hole */
 	RH_KABI_EXTEND(kmemcheck_bitfield_end(flags3))
 
+	/* private: */
+	RH_KABI_EXTEND(__u32	headers_end[0])
+	/* public: */
+
 	/* RHEL SPECIFIC
 	 *
 	 * The following padding has been inserted before ABI freeze to
 	 * allow extending the structure while preserve ABI. Feel free
 	 * to replace reserved slots with required structure field
-	 * additions of your backport.
+	 * additions of your backport, eventually moving the replaced slot
+	 * before headers_end, if it need to be copied by __copy_skb_header()
 	 */
 	u32			rh_reserved1;
 	u32			rh_reserved2;

commit fb9d4b988aadb370fc96add6d7886190b4dc67ef

[net] skb: preserve value for head_frag and xmit more
Message-id: <1f6eddc56c67a0bbad60fc71d1ef74d0c714aedc.1467620017.git.pabeni@redhat.com>
Patchwork-id: 154634
O-Subject: [RHEL7.3 net PATCH] skb: preserve value for head_frag and xmit more
Bugzilla: 1334175
RH-Acked-by: Xin Long <lxin@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
RH-Acked-by: David S. Miller <davem@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1334175
Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11285950
Tested: with bz reproducer on pek2 testbed

RHEL only change, since it fixes an issue introduced by the delta vs
upstream of a previous backport.

__copy_skb_header() can be invoked also on already initialized skb
as target (by skb_segment()), so the fields that are inside the
headers_start/headers_end on rhel and outside it upstream can't be
initialized to 0 and their value must be explicitly restored after
the memcpy.

Fixes: dbe9b2c4beb2 ("[net] fix __copy_skb_header()")
Signed-off-by: Paolo Abeni <pabeni@redhat.com>
Signed-off-by: Jiri Benc <jbenc@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

net/core/skbuff.c
@@ -842,6 +842,9 @@ EXPORT_SYMBOL(napi_consume_skb);
 
 static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 {
+	int previous_head_frag = new->head_frag;
+	int previous_xmit_more = new->xmit_more;
+
 	new->tstamp		= old->tstamp;
 	/* We do not copy old->sk */
 	new->dev		= old->dev;
@@ -903,10 +906,10 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 
 	/* RHEL: the following fields are placed between headers_start and
 	 * headers_end on RHEL skb layout, but must not be copied; reset them
-	 * here allow us to use a single memcpy().
+	 * to their previous value to allow us using a single memcpy().
 	 */
-	new->head_frag = 0;
-	new->xmit_more = 0;
+	new->head_frag = previous_head_frag;
+	new->xmit_more = previous_xmit_more;
 }
 
 /*