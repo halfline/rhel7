99603d44b1074f38677f40732132d71798b79524
Jeff Moyer <jmoyer@redhat.com>
2016-07-29 17:50:20 -0400

    [kernel] fs, block: force direct-I/O for dax-enabled block devices
    Message-id: <20160729175048.16035-112-jmoyer@redhat.com>
    Patchwork-id: 156225
    O-Subject: [RHEL7 PATCH 111/139] fs, block: force direct-I/O for dax-enabled block devices
    Bugzilla: 1346083 1346084 1346445 1346449 1346472 1347091 1359806
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    From: Dan Williams <dan.j.williams@intel.com>
    
    BZ: 1346445, 1346449, 1346472, 1359806, 1346083, 1346084, 1347091
    
    commit 65f87ee71852a754f7981d0653e7136039b8798a
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Mon Jan 25 17:23:18 2016 -0800
    
        fs, block: force direct-I/O for dax-enabled block devices
    
        Similar to the file I/O path, re-direct all I/O to the DAX path for I/O
        to a block-device special file.  Both regular files and device special
        files can use the common filp->f_mapping->host lookup to determing is
        DAX is enabled.
    
        Otherwise, we confuse the DAX code that does not expect to find live
        data in the page cache:
    
            ------------[ cut here ]------------
            WARNING: CPU: 0 PID: 7676 at mm/filemap.c:217
            __delete_from_page_cache+0x9f6/0xb60()
            Modules linked in:
            CPU: 0 PID: 7676 Comm: a.out Not tainted 4.4.0+ #276
            Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS Bochs 01/01/2011
             00000000ffffffff ffff88006d3f7738 ffffffff82999e2d 0000000000000000
             ffff8800620a0000 ffffffff86473d20 ffff88006d3f7778 ffffffff81352089
             ffffffff81658d36 ffffffff86473d20 00000000000000d9 ffffea0000009d60
            Call Trace:
             [<     inline     >] __dump_stack lib/dump_stack.c:15
             [<ffffffff82999e2d>] dump_stack+0x6f/0xa2 lib/dump_stack.c:50
             [<ffffffff81352089>] warn_slowpath_common+0xd9/0x140 kernel/panic.c:482
             [<ffffffff813522b9>] warn_slowpath_null+0x29/0x30 kernel/panic.c:515
             [<ffffffff81658d36>] __delete_from_page_cache+0x9f6/0xb60 mm/filemap.c:217
             [<ffffffff81658fb2>] delete_from_page_cache+0x112/0x200 mm/filemap.c:244
             [<ffffffff818af369>] __dax_fault+0x859/0x1800 fs/dax.c:487
             [<ffffffff8186f4f6>] blkdev_dax_fault+0x26/0x30 fs/block_dev.c:1730
             [<     inline     >] wp_pfn_shared mm/memory.c:2208
             [<ffffffff816e9145>] do_wp_page+0xc85/0x14f0 mm/memory.c:2307
             [<     inline     >] handle_pte_fault mm/memory.c:3323
             [<     inline     >] __handle_mm_fault mm/memory.c:3417
             [<ffffffff816ecec3>] handle_mm_fault+0x2483/0x4640 mm/memory.c:3446
             [<ffffffff8127eff6>] __do_page_fault+0x376/0x960 arch/x86/mm/fault.c:1238
             [<ffffffff8127f738>] trace_do_page_fault+0xe8/0x420 arch/x86/mm/fault.c:1331
             [<ffffffff812705c4>] do_async_page_fault+0x14/0xd0 arch/x86/kernel/kvm.c:264
             [<ffffffff86338f78>] async_page_fault+0x28/0x30 arch/x86/entry/entry_64.S:986
             [<ffffffff86336c36>] entry_SYSCALL_64_fastpath+0x16/0x7a
            arch/x86/entry/entry_64.S:185
            ---[ end trace dae21e0f85f1f98c ]---
    
        Fixes: 5a023cdba50c ("block: enable dax for raw block devices")
        Reported-by: Dmitry Vyukov <dvyukov@google.com>
        Reported-by: Kirill A. Shutemov <kirill@shutemov.name>
        Suggested-by: Jan Kara <jack@suse.cz>
        Reviewed-by: Jan Kara <jack@suse.cz>
        Suggested-by: Matthew Wilcox <willy@linux.intel.com>
        Tested-by: Ross Zwisler <ross.zwisler@linux.intel.com>
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2973,7 +2973,7 @@ extern void replace_mount_options(struct super_block *sb, char *options);
 
 static inline bool io_is_direct(struct file *filp)
 {
-	return (filp->f_flags & O_DIRECT) || IS_DAX(file_inode(filp));
+	return (filp->f_flags & O_DIRECT) || IS_DAX(filp->f_mapping->host);
 }
 
 static inline ino_t parent_ino(struct dentry *dentry)
