8ef64549d8c1ac1366c995f0001e85b2b9088db9
Jeff Moyer <jmoyer@redhat.com>
2016-07-29 17:50:10 -0400

    [mm] dax: support dirty DAX entries in radix tree
    Message-id: <20160729175048.16035-102-jmoyer@redhat.com>
    Patchwork-id: 156227
    O-Subject: [RHEL7 PATCH 101/139] dax: support dirty DAX entries in radix tree
    Bugzilla: 1346083 1346084 1346445 1346449 1346472 1347091 1359806
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Eric Sandeen <sandeen@redhat.com>
    
    From: Ross Zwisler <ross.zwisler@linux.intel.com>
    
    BZ: 1346445, 1346449, 1346472, 1359806, 1346083, 1346084, 1347091
    
        commit f9fe48bece3af2d60e1bad65db4825f5a025dd36
        Author: Ross Zwisler <ross.zwisler@linux.intel.com>
        Date:   Fri Jan 22 15:10:40 2016 -0800
    
            dax: support dirty DAX entries in radix tree
    
            Add support for tracking dirty DAX entries in the struct address_space
            radix tree.  This tree is already used for dirty page writeback, and it
            already supports the use of exceptional (non struct page*) entries.
    
            In order to properly track dirty DAX pages we will insert new
            exceptional entries into the radix tree that represent dirty DAX PTE or
            PMD pages.  These exceptional entries will also contain the writeback
            addresses for the PTE or PMD faults that we can use at fsync/msync time.
    
            There are currently two types of exceptional entries (shmem and shadow)
            that can be placed into the radix tree, and this adds a third.  We rely
            on the fact that only one type of exceptional entry can be found in a
            given radix tree based on its usage.  This happens for free with DAX vs
            shmem but we explicitly prevent shadow entries from being added to radix
            trees for DAX mappings.
    
            The only shadow entries that would be generated for DAX radix trees
            would be to track zero page mappings that were created for holes.  These
            pages would receive minimal benefit from having shadow entries, and the
            choice to have only one type of exceptional entry in a given radix tree
            makes the logic simpler both in clear_exceptional_entry() and in the
            rest of DAX.
    
            Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
            Cc: "H. Peter Anvin" <hpa@zytor.com>
            Cc: "J. Bruce Fields" <bfields@fieldses.org>
            Cc: "Theodore Ts'o" <tytso@mit.edu>
            Cc: Alexander Viro <viro@zeniv.linux.org.uk>
            Cc: Andreas Dilger <adilger.kernel@dilger.ca>
            Cc: Dave Chinner <david@fromorbit.com>
            Cc: Ingo Molnar <mingo@redhat.com>
            Cc: Jan Kara <jack@suse.com>
            Cc: Jeff Layton <jlayton@poochiereds.net>
            Cc: Matthew Wilcox <willy@linux.intel.com>
            Cc: Thomas Gleixner <tglx@linutronix.de>
            Cc: Dan Williams <dan.j.williams@intel.com>
            Cc: Matthew Wilcox <matthew.r.wilcox@intel.com>
            Cc: Dave Hansen <dave.hansen@linux.intel.com>
            Cc: Hugh Dickins <hughd@google.com>
            Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
            Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -43,6 +43,7 @@
 #include <linux/sysctl.h>
 #include <linux/oom.h>
 #include <linux/prefetch.h>
+#include <linux/dax.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -542,9 +543,15 @@ static int __remove_mapping(struct address_space *mapping, struct page *page,
 		 * inode reclaim needs to empty out the radix tree or
 		 * the nodes are lost.  Don't plant shadows behind its
 		 * back.
+		 *
+		 * We also don't store shadows for DAX mappings because the
+		 * only page cache pages found in these are zero pages
+		 * covering holes, and because we don't want to mix DAX
+		 * exceptional entries and shadow exceptional entries in the
+		 * same page_tree.
 		 */
 		if (reclaimed && page_is_file_cache(page) &&
-		    !mapping_exiting(mapping))
+		    !mapping_exiting(mapping) && !dax_mapping(mapping))
 			shadow = workingset_eviction(mapping, page);
 		__delete_from_page_cache(page, shadow);
 		spin_unlock_irq(&mapping->tree_lock);
