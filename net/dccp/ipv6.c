c743d3bfb807fdd331a64dd94895d25446ab95f8
Hangbin Liu <haliu@redhat.com>
2016-08-02 13:55:44 -0400

    [net] inet: introduce ireq_family
    Message-id: <1470146145-28380-2-git-send-email-haliu@redhat.com>
    Patchwork-id: 156339
    O-Subject: [RHEL7.3 net PATCH 1/2] inet: introduce ireq_family
    Bugzilla: 1360685
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1360685
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11481307
    Upstream Status: net.git commit 3f66b08
    
    In 7.3 commit 7398832, function inet_diag_fill_req(), we missed upstream
    commit 3f66b08. Which lead ireq->ireq_family not initialized. Since we
    haven't backport ireq_state and ireq_refcnt, apply this patch manually.
    
    commit 3f66b083a5b7f1a63540c24df3679c24f2e935a9
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Thu Mar 12 16:44:10 2015 -0700
    
        inet: introduce ireq_family
    
        Before inserting request socks into general hash table,
        fill their socket family.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/dccp/ipv6.c
+++ net/dccp/ipv6.c
@@ -408,6 +408,7 @@ static int dccp_v6_conn_request(struct sock *sk, struct sk_buff *skb)
 	ireq = inet_rsk(req);
 	ireq->ir_v6_rmt_addr = ipv6_hdr(skb)->saddr;
 	ireq->ir_v6_loc_addr = ipv6_hdr(skb)->daddr;
+	ireq->ireq_family = AF_INET6;
 
 	if (ipv6_opt_accepted(sk, skb) ||
 	    np->rxopt.bits.rxinfo || np->rxopt.bits.rxoinfo ||
