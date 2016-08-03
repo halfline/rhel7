1148cf6c8693acea89a8150cd82a84b87dead9f5
Jeff Moyer <jmoyer@redhat.com>
2016-08-02 22:40:22 -0400

    [acpi] Change NFIT driver to insert new resource
    Message-id: <20160802224022.23417-12-jmoyer@redhat.com>
    Patchwork-id: 156369
    O-Subject: [RHEL7 PATCH 11/11] ACPI: Change NFIT driver to insert new resource
    Bugzilla: 1342696
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Toshi Kani <toshi.kani@hpe.com>
    
    BZ: 1342696
    
    commit af1996ef59dbcb36fe4878df7c717a02eb89d07a
    Author: Toshi Kani <toshi.kani@hpe.com>
    Date:   Wed Mar 9 12:47:06 2016 -0700
    
        ACPI: Change NFIT driver to insert new resource
    
        ACPI 6 defines persistent memory (PMEM) ranges in multiple
        firmware interfaces, e820, EFI, and ACPI NFIT table.  This EFI
        change, however, leads to hit a bug in the grub bootloader, which
        treats EFI_PERSISTENT_MEMORY type as regular memory and corrupts
        stored user data [1].
    
        Therefore, BIOS may set generic reserved type in e820 and EFI to
        cover PMEM ranges.  The kernel can initialize PMEM ranges from
        ACPI NFIT table alone.
    
        This scheme causes a problem in the iomem table, though.  On x86,
        for instance, e820_reserve_resources() initializes top-level entries
        (iomem_resource.child) from the e820 table at early boot-time.
        This creates "reserved" entry for a PMEM range, which does not allow
        region_intersects() to check with PMEM type.
    
        Change acpi_nfit_register_region() to call acpi_nfit_insert_resource(),
        which calls insert_resource() to insert a PMEM entry from NFIT when
        the iomem table does not have a PMEM entry already.  That is, when
        a PMEM range is marked as reserved type in e820, it inserts
        "Persistent Memory" entry, which results as follows.
    
         + "Persistent Memory"
            + "reserved"
    
        This allows the EINJ driver, which calls region_intersects() to check
        PMEM ranges, to work continuously even if BIOS sets reserved type
        (or sets nothing) to PMEM ranges in e820 and EFI.
    
        [1]: https://lists.gnu.org/archive/html/grub-devel/2015-11/msg00209.html
        Signed-off-by: Toshi Kani <toshi.kani@hpe.com>
        Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
        Cc: Dan Williams <dan.j.williams@intel.com>
        Cc: Ingo Molnar <mingo@kernel.org>
        Cc: Borislav Petkov <bp@suse.de>
        Cc: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/acpi/nfit.c
+++ drivers/acpi/nfit.c
@@ -1571,6 +1571,46 @@ static int ars_status_process_records(struct nvdimm_bus *nvdimm_bus,
 	return 0;
 }
 
+static void acpi_nfit_remove_resource(void *data)
+{
+	struct resource *res = data;
+
+	remove_resource(res);
+}
+
+static int acpi_nfit_insert_resource(struct acpi_nfit_desc *acpi_desc,
+		struct nd_region_desc *ndr_desc)
+{
+	struct resource *res, *nd_res = ndr_desc->res;
+	int is_pmem, ret;
+
+	/* No operation if the region is already registered as PMEM */
+	is_pmem = region_intersects_pmem(nd_res->start, resource_size(nd_res));
+	if (is_pmem == REGION_INTERSECTS)
+		return 0;
+
+	res = devm_kzalloc(acpi_desc->dev, sizeof(*res), GFP_KERNEL);
+	if (!res)
+		return -ENOMEM;
+
+	res->name = "Persistent Memory";
+	res->start = nd_res->start;
+	res->end = nd_res->end;
+	res->flags = IORESOURCE_MEM;
+
+	ret = insert_resource(&iomem_resource, res);
+	if (ret)
+		return ret;
+
+	ret = devm_add_action(acpi_desc->dev, acpi_nfit_remove_resource, res);
+	if (ret) {
+		remove_resource(res);
+		return ret;
+	}
+
+	return 0;
+}
+
 static int acpi_nfit_find_poison(struct acpi_nfit_desc *acpi_desc,
 		struct nd_region_desc *ndr_desc)
 {
@@ -1788,6 +1828,12 @@ static int acpi_nfit_register_region(struct acpi_nfit_desc *acpi_desc,
 
 	nvdimm_bus = acpi_desc->nvdimm_bus;
 	if (nfit_spa_type(spa) == NFIT_SPA_PM) {
+		rc = acpi_nfit_insert_resource(acpi_desc, ndr_desc);
+		if (rc)
+			dev_warn(acpi_desc->dev,
+				"failed to insert pmem resource to iomem: %d\n",
+				rc);
+
 		rc = acpi_nfit_find_poison(acpi_desc, ndr_desc);
 		if (rc) {
 			dev_err(acpi_desc->dev,
