17cda3a81bbf81aae2cd2ab0462beaee13e8f431
Mike Snitzer <snitzer@redhat.com>
2016-08-03 13:52:47 -0400

    [block] revert "remove struct bio_batch"
    Message-id: <1470232367-27728-7-git-send-email-snitzer@redhat.com>
    Patchwork-id: 156399
    O-Subject: [RHEL7.3 PATCH 6/6] Revert "[block] remove struct bio_batch"
    Bugzilla: 1361484
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    RH-Acked-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    
    This reverts commit 313954aa969125ac1b653cccf35937ab017d81d4.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- block/blk-lib.c
+++ block/blk-lib.c
@@ -9,17 +9,21 @@
 
 #include "blk.h"
 
-static struct bio *next_bio(struct bio *bio, int rw, unsigned int nr_pages,
-			    gfp_t gfp)
+struct bio_batch {
+	atomic_t		done;
+	unsigned long		flags;
+	struct completion	*wait;
+};
+
+static void bio_batch_end_io(struct bio *bio, int err)
 {
-	struct bio *new = bio_alloc(gfp, nr_pages);
+	struct bio_batch *bb = bio->bi_private;
 
-	if (bio) {
-		bio_chain(bio, new);
-		submit_bio(rw, bio);
-	}
-
-	return new;
+	if (err && (err != -EOPNOTSUPP))
+		clear_bit(BIO_UPTODATE, &bb->flags);
+	if (atomic_dec_and_test(&bb->done))
+		complete(bb->wait);
+	bio_put(bio);
 }
 
 /**
@@ -36,11 +40,13 @@ static struct bio *next_bio(struct bio *bio, int rw, unsigned int nr_pages,
 int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 		sector_t nr_sects, gfp_t gfp_mask, unsigned long flags)
 {
+	DECLARE_COMPLETION_ONSTACK(wait);
 	struct request_queue *q = bdev_get_queue(bdev);
 	int type = REQ_WRITE | REQ_DISCARD;
 	unsigned int max_discard_sectors, granularity;
 	int alignment;
-	struct bio *bio = NULL;
+	struct bio_batch bb;
+	struct bio *bio;
 	int ret = 0;
 	struct blk_plug plug;
 
@@ -71,14 +77,24 @@ int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 		type |= REQ_SECURE;
 	}
 
+	atomic_set(&bb.done, 1);
+	bb.flags = 1 << BIO_UPTODATE;
+	bb.wait = &wait;
+
 	blk_start_plug(&plug);
 	while (nr_sects) {
 		unsigned int req_sects;
 		sector_t end_sect, tmp;
 
+		bio = bio_alloc(gfp_mask, 1);
+		if (!bio) {
+			ret = -ENOMEM;
+			break;
+		}
+
 		req_sects = min_t(sector_t, nr_sects, max_discard_sectors);
 
-		/**
+		/*
 		 * If splitting a request, and the next starting sector would be
 		 * misaligned, stop the discard at the previous aligned sector.
 		 */
@@ -92,13 +108,17 @@ int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 			req_sects = end_sect - sector;
 		}
 
-		bio = next_bio(bio, type, 1, gfp_mask);
 		bio->bi_sector = sector;
+		bio->bi_end_io = bio_batch_end_io;
 		bio->bi_bdev = bdev;
+		bio->bi_private = &bb;
 
 		bio->bi_size = req_sects << 9;
 		nr_sects -= req_sects;
 		sector = end_sect;
+
+		atomic_inc(&bb.done);
+		submit_bio(type, bio);
 
 		/*
 		 * We can loop for a long time in here, if someone does
@@ -108,11 +128,16 @@ int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 		 */
 		cond_resched();
 	}
-	if (bio)
-		ret = submit_bio_wait(type, bio);
 	blk_finish_plug(&plug);
 
-	return ret != -EOPNOTSUPP ? ret : 0;
+	/* Wait for bios in-flight */
+	if (!atomic_dec_and_test(&bb.done))
+		wait_for_completion_io(&wait);
+
+	if (!test_bit(BIO_UPTODATE, &bb.flags))
+		ret = -EIO;
+
+	return ret;
 }
 EXPORT_SYMBOL(blkdev_issue_discard);
 
@@ -131,9 +156,11 @@ int blkdev_issue_write_same(struct block_device *bdev, sector_t sector,
 			    sector_t nr_sects, gfp_t gfp_mask,
 			    struct page *page)
 {
+	DECLARE_COMPLETION_ONSTACK(wait);
 	struct request_queue *q = bdev_get_queue(bdev);
 	unsigned int max_write_same_sectors;
-	struct bio *bio = NULL;
+	struct bio_batch bb;
+	struct bio *bio;
 	int ret = 0;
 
 	if (!q)
@@ -144,11 +171,21 @@ int blkdev_issue_write_same(struct block_device *bdev, sector_t sector,
 	if (max_write_same_sectors == 0)
 		return -EOPNOTSUPP;
 
+	atomic_set(&bb.done, 1);
+	bb.flags = 1 << BIO_UPTODATE;
+	bb.wait = &wait;
+
 	while (nr_sects) {
-		bio = next_bio(bio, REQ_WRITE | REQ_WRITE_SAME, 1, gfp_mask);
+		bio = bio_alloc(gfp_mask, 1);
+		if (!bio) {
+			ret = -ENOMEM;
+			break;
+		}
 
 		bio->bi_sector = sector;
+		bio->bi_end_io = bio_batch_end_io;
 		bio->bi_bdev = bdev;
+		bio->bi_private = &bb;
 		bio->bi_vcnt = 1;
 		bio->bi_io_vec->bv_page = page;
 		bio->bi_io_vec->bv_offset = 0;
@@ -162,11 +199,19 @@ int blkdev_issue_write_same(struct block_device *bdev, sector_t sector,
 			bio->bi_size = nr_sects << 9;
 			nr_sects = 0;
 		}
+
+		atomic_inc(&bb.done);
+		submit_bio(REQ_WRITE | REQ_WRITE_SAME, bio);
 	}
 
-	if (bio)
-		ret = submit_bio_wait(REQ_WRITE | REQ_WRITE_SAME, bio);
-	return ret != -EOPNOTSUPP ? ret : 0;
+	/* Wait for bios in-flight */
+	if (!atomic_dec_and_test(&bb.done))
+		wait_for_completion_io(&wait);
+
+	if (!test_bit(BIO_UPTODATE, &bb.flags))
+		ret = -ENOTSUPP;
+
+	return ret;
 }
 EXPORT_SYMBOL(blkdev_issue_write_same);
 
@@ -185,16 +230,28 @@ static int __blkdev_issue_zeroout(struct block_device *bdev, sector_t sector,
 				  sector_t nr_sects, gfp_t gfp_mask)
 {
 	int ret;
-	struct bio *bio = NULL;
+	struct bio *bio;
+	struct bio_batch bb;
 	unsigned int sz;
+	DECLARE_COMPLETION_ONSTACK(wait);
 
+	atomic_set(&bb.done, 1);
+	bb.flags = 1 << BIO_UPTODATE;
+	bb.wait = &wait;
+
+	ret = 0;
 	while (nr_sects != 0) {
-		bio = next_bio(bio, WRITE,
-			       min(nr_sects, (sector_t)BIO_MAX_PAGES),
-			       gfp_mask);
+		bio = bio_alloc(gfp_mask,
+				min(nr_sects, (sector_t)BIO_MAX_PAGES));
+		if (!bio) {
+			ret = -ENOMEM;
+			break;
+		}
 
 		bio->bi_sector = sector;
 		bio->bi_bdev   = bdev;
+		bio->bi_end_io = bio_batch_end_io;
+		bio->bi_private = &bb;
 
 		while (nr_sects != 0) {
 			sz = min((sector_t) PAGE_SIZE >> 9 , nr_sects);
@@ -204,11 +261,20 @@ static int __blkdev_issue_zeroout(struct block_device *bdev, sector_t sector,
 			if (ret < (sz << 9))
 				break;
 		}
+		ret = 0;
+		atomic_inc(&bb.done);
+		submit_bio(WRITE, bio);
 	}
 
-	if (bio)
-		return submit_bio_wait(WRITE, bio);
-	return 0;
+	/* Wait for bios in-flight */
+	if (!atomic_dec_and_test(&bb.done))
+		wait_for_completion_io(&wait);
+
+	if (!test_bit(BIO_UPTODATE, &bb.flags))
+		/* One of bios in the batch was completed with error.*/
+		ret = -EIO;
+
+	return ret;
 }
 
 /**
