halfline/rhel7

commit 01e7cd3188dde7b7bf3ec087faa4b0694bb06821

[mm] vfs,ext2: introduce IS_DAX(inode)
Message-id: <1464212242-20208-21-git-send-email-sandeen@redhat.com>
Patchwork-id: 147536
O-Subject: [RHEL7 PATCH 020/126] vfs,ext2: introduce IS_DAX(inode)
Bugzilla: 1274459
RH-Acked-by: Rafael Aquini <aquini@redhat.com>
RH-Acked-by: Rik van Riel <riel@redhat.com>
RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>

From: Matthew Wilcox <matthew.r.wilcox@intel.com>

BZ: 1274459

commit fbbbad4bc2101e452b24e6e65d3d5e11314a0b5f
Author: Matthew Wilcox <matthew.r.wilcox@intel.com>
Date:   Mon Feb 16 15:58:53 2015 -0800

    vfs,ext2: introduce IS_DAX(inode)

    Use an inode flag to tag inodes which should avoid using the page cache.
    Convert ext2 to use it instead of mapping_is_xip().  Prevent I/Os to files
    tagged with the DAX flag from falling back to buffered I/O.

    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Cc: Boaz Harrosh <boaz@plexistor.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
Signed-off-by: Eric Sandeen <sandeen@redhat.com>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

mm/filemap.c
@@ -1963,9 +1963,11 @@ generic_file_aio_read(struct kiocb *iocb, const struct iovec *iov,
 			 * we've already read everything we wanted to, or if
 			 * there was a short read because we hit EOF, go ahead
 			 * and return.  Otherwise fallthrough to buffered io for
-			 * the rest of the read.
+			 * the rest of the read.  Buffered reads will not work for
+			 * DAX files, so don't bother trying.
 			 */
-			if (retval < 0 || !count || *ppos >= size) {
+			if (retval < 0 || !count || *ppos >= size ||
+			    IS_DAX(inode)) {
 				file_accessed(filp);
 				goto out;
 			}
@@ -2992,12 +2994,16 @@ ssize_t __generic_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 
 		written = generic_file_direct_write(iocb, iov, &nr_segs, pos,
 							ppos, count, ocount);
-		if (written < 0 || written == count)
-			goto out;
 		/*
-		 * direct-io write to a hole: fall through to buffered I/O
-		 * for completing the rest of the request.
+		 * If the write stopped short of completing, fall back to
+		 * buffered writes.  Some filesystems do this for writes to
+		 * holes, for example.  For DAX files, a buffered write will
+		 * not succeed (even if it did, DAX does not handle dirty
+		 * page-cache pages correctly).
 		 */
+		if (written < 0 || written == count || IS_DAX(inode))
+			goto out;
+
 		pos += written;
 		count -= written;
 		written_buffered = generic_file_buffered_write(iocb, iov,

fs/ext2/inode.c
@@ -729,7 +729,7 @@ static int ext2_get_blocks(struct inode *inode,
 		goto cleanup;
 	}
 
-	if (ext2_use_xip(inode->i_sb)) {
+	if (IS_DAX(inode)) {
 		/*
 		 * we need to clear the block
 		 */
@@ -1199,7 +1199,7 @@ static int ext2_setsize(struct inode *inode, loff_t newsize)
 
 	inode_dio_wait(inode);
 
-	if (mapping_is_xip(inode->i_mapping))
+	if (IS_DAX(inode))
 		error = xip_truncate_page(inode->i_mapping, newsize);
 	else if (test_opt(inode->i_sb, NOBH))
 		error = nobh_truncate_page(inode->i_mapping,
@@ -1271,7 +1271,8 @@ void ext2_set_inode_flags(struct inode *inode)
 {
 	unsigned int flags = EXT2_I(inode)->i_flags;
 
-	inode->i_flags &= ~(S_SYNC|S_APPEND|S_IMMUTABLE|S_NOATIME|S_DIRSYNC);
+	inode->i_flags &= ~(S_SYNC | S_APPEND | S_IMMUTABLE | S_NOATIME |
+				S_DIRSYNC | S_DAX);
 	if (flags & EXT2_SYNC_FL)
 		inode->i_flags |= S_SYNC;
 	if (flags & EXT2_APPEND_FL)
@@ -1282,6 +1283,8 @@ void ext2_set_inode_flags(struct inode *inode)
 		inode->i_flags |= S_NOATIME;
 	if (flags & EXT2_DIRSYNC_FL)
 		inode->i_flags |= S_DIRSYNC;
+	if (test_opt(inode->i_sb, XIP))
+		inode->i_flags |= S_DAX;
 }
 
 /* Propagate flags from i_flags to EXT2_I(inode)->i_flags */

fs/ext2/xip.h
@@ -16,9 +16,7 @@ static inline int ext2_use_xip (struct super_block *sb)
 }
 int ext2_get_xip_mem(struct address_space *, pgoff_t, int,
 				void **, unsigned long *);
-#define mapping_is_xip(map) unlikely(map->a_ops->get_xip_mem)
 #else
-#define mapping_is_xip(map)			0
 #define ext2_xip_verify_sb(sb)			do { } while (0)
 #define ext2_use_xip(sb)			0
 #define ext2_clear_xip_target(inode, chain)	0

include/linux/fs.h
@@ -1820,6 +1820,12 @@ struct super_operations {
 #define S_NOSEC		4096	/* no suid or xattr security attributes */
 #define S_IOPS_WRAPPER	8192	/* i_op points to struct inode_operations_wrapper */
 
+#ifdef CONFIG_FS_XIP
+#define S_DAX		16384	/* Direct Access, avoiding the page cache */
+#else
+#define S_DAX		0	/* Make all the DAX code disappear */
+#endif
+
 /*
  * Note that nosuid etc flags are inode-specific: setting some file-system
  * flags just means all the inodes inherit those flags by default. It might be
@@ -1857,6 +1863,7 @@ struct super_operations {
 #define IS_AUTOMOUNT(inode)	((inode)->i_flags & S_AUTOMOUNT)
 #define IS_NOSEC(inode)		((inode)->i_flags & S_NOSEC)
 #define IS_IOPS_WRAPPER(inode)	((inode)->i_flags & S_IOPS_WRAPPER)
+#define IS_DAX(inode)		((inode)->i_flags & S_DAX)
 
 #define IS_WHITEOUT(inode)	(S_ISCHR(inode->i_mode) && \
 				 (inode)->i_rdev == WHITEOUT_DEV)

commit e98abd0072e05f037b73ad5fb9923c5ac6e3b589

[mm] vfs: prevent buffered I/O reads to DAX inodes
Message-id: <20160729175048.16035-140-jmoyer@redhat.com>
Patchwork-id: 156185
O-Subject: [RHEL7 PATCH 139/139] vfs: prevent buffered I/O reads to DAX inodes
Bugzilla: 1346083 1346084 1346445 1346449 1346472 1347091 1359806
RH-Acked-by: Rafael Aquini <aquini@redhat.com>
RH-Acked-by: Eric Sandeen <sandeen@redhat.com>

From: Ross Zwisler <ross.zwisler@linux.intel.com>

BZ: 1346445, 1346449, 1346472, 1359806, 1346083, 1346084, 1347091

generic_file_read_iter() in the upstream kernel unconditionally skips the
call to do_generic_file_read() for DAX inodes.  The code in Red Hat's
generic_file_aio_read(), though, has the IS_DAX(inode) check hidden inside
of an "if (pos < size)" conditional, which means that in some cases we *do*
execute page cache I/O for DAX inodes via the following path:

add_to_page_cache_lru
mpage_readpages
_ext4_get_block
alloc_pages_current
ext4_readpages
__do_page_cache_readahead
ondemand_readahead
page_cache_sync_readahead
generic_file_aio_read

Prevent this by unconditionally bailing out of generic_file_aio_read()
without doing any buffered I/O if we are talking to a DAX inode.

Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
Fixes: 01e7cd3188dd ("vfs,ext2: introduce IS_DAX(inode)")
Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

mm/filemap.c
@@ -2037,14 +2037,21 @@ generic_file_aio_read(struct kiocb *iocb, const struct iovec *iov,
 			 * we've already read everything we wanted to, or if
 			 * there was a short read because we hit EOF, go ahead
 			 * and return.  Otherwise fallthrough to buffered io for
-			 * the rest of the read.  Buffered reads will not work for
-			 * DAX files, so don't bother trying.
+			 * the rest of the read.
 			 */
-			if (retval < 0 || !count || *ppos >= size ||
-			    IS_DAX(inode)) {
+			if (retval < 0 || !count || *ppos >= size) {
 				file_accessed(filp);
 				goto out;
 			}
+		}
+
+		/*
+		 *  Buffered reads will not work for DAX files, so don't
+		 *  bother trying.
+		 */
+		if (IS_DAX(inode)) {
+			file_accessed(filp);
+			goto out;
 		}
 	}
 