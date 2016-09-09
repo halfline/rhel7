6cbfcf1526c74681ac81cda667f6f89016b47b8f
Lauro Ramos Venancio <lvenanci@redhat.com>
2016-09-09 14:38:17 -0400

    [kernel] rcu: sysctl: Panic on RCU Stall
    Message-id: <1473431897-10309-2-git-send-email-lvenanci@redhat.com>
    Patchwork-id: 157744
    O-Subject: [RHEL7.4 PATCH 2/2] rcu: sysctl: Panic on RCU Stall
    Bugzilla: 1360867
    RH-Acked-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1360867
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11723174
    Upstream: linus commit 088e9d253d3a4ab7e058dd84bb532c32dadf1882
    Tested: kt1lite
    
    commit 088e9d253d3a4ab7e058dd84bb532c32dadf1882
    Author: Daniel Bristot de Oliveira <bristot@redhat.com>
    Date:   Thu Jun 2 13:51:41 2016 -0300
    
        rcu: sysctl: Panic on RCU Stall
    
        It is not always easy to determine the cause of an RCU stall just by
        analysing the RCU stall messages, mainly when the problem is caused
        by the indirect starvation of rcu threads. For example, when preempt_rcu
        is not awakened due to the starvation of a timer softirq.
    
        We have been hard coding panic() in the RCU stall functions for
        some time while testing the kernel-rt. But this is not possible in
        some scenarios, like when supporting customers.
    
        This patch implements the sysctl kernel.panic_on_rcu_stall. If
        set to 1, the system will panic() when an RCU stall takes place,
        enabling the capture of a vmcore. The vmcore provides a way to analyze
        all kernel/tasks states, helping out to point to the culprit and the
        solution for the stall.
    
        The kernel.panic_on_rcu_stall sysctl is disabled by default.
    
        Changes from v1:
        - Fixed a typo in the git log
        - The if(sysctl_panic_on_rcu_stall) panic() is in a static function
        - Fixed the CONFIG_TINY_RCU compilation issue
        - The var sysctl_panic_on_rcu_stall is now __read_mostly
    
        Cc: Jonathan Corbet <corbet@lwn.net>
        Cc: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
        Cc: Josh Triplett <josh@joshtriplett.org>
        Cc: Steven Rostedt <rostedt@goodmis.org>
        Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
        Cc: Lai Jiangshan <jiangshanlai@gmail.com>
        Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
        Reviewed-by: Josh Triplett <josh@joshtriplett.org>
        Reviewed-by: Arnaldo Carvalho de Melo <acme@kernel.org>
        Tested-by: "Luis Claudio R. Goncalves" <lgoncalv@redhat.com>
        Signed-off-by: Daniel Bristot de Oliveira <bristot@redhat.com>
        Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    
    Signed-off-by: Lauro Ramos Venancio <lvenanci@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -101,6 +101,8 @@ static int num_rcu_lvl[] = {  /* Number of rcu_nodes at specified level. */
 	NUM_RCU_LVL_4,
 };
 int rcu_num_nodes __read_mostly = NUM_RCU_NODES; /* Total # rcu_nodes in use. */
+/* panic() on RCU Stall sysctl. */
+int sysctl_panic_on_rcu_stall __read_mostly;
 
 /*
  * The rcu_scheduler_active variable transitions from zero to one just
@@ -878,6 +880,12 @@ static void rcu_dump_cpu_stacks(struct rcu_state *rsp)
 	}
 }
 
+static inline void panic_on_rcu_stall(void)
+{
+	if (sysctl_panic_on_rcu_stall)
+		panic("RCU Stall\n");
+}
+
 static void print_other_cpu_stall(struct rcu_state *rsp, unsigned long gpnum)
 {
 	int cpu;
@@ -958,6 +966,8 @@ static void print_other_cpu_stall(struct rcu_state *rsp, unsigned long gpnum)
 
 	rcu_print_detail_task_stall(rsp);
 
+	panic_on_rcu_stall();
+
 	force_quiescent_state(rsp);  /* Kick them all. */
 }
 
@@ -1005,6 +1015,8 @@ static void check_cpu_stall(struct rcu_state *rsp, struct rcu_data *rdp)
 	if (rcu_cpu_stall_suppress || !rcu_gp_in_progress(rsp))
 		return;
 	j = ACCESS_ONCE(jiffies);
+
+	panic_on_rcu_stall();
 
 	/*
 	 * Lots of memory barriers to reject false positives.
