e015b0acd82e41312ae4df356a0a1266dbe648b1
Jiri Olsa <jolsa@redhat.com>
2016-09-22 15:02:30 -0400

    [x86] perf/x86/intel/rapl: Fix pmus free during cleanup
    Message-id: <1474556776-20705-27-git-send-email-jolsa@redhat.com>
    Patchwork-id: 158090
    O-Subject: [PATCH RHEL7.3 BZ1373817 026/252] perf/x86/intel/rapl: Fix pmus free during cleanup
    Bugzilla: 1373817
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: 1373817
    https://bugzilla.redhat.com/show_bug.cgi?id=1373817
    
    upstream
    ========
    commit 275ae411e56f8f900fa364da29c4706f9af4e1f3
    Author: Vincent Stehl <vincent.stehle@intel.com>
    Date: Tue May 24 16:53:49 2016 +0200
    
    description
    ===========
    On rapl cleanup path, kfree() is given by mistake the address of the
    pointer of the structure to free (rapl_pmus->pmus + i). Pass the pointer
    instead (rapl_pmus->pmus[i]).
    
    Fixes: 9de8d686955b "perf/x86/intel/rapl: Convert it to a per package facility"
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/events/intel/rapl.c
+++ arch/x86/events/intel/rapl.c
@@ -675,7 +675,7 @@ static void __init cleanup_rapl_pmus(void)
 	int i;
 
 	for (i = 0; i < rapl_pmus->maxpkg; i++)
-		kfree(rapl_pmus->pmus + i);
+		kfree(rapl_pmus->pmus[i]);
 	kfree(rapl_pmus);
 }
 
