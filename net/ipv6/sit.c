033205ebeefc54793653b5182ab971a6403ba262
Ivan Vecera <ivecera@redhat.com>
2016-10-24 15:29:11 -0400

    [net] sit: use kfree_skb to replace dev_kfree_skb
    Message-id: <1477322982-14599-2-git-send-email-ivecera@redhat.com>
    Patchwork-id: 159522
    O-Subject: [RHEL7.4 PATCH 01/32] sit: use kfree_skb to replace dev_kfree_skb
    Bugzilla: 1382040
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Jakub Sitnicki <jkbs@redhat.com>
    RH-Acked-by: Eric Garver <egarver@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    BZ#1382040
    
    Upstream commit(s):
    commit 66028310aedc782e3a9a221f1a9ec12b7e587e50
    Author: Gao feng <gaofeng@cn.fujitsu.com>
    Date:   Mon Nov 25 17:21:24 2013 +0800
    
        sit: use kfree_skb to replace dev_kfree_skb
    
        In failure case, we should use kfree_skb not
        dev_kfree_skb to free skbuff, dev_kfree_skb
        is defined as consume_skb.
    
        Trace takes advantage of this point.
    
        Signed-off-by: Gao feng <gaofeng@cn.fujitsu.com>
        Acked-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -873,7 +873,7 @@ static netdev_tx_t ipip6_tunnel_xmit(struct sk_buff *skb,
 		if (!new_skb) {
 			ip_rt_put(rt);
 			dev->stats.tx_dropped++;
-			dev_kfree_skb(skb);
+			kfree_skb(skb);
 			return NETDEV_TX_OK;
 		}
 		if (skb->sk)
@@ -901,7 +901,7 @@ static netdev_tx_t ipip6_tunnel_xmit(struct sk_buff *skb,
 tx_error_icmp:
 	dst_link_failure(skb);
 tx_error:
-	dev_kfree_skb(skb);
+	kfree_skb(skb);
 	dev->stats.tx_errors++;
 	return NETDEV_TX_OK;
 }
@@ -942,7 +942,7 @@ static netdev_tx_t sit_tunnel_xmit(struct sk_buff *skb,
 
 tx_err:
 	dev->stats.tx_errors++;
-	dev_kfree_skb(skb);
+	kfree_skb(skb);
 	return NETDEV_TX_OK;
 
 }
