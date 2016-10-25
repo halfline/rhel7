ed257f17d42e66730faa1621a3e9050db800cdb0
Jiri Olsa <jolsa@redhat.com>
2016-10-25 12:46:10 -0400

    [tools] perf record: Add --tail-synthesize option
    Message-id: <1477399629-8377-264-git-send-email-jolsa@redhat.com>
    Patchwork-id: 159659
    O-Subject: [PATCH RHEL7.4 BZ1387909 263/322] perf record: Add --tail-synthesize option
    Bugzilla: 1387909
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1387909
    https://bugzilla.redhat.com/show_bug.cgi?id=1387909
    
    upstream
    ========
    commit 4ea648aec01982d5a57816a95c4665d6081e78f9
    Author: Wang Nan <wangnan0@huawei.com>
    Date: Thu Jul 14 08:34:47 2016 +0000
    
    description
    ===========
    When working with overwritable ring buffer there's a inconvenience
    problem: if perf dumps data after a long period after it starts,
    non-sample events may lost, which makes following 'perf report' unable
    to identify proc name and mmap layout. For example:
    
     # perf record -m 4 -e raw_syscalls:* -g --overwrite --switch-output \
            dd if=/dev/zero of=/dev/null
    
    send SIGUSR2 after dd runs long enough. The resuling perf.data lost
    correct comm and mmap events:
    
     # perf script -i perf.data.2016061522374354
     perf 24478 [004] 2581325.601789:  raw_syscalls:sys_exit: NR 0 = 512
     ^^^^
     Should be 'dd'
                       27b2e8 syscall_slow_exit_work+0xfe2000e3 (/lib/modules/4.6.0-rc3+/build/vmlinux)
                       203cc7 do_syscall_64+0xfe200117 (/lib/modules/4.6.0-rc3+/build/vmlinux)
                       b18d83 return_from_SYSCALL_64+0xfe200000 (/lib/modules/4.6.0-rc3+/build/vmlinux)
                 7f47c417edf0 [unknown] ([unknown])
                 ^^^^^^^^^^^^
                 Fail to unwind
    
    This patch provides a '--tail-synthesize' option, allows perf to collect
    system status when finalizing output file. In resuling output file, the
    non-sample events reflect system status when dumping data.
    
    After this patch:
     # perf record -m 4 -e raw_syscalls:* -g --overwrite --switch-output --tail-synthesize \
            dd if=/dev/zero of=/dev/null
    
     # perf script -i perf.data.2016061600544998
     dd 27364 [004] 2583244.994464: raw_syscalls:sys_enter: NR 1 (1, ...
     ^^
     Correct comm
                       203a18 syscall_trace_enter_phase2+0xfe2001a8 ([kernel.kallsyms])
                       203aa5 syscall_trace_enter+0xfe200055 ([kernel.kallsyms])
                       203caa do_syscall_64+0xfe2000fa ([kernel.kallsyms])
                       b18d83 return_from_SYSCALL_64+0xfe200000 ([kernel.kallsyms])
                        d8e50 __GI___libc_write+0xffff01d9639f4010 (/tmp/oxygen_root-w00229757/lib64/libc-2.18.so)
                        ^^^^^
                        Correct unwind
    
    This option doesn't aim to solve this problem completely. If a process
    terminates before SIGUSR2, we still lost its COMM and MMAP events. For
    example, we can't unwind correctly from the final perf.data we get from
    the previous example, because when perf collects the final output file
    (when we press C-c), 'dd' has been terminated so its '/proc/<pid>/mmap'
    becomes empty.
    
    However, this is a cheaper choice. To completely solve this problem we
    need to continously output non-sample events. To satisify the
    requirement of daemonization, we need to merge them periodically. It is
    possible but requires much more code and cycles.
    
    Automatically select --tail-synthesize when --overwrite is provided.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/builtin-record.c
+++ tools/perf/builtin-record.c
@@ -602,12 +602,15 @@ record__finish_output(struct record *rec)
 	return;
 }
 
-static int record__synthesize_workload(struct record *rec)
+static int record__synthesize_workload(struct record *rec, bool tail)
 {
 	struct {
 		struct thread_map map;
 		struct thread_map_data map_data;
 	} thread_map;
+
+	if (rec->opts.tail_synthesize != tail)
+		return 0;
 
 	thread_map.map.nr = 1;
 	thread_map.map.map[0].pid = rec->evlist->workload.pid;
@@ -619,7 +622,7 @@ static int record__synthesize_workload(struct record *rec)
 						 rec->opts.proc_map_timeout);
 }
 
-static int record__synthesize(struct record *rec);
+static int record__synthesize(struct record *rec, bool tail);
 
 static int
 record__switch_output(struct record *rec, bool at_exit)
@@ -629,6 +632,10 @@ record__switch_output(struct record *rec, bool at_exit)
 
 	/* Same Size:      "2015122520103046"*/
 	char timestamp[] = "InvalidTimestamp";
+
+	record__synthesize(rec, true);
+	if (target__none(&rec->opts.target))
+		record__synthesize_workload(rec, true);
 
 	rec->samples = 0;
 	record__finish_output(rec);
@@ -652,7 +659,7 @@ record__switch_output(struct record *rec, bool at_exit)
 
 	/* Output tracking events */
 	if (!at_exit) {
-		record__synthesize(rec);
+		record__synthesize(rec, false);
 
 		/*
 		 * In 'perf record --switch-output' without -a,
@@ -664,7 +671,7 @@ record__switch_output(struct record *rec, bool at_exit)
 		 * perf_event__synthesize_thread_map() for those events.
 		 */
 		if (target__none(&rec->opts.target))
-			record__synthesize_workload(rec);
+			record__synthesize_workload(rec, false);
 	}
 	return fd;
 }
@@ -718,7 +725,7 @@ static const struct perf_event_mmap_page *record__pick_pc(struct record *rec)
 	return NULL;
 }
 
-static int record__synthesize(struct record *rec)
+static int record__synthesize(struct record *rec, bool tail)
 {
 	struct perf_session *session = rec->session;
 	struct machine *machine = &session->machines.host;
@@ -727,6 +734,9 @@ static int record__synthesize(struct record *rec)
 	struct perf_tool *tool = &rec->tool;
 	int fd = perf_data_file__fd(file);
 	int err = 0;
+
+	if (rec->opts.tail_synthesize != tail)
+		return 0;
 
 	if (file->is_pipe) {
 		err = perf_event__synthesize_attrs(tool, session,
@@ -881,7 +891,7 @@ static int __cmd_record(struct record *rec, int argc, const char **argv)
 
 	machine = &session->machines.host;
 
-	err = record__synthesize(rec);
+	err = record__synthesize(rec, false);
 	if (err < 0)
 		goto out_child;
 
@@ -1045,6 +1055,9 @@ static int __cmd_record(struct record *rec, int argc, const char **argv)
 	if (!quiet)
 		fprintf(stderr, "[ perf record: Woken up %ld times to write data ]\n", waking);
 
+	if (target__none(&rec->opts.target))
+		record__synthesize_workload(rec, true);
+
 out_child:
 	if (forks) {
 		int exit_status;
@@ -1063,6 +1076,7 @@ out_child:
 	} else
 		status = err;
 
+	record__synthesize(rec, true);
 	/* this will be recalculated during process_buildids() */
 	rec->samples = 0;
 
@@ -1303,6 +1317,8 @@ struct option __record_options[] = {
 	OPT_BOOLEAN_SET('i', "no-inherit", &record.opts.no_inherit,
 			&record.opts.no_inherit_set,
 			"child tasks do not inherit counters"),
+	OPT_BOOLEAN(0, "tail-synthesize", &record.opts.tail_synthesize,
+		    "synthesize non-sample events at the end of output"),
 	OPT_BOOLEAN(0, "overwrite", &record.opts.overwrite, "use overwrite mode"),
 	OPT_UINTEGER('F', "freq", &record.opts.user_freq, "profile at this frequency"),
 	OPT_CALLBACK('m', "mmap-pages", &record.opts, "pages[,pages]",
@@ -1482,6 +1498,9 @@ int cmd_record(int argc, const char **argv, const char *prefix __maybe_unused)
 			disable_buildid_cache();
 		}
 	}
+
+	if (record.opts.overwrite)
+		record.opts.tail_synthesize = true;
 
 	if (rec->evlist->nr_entries == 0 &&
 	    perf_evlist__add_default(rec->evlist) < 0) {
