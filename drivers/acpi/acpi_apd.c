7b46958a977e59980452a3da3dc826a0f6d57659
Suravee Suthikulpanit <ssuthiku@redhat.com>
2016-10-26 21:00:10 -0400

    [acpi] acpi / apd: Add device HID for future AMD UART controller
    Message-id: <1477515611-10272-4-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 160023
    O-Subject: [ PATCH BZ 1329004 3/4] ACPI / APD: Add device HID for future AMD UART controller
    Bugzilla: 1329004
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1329004
    Upstream Status: upstreamed
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11956111
    
    commit f5eda99ee6c0c3e85be9d6f20685151514d5102f
    Author: Wang Hongcheng <annie.wang@amd.com>
    Date:   Fri Mar 11 17:28:23 2016 +0800
    
        ACPI / APD: Add device HID for future AMD UART controller
    
        Add device HID AMDI0020 to match the AMD ACPI Vendor ID (AMDI) as
        registered in http://www.uefi.org/acpi_id_list, and the UART
        controller on future AMD paltform will use the HID instead of AMD0020.
    
        Signed-off-by: Wang Hongcheng <annie.wang@amd.com>
        Acked-by: Ken Xue <Ken.Xue@amd.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/acpi/acpi_apd.c
+++ drivers/acpi/acpi_apd.c
@@ -135,6 +135,7 @@ static const struct acpi_device_id acpi_apd_device_ids[] = {
 	/* Generic apd devices */
 	{ "AMD0010", APD_ADDR(cz_i2c_desc) },
 	{ "AMD0020", APD_ADDR(cz_uart_desc) },
+	{ "AMDI0020", APD_ADDR(cz_uart_desc) },
 	{ "AMD0030", },
 	{ }
 };
