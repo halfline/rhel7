5d89962f4dfab60b4a3149c4b500dec63980ba40
Steve Best <sbest@redhat.com>
2016-11-11 16:31:06 -0500

    [powerpc] powernv: Drop reference added by kset_find_obj()
    Message-id: <20161111163106.15082.5563.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 160331
    O-Subject: [PATCH RHEL7.4 BZ1394164] powerpc/powernv : Drop reference added by kset_find_obj()
    Bugzilla: 1394164
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1394164
    
    Description:
    ------------
    In a situation, where Linux kernel gets notified about duplicate error log
    from OPAL, it is been observed that kernel fails to remove sysfs entries
    (/sys/firmware/opal/elog/0xXXXXXXXX) of such error logs. This is because,
    we currently search the error log/dump kobject in the kset list via
    'kset_find_obj()' routine. Which eventually increment the reference count
    by one, once it founds the kobject.
    
    So, unless we decrement the reference count by one after it found the kobject,
    we would not be able to release the kobject properly later.
    
    This patch adds the 'kobject_put()' which was missing earlier.
    
    Signed-off-by: Mukesh Ojha <mukesh02@linux.vnet.ibm.com>
    Cc: stable@vger.kernel.org
    Reviewed-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    
    Brew:
    -----
    https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12081216
    
    Upstream:
    ---------
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=a9cbf0b2195b695cbeeeecaa4e2770948c212e9a
    
    Test Status:
    ------------
    Successfully tested by IBM.
    
    I did sanity boot testing on the following (ibm-p8-garrison-06.lab4) power system.
    
    ---------------------------------------------------------------
    Steve Best
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/platforms/powernv/opal-elog.c
+++ arch/powerpc/platforms/powernv/opal-elog.c
@@ -251,6 +251,7 @@ static void elog_work_fn(struct work_struct *work)
 	uint64_t elog_type;
 	int rc;
 	char name[2+16+1];
+	struct kobject *kobj;
 
 	rc = opal_get_elog_size(&id, &size, &type);
 	if (rc != OPAL_SUCCESS) {
@@ -273,8 +274,12 @@ static void elog_work_fn(struct work_struct *work)
 	 * that gracefully and not create two conflicting
 	 * entries.
 	 */
-	if (kset_find_obj(elog_kset, name))
+	kobj = kset_find_obj(elog_kset, name);
+	if (kobj) {
+		/* Drop reference added by kset_find_obj() */
+		kobject_put(kobj);
 		return;
+	}
 
 	create_elog_obj(log_id, elog_size, elog_type);
 }
