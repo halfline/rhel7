31bf49310be208f118b5dd691604a2cebf1577d0
David Arcari <darcari@redhat.com>
2016-11-15 14:40:34 -0500

    [edac] mce_amd: Add missing SMCA error descriptions
    Message-id: <1479220841-110730-30-git-send-email-darcari@redhat.com>
    Patchwork-id: 160415
    O-Subject: [RHEL7.4 PATCH BZ 1389383 29/36] EDAC/mce_amd: Add missing SMCA error descriptions
    Bugzilla: 1389383
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Aristeu Rozanski <aris@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1389383
    
    commit c019b951e1f9f1de0c5b0726032e3adf34c523a7
    Author: Yazen Ghannam <Yazen.Ghannam@amd.com>
    Date:   Mon Sep 12 09:59:32 2016 +0200
    
        EDAC/mce_amd: Add missing SMCA error descriptions
    
        Add missing SMCA error descriptions to the error descriptions arrays.
    
        Signed-off-by: Yazen Ghannam <Yazen.Ghannam@amd.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Link: http://lkml.kernel.org/r/1472673994-12235-3-git-send-email-Yazen.Ghannam@amd.com
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Cc: Aristeu Rozanski <aris@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/edac/mce_amd.c
+++ drivers/edac/mce_amd.c
@@ -176,6 +176,8 @@ static const char * const f17h_if_mce_desc[] = {
 	"BPQ snoop parity on Thread 1",
 	"L1 BTB multi-match error",
 	"L2 BTB multi-match error",
+	"L2 Cache Response Poison error",
+	"System Read Data error",
 };
 
 static const char * const f17h_l2_mce_desc[] = {
@@ -189,6 +191,7 @@ static const char * const f17h_de_mce_desc[] = {
 	"uop cache tag parity error",
 	"uop cache data parity error",
 	"Insn buffer parity error",
+	"uop queue parity error",
 	"Insn dispatch queue parity error",
 	"Fetch address FIFO parity",
 	"Patch RAM data parity",
@@ -205,6 +208,9 @@ static const char * const f17h_ex_mce_desc[] = {
 	"EX payload parity",
 	"Checkpoint queue parity",
 	"Retire dispatch queue parity",
+	"Retire status queue parity error",
+	"Scheduling queue parity error",
+	"Branch buffer queue parity error",
 };
 
 static const char * const f17h_fp_mce_desc[] = {
@@ -214,6 +220,7 @@ static const char * const f17h_fp_mce_desc[] = {
 	"NSQ parity error",
 	"Retire queue parity",
 	"Status register file parity",
+	"Hardware assertion",
 };
 
 static const char * const f17h_l3_mce_desc[] = {
