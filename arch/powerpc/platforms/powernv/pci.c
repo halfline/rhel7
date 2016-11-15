e2b58579d7ec20b0037653a8f94f514fba0a49dd
Steve Best <sbest@redhat.com>
2016-11-15 16:53:49 -0500

    [powerpc] powernv: Fix stale PE primary bus
    Message-id: <20161115165349.15390.2349.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 160429
    O-Subject: [PATCH RHEL7.4 BZ1395275] powerpc/powernv: Fix stale PE primary bus
    Bugzilla: 1395275
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1395275
    
    Description:
    ------------
    When PCI bus is unplugged during full hotplug for EEH recovery,
    the platform PE instance (struct pnv_ioda_pe) isn't released and
    it dereferences the stale PCI bus that has been released. It leads
    to kernel crash when referring to the stale PCI bus.
    
    This fixes the issue by correcting the PE's primary bus when it's
    oneline at plugging time, in pnv_pci_dma_bus_setup() which is to
    be called by pcibios_fixup_bus().
    
    Cc: stable@vger.kernel.org # v4.1+
    Reported-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Reported-by: Pradipta Ghosh <pradghos@in.ibm.com>
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Tested-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Brew:
    -----
    https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12098486
    
    Upstream:
    ---------
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=1bc74f1ccd457832dc515fc1febe6655985fdcd2
    
    Test Status:
    ------------
    Successfully tested by IBM.
    
    I did sanity boot testing on the following (ibm-p8-rhevm-09.lab4) power system.
    
    ---------------------------------------------------------------
    Steve Best
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/platforms/powernv/pci.c
+++ arch/powerpc/platforms/powernv/pci.c
@@ -779,6 +779,26 @@ u64 pnv_pci_dma_get_required_mask(struct pci_dev *pdev)
 	return __dma_get_required_mask(&pdev->dev);
 }
 
+void pnv_pci_dma_bus_setup(struct pci_bus *bus)
+{
+	struct pci_controller *hose = bus->sysdata;
+	struct pnv_phb *phb = hose->private_data;
+	struct pnv_ioda_pe *pe;
+
+	list_for_each_entry(pe, &phb->ioda.pe_list, list) {
+		if (!(pe->flags & (PNV_IODA_PE_BUS | PNV_IODA_PE_BUS_ALL)))
+			continue;
+
+		if (!pe->pbus)
+			continue;
+
+		if (bus->number == ((pe->rid >> 8) & 0xFF)) {
+			pe->pbus = bus;
+			break;
+		}
+	}
+}
+
 void pnv_pci_shutdown(void)
 {
 	struct pci_controller *hose;
