0ad4e65b7e676258b5162e93f6b84736087e3902
Ivan Vecera <ivecera@redhat.com>
2016-11-16 10:29:28 -0500

    [net] libnl: nla_put_msecs(): align on a 64-bit area
    Message-id: <1479292191-17634-37-git-send-email-ivecera@redhat.com>
    Patchwork-id: 160525
    O-Subject: [RHEL7.4 net PATCH 36/59] libnl: nla_put_msecs(): align on a 64-bit area
    Bugzilla: 1382040
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    BZ#1382040
    
    Backport note: Prefixed enumeration for NDTPA_MCAST_REPROBES to preserve
    userspace ABI with upstream.
    
    Upstream commit(s):
    commit 2175d87cc3561c02e605bc8ac81ee5d875a51b9e
    Author: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Date:   Fri Apr 22 17:31:21 2016 +0200
    
        libnl: nla_put_msecs(): align on a 64-bit area
    
        nla_data() is now aligned on a 64-bit area.
    
        Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/uapi/linux/neighbour.h
+++ include/uapi/linux/neighbour.h
@@ -126,6 +126,8 @@ enum {
 	NDTPA_PROXY_QLEN,		/* u32 */
 	NDTPA_LOCKTIME,			/* u64, msecs */
 	NDTPA_QUEUE_LENBYTES,		/* u32 */
+	__RH_RESERVED_NDTPA_MCAST_REPROBES,	/* u32 */
+	NDTPA_PAD,
 	__NDTPA_MAX
 };
 #define NDTPA_MAX (__NDTPA_MAX - 1)
@@ -158,6 +160,7 @@ enum {
 	NDTA_PARMS,			/* nested TLV NDTPA_* */
 	NDTA_STATS,			/* struct ndt_stats, read-only */
 	NDTA_GC_INTERVAL,		/* u64, msecs */
+	NDTA_PAD,
 	__NDTA_MAX
 };
 #define NDTA_MAX (__NDTA_MAX - 1)
