9a847db4b0159f4f626b9a4c695e04e14ac7d6b8
Eric Garver <egarver@redhat.com>
2016-12-02 15:00:43 -0500

    [net] Move skb_has_shared_frag check out of GRE code and into segmentation
    Message-id: <20161202150100.24663-7-egarver@redhat.com>
    Patchwork-id: 160968
    O-Subject: [PATCH 06/23] net: Move skb_has_shared_frag check out of GRE code and into segmentation
    Bugzilla: 1326353
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Jakub Sitnicki <jkbs@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1326353
    Upstream Status: linus commit ddff00d420432d54eb420bb33034bb8e22dd2543
    
    commit ddff00d420432d54eb420bb33034bb8e22dd2543
    Author: Alexander Duyck <aduyck@mirantis.com>
    Date:   Fri Feb 5 15:27:55 2016 -0800
    
        net: Move skb_has_shared_frag check out of GRE code and into segmentation
    
        The call skb_has_shared_frag is used in the GRE path and skb_checksum_help
        to verify that no frags can be modified by an external entity.  This check
        really doesn't belong in the GRE path but in the skb_segment function
        itself.  This way any protocol that might be segmented will be performing
        this check before attempting to offload a checksum to software.
    
        Signed-off-by: Alexander Duyck <aduyck@mirantis.com>
        Acked-by: Tom Herbert <tom@herbertland.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Eric Garver <egarver@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3285,6 +3285,11 @@ skip_fraglist:
 
 perform_csum_check:
 		if (!csum) {
+			if (skb_has_shared_frag(nskb)) {
+				err = __skb_linearize(nskb);
+				if (err)
+					goto err;
+			}
 			if (!nskb->remcsum_offload)
 				nskb->ip_summed = CHECKSUM_NONE;
 			SKB_GSO_CB(nskb)->csum =
