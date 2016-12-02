5ddcf36452b0569c09d96543c3900e40e3aba45b
Hangbin Liu <haliu@redhat.com>
2016-12-02 04:07:04 -0500

    [net] sctp: rename WORD_TRUNC/ROUND macros
    Message-id: <1480651625-25420-3-git-send-email-haliu@redhat.com>
    Patchwork-id: 160960
    O-Subject: [RHEL7.4 net PATCH 2/3] sctp: rename WORD_TRUNC/ROUND macros
    Bugzilla: 1399459
    CVE: CVE-2016-9555
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Davide Caratti <dcaratti@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1399459
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=12187771
    Upstream Status: net.git commit e2f036a97271
    
    commit e2f036a97271cf5811ee754bf321a29a814577f9
    Author: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Date:   Wed Sep 21 08:45:55 2016 -0300
    
        sctp: rename WORD_TRUNC/ROUND macros
    
        To something more meaningful these days, specially because this is
        working on packet headers or lengths and which are not tied to any CPU
        arch but to the protocol itself.
    
        So, WORD_TRUNC becomes SCTP_TRUNC4 and WORD_ROUND becomes SCTP_PAD4.
    
        Reported-by: David Laight <David.Laight@ACULAB.COM>
        Reported-by: David Miller <davem@davemloft.net>
        Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sctp/transport.c
+++ net/sctp/transport.c
@@ -240,7 +240,7 @@ void sctp_transport_pmtu(struct sctp_transport *transport, struct sock *sk)
 	}
 
 	if (transport->dst) {
-		transport->pathmtu = WORD_TRUNC(dst_mtu(transport->dst));
+		transport->pathmtu = SCTP_TRUNC4(dst_mtu(transport->dst));
 	} else
 		transport->pathmtu = SCTP_DEFAULT_MAXSEGMENT;
 }
@@ -294,7 +294,7 @@ void sctp_transport_route(struct sctp_transport *transport,
 		return;
 	}
 	if (transport->dst) {
-		transport->pathmtu = WORD_TRUNC(dst_mtu(transport->dst));
+		transport->pathmtu = SCTP_TRUNC4(dst_mtu(transport->dst));
 
 		/* Initialize sk->sk_rcv_saddr, if the transport is the
 		 * association's active path for getsockname().
