989aa2a5917ddffd49a90a4cda2b2757fcb71b7f
Gustavo Duarte <gduarte@redhat.com>
2016-12-02 21:06:49 -0500

    [powerpc] eeh: EEH device for VF
    Message-id: <1480712817-27602-27-git-send-email-gduarte@redhat.com>
    Patchwork-id: 161015
    O-Subject: [RHEL7.4 PATCH BZ 1315131 26/34] powerpc/eeh: EEH device for VF
    Bugzilla: 1315131
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    
    BZ 1315131
    Upstream Status: 39218cd00ebf08b16edf015adc363de42d9ad612
    
    Backport: Used RH_KABI_EXTEND macro to avoid breaking kABI.
    
    commit 39218cd00ebf08b16edf015adc363de42d9ad612
    Author: Wei Yang <weiyang@linux.vnet.ibm.com>
    Date:   Fri Mar 4 10:53:07 2016 +1100
    
        powerpc/eeh: EEH device for VF
    
        VFs and their corresponding pdn are created and released dynamically
        when their PF's SRIOV capability is enabled and disabled. This creates
        and releases EEH devices for VFs when creating and releasing their pdn
        instances, which means EEH devices and pdn instances have same life
        cycle. Also, VF's EEH device is identified by (struct eeh_dev::physfn).
    
        Signed-off-by: Wei Yang <weiyang@linux.vnet.ibm.com>
        Acked-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/include/asm/eeh.h
+++ arch/powerpc/include/asm/eeh.h
@@ -150,6 +150,7 @@ struct eeh_dev {
 	RH_KABI_EXTEND(int pcix_cap)	/* Saved PCIx capability	*/
 	RH_KABI_EXTEND(int pcie_cap)	/* Saved PCIe capability	*/
 	RH_KABI_EXTEND(int aer_cap)	/* Saved AER capability		*/
+	RH_KABI_EXTEND(struct pci_dev *physfn)	/* Associated SRIOV PF	*/
 };
 
 static inline struct pci_dn *eeh_dev_to_pdn(struct eeh_dev *edev)
