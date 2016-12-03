6b61da8481618f81fb38c60abee9329e67e2ad57
Gustavo Duarte <gduarte@redhat.com>
2016-12-02 21:06:45 -0500

    [powerpc] pci/iov: Rename and export virtfn_{add, remove}
    Message-id: <1480712817-27602-23-git-send-email-gduarte@redhat.com>
    Patchwork-id: 161016
    O-Subject: [RHEL7.4 PATCH BZ 1315131 22/34] PCI/IOV: Rename and export virtfn_{add, remove}
    Bugzilla: 1315131
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    
    BZ 1315131
    Upstream Status: c194f7ea7f68f2690533832ec22f0d7ed4f2d74d
    
    commit c194f7ea7f68f2690533832ec22f0d7ed4f2d74d
    Author: Wei Yang <weiyang@linux.vnet.ibm.com>
    Date:   Fri Mar 4 10:53:03 2016 +1100
    
        PCI/IOV: Rename and export virtfn_{add, remove}
    
        During EEH recovery, hotplug is applied to the devices which don't
        have drivers or their drivers don't support EEH. However, the hotplug,
        which was implemented based on PCI bus, can't be applied to VF directly.
        Instead, we unplug and plug individual PCI devices (VFs).
    
        This renames virtn_{add,remove}() and exports them so they can be used
        in PCI hotplug during EEH recovery.
    
        Signed-off-by: Wei Yang <weiyang@linux.vnet.ibm.com>
        Reviewed-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
        Acked-by: Bjorn Helgaas <bhelgaas@google.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/pci/iov.c
+++ drivers/pci/iov.c
@@ -113,7 +113,7 @@ resource_size_t pci_iov_resource_size(struct pci_dev *dev, int resno)
 	return dev->sriov->barsz[resno - PCI_IOV_RESOURCES];
 }
 
-static int virtfn_add(struct pci_dev *dev, int id, int reset)
+int pci_iov_add_virtfn(struct pci_dev *dev, int id, int reset)
 {
 	int i;
 	int rc = -ENOMEM;
@@ -188,7 +188,7 @@ failed:
 	return rc;
 }
 
-static void virtfn_remove(struct pci_dev *dev, int id, int reset)
+void pci_iov_remove_virtfn(struct pci_dev *dev, int id, int reset)
 {
 	char buf[VIRTFN_ID_LEN];
 	struct pci_dev *virtfn;
@@ -329,7 +329,7 @@ static int sriov_enable(struct pci_dev *dev, int nr_virtfn)
 	}
 
 	for (i = 0; i < initial; i++) {
-		rc = virtfn_add(dev, i, 0);
+		rc = pci_iov_add_virtfn(dev, i, 0);
 		if (rc)
 			goto failed;
 	}
@@ -341,7 +341,7 @@ static int sriov_enable(struct pci_dev *dev, int nr_virtfn)
 
 failed:
 	for (j = 0; j < i; j++)
-		virtfn_remove(dev, j, 0);
+		pci_iov_remove_virtfn(dev, j, 0);
 
 	pcibios_sriov_disable(dev);
 err_pcibios:
@@ -367,7 +367,7 @@ static void sriov_disable(struct pci_dev *dev)
 		return;
 
 	for (i = 0; i < iov->num_VFs; i++)
-		virtfn_remove(dev, i, 0);
+		pci_iov_remove_virtfn(dev, i, 0);
 
 	pcibios_sriov_disable(dev);
 
