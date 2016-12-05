23172ddeef55c6a847eac248220eb7b29f4c4201
Prarit Bhargava <prarit@redhat.com>
2016-12-05 15:04:13 -0500

    [x86] revert "perf/uncore: Disable uncore on kdump kernel"
    Message-id: <1480950253-31765-5-git-send-email-prarit@redhat.com>
    Patchwork-id: 161060
    O-Subject: [RHEL7.4 PATCH BZ 1373738 4/4] Revert "[x86] perf/uncore: Disable uncore on kdump kernel"
    Bugzilla: 1373738
    RH-Acked-by: Baoquan He <bhe@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1373738
    
    This reverts commit d7aa19f928ad6e65d7bbf537d9ca34140dc13739, which was
    introduced as a workaround.
    
    Cc: Jiri Olsa <jolsa@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/events/intel/uncore.c
+++ arch/x86/events/intel/uncore.c
@@ -1,4 +1,3 @@
-#include <linux/crash_dump.h>
 #include "uncore.h"
 
 static struct intel_uncore_type *empty_uncore[] = { NULL, };
@@ -1389,9 +1388,6 @@ static int __init intel_uncore_init(void)
 		return -ENODEV;
 
 	if (cpu_has_hypervisor)
-		return -ENODEV;
-
-	if (is_kdump_kernel())
 		return -ENODEV;
 
 	max_packages = topology_max_packages();
