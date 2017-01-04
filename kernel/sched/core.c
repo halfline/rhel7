408a3bfaf705033bc742360ad93518aa489c6951
Luiz Capitulino <lcapitulino@redhat.com>
2017-01-04 15:42:48 -0500

    [kernel] nohz: Affine unpinned timers to housekeepers
    Message-id: <1483544570-317-3-git-send-email-lcapitulino@redhat.com>
    Patchwork-id: 162062
    O-Subject: [RHEL7.4 PATCH 2/4] nohz: Affine unpinned timers to housekeepers
    Bugzilla: 1392593
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Pankaj Gupta <pagupta@redhat.com>
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    
    From: Vatika Harlalka <vatikaharlalka@gmail.com>
    
    The problem addressed in this patch is about affining unpinned
    timers. Adaptive or Full Dynticks CPUs are currently disturbed
    by unnecessary jitter due to firing of such timers on them.
    
    This patch will affine timers to online CPUs which are not full
    dynticks in NOHZ_FULL configured systems. It should not
    introduce overhead in nohz full off case due to static keys.
    
    Signed-off-by: Vatika Harlalka <vatikaharlalka@gmail.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Reviewed-by: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Chris Metcalf <cmetcalf@ezchip.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1441119060-2230-2-git-send-email-fweisbec@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 9642d18eee2cd169b60c6ac0f20bda745b5a3d1e)
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	include/linux/tick.h
    	kernel/sched/core.c
    
    RHEL7-NOTEs:
    
     - Conflicts are caused because of refactorings done upstream
       on the timer and nohz_full code
    
     - The huge timer refactoring done upstream starting at 3bb475a34,
       moved the idle_cpu() checks from hrtimer_get_target() and __mod_timer()
       to get_nohz_timer_target(). As we don't have this refactoring in
       RHEL7, I had to drop the checks from those functions as part of
       the conflict resolution
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -592,15 +592,21 @@ int get_nohz_timer_target(void)
 	int i;
 	struct sched_domain *sd;
 
+	if (!idle_cpu(cpu) && is_housekeeping_cpu(cpu))
+		return cpu;
+
 	rcu_read_lock();
 	for_each_domain(cpu, sd) {
 		for_each_cpu(i, sched_domain_span(sd)) {
-			if (!idle_cpu(i)) {
+			if (!idle_cpu(i) && is_housekeeping_cpu(cpu)) {
 				cpu = i;
 				goto unlock;
 			}
 		}
 	}
+
+	if (!is_housekeeping_cpu(cpu))
+		cpu = housekeeping_any_cpu();
 unlock:
 	rcu_read_unlock();
 	return cpu;
