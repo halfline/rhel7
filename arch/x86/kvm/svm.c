5cdef0ab14b97856e41623a9b86dc3bd04bf8bc0
Suravee Suthikulpanit <ssuthiku@redhat.com>
2017-01-09 06:38:39 -0500

    [x86] kvm: x86: Introducing kvm_x86_ops.apicv_post_state_restore
    Message-id: <1483943924-26394-14-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 162201
    O-Subject: [RHEL7.4 PATCH BZ 1133711 13/18] KVM: x86: Introducing kvm_x86_ops.apicv_post_state_restore
    Bugzilla: 1133711
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1133711
    
    commit be8ca170edf342cc45ae4a6431ef192e5cbe211e
    Author: Suravee Suthikulpanit <Suravee.Suthikulpanit@amd.com>
    Date:   Wed May 4 14:09:49 2016 -0500
    
        KVM: x86: Introducing kvm_x86_ops.apicv_post_state_restore
    
        Adding kvm_x86_ops hooks to allow APICv to do post state restore.
        This is required to support VM save and restore feature.
    
        Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krcmar <rkrcmar@redhat.com>
    Cc: Bandan Das <bsd@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Cc: Paul Lai <plai@redhat.com>
    Cc: rhvirt-patches@redhat.com
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -4770,6 +4770,15 @@ static void svm_sched_in(struct kvm_vcpu *vcpu, int cpu)
 {
 }
 
+static inline void avic_post_state_restore(struct kvm_vcpu *vcpu)
+{
+	if (avic_handle_apic_id_update(vcpu) != 0)
+		return;
+	if (avic_handle_dfr_update(vcpu) != 0)
+		return;
+	avic_handle_ldr_update(vcpu);
+}
+
 static struct kvm_x86_ops svm_x86_ops = {
 	.cpu_has_kvm_support = has_svm,
 	.disabled_by_bios = is_disabled,
@@ -4847,6 +4856,7 @@ static struct kvm_x86_ops svm_x86_ops = {
 	.sync_pir_to_irr = svm_sync_pir_to_irr,
 	.hwapic_irr_update = svm_hwapic_irr_update,
 	.hwapic_isr_update = svm_hwapic_isr_update,
+	.apicv_post_state_restore = avic_post_state_restore,
 
 	.set_tss_addr = svm_set_tss_addr,
 	.get_tdp_level = get_npt_level,
