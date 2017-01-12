b31d2ee2d54527ce650f0073eba34e1a38375f76
Jiri Olsa <jolsa@redhat.com>
2017-01-12 11:38:20 -0500

    [tools] toops: Sync tools/include/uapi/linux/perf_event.h with the kernel
    Message-id: <1484221101-7047-10-git-send-email-jolsa@redhat.com>
    Patchwork-id: 162375
    O-Subject: [PATCH RHEL7.4 BZ1391242 09/10] toops: Sync tools/include/uapi/linux/perf_event.h with the kernel
    Bugzilla: 1391242
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1391242
    https://bugzilla.redhat.com/show_bug.cgi?id=1391242
    
    upstream
    ========
    RHEL7 only
    
    description
    ===========
    Updating the tools header with clockid changes. We did not have
    tools headers when clockid was introduced so there's no related
    upsteram change.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/include/uapi/linux/perf_event.h
+++ tools/include/uapi/linux/perf_event.h
@@ -341,9 +341,10 @@ struct perf_event_attr {
 				__reserved_1   : 40;
 #else
 				comm_exec      :  1, /* flag comm events that are due to an exec */
+				use_clockid    :  1, /* use @clockid for time fields */
 				context_switch :  1, /* context switch data */
 				write_backward :  1, /* Write ring buffer from end to beginning */
-				__reserved_1   : 37;
+				__reserved_1   : 36;
 #endif
 
 	union {
@@ -373,8 +374,7 @@ struct perf_event_attr {
 	 */
 	__u32	sample_stack_user;
 
-	/* Align to u64. */
-	__u32	__reserved_2;
+	__s32	clockid;
 	/*
 	 * Defines set of regs to dump for each sample
 	 * state captured on:
