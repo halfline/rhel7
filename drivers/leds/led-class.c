64cee6805705883cd990ddf7bb6a52727c2926f7
David Arcari <darcari@redhat.com>
2017-01-13 13:21:58 -0500

    [kernel] leds: add led-class attribute-group support
    Message-id: <1484313768-84649-27-git-send-email-darcari@redhat.com>
    Patchwork-id: 162429
    O-Subject: [RHEL7.4 PATCH BZ 1396495 26/76] leds: add led-class attribute-group support
    Bugzilla: 1396495
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1396495
    
    commit d0d480cce8f522b37c2c1de38230fc9ad15fa506
    Author: Johan Hovold <johan@kernel.org>
    Date:   Wed Jun 25 10:08:44 2014 -0700
    
        leds: add led-class attribute-group support
    
        Allow led-class devices to be created with optional attribute groups.
    
        This is needed in order to allow led drivers to create custom device
        attributes in a race-free manner.
    
        Signed-off-by: Johan Hovold <johan@kernel.org>
        Signed-off-by: Bryan Wu <cooloney@gmail.com>
    
    Cc: David Arcari <darcari@redhat.com>
    Cc: Gustavo Duarte <gduarte@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -183,8 +183,9 @@ static int led_resume(struct device *dev)
  */
 int led_classdev_register(struct device *parent, struct led_classdev *led_cdev)
 {
-	led_cdev->dev = device_create(leds_class, parent, 0, led_cdev,
-				      "%s", led_cdev->name);
+	led_cdev->dev = device_create_with_groups(leds_class, parent, 0,
+					led_cdev, led_cdev->groups,
+					"%s", led_cdev->name);
 	if (IS_ERR(led_cdev->dev))
 		return PTR_ERR(led_cdev->dev);
 
