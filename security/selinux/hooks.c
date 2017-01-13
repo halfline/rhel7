88c9664539eb5f50c62d3b21b20f15815addc758
Andreas Grunbacher <agruenba@redhat.com>
2017-01-13 12:56:01 -0500

    [security] selinux: don't revalidate inodes in selinux_socket_getpeersec_dgram()
    Message-id: <1484312169-30737-11-git-send-email-agruenba@redhat.com>
    Patchwork-id: 162401
    O-Subject: [RHEL7.4 PATCH 10/18] selinux: don't revalidate inodes in selinux_socket_getpeersec_dgram()
    Bugzilla: 437984
    RH-Acked-by: Paul Moore <pmoore@redhat.com>
    RH-Acked-by: Robert S Peterson <rpeterso@redhat.com>
    RH-Acked-by: Abhijith Das <adas@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=437984,
              https://bugzilla.redhat.com/show_bug.cgi?id=1389003
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12353175
    
    Upstream commit 899134f2f6e27dcae1fee12593c492577cc80987
    Author: Paul Moore <paul@paul-moore.com>
    Date:   Mon Mar 28 15:19:10 2016 -0400
    
        We don't have to worry about socket inodes being invalidated so
        use inode_security_novalidate() to fetch the inode's security blob.
    
        Signed-off-by: Paul Moore <paul@paul-moore.com>
    
        Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    [agruenba: With the previous commit (selinux: Don't sleep inside
    inode_getsecid hook), this commit effectively doesn't change anything.]
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -4547,6 +4547,7 @@ static int selinux_socket_getpeersec_dgram(struct socket *sock, struct sk_buff *
 {
 	u32 peer_secid = SECSID_NULL;
 	u16 family;
+	struct inode_security_struct *isec;
 
 	if (skb && skb->protocol == htons(ETH_P_IP))
 		family = PF_INET;
@@ -4557,9 +4558,10 @@ static int selinux_socket_getpeersec_dgram(struct socket *sock, struct sk_buff *
 	else
 		goto out;
 
-	if (sock && family == PF_UNIX)
-		selinux_inode_getsecid(SOCK_INODE(sock), &peer_secid);
-	else if (skb)
+	if (sock && family == PF_UNIX) {
+		isec = inode_security_novalidate(SOCK_INODE(sock));
+		peer_secid = isec->sid;
+	} else if (skb)
 		selinux_skb_peerlbl_sid(skb, family, &peer_secid);
 
 out:
