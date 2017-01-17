2505490a3c038276b12a6909d2e58a635162b62a
Gustavo Duarte <gduarte@redhat.com>
2017-01-16 19:44:09 -0500

    [cpuidle] menu: Fix menu_select() for CPUIDLE_DRIVER_STATE_START == 0
    Message-id: <1484595849-2702-1-git-send-email-gduarte@redhat.com>
    Patchwork-id: 162640
    O-Subject: [RHEL7.4 PATCH BZ 1409211] cpuidle: menu: Fix menu_select() for CPUIDLE_DRIVER_STATE_START == 0
    Bugzilla: 1409211
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ 1409211
    Upstream Status: 9c4b2867ed7c8c8784dd417ffd16e705e81eb145
    Backport: minor changes because upstream uses interactivity_req instead of expected_us for polling.
    
    commit 9c4b2867ed7c8c8784dd417ffd16e705e81eb145
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Thu Jan 14 23:24:22 2016 +0100
    
        cpuidle: menu: Fix menu_select() for CPUIDLE_DRIVER_STATE_START == 0
    
        Commit a9ceb78bc75c (cpuidle,menu: use interactivity_req to disable
        polling) exposed a bug in menu_select() causing it to return -1
        on systems with CPUIDLE_DRIVER_STATE_START equal to zero, although
        it should have returned 0.  As a result, idle states are not entered
        by CPUs on those systems.
    
        Namely, on the systems in question data->last_state_idx is initially
        equal to -1 and the above commit modified the condition that would
        have caused it to be changed to 0 to be less likely to trigger which
        exposed the problem.  However, setting data->last_state_idx initially
        to -1 doesn't make sense at all and on the affected systems it should
        always be set to CPUIDLE_DRIVER_STATE_START (ie. 0) unconditionally,
        so make that happen.
    
        Fixes: a9ceb78bc75c (cpuidle,menu: use interactivity_req to disable polling)
        Reported-and-tested-by: Sudeep Holla <sudeep.holla@arm.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -269,7 +269,6 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 		data->needs_update = 0;
 	}
 
-	data->last_state_idx = CPUIDLE_DRIVER_STATE_START - 1;
 	data->exit_us = 0;
 
 	/* Special case when user has set very strict latency requirement */
@@ -299,14 +298,19 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 
 	get_typical_interval(data);
 
-	/*
-	 * We want to default to C1 (hlt), not to busy polling
-	 * unless the timer is happening really really soon.
-	 */
-	if (data->expected_us > 5 &&
-	    !drv->states[CPUIDLE_DRIVER_STATE_START].disabled &&
-		dev->states_usage[CPUIDLE_DRIVER_STATE_START].disable == 0)
+	if (CPUIDLE_DRIVER_STATE_START > 0) {
+		data->last_state_idx = CPUIDLE_DRIVER_STATE_START - 1;
+		/*
+		 * We want to default to C1 (hlt), not to busy polling
+		 * unless the timer is happening really really soon.
+		 */
+		if (data->expected_us > 5 &&
+		    !drv->states[CPUIDLE_DRIVER_STATE_START].disabled &&
+			dev->states_usage[CPUIDLE_DRIVER_STATE_START].disable == 0)
+			data->last_state_idx = CPUIDLE_DRIVER_STATE_START;
+	} else {
 		data->last_state_idx = CPUIDLE_DRIVER_STATE_START;
+	}
 
 	/*
 	 * Find the idle state with the lowest power while satisfying
