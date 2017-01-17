6c42e993feaec39c915def60fefa6549791d3767
Jes Sorensen <Jes.Sorensen@redhat.com>
2017-01-16 19:06:10 -0500

    [md] raid5-cache: remove unnecessary function parameters
    Message-id: <20170116190626.7813-96-Jes.Sorensen@redhat.com>
    Patchwork-id: 162595
    O-Subject: [RHEL7.4 PATCH 095/111] md/raid5-cache: remove unnecessary function parameters
    Bugzilla: 1380016
    RH-Acked-by: Xiao Ni <xni@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: JackieLiu <liuyun01@kylinos.cn>
    
    The function parameter 'recovery_list' is not used in
    body, we can delete it
    
    Signed-off-by: JackieLiu <liuyun01@kylinos.cn>
    Reviewed-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Shaohua Li <shli@fb.com>
    (cherry picked from commit 9b69173e5c6000b2c6fafc5085dcd7b173f073c8)
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/raid5-cache.c
+++ drivers/md/raid5-cache.c
@@ -66,7 +66,7 @@ static char *r5c_journal_mode_str[] = {"write-through",
 /*
  * raid5 cache state machine
  *
- * With rhe RAID cache, each stripe works in two phases:
+ * With the RAID cache, each stripe works in two phases:
  *	- caching phase
  *	- writing-out phase
  *
@@ -1567,7 +1567,6 @@ out:
 
 static struct stripe_head *
 r5c_recovery_alloc_stripe(struct r5conf *conf,
-			  struct list_head *recovery_list,
 			  sector_t stripe_sect,
 			  sector_t log_start)
 {
@@ -1748,8 +1747,7 @@ r5c_recovery_analyze_meta_block(struct r5l_log *log,
 						stripe_sect);
 
 		if (!sh) {
-			sh = r5c_recovery_alloc_stripe(conf, cached_stripe_list,
-						       stripe_sect, ctx->pos);
+			sh = r5c_recovery_alloc_stripe(conf, stripe_sect, ctx->pos);
 			/*
 			 * cannot get stripe from raid5_get_active_stripe
 			 * try replay some stripes
@@ -1758,8 +1756,7 @@ r5c_recovery_analyze_meta_block(struct r5l_log *log,
 				r5c_recovery_replay_stripes(
 					cached_stripe_list, ctx);
 				sh = r5c_recovery_alloc_stripe(
-					conf, cached_stripe_list,
-					stripe_sect, ctx->pos);
+					conf, stripe_sect, ctx->pos);
 			}
 			if (!sh) {
 				pr_debug("md/raid:%s: Increasing stripe cache size to %d to recovery data on journal.\n",
@@ -1768,8 +1765,7 @@ r5c_recovery_analyze_meta_block(struct r5l_log *log,
 				raid5_set_cache_size(mddev,
 						     conf->min_nr_stripes * 2);
 				sh = r5c_recovery_alloc_stripe(
-					conf, cached_stripe_list, stripe_sect,
-					ctx->pos);
+					conf, stripe_sect, ctx->pos);
 			}
 			if (!sh) {
 				pr_err("md/raid:%s: Cannot get enough stripes due to memory pressure. Recovery failed.\n",
