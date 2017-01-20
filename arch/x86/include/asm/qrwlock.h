16d353c956e7f7deb6787252462b9f65a1d1aa87
Waiman Long <longman@redhat.com>
2017-01-19 19:24:53 -0500

    [kernel] locking/qrwlock: Rename functions to queued_*()
    Message-id: <1484853897-25697-43-git-send-email-longman@redhat.com>
    Patchwork-id: 162970
    O-Subject: [RHEL7.4 PATCH 42/46] locking/qrwlock: Rename functions to queued_*()
    Bugzilla: 1241990
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1241990
    
    Commit: f7d71f2052555ae57b47322f2c2f6c29ff2438ae
    Author: Waiman Long <Waiman.Long@hp.com>
    Date: Fri, 19 Jun 2015 11:50:00 -0400
    
        locking/qrwlock: Rename functions to queued_*()
    
        To sync up with the naming convention used in qspinlock, all the
        qrwlock functions were renamed to started with "queued" instead of
        "queue".
    
        Signed-off-by: Waiman Long <Waiman.Long@hp.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Arnd Bergmann <arnd@arndb.de>
        Cc: Douglas Hatch <doug.hatch@hp.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Scott J Norton <scott.norton@hp.com>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Will Deacon <will.deacon@arm.com>
        Link:
        http://lkml.kernel.org/r/1434729002-57724-2-git-send-email-Waiman.Long@hp.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/include/asm/qrwlock.h
+++ arch/x86/include/asm/qrwlock.h
@@ -4,8 +4,8 @@
 #include <asm-generic/qrwlock_types.h>
 
 #if !defined(CONFIG_X86_OOSTORE) && !defined(CONFIG_X86_PPRO_FENCE)
-#define queue_write_unlock queue_write_unlock
-static inline void queue_write_unlock(struct qrwlock *lock)
+#define queued_write_unlock queue_writed_unlock
+static inline void queued_write_unlock(struct qrwlock *lock)
 {
         barrier();
         ACCESS_ONCE(*(u8 *)&lock->cnts) = 0;
