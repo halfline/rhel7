e03b2d39fced830f556a097400106ce9f50d2226
Prarit Bhargava <prarit@redhat.com>
2017-01-20 18:12:28 -0500

    [drivers] Set dev->device_rh to NULL after free
    Message-id: <1484935948-30189-1-git-send-email-prarit@redhat.com>
    Patchwork-id: 163099
    O-Subject: [RHEL7.4 PATCH BZ 1414064] drivers: Set dev->device_rh to NULL after free
    Bugzilla: 1414064
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    RH-Acked-by: Maurizio Lombardi <mlombard@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1414064
    
    Build info:
    Date: Fri Jan 20 13:07:42 EST 2017
    Build OS: Red Hat Enterprise Linux Server release 7.4 Beta (Maipo)
    System name: intel-purley-04.khw.lab.eng.bos.redhat.com with -j56
    Built on: kernel-3.10.0-547.el7
    Arch built: ppc64 s390x x86_64
    
    RHEL_only.
    
    The ses driver was panicking during rmmod.  This occurs because the
    enclosure driver (used by ses) is doing a device_del() followed by a
    device_add() in enclosure_remove_device(). In this case the device_add()
    will not allocate a struct device_rh and accesses to the device_rh struct
    will result in a kernel will panic or corrupted memory.
    
    device_del() must set the dev->device_rh pointer to NULL.
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    
    Cc: Vitaly Kuznetsov <vkuznets@redhat.com>
    Cc: mlombard@redhat.com
    Cc: coughlan@redhat.com
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1341,6 +1341,7 @@ void device_del(struct device *dev)
 	kobject_del(&dev->kobj);
 	/* This free's the allocation done in device_add() */
 	kfree(dev->device_rh);
+	dev->device_rh = NULL;
 	put_device(parent);
 }
 
