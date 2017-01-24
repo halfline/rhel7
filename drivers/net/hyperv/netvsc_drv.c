947a82b255cff7711e046baf11c51b44474d6997
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-01-24 14:08:51 -0500

    [netdrv] netvsc: add rcu_read locking to netvsc callback
    Message-id: <20170124140851.13819-36-vkuznets@redhat.com>
    Patchwork-id: 163213
    O-Subject: [RHEL7.4 KERNEL PATCH 35/35] netvsc: add rcu_read locking to netvsc callback
    Bugzilla: 1395600
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    commit 0719e72ccb801829a3d735d187ca8417f0930459
    Author: stephen hemminger <stephen@networkplumber.org>
    Date:   Wed Jan 11 09:16:32 2017 -0800
    
        netvsc: add rcu_read locking to netvsc callback
    
        The receive callback (in tasklet context) is using RCU to get reference
        to associated VF network device but this is not safe. RCU read lock
        needs to be held. Found by running with full lockdep debugging
        enabled.
    
        Fixes: f207c10d9823 ("hv_netvsc: use RCU to protect vf_netdev")
        Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -660,6 +660,7 @@ int netvsc_recv_callback(struct hv_device *device_obj,
 	 * policy filters on the host). Deliver these via the VF
 	 * interface in the guest.
 	 */
+	rcu_read_lock();
 	vf_netdev = rcu_dereference(net_device_ctx->vf_netdev);
 	if (vf_netdev && (vf_netdev->flags & IFF_UP))
 		net = vf_netdev;
@@ -668,6 +669,7 @@ int netvsc_recv_callback(struct hv_device *device_obj,
 	skb = netvsc_alloc_recv_skb(net, packet, csum_info, *data, vlan_tci);
 	if (unlikely(!skb)) {
 		++net->stats.rx_dropped;
+		rcu_read_unlock();
 		return NVSP_STAT_FAIL;
 	}
 
@@ -697,6 +699,7 @@ int netvsc_recv_callback(struct hv_device *device_obj,
 	 * TODO - use NAPI?
 	 */
 	netif_rx(skb);
+	rcu_read_unlock();
 
 	return 0;
 }
