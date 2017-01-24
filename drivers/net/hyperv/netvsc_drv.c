7a7c27620a09776d4298bcafe0df1f313e3d1313
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-01-24 14:08:34 -0500

    [netdrv] hv_netvsc: style cleanups
    Message-id: <20170124140851.13819-19-vkuznets@redhat.com>
    Patchwork-id: 163195
    O-Subject: [RHEL7.4 KERNEL PATCH 18/35] hv_netvsc: style cleanups
    Bugzilla: 1395600
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    commit 796cc88c32c1bd1f833d596448ac785a8736e57c
    Author: Stephen Hemminger <sthemmin@microsoft.com>
    Date:   Tue Aug 23 12:17:47 2016 -0700
    
        hv_netvsc: style cleanups
    
        Fix most of the complaints about the style of the code.
        Things like extra blank lines and return statements.
    
        Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -41,7 +41,6 @@
 
 #include "hyperv_net.h"
 
-
 #define RING_SIZE_MIN 64
 #define LINKCHANGE_INT (2 * HZ)
 #define NETVSC_HW_FEATURES	(NETIF_F_RXCSUM | \
@@ -408,7 +407,6 @@ static int netvsc_start_xmit(struct sk_buff *skb, struct net_device *net)
 			FIELD_SIZEOF(struct sk_buff, cb));
 	packet = (struct hv_netvsc_packet *)skb->cb;
 
-
 	packet->q_idx = skb_get_queue_mapping(skb);
 
 	packet->total_data_buflen = skb->len;
@@ -594,7 +592,6 @@ void netvsc_linkstatus_callback(struct hv_device *device_obj,
 
 	schedule_delayed_work(&ndev_ctx->dwork, 0);
 }
-
 
 static struct sk_buff *netvsc_alloc_recv_skb(struct net_device *net,
 				struct hv_netvsc_packet *packet,
@@ -1300,7 +1297,6 @@ static int netvsc_vf_up(struct net_device *vf_netdev)
 	return NOTIFY_OK;
 }
 
-
 static int netvsc_vf_down(struct net_device *vf_netdev)
 {
 	struct net_device *ndev;
@@ -1333,7 +1329,6 @@ static int netvsc_vf_down(struct net_device *vf_netdev)
 
 	return NOTIFY_OK;
 }
-
 
 static int netvsc_unregister_vf(struct net_device *vf_netdev)
 {
@@ -1463,7 +1458,6 @@ static int netvsc_remove(struct hv_device *dev)
 		return 0;
 	}
 
-
 	ndev_ctx = netdev_priv(net);
 	net_device = ndev_ctx->nvdev;
 
@@ -1509,7 +1503,6 @@ static struct  hv_driver netvsc_drv = {
 	.probe = netvsc_probe,
 	.remove = netvsc_remove,
 };
-
 
 /*
  * On Hyper-V, every VF interface is matched with a corresponding
