9a3f6fc8b9612b475f41b83aa2c488a2710e7f93
Don Dutile <ddutile@redhat.com>
2017-01-30 22:30:44 -0500

    [net] rds: tcp: block BH in TCP callbacks
    Message-id: <1485815854-24569-166-git-send-email-ddutile@redhat.com>
    Patchwork-id: 163920
    O-Subject: [PATCH RHEL-7.4 165/575] rds: tcp: block BH in TCP callbacks
    Bugzilla: 1417284
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    commit 38036629cded6b96a9f9689758a88d067c4d4d44
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Tue May 17 17:44:08 2016 -0700
    
        rds: tcp: block BH in TCP callbacks
    
        TCP stack can now run from process context.
    
        Use read_lock_bh(&sk->sk_callback_lock) variant to restore previous
        assumption.
    
        Fixes: 5413d1babe8f ("net: do not block BH while processing socket backlog")
        Fixes: d41a69f1d390 ("tcp: make tcp_sendmsg() aware of socket backlog")
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    RHEL-7 note:
    Minor source differences, but patches apply.
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417284
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/rds/tcp_listen.c
+++ net/rds/tcp_listen.c
@@ -167,7 +167,7 @@ void rds_tcp_listen_data_ready(struct sock *sk, int bytes)
 
 	rdsdebug("listen data ready sk %p\n", sk);
 
-	read_lock(&sk->sk_callback_lock);
+	read_lock_bh(&sk->sk_callback_lock);
 	ready = sk->sk_user_data;
 	if (!ready) { /* check for teardown race */
 		ready = sk->sk_data_ready;
@@ -184,7 +184,7 @@ void rds_tcp_listen_data_ready(struct sock *sk, int bytes)
 		rds_tcp_accept_work(sk);
 
 out:
-	read_unlock(&sk->sk_callback_lock);
+	read_unlock_bh(&sk->sk_callback_lock);
 	ready(sk, bytes);
 }
 
