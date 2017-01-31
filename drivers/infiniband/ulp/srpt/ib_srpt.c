43e34b663b0dcc1d7989ee9fa30d0450125a0803
Don Dutile <ddutile@redhat.com>
2017-01-30 22:28:15 -0500

    [infiniband] ib/srpt: Simplify srpt_shutdown_session()
    Message-id: <1485815854-24569-17-git-send-email-ddutile@redhat.com>
    Patchwork-id: 163754
    O-Subject: [PATCH RHEL-7.4 016/575] IB/srpt: Simplify srpt_shutdown_session()
    Bugzilla: 1417284
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    commit 88936259c6a42c98e23181c9e1bba4b0bddb966a
    Author: Bart Van Assche <bart.vanassche@sandisk.com>
    Date:   Thu Feb 11 11:05:58 2016 -0800
    
        IB/srpt: Simplify srpt_shutdown_session()
    
        The target core guarantees that shutdown_session() is only invoked
        once per session. This means that the ib_srpt target driver doesn't
        have to track whether or not shutdown_session() has been called.
        Additionally, ensure that target_sess_cmd_list_set_waiting() is
        called before target_wait_for_sess_cmds() by moving it into
        srpt_release_channel_work().
    
        Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Sagi Grimberg <sagig@mellanox.com>
        Cc: Alex Estrin <alex.estrin@intel.com>
        Signed-off-by: Doug Ledford <dledford@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417284
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/infiniband/ulp/srpt/ib_srpt.c
+++ drivers/infiniband/ulp/srpt/ib_srpt.c
@@ -1895,20 +1895,7 @@ static void srpt_close_ch(struct srpt_rdma_ch *ch)
  */
 static int srpt_shutdown_session(struct se_session *se_sess)
 {
-	struct srpt_rdma_ch *ch = se_sess->fabric_sess_ptr;
-	unsigned long flags;
-
-	spin_lock_irqsave(&ch->spinlock, flags);
-	if (ch->in_shutdown) {
-		spin_unlock_irqrestore(&ch->spinlock, flags);
-		return true;
-	}
-
-	ch->in_shutdown = true;
-	target_sess_cmd_list_set_waiting(se_sess);
-	spin_unlock_irqrestore(&ch->spinlock, flags);
-
-	return true;
+	return 1;
 }
 
 /**
@@ -2011,6 +1998,7 @@ static void srpt_release_channel_work(struct work_struct *w)
 	se_sess = ch->sess;
 	BUG_ON(!se_sess);
 
+	target_sess_cmd_list_set_waiting(se_sess);
 	target_wait_for_sess_cmds(se_sess);
 
 	transport_deregister_session_configfs(se_sess);
