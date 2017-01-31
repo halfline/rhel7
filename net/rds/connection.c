705d7f77cea691dd5a7aa67b152e3cb4465b98e8
Don Dutile <ddutile@redhat.com>
2017-01-30 22:34:43 -0500

    [net] rds: tcp: Enable multipath RDS for TCP
    Message-id: <1485815854-24569-405-git-send-email-ddutile@redhat.com>
    Patchwork-id: 164306
    O-Subject: [PATCH RHEL-7.4 404/575] RDS: TCP: Enable multipath RDS for TCP
    Bugzilla: 1417284
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    commit 5916e2c1554f3e36f770401c989c3c7fadf619ca
    Author: Sowmini Varadhan <sowmini.varadhan@oracle.com>
    Date:   Thu Jul 14 03:51:03 2016 -0700
    
        RDS: TCP: Enable multipath RDS for TCP
    
        Use RDS probe-ping to compute how many paths may be used with
        the peer, and to synchronously start the multiple paths. If mprds is
        supported, hash outgoing traffic to one of multiple paths in rds_sendmsg()
        when multipath RDS is supported by the transport.
    
        CC: Santosh Shilimkar <santosh.shilimkar@oracle.com>
        Signed-off-by: Sowmini Varadhan <sowmini.varadhan@oracle.com>
        Acked-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417284
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/rds/connection.c
+++ net/rds/connection.c
@@ -155,7 +155,7 @@ static struct rds_connection *__rds_conn_create(struct net *net,
 	struct hlist_head *head = rds_conn_bucket(laddr, faddr);
 	struct rds_transport *loop_trans;
 	unsigned long flags;
-	int ret;
+	int ret, i;
 	struct rds_transport *otrans = trans;
 
 	if (!is_outgoing && otrans->t_type == RDS_TRANS_TCP)
@@ -215,6 +215,12 @@ new_conn:
 
 	conn->c_trans = trans;
 
+	init_waitqueue_head(&conn->c_hs_waitq);
+	for (i = 0; i < RDS_MPATH_WORKERS; i++) {
+		__rds_conn_path_init(conn, &conn->c_path[i],
+				     is_outgoing);
+		conn->c_path[i].cp_index = i;
+	}
 	ret = trans->conn_alloc(conn, gfp);
 	if (ret) {
 		kmem_cache_free(rds_conn_slab, conn);
@@ -267,14 +273,6 @@ new_conn:
 			kmem_cache_free(rds_conn_slab, conn);
 			conn = found;
 		} else {
-			int i;
-
-			for (i = 0; i < RDS_MPATH_WORKERS; i++) {
-				__rds_conn_path_init(conn, &conn->c_path[i],
-						     is_outgoing);
-				conn->c_path[i].cp_index = i;
-			}
-
 			hlist_add_head_rcu(&conn->c_hash_node, head);
 			rds_cong_add_conn(conn);
 			rds_conn_count++;
@@ -672,6 +670,7 @@ EXPORT_SYMBOL_GPL(rds_conn_path_drop);
 
 void rds_conn_drop(struct rds_connection *conn)
 {
+	WARN_ON(conn->c_trans->t_mp_capable);
 	rds_conn_path_drop(&conn->c_path[0]);
 }
 EXPORT_SYMBOL_GPL(rds_conn_drop);
