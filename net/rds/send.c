3249399593037f59326eaa5ea69b4b622cba9afa
Don Dutile <ddutile@redhat.com>
2017-01-30 22:32:34 -0500

    [net] rds: Add rds_conn_path_connect_if_down() for MP-aware callers
    Message-id: <1485815854-24569-276-git-send-email-ddutile@redhat.com>
    Patchwork-id: 163998
    O-Subject: [PATCH RHEL-7.4 275/575] RDS: Add rds_conn_path_connect_if_down() for MP-aware callers
    Bugzilla: 1417284
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    commit 3c0a59001a416ec2a1c46576917732fe5b99336b
    Author: Sowmini Varadhan <sowmini.varadhan@oracle.com>
    Date:   Mon Jun 13 09:44:37 2016 -0700
    
        RDS: Add rds_conn_path_connect_if_down() for MP-aware callers
    
        rds_conn_path_connect_if_down() works on the rds_conn_path
        that it is passed. Callers who are not t_m_capable may continue
        calling rds_conn_connect_if_down, which will invoke
        rds_conn_path_connect_if_down() with the default c_path[0].
    
        Signed-off-by: Sowmini Varadhan <sowmini.varadhan@oracle.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417284
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/rds/send.c
+++ net/rds/send.c
@@ -1089,16 +1089,15 @@ int rds_sendmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *msg,
 		goto out;
 	}
 
-	rds_conn_connect_if_down(conn);
+	cpath = &conn->c_path[0];
+
+	rds_conn_path_connect_if_down(cpath);
 
 	ret = rds_cong_wait(conn->c_fcong, dport, nonblock, rs);
 	if (ret) {
 		rs->rs_seen_congestion = 1;
 		goto out;
 	}
-
-	cpath = &conn->c_path[0];
-
 	while (!rds_send_queue_rm(rs, conn, cpath, rm, rs->rs_bound_port,
 				  dport, &queued)) {
 		rds_stats_inc(s_send_queue_full);
@@ -1168,7 +1167,7 @@ rds_send_pong(struct rds_conn_path *cp, __be16 dport)
 	rm->m_daddr = cp->cp_conn->c_faddr;
 	rm->data.op_active = 1;
 
-	rds_conn_connect_if_down(cp->cp_conn);
+	rds_conn_path_connect_if_down(cp);
 
 	ret = rds_cong_wait(cp->cp_conn->c_fcong, dport, 1, NULL);
 	if (ret)
