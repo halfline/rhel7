d68527d994351a0ef3042ae76b63aa6b81c16753
Suravee Suthikulpanit <ssuthiku@redhat.com>
2017-01-31 03:51:19 -0500

    [acpi] acpica: Add "Windows 2015" string to _OSI support
    Message-id: <1485834706-31516-2-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 164319
    O-Subject: [RHEL74 PATCH v4 BZ 1329005 01/28] ACPICA: Add "Windows 2015" string to _OSI support.
    Bugzilla: 1329005 1354637
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1329005
    
    commit 796888e942b34cbbd738d9e5478b7d103ee38061
    Author: Bob Moore <robert.moore@intel.com>
    Date:   Mon Apr 13 11:50:37 2015 +0800
    
        ACPICA: Add "Windows 2015" string to _OSI support.
    
        ACPICA commit b293f602a67da478ae0bec129e68bd99787d9908
    
        This change adds this string for Windows 10.
    
        Link: https://github.com/acpica/acpica/commit/b293f602
        Signed-off-by: Bob Moore <robert.moore@intel.com>
        Signed-off-by: Lv Zheng <lv.zheng@intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/acpi/actypes.h
+++ include/acpi/actypes.h
@@ -1158,5 +1158,6 @@ struct acpi_memory_list {
 #define ACPI_OSI_WIN_VISTA_SP2          0x0A
 #define ACPI_OSI_WIN_7                  0x0B
 #define ACPI_OSI_WIN_8                  0x0C
+#define ACPI_OSI_WIN_10                 0x0D
 
 #endif				/* __ACTYPES_H__ */
