7f0597e163d86fad23da2c705f013a540c1d2d46
Suravee Suthikulpanit <ssuthiku@redhat.com>
2017-02-10 09:21:35 -0500

    [ntb] ntb_netdev: Fix skb free issue in open
    Message-id: <1486718506-16087-137-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 165747
    O-Subject: [RHEL7.4 PATCH BZ 1303727 136/147] ntb_netdev: Fix skb free issue in open
    Bugzilla: 1303727
    RH-Acked-by: Torez Smith <torez@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1303727
    
    commit e8bc2ebdceea3c0c6de16d899f0679654c308739
    Author: Jon Mason <jon.mason@intel.com>
    Date:   Fri Nov 22 16:50:57 2013 -0700
    
        ntb_netdev: Fix skb free issue in open
    
        In ntb_netdev_open, when ntb_transport_rx_enqueue fails the skb that was
        attempting to be enqueued is not freed.  Free this skb on the
        ntb_transport_rx_enqueue error.
    
        Signed-off-by: Jon Mason <jon.mason@intel.com>
    
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Cc: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ntb_netdev.c
+++ drivers/net/ntb_netdev.c
@@ -263,8 +263,10 @@ static int ntb_netdev_open(struct net_device *ndev)
 
 		rc = ntb_transport_rx_enqueue(dev->qp, skb, skb->data,
 					      ndev->mtu + ETH_HLEN);
-		if (rc == -EINVAL)
+		if (rc == -EINVAL) {
+			dev_kfree_skb(skb);
 			goto err;
+		}
 	}
 
 	setup_timer(&dev->tx_timer, ntb_netdev_tx_timer, (unsigned long)ndev);
