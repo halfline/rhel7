bbaa2317ddb2946f7e6cd60f94336fdaf9d80118
Stefan Hajnoczi <stefanha@redhat.com>
2017-02-14 09:59:49 -0500

    [vhost] vhost-vsock: remove unused vq variable
    Message-id: <20170214095955.24228-25-stefanha@redhat.com>
    Patchwork-id: 166069
    O-Subject: [RHEL7.4 kernel PATCH RESEND 24/30] vhost-vsock: remove unused vq variable
    Bugzilla: 1291282
    RH-Acked-by: Jason Wang <jasowang@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    From: Peng Tao <bergwolf@gmail.com>
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1440a3a12ba0a00f9fec7e6a62e49b11e079d381)
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/vhost/vsock.c
+++ drivers/vhost/vsock.c
@@ -199,7 +199,6 @@ static int
 vhost_transport_send_pkt(struct virtio_vsock_pkt *pkt)
 {
 	struct vhost_vsock *vsock;
-	struct vhost_virtqueue *vq;
 	int len = pkt->len;
 
 	/* Find the vhost_vsock according to guest context id  */
@@ -208,8 +207,6 @@ vhost_transport_send_pkt(struct virtio_vsock_pkt *pkt)
 		virtio_transport_free_pkt(pkt);
 		return -ENODEV;
 	}
-
-	vq = &vsock->vqs[VSOCK_VQ_RX];
 
 	if (pkt->reply)
 		atomic_inc(&vsock->queued_replies);
