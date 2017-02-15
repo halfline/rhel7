c0b65795815c7eecbd5f9b2453bd8846d50b5469
Baoquan He <bhe@redhat.com>
2017-02-15 14:45:48 -0500

    [x86] mm/kaslr: Propagate KASLR status to kernel proper
    Message-id: <1487170014-11002-40-git-send-email-bhe@redhat.com>
    Patchwork-id: 166225
    O-Subject: [RHEL7.4 PATCH v2 039/105] x86/mm/KASLR: Propagate KASLR status to kernel proper
    Bugzilla: 1290840
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Xunlei Pang <xpang@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    This is back ported from upstram directly.
    
    commit 78cac48c0434c82e860fade3cd0420a7a4adbb08
    Author: Borislav Petkov <bp@suse.de>
    Date:   Wed Apr 1 12:49:52 2015 +0200
    
        x86/mm/KASLR: Propagate KASLR status to kernel proper
    
        Commit:
    
          e2b32e678513 ("x86, kaslr: randomize module base load address")
    
        made module base address randomization unconditional and didn't regard
        disabled KKASLR due to CONFIG_HIBERNATION and command line option
        "nokaslr". For more info see (now reverted) commit:
    
          f47233c2d34f ("x86/mm/ASLR: Propagate base load address calculation")
    
        In order to propagate KASLR status to kernel proper, we need a single bit
        in boot_params.hdr.loadflags and we've chosen bit 1 thus leaving the
        top-down allocated bits for bits supposed to be used by the bootloader.
    
        Originally-From: Jiri Kosina <jkosina@suse.cz>
        Suggested-by: H. Peter Anvin <hpa@zytor.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Cc: Kees Cook <keescook@chromium.org>
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -932,10 +932,15 @@ static void rh_check_supported(void)
 static int
 dump_kernel_offset(struct notifier_block *self, unsigned long v, void *p)
 {
-	pr_emerg("Kernel Offset: 0x%lx from 0x%lx "
-		 "(relocation range: 0x%lx-0x%lx)\n",
-		 (unsigned long)&_text - __START_KERNEL, __START_KERNEL,
-		 __START_KERNEL_map, MODULES_VADDR-1);
+	if (kaslr_enabled()) {
+		pr_emerg("Kernel Offset: 0x%lx from 0x%lx (relocation range: 0x%lx-0x%lx)\n",
+			 (unsigned long)&_text - __START_KERNEL,
+			 __START_KERNEL,
+			 __START_KERNEL_map,
+			 MODULES_VADDR-1);
+	} else {
+		pr_emerg("Kernel Offset: disabled\n");
+	}
 
 	return 0;
 }
