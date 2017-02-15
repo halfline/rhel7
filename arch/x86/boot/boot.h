65a28dc61a8c05687f9514f07d5be7cda1208c7c
Baoquan He <bhe@redhat.com>
2017-02-15 14:45:13 -0500

    [x86] boot: Move CPU flags out of cpucheck
    Message-id: <1487170014-11002-5-git-send-email-bhe@redhat.com>
    Patchwork-id: 166189
    O-Subject: [RHEL7.4 PATCH v2 004/105] x86, boot: Move CPU flags out of cpucheck
    Bugzilla: 1290840
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Xunlei Pang <xpang@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    This is back ported from upstream. Conflict happened because
    earlier back port:
    commit b76e7c4f [x86] Disallow running with 32-bit PTEs to work around erratum
    
    commit dd78b97367bd575918204cc89107c1479d3fc1a7
    Author: Kees Cook <keescook@chromium.org>
    Date:   Thu Oct 10 17:18:13 2013 -0700
    
        x86, boot: Move CPU flags out of cpucheck
    
        Refactor the CPU flags handling out of the cpucheck routines so that
        they can be reused by the future ASLR routines (in order to detect CPU
        features like RDRAND and RDTSC).
    
        This reworks has_eflag() and has_fpu() to be used on both 32-bit and
        64-bit, and refactors the calls to cpuid to make them PIC-safe on 32-bit.
    
        Signed-off-by: Kees Cook <keescook@chromium.org>
        Link: http://lkml.kernel.org/r/1381450698-28710-2-git-send-email-keescook@chromium.org
        Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/boot/boot.h
+++ arch/x86/boot/boot.h
@@ -26,9 +26,8 @@
 #include <asm/boot.h>
 #include <asm/setup.h>
 #include "bitops.h"
-#include <asm/cpufeature.h>
-#include <asm/processor-flags.h>
 #include "ctype.h"
+#include "cpuflags.h"
 
 /* Useful macros */
 #define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
@@ -294,15 +293,7 @@ static inline int cmdline_find_option_bool(const char *option)
 	return __cmdline_find_option_bool(cmd_line_ptr, option);
 }
 
-
 /* cpu.c, cpucheck.c */
-struct cpu_features {
-	int level;		/* Family, or 64 for x86-64 */
-	int family;		/* Family, always */
-	int model;
-	u32 flags[NCAPINTS];
-};
-extern struct cpu_features cpu;
 int check_cpu(int *cpu_level_ptr, int *req_level_ptr, u32 **err_flags_ptr);
 int check_knl_erratum(void);
 int validate_cpu(void);
