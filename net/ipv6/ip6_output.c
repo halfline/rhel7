0907003d034ea68d58113724ac8998f8fb60efb9
Jakub Sitnicki <jkbs@redhat.com>
2017-02-21 12:29:52 -0500

    [net] ipv6: Don't use ufo handling on later transformed packets
    Message-id: <1487680192-29343-1-git-send-email-jkbs@redhat.com>
    Patchwork-id: 166657
    O-Subject: [RHEL7.4 net] ipv6: Don't use ufo handling on later transformed packets
    Bugzilla: 1388846
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Eric Garver <egarver@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1388846
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=12572264
    Tested: On local VM according to steps from the commit description
    Conflicts: Different context because we are missing:
      - acf8dd0a9d0b ("udp: only allow UFO for packets from SOCK_DGRAM sockets")
      - 40ba330227ad ("udp: disallow UFO for sockets with SO_NO_CHECK option")
    
    commit f89c56ce710afa65e1b2ead555b52c4807f34ff7
    Author: Jakub Sitnicki <jkbs@redhat.com>
    Date:   Wed Oct 26 11:21:14 2016 +0200
    
        ipv6: Don't use ufo handling on later transformed packets
    
        Similar to commit c146066ab802 ("ipv4: Don't use ufo handling on later
        transformed packets"), don't perform UFO on packets that will be IPsec
        transformed. To detect it we rely on the fact that headerlen in
        dst_entry is non-zero only for transformation bundles (xfrm_dst
        objects).
    
        Unwanted segmentation can be observed with a NETIF_F_UFO capable device,
        such as a dummy device:
    
          DEV=dum0 LEN=1493
    
          ip li add $DEV type dummy
          ip addr add fc00::1/64 dev $DEV nodad
          ip link set $DEV up
          ip xfrm policy add dir out src fc00::1 dst fc00::2 \
             tmpl src fc00::1 dst fc00::2 proto esp spi 1
          ip xfrm state add src fc00::1 dst fc00::2 \
             proto esp spi 1 enc 'aes' 0x0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b
    
          tcpdump -n -nn -i $DEV -t &
          socat /dev/zero,readbytes=$LEN udp6:[fc00::2]:$LEN
    
        tcpdump output before:
    
          IP6 fc00::1 > fc00::2: frag (0|1448) ESP(spi=0x00000001,seq=0x1), length 1448
          IP6 fc00::1 > fc00::2: frag (1448|48)
          IP6 fc00::1 > fc00::2: ESP(spi=0x00000001,seq=0x2), length 88
    
        ... and after:
    
          IP6 fc00::1 > fc00::2: frag (0|1448) ESP(spi=0x00000001,seq=0x1), length 1448
          IP6 fc00::1 > fc00::2: frag (1448|80)
    
        Fixes: e89e9cf539a2 ("[IPv4/IPv6]: UFO Scatter-gather approach")
    
        Signed-off-by: Jakub Sitnicki <jkbs@redhat.com>
        Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jakub Sitnicki <jkbs@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1276,7 +1276,7 @@ emsgsize:
 	if (((length > mtu) ||
 	     (skb && skb_is_gso(skb))) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
-	    (rt->dst.dev->features & NETIF_F_UFO)) {
+	    (rt->dst.dev->features & NETIF_F_UFO) && !rt->dst.header_len) {
 		err = ip6_ufo_append_data(sk, queue, getfrag, from, length,
 					  hh_len, fragheaderlen, exthdrlen,
 					  transhdrlen, mtu, flags, fl6);
