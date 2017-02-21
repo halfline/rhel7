acba86f08affe9649954ee3d56c81abfba47150d
Jonathan Toppins <jtoppins@redhat.com>
2017-02-21 03:13:28 -0500

    [netdrv] bnxt_en: Implement DCBNL to support host-based DCBX
    Message-id: <99e09937de06ae47276c2346e71924ce8592edf5.1487635857.git.jtoppins@redhat.com>
    Patchwork-id: 166606
    O-Subject: [PATCH RHEL7.4 57/95] bnxt_en: Implement DCBNL to support host-based DCBX.
    Bugzilla: 1382378
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    Support only IEEE DCBX initially.  Add IEEE DCBNL ops and functions to
    get and set the hardware DCBX parameters.  The DCB code is conditional on
    Kconfig CONFIG_BNXT_DCB.
    
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 7df4ae9fe85567a1710048da8229bd85e0da9df7)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	drivers/net/ethernet/broadcom/bnxt/bnxt.c
    	[jt: fix context difference due to upstream commit
    	 e1c6dccaf3af ("ethernet/broadcom: use core min/max MTU checking")
    	 not being backported. The MTU commit was dropped as the core infra
    	 has not been backported.]
    Bugzilla: 1382378
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12572150
    Tested: compile tested
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -54,6 +54,7 @@
 #include "bnxt.h"
 #include "bnxt_sriov.h"
 #include "bnxt_ethtool.h"
+#include "bnxt_dcb.h"
 
 #define BNXT_TX_TIMEOUT		(5 * HZ)
 
@@ -4997,7 +4998,7 @@ static void bnxt_enable_napi(struct bnxt *bp)
 	}
 }
 
-static void bnxt_tx_disable(struct bnxt *bp)
+void bnxt_tx_disable(struct bnxt *bp)
 {
 	int i;
 	struct bnxt_tx_ring_info *txr;
@@ -5015,7 +5016,7 @@ static void bnxt_tx_disable(struct bnxt *bp)
 	netif_carrier_off(bp->dev);
 }
 
-static void bnxt_tx_enable(struct bnxt *bp)
+void bnxt_tx_enable(struct bnxt *bp)
 {
 	int i;
 	struct bnxt_tx_ring_info *txr;
@@ -6690,6 +6691,7 @@ static void bnxt_remove_one(struct pci_dev *pdev)
 
 	bnxt_hwrm_func_drv_unrgtr(bp);
 	bnxt_free_hwrm_resources(bp);
+	bnxt_dcb_free(bp);
 	pci_iounmap(pdev, bp->bar2);
 	pci_iounmap(pdev, bp->bar1);
 	pci_iounmap(pdev, bp->bar0);
@@ -6913,6 +6915,8 @@ static int bnxt_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 			    NETIF_F_HW_VLAN_STAG_RX | NETIF_F_HW_VLAN_STAG_TX;
 	dev->features |= dev->hw_features | NETIF_F_HIGHDMA;
 	dev->priv_flags |= IFF_UNICAST_FLT;
+
+	bnxt_dcb_init(bp);
 
 #ifdef CONFIG_BNXT_SRIOV
 	init_waitqueue_head(&bp->sriov_cfg_wait);
