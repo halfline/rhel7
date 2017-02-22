6525ceabbae099fb367e57703bb0fe6cf7e81fa9
Prarit Bhargava <prarit@redhat.com>
2017-02-22 18:23:03 -0500

    [acpi] acpica: Update acpidump related header file changes
    Message-id: <1487787876-10121-9-git-send-email-prarit@redhat.com>
    Patchwork-id: 166776
    O-Subject: [RHEL7.4 PATCH BZ 1425180 008/101] ACPICA: Update acpidump related header file changes.
    Bugzilla: 1425180
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Al Stone <ahs3@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1425180
    
    commit efb835429fff1488e4718138cdf5bc855a6762a4
    Author: Lv Zheng <lv.zheng@intel.com>
    Date:   Thu Oct 31 09:31:12 2013 +0800
    
        ACPICA: Update acpidump related header file changes.
    
        This patch updates header files used by acpidump to reduce the
        source code differences between Linux and ACPICA upstream.
    
        This patch does not affect the generation of the Linux kernel binary.
    
        Signed-off-by: Lv Zheng <lv.zheng@intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/acpi/actypes.h
+++ include/acpi/actypes.h
@@ -265,6 +265,14 @@ typedef u32 acpi_physical_address;
 #define acpi_mutex                      void *
 #endif
 
+/*
+ * Compiler/Clibrary-dependent debug initialization. Used for ACPICA
+ * utilities only.
+ */
+#ifndef ACPI_DEBUG_INITIALIZE
+#define ACPI_DEBUG_INITIALIZE()
+#endif
+
 /*******************************************************************************
  *
  * Compiler-dependent types
