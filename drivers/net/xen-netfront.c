cce995571deaaa4d53df0f738f6baee4aa1232fe
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-02-22 15:59:57 -0500

    [netdrv] xen-netfront: handle backend CLOSED without CLOSING
    Message-id: <20170222160031.5322-3-vkuznets@redhat.com>
    Patchwork-id: 166723
    O-Subject: [RHEL7.4 KERNEL PATCH v2 02/36] xen-netfront: handle backend CLOSED without CLOSING
    Bugzilla: 1102638
    RH-Acked-by: Jason Wang <jasowang@redhat.com>
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    commit bce3ea81d5b2a33ed0e275d58a45f10ce95cbd50
    Author: David Vrabel <david.vrabel@citrix.com>
    Date:   Tue Feb 4 18:50:26 2014 +0000
    
        xen-netfront: handle backend CLOSED without CLOSING
    
        Backend drivers shouldn't transistion to CLOSED unless the frontend is
        CLOSED.  If a backend does transition to CLOSED too soon then the
        frontend may not see the CLOSING state and will not properly shutdown.
    
        So, treat an unexpected backend CLOSED state the same as CLOSING.
    
        Signed-off-by: David Vrabel <david.vrabel@citrix.com>
        Acked-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -1841,7 +1841,6 @@ static void netback_changed(struct xenbus_device *dev,
 	case XenbusStateReconfiguring:
 	case XenbusStateReconfigured:
 	case XenbusStateUnknown:
-	case XenbusStateClosed:
 		break;
 
 	case XenbusStateInitWait:
@@ -1856,6 +1855,10 @@ static void netback_changed(struct xenbus_device *dev,
 		netdev_notify_peers(netdev);
 		break;
 
+	case XenbusStateClosed:
+		if (dev->state == XenbusStateClosed)
+			break;
+		/* Missed the backend's CLOSING state -- fallthrough */
 	case XenbusStateClosing:
 		xenbus_frontend_closed(dev);
 		break;
