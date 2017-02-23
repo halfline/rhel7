e5a794660e077884a15d9fbce3e649008bf8ad88
Don Dutile <ddutile@redhat.com>
2017-02-23 05:37:16 -0500

    [netdrv] cxgb4: Convert to use simple_open()
    Message-id: <1487828466-9959-119-git-send-email-ddutile@redhat.com>
    Patchwork-id: 167186
    O-Subject: [PATCH RHEL-7.4 118/348] cxgb4: Convert to use simple_open()
    Bugzilla: 1385330 1417285
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417285
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1385330
    
    commit 524605e5ba5a897fb0a8c29398ed049100fe80aa
    Author: Wei Yongjun <weiyongjun1@huawei.com>
    Date:   Wed Sep 21 15:09:16 2016 +0000
    
        cxgb4: Convert to use simple_open()
    
        Remove an open coded simple_open() function and replace file
        operations references to the function with simple_open()
        instead.
    
        Generated by: scripts/coccinelle/api/simple_open.cocci
    
        Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/chelsio/cxgb4/cxgb4_debugfs.c
+++ drivers/net/ethernet/chelsio/cxgb4/cxgb4_debugfs.c
@@ -2757,12 +2757,6 @@ static void add_debugfs_mem(struct adapter *adap, const char *name,
 		de->d_inode->i_size = size_mb << 20;
 }
 
-static int blocked_fl_open(struct inode *inode, struct file *file)
-{
-	file->private_data = inode->i_private;
-	return 0;
-}
-
 static ssize_t blocked_fl_read(struct file *filp, char __user *ubuf,
 			       size_t count, loff_t *ppos)
 {
@@ -2806,7 +2800,7 @@ static ssize_t blocked_fl_write(struct file *filp, const char __user *ubuf,
 
 static const struct file_operations blocked_fl_fops = {
 	.owner   = THIS_MODULE,
-	.open    = blocked_fl_open,
+	.open    = simple_open,
 	.read    = blocked_fl_read,
 	.write   = blocked_fl_write,
 	.llseek  = generic_file_llseek,
