3f6f3d05799a57ca407160c45b69680631b8b087
Don Dutile <ddutile@redhat.com>
2017-02-23 05:38:30 -0500

    [infiniband] ib/mlx5: Increase flow table reference count in create rule
    Message-id: <1487828466-9959-193-git-send-email-ddutile@redhat.com>
    Patchwork-id: 167306
    O-Subject: [PATCH RHEL-7.4 192/348] IB/mlx5: Increase flow table reference count in create rule
    Bugzilla: 1385256 1385330 1417285
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417285
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1385330
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1385256
    
    commit d9d4980af21df94f527caeecd35bfa4bafe38c9c
    Author: Maor Gottlieb <maorg@mellanox.com>
    Date:   Sun Aug 28 14:16:33 2016 +0300
    
        IB/mlx5: Increase flow table reference count in create rule
    
        Move the reference count increasing of flow table to be in
        create_flow_rule, it will increase the reference count for each rule
        creation and not for each flow.
    
        Signed-off-by: Maor Gottlieb <maorg@mellanox.com>
        Signed-off-by: Leon Romanovsky <leon@kernel.org>
        Signed-off-by: Doug Ledford <dledford@redhat.com>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -1784,6 +1784,7 @@ static struct mlx5_ib_flow_handler *create_flow_rule(struct mlx5_ib_dev *dev,
 		goto free;
 	}
 
+	ft_prio->refcount++;
 	handler->prio = ft_prio;
 
 	ft_prio->flow_table = ft;
@@ -1808,6 +1809,7 @@ static struct mlx5_ib_flow_handler *create_dont_trap_rule(struct mlx5_ib_dev *de
 					       flow_attr, dst);
 		if (IS_ERR(handler_dst)) {
 			mlx5_del_flow_rule(handler->rule);
+			ft_prio->refcount--;
 			kfree(handler);
 			handler = handler_dst;
 		} else {
@@ -1870,6 +1872,7 @@ static struct mlx5_ib_flow_handler *create_leftovers_rule(struct mlx5_ib_dev *de
 						 dst);
 		if (IS_ERR(handler_ucast)) {
 			mlx5_del_flow_rule(handler->rule);
+			ft_prio->refcount--;
 			kfree(handler);
 			handler = handler_ucast;
 		} else {
@@ -1940,7 +1943,6 @@ static struct ib_flow *mlx5_ib_create_flow(struct ib_qp *qp,
 		goto destroy_ft;
 	}
 
-	ft_prio->refcount++;
 	mutex_unlock(&dev->flow_db.lock);
 	kfree(dst);
 
