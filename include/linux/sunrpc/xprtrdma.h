815eb43fc8cebe42e8dc8e3642354521370581ee
Don Dutile <ddutile@redhat.com>
2017-02-23 05:36:56 -0500

    [net] xprtrdma: Support larger inline thresholds
    Message-id: <1487828466-9959-99-git-send-email-ddutile@redhat.com>
    Patchwork-id: 167135
    O-Subject: [PATCH RHEL-7.4 098/348] xprtrdma: Support larger inline thresholds
    Bugzilla: 1417285
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1417285
    
    commit 44829d02d2d7a7064842ecf36239ea24df1cdf58
    Author: Chuck Lever <chuck.lever@oracle.com>
    Date:   Thu Sep 15 10:57:32 2016 -0400
    
        xprtrdma: Support larger inline thresholds
    
        The Version One default inline threshold is still 1KB. But allow
        testing with thresholds up to 64KB.
    
        This maximum is somewhat arbitrary. There's no fundamental
        architectural limit I'm aware of, but it's good to keep the size of
        Receive buffers reasonable. Now that Send can use a s/g list, a
        Send buffer is only as large as each RPC requires. Receive buffers
        are always the size of the inline threshold, however.
    
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
        Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/sunrpc/xprtrdma.h
+++ include/linux/sunrpc/xprtrdma.h
@@ -53,8 +53,8 @@
 #define RPCRDMA_MAX_SLOT_TABLE	(256U)
 
 #define RPCRDMA_MIN_INLINE  (1024)	/* min inline thresh */
-#define RPCRDMA_DEF_INLINE  (1024)	/* default inline thresh */
-#define RPCRDMA_MAX_INLINE  (3068)	/* max inline thresh */
+#define RPCRDMA_DEF_INLINE  (4096)	/* default inline thresh */
+#define RPCRDMA_MAX_INLINE  (65536)	/* max inline thresh */
 
 #define RPCRDMA_INLINE_PAD_THRESH  (512)/* payload threshold to pad (bytes) */
 
