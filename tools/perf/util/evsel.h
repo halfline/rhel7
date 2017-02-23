978ae8709238bdf3e82ebc5bdd471ad5e669fdb2
Jiri Olsa <jolsa@redhat.com>
2017-02-23 12:04:41 -0500

    [tools] perf tools: Implement branch_type event parameter
    Message-id: <1487851678-22505-102-git-send-email-jolsa@redhat.com>
    Patchwork-id: 167512
    O-Subject: [PATCH RHEL7.4 BZ1425612 101/298] perf tools: Implement branch_type event parameter
    Bugzilla: 1425612
    RH-Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1425612
    https://bugzilla.redhat.com/show_bug.cgi?id=1425612
    
    upstream
    ========
    commit ac12f6764c5097e791cd44f10b8943b40f44bfe7
    Author: Andi Kleen <ak@linux.intel.com>
    Date: Wed Oct 12 14:02:06 2016 -0700
    
    description
    ===========
    It can be useful to specify branch type state per event, for example if
    we want to collect both software trace points and last branch PMU events
    in a single collection. Currently this doesn't work because the software
    trace point errors out with -b.
    
    There was already a branch-type parameter to configure branch sample
    types per event in the parser, but it was stubbed out. This patch
    implements the necessary plumbing to actually enable it.
    
    Now:
    
      $ perf record -e sched:sched_switch,cpu/cpu-cycles,branch_type=any/ ...
    
    works.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/util/evsel.h
+++ tools/perf/util/evsel.h
@@ -46,6 +46,7 @@ enum {
 	PERF_EVSEL__CONFIG_TERM_INHERIT,
 	PERF_EVSEL__CONFIG_TERM_OVERWRITE,
 	PERF_EVSEL__CONFIG_TERM_DRV_CFG,
+	PERF_EVSEL__CONFIG_TERM_BRANCH,
 	PERF_EVSEL__CONFIG_TERM_MAX,
 };
 
@@ -61,6 +62,7 @@ struct perf_evsel_config_term {
 		u64	stack_user;
 		bool	inherit;
 		bool	overwrite;
+		char	*branch;
 	} val;
 };
 
