1e0d86bce95b2577b6c1fbd12063cc852ba86121
Paul Lai <plai@redhat.com>
2017-03-02 17:21:39 -0500

    [x86] kvm: mtrr: sort variable MTRRs
    Message-id: <1488475334-25772-10-git-send-email-plai@redhat.com>
    Patchwork-id: 169326
    O-Subject: [RHEL7.4 PATCH BZ1380113 v4 09/44] KVM: MTRR: sort variable MTRRs
    Bugzilla: 1380113
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    From: Xiao Guangrong <guangrong.xiao@linux.intel.com>
    
    Sort all valid variable MTRRs based on its base address, it will help us to
    check a range to see if it's fully contained in variable MTRRs
    
    Signed-off-by: Xiao Guangrong <guangrong.xiao@linux.intel.com>
    [Fix list insertion sort, simplify var_mtrr_range_is_valid to just
     test the V bit. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit 19efffa244071ccd0385b240d03adb38feaab04e)
    Signed-off-by: Paul Lai <plai@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -366,12 +366,15 @@ struct kvm_vcpu_hv {
 struct kvm_mtrr_range {
 	u64 base;
 	u64 mask;
+	struct list_head node;
 };
 
 struct kvm_mtrr {
 	struct kvm_mtrr_range var_ranges[KVM_NR_VAR_MTRR];
 	mtrr_type fixed_ranges[KVM_NR_FIXED_MTRR_REGION];
 	u64 deftype;
+
+	struct list_head head;
 };
 
 struct kvm_vcpu_arch {
