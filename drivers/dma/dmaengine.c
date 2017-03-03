6a251120125acd3f4bec6488c50dae383f626e0a
Suravee Suthikulpanit <ssuthiku@redhat.com>
2017-03-03 10:43:10 -0500

    [dma] dmaengine: reference counted unmap data
    Message-id: <1488537790-11682-1-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 169661
    O-Subject: [RHEL7.4 PATCH v2 BZ 1303727 004/147] dmaengine: reference counted unmap data
    Bugzilla: 1303727
    RH-Acked-by: Torez Smith <torez@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Myron Stowe <mstowe@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1303727
    
    commit 45c463ae924c62af4aa64ded1ca831f334a1db65
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Fri Oct 18 19:35:24 2013 +0200
    
        dmaengine: reference counted unmap data
    
        Hang a common 'unmap' object off of dma descriptors for the purpose of
        providing a unified unmapping interface.  The lifetime of a mapping may
        span multiple descriptors, so these unmap objects are reference counted
        by related descriptor.
    
        Cc: Vinod Koul <vinod.koul@intel.com>
        Cc: Tomasz Figa <t.figa@samsung.com>
        Cc: Dave Jiang <dave.jiang@intel.com>
        [bzolnier: fix IS_ENABLED() check]
        [bzolnier: fix release ordering in dmaengine_destroy_unmap_pool()]
        [bzolnier: fix check for success in dmaengine_init_unmap_pool()]
        [bzolnier: use mempool_free() instead of kmem_cache_free()]
        [bzolnier: add missing unmap->len initializations]
        [bzolnier: add __init tag to dmaengine_init_unmap_pool()]
        Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
        Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
        [djbw: move DMAENGINE=n support to this patch for async_tx]
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
        Conflict:
        RHEL7 commit 7fd4cdaa0 ("[net] net_dma: mark broken") removed
        dma_async_memcpy_buf_to_buf(). This prevents inclusion of the precursor
        commit 56ea27fd61f ("dmaengine: consolidate memcpy apis"), which introduces
        dma_async_memcpy_pg_to_pg() in place of dma_async_memcpy_buf_to_buf().
        Therefore, this results in the conflict for this patch in the function
        dma_async_memcpy_pg_to_pg() since it is not available.
    
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/dma/dmaengine.c
+++ drivers/dma/dmaengine.c
@@ -65,6 +65,7 @@
 #include <linux/acpi.h>
 #include <linux/acpi_dma.h>
 #include <linux/of_dma.h>
+#include <linux/mempool.h>
 
 static DEFINE_MUTEX(dma_list_mutex);
 static DEFINE_IDR(dma_idr);
@@ -881,6 +882,129 @@ void dma_async_device_unregister(struct dma_device *device)
 }
 EXPORT_SYMBOL(dma_async_device_unregister);
 
+struct dmaengine_unmap_pool {
+	struct kmem_cache *cache;
+	const char *name;
+	mempool_t *pool;
+	size_t size;
+};
+
+#define __UNMAP_POOL(x) { .size = x, .name = "dmaengine-unmap-" __stringify(x) }
+static struct dmaengine_unmap_pool unmap_pool[] = {
+	__UNMAP_POOL(2),
+	#if IS_ENABLED(CONFIG_ASYNC_TX_DMA)
+	__UNMAP_POOL(16),
+	__UNMAP_POOL(128),
+	__UNMAP_POOL(256),
+	#endif
+};
+
+static struct dmaengine_unmap_pool *__get_unmap_pool(int nr)
+{
+	int order = get_count_order(nr);
+
+	switch (order) {
+	case 0 ... 1:
+		return &unmap_pool[0];
+	case 2 ... 4:
+		return &unmap_pool[1];
+	case 5 ... 7:
+		return &unmap_pool[2];
+	case 8:
+		return &unmap_pool[3];
+	default:
+		BUG();
+		return NULL;
+	}
+}
+
+static void dmaengine_unmap(struct kref *kref)
+{
+	struct dmaengine_unmap_data *unmap = container_of(kref, typeof(*unmap), kref);
+	struct device *dev = unmap->dev;
+	int cnt, i;
+
+	cnt = unmap->to_cnt;
+	for (i = 0; i < cnt; i++)
+		dma_unmap_page(dev, unmap->addr[i], unmap->len,
+			       DMA_TO_DEVICE);
+	cnt += unmap->from_cnt;
+	for (; i < cnt; i++)
+		dma_unmap_page(dev, unmap->addr[i], unmap->len,
+			       DMA_FROM_DEVICE);
+	cnt += unmap->bidi_cnt;
+	for (; i < cnt; i++)
+		dma_unmap_page(dev, unmap->addr[i], unmap->len,
+			       DMA_BIDIRECTIONAL);
+	mempool_free(unmap, __get_unmap_pool(cnt)->pool);
+}
+
+void dmaengine_unmap_put(struct dmaengine_unmap_data *unmap)
+{
+	if (unmap)
+		kref_put(&unmap->kref, dmaengine_unmap);
+}
+EXPORT_SYMBOL_GPL(dmaengine_unmap_put);
+
+static void dmaengine_destroy_unmap_pool(void)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(unmap_pool); i++) {
+		struct dmaengine_unmap_pool *p = &unmap_pool[i];
+
+		if (p->pool)
+			mempool_destroy(p->pool);
+		p->pool = NULL;
+		if (p->cache)
+			kmem_cache_destroy(p->cache);
+		p->cache = NULL;
+	}
+}
+
+static int __init dmaengine_init_unmap_pool(void)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(unmap_pool); i++) {
+		struct dmaengine_unmap_pool *p = &unmap_pool[i];
+		size_t size;
+
+		size = sizeof(struct dmaengine_unmap_data) +
+		       sizeof(dma_addr_t) * p->size;
+
+		p->cache = kmem_cache_create(p->name, size, 0,
+					     SLAB_HWCACHE_ALIGN, NULL);
+		if (!p->cache)
+			break;
+		p->pool = mempool_create_slab_pool(1, p->cache);
+		if (!p->pool)
+			break;
+	}
+
+	if (i == ARRAY_SIZE(unmap_pool))
+		return 0;
+
+	dmaengine_destroy_unmap_pool();
+	return -ENOMEM;
+}
+
+static struct dmaengine_unmap_data *
+dmaengine_get_unmap_data(struct device *dev, int nr, gfp_t flags)
+{
+	struct dmaengine_unmap_data *unmap;
+
+	unmap = mempool_alloc(__get_unmap_pool(nr)->pool, flags);
+	if (!unmap)
+		return NULL;
+
+	memset(unmap, 0, sizeof(*unmap));
+	kref_init(&unmap->kref);
+	unmap->dev = dev;
+
+	return unmap;
+}
+
 void dma_async_tx_descriptor_init(struct dma_async_tx_descriptor *tx,
 	struct dma_chan *chan)
 {
@@ -954,6 +1078,10 @@ EXPORT_SYMBOL_GPL(dma_run_dependencies);
 
 static int __init dma_bus_init(void)
 {
+	int err = dmaengine_init_unmap_pool();
+
+	if (err)
+		return err;
 	return class_register(&dma_devclass);
 }
 arch_initcall(dma_bus_init);
