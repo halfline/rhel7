350aeb184537e7aaa8ac3d8d447818219153ffbd
Ivan Vecera <ivecera@redhat.com>
2017-03-02 22:16:29 -0500

    [net] sched: remove tcf_proto from ematch calls
    Message-id: <20170302221709.15773-34-ivecera@redhat.com>
    Patchwork-id: 169564
    O-Subject: [RHEL7.4 net PATCH 33/73] net: sched: remove tcf_proto from ematch calls
    Bugzilla: 1428588
    RH-Acked-by: Aaron Conole <aconole@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ#1428588
    
    Backport note: One hunk for cls_cgroup.c removed due to already applied
    commit 13990f815686 ("net: sched: cls_cgroup tear down exts and ematch
    from rcu callback"). The change for em_ipset.c is also a little bit
    different due to missing commit 1785e8f47308 ("netfiler: ipset: Add net
    namespace for ipset").
    
    Upstream commit(s):
    commit 82a470f1119eb7d2e4941b915bf9cd6fd8d54494
    Author: John Fastabend <john.fastabend@gmail.com>
    Date:   Sun Oct 5 21:27:53 2014 -0700
    
        net: sched: remove tcf_proto from ematch calls
    
        This removes the tcf_proto argument from the ematch code paths that
        only need it to reference the net namespace. This allows simplifying
        qdisc code paths especially when we need to tear down the ematch
        from an RCU callback. In this case we can not guarentee that the
        tcf_proto structure is still valid.
    
        Signed-off-by: John Fastabend <john.r.fastabend@intel.com>
        Acked-by: Cong Wang <cwang@twopensource.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sched/cls_cgroup.c
+++ net/sched/cls_cgroup.c
@@ -180,7 +180,7 @@ static void cls_cgroup_destroy_rcu(struct rcu_head *root)
 						    rcu);
 
 	tcf_exts_destroy(&head->exts);
-	tcf_em_tree_destroy(head->tp, &head->ematches);
+	tcf_em_tree_destroy(&head->ematches);
 	kfree(head);
 }
 
