4f627a49d0c9852bc5ee4730db0378a610bc36a7
Herbert Xu <herbert.xu@redhat.com>
2017-03-03 16:19:41 -0500

    [char] random: printk notifications for urandom pool initialization
    Message-id: <20170303161941.GA30539@gondor.apana.org.au>
    Patchwork-id: 169994
    O-Subject: [RHEL7.4 PATCH] random: printk notifications for urandom pool initialization
    Bugzilla: 1298643
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    RHEL7.4 Bugzilla #1298643
    https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12680045
    
    Upstream commit: 301f0595c0e788edacc3521c4caa90b4e56ffee1
    
    Print a notification to the console when the nonblocking pool is
    initialized.  Also printk a warning when a process tries reading from
    /dev/urandom before it is fully initialized.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -665,6 +665,9 @@ retry:
 	if (!r->initialized && nbits > 0) {
 		r->entropy_total += nbits;
 		if (r->entropy_total > 128) {
+			if (r == &nonblocking_pool)
+				pr_notice("random: %s pool is initialized\n",
+					  r->name);
 			r->initialized = 1;
 			if (r == &nonblocking_pool) {
 				prandom_reseed_late();
@@ -1304,6 +1307,11 @@ random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
+	if (unlikely(nonblocking_pool.initialized == 0))
+		printk_once(KERN_NOTICE "random: %s urandom read "
+			    "with %d bits of entropy available\n",
+			    current->comm, nonblocking_pool.entropy_total);
+
 	return extract_entropy_user(&nonblocking_pool, buf, nbytes);
 }
 
