7daf24d3747d73a6f511c2a23ec603e6f34a1af5
Harish Patil <hpatil@redhat.com>
2017-03-03 23:18:54 -0500

    [netdrv] qed*: Don't reset statistics on inner reload
    Message-id: <1488583261-20374-28-git-send-email-hpatil@redhat.com>
    Patchwork-id: 170207
    O-Subject: [PATCH RHEL7.4 v2 027/154] qed*: Don't reset statistics on inner reload
    Bugzilla: 1391272
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Yuval Mintz <Yuval.Mintz@qlogic.com>
    
    BZ 1391272
    
    upstream commit id a0d26d5a4fc8e13993279f788deeb08069e73b69
    
    Several user APIs can cause driver to perform an inner-reload.
    Currently, doing this would cause the HW/FW statistics of the
    adapter to reset, which isn't the expected behavior [statistics
    should only reset on explicit unloads].
    
    Signed-off-by: Yuval Mintz <Yuval.Mintz@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/qlogic/qed/qed_l2.c
+++ drivers/net/ethernet/qlogic/qed/qed_l2.c
@@ -1753,7 +1753,8 @@ static int qed_start_vport(struct qed_dev *cdev,
 			   start.vport_id, start.mtu);
 	}
 
-	qed_reset_vport_stats(cdev);
+	if (params->clear_stats)
+		qed_reset_vport_stats(cdev);
 
 	return 0;
 }
