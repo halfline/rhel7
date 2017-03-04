59a4426c17c0cb55b6b344448ceb2d10ca0ab01f
Ken Cox <jkc@redhat.com>
2017-03-03 20:06:29 -0500

    [netdrv] ixgbe: Force VLNCTRL.VFE to be set in all VMDq paths
    Message-id: <8e2bc8cf2a8ce7694baf028d82abb256580338d1.1488564026.git.jkc@redhat.com>
    Patchwork-id: 170045
    O-Subject: [RHEL7.4 BZ 1383524 18/72] ixgbe: Force VLNCTRL.VFE to be set in all VMDq paths
    Bugzilla: 1383524
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1383524
    
    commit f60439bc21e3337429838e477903214f5bd8277f
    Author: Alexander Duyck <alexander.h.duyck@intel.com>
    Date:   Thu Aug 11 14:51:56 2016 -0700
    
        ixgbe: Force VLNCTRL.VFE to be set in all VMDq paths
    
        When I was adding the code for enabling VLAN promiscuous mode with SR-IOV
        enabled I had inadvertently left the VLNCTRL.VFE bit unchanged as I has
        assumed there was code in another path that was setting it when we enabled
        SR-IOV.  This wasn't the case and as a result we were just disabling VLAN
        filtering for all the VFs apparently.
    
        Also the previous patches were always clearing CFIEN which was always set
        to 0 by the hardware anyway so I am dropping the redundant bit clearing.
    
        Fixes: 16369564915a ("ixgbe: Add support for VLAN promiscuous with SR-IOV")
        Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
        Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
        Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    
    Signed-off-by: Ken Cox <jkc@redhat.com>
    
    Cc: Ken Cox <jkc@redhat.com>
    Cc: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -4091,6 +4091,8 @@ static void ixgbe_vlan_promisc_enable(struct ixgbe_adapter *adapter)
 	struct ixgbe_hw *hw = &adapter->hw;
 	u32 vlnctrl, i;
 
+	vlnctrl = IXGBE_READ_REG(hw, IXGBE_VLNCTRL);
+
 	switch (hw->mac.type) {
 	case ixgbe_mac_82599EB:
 	case ixgbe_mac_X540:
@@ -4103,8 +4105,7 @@ static void ixgbe_vlan_promisc_enable(struct ixgbe_adapter *adapter)
 		/* fall through */
 	case ixgbe_mac_82598EB:
 		/* legacy case, we can just disable VLAN filtering */
-		vlnctrl = IXGBE_READ_REG(hw, IXGBE_VLNCTRL);
-		vlnctrl &= ~(IXGBE_VLNCTRL_VFE | IXGBE_VLNCTRL_CFIEN);
+		vlnctrl &= ~IXGBE_VLNCTRL_VFE;
 		IXGBE_WRITE_REG(hw, IXGBE_VLNCTRL, vlnctrl);
 		return;
 	}
@@ -4115,6 +4116,10 @@ static void ixgbe_vlan_promisc_enable(struct ixgbe_adapter *adapter)
 
 	/* Set flag so we don't redo unnecessary work */
 	adapter->flags2 |= IXGBE_FLAG2_VLAN_PROMISC;
+
+	/* For VMDq and SR-IOV we must leave VLAN filtering enabled */
+	vlnctrl |= IXGBE_VLNCTRL_VFE;
+	IXGBE_WRITE_REG(hw, IXGBE_VLNCTRL, vlnctrl);
 
 	/* Add PF to all active pools */
 	for (i = IXGBE_VLVF_ENTRIES; --i;) {
@@ -4182,6 +4187,11 @@ static void ixgbe_vlan_promisc_disable(struct ixgbe_adapter *adapter)
 	struct ixgbe_hw *hw = &adapter->hw;
 	u32 vlnctrl, i;
 
+	/* Set VLAN filtering to enabled */
+	vlnctrl = IXGBE_READ_REG(hw, IXGBE_VLNCTRL);
+	vlnctrl |= IXGBE_VLNCTRL_VFE;
+	IXGBE_WRITE_REG(hw, IXGBE_VLNCTRL, vlnctrl);
+
 	switch (hw->mac.type) {
 	case ixgbe_mac_82599EB:
 	case ixgbe_mac_X540:
@@ -4193,10 +4203,6 @@ static void ixgbe_vlan_promisc_disable(struct ixgbe_adapter *adapter)
 			break;
 		/* fall through */
 	case ixgbe_mac_82598EB:
-		vlnctrl = IXGBE_READ_REG(hw, IXGBE_VLNCTRL);
-		vlnctrl &= ~IXGBE_VLNCTRL_CFIEN;
-		vlnctrl |= IXGBE_VLNCTRL_VFE;
-		IXGBE_WRITE_REG(hw, IXGBE_VLNCTRL, vlnctrl);
 		return;
 	}
 
