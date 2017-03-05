737e467abc34182dcc1e11327e32789782a9ab3d
Xunlei Pang <xlpang@redhat.com>
2017-03-05 01:34:13 -0500

    [kernel] kexec: remove never used member destination in kimage
    Message-id: <1488677661-7494-7-git-send-email-xlpang@redhat.com>
    Patchwork-id: 170492
    O-Subject: [RHEL7.4 PATCH v2 06/14] kexec: remove never used member destination in kimage
    Bugzilla: 1415443
    RH-Acked-by: Baoquan He <bhe@redhat.com>
    RH-Acked-by: Pratyush Anand <panand@redhat.com>
    RH-Acked-by: Dave Young <dyoung@redhat.com>
    
    Resolves:
    https://bugzilla.redhat.com/show_bug.cgi?id=1415443
    
    Build Info:
    https://brewweb.devel.redhat.com/taskinfo?taskID=12686692
    
    This is back ported from upstream, and is 100% cleanly patched.
    
    commit 73d7e3eac01da3cef32ab25cbc6a36a6202c4ea6
    Author: Baoquan He <bhe@redhat.com>
    Date:   Tue Feb 17 13:45:44 2015 -0800
    
        kexec: remove never used member destination in kimage
    
        struct kimage has a member destination which is used to store the real
        destination address of each page when load segment from user space buffer
        to kernel.  But we never retrieve the value stored in kimage->destination,
        so this member variable in kimage and its assignment operation are
        redundent code.
    
        I guess for_each_kimage_entry just does the work that kimage->destination
        is expected to do.
    
        So in this patch just make a cleanup to remove it.
    
        Signed-off-by: Baoquan He <bhe@redhat.com>
        Cc: "Eric W. Biederman" <ebiederm@xmission.com>
        Cc: Vivek Goyal <vgoyal@redhat.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Xunlei Pang <xlpang@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/kexec.c
+++ kernel/kexec.c
@@ -868,8 +868,6 @@ static int kimage_set_destination(struct kimage *image,
 
 	destination &= PAGE_MASK;
 	result = kimage_add_entry(image, destination | IND_DESTINATION);
-	if (result == 0)
-		image->destination = destination;
 
 	return result;
 }
@@ -881,8 +879,6 @@ static int kimage_add_page(struct kimage *image, unsigned long page)
 
 	page &= PAGE_MASK;
 	result = kimage_add_entry(image, page | IND_SOURCE);
-	if (result == 0)
-		image->destination += PAGE_SIZE;
 
 	return result;
 }
