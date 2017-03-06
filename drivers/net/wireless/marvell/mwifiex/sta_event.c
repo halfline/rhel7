fdaba9c3354f293fe79830b76497c06cb74a5489
Stanislaw Gruszka <sgruszka@redhat.com>
2017-03-06 17:20:19 -0500

    [wireless] Update mwifiex driver to compile with cfg80211 from linux-4.11-rc1
    Message-id: <1488820820-16439-10-git-send-email-sgruszka@redhat.com>
    Patchwork-id: 170932
    O-Subject: [RHEL7 PATCH 09/10] Update mwifiex driver to compile with cfg80211 from linux-4.11-rc1
    Bugzilla: 1421597
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    BZ#1421597
    
    Description:
    Apply hunks from:
    
    commit 1d76250bd34af86c6498fc51e50cab3bfbbeceaa
    Author: Avraham Stern <avraham.stern@intel.com>
    Date:   Tue Jul 5 17:10:13 2016 +0300
    
        nl80211: support beacon report scanning
    
    commit 8b935ee2ea17db720d70f6420f77f594c0c93f75
    Author: Johannes Berg <johannes.berg@intel.com>
    Date:   Wed Oct 5 16:17:01 2016 +0200
    
        cfg80211: add ability to check DA/SA in A-MSDU decapsulation
    
    commit 543b921b475ad2e1897d5e7784437af238b33705
    Author: Arend Van Spriel <arend.vanspriel@broadcom.com>
    Date:   Thu Nov 17 12:48:53 2016 +0000
    
        cfg80211: get rid of name indirection trick for ieee80211_get_channel()
    
    commit bee427b86217b78a0a5fc85575cc155e4c32bbf9
    Author: Andrzej Zaborowski <andrew.zaborowski@intel.com>
    Date:   Wed Jan 25 12:43:41 2017 +0100
    
        cfg80211: Pass new RSSI level in CQM RSSI notification
    
    to make mwifiex driver compile with cfg80211 from linux-4.11-rc1
    
    Brew:
    https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=126932211
    
    Testing:
    Not tested due to lack of hardware.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/wireless/marvell/mwifiex/sta_event.c
+++ drivers/net/wireless/marvell/mwifiex/sta_event.c
@@ -723,7 +723,7 @@ int mwifiex_process_sta_event(struct mwifiex_private *priv)
 	case EVENT_RSSI_LOW:
 		cfg80211_cqm_rssi_notify(priv->netdev,
 					 NL80211_CQM_RSSI_THRESHOLD_EVENT_LOW,
-					 GFP_KERNEL);
+					 0, GFP_KERNEL);
 		mwifiex_send_cmd(priv, HostCmd_CMD_RSSI_INFO,
 				 HostCmd_ACT_GEN_GET, 0, NULL, false);
 		priv->subsc_evt_rssi_state = RSSI_LOW_RECVD;
@@ -738,7 +738,7 @@ int mwifiex_process_sta_event(struct mwifiex_private *priv)
 	case EVENT_RSSI_HIGH:
 		cfg80211_cqm_rssi_notify(priv->netdev,
 					 NL80211_CQM_RSSI_THRESHOLD_EVENT_HIGH,
-					 GFP_KERNEL);
+					 0, GFP_KERNEL);
 		mwifiex_send_cmd(priv, HostCmd_CMD_RSSI_INFO,
 				 HostCmd_ACT_GEN_GET, 0, NULL, false);
 		priv->subsc_evt_rssi_state = RSSI_HIGH_RECVD;
