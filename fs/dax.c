62416ec6c43ba7baa5a33277f771fd2bf051ce43
Eric Sandeen <sandeen@redhat.com>
2017-03-07 15:32:27 -0500

    [fs] dax: Remove redundant inode size checks
    Message-id: <1488900782-19770-19-git-send-email-sandeen@redhat.com>
    Patchwork-id: 172401
    O-Subject: [RHEL7.4 fs 18/53] dax: Remove redundant inode size checks
    Bugzilla: 1430424
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Jan Kara <jack@suse.cz>
    
    Bugzilla: 1399582
    Bugzilla: 1399576
    Upstream Status: committed
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12697919
    Tested: with xfstests
    
    commit 7795bec89ebf927ea3ad9ed5f396c227e5c73271
    Author: Jan Kara <jack@suse.cz>
    Date:   Wed May 11 11:58:53 2016 +0200
    
        dax: Remove redundant inode size checks
    
        Callers of dax fault handlers must make sure these calls cannot race
        with truncate. Thus it is enough to check inode size when entering the
        function and we don't have to recheck it again later in the handler.
        Note that inode size itself can be decreased while the fault handler
        runs but filesystem locking prevents against any radix tree or block
        mapping information changes resulting from the truncate and that is what
        we really care about.
    
        Reviewed-by: Ross Zwisler <ross.zwisler@linux.intel.com>
        Signed-off-by: Jan Kara <jack@suse.cz>
        Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/dax.c
+++ fs/dax.c
@@ -327,20 +327,11 @@ EXPORT_SYMBOL_GPL(dax_do_io);
 static int dax_load_hole(struct address_space *mapping, struct page *page,
 							struct vm_fault *vmf)
 {
-	unsigned long size;
-	struct inode *inode = mapping->host;
 	if (!page)
 		page = find_or_create_page(mapping, vmf->pgoff,
 						GFP_KERNEL | __GFP_ZERO);
 	if (!page)
 		return VM_FAULT_OOM;
-	/* Recheck i_size under page lock to avoid truncate race */
-	size = (i_size_read(inode) + PAGE_SIZE - 1) >> PAGE_SHIFT;
-	if (vmf->pgoff >= size) {
-		unlock_page(page);
-		page_cache_release(page);
-		return VM_FAULT_SIGBUS;
-	}
 
 	vmf->page = page;
 	return VM_FAULT_LOCKED;
@@ -570,23 +561,9 @@ static int dax_insert_mapping(struct inode *inode, struct buffer_head *bh,
 		.sector = to_sector(bh, inode),
 		.size = bh->b_size,
 	};
-	pgoff_t size;
 	int error;
 
 	mutex_lock(&mapping->i_mmap_mutex);
-
-	/*
-	 * Check truncate didn't happen while we were allocating a block.
-	 * If it did, this block may or may not be still allocated to the
-	 * file.  We can't tell the filesystem to free it because we can't
-	 * take i_mutex here.  In the worst case, the file still has blocks
-	 * allocated past the end of the file.
-	 */
-	size = (i_size_read(inode) + PAGE_SIZE - 1) >> PAGE_SHIFT;
-	if (unlikely(vmf->pgoff >= size)) {
-		error = -EIO;
-		goto out;
-	}
 
 	if (dax_map_atomic(bdev, &dax) < 0) {
 		error = PTR_ERR(dax.addr);
@@ -654,15 +631,6 @@ int __dax_fault(struct vm_area_struct *vma, struct vm_fault *vmf,
 			page_cache_release(page);
 			goto repeat;
 		}
-		size = (i_size_read(inode) + PAGE_SIZE - 1) >> PAGE_SHIFT;
-		if (unlikely(vmf->pgoff >= size)) {
-			/*
-			 * We have a struct page covering a hole in the file
-			 * from a read fault and we've raced with a truncate
-			 */
-			error = -EIO;
-			goto unlock_page;
-		}
 	}
 
 	error = get_block(inode, block, &bh, 0);
@@ -695,17 +663,8 @@ int __dax_fault(struct vm_area_struct *vma, struct vm_fault *vmf,
 		if (error)
 			goto unlock_page;
 		vmf->page = page;
-		if (!page) {
+		if (!page)
 			mutex_lock(&mapping->i_mmap_mutex);
-			/* Check we didn't race with truncate */
-			size = (i_size_read(inode) + PAGE_SIZE - 1) >>
-								PAGE_SHIFT;
-			if (vmf->pgoff >= size) {
-				mutex_unlock(&mapping->i_mmap_mutex);
-				error = -EIO;
-				goto out;
-			}
-		}
 		return VM_FAULT_LOCKED;
 	}
 
@@ -861,20 +820,6 @@ int __dax_pmd_fault(struct vm_area_struct *vma, unsigned long address,
 		unmap_mapping_range(mapping, pgoff << PAGE_SHIFT, PMD_SIZE, 0);
 		mutex_lock(&mapping->i_mmap_mutex);
 	}
-
-	/*
-	 * If a truncate happened while we were allocating blocks, we may
-	 * leave blocks allocated to the file that are beyond EOF.  We can't
-	 * take i_mutex here, so just leave them hanging; they'll be freed
-	 * when the file is deleted.
-	 */
-	size = (i_size_read(inode) + PAGE_SIZE - 1) >> PAGE_SHIFT;
-	if (pgoff >= size) {
-		result = VM_FAULT_SIGBUS;
-		goto out;
-	}
-	if ((pgoff | PG_PMD_COLOUR) >= size)
-		goto fallback;
 
 	if (!write && !buffer_mapped(&bh) && buffer_uptodate(&bh)) {
 		spinlock_t *ptl;
