fcc2f2b4c0a4cb361a4ed04d95c3d3266e01192a
Jakub Sitnicki <jkbs@redhat.com>
2017-03-07 15:20:00 -0500

    [net] ipv4: Set skb->protocol properly for local output
    Message-id: <1488900001-10118-1-git-send-email-jkbs@redhat.com>
    Patchwork-id: 172388
    O-Subject: [RHEL7.4 net 15/16] ipv4: Set skb->protocol properly for local output
    Bugzilla: 1336001
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Davide Caratti <dcaratti@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1336001
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12701624
    Conflicts: \
      Changed moved to __ip_local_out_sk because we don't have:
      b92dacd45698 ("ipv4: Merge __ip_local_out and __ip_local_out_sk")
      Also context differs due to lack of:
      a8e3e1a9f020 ("net: l3mdev: Add hook to output path")
    
    commit f4180439109aa720774baafdd798b3234ab1a0d2
    Author: Eli Cooper <elicooper@gmx.com>
    Date:   Thu Dec 1 10:05:10 2016 +0800
    
        ipv4: Set skb->protocol properly for local output
    
        When xfrm is applied to TSO/GSO packets, it follows this path:
    
            xfrm_output() -> xfrm_output_gso() -> skb_gso_segment()
    
        where skb_gso_segment() relies on skb->protocol to function properly.
    
        This patch sets skb->protocol to ETH_P_IP before dst_output() is called,
        fixing a bug where GSO packets sent through a sit tunnel are dropped
        when xfrm is involved.
    
        Cc: stable@vger.kernel.org
        Signed-off-by: Eli Cooper <elicooper@gmx.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jakub Sitnicki <jkbs@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -101,6 +101,8 @@ static int __ip_local_out_sk(struct sock *sk, struct sk_buff *skb)
 
 	iph->tot_len = htons(skb->len);
 	ip_send_check(iph);
+	skb->protocol = htons(ETH_P_IP);
+
 	return nf_hook(NFPROTO_IPV4, NF_INET_LOCAL_OUT, sk, skb, NULL,
 		       skb_dst(skb)->dev, dst_output_sk);
 }
