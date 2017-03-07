b2c45a8307eb29a0764cfbbd4e6f39191a74b1b3
Bill O'Donnell <billodo@redhat.com>
2017-03-07 00:01:17 -0500

    [fs] btrfs: fix callers of btrfs_block_rsv_migrate
    Message-id: <20170307000403.5752-320-billodo@redhat.com>
    Patchwork-id: 171401
    O-Subject: [RHEL7.4 PATCH BZ 1399590 319/485] Btrfs: fix callers of btrfs_block_rsv_migrate
    Bugzilla: 1399590
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    
    Bugzilla: 1399590
    Upstream Status: Committed upstream
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=12695624
    Tested: Locally with fstests
    
    Backport of the following upstream commit:
    
        commit 25d609f86d6808eb1f0e8a6cafc3edb4a2b5ae35
        Author: Josef Bacik <jbacik@fb.com>
        Date:   Fri Mar 25 13:25:48 2016 -0400
    
        Btrfs: fix callers of btrfs_block_rsv_migrate
    
        So btrfs_block_rsv_migrate just unconditionally calls block_rsv_migrate_bytes.
        Not only this but it unconditionally changes the size of the block_rsv.  This
        isn't a bug strictly speaking, but it makes truncate block rsv's look funny
        because every time we migrate bytes over its size grows, even though we only
        want it to be a specific size.  So collapse this into one function that takes an
        update_size argument and make truncate and evict not update the size for
        consistency sake.  Thanks,
    
        Signed-off-by: Josef Bacik <jbacik@fb.com>
        Signed-off-by: David Sterba <dsterba@suse.com>
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2501,7 +2501,7 @@ static int btrfs_punch_hole(struct inode *inode, loff_t offset, loff_t len)
 	}
 
 	ret = btrfs_block_rsv_migrate(&root->fs_info->trans_block_rsv, rsv,
-				      min_size);
+				      min_size, 0);
 	BUG_ON(ret);
 	trans->block_rsv = rsv;
 
@@ -2544,7 +2544,7 @@ static int btrfs_punch_hole(struct inode *inode, loff_t offset, loff_t len)
 		}
 
 		ret = btrfs_block_rsv_migrate(&root->fs_info->trans_block_rsv,
-					      rsv, min_size);
+					      rsv, min_size, 0);
 		BUG_ON(ret);	/* shouldn't happen */
 		trans->block_rsv = rsv;
 
