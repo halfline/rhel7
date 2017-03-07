d8bbf824724da3b0ccd8e57de566f7e9044e584c
Eric Sandeen <sandeen@redhat.com>
2017-03-07 15:32:33 -0500

    [fs] dax: Fix condition for filling of PMD holes
    Message-id: <1488900782-19770-25-git-send-email-sandeen@redhat.com>
    Patchwork-id: 172411
    O-Subject: [RHEL7.4 fs 24/53] dax: Fix condition for filling of PMD holes
    Bugzilla: 1430424
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Jan Kara <jack@suse.cz>
    
    Bugzilla: 1399582
    Bugzilla: 1399576
    Upstream Status: committed
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12697919
    Tested: with xfstests
    
    commit b9953536c95fd0013695542bfa4694c7468673aa
    Author: Jan Kara <jack@suse.cz>
    Date:   Thu May 12 18:29:14 2016 +0200
    
        dax: Fix condition for filling of PMD holes
    
        Currently dax_pmd_fault() decides to fill a PMD-sized hole only if
        returned buffer has BH_Uptodate set. However that doesn't get set for
        any mapping buffer so that branch is actually a dead code. The
        BH_Uptodate check doesn't make any sense so just remove it.
    
        Signed-off-by: Jan Kara <jack@suse.cz>
        Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/dax.c
+++ fs/dax.c
@@ -790,7 +790,7 @@ int __dax_pmd_fault(struct vm_area_struct *vma, unsigned long address,
 		mutex_lock(&mapping->i_mmap_mutex);
 	}
 
-	if (!write && !buffer_mapped(&bh) && buffer_uptodate(&bh)) {
+	if (!write && !buffer_mapped(&bh)) {
 		spinlock_t *ptl;
 		pmd_t entry;
 		struct page *zero_page = get_huge_zero_page();
