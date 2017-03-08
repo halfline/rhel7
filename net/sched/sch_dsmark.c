f049154bf6c402b8a19a13cf55496c4ae61fd8c3
Jonathan Toppins <jtoppins@redhat.com>
2017-03-07 23:57:04 -0500

    [net] sched: fix skb->protocol use in case of accelerated vlan path
    Message-id: <143c566f5f5b55ad3b1976e5bf24c45f2d054190.1488930758.git.jtoppins@redhat.com>
    Patchwork-id: 172701
    O-Subject: [PATCH RHEL7.4 net 09/91] net: sched: fix skb->protocol use in case of accelerated vlan path
    Bugzilla: 1428557
    RH-Acked-by: Aaron Conole <aconole@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    tc code implicitly considers skb->protocol even in case of accelerated
    vlan paths and expects vlan protocol type here. However, on rx path,
    if the vlan header was already stripped, skb->protocol contains value
    of next header. Similar situation is on tx path.
    
    So for skbs that use skb->vlan_tci for tagging, use skb->vlan_proto instead.
    
    Reported-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d8b9605d2697c48fb822c821c5751afbb4567003)
    
    Conflicts: \
    	include/net/pkt_sched.h
    	[jt: change vlan_tx_tag_present to skb_vlan_tag_present due to
    	 RHEL commit 7cdfedadd7e7 ("[net] vlan: rename vlan_tx_* helpers
    	 since "tx" is misleading there")]
    Bugzilla: 1428557
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12706028
    Tested: compile tested
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sched/sch_dsmark.c
+++ net/sched/sch_dsmark.c
@@ -196,7 +196,7 @@ static int dsmark_enqueue(struct sk_buff *skb, struct Qdisc *sch)
 	pr_debug("dsmark_enqueue(skb %p,sch %p,[qdisc %p])\n", skb, sch, p);
 
 	if (p->set_tc_index) {
-		switch (skb->protocol) {
+		switch (tc_skb_protocol(skb)) {
 		case htons(ETH_P_IP):
 			if (skb_cow_head(skb, sizeof(struct iphdr)))
 				goto drop;
@@ -284,7 +284,7 @@ static struct sk_buff *dsmark_dequeue(struct Qdisc *sch)
 	index = skb->tc_index & (p->indices - 1);
 	pr_debug("index %d->%d\n", skb->tc_index, index);
 
-	switch (skb->protocol) {
+	switch (tc_skb_protocol(skb)) {
 	case htons(ETH_P_IP):
 		ipv4_change_dsfield(ip_hdr(skb), p->mask[index],
 				    p->value[index]);
@@ -300,8 +300,8 @@ static struct sk_buff *dsmark_dequeue(struct Qdisc *sch)
 		 * and don't need yet another qdisc as a bypass.
 		 */
 		if (p->mask[index] != 0xff || p->value[index])
-			pr_warning("dsmark_dequeue: unsupported protocol %d\n",
-				   ntohs(skb->protocol));
+			pr_warn("%s: unsupported protocol %d\n",
+				__func__, ntohs(tc_skb_protocol(skb)));
 		break;
 	}
 
