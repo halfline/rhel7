3ed038477abbdb5c4012e193c18ced7b5003feba
Jonathan Toppins <jtoppins@redhat.com>
2017-03-07 23:57:42 -0500

    [net] flow_dissector: fix ipv6 dst, hop-by-hop and routing ext hdrs
    Message-id: <3816d467ca1254bc66331e4e750890756c6aa4fc.1488930759.git.jtoppins@redhat.com>
    Patchwork-id: 172741
    O-Subject: [PATCH RHEL7.4 net 47/91] flow_dissector: fix ipv6 dst, hop-by-hop and routing ext hdrs
    Bugzilla: 1428557
    RH-Acked-by: Aaron Conole <aconole@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    __skb_header_pointer() returns a pointer that must be checked.
    
    Fixes infinite loop reported by Alexei, and add __must_check to
    catch these errors earlier.
    
    Fixes: 6a74fcf426f5 ("flow_dissector: add support for dst, hop-by-hop and routing ext hdrs")
    Reported-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Tested-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1e98a0f08abddde87f0f93237f10629ecb4880ef)
    Bugzilla: 1428557
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12706028
    Tested: compile tested
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -2900,8 +2900,9 @@ __wsum __skb_checksum(const struct sk_buff *skb, int offset, int len,
 __wsum skb_checksum(const struct sk_buff *skb, int offset, int len,
 		    __wsum csum);
 
-static inline void *__skb_header_pointer(const struct sk_buff *skb, int offset,
-					 int len, void *data, int hlen, void *buffer)
+static inline void * __must_check
+__skb_header_pointer(const struct sk_buff *skb, int offset,
+		     int len, void *data, int hlen, void *buffer)
 {
 	if (hlen - offset >= len)
 		return data + offset;
@@ -2913,8 +2914,8 @@ static inline void *__skb_header_pointer(const struct sk_buff *skb, int offset,
 	return buffer;
 }
 
-static inline void *skb_header_pointer(const struct sk_buff *skb, int offset,
-				       int len, void *buffer)
+static inline void * __must_check
+skb_header_pointer(const struct sk_buff *skb, int offset, int len, void *buffer)
 {
 	return __skb_header_pointer(skb, offset, len, skb->data,
 				    skb_headlen(skb), buffer);
