1d7d737c81243e66955d6a589ab20cfc0b5d3526
David Arcari <darcari@redhat.com>
2017-03-07 20:44:04 -0500

    [net] ipv6: Add checks for 6LOWPAN ARP type
    Message-id: <1488919619-50524-2-git-send-email-darcari@redhat.com>
    Patchwork-id: 172527
    O-Subject: [RHEL7.4 PATCH BZ 1415327 001/176] ipv6: Add checks for 6LOWPAN ARP type
    Bugzilla: 1415327
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Eric Garver <egarver@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1415327
    
    commit e74bccb8a598c37840b547a7d606bd8c959cc2e4
    Author: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    Date:   Wed Dec 11 17:05:36 2013 +0200
    
        ipv6: Add checks for 6LOWPAN ARP type
    
        Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
        Acked-by: David S. Miller <davem@davemloft.net>
        Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    
    Cc: Hannes Frederic Sowa <hsowa@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>
    Cc: Jiri Benc <jbenc@redhat.com>
    Cc: Rashid Khan <rkhan@redhat.com>
    Cc: Ivan Vecera <ivecera@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Cc: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -1873,6 +1873,7 @@ static int ipv6_generate_eui64(u8 *eui, struct net_device *dev)
 		return addrconf_ifid_sit(eui, dev);
 	case ARPHRD_IPGRE:
 		return addrconf_ifid_gre(eui, dev);
+	case ARPHRD_6LOWPAN:
 	case ARPHRD_IEEE802154:
 		return addrconf_ifid_eui64(eui, dev);
 	case ARPHRD_IEEE1394:
@@ -2956,6 +2957,7 @@ static void addrconf_dev_config(struct net_device *dev)
 	    (dev->type != ARPHRD_IEEE802154) &&
 	    (dev->type != ARPHRD_IEEE1394) &&
 	    (dev->type != ARPHRD_TUNNEL6) &&
+	    (dev->type != ARPHRD_6LOWPAN) &&
 	    (dev->type != ARPHRD_NONE)) {
 		/* Alas, we support only Ethernet autoconfiguration. */
 		return;
