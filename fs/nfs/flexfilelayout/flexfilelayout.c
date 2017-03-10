951a48b35bcaa4b0dfa673c5422667184e165b34
Steve Dickson <SteveD@redhat.com>
2017-03-10 15:02:34 -0500

    [fs] pnfs: Files and flexfiles always need to commit before layoutcommit
    Message-id: <20170310150235.10315-167-steved@redhat.com>
    Patchwork-id: 173177
    O-Subject: [RHEL7.4 fs PATCH 166/167] pNFS: Files and flexfiles always need to commit before layoutcommit
    Bugzilla: 1349668
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Benjamin Coddington <bcodding@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    
    From: Trond Myklebust <trond.myklebust@primarydata.com>
    
    So ensure that we mark the layout for commit once the write is done,
    and then ensure that the commit to ds is finished before sending
    layoutcommit.
    
    Note that by doing this, we're able to optimise away the commit
    for the case of servers that don't need layoutcommit in order to
    return updated attributes.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    (cherry picked from commit 2e18d4d822ea9cc811ea26a880cf2ed47cbf8889)
    Bugzilla: 1349668
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -1438,6 +1438,7 @@ static void ff_layout_read_release(void *data)
 static int ff_layout_write_done_cb(struct rpc_task *task,
 				struct nfs_pgio_header *hdr)
 {
+	loff_t end_offs = 0;
 	int err;
 
 	trace_nfs4_pnfs_write(hdr, task->tk_status);
@@ -1463,8 +1464,10 @@ static int ff_layout_write_done_cb(struct rpc_task *task,
 
 	if (hdr->res.verf->committed == NFS_FILE_SYNC ||
 	    hdr->res.verf->committed == NFS_DATA_SYNC)
-		ff_layout_set_layoutcommit(hdr->inode, hdr->lseg,
-				hdr->mds_offset + (loff_t)hdr->res.count);
+		end_offs = hdr->mds_offset + (loff_t)hdr->res.count;
+
+	/* Note: if the write is unstable, don't set end_offs until commit */
+	ff_layout_set_layoutcommit(hdr->inode, hdr->lseg, end_offs);
 
 	/* zero out fattr since we don't care DS attr at all */
 	hdr->fattr.valid = 0;
