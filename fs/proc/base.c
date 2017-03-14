3e5ac15d099f505b3835ee7666404ecefc49a93d
Eric W. Biederman <ebiederm@redhat.com>
2017-03-14 07:06:52 -0400

    [fs] vfs: Remove d_drop calls from d_revalidate implementations
    Message-id: <1489475245-12948-71-git-send-email-ebiederm@redhat.com>
    Patchwork-id: 174158
    O-Subject: [RHEL7.4 BZ1247935 v2 071/104] vfs: Remove d_drop calls from d_revalidate implementations
    Bugzilla: 1247935
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Ian Kent <ikent@redhat.com>
    RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    
    From: "Eric W. Biederman" <ebiederm@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1247935
    Upstream Status: linux.git c143c2333c48f1430231b31a8c17e074b9b504eb
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=12742028
    Testing: https://beaker.engineering.redhat.com/jobs/1757657
    
    commit c143c2333c48f1430231b31a8c17e074b9b504eb
    Author: Eric W. Biederman <ebiederm@xmission.com>
    Date:   Thu Feb 13 10:19:10 2014 -0800
    
        vfs: Remove d_drop calls from d_revalidate implementations
    
        Now that d_invalidate always succeeds it is not longer necessary or
        desirable to hard code d_drop calls into filesystem specific
        d_revalidate implementations.
    
        Remove the unnecessary d_drop calls and rely on d_invalidate
        to drop the dentries.  Using d_invalidate ensures that paths
        to mount points will not be dropped.
    
        Reviewed-by: Miklos Szeredi <miklos@szeredi.hu>
        Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Signed-off-by: Eric W. Biederman <ebiederm@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1832,7 +1832,6 @@ int pid_revalidate(struct dentry *dentry, unsigned int flags)
 		put_task_struct(task);
 		return 1;
 	}
-	d_drop(dentry);
 	return 0;
 }
 
@@ -1976,9 +1975,6 @@ out:
 	put_task_struct(task);
 
 out_notask:
-	if (status <= 0)
-		d_drop(dentry);
-
 	return status;
 }
 
