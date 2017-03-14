38a447feccbc864448a08845bbaeeccd16bd45a1
Eric W. Biederman <ebiederm@redhat.com>
2017-03-14 07:06:10 -0400

    [fs] get rid of {lock, unlock}_rcu_walk()
    Message-id: <1489475245-12948-29-git-send-email-ebiederm@redhat.com>
    Patchwork-id: 174137
    O-Subject: [RHEL7.4 BZ1247935 v2 029/104] get rid of {lock,unlock}_rcu_walk()
    Bugzilla: 1247935
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Ian Kent <ikent@redhat.com>
    RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    
    From: Al Viro <viro@zeniv.linux.org.uk>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1247935
    Upstream Status: linux.git 8b61e74ffc6310e1d35a9b51c8463093851f8bcf
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=12742028
    Testing: https://beaker.engineering.redhat.com/jobs/1757657
    
    commit 8b61e74ffc6310e1d35a9b51c8463093851f8bcf
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Fri Nov 8 12:45:01 2013 -0500
    
        get rid of {lock,unlock}_rcu_walk()
    
        those have become aliases for rcu_read_{lock,unlock}()
    
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    RHEL Notes:
        This change is included simply so the bug fix that
        that follows this change does not need to be modified.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/namei.c
+++ fs/namei.c
@@ -520,16 +520,6 @@ static bool path_connected(const struct path *path)
  * to restart the path walk from the beginning in ref-walk mode.
  */
 
-static inline void lock_rcu_walk(void)
-{
-	rcu_read_lock();
-}
-
-static inline void unlock_rcu_walk(void)
-{
-	rcu_read_unlock();
-}
-
 /**
  * unlazy_walk - try to switch to ref-walk mode.
  * @nd: nameidata pathwalk data
@@ -561,7 +551,7 @@ static int unlazy_walk(struct nameidata *nd, struct dentry *dentry)
 
 	if (!lockref_get_not_dead(&parent->d_lockref)) {
 		nd->path.dentry = NULL;	
-		unlock_rcu_walk();
+		rcu_read_unlock();
 		return -ECHILD;
 	}
 
@@ -599,17 +589,17 @@ static int unlazy_walk(struct nameidata *nd, struct dentry *dentry)
 		spin_unlock(&fs->lock);
 	}
 
-	unlock_rcu_walk();
+	rcu_read_unlock();
 	return 0;
 
 unlock_and_drop_dentry:
 	spin_unlock(&fs->lock);
 drop_dentry:
-	unlock_rcu_walk();
+	rcu_read_unlock();
 	dput(dentry);
 	goto drop_root_mnt;
 out:
-	unlock_rcu_walk();
+	rcu_read_unlock();
 drop_root_mnt:
 	if (!(nd->flags & LOOKUP_ROOT))
 		nd->root.mnt = NULL;
@@ -642,21 +632,21 @@ static int complete_walk(struct nameidata *nd)
 			nd->root.mnt = NULL;
 
 		if (!legitimize_mnt(nd->path.mnt, nd->m_seq)) {
-			unlock_rcu_walk();
+			rcu_read_unlock();
 			return -ECHILD;
 		}
 		if (unlikely(!lockref_get_not_dead(&dentry->d_lockref))) {
-			unlock_rcu_walk();
+			rcu_read_unlock();
 			mntput(nd->path.mnt);
 			return -ECHILD;
 		}
 		if (read_seqcount_retry(&dentry->d_seq, nd->seq)) {
-			unlock_rcu_walk();
+			rcu_read_unlock();
 			dput(dentry);
 			mntput(nd->path.mnt);
 			return -ECHILD;
 		}
-		unlock_rcu_walk();
+		rcu_read_unlock();
 	}
 
 	if (likely(!(nd->flags & LOOKUP_JUMPED)))
@@ -1224,7 +1214,7 @@ failed:
 	nd->flags &= ~LOOKUP_RCU;
 	if (!(nd->flags & LOOKUP_ROOT))
 		nd->root.mnt = NULL;
-	unlock_rcu_walk();
+	rcu_read_unlock();
 	return -ECHILD;
 }
 
@@ -1556,7 +1546,7 @@ static void terminate_walk(struct nameidata *nd)
 		nd->flags &= ~LOOKUP_RCU;
 		if (!(nd->flags & LOOKUP_ROOT))
 			nd->root.mnt = NULL;
-		unlock_rcu_walk();
+		rcu_read_unlock();
 	}
 }
 
@@ -1889,7 +1879,7 @@ static int path_init(int dfd, const char *name, unsigned int flags,
 		nd->path = nd->root;
 		nd->inode = inode;
 		if (flags & LOOKUP_RCU) {
-			lock_rcu_walk();
+			rcu_read_lock();
 			nd->seq = __read_seqcount_begin(&nd->path.dentry->d_seq);
 			nd->m_seq = read_seqbegin(&mount_lock);
 		} else {
@@ -1903,7 +1893,7 @@ static int path_init(int dfd, const char *name, unsigned int flags,
 	nd->m_seq = read_seqbegin(&mount_lock);
 	if (*name=='/') {
 		if (flags & LOOKUP_RCU) {
-			lock_rcu_walk();
+			rcu_read_lock();
 			set_root_rcu(nd);
 		} else {
 			set_root(nd);
@@ -1915,7 +1905,7 @@ static int path_init(int dfd, const char *name, unsigned int flags,
 			struct fs_struct *fs = current->fs;
 			unsigned seq;
 
-			lock_rcu_walk();
+			rcu_read_lock();
 
 			do {
 				seq = read_seqcount_begin(&fs->seq);
@@ -1947,7 +1937,7 @@ static int path_init(int dfd, const char *name, unsigned int flags,
 			if (f.flags & FDPUT_FPUT)
 				*fp = f.file;
 			nd->seq = __read_seqcount_begin(&nd->path.dentry->d_seq);
-			lock_rcu_walk();
+			rcu_read_lock();
 		} else {
 			path_get(&nd->path);
 			fdput(f);
