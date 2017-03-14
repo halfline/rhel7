228c7c464987c82cebedcf9259879a6bcf34609b
Florian Westphal <fwestpha@redhat.com>
2017-03-14 13:11:19 -0400

    [net] ipv6: avoid write to a possibly cloned skb
    Message-id: <1489497080-4314-3-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 174204
    O-Subject: [RHEL7.4 net 2/3] ipv6: avoid write to a possibly cloned skb
    Bugzilla: 1430571
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Hannes Frederic Sowa <hannes@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1430571
    Upstream Status: net.git
    
    commit 79e49503efe53a8c51d8b695bedc8a346c5e4a87
    Author: Florian Westphal <fw@strlen.de>
    Date:   Mon Mar 13 16:24:28 2017 +0100
    
        ipv6: avoid write to a possibly cloned skb
    
        ip6_fragment, in case skb has a fraglist, checks if the
        skb is cloned.  If it is, it will move to the 'slow path' and allocates
        new skbs for each fragment.
    
        However, right before entering the slowpath loop, it updates the
        nexthdr value of the last ipv6 extension header to NEXTHDR_FRAGMENT,
        to account for the fragment header that will be inserted in the new
        ipv6-fragment skbs.
    
        In case original skb is cloned this munges nexthdr value of another
        skb.  Avoid this by doing the nexthdr update for each of the new fragment
        skbs separately.
    
        This was observed with tcpdump on a bridge device where netfilter ipv6
        reassembly is active:  tcpdump shows malformed fragment headers as
        the l4 header (icmpv6, tcp, etc). is decoded as a fragment header.
    
        Cc: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Reported-by: Andreas Karis <akaris@redhat.com>
        Signed-off-by: Florian Westphal <fw@strlen.de>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -707,14 +707,15 @@ slow_path:
 	 *	Fragment the datagram.
 	 */
 
-	*prevhdr = NEXTHDR_FRAGMENT;
 	hroom = LL_RESERVED_SPACE(rt->dst.dev);
 	troom = rt->dst.dev->needed_tailroom;
 
 	/*
 	 *	Keep copying data until we run out.
 	 */
-	while(left > 0)	{
+	while (left > 0)	{
+		u8 *fragnexthdr_offset;
+
 		len = left;
 		/* IF: it doesn't fit, use 'mtu' - the data space left */
 		if (len > mtu)
@@ -760,6 +761,10 @@ slow_path:
 		 *	Copy the packet header into the new buffer.
 		 */
 		skb_copy_from_linear_data(skb, skb_network_header(frag), hlen);
+
+		fragnexthdr_offset = skb_network_header(frag);
+		fragnexthdr_offset += prevhdr - skb_network_header(skb);
+		*fragnexthdr_offset = NEXTHDR_FRAGMENT;
 
 		/*
 		 *	Build fragment header.
