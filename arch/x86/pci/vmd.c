torvalds/linux

commit 97e92306357583c1741f0a111c7befe8673b91ee

x86/PCI: VMD: Initialize list item in IRQ disable
Multiple calls to disable an IRQ would have caused the driver to
dereference a poisoned list item.  This re-initializes the list to allow
multiple requests to disable the IRQ.

Signed-off-by: Keith Busch <keith.busch@intel.com>
Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
Acked-by Jon Derrick: <jonathan.derrick@intel.com>

arch/x86/pci/vmd.c
@@ -135,6 +135,7 @@ static void vmd_irq_disable(struct irq_data *data)
 
 	raw_spin_lock(&list_lock);
 	list_del_rcu(&vmdirq->node);
+	INIT_LIST_HEAD_RCU(&vmdirq->node);
 	raw_spin_unlock(&list_lock);
 }
 

halfline/rhel7

commit a02a8b73a9c99ee5cfc5e0d0e752c8922ce0ea93

[x86] pci: vmd: Fix infinite loop executing irq's
Message-id: <20170307035832.20518.84372.stgit@amt.stowe>
Patchwork-id: 172032
O-Subject: [RHEL7.4 PATCH 7/9] x86/PCI: VMD: Fix infinite loop executing irq's
Bugzilla: 1388664
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1388664
Upstream Status: 21c80c9fefc3db10b530a96eb0478c29eb28bf77

commit 21c80c9fefc3db10b530a96eb0478c29eb28bf77
Author: Keith Busch <keith.busch@intel.com>
Date:   Tue Aug 23 16:36:42 2016 -0500

    x86/PCI: VMD: Fix infinite loop executing irq's

    We can't initialize the list head on deletion as this causes the node to
    point to itself, which causes an infinite loop if vmd_irq() happens to be
    servicing that node.

    The list initialization was trying to fix a bug from multiple calls to
    disable the same IRQ.  Fix this instead by having the VMD driver track if
    the interrupt is enabled.

    [bhelgaas: changelog, add "Fixes"]
    Fixes: 97e923063575 ("x86/PCI: VMD: Initialize list item in IRQ disable")
    Reported-by: Grzegorz Koczot <grzegorz.koczot@intel.com>
    Tested-by: Miroslaw Drost <miroslaw.drost@intel.com>
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by Jon Derrick: <jonathan.derrick@intel.com>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

arch/x86/pci/vmd.c
@@ -39,6 +39,7 @@ static DEFINE_RAW_SPINLOCK(list_lock);
  * @node:	list item for parent traversal.
  * @rcu:	RCU callback item for freeing.
  * @irq:	back pointer to parent.
+ * @enabled:	true if driver enabled IRQ
  * @virq:	the virtual IRQ value provided to the requesting driver.
  *
  * Every MSI/MSI-X IRQ requested for a device in a VMD domain will be mapped to
@@ -48,6 +49,7 @@ struct vmd_irq {
 	struct list_head	node;
 	struct rcu_head		rcu;
 	struct vmd_irq_list	*irq;
+	bool			enabled;
 	unsigned int		virq;
 };
 
@@ -103,7 +105,9 @@ static void vmd_irq_enable(struct irq_data *data)
 	unsigned long flags;
 
 	raw_spin_lock_irqsave(&list_lock, flags);
+	WARN_ON(vmdirq->enabled);
 	list_add_tail_rcu(&vmdirq->node, &vmdirq->irq->irq_list);
+	vmdirq->enabled = true;
 	raw_spin_unlock_irqrestore(&list_lock, flags);
 
 	data->chip->irq_unmask(data);
@@ -118,8 +122,10 @@ static void vmd_irq_disable(struct irq_data *data)
 	data->chip->irq_mask(data);
 
 	raw_spin_lock_irqsave(&list_lock, flags);
-	list_del_rcu(&vmdirq->node);
-	INIT_LIST_HEAD_RCU(&vmdirq->node);
+	if (vmdirq->enabled) {
+		list_del_rcu(&vmdirq->node);
+		vmdirq->enabled = false;
+	}
 	raw_spin_unlock_irqrestore(&list_lock, flags);
 }
 