8cc89a18818ccfe12757b12795d0d614233c920c
Sachin Prabhu <sprabhu@redhat.com>
2017-03-15 15:40:33 -0400

    [fs] cifs: stuff the fl_owner into "pid" field in the lock request
    Message-id: <20170315154106.18371-21-sprabhu@redhat.com>
    Patchwork-id: 174289
    O-Subject: [RHEL7.4 PATCH fs 20/53] [bz1416808]cifs: stuff the fl_owner into "pid" field in the lock request
    Bugzilla: 1416808
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Jeff Layton <jlayton@poochiereds.net>
    
    Fixes: bz 1416808
    
    Right now, we send the tgid cross the wire. What we really want to send
    though is a hashed fl_owner_t since samba treats this field as a generic
    lockowner.
    
    It turns out that because we enforce and release locks locally before
    they are ever sent to the server, this patch makes no difference in
    behavior. Still, setting OFD locks on the server using the process
    pid seems wrong, so I think this patch still makes sense.
    
    Signed-off-by: Jeff Layton <jlayton@poochiereds.net>
    Signed-off-by: Steve French <smfrench@gmail.com>
    Acked-by: Pavel Shilovsky <pshilovsky@samba.org>
    Acked-by: Sachin Prabhu <sprabhu@redhat.com>
    (cherry picked from commit 3d22462ae915743f3be5bf1ab3d4a6b72c2bb6c9)
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -1111,6 +1111,12 @@ cifs_push_mandatory_locks(struct cifsFileInfo *cfile)
 	return rc;
 }
 
+static __u32
+hash_lockowner(fl_owner_t owner)
+{
+	return cifs_lock_secret ^ hash32_ptr((const void *)owner);
+}
+
 /* copied from fs/locks.c with a name change */
 #define cifs_for_each_lock(inode, lockp) \
 	for (lockp = &inode->i_flock; *lockp != NULL; \
@@ -1182,7 +1188,7 @@ cifs_push_posix_locks(struct cifsFileInfo *cfile)
 		else
 			type = CIFS_WRLCK;
 		lck = list_entry(el, struct lock_to_push, llist);
-		lck->pid = flock->fl_pid;
+		lck->pid = hash_lockowner(flock->fl_owner);
 		lck->netfid = cfile->fid.netfid;
 		lck->length = length;
 		lck->type = type;
@@ -1310,7 +1316,8 @@ cifs_getlk(struct file *file, struct file_lock *flock, __u32 type,
 			posix_lock_type = CIFS_RDLCK;
 		else
 			posix_lock_type = CIFS_WRLCK;
-		rc = CIFSSMBPosixLock(xid, tcon, netfid, current->tgid,
+		rc = CIFSSMBPosixLock(xid, tcon, netfid,
+				      hash_lockowner(flock->fl_owner),
 				      flock->fl_start, length, flock,
 				      posix_lock_type, wait_flag);
 		return rc;
@@ -1510,7 +1517,8 @@ cifs_setlk(struct file *file, struct file_lock *flock, __u32 type,
 			posix_lock_type = CIFS_UNLCK;
 
 		rc = CIFSSMBPosixLock(xid, tcon, cfile->fid.netfid,
-				      current->tgid, flock->fl_start, length,
+				      hash_lockowner(flock->fl_owner),
+				      flock->fl_start, length,
 				      NULL, posix_lock_type, wait_flag);
 		goto out;
 	}
