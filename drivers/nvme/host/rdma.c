dfc11b6b493f260228a05a69e09e348cd259a8e5
David Milburn <dmilburn@redhat.com>
2017-03-17 22:46:46 -0400

    [nvme] nvme-rdma: only clear queue flags after successful connect
    Message-id: <1489790877-16541-88-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 174692
    O-Subject: [RHEL7.4 PATCH 087/158] nvme-rdma: only clear queue flags after successful connect
    Bugzilla: 1384526 1389755 1366753 1374291 1383834
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Sagi Grimberg <sagi@grimberg.me>
    
    Otherwise, nvme_rdma_stop_and_clear_queue() will incorrectly
    try to stop/free rdma qps/cm_ids that are already freed.
    
    Fixes: e89ca58f9c90 ("nvme-rdma: add DELETING queue flag")
    Reported-by: Steve Wise <swise@opengridcomputing.com>
    Tested-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit 3b4ac78610690bd83fb33762ef97e8b8a89285ae)
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    
    BZ 1383834
    BZ 1374291
    BZ 1366753
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/rdma.c
+++ drivers/nvme/host/rdma.c
@@ -548,7 +548,6 @@ static int nvme_rdma_init_queue(struct nvme_rdma_ctrl *ctrl,
 
 	queue = &ctrl->queues[idx];
 	queue->ctrl = ctrl;
-	queue->flags = 0;
 	init_completion(&queue->cm_done);
 
 	if (idx > 0)
@@ -582,6 +581,7 @@ static int nvme_rdma_init_queue(struct nvme_rdma_ctrl *ctrl,
 		goto out_destroy_cm_id;
 	}
 
+	clear_bit(NVME_RDMA_Q_DELETING, &queue->flags);
 	set_bit(NVME_RDMA_Q_CONNECTED, &queue->flags);
 
 	return 0;
