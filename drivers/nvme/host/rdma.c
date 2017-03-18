6afe8a47a6a2ff51ee5762068618a3166a3fd2c2
David Milburn <dmilburn@redhat.com>
2017-03-17 22:46:42 -0400

    [nvme] nvme-rdma: destroy nvme queue rdma resources on connect failure
    Message-id: <1489790877-16541-84-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 174702
    O-Subject: [RHEL7.4 PATCH 083/158] nvme-rdma: destroy nvme queue rdma resources on connect failure
    Bugzilla: 1384526 1389755 1366753 1374291 1383834
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Steve Wise <swise@opengridcomputing.com>
    
    After address resolution, the nvme_rdma_queue rdma resources are
    allocated.  If rdma route resolution or the connect fails, or the
    controller reconnect times out and gives up, then the rdma resources
    need to be freed.  Otherwise, rdma resources are leaked.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Sagi Grimberg <sagi@grimbrg.me>
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    (cherry picked from commit f361e5a01ed35c0f9a00816d76a910d8a5cb4547)
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    
    BZ 1383834
    BZ 1374291
    BZ 1366753
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/rdma.c
+++ drivers/nvme/host/rdma.c
@@ -83,6 +83,7 @@ struct nvme_rdma_request {
 
 enum nvme_rdma_queue_flags {
 	NVME_RDMA_Q_CONNECTED = (1 << 0),
+	NVME_RDMA_IB_QUEUE_ALLOCATED = (1 << 1),
 };
 
 struct nvme_rdma_queue {
@@ -467,9 +468,14 @@ out_err:
 
 static void nvme_rdma_destroy_queue_ib(struct nvme_rdma_queue *queue)
 {
-	struct nvme_rdma_device *dev = queue->device;
-	struct ib_device *ibdev = dev->dev;
+	struct nvme_rdma_device *dev;
+	struct ib_device *ibdev;
 
+	if (!test_and_clear_bit(NVME_RDMA_IB_QUEUE_ALLOCATED, &queue->flags))
+		return;
+
+	dev = queue->device;
+	ibdev = dev->dev;
 	rdma_destroy_qp(queue->cm_id);
 	ib_free_cq(queue->ib_cq);
 
@@ -520,6 +526,7 @@ static int nvme_rdma_create_queue_ib(struct nvme_rdma_queue *queue,
 		ret = -ENOMEM;
 		goto out_destroy_qp;
 	}
+	set_bit(NVME_RDMA_IB_QUEUE_ALLOCATED, &queue->flags);
 
 	return 0;
 
@@ -577,6 +584,7 @@ static int nvme_rdma_init_queue(struct nvme_rdma_ctrl *ctrl,
 	return 0;
 
 out_destroy_cm_id:
+	nvme_rdma_destroy_queue_ib(queue);
 	rdma_destroy_id(queue->cm_id);
 	return ret;
 }
@@ -639,7 +647,7 @@ static int nvme_rdma_init_io_queues(struct nvme_rdma_ctrl *ctrl)
 	return 0;
 
 out_free_queues:
-	for (; i >= 1; i--)
+	for (i--; i >= 1; i--)
 		nvme_rdma_stop_and_free_queue(&ctrl->queues[i]);
 
 	return ret;
