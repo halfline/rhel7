9208d5516650ee96c8f0cdda9d62f0ca29250b7c
David Milburn <dmilburn@redhat.com>
2017-03-17 22:47:12 -0400

    [nvme] pci: Don't free queues on error
    Message-id: <1489790877-16541-114-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 174740
    O-Subject: [RHEL7.4 PATCH 113/158] nvme/pci: Don't free queues on error
    Bugzilla: 1384526 1389755 1366753 1374291 1383834
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Keith Busch <keith.busch@intel.com>
    
    The nvme_remove function tears down all allocated resources in the correct
    order, so no need to free queues on error during initialization. This
    fixes possible use-after-free errors when queues are still associated
    with a blk-mq hctx.
    
    Reported-by: Scott Bauer <scott.bauer@intel.com>
    Tested-by: Scott Bauer <scott.bauer@intel.com>
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Sagi Grimberg <sagi@grimbeg.me>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Cc: stable@vger.kernel.org
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit d48756228ee9161ac8836b346589a43fabdc9f3c)
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    
    BZ 1383834
    BZ 1374291
    BZ 1366753
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -1138,19 +1138,15 @@ static int nvme_configure_admin_queue(struct nvme_dev *dev)
 
 	result = nvme_enable_ctrl(&dev->ctrl, cap);
 	if (result)
-		goto free_nvmeq;
+		return result;
 
 	nvmeq->cq_vector = 0;
 	result = queue_request_irq(dev, nvmeq, nvmeq->irqname);
 	if (result) {
 		nvmeq->cq_vector = -1;
-		goto free_nvmeq;
+		return result;
 	}
 
-	return result;
-
- free_nvmeq:
-	nvme_free_queues(dev, 0);
 	return result;
 }
 
@@ -1213,10 +1209,8 @@ static int nvme_create_io_queues(struct nvme_dev *dev)
 	max = min(dev->max_qid, dev->queue_count - 1);
 	for (i = dev->online_queues; i <= max; i++) {
 		ret = nvme_create_queue(dev->queues[i], i);
-		if (ret) {
-			nvme_free_queues(dev, i);
+		if (ret)
 			break;
-		}
 	}
 
 	/*
@@ -1369,13 +1363,9 @@ static int nvme_setup_io_queues(struct nvme_dev *dev)
 	result = queue_request_irq(dev, adminq, adminq->irqname);
 	if (result) {
 		adminq->cq_vector = -1;
-		goto free_queues;
+		return result;
 	}
 	return nvme_create_io_queues(dev);
-
- free_queues:
-	nvme_free_queues(dev, 1);
-	return result;
 }
 
 static void nvme_pci_post_scan(struct nvme_ctrl *ctrl)
