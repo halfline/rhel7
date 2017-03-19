9166b0600bbc489a332a1938a1e9143660a21dae
Wei Xu <wexu@redhat.com>
2017-03-19 17:20:52 -0400

    [netdrv] macvlan: fix leak in macvlan_handle_frame
    Message-id: <1489944056-42830-2-git-send-email-wexu@redhat.com>
    Patchwork-id: 174809
    O-Subject: [RHEL7.4 PATCH v2 1/5] macvlan: fix leak in macvlan_handle_frame
    Bugzilla: 1414006
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    RH-Acked-by: Jason Wang <jasowang@redhat.com>
    
    From: Sabrina Dubroca <sd@queasysnail.net>
    
    Reset pskb in macvlan_handle_frame in case skb_share_check returned a
    clone.
    
    Fixes: 8a4eb5734e8d ("net: introduce rx_handler results and logic around that")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit e639b8d8a7a728f0b05ef2df6cb6b45dc3d4e556)
    Signed-off-by: Wei Xu <wexu@redhat.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/macvlan.c
+++ drivers/net/macvlan.c
@@ -283,6 +283,7 @@ static rx_handler_result_t macvlan_handle_frame(struct sk_buff **pskb)
 		skb = ip_check_defrag(skb, IP_DEFRAG_MACVLAN);
 		if (!skb)
 			return RX_HANDLER_CONSUMED;
+		*pskb = skb;
 		eth = eth_hdr(skb);
 		src = macvlan_hash_lookup(port, eth->h_source);
 		if (src && src->mode != MACVLAN_MODE_VEPA &&
@@ -322,6 +323,7 @@ static rx_handler_result_t macvlan_handle_frame(struct sk_buff **pskb)
 		goto out;
 	}
 
+	*pskb = skb;
 	skb->dev = dev;
 	skb->pkt_type = PACKET_HOST;
 
