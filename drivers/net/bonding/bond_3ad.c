c8128232b2007d8c68a2952234a2ded86f31cb8a
Jarod Wilson <jarod@redhat.com>
2017-04-11 17:16:51 -0400

    [netdrv] bonding: remove "port-moved" state that was never implemented
    Message-id: <20170411171700.33431-8-jarod@redhat.com>
    Patchwork-id: 176635
    O-Subject: [RHEL7 PATCH 07/16] bonding: remove "port-moved" state that was never implemented
    Bugzilla: 1412921
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    commit ec891c8b8da2c3862a5f76ba72f6d140c418d812
    Author: Mahesh Bandewar <maheshb@google.com>
    Date:   Wed Mar 8 10:55:59 2017 -0800
    
        bonding: remove "port-moved" state that was never implemented
    
        LACP state-machine defines "port-moved" state when the same ActorSystemID
        and Port are seen in a LACPDU received on different port. The state is
        never set since it's not implemented. However the state-machine attempts
        to clear that state occasionally. LACP state machine is already complicated
        and since this state is not implemented, removing it's checks makes the
        state-machine little simpler.
    
        Signed-off-by: Mahesh Bandewar <maheshb@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/bonding/bond_3ad.c
+++ drivers/net/bonding/bond_3ad.c
@@ -1053,8 +1053,7 @@ static void ad_rx_machine(struct lacpdu *lacpdu, struct port *port)
 		port->sm_rx_state = AD_RX_INITIALIZE;
 		port->sm_vars |= AD_PORT_CHURNED;
 	/* check if port is not enabled */
-	} else if (!(port->sm_vars & AD_PORT_BEGIN)
-		 && !port->is_enabled && !(port->sm_vars & AD_PORT_MOVED))
+	} else if (!(port->sm_vars & AD_PORT_BEGIN) && !port->is_enabled)
 		port->sm_rx_state = AD_RX_PORT_DISABLED;
 	/* check if new lacpdu arrived */
 	else if (lacpdu && ((port->sm_rx_state == AD_RX_EXPIRED) ||
@@ -1082,11 +1081,8 @@ static void ad_rx_machine(struct lacpdu *lacpdu, struct port *port)
 			/* if no lacpdu arrived and no timer is on */
 			switch (port->sm_rx_state) {
 			case AD_RX_PORT_DISABLED:
-				if (port->sm_vars & AD_PORT_MOVED)
-					port->sm_rx_state = AD_RX_INITIALIZE;
-				else if (port->is_enabled
-					 && (port->sm_vars
-					     & AD_PORT_LACP_ENABLED))
+				if (port->is_enabled &&
+				    (port->sm_vars & AD_PORT_LACP_ENABLED))
 					port->sm_rx_state = AD_RX_EXPIRED;
 				else if (port->is_enabled
 					 && ((port->sm_vars
@@ -1116,7 +1112,6 @@ static void ad_rx_machine(struct lacpdu *lacpdu, struct port *port)
 			port->sm_vars &= ~AD_PORT_SELECTED;
 			__record_default(port);
 			port->actor_oper_port_state &= ~AD_STATE_EXPIRED;
-			port->sm_vars &= ~AD_PORT_MOVED;
 			port->sm_rx_state = AD_RX_PORT_DISABLED;
 
 			/* Fall Through */
