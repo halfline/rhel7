a8bbfc92feebc1e87332e53b03a300fcf45a88e5
Ivan Vecera <ivecera@redhat.com>
2017-04-11 14:29:21 -0400

    [netdrv] mlxsw: spectrum: Remove redundant function argument
    Message-id: <20170411143300.8140-14-ivecera@redhat.com>
    Patchwork-id: 176404
    O-Subject: [RHEL7.4 PATCH v2 013/232] mlxsw: spectrum: Remove redundant function argument
    Bugzilla: 1414400
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    BZ#1414400
    
    Upstream commit(s):
    commit 47a0a9e6c38325028d8fa4eb764687fbeaee8ef2
    Author: Ido Schimmel <idosch@mellanox.com>
    Date:   Mon Jun 20 23:04:08 2016 +0200
    
        mlxsw: spectrum: Remove redundant function argument
    
        In all call sites 'only_uc' is set to false, so strip it.
    
        Signed-off-by: Ido Schimmel <idosch@mellanox.com>
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.c
@@ -795,7 +795,7 @@ int mlxsw_sp_port_add_vid(struct net_device *dev, __be16 __always_unused proto,
 
 	if (!vfid->nr_vports) {
 		err = mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid,
-					       true, false);
+					       true);
 		if (err) {
 			netdev_err(dev, "Failed to setup flooding for vFID=%d\n",
 				   vfid->vfid);
@@ -863,8 +863,7 @@ err_port_vid_to_fid_set:
 		mlxsw_sp_port_vlan_mode_trans(mlxsw_sp_port);
 err_port_vp_mode_trans:
 	if (!vfid->nr_vports)
-		mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, false,
-					 false);
+		mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, false);
 err_vport_flood_set:
 	mlxsw_sp_port_vport_destroy(mlxsw_sp_vport);
 err_port_vport_create:
@@ -3272,8 +3271,7 @@ static void mlxsw_sp_vport_bridge_leave(struct mlxsw_sp_port *mlxsw_sp_vport,
 		goto err_port_vid_learning_set;
 	}
 
-	err = mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, false,
-				       false);
+	err = mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, false);
 	if (err) {
 		netdev_err(dev, "Failed clear to clear flooding\n");
 		goto err_vport_flood_set;
@@ -3332,7 +3330,7 @@ static int mlxsw_sp_vport_bridge_join(struct mlxsw_sp_port *mlxsw_sp_vport,
 		}
 	}
 
-	err = mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, true, false);
+	err = mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, true);
 	if (err) {
 		netdev_err(dev, "Failed to setup flooding for vFID=%d\n",
 			   vfid->vfid);
@@ -3391,7 +3389,7 @@ err_port_vid_to_fid_validate:
 err_port_vid_to_fid_invalidate:
 	mlxsw_sp_port_vid_learning_set(mlxsw_sp_vport, vid, false);
 err_port_vid_learning_set:
-	mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, false, false);
+	mlxsw_sp_vport_flood_set(mlxsw_sp_vport, vfid->vfid, false);
 err_port_flood_set:
 	if (!vfid->nr_vports)
 		mlxsw_sp_br_vfid_destroy(mlxsw_sp, vfid);
