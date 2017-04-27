494a4ca610961f3671915973f85684da1d84bf09
Ewan Milne <emilne@redhat.com>
2017-04-27 15:13:52 -0400

    [scsi] revert "lpfc: Fix eh_deadline setting for sli3 adapters"
    Message-id: <1493306063-11908-30-git-send-email-emilne@redhat.com>
    Patchwork-id: 177291
    O-Subject: [RHEL7.4 PATCH e-stor 29/60] Revert: [scsi] lpfc: Fix eh_deadline setting for sli3 adapters
    Bugzilla: 1444045
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Maurizio Lombardi <mlombard@redhat.com>
    
    This reverts commit e365c8ca0669832bdb8fd4bd0164974fcd1fa0ad
    
    Regression found preventing a system with Emulex FCoE adapter
    from booting, reverting the entire series.  (BZ 1444045)
    
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/lpfc/lpfc.h
+++ drivers/scsi/lpfc/lpfc.h
@@ -99,7 +99,6 @@ struct lpfc_sli2_slim;
 #define FC_MAX_ADPTMSG		64
 
 #define MAX_HBAEVT	32
-#define MAX_HBAS_NO_RESET 16
 
 /* Number of MSI-X vectors the driver uses */
 #define LPFC_MSIX_VECTORS	2
