f7e8393930055adaf34d526cde3082aee0f7ae4b
Ewan Milne <emilne@redhat.com>
2017-04-27 15:14:22 -0400

    [scsi] revert "lpfc: Correct WQ creation for pagesize"
    Message-id: <1493306063-11908-60-git-send-email-emilne@redhat.com>
    Patchwork-id: 177324
    O-Subject: [RHEL7.4 PATCH e-stor 59/60] Revert: [scsi] lpfc: Correct WQ creation for pagesize
    Bugzilla: 1444045
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Maurizio Lombardi <mlombard@redhat.com>
    
    This reverts commit fd0d8d1280b4d4fdbe4433ebf5d7385dca83ccbd
    
    Regression found preventing a system with Emulex FCoE adapter
    from booting, reverting the entire series.  (BZ 1444045)
    
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/lpfc/lpfc_sli.c
+++ drivers/scsi/lpfc/lpfc_sli.c
@@ -13674,7 +13674,7 @@ lpfc_wq_create(struct lpfc_hba *phba, struct lpfc_queue *wq,
 			       LPFC_WQ_WQE_SIZE_128);
 			bf_set(lpfc_mbx_wq_create_page_size,
 			       &wq_create->u.request_1,
-			       LPFC_WQ_PAGE_SIZE_4096);
+			       (PAGE_SIZE/SLI4_PAGE_SIZE));
 			page = wq_create->u.request_1.page;
 			break;
 		}
@@ -13700,9 +13700,8 @@ lpfc_wq_create(struct lpfc_hba *phba, struct lpfc_queue *wq,
 			       LPFC_WQ_WQE_SIZE_128);
 			break;
 		}
-		bf_set(lpfc_mbx_wq_create_page_size,
-		       &wq_create->u.request_1,
-		       LPFC_WQ_PAGE_SIZE_4096);
+		bf_set(lpfc_mbx_wq_create_page_size, &wq_create->u.request_1,
+		       (PAGE_SIZE/SLI4_PAGE_SIZE));
 		page = wq_create->u.request_1.page;
 		break;
 	default:
@@ -13888,7 +13887,7 @@ lpfc_rq_create(struct lpfc_hba *phba, struct lpfc_queue *hrq,
 		       LPFC_RQE_SIZE_8);
 		bf_set(lpfc_rq_context_page_size,
 		       &rq_create->u.request.context,
-		       LPFC_RQ_PAGE_SIZE_4096);
+		       (PAGE_SIZE/SLI4_PAGE_SIZE));
 	} else {
 		switch (hrq->entry_count) {
 		default:
