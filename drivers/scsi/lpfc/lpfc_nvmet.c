5dc8ef8067e10d36bb46adbd12a1971619a31eb8
Ewan Milne <emilne@redhat.com>
2017-04-27 15:14:09 -0400

    [scsi] revert "lpfc: Update copyrights"
    Message-id: <1493306063-11908-47-git-send-email-emilne@redhat.com>
    Patchwork-id: 177314
    O-Subject: [RHEL7.4 PATCH e-stor 46/60] Revert: [scsi] lpfc: Update copyrights
    Bugzilla: 1444045
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Rob Evers <revers@redhat.com>
    RH-Acked-by: Maurizio Lombardi <mlombard@redhat.com>
    
    This reverts commit ff816ba783bf86f905d68fe5be760b62f4b67f2f
    
    Regression found preventing a system with Emulex FCoE adapter
    from booting, reverting the entire series.  (BZ 1444045)
    
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/lpfc/lpfc_nvmet.c
+++ drivers/scsi/lpfc/lpfc_nvmet.c
@@ -1,11 +1,9 @@
 /*******************************************************************
  * This file is part of the Emulex Linux Device Driver for         *
- * Fibre Channsel Host Bus Adapters.                               *
- * Copyright (C) 2017 Broadcom. All Rights Reserved. The term      *
- * “Broadcom” refers to Broadcom Limited and/or its subsidiaries.  *
+ * Fibre Channsel Host Bus Adapters.                                *
  * Copyright (C) 2004-2016 Emulex.  All rights reserved.           *
  * EMULEX and SLI are trademarks of Emulex.                        *
- * www.broadcom.com                                                *
+ * www.emulex.com                                                  *
  * Portions Copyright (C) 2004-2005 Christoph Hellwig              *
  *                                                                 *
  * This program is free software; you can redistribute it and/or   *
