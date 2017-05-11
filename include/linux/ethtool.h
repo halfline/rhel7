a142fc5e967de4146c7164a1679d67efe7fc5408
Ivan Vecera <ivecera@redhat.com>
2017-05-11 15:32:05 -0400

    [net] ethtool: remove unused __ethtool_get_settings
    Message-id: <20170511153205.26897-8-ivecera@redhat.com>
    Patchwork-id: 178168
    O-Subject: [RHEL7.4 net PATCH 7/7] net: ethtool: remove unused __ethtool_get_settings
    Bugzilla: 1449330
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    
    BZ#1449330
    
    Backport note: We need to preserve __ethtool_get_settings() as it is part
    of kABI. We need to extend it to use newer link_ksettings API implemented
    by newer drivers.
    
    Upstream commit(s):
    commit 3237fc63a3297d472a8cec33cb914f20570cfc23
    Author: David Decotigny <decot@googlers.com>
    Date:   Wed Feb 24 10:58:11 2016 -0800
    
        net: ethtool: remove unused __ethtool_get_settings
    
        replaced by __ethtool_get_link_ksettings.
    
        Signed-off-by: David Decotigny <decot@googlers.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/ethtool.h
+++ include/linux/ethtool.h
@@ -151,10 +151,6 @@ extern int
 __ethtool_get_link_ksettings(struct net_device *dev,
 			     struct ethtool_link_ksettings *link_ksettings);
 
-/* DEPRECATED, use __ethtool_get_link_ksettings */
-extern int __ethtool_get_settings(struct net_device *dev,
-				  struct ethtool_cmd *cmd);
-
 void ethtool_convert_legacy_u32_to_link_mode(unsigned long *dst,
 					     u32 legacy_u32);
 
