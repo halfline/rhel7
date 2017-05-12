8f128136fea802162914a7506674957c1bf416dc
Ivan Vecera <ivecera@redhat.com>
2017-05-12 15:58:11 -0400

    [net] sched: cls: also reject deleting all filters when TCA_KIND present
    Message-id: <20170512155811.27652-3-ivecera@redhat.com>
    Patchwork-id: 178305
    O-Subject: [RHEL7.4 net PATCH 2/2] net, cls: also reject deleting all filters when TCA_KIND present
    Bugzilla: 1450137
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    BZ#1450137
    
    Upstream commit(s):
    commit 9f6ed032cd951d2427995578c51af1b50c054efa
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date:   Thu Jun 16 23:19:29 2016 +0200
    
        net, cls: also reject deleting all filters when TCA_KIND present
    
        When we check for RTM_DELTFILTER, we should also reject the request
        for deleting all filters under a given parent when TCA_KIND attribute
        is present. If present, it's currently just ignored but there's also
        no point to let it pass in the first place either since this doesn't
        have any meaning with wild-card removal.
    
        Fixes: ea7f8277f907 ("net, cls: allow for deleting all filters for given parent")
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -168,7 +168,7 @@ replay:
 	if (prio == 0) {
 		switch (n->nlmsg_type) {
 		case RTM_DELTFILTER:
-			if (protocol || t->tcm_handle)
+			if (protocol || t->tcm_handle || tca[TCA_KIND])
 				return -ENOENT;
 			break;
 		case RTM_NEWTFILTER:
