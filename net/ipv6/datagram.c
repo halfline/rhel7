f492880289fbf6851e5ca8124fb84420b21d7fea
Florian Westphal <fwestpha@redhat.com>
2017-05-17 08:36:04 -0400

    [net] ipv6: lock socket in ip6_datagram_connect()
    Message-id: <1495010169-473-3-git-send-email-fwestpha@redhat.com>
    Patchwork-id: 178462
    O-Subject: [RHEL7.4 net 2/7] ipv6: lock socket in ip6_datagram_connect()
    Bugzilla: 1441554
    RH-Acked-by: Lance Richardson <lrichard@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Eric Garver <egarver@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1441554
    Upstream Status: commit 03645a1
    
    Conflicts: \
     net/ipv6/datagram.c
    
    ... because RHEL7 contains backorts of commits made after this change
    in upstream (esp. Martin KaFai Laus pmtu changes ("ipv6: datagram: Update
    dst cache of a connected datagram sk during pmtu update").
    
    This change itself was not modified, the conflict was only due to
    differences in sourrounding code.
    
    Looks like we want this in rhel7 too; also makes next patch apply
    without changes.
    
    commit 03645a11a570d52e70631838cb786eb4253eb463
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Tue Jul 14 08:10:22 2015 +0200
    
        ipv6: lock socket in ip6_datagram_connect()
    
        ip6_datagram_connect() is doing a lot of socket changes without
        socket being locked.
    
        This looks wrong, at least for udp_lib_rehash() which could corrupt
        lists because of concurrent udp_sk(sk)->udp_portaddr_hash accesses.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/ipv6/datagram.c
+++ net/ipv6/datagram.c
@@ -139,7 +139,7 @@ void ip6_datagram_release_cb(struct sock *sk)
 }
 EXPORT_SYMBOL_GPL(ip6_datagram_release_cb);
 
-int ip6_datagram_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
+static int __ip6_datagram_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
 {
 	struct sockaddr_in6	*usin = (struct sockaddr_in6 *) uaddr;
 	struct inet_sock      	*inet = inet_sk(sk);
@@ -152,7 +152,7 @@ int ip6_datagram_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
 	if (usin->sin6_family == AF_INET) {
 		if (__ipv6_only_sock(sk))
 			return -EAFNOSUPPORT;
-		err = ip4_datagram_connect(sk, uaddr, addr_len);
+		err = __ip4_datagram_connect(sk, uaddr, addr_len);
 		goto ipv4_connected;
 	}
 
@@ -187,9 +187,9 @@ int ip6_datagram_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
 		sin.sin_addr.s_addr = daddr->s6_addr32[3];
 		sin.sin_port = usin->sin6_port;
 
-		err = ip4_datagram_connect(sk,
-					   (struct sockaddr *) &sin,
-					   sizeof(sin));
+		err = __ip4_datagram_connect(sk,
+					     (struct sockaddr *) &sin,
+					     sizeof(sin));
 
 ipv4_connected:
 		if (err)
@@ -251,6 +251,16 @@ ipv4_connected:
 	sk_set_txhash(sk);
 out:
 	return err;
+}
+
+int ip6_datagram_connect(struct sock *sk, struct sockaddr *uaddr, int addr_len)
+{
+	int res;
+
+	lock_sock(sk);
+	res = __ip6_datagram_connect(sk, uaddr, addr_len);
+	release_sock(sk);
+	return res;
 }
 EXPORT_SYMBOL_GPL(ip6_datagram_connect);
 
