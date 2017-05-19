d392d712cb52caa8b4b5733e56a5cf53ad679f3c
Jonathan Toppins <jtoppins@redhat.com>
2017-05-19 01:42:09 -0400

    [netdrv] mlx5e: Support offload cls_flower with skbedit mark action
    Message-id: <bfd3bd10b5d25d8d7c8ed97c1a7e2fb82408c9dd.1495147746.git.jtoppins@redhat.com>
    Patchwork-id: 178651
    O-Subject: [RHEL7.4 07/53] net/mlx5e: Support offload cls_flower with skbedit mark action
    Bugzilla: 1383217
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Introduce offloading of skbedit mark action.
    
    For example, to mark with 0x1234, all TCP (ip_proto 6) packets arriving
    to interface ens9:
    
     # tc qdisc add dev ens9 ingress
     # tc filter add dev ens9 protocol ip parent ffff: \
         flower ip_proto 6 \
         indev ens9 \
         action skbedit mark 0x1234
    
    Signed-off-by: Amir Vadai <amir@vadai.me>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 12185a9fafa9cf39b73588c92aa49300ff3bf191)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
    	[jt: context fixup due to the driver being at v4.10]
    Bugzilla: 1383217
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=13231812
    Tested: mellanox tested this with there regression test suite
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
@@ -36,6 +36,7 @@
 #include <linux/tcp.h>
 #include <net/busy_poll.h>
 #include "en.h"
+#include "en_tc.h"
 #include "eswitch.h"
 
 static inline bool mlx5e_rx_hw_stamp(struct mlx5e_tstamp *tstamp)
@@ -622,6 +623,8 @@ static inline void mlx5e_build_rx_skb(struct mlx5_cqe64 *cqe,
 	if (cqe_has_vlan(cqe))
 		__vlan_hwaccel_put_tag(skb, htons(ETH_P_8021Q),
 				       be16_to_cpu(cqe->vlan_info));
+
+	skb->mark = be32_to_cpu(cqe->sop_drop_qpn) & MLX5E_TC_FLOW_ID_MASK;
 
 	mlx5e_handle_csum(netdev, cqe, rq, skb, !!lro_num_seg);
 	skb->protocol = eth_type_trans(skb, netdev);
