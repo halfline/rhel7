b6371fac1a0aa5e5b01f01570411962a54437b09
Jonathan Toppins <jtoppins@redhat.com>
2017-05-19 01:42:31 -0400

    [netdrv] mlx5e: Add TC tunnel release action for SRIOV offloads
    Message-id: <06646ac7d5a0cc9a56794ea5ee72901571671b07.1495147746.git.jtoppins@redhat.com>
    Patchwork-id: 178678
    O-Subject: [RHEL7.4 29/53] net/mlx5e: Add TC tunnel release action for SRIOV offloads
    Bugzilla: 1383217
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    
    Enhance the parsing of offloaded TC rules to set HW matching on outer
    (encapsulation) headers.
    Parse TC tunnel release action and set it as mlx5 decap action when the
    required capabilities are supported.
    
    Signed-off-by: Hadar Hen Zion <hadarh@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit bbd00f7e2349e53345d0a151c04c8cb7e10bf2e7)
    Bugzilla: 1383217
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=13231812
    Tested: mellanox tested this with there regression test suite
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
@@ -83,6 +83,8 @@ mlx5_eswitch_add_offloaded_rule(struct mlx5_eswitch *esw,
 
 	spec->match_criteria_enable = MLX5_MATCH_OUTER_HEADERS |
 				      MLX5_MATCH_MISC_PARAMETERS;
+	if (flow_act.action & MLX5_FLOW_CONTEXT_ACTION_DECAP)
+		spec->match_criteria_enable |= MLX5_MATCH_INNER_HEADERS;
 
 	if (attr->encap)
 		flow_act.encap_id = attr->encap->encap_id;
@@ -413,6 +415,7 @@ static int esw_create_offloads_fdb_table(struct mlx5_eswitch *esw, int nvports)
 	u32 *flow_group_in;
 	void *match_criteria;
 	int table_size, ix, err = 0;
+	u32 flags = 0;
 
 	flow_group_in = mlx5_vzalloc(inlen);
 	if (!flow_group_in)
@@ -428,10 +431,14 @@ static int esw_create_offloads_fdb_table(struct mlx5_eswitch *esw, int nvports)
 	esw_debug(dev, "Create offloads FDB table, log_max_size(%d)\n",
 		  MLX5_CAP_ESW_FLOWTABLE_FDB(dev, log_max_ft_size));
 
+	if (MLX5_CAP_ESW_FLOWTABLE_FDB(dev, encap) &&
+	    MLX5_CAP_ESW_FLOWTABLE_FDB(dev, decap))
+		flags |= MLX5_FLOW_TABLE_TUNNEL_EN;
+
 	fdb = mlx5_create_auto_grouped_flow_table(root_ns, FDB_FAST_PATH,
 						  ESW_OFFLOADS_NUM_ENTRIES,
 						  ESW_OFFLOADS_NUM_GROUPS, 0,
-						  0);
+						  flags);
 	if (IS_ERR(fdb)) {
 		err = PTR_ERR(fdb);
 		esw_warn(dev, "Failed to create Fast path FDB Table err %d\n", err);
