658aaba802c1d088b5e80813e1ffe080b28a1643
Paul Lai <plai@redhat.com>
2017-06-01 22:07:46 -0400

    [x86] kvm: x86: Expose Intel AVX512IFMA/AVX512VBMI/SHA features to guest
    Message-id: <1496354868-21771-3-git-send-email-plai@redhat.com>
    Patchwork-id: 179246
    O-Subject: [RHEL7.4 PATCH BZ1415961 RESEND v3 2/4] KVM: x86: Expose Intel AVX512IFMA/AVX512VBMI/SHA features to guest.
    Bugzilla: 1415961
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    
    From: Yi Sun <yi.y.sun@linux.intel.com>
    
    Expose AVX512IFMA/AVX512VBMI/SHA features to guest.
    
    AVX512 spec can be found at:
    https://software.intel.com/sites/default/files/managed/26/40/319433-026.pdf
    
    SHA spec can be found at:
    https://software.intel.com/sites/default/files/managed/39/c5/325462-sdm-vol-1-2abcd-3abcd.pdf
    
    This patch depends on below patch.
    http://marc.info/?l=linux-kernel&m=147932800828178&w=2
    
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 83781d180b219bd079ae72b341ee3f21fb236e97)
    Signed-off-by: Paul Lai <plai@redhat.com>
    
    Resolved Conflicts:
    	arch/x86/kvm/cpuid.c
    		abandoned the PK (Protection Key) parts of tha patch
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -361,13 +361,17 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 	const u32 kvm_supported_word9_x86_features =
 		F(FSGSBASE) | F(BMI1) | F(HLE) | F(AVX2) | F(SMEP) |
 		F(BMI2) | F(ERMS) | f_invpcid | F(RTM) | f_mpx | F(RDSEED) |
-		F(ADX) | F(SMAP) | F(AVX512F) | F(AVX512PF) | F(AVX512ER) |
-		F(AVX512CD) | F(CLFLUSHOPT) | F(CLWB) | F(AVX512DQ) |
-		F(AVX512BW) | F(AVX512VL);
+		F(ADX) | F(SMAP) | F(AVX512IFMA) | F(AVX512F) | F(AVX512PF) |
+		F(AVX512ER) | F(AVX512CD) | F(CLFLUSHOPT) | F(CLWB) | F(AVX512DQ) |
+		F(SHA_NI) | F(AVX512BW) | F(AVX512VL);
 
 	/* cpuid 0xD.1.eax */
 	const u32 kvm_supported_word10_x86_features =
 		F(XSAVEOPT) | F(XSAVEC) | F(XGETBV1);
+
+	/* cpuid 7.0.ecx*/
+	const u32 kvm_cpuid_7_0_ecx_x86_features =
+		F(AVX512VBMI);
 
 	/* cpuid 7.0.edx*/
 	const u32 kvm_cpuid_7_0_edx_x86_features =
@@ -450,14 +454,16 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 			cpuid_mask(&entry->ebx, 9);
 			// TSC_ADJUST is emulated
 			entry->ebx |= F(TSC_ADJUST);
+			entry->ecx &= kvm_cpuid_7_0_ecx_x86_features;
+			cpuid_mask(&entry->ecx, CPUID_7_ECX);
 			entry->edx &= kvm_cpuid_7_0_edx_x86_features;
 			entry->edx &= get_scattered_cpuid_leaf(7, 0, CPUID_EDX);
 		} else {
 			entry->ebx = 0;
+			entry->ecx = 0;
 			entry->edx = 0;
 		}
 		entry->eax = 0;
-		entry->ecx = 0;
 		break;
 	}
 	case 9:
