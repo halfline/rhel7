halfline/rhel7

commit 0c6649509d63fe065db9ddb966a71301df2c2463

[fs] mnt: Move unprivileged use of the mntns to tech preview
Message-id: <gvs4lz680r3.fsf@ebiederm.remote.csb>
Patchwork-id: 171179
O-Subject: [RHEL7.4 BZ1350553 PATCH 1/1] mnt: Move unprivileged use of the mntns to tech preview
Bugzilla: 1350553
RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
RH-Acked-by: Ian Kent <ikent@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1350553
Upstream Status: This is a RHEL specific patch
Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=12693873
Testing: https://beaker.engineering.redhat.com/jobs/1748967
Testing:

    Booted with "user_namespace.enable=1 namespace.unpiv_enable=1" and
    verified that unsharing the user namespace works and gives a tech
    preview warning.  Verified that then unsharing the mount namespace
    works and gives my new tech preview warning.

    Booted with just "user_namespace.enable=1" and verified I can't
    unshare the mount namespace.

    Booted normally and verified I can't unshare either the
    user namespace or the mount namespace.

This fixes a bug in copy_mnt_ns where it was limiting creation of
unprivileged mnt namespaces by testing for capable(CAP_SYS_ADMIN)
instead of testing if user_ns != &init_user_ns.  This limits us to
creating only mount namespaces requiring CAP_SYS_ADMIN in the initial
mount namespace.  The callers all guarantee that we have
CAP_SYS_ADMIN in the user namespace we are creating the mount
namespace in, so nothing is lost by removing the permission
check.

A command line option is added namespace.unpriv_enable=1 to enable
the root in a user namespace to create mount namespaces.  The first
time it is used we say something about tech-preview to keep the support
burden in check.

The command line guard is important in this case as there are still
some unpatched CVE worthy bugs related to unprivileged use of the
mount namespaces in RHEL7.  Perhaps we will have them all stomped
by RHEL7.5 and be able to remove the tech-preview status.

fs/namespace.c
@@ -22,6 +22,7 @@
 #include <linux/uaccess.h>
 #include <linux/proc_ns.h>
 #include <linux/magic.h>
+#include <linux/module.h>
 #include <linux/kernfs.h>
 #include <linux/bootmem.h>
 #include <linux/task_work.h>
@@ -2789,6 +2790,11 @@ static struct mnt_namespace *alloc_mnt_ns(struct user_namespace *user_ns)
 	return new_ns;
 }
 
+/* namespace.unpriv_enable = 1 */
+static bool enable_unpriv_mnt_ns_creation;
+module_param_named(unpriv_enable, enable_unpriv_mnt_ns_creation, bool, 0444);
+MODULE_PARM_DESC(unpriv_enable, "Enable unprivileged creation of mount namespaces");
+
 struct mnt_namespace *copy_mnt_ns(unsigned long flags, struct mnt_namespace *ns,
 		struct user_namespace *user_ns, struct fs_struct *new_fs)
 {
@@ -2806,9 +2812,17 @@ struct mnt_namespace *copy_mnt_ns(unsigned long flags, struct mnt_namespace *ns,
 		return ns;
 	}
 
-	/* Unprivileged creation currently disabled in RHEL7  */
-	if (!capable(CAP_SYS_ADMIN))
-		return ERR_PTR(-EPERM);
+	/* Unprivileged creation currently tech preview in RHEL7  */
+	if (user_ns != &init_user_ns) {
+		static int __read_mostly called_mark_tech_preview = 0;
+		if (!enable_unpriv_mnt_ns_creation) {
+			put_mnt_ns(ns);
+			return ERR_PTR(-EPERM);
+		}
+		if (!called_mark_tech_preview &&
+		    !xchg(&called_mark_tech_preview, 1))
+			mark_tech_preview("unpriv mount namespace", NULL);
+	}
 
 	old = ns->root;
 

commit fd6742bd383021c232c71c4bbb2a9f9cb4925fcd

[fs] mntns: Remove incorrect put_mnt_ns
Message-id: <gvsvanp1pte.fsf_-_@ebiederm.remote.csb>
Patchwork-id: 180030
O-Subject: [RHEL7.4 BZ1463072 PATCH] mntns: Remove incorrect put_mnt_ns
Bugzilla: 1463072
RH-Acked-by: Oleg Nesterov <oleg@redhat.com>
RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>

When the unprivileged mntns support was rebased because of
5a910408af72 ("[fs] fold dup_mnt_ns() into its only surviving caller")
the need for calling put_mnt_ns disappeared.  Unfortunately I missed
that fact.

Now unshare -Urm called several times, results in a kernel oops,
and anyone can do this.

Fix this by removing the unnecessary line of code.

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1463072
Upstream Status: RHEL specific
Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=13486016
Testing: https://beaker.engineering.redhat.com/jobs/1915441
Testing: Chao Ye <cye@redhat.com> verified that the code (essentially
         "unshare -Urm") that was triggering kernel oopses and hangs no longer
         triggers them with this patch.

Fixes: 0c6649509d63 ("[fs] mnt: Move unprivileged use of the mntns to tech preview")
Signed-off-by: "Eric W. Biederman" <ebiederm@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

fs/namespace.c
@@ -2816,7 +2816,6 @@ struct mnt_namespace *copy_mnt_ns(unsigned long flags, struct mnt_namespace *ns,
 	if (user_ns != &init_user_ns) {
 		static int __read_mostly called_mark_tech_preview = 0;
 		if (!enable_unpriv_mnt_ns_creation) {
-			put_mnt_ns(ns);
 			return ERR_PTR(-EPERM);
 		}
 		if (!called_mark_tech_preview &&