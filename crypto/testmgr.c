cd2e45c3c957bb7feeebaf685d4acb5608825305
Herbert Xu <herbert.xu@redhat.com>
2017-06-28 16:14:49 -0400

    [crypto] testmgr - Reenable sha1/aes in FIPS mode
    Message-id: <E1dQFc9-0004R5-By@gondobar>
    Patchwork-id: 180228
    O-Subject: [RHEL7.4 v2 PATCH 2/2] crypto: testmgr - Reenable sha1/aes in FIPS mode
    Bugzilla: 1465234
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Bruno E. O. Meneguele <brdeoliv@redhat.com>
    RH-Acked-by: Jan Stancek <jstancek@redhat.com>
    
    RHEL7.4 Bugzilla #1465234
    
    commit bcf741cb779283081db47853264cc94854e7ad83
    
    The combination of sha1 and aes was disabled in FIPS Mode
    accidentally.  This patch reenables it.
    
    Fixes: 284a0f6e87b0 ("crypto: testmgr - Disable fips-allowed for...")
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Acked-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -2139,6 +2139,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "authenc(hmac(sha1),cbc(aes))",
 		.test = alg_test_aead,
+		.fips_allowed = 1,
 		.suite = {
 			.aead = {
 				.enc = {
