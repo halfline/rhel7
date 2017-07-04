7008a063f23da7ce5fe713a4f223f9e86dacf341
Gustavo Duarte <gduarte@redhat.com>
2017-07-04 19:45:19 -0400

    [powerpc] Simplify module TOC handling
    Message-id: <1499197520-1736-2-git-send-email-gduarte@redhat.com>
    Patchwork-id: 180358
    O-Subject: [RHEL7.5 PATCH BZ 1464009 1/2] powerpc: Simplify module TOC handling
    Bugzilla: 1464009
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    BZ 1464009
    Upstream Status: c153693d7eb9eeb28478aa2deaaf0b4e7b5ff5e9
    
    commit c153693d7eb9eeb28478aa2deaaf0b4e7b5ff5e9
    Author: Alan Modra <amodra@gmail.com>
    Date:   Fri Jan 15 20:52:22 2016 +1100
    
        powerpc: Simplify module TOC handling
    
        PowerPC64 uses the symbol .TOC. much as other targets use
        _GLOBAL_OFFSET_TABLE_. It identifies the value of the GOT pointer (or in
        powerpc parlance, the TOC pointer). Global offset tables are generally
        local to an executable or shared library, or in the kernel, module. Thus
        it does not make sense for a module to resolve a relocation against
        .TOC. to the kernel's .TOC. value. A module has its own .TOC., and
        indeed the powerpc64 module relocation processing ignores the kernel
        value of .TOC. and instead calculates a module-local value.
    
        This patch removes code involved in exporting the kernel .TOC., tweaks
        modpost to ignore an undefined .TOC., and the module loader to twiddle
        the section symbol so that .TOC. isn't seen as undefined.
    
        Note that if the kernel was compiled with -msingle-pic-base then ELFv2
        would not have function global entry code setting up r2. In that case
        the module call stubs would need to be modified to set up r2 using the
        kernel .TOC. value, requiring some of this code to be reinstated.
    
        mpe: Furthermore a change in binutils master (not yet released) causes
        the current way we handle the TOC to no longer work when building with
        MODVERSIONS=y and RELOCATABLE=n. The symptom is that modules can not be
        loaded due to there being no version found for TOC.
    
        Cc: stable@vger.kernel.org # 3.16+
        Signed-off-by: Alan Modra <amodra@gmail.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/powerpc/kernel/module_64.c
+++ arch/powerpc/kernel/module_64.c
@@ -328,7 +328,10 @@ static void dedotify_versions(struct modversion_info *vers,
 		}
 }
 
-/* Undefined symbols which refer to .funcname, hack to funcname (or .TOC.) */
+/*
+ * Undefined symbols which refer to .funcname, hack to funcname. Make .TOC.
+ * seem to be defined (value set later).
+ */
 static void dedotify(Elf64_Sym *syms, unsigned int numsyms, char *strtab)
 {
 	unsigned int i;
@@ -336,8 +339,11 @@ static void dedotify(Elf64_Sym *syms, unsigned int numsyms, char *strtab)
 	for (i = 1; i < numsyms; i++) {
 		if (syms[i].st_shndx == SHN_UNDEF) {
 			char *name = strtab + syms[i].st_name;
-			if (name[0] == '.')
+			if (name[0] == '.') {
+				if (strcmp(name+1, "TOC.") == 0)
+					syms[i].st_shndx = SHN_ABS;
 				memmove(name, name+1, strlen(name));
+			}
 		}
 	}
 }
@@ -353,7 +359,7 @@ static Elf64_Sym *find_dot_toc(Elf64_Shdr *sechdrs,
 	numsyms = sechdrs[symindex].sh_size / sizeof(Elf64_Sym);
 
 	for (i = 1; i < numsyms; i++) {
-		if (syms[i].st_shndx == SHN_UNDEF
+		if (syms[i].st_shndx == SHN_ABS
 		    && strcmp(strtab + syms[i].st_name, "TOC.") == 0)
 			return &syms[i];
 	}
