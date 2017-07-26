88d9cb23d97c6088a6abbd2cbaf41079af256591
Ilya Dryomov <idryomov@redhat.com>
2017-07-26 14:42:26 -0400

    [fs] ceph: properly set issue_seq for cap release
    Message-id: <1501080235-8156-96-git-send-email-idryomov@redhat.com>
    Patchwork-id: 181235
    O-Subject: [RHEL7.5 fs PATCH 095/184] ceph: properly set issue_seq for cap release
    Bugzilla: 1450262
    RH-Acked-by: Sage Weil <sweil@redhat.com>
    RH-Acked-by: Jeff Layton <jlayton@redhat.com>
    
    From: "Yan, Zheng" <zyan@redhat.com>
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    (cherry picked from commit dc24de82d61b1edc3d195badd178e7875ba9398f)
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -3639,6 +3639,7 @@ void ceph_handle_caps(struct ceph_mds_session *session,
 			cap->cap_id = cap_id;
 			cap->mseq = mseq;
 			cap->seq = seq;
+			cap->issue_seq = seq;
 			spin_lock(&session->s_cap_lock);
 			list_add_tail(&cap->session_caps,
 					&session->s_cap_releases);
