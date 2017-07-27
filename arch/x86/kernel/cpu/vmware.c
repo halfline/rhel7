0e20e6c9f38b7f2d734c4343086508505c41eb14
Prarit Bhargava <prarit@redhat.com>
2017-07-27 13:30:39 -0400

    [x86] vmware: Use tsc_khz value for calibrate_cpu()
    Message-id: <1501162239-11502-1-git-send-email-prarit@redhat.com>
    Patchwork-id: 181427
    O-Subject: [URGENT: RHEL7.5 BZ 1475716] x86/vmware: Use tsc_khz value for calibrate_cpu()
    Bugzilla: 1475716
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1475716
    
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=13734086
    Testing: Done by Motorola.  Sanity booted brew build on a few systems.
    
    Conflicts: Small difference between upstream and RHEL7 requires additional
    brackets around if-then-else statements.
    
    commit 687bca8d664ac9b098005b57846773eb62040ae0
    Author: Alexey Makhalov <amakhalov@vmware.com>
    Date:   Fri Oct 28 00:54:30 2016 -0700
    
        x86/vmware: Use tsc_khz value for calibrate_cpu()
    
        Commit aa297292d708 ("x86/tsc: Enumerate SKL cpu_khz and tsc_khz via
        CPUID") separated the calibration mechanisms for cpu_khz and tsc_khz.
    
        Since the vmware hypervisor provides a constant frequency TSC to the guest,
        this change can lead to divergence between the tsc and the cpu frequency
        after vmotion, which might confuse the user.
    
        Solve this by overriding the x86 platform cpu calibration callback with the
        vmware specific tsc calibration function.
    
        Signed-off-by: Alexey Makhalov <amakhalov@vmware.com>
        Acked-by: Alok N Kataria <akataria@vmware.com>
        Cc: linux-doc@vger.kernel.org
        Cc: pv-drivers@vmware.com
        Cc: corbet@lwn.net
        Cc: virtualization@lists.linux-foundation.org
        Link: http://lkml.kernel.org/r/20161028075432.90579-2-amakhalov@vmware.com
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/vmware.c
+++ arch/x86/kernel/cpu/vmware.c
@@ -81,11 +81,13 @@ static void __init vmware_platform_setup(void)
 
 	VMWARE_PORT(GETHZ, eax, ebx, ecx, edx);
 
-	if (ebx != UINT_MAX)
+	if (ebx != UINT_MAX) {
 		x86_platform.calibrate_tsc = vmware_get_tsc_khz;
-	else
+		x86_platform.calibrate_cpu = vmware_get_tsc_khz;
+	} else {
 		printk(KERN_WARNING
 		       "Failed to get TSC freq from the hypervisor\n");
+	}
 }
 
 /*
