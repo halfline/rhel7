torvalds/linux

commit ab1a187bba5c4e5ed8aa6e7bb7dcd7cd7e065aef

PCI: Check device_attach() return value always
Previously we checked the device_attach() return value only when
CONFIG_BUG=y.  That caused this warning in builds where CONFIG_BUG is not
set:

  drivers/pci/bus.c:237:6: warning: variable 'retval' set but not used [-Wunused-but-set-variable]

Check the return value of device_attach() always and clean up after
failure.

Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

drivers/pci/bus.c
@@ -291,7 +291,12 @@ void pci_bus_add_device(struct pci_dev *dev)
 
 	dev->match_driver = true;
 	retval = device_attach(&dev->dev);
-	WARN_ON(retval < 0);
+	if (retval < 0) {
+		dev_warn(&dev->dev, "device attach failed (%d)\n", retval);
+		pci_proc_detach_device(dev);
+		pci_remove_sysfs_dev_files(dev);
+		return;
+	}
 
 	dev->is_added = 1;
 }

halfline/rhel7

commit ae2ad9e7b1a3421a145aade17a17655328671267

[pci] Fix BUG on device attach failure
Message-id: <20170720160927.26696.74135.stgit@amt.stowe>
Patchwork-id: 181034
O-Subject: [RHEL7.5 PATCH 19/21] PCI: Fix BUG on device attach failure
Bugzilla: 1466891
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1466891
Upstream Status: 1e398eae8407abdc02cde8a449b14d17ed193d56

commit 1e398eae8407abdc02cde8a449b14d17ed193d56
Author: Lukas Wunner <lukas@wunner.de>
Date:   Mon May 2 13:48:25 2016 -0500

    PCI: Fix BUG on device attach failure

    Previously when pci_bus_add_device() called device_attach() and it returned
    a negative value, we emitted a WARN but carried on.

    Commit ab1a187bba5c ("PCI: Check device_attach() return value always"),
    introduced in Linux 4.6-rc1, changed this to unwind all steps preceding
    device_attach() and to not set dev->is_added = 1.

    The latter leads to a BUG if pci_bus_add_device() was called from
    pci_bus_add_devices().  Fix by not recursing to a child bus if
    device_attach() failed for the bridge leading to it.

    This can be triggered by plugging in a PCI device (e.g. Thunderbolt) while
    the system is asleep.  The system locks up when woken because
    device_attach() returns -EPROBE_DEFER.

    Fixes: ab1a187bba5c ("PCI: Check device_attach() return value always")
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/pci/bus.c
@@ -325,7 +325,9 @@ void pci_bus_add_devices(const struct pci_bus *bus)
 	}
 
 	list_for_each_entry(dev, &bus->devices, bus_list) {
-		BUG_ON(!dev->is_added);
+		/* Skip if device attach failed */
+		if (!dev->is_added)
+			continue;
 		child = dev->subordinate;
 		if (child)
 			pci_bus_add_devices(child);