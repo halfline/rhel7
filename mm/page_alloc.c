8af9f2555c803cdee923ce7cfa9b6ac6a621f7d8
Waiman Long <longman@redhat.com>
2017-08-22 15:08:03 -0400

    [mm] debug_pagealloc: ask users for default setting of debug_pagealloc
    Message-id: <1503414494-14160-7-git-send-email-longman@redhat.com>
    Patchwork-id: 183096
    O-Subject: [RHEL7.5 PATCH 06/17] mm/debug_pagealloc: ask users for default setting of debug_pagealloc
    Bugzilla: 1481847
    RH-Acked-by: Rik van Riel <riel@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1481847
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=13900495
    
    commit ea6eabb05b26bd3d6f60b29b77a03bc61479fc0f
    Author: Christian Borntraeger <borntraeger@de.ibm.com>
    Date:   Tue Mar 15 14:55:30 2016 -0700
    
        mm/debug_pagealloc: ask users for default setting of debug_pagealloc
    
        Since commit 031bc5743f158 ("mm/debug-pagealloc: make debug-pagealloc
        boottime configurable") CONFIG_DEBUG_PAGEALLOC is by default not adding
        any page debugging.
    
        This resulted in several unnoticed bugs, e.g.
    
            https://lkml.kernel.org/g/<569F5E29.3090107@de.ibm.com>
        or
            https://lkml.kernel.org/g/<56A20F30.4050705@de.ibm.com>
    
        as this behaviour change was not even documented in Kconfig.
    
        Let's provide a new Kconfig symbol that allows to change the default
        back to enabled, e.g.  for debug kernels.  This also makes the change
        obvious to kernel packagers.
    
        Let's also change the Kconfig description for CONFIG_DEBUG_PAGEALLOC, to
        indicate that there are two stages of overhead.
    
        Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
        Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -529,7 +529,8 @@ static inline void prep_zero_page(struct page *page, int order, gfp_t gfp_flags)
 
 #ifdef CONFIG_DEBUG_PAGEALLOC
 unsigned int _debug_guardpage_minorder;
-bool _debug_pagealloc_enabled __read_mostly;
+bool _debug_pagealloc_enabled __read_mostly
+			= IS_ENABLED(CONFIG_DEBUG_PAGEALLOC_ENABLE_DEFAULT);
 bool _debug_guardpage_enabled __read_mostly;
 
 static int __init early_debug_pagealloc(char *buf)
@@ -539,6 +540,9 @@ static int __init early_debug_pagealloc(char *buf)
 
 	if (strcmp(buf, "on") == 0)
 		_debug_pagealloc_enabled = true;
+
+	if (strcmp(buf, "off") == 0)
+		_debug_pagealloc_enabled = false;
 
 	return 0;
 }
