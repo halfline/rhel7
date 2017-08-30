dcdae3dc7668765a72ff3c79eb62eeab6d3b36de
Prarit Bhargava <prarit@redhat.com>
2017-08-30 19:03:12 -0400

    [kernel] sched/headers: Prepare for new header dependencies before moving code to <linux/sched/cpufreq.h>
    Message-id: <1504119829-27117-144-git-send-email-prarit@redhat.com>
    Patchwork-id: 183841
    O-Subject: [RHEL7.5 BZ 1465349 143/179 v2] sched/headers: Prepare for new header dependencies before moving code to <linux/sched/cpufreq.h>
    Bugzilla: 1465349
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1465349
    
    commit 55687da166bf51129ed6b110d7711f4c7560abe2
    Author: Ingo Molnar <mingo@kernel.org>
    Date:   Wed Feb 8 18:51:31 2017 +0100
    
        sched/headers: Prepare for new header dependencies before moving code to <linux/sched/cpufreq.h>
    
        We are going to split <linux/sched/cpufreq.h> out of <linux/sched.h>, which
        will have to be picked up from other headers and a couple of .c files.
    
        Create a trivial placeholder <linux/sched/cpufreq.h> file that just
        maps to <linux/sched.h> to make this patch obviously correct and
        bisectable.
    
        Include the new header in the files that are going to need it.
    
        Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Mike Galbraith <efault@gmx.de>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: linux-kernel@vger.kernel.org
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Cc: Xunlei Pang <xlpang@redhat.com>
    Cc: Lauro Ramos Venancio <lvenanci@redhat.com>
    Cc: Waiman Long <longman@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3,6 +3,7 @@
 #include <linux/sched/sysctl.h>
 #include <linux/sched/rt.h>
 #include <linux/sched/deadline.h>
+#include <linux/sched/cpufreq.h>
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
 #include <linux/stop_machine.h>
