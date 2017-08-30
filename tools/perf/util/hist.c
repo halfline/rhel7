21e17d8135e6be0a119383860b425000b815ad34
Jiri Olsa <jolsa@redhat.com>
2017-08-30 16:03:08 -0400

    [tools] perf report: Show inline stack for browser mode
    Message-id: <20170830160540.26618-97-jolsa@redhat.com>
    Patchwork-id: 183511
    O-Subject: [PATCH RHEL7.5 BZ1480522 096/248] perf report: Show inline stack for browser mode
    Bugzilla: 1480522
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1480522
    https://bugzilla.redhat.com/show_bug.cgi?id=1480522
    
    upstream
    ========
    commit 0d3eb0b7783f1ee6d3314f101b9cbfb988020222
    Author: Jin Yao <yao.jin@linux.intel.com>
    Date: Sun Mar 26 04:34:29 2017 +0800
    
    description
    ===========
    If the address belongs to an inlined function, the source information
    back to the first non-inlined function will be printed.
    
    For example:
    
    1. Show inlined function name
       perf report -g function --inline
    
    -    0.69%     0.00%  inline   ld-2.23.so           [.] dl_main
       - dl_main
            0.56% _dl_relocate_object
             _dl_relocate_object (inline)
             elf_dynamic_do_Rela (inline)
    
    2. Show the file/line information
       perf report -g address --inline
    
    -    0.69%     0.00%  inline   ld-2.23.so           [.] _dl_start
         _dl_start rtld.c:307
          /build/glibc-GKVZIf/glibc-2.23/elf/rtld.c:413 (inline)
       + _dl_sysdep_start dl-sysdep.c:250
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -1129,6 +1129,11 @@ void hist_entry__delete(struct hist_entry *he)
 		zfree(&he->mem_info);
 	}
 
+	if (he->inline_node) {
+		inline_node__delete(he->inline_node);
+		he->inline_node = NULL;
+	}
+
 	zfree(&he->stat_acc);
 	free_srcline(he->srcline);
 	if (he->srcfile && he->srcfile[0])
