e8b9d11111fe7cc96aa1e4cd5b54c9d4e461b2a1
Prarit Bhargava <prarit@redhat.com>
2017-08-30 19:03:35 -0400

    [cpufreq] intel_pstate: Skip unnecessary PID resets on init
    Message-id: <1504119829-27117-167-git-send-email-prarit@redhat.com>
    Patchwork-id: 183874
    O-Subject: [RHEL7.5 BZ 1465349 166/179 v2] cpufreq: intel_pstate: Skip unnecessary PID resets on init
    Bugzilla: 1465349
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1465349
    
    commit 694cb173475a048a05daebf27cc8fdb7865c158b
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Tue Mar 28 00:11:53 2017 +0200
    
        cpufreq: intel_pstate: Skip unnecessary PID resets on init
    
        PID controller parameters only need to be initialized if the
        get_target_pstate_use_performance() P-state selection routine
        is going to be used.  It is not necessary to initialize them
        otherwise, so don't do that.
    
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    v2: remove extra sample_rate_ns calculation in intel_pstate_init_cpu()
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -1854,11 +1854,11 @@ static int intel_pstate_init_cpu(unsigned int cpunum)
 			intel_pstate_disable_ee(cpunum);
 
 		intel_pstate_hwp_enable(cpu);
+	} else if (pstate_funcs.get_target_pstate == get_target_pstate_use_performance) {
+		intel_pstate_pid_reset(cpu);
 	}
 
 	intel_pstate_get_cpu_pstates(cpu);
-
-	intel_pstate_pid_reset(cpu);
 
 	cpu->update_util.func = intel_pstate_update_util;
 
