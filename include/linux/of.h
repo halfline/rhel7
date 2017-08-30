6991268d6302ee841b6bb5ce6bebc1e6fe97d3d0
Prarit Bhargava <prarit@redhat.com>
2017-08-30 19:00:51 -0400

    [kernel] of: make of_property_for_each_{u32|string}() use parameters if OF is not enabled
    Message-id: <1504119829-27117-3-git-send-email-prarit@redhat.com>
    Patchwork-id: 183704
    O-Subject: [RHEL7.5 BZ 1465349 002/179 v2] OF: make of_property_for_each_{u32|string}() use parameters if OF is not enabled
    Bugzilla: 1465349
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1465349
    
    commit 2adfffa223500b739b17dcf3d710cec344f7608f
    Author: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Date:   Mon Jun 17 16:48:13 2013 +0200
    
        OF: make of_property_for_each_{u32|string}() use parameters if OF is not enabled
    
        I am getting a few
        |warning: unused variable p [-Wunused-variable]
        |warning: unused variable prop [-Wunused-variable]
    
        in the case where CONFIG_OF is not defined and the parameters are only
        used in the loop macro.
    
        Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
        Signed-off-by: Grant Likely <grant.likely@linaro.org>
    
    Cc: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -348,12 +348,6 @@ extern int of_detach_node(struct device_node *);
  */
 const __be32 *of_prop_next_u32(struct property *prop, const __be32 *cur,
 			       u32 *pu);
-#define of_property_for_each_u32(np, propname, prop, p, u)	\
-	for (prop = of_find_property(np, propname, NULL),	\
-		p = of_prop_next_u32(prop, NULL, &u);		\
-		p;						\
-		p = of_prop_next_u32(prop, p, &u))
-
 /*
  * struct property *prop;
  * const char *s;
@@ -362,11 +356,6 @@ const __be32 *of_prop_next_u32(struct property *prop, const __be32 *cur,
  *         printk("String value: %s\n", s);
  */
 const char *of_prop_next_string(struct property *prop, const char *cur);
-#define of_property_for_each_string(np, propname, prop, s)	\
-	for (prop = of_find_property(np, propname, NULL),	\
-		s = of_prop_next_string(prop, NULL);		\
-		s;						\
-		s = of_prop_next_string(prop, s))
 
 #else /* CONFIG_OF */
 
@@ -571,12 +560,20 @@ static inline int of_machine_is_compatible(const char *compat)
 	return 0;
 }
 
+static inline const __be32 *of_prop_next_u32(struct property *prop,
+		const __be32 *cur, u32 *pu)
+{
+	return NULL;
+}
+
+static inline const char *of_prop_next_string(struct property *prop,
+		const char *cur)
+{
+	return NULL;
+}
+
 #define of_match_ptr(_ptr)	NULL
 #define of_match_node(_matches, _node)	NULL
-#define of_property_for_each_u32(np, propname, prop, p, u) \
-	while (0)
-#define of_property_for_each_string(np, propname, prop, s) \
-	while (0)
 #endif /* CONFIG_OF */
 
 #if defined(CONFIG_OF) && defined(CONFIG_NUMA)
@@ -812,6 +809,18 @@ static inline int of_get_available_child_count(const struct device_node *np)
 
 	return num;
 }
+
+#define of_property_for_each_u32(np, propname, prop, p, u)	\
+	for (prop = of_find_property(np, propname, NULL),	\
+		p = of_prop_next_u32(prop, NULL, &u);		\
+		p;						\
+		p = of_prop_next_u32(prop, p, &u))
+
+#define of_property_for_each_string(np, propname, prop, s)	\
+	for (prop = of_find_property(np, propname, NULL),	\
+		s = of_prop_next_string(prop, NULL);		\
+		s;						\
+		s = of_prop_next_string(prop, s))
 
 #if defined(CONFIG_PROC_FS) && defined(CONFIG_PROC_DEVICETREE)
 extern void proc_device_tree_add_node(struct device_node *, struct proc_dir_entry *);
