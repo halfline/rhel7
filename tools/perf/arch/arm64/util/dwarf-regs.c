fa2e40f0c78181314239b211883cce586e2cdd6d
Jiri Olsa <jolsa@redhat.com>
2017-08-30 16:04:16 -0400

    [tools] perf tools: Replace STR() calls with __stringify()
    Message-id: <20170830160540.26618-165-jolsa@redhat.com>
    Patchwork-id: 183580
    O-Subject: [PATCH RHEL7.5 BZ1480522 164/248] perf tools: Replace STR() calls with __stringify()
    Bugzilla: 1480522
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1480522
    https://bugzilla.redhat.com/show_bug.cgi?id=1480522
    
    upstream
    ========
    commit aa8cc2f6b5faae3511ea229846d8e54bd8df40d1
    Author: Arnaldo Carvalho de Melo <acme@redhat.com>
    Date: Mon Apr 17 15:36:40 2017 -0300
    
    description
    ===========
    Both do the same thing, the later is the one we get from
    linux/stringify.h, i.e. we now use the same function name/practice as
    the kernel sources.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/arch/arm64/util/dwarf-regs.c
+++ tools/perf/arch/arm64/util/dwarf-regs.c
@@ -10,6 +10,7 @@
 
 #include <stddef.h>
 #include <dwarf-regs.h>
+#include <linux/stringify.h>
 
 struct pt_regs_dwarfnum {
 	const char *name;
@@ -19,7 +20,7 @@ struct pt_regs_dwarfnum {
 #define STR(s) #s
 #define REG_DWARFNUM_NAME(r, num) {.name = r, .dwarfnum = num}
 #define GPR_DWARFNUM_NAME(num) \
-	{.name = STR(%x##num), .dwarfnum = num}
+	{.name = __stringify(%x##num), .dwarfnum = num}
 #define REG_DWARFNUM_END {.name = NULL, .dwarfnum = 0}
 
 /*
