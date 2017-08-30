2ef2cf438043cc0a646823c1592d12642f7e2a5c
Prarit Bhargava <prarit@redhat.com>
2017-08-30 19:01:36 -0400

    [x86] Enable Intel Turbo Boost Max Technology 3.0
    Message-id: <1504119829-27117-48-git-send-email-prarit@redhat.com>
    Patchwork-id: 183750
    O-Subject: [RHEL7.5 BZ 1465349 047/179 v2] x86: Enable Intel Turbo Boost Max Technology 3.0
    Bugzilla: 1465349
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1465349
    
    Note: CONFIG_SCHED_ITMT is enabled by default on x86_64.  There is no
    need for a redhat/configs change.  Some minor drift conflicts between
    RHEL7 and upstream.
    
    commit 5e76b2ab36b40ca33023e78725bdc69eafd63134
    Author: Tim Chen <tim.c.chen@linux.intel.com>
    Date:   Tue Nov 22 12:23:55 2016 -0800
    
        x86: Enable Intel Turbo Boost Max Technology 3.0
    
        On platforms supporting Intel Turbo Boost Max Technology 3.0, the maximum
        turbo frequencies of some cores in a CPU package may be higher than for
        the other cores in the same package.  In that case, better performance
        (and possibly lower energy consumption as well) can be achieved by
        making the scheduler prefer to run tasks on the CPUs with higher max
        turbo frequencies.
    
        To that end, set up a core priority metric to abstract the core
        preferences based on the maximum turbo frequency.  In that metric,
        the cores with higher maximum turbo frequencies are higher-priority
        than the other cores in the same package and that causes the scheduler
        to favor them when making load-balancing decisions using the asymmertic
        packing approach.  At the same time, the priority of SMT threads with a
        higher CPU number is reduced so as to avoid scheduling tasks on all of
        the threads that belong to a favored core before all of the other cores
        have been given a task to run.
    
        The priority metric will be initialized by the P-state driver with the
        help of the sched_set_itmt_core_prio() function.  The P-state driver
        will also determine whether or not ITMT is supported by the platform
        and will call sched_set_itmt_support() to indicate that.
    
        Co-developed-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Co-developed-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
        Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
        Cc: linux-pm@vger.kernel.org
        Cc: peterz@infradead.org
        Cc: jolsa@redhat.com
        Cc: rjw@rjwysocki.net
        Cc: linux-acpi@vger.kernel.org
        Cc: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
        Cc: bp@suse.de
        Link: http://lkml.kernel.org/r/cd401ccdff88f88c8349314febdc25d51f7c48f7.1479844244.git.tim.c.chen@linux.intel.com
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Bhupesh Sharma <bhsharma@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Rui Wang <ruwang@redhat.com>
    Cc: Xunlei Pang <xlpang@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Rafael Aquini <aquini@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/include/asm/topology.h
+++ arch/x86/include/asm/topology.h
@@ -148,6 +148,33 @@ struct pci_bus;
 int x86_pci_root_bus_node(int bus);
 void x86_pci_root_bus_resources(int bus, struct list_head *resources);
 
+#ifdef CONFIG_SCHED_ITMT
+#include <asm/percpu.h>
+
+DECLARE_PER_CPU_READ_MOSTLY(int, sched_core_priority);
+
+/* Interface to set priority of a cpu */
+void sched_set_itmt_core_prio(int prio, int core_cpu);
+
+/* Interface to notify scheduler that system supports ITMT */
+void sched_set_itmt_support(void);
+
+/* Interface to notify scheduler that system revokes ITMT support */
+void sched_clear_itmt_support(void);
+
+#else /* CONFIG_SCHED_ITMT */
+
+static inline void sched_set_itmt_core_prio(int prio, int core_cpu)
+{
+}
+static inline void sched_set_itmt_support(void)
+{
+}
+static inline void sched_clear_itmt_support(void)
+{
+}
+#endif /* CONFIG_SCHED_ITMT */
+
 #ifdef CONFIG_SMP
 #define mc_capable()	((boot_cpu_data.x86_max_cores > 1) && \
 			(cpumask_weight(cpu_core_mask(0)) != nr_cpu_ids))
