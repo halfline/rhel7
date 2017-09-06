f4cef6da360a59dae93ad99fccd40445b2a071d4
David Arcari <darcari@redhat.com>
2017-09-06 18:33:25 -0400

    [i2c] i801: Cleanup Intel Kaby Lake support
    Message-id: <1504722809-80802-2-git-send-email-darcari@redhat.com>
    Patchwork-id: 184099
    O-Subject: [RHEL7.5 PATCH BZ 1457647 1/5] i2c: i801: Cleanup Intel Kaby Lake support
    Bugzilla: 1457647
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1457647
    Upstream: RHEL-Only
    
    Due to time constraints, the following RHEL patch was committed when the
    associated upstream patch was first posted:
    
    fab6d8acb66f ("[i2c] i801: Add support for Kaby Lake PCH-H")
    
    This patch modifies the affected code to match the eventual upstream commit:
    
    31158763ef2d ("i2c: i801: Add support for Kaby Lake PCH-H")
    
    This allows for easier backports going forward.
    
    There is no functional change.
    
    Cc: David Arcari <darcari@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/i2c/busses/i2c-i801.c
+++ drivers/i2c/busses/i2c-i801.c
@@ -62,7 +62,8 @@
  * DNV (SOC)			0x19df	32	hard	yes	yes	yes
  * Lewisburg (PCH)       	0xa1a3	32	hard	yes	yes	yes
  * Lewisburg Supersku (PCH)   	0xa223	32	hard	yes	yes	yes
- * 
+ * Kaby Lake PCH-H (PCH)	0xa2a3	32	hard	yes	yes	yes
+ *
  * Features supported by this driver:
  * Software PEC				no
  * Hardware PEC				yes
@@ -206,7 +207,7 @@
 #define PCI_DEVICE_ID_INTEL_DNV_SMBUS			0x19df
 #define PCI_DEVICE_ID_INTEL_LEWISBURG_SMBUS		0xa1a3
 #define PCI_DEVICE_ID_INTEL_LEWISBURG_SSKU_SMBUS	0xa223
-#define PCI_DEVICE_ID_INTEL_KABYPOINT_H_SMBUS		0xa2a3
+#define PCI_DEVICE_ID_INTEL_KABYLAKE_PCH_H_SMBUS	0xa2a3
 
 struct i801_mux_config {
 	char *gpio_chip;
@@ -895,7 +896,7 @@ static const struct pci_device_id i801_ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_DNV_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_LEWISBURG_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_LEWISBURG_SSKU_SMBUS) },
-	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_KABYPOINT_H_SMBUS) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_KABYLAKE_PCH_H_SMBUS) },
 	{ 0, }
 };
 
@@ -1374,7 +1375,7 @@ static int i801_probe(struct pci_dev *dev, const struct pci_device_id *id)
 	case PCI_DEVICE_ID_INTEL_LEWISBURG_SMBUS:
 	case PCI_DEVICE_ID_INTEL_LEWISBURG_SSKU_SMBUS:
 	case PCI_DEVICE_ID_INTEL_DNV_SMBUS:
-	case PCI_DEVICE_ID_INTEL_KABYPOINT_H_SMBUS:
+	case PCI_DEVICE_ID_INTEL_KABYLAKE_PCH_H_SMBUS:
 		priv->features |= FEATURE_I2C_BLOCK_READ;
 		priv->features |= FEATURE_IRQ;
 		priv->features |= FEATURE_SMBUS_PEC;
