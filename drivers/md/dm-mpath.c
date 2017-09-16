7a9a925458ae405663c988a86f794c1a0ea0babf
Mike Snitzer <snitzer@redhat.com>
2017-09-15 22:23:24 -0400

    [md] dm mpath: delay requeuing while path initialization is in progress
    Message-id: <1505514230-31675-13-git-send-email-snitzer@redhat.com>
    Patchwork-id: 185351
    O-Subject: [RHEL7.5 PATCH 12/38] dm mpath: delay requeuing while path initialization is in progress
    Bugzilla: 1492250
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    RH-Acked-by: Mikulas Patocka <mpatocka@redhat.com>
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    
    BZ: 1492250
    
    Upstream commit c1d7ecf7ca11d0edd3085262c8597203440d056c
    Author: Bart Van Assche <bart.vanassche@sandisk.com>
    Date:   Thu Apr 27 10:11:16 2017 -0700
    
        dm mpath: delay requeuing while path initialization is in progress
    
        Requeuing a request immediately while path initialization is ongoing
        causes high CPU usage, something that is undesired.  Hence delay
        requeuing while path initialization is in progress.
    
        Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
        Reviewed-by: Hannes Reinecke <hare@suse.com>
        Cc: Christoph Hellwig <hch@lst.de>
        Cc: <stable@vger.kernel.org>
        Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/dm-mpath.c
+++ drivers/md/dm-mpath.c
@@ -376,13 +376,16 @@ static int __pg_init_all_paths(struct multipath *m)
 	return atomic_read(&m->pg_init_in_progress);
 }
 
-static void pg_init_all_paths(struct multipath *m)
+static int pg_init_all_paths(struct multipath *m)
 {
+	int ret;
 	unsigned long flags;
 
 	spin_lock_irqsave(&m->lock, flags);
-	__pg_init_all_paths(m);
+	ret = __pg_init_all_paths(m);
 	spin_unlock_irqrestore(&m->lock, flags);
+
+	return ret;
 }
 
 static void __switch_pg(struct multipath *m, struct priority_group *pg)
@@ -556,7 +559,8 @@ static int __multipath_map(struct dm_target *ti, struct request *clone,
 		return -EIO;	/* Failed */
 	} else if (test_bit(MPATHF_QUEUE_IO, &m->flags) ||
 		   test_bit(MPATHF_PG_INIT_REQUIRED, &m->flags)) {
-		pg_init_all_paths(m);
+		if (pg_init_all_paths(m))
+			return DM_MAPIO_DELAY_REQUEUE;
 		return DM_MAPIO_REQUEUE;
 	}
 
