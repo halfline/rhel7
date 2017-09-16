9d8b42882e2729bcc1615abf99c9cd793b51a20d
Mike Snitzer <snitzer@redhat.com>
2017-09-15 22:23:31 -0400

    [md] dm mpath: make it easier to detect unintended I_O request flushes
    Message-id: <1505514230-31675-20-git-send-email-snitzer@redhat.com>
    Patchwork-id: 185358
    O-Subject: [RHEL7.5 PATCH 19/38] dm mpath: make it easier to detect unintended I_O request flushes
    Bugzilla: 1492250
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    RH-Acked-by: Mikulas Patocka <mpatocka@redhat.com>
    RH-Acked-by: Joe Thornber <thornber@redhat.com>
    
    BZ: 1492250
    
    Upstream commit 86331f39a5935b092d3ea59446d416563ed05d16
    Author: Bart Van Assche <bart.vanassche@sandisk.com>
    Date:   Thu Apr 27 10:11:26 2017 -0700
    
        dm mpath: make it easier to detect unintended I/O request flushes
    
        I/O errors triggered by multipathd incorrectly not enabling the no-flush
        flag for DM_DEVICE_SUSPEND or DM_DEVICE_RESUME are hard to debug.  Add
        more logging to make it easier to debug this.
    
        Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
        Reviewed-by: Hannes Reinecke <hare@suse.com>
        Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/dm-mpath.c
+++ drivers/md/dm-mpath.c
@@ -496,6 +496,23 @@ failed:
 }
 
 /*
+ * dm_report_EIO() is a macro instead of a function to make pr_debug()
+ * report the function name and line number of the function from which
+ * it has been invoked.
+ */
+#define dm_report_EIO(m)						\
+({									\
+	struct mapped_device *md = dm_table_get_md((m)->ti->table);	\
+									\
+	pr_debug("%s: returning EIO; QIFNP = %d; SQIFNP = %d; DNFS = %d\n", \
+		 dm_device_name(md),					\
+		 test_bit(MPATHF_QUEUE_IF_NO_PATH, &(m)->flags),	\
+		 test_bit(MPATHF_SAVED_QUEUE_IF_NO_PATH, &(m)->flags),	\
+		 dm_noflush_suspending((m)->ti));			\
+	-EIO;								\
+})
+
+/*
  * Map cloned requests
  */
 static int __multipath_map(struct dm_target *ti, struct request *clone,
@@ -517,7 +534,7 @@ static int __multipath_map(struct dm_target *ti, struct request *clone,
 	if (!pgpath) {
 		if (test_bit(MPATHF_QUEUE_IF_NO_PATH, &m->flags))
 			return DM_MAPIO_DELAY_REQUEUE;
-		return -EIO;	/* Failed */
+		return dm_report_EIO(m);	/* Failed */
 	} else if (test_bit(MPATHF_QUEUE_IO, &m->flags) ||
 		   test_bit(MPATHF_PG_INIT_REQUIRED, &m->flags)) {
 		if (pg_init_all_paths(m))
@@ -629,7 +646,7 @@ static int __multipath_map_bio(struct multipath *m, struct bio *bio, struct dm_m
 	if (!pgpath) {
 		if (test_bit(MPATHF_QUEUE_IF_NO_PATH, &m->flags))
 			return DM_MAPIO_REQUEUE;
-		return -EIO;
+		return dm_report_EIO(m);
 	}
 
 	mpio->pgpath = pgpath;
@@ -1575,7 +1592,7 @@ static int do_end_io(struct multipath *m, struct request *clone,
 
 	if (atomic_read(&m->nr_valid_paths) == 0 &&
 	    !test_bit(MPATHF_QUEUE_IF_NO_PATH, &m->flags))
-		r = -EIO;
+		r = dm_report_EIO(m);
 
 	return r;
 }
@@ -1619,7 +1636,7 @@ static int do_end_io_bio(struct multipath *m, struct bio *clone,
 
 	if (atomic_read(&m->nr_valid_paths) == 0 &&
 	    !test_bit(MPATHF_QUEUE_IF_NO_PATH, &m->flags))
-		return -EIO;
+		return dm_report_EIO(m);
 
 	/* Queue for the daemon to resubmit */
 	dm_bio_restore(get_bio_details_from_bio(clone), clone);
