3e81a7e6e7fbdf3537154d88527bf2cd526c58da
Arjun Vynipadath <avynipad@redhat.com>
2017-09-18 16:24:07 -0400

    [netdrv] cxgb4: Fix missing initialization of win0_lock
    Message-id: <1505751877-18598-2-git-send-email-avynipad@redhat.com>
    Patchwork-id: 185496
    O-Subject: [RHEL7.5 PATCHv2 BZ 1458296 01/31] cxgb4 : Fix missing initialization of win0_lock
    Bugzilla: 1458296
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1458296
    
    commit e327c225c911529898ec300cb96d2088893de3df
    Author: Anish Bhatt <anish@chelsio.com>
    Date:   Wed Oct 29 17:54:03 2014 -0700
    
        cxgb4 : Fix missing initialization of win0_lock
    
        win0_lock was being used un-initialized, resulting in warning traces
        being seen when lock debugging is enabled (and just wrong)
    
        Fixes : fc5ab0209650 ('cxgb4: Replaced the backdoor mechanism to access the HW
         memory with PCIe Window method')
    
        Signed-off-by: Anish Bhatt <anish@chelsio.com>
        Signed-off-by: Casey Leedom <leedom@chelsio.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Cc: Sai Vemuri <svemuri@redhat.com>
    Cc: Don Dutile <ddutile@redhat.com>
    Cc: Arjun Vynipadath <avynipad@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
+++ drivers/net/ethernet/chelsio/cxgb4/cxgb4_main.c
@@ -4784,6 +4784,7 @@ static int init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 	spin_lock_init(&adapter->stats_lock);
 	spin_lock_init(&adapter->tid_release_lock);
 	spin_lock_init(&adapter->mbox_lock);
+	spin_lock_init(&adapter->win0_lock);
 
 	INIT_LIST_HEAD(&adapter->mlist.list);
 
