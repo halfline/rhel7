2e75f93f136aa0a9d8b6d038ce6bf54c21f67a36
Arjun Vynipadath <avynipad@redhat.com>
2017-09-19 09:05:43 -0400

    [scsi] csiostor: Remove T4 FCoE Support
    Message-id: <1505811955-982-6-git-send-email-avynipad@redhat.com>
    Patchwork-id: 185571
    O-Subject: [RHEL7.5 PATCH BZ 1458318 05/17] csiostor:Remove T4 FCoE Support.
    Bugzilla: 1458318
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1458318
    
    commit 3fb4c22eaaf04b60d8a55f2c6bfa28314ced9df6
    Author: Praveen Madhavan <praveenm@chelsio.com>
    Date:   Fri Jan 16 21:30:19 2015 +0530
    
        csiostor:Remove T4 FCoE Support.
    
        We found a subtle issue with FCoE on T4 very late in the game
        and decided not to productize FCoE on T4 and therefore there
        are no customers that will be impacted by this change. Hence
        T4 FCoE support is removed. FCoE supported only on T5 cards.
    
        changes in v2:
          - Make the commit message more clearer.
    
        Signed-off-by: Praveen Madhavan <praveenm@chelsio.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Cc: David Milburn <dmilburn@redhat.com>
    Cc: Arjun Vynipadath <avynipad@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/csiostor/csio_init.c
+++ drivers/scsi/csiostor/csio_init.c
@@ -1178,9 +1178,8 @@ static struct pci_error_handlers csio_err_handler = {
  */
 #define CH_PCI_DEVICE_ID_TABLE_DEFINE_BEGIN \
 	static const struct pci_device_id csio_pci_tbl[] = {
-/* Define for iSCSI uses PF5, FCoE uses PF6 */
-#define CH_PCI_DEVICE_ID_FUNCTION	0x5
-#define CH_PCI_DEVICE_ID_FUNCTION2	0x6
+/* Define for FCoE uses PF6 */
+#define CH_PCI_DEVICE_ID_FUNCTION	0x6
 
 #define CH_PCI_ID_TABLE_ENTRY(devid) \
 		{ PCI_VDEVICE(CHELSIO, (devid)), 0 }
@@ -1258,5 +1257,4 @@ MODULE_DESCRIPTION(CSIO_DRV_DESC);
 MODULE_LICENSE(CSIO_DRV_LICENSE);
 MODULE_DEVICE_TABLE(pci, csio_pci_tbl);
 MODULE_VERSION(CSIO_DRV_VERSION);
-MODULE_FIRMWARE(FW_FNAME_T4);
 MODULE_FIRMWARE(FW_FNAME_T5);
