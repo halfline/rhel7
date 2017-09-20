torvalds/linux

commit 111b8b3fe4fae35d4a85e9f524b077f5c4951e65

cpufreq: intel_pstate: Always keep all limits settings in sync
Make intel_pstate update per-logical-CPU limits when the global
settings are changed to ensure that they are always in sync and
users will not see confusing values in per-logical-CPU sysfs
attributes.

This also fixes the problem that setting the "no_turbo" global
attribute to 1 in the "passive" mode (ie. when intel_pstate acts
as a regular cpufreq driver) when scaling_governor is set to
"performance" has no effect.

Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

drivers/cpufreq/intel_pstate.c
@@ -803,13 +803,13 @@ static struct freq_attr *hwp_cpufreq_attrs[] = {
 	NULL,
 };
 
-static void intel_pstate_hwp_set(const struct cpumask *cpumask)
+static void intel_pstate_hwp_set(struct cpufreq_policy *policy)
 {
 	int min, hw_min, max, hw_max, cpu, range, adj_range;
 	struct perf_limits *perf_limits = limits;
 	u64 value, cap;
 
-	for_each_cpu(cpu, cpumask) {
+	for_each_cpu(cpu, policy->cpus) {
 		int max_perf_pct, min_perf_pct;
 		struct cpudata *cpu_data = all_cpu_data[cpu];
 		s16 epp;
@@ -895,7 +895,7 @@ skip_epp:
 static int intel_pstate_hwp_set_policy(struct cpufreq_policy *policy)
 {
 	if (hwp_active)
-		intel_pstate_hwp_set(policy->cpus);
+		intel_pstate_hwp_set(policy);
 
 	return 0;
 }
@@ -930,11 +930,12 @@ static int intel_pstate_resume(struct cpufreq_policy *policy)
 	return ret;
 }
 
-static void intel_pstate_hwp_set_online_cpus(void)
+static void intel_pstate_update_policies(void)
 {
-	get_online_cpus();
-	intel_pstate_hwp_set(cpu_online_mask);
-	put_online_cpus();
+	int cpu;
+
+	for_each_possible_cpu(cpu)
+		cpufreq_update_policy(cpu);
 }
 
 /************************** debugfs begin ************************/
@@ -1055,10 +1056,9 @@ static ssize_t store_no_turbo(struct kobject *a, struct attribute *b,
 
 	limits->no_turbo = clamp_t(int, input, 0, 1);
 
-	if (hwp_active)
-		intel_pstate_hwp_set_online_cpus();
-
 	mutex_unlock(&intel_pstate_limits_lock);
+
+	intel_pstate_update_policies();
 
 	return count;
 }
@@ -1084,10 +1084,9 @@ static ssize_t store_max_perf_pct(struct kobject *a, struct attribute *b,
 				   limits->max_perf_pct);
 	limits->max_perf = div_ext_fp(limits->max_perf_pct, 100);
 
-	if (hwp_active)
-		intel_pstate_hwp_set_online_cpus();
-
 	mutex_unlock(&intel_pstate_limits_lock);
+
+	intel_pstate_update_policies();
 
 	return count;
 }
@@ -1113,10 +1112,9 @@ static ssize_t store_min_perf_pct(struct kobject *a, struct attribute *b,
 				   limits->min_perf_pct);
 	limits->min_perf = div_ext_fp(limits->min_perf_pct, 100);
 
-	if (hwp_active)
-		intel_pstate_hwp_set_online_cpus();
-
 	mutex_unlock(&intel_pstate_limits_lock);
+
+	intel_pstate_update_policies();
 
 	return count;
 }

halfline/rhel7

commit 085277f1bf26bdf18eb45e3865768a7522422ca4

[cpufreq] intel_pstate: Fix global settings in active mode
Message-id: <1504119829-27117-148-git-send-email-prarit@redhat.com>
Patchwork-id: 183846
O-Subject: [RHEL7.5 BZ 1465349 147/179 v2] cpufreq: intel_pstate: Fix global settings in active mode
Bugzilla: 1465349
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: David Arcari <darcari@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1465349

commit cd59b4bed9d11a2aefc4bb44eed9de0e6c1eea06
Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Date:   Wed Mar 1 00:07:36 2017 +0100

    cpufreq: intel_pstate: Fix global settings in active mode

    Commit 111b8b3fe4fa (cpufreq: intel_pstate: Always keep all
    limits settings in sync) changed intel_pstate to invoke
    cpufreq_update_policy() for every registered CPU on global sysfs
    attributes updates, but that led to undesirable effects in the
    active mode if the "performance" P-state selection algorithm is
    configufred for one CPU and the "powersave" one is chosen for
    all of the other CPUs.

    Namely, in that case, the following is possible:

     # cd /sys/devices/system/cpu/
     # cat intel_pstate/max_perf_pct
     100
     # cat intel_pstate/min_perf_pct
     26
     # echo performance > cpufreq/policy0/scaling_governor
     # cat intel_pstate/max_perf_pct
     100
     # cat intel_pstate/min_perf_pct
     100
     # echo 94 > intel_pstate/min_perf_pct
     # cat intel_pstate/min_perf_pct
     26

    The reason why this happens is because intel_pstate attempts to
    maintain two sets of global limits in the active mode, one for
    the "performance" P-state selection algorithm and one for the
    "powersave"  P-state selection algorithm, but the P-state selection
    algorithms are set per policy, so the global limits cannot reflect
    all of them at the same time if they are different for different
    policies.

    In the particular situation above, the attempt to change
    min_perf_pct to 94 caused cpufreq_update_policy() to be run
    for a CPU with the "powersave"  P-state selection algorithm
    and intel_pstate_set_policy() called by it silently switched the
    global limits to the "powersave" set which finally was reflected
    by the sysfs interface.

    To prevent that from happening, modify intel_pstate_update_policies()
    to always switch back to the set of limits that was used right before
    it has been invoked.

    Fixes: 111b8b3fe4fa (cpufreq: intel_pstate: Always keep all limits settings in sync)

drivers/cpufreq/intel_pstate.c
@@ -969,11 +969,20 @@ static int intel_pstate_resume(struct cpufreq_policy *policy)
 }
 
 static void intel_pstate_update_policies(void)
+	__releases(&intel_pstate_limits_lock)
+	__acquires(&intel_pstate_limits_lock)
 {
+	struct perf_limits *saved_limits = limits;
 	int cpu;
+
+	mutex_unlock(&intel_pstate_limits_lock);
 
 	for_each_possible_cpu(cpu)
 		cpufreq_update_policy(cpu);
+
+	mutex_lock(&intel_pstate_limits_lock);
+
+	limits = saved_limits;
 }
 
 /************************** debugfs begin ************************/
@@ -1181,9 +1190,9 @@ static ssize_t store_no_turbo(struct kobject *a, struct attribute *b,
 
 	limits->no_turbo = clamp_t(int, input, 0, 1);
 
-	mutex_unlock(&intel_pstate_limits_lock);
-
 	intel_pstate_update_policies();
+
+	mutex_unlock(&intel_pstate_limits_lock);
 
 	mutex_unlock(&intel_pstate_driver_lock);
 
@@ -1218,9 +1227,9 @@ static ssize_t store_max_perf_pct(struct kobject *a, struct attribute *b,
 				   limits->max_perf_pct);
 	limits->max_perf = div_ext_fp(limits->max_perf_pct, 100);
 
-	mutex_unlock(&intel_pstate_limits_lock);
-
 	intel_pstate_update_policies();
+
+	mutex_unlock(&intel_pstate_limits_lock);
 
 	mutex_unlock(&intel_pstate_driver_lock);
 
@@ -1255,9 +1264,9 @@ static ssize_t store_min_perf_pct(struct kobject *a, struct attribute *b,
 				   limits->min_perf_pct);
 	limits->min_perf = div_ext_fp(limits->min_perf_pct, 100);
 
-	mutex_unlock(&intel_pstate_limits_lock);
-
 	intel_pstate_update_policies();
+
+	mutex_unlock(&intel_pstate_limits_lock);
 
 	mutex_unlock(&intel_pstate_driver_lock);
 