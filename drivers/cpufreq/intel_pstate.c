torvalds/linux

commit 1443ebbacfd7f8d84fbbbf629ef66a12dc8a4b4e

cpufreq: intel_pstate: Fix sysfs limits enforcement for performance policy
A side effect of keeping intel_pstate sysfs limits in sync with cpufreq
is that the now sysfs limits can't enforced under performance policy.

For example, if the max_perf_pct is changed from 100 to 80, this will call
intel_pstate_set_policy(), which will change the max_perf to 100 again for
performance policy. Same issue happens, when no_turbo is set.

This change calculates max and min frequency using sysfs performance
limits in intel_pstate_verify_policy() and adjusts policy limits by
calling cpufreq_verify_within_limits().

Also, it causes the setting of performance limits to be skipped if
no_turbo is set.

Fixes: 111b8b3fe4fa (cpufreq: intel_pstate: Always keep all limits settings in sync)
Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

drivers/cpufreq/intel_pstate.c
@@ -1951,7 +1951,8 @@ static int intel_pstate_set_policy(struct cpufreq_policy *policy)
 			limits = &performance_limits;
 			perf_limits = limits;
 		}
-		if (policy->max >= policy->cpuinfo.max_freq) {
+		if (policy->max >= policy->cpuinfo.max_freq &&
+		    !limits->no_turbo) {
 			pr_debug("set performance\n");
 			intel_pstate_set_performance_limits(perf_limits);
 			goto out;
@@ -1992,6 +1993,17 @@ static int intel_pstate_verify_policy(struct cpufreq_policy *policy)
 	if (policy->policy != CPUFREQ_POLICY_POWERSAVE &&
 	    policy->policy != CPUFREQ_POLICY_PERFORMANCE)
 		return -EINVAL;
+
+	/* When per-CPU limits are used, sysfs limits are not used */
+	if (!per_cpu_limits) {
+		unsigned int max_freq, min_freq;
+
+		max_freq = policy->cpuinfo.max_freq *
+						limits->max_sysfs_pct / 100;
+		min_freq = policy->cpuinfo.max_freq *
+						limits->min_sysfs_pct / 100;
+		cpufreq_verify_within_limits(policy, min_freq, max_freq);
+	}
 
 	return 0;
 }

halfline/rhel7

commit bb577e4676d2fcc5d043aeec7071782437012de3

[cpufreq] intel_pstate: Fix intel_pstate_verify_policy()
Message-id: <1504119829-27117-149-git-send-email-prarit@redhat.com>
Patchwork-id: 183849
O-Subject: [RHEL7.5 BZ 1465349 148/179 v2] cpufreq: intel_pstate: Fix intel_pstate_verify_policy()
Bugzilla: 1465349
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: David Arcari <darcari@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1465349

commit d74b19929130c9b5395a497030dcf161353cd526
Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Date:   Wed Mar 1 00:11:05 2017 +0100

    cpufreq: intel_pstate: Fix intel_pstate_verify_policy()

    The code added to intel_pstate_verify_policy() by commit 1443ebbacfd7
    (cpufreq: intel_pstate: Fix sysfs limits enforcement for performance
    policy) should use perf_limits instead of limits, because otherwise
    setting global limits via sysfs may affect policies inconsistently.

    For example, in the sequence of shell commands below, the
    scaling_min_freq attribute for policy1 and policy2 should be
    affected in the same way, because scaling_governor is set in
    the same way for both of them:

     # cat cpufreq/policy1/scaling_governor
     powersave
     # cat cpufreq/policy2/scaling_governor
     powersave
     # echo performance > cpufreq/policy0/scaling_governor
     # echo 94 > intel_pstate/min_perf_pct
     # cat cpufreq/policy0/scaling_min_freq
     2914000
     # cat cpufreq/policy1/scaling_min_freq
     2914000
     # cat cpufreq/policy2/scaling_min_freq
     800000

    The are affected differently, because intel_pstate_verify_policy()
    is invoked with limits set to &performance_limits (left behind by
    policy0) for policy1 and with limits set to &powersave_limits (left
    behind by policy1) for policy2.  Since perf_limits is set to the
    set of limits matching the policy being updated, using it instead
    of limits fixes the inconsistency.

    Fixes: 1443ebbacfd7 (cpufreq: intel_pstate: Fix sysfs limits enforcement for performance policy)

drivers/cpufreq/intel_pstate.c
@@ -2135,9 +2135,9 @@ static int intel_pstate_verify_policy(struct cpufreq_policy *policy)
 		unsigned int max_freq, min_freq;
 
 		max_freq = policy->cpuinfo.max_freq *
-						limits->max_sysfs_pct / 100;
+					perf_limits->max_sysfs_pct / 100;
 		min_freq = policy->cpuinfo.max_freq *
-						limits->min_sysfs_pct / 100;
+					perf_limits->min_sysfs_pct / 100;
 		cpufreq_verify_within_limits(policy, min_freq, max_freq);
 	}
 