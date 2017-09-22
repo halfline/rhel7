3fce80e3c5cd4d210e1337c1bcbcc2ce9e6f54cc
Ming Lei <ming.lei@redhat.com>
2017-09-22 01:42:59 -0400

    [kernel] pci: Use positive flags in pci_alloc_irq_vectors()
    Message-id: <20170922014340.22906-15-ming.lei@redhat.com>
    Patchwork-id: 186206
    O-Subject: [RHEL 7.5 v3 14/55] PCI: Use positive flags in pci_alloc_irq_vectors()
    Bugzilla: 1389540
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    BZ: 1389540
    
    commit 4fe0d154880bb6eb833cbe84fa6f385f400f0b9c
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Thu Aug 11 07:11:04 2016 -0700
    
        PCI: Use positive flags in pci_alloc_irq_vectors()
    
        Instead of passing negative flags like PCI_IRQ_NOMSI to prevent use of
        certain interrupt types, pass positive flags like PCI_IRQ_LEGACY,
        PCI_IRQ_MSI, etc., to specify the acceptable interrupt types.
    
        This is based on a number of pending driver conversions that just happend
        to be a whole more obvious to read this way, and given that we have no
        users in the tree yet it can still easily be done.
    
        I've also added a PCI_IRQ_ALL_TYPES catchall to keep the case of accepting
        all interrupt types very simple.
    
        [bhelgaas: changelog, fix PCI_IRQ_AFFINITY doc typo, remove mention of
        PCI_IRQ_NOLEGACY]
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Reviewed-by: Alexander Gordeev <agordeev@redhat.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -1115,7 +1115,7 @@ static int __pci_enable_msi_range(struct pci_dev *dev, int minvec, int maxvec,
 		nvec = maxvec;
 
 	for (;;) {
-		if (!(flags & PCI_IRQ_NOAFFINITY)) {
+		if (flags & PCI_IRQ_AFFINITY) {
 			dev->irq_affinity = irq_create_affinity_mask(&nvec);
 			if (nvec < minvec)
 				return -ENOSPC;
@@ -1151,7 +1151,7 @@ static int __pci_enable_msi_range(struct pci_dev *dev, int minvec, int maxvec,
  **/
 int pci_enable_msi_range(struct pci_dev *dev, int minvec, int maxvec)
 {
-	return __pci_enable_msi_range(dev, minvec, maxvec, PCI_IRQ_NOAFFINITY);
+	return __pci_enable_msi_range(dev, minvec, maxvec, 0);
 }
 EXPORT_SYMBOL(pci_enable_msi_range);
 
@@ -1166,7 +1166,7 @@ static int __pci_enable_msix_range(struct pci_dev *dev,
 		return -ERANGE;
 
 	for (;;) {
-		if (!(flags & PCI_IRQ_NOAFFINITY)) {
+		if (flags & PCI_IRQ_AFFINITY) {
 			dev->irq_affinity = irq_create_affinity_mask(&nvec);
 			if (nvec < minvec)
 				return -ENOSPC;
@@ -1206,8 +1206,7 @@ static int __pci_enable_msix_range(struct pci_dev *dev,
 int pci_enable_msix_range(struct pci_dev *dev, struct msix_entry *entries,
 		int minvec, int maxvec)
 {
-	return __pci_enable_msix_range(dev, entries, minvec, maxvec,
-			PCI_IRQ_NOAFFINITY);
+	return __pci_enable_msix_range(dev, entries, minvec, maxvec, 0);
 }
 EXPORT_SYMBOL(pci_enable_msix_range);
 
@@ -1233,21 +1232,21 @@ int pci_alloc_irq_vectors(struct pci_dev *dev, unsigned int min_vecs,
 {
 	int vecs = -ENOSPC;
 
-	if (!(flags & PCI_IRQ_NOMSIX)) {
+	if (flags & PCI_IRQ_MSIX) {
 		vecs = __pci_enable_msix_range(dev, NULL, min_vecs, max_vecs,
 				flags);
 		if (vecs > 0)
 			return vecs;
 	}
 
-	if (!(flags & PCI_IRQ_NOMSI)) {
+	if (flags & PCI_IRQ_MSI) {
 		vecs = __pci_enable_msi_range(dev, min_vecs, max_vecs, flags);
 		if (vecs > 0)
 			return vecs;
 	}
 
 	/* use legacy irq if allowed */
-	if (!(flags & PCI_IRQ_NOLEGACY) && min_vecs == 1)
+	if ((flags & PCI_IRQ_LEGACY) && min_vecs == 1)
 		return 1;
 	return vecs;
 }
