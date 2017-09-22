4765dff63a5e3ae74a61da88f8644933192c0997
Ming Lei <ming.lei@redhat.com>
2017-09-22 01:43:29 -0400

    [block] blk-mq: reduce unnecessary software queue looping
    Message-id: <20170922014340.22906-45-ming.lei@redhat.com>
    Patchwork-id: 186236
    O-Subject: [RHEL 7.5 v3 44/55] blk-mq: reduce unnecessary software queue looping
    Bugzilla: 1389540 1458104
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    BZ: 1389540
    BZ: 1458104
    
    commit 889fa31f00b218a2cef96c32a6b3f57e6d3bf918
    Author: Chong Yuan <chong.yuan@memblaze.com>
    Date:   Wed Apr 15 11:39:29 2015 -0600
    
        blk-mq: reduce unnecessary software queue looping
    
        In flush_busy_ctxs() and blk_mq_hctx_has_pending(), regardless of how many
        ctxs assigned to one hctx, they will all loop hctx->ctx_map.map_size
        times. Here hctx->ctx_map.map_size is a const ALIGN(nr_cpu_ids, 8) / 8.
        Especially, flush_busy_ctxs() is in hot code path. And it's unnecessary.
        Change ->map_size to contain the actually mapped software queues, so we
        only loop for as many iterations as we have to.
    
        And remove cpumask setting and nr_ctx count in blk_mq_init_cpu_queues()
        since they are all re-done in blk_mq_map_swqueue().
        blk_mq_map_swqueue().
    
        Signed-off-by: Chong Yuan <chong.yuan@memblaze.com>
        Reviewed-by: Wenbo Wang <wenbo.wang@memblaze.com>
    
        Updated by me for formatting and commenting.
    
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1691,8 +1691,6 @@ static int blk_mq_alloc_bitmap(struct blk_mq_ctxmap *bitmap, int node)
 	if (!bitmap->map)
 		return -ENOMEM;
 
-	bitmap->map_size = num_maps;
-
 	total = nr_cpu_ids;
 	for (i = 0; i < num_maps; i++) {
 		bitmap->map[i].depth = min(total, bitmap->bits_per_word);
@@ -1889,8 +1887,6 @@ static void blk_mq_init_cpu_queues(struct request_queue *q,
 			continue;
 
 		hctx = q->mq_ops->map_queue(q, i);
-		cpumask_set_cpu(i, hctx->cpumask);
-		hctx->nr_ctx++;
 
 		/*
 		 * Set local node, IFF we have more than one hw queue. If
@@ -1953,6 +1949,8 @@ static void blk_mq_map_swqueue(struct request_queue *q,
 	mutex_unlock(&q->sysfs_lock);
 
 	queue_for_each_hw_ctx(q, hctx, i) {
+		struct blk_mq_ctxmap *map = &hctx->ctx_map;
+
 		/*
 		 * If no software queues are mapped to this hardware queue,
 		 * disable it and free the request entries.
@@ -1974,6 +1972,13 @@ static void blk_mq_map_swqueue(struct request_queue *q,
 		WARN_ON(!hctx->tags);
 
 		cpumask_copy(hctx->tags->cpumask, hctx->cpumask);
+		/*
+		 * Set the map size to the number of mapped software queues.
+		 * This is more accurate and more efficient than looping
+		 * over all possibly mapped software queues.
+		 */
+		map->map_size = hctx->nr_ctx / map->bits_per_word;
+
 		/*
 		 * Initialize batch roundrobin counts
 		 */
