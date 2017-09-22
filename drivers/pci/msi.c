c24712b72de57827c158942bf845a84ae3e73449
Ming Lei <ming.lei@redhat.com>
2017-09-22 01:42:53 -0400

    [pci] Add pci_msix_desc_addr() helper
    Message-id: <20170922014340.22906-9-ming.lei@redhat.com>
    Patchwork-id: 186200
    O-Subject: [RHEL 7.5 v3 08/55] PCI: Add pci_msix_desc_addr() helper
    Bugzilla: 1389540
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    BZ: 1389540
    
    commit 5eb6d660193ccc471b415d6f31e17312a63de167
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Tue Jul 12 18:20:14 2016 +0900
    
        PCI: Add pci_msix_desc_addr() helper
    
        Add a pci_msix_desc_addr() helper to factor out the calculation of the base
        address for a given MSI-X vector.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Reviewed-by: Alexander Gordeev <agordeev@redhat.com>
    
    (backported from 5eb6d660193c)
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -171,6 +171,12 @@ static void msi_mask_irq(struct msi_desc *desc, u32 mask, u32 flag)
 	desc->masked = arch_msi_mask_irq(desc, mask, flag);
 }
 
+static void __iomem *pci_msix_desc_addr(struct msi_desc *desc)
+{
+	return desc->mask_base +
+		desc->msi_attrib.entry_nr * PCI_MSIX_ENTRY_SIZE;
+}
+
 /*
  * This internal function does not flush PCI writes to the device.
  * All users must ensure that they read from the device before either
@@ -181,12 +187,11 @@ static void msi_mask_irq(struct msi_desc *desc, u32 mask, u32 flag)
 u32 default_msix_mask_irq(struct msi_desc *desc, u32 flag)
 {
 	u32 mask_bits = desc->masked;
-	unsigned offset = desc->msi_attrib.entry_nr * PCI_MSIX_ENTRY_SIZE +
-						PCI_MSIX_ENTRY_VECTOR_CTRL;
+
 	mask_bits &= ~PCI_MSIX_ENTRY_CTRL_MASKBIT;
 	if (flag)
 		mask_bits |= PCI_MSIX_ENTRY_CTRL_MASKBIT;
-	writel(mask_bits, desc->mask_base + offset);
+	writel(mask_bits, pci_msix_desc_addr(desc) + PCI_MSIX_ENTRY_VECTOR_CTRL);
 
 	return mask_bits;
 }
@@ -241,8 +246,7 @@ void __read_msi_msg(struct msi_desc *entry, struct msi_msg *msg)
 	BUG_ON(entry->dev->current_state != PCI_D0);
 
 	if (entry->msi_attrib.is_msix) {
-		void __iomem *base = entry->mask_base +
-			entry->msi_attrib.entry_nr * PCI_MSIX_ENTRY_SIZE;
+		void __iomem *base = pci_msix_desc_addr(entry);
 
 		msg->address_lo = readl(base + PCI_MSIX_ENTRY_LOWER_ADDR);
 		msg->address_hi = readl(base + PCI_MSIX_ENTRY_UPPER_ADDR);
@@ -296,9 +300,7 @@ void __write_msi_msg(struct msi_desc *entry, struct msi_msg *msg)
 	if (entry->dev->current_state != PCI_D0) {
 		/* Don't touch the hardware now */
 	} else if (entry->msi_attrib.is_msix) {
-		void __iomem *base;
-		base = entry->mask_base +
-			entry->msi_attrib.entry_nr * PCI_MSIX_ENTRY_SIZE;
+		void __iomem *base = pci_msix_desc_addr(entry);
 
 		writel(msg->address_lo, base + PCI_MSIX_ENTRY_LOWER_ADDR);
 		writel(msg->address_hi, base + PCI_MSIX_ENTRY_UPPER_ADDR);
