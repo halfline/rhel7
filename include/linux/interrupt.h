d9a89cd76aeffe5839d65ac2e6b81d2a64845ffd
Ming Lei <ming.lei@redhat.com>
2017-09-22 01:42:52 -0400

    [kernel] genirq: Add a helper to spread an affinity mask for MSI/MSI-X vectors
    Message-id: <20170922014340.22906-8-ming.lei@redhat.com>
    Patchwork-id: 186199
    O-Subject: [RHEL 7.5 v3 07/55] genirq: Add a helper to spread an affinity mask for MSI/MSI-X vectors
    Bugzilla: 1389540
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    BZ: 1389540
    
    commit 5e385a6ef31fbbf2acbda770aecc2bc2ff933d17
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Mon Jul 4 17:39:27 2016 +0900
    
        genirq: Add a helper to spread an affinity mask for MSI/MSI-X vectors
    
        This is lifted from the blk-mq code and adopted to use the affinity mask
        concept just introduced in the irq handling code.  It tries to keep the
        algorithm the same as the one current used by blk-mq, but improvements
        like assining vectors on a per-node basis instead of just per sibling
        are possible with this simple move and refactoring.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Cc: linux-block@vger.kernel.org
        Cc: linux-pci@vger.kernel.org
        Cc: linux-nvme@lists.infradead.org
        Cc: axboe@fb.com
        Cc: agordeev@redhat.com
        Link: http://lkml.kernel.org/r/1467621574-8277-7-git-send-email-hch@lst.de
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    (backport from 5e385a6ef31f)
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -299,6 +299,8 @@ struct irq_affinity_notify {
 extern int
 irq_set_affinity_notifier(unsigned int irq, struct irq_affinity_notify *notify);
 
+struct cpumask *irq_create_affinity_mask(unsigned int *nr_vecs);
+
 #else /* CONFIG_SMP */
 
 static inline int irq_set_affinity(unsigned int irq, const struct cpumask *m)
@@ -317,6 +319,12 @@ static inline int irq_set_affinity_hint(unsigned int irq,
 					const struct cpumask *m)
 {
 	return -EINVAL;
+}
+
+static inline struct cpumask *irq_create_affinity_mask(unsigned int *nr_vecs)
+{
+	*nr_vecs = 1;
+	return NULL;
 }
 #endif /* CONFIG_SMP && CONFIG_GENERIC_HARDIRQS */
 
