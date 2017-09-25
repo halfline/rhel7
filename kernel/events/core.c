f6d296556b9854117e7c047d71b542a899cb8033
Jiri Olsa <jolsa@redhat.com>
2017-09-25 12:20:02 -0400

    [x86] perf/cqm: Wipe out perf based cqm
    Message-id: <20170925122043.19483-2-jolsa@redhat.com>
    Patchwork-id: 186431
    O-Subject: [PATCH RHEL7.5 01/42] x86/perf/cqm: Wipe out perf based cqm
    Bugzilla: 1457533
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1457533
    https://bugzilla.redhat.com/show_bug.cgi?id=1457533
    
    upstream
    ========
    commit c39a0e2c8850f08249383f2425dbd8dbe4baad69
    Author: Vikas Shivappa <vikas.shivappa@linux.intel.com>
    Date: Tue Jul 25 14:14:20 2017 -0700
    
    RHEL7
    =====
    Small conflicts in core.c (different context) due to untaken BPF changes.
    Plus KABI related conflicts related to removing KABI fields.
    
    description
    ===========
    'perf cqm' never worked due to the incompatibility between perf
    infrastructure and cqm hardware support.  The hardware uses RMIDs to
    track the llc occupancy of tasks and these RMIDs are per package. This
    makes monitoring a hierarchy like cgroup along with monitoring of tasks
    separately difficult and several patches sent to lkml to fix them were
    NACKed. Further more, the following issues in the current perf cqm make
    it almost unusable:
    
        1. No support to monitor the same group of tasks for which we do
        allocation using resctrl.
    
        2. It gives random and inaccurate data (mostly 0s) once we run out
        of RMIDs due to issues in Recycling.
    
        3. Recycling results in inaccuracy of data because we cannot
        guarantee that the RMID was stolen from a task when it was not
        pulling data into cache or even when it pulled the least data. Also
        for monitoring llc_occupancy, if we stop using an RMID_x and then
        start using an RMID_y after we reclaim an RMID from an other event,
        we miss accounting all the occupancy that was tagged to RMID_x at a
        later perf_count.
    
        2. Recycling code makes the monitoring code complex including
        scheduling because the event can lose RMID any time. Since MBM
        counters count bandwidth for a period of time by taking snap shot of
        total bytes at two different times, recycling complicates the way we
        count MBM in a hierarchy. Also we need a spin lock while we do the
        processing to account for MBM counter overflow. We also currently
        use a spin lock in scheduling to prevent the RMID from being taken
        away.
    
        4. Lack of support when we run different kind of event like task,
        system-wide and cgroup events together. Data mostly prints 0s. This
        is also because we can have only one RMID tied to a cpu as defined
        by the cqm hardware but a perf can at the same time tie multiple
        events during one sched_in.
    
        5. No support of monitoring a group of tasks. There is partial support
        for cgroup but it does not work once there is a hierarchy of cgroups
        or if we want to monitor a task in a cgroup and the cgroup itself.
    
        6. No support for monitoring tasks for the lifetime without perf
        overhead.
    
        7. It reported the aggregate cache occupancy or memory bandwidth over
        all sockets. But most cloud and VMM based use cases want to know the
        individual per-socket usage.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3578,10 +3578,7 @@ unlock:
 
 static inline u64 perf_event_count(struct perf_event *event)
 {
-	if (event->pmu->count)
-		return event->pmu->count(event);
-
-	return __perf_event_count(event);
+	return local64_read(&event->count) + atomic64_read(&event->child_count);
 }
 
 /*
@@ -3616,12 +3613,6 @@ u64 perf_event_read_local(struct perf_event *event)
 	 * all child counters from atomic context.
 	 */
 	WARN_ON_ONCE(event->attr.inherit);
-
-	/*
-	 * It must not have a pmu::count method, those are not
-	 * NMI safe.
-	 */
-	WARN_ON_ONCE(event->pmu->count);
 
 	/*
 	 * If the event is currently on this CPU, its either a per-task event,
