1b52cb3598b8b314ff83a83b30abc50ea6a9b1af
Mohammed Gamal <mgamal@redhat.com>
2017-09-27 17:08:01 -0400

    [netdrv] netvsc: report per-channel stats in ethtool statistics
    Message-id: <1506532134-23248-17-git-send-email-mgamal@redhat.com>
    Patchwork-id: 186702
    O-Subject: [RHEL7.5 PATCH v3 16/69] [netdrv] netvsc: report per-channel stats in ethtool statistics
    Bugzilla: 1465469
    RH-Acked-by: Eduardo Otubo <otubo@redhat.com>
    RH-Acked-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Cathy Avery <cavery@redhat.com>
    
    commit 6c80f3fc2398aef22798e8ac4258454b1062f3fb
    Author: Simon Xiao <sixiao@microsoft.com>
    Date:   Tue Jan 24 13:06:13 2017 -0800
    
        netvsc: report per-channel stats in ethtool statistics
    
        Report packets and bytes transferred through a vmbus channel via ethtool.
        This supersedes need for per-cpu statistics.
    
        Example:
        $ ethtool -S eth0
        NIC statistics:
        ...
             tx_queue_0_packets: 3523179
             tx_queue_0_bytes: 505370920
             rx_queue_0_packets: 41430490
             rx_queue_0_bytes: 62714661254
             tx_queue_1_packets: 0
             tx_queue_1_bytes: 0
             rx_queue_1_packets: 0
             rx_queue_1_bytes: 0
        ...
    
        Reviewed-by: Long Li <longli@microsoft.com>
        Reviewed-by: K. Y. Srinivasan <kys@microsoft.com>
        Reviewed-by: Haiyang Zhang <haiyangz@microsoft.com>
        Signed-off-by: Simon Xiao <sixiao@microsoft.com>
        Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Mohammed Gamal <mgamal@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/hyperv/hyperv_net.h
+++ drivers/net/hyperv/hyperv_net.h
@@ -700,8 +700,6 @@ struct net_device_context {
 	u32 msg_enable; /* debug level */
 
 	u32 tx_checksum_mask;
-	struct netvsc_stats __percpu *tx_stats;
-	struct netvsc_stats __percpu *rx_stats;
 
 	/* Ethtool settings */
 	u8 duplex;
@@ -726,6 +724,9 @@ struct netvsc_channel {
 	struct multi_send_data msd;
 	struct multi_recv_comp mrc;
 	atomic_t queue_sends;
+
+	struct netvsc_stats tx_stats;
+	struct netvsc_stats rx_stats;
 };
 
 /* Per netvsc device */
