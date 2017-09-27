58221b049283b2be163a7c3b7f236aee00e34a36
Mohammed Gamal <mgamal@redhat.com>
2017-09-27 17:08:28 -0400

    [netdrv] netvsc: Properly initialize the return value
    Message-id: <1506532134-23248-44-git-send-email-mgamal@redhat.com>
    Patchwork-id: 186729
    O-Subject: [RHEL7.5 PATCH v3 43/69] [netdrv] netvsc: Properly initialize the return value
    Bugzilla: 1465469
    RH-Acked-by: Eduardo Otubo <otubo@redhat.com>
    RH-Acked-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Cathy Avery <cavery@redhat.com>
    
    commit 386f57622cc3bbb39b9c00fc8762bc9ab28e0f8d
    Author: K. Y. Srinivasan <kys@microsoft.com>
    Date:   Fri Mar 24 20:54:37 2017 -0700
    
        netvsc: Properly initialize the return value
    
        Initialize the return value correctly.
    
        Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Mohammed Gamal <mgamal@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -857,7 +857,7 @@ static int netvsc_change_mtu(struct net_device *ndev, int mtu)
 	struct netvsc_device_info device_info;
 	int limit = ETH_DATA_LEN;
 	bool was_running;
-	int ret;
+	int ret = 0;
 
 	if (!nvdev || nvdev->destroy)
 		return -ENODEV;
