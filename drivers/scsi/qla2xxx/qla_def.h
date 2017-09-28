6d6e8c6de3e361c4a59b51b982d2dcca38cedb54
Himanshu Madhani <hmadhani@redhat.com>
2017-09-27 21:44:21 -0400

    [scsi] qla2xxx: Move target stat counters from vha to qpair
    Message-id: <20170927214454.8537-64-hmadhani@redhat.com>
    Patchwork-id: 186882
    O-Subject: [RHEL 7.5 e-stor PATCH v2 63/96] scsi: qla2xxx: Move target stat counters from vha to qpair.
    Bugzilla: 1460030
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Quinn Tran <quinn.tran@cavium.com>
    
    Bugzilla 1460030
    
    commit 60a9eadb19f33a7b3e183207b5b5e1d01d8a6add
    
    Move counters to qpair to reduce cache miss.
    
    [ hmadhani: In qla_def.h removal of tgt_counters is different ]
    [ due to placement of code which was pulled into RH before ]
    [ Target MQ support was added into scsi_qla_host structure ]
    
    Signed-off-by: Quinn Tran <quinn.tran@cavium.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    ---
     drivers/scsi/qla2xxx/qla_def.h     | 27 +++++++++++----------
     drivers/scsi/qla2xxx/qla_dfs.c     | 48 +++++++++++++++++++++++++++++++-------
     drivers/scsi/qla2xxx/qla_iocb.c    |  2 +-
     drivers/scsi/qla2xxx/qla_target.c  | 13 +++++------
     drivers/scsi/qla2xxx/tcm_qla2xxx.c |  8 +++----
     5 files changed, 63 insertions(+), 35 deletions(-)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -3193,6 +3193,18 @@ struct qla_tc_param {
 #define QLA_MAX_VPORTS_QLA24XX	128
 #define QLA_MAX_VPORTS_QLA25XX	256
 
+struct qla_tgt_counters {
+	uint64_t qla_core_sbt_cmd;
+	uint64_t core_qla_que_buf;
+	uint64_t qla_core_ret_ctio;
+	uint64_t core_qla_snd_status;
+	uint64_t qla_core_ret_sta_ctio;
+	uint64_t core_qla_free_cmd;
+	uint64_t num_q_full_sent;
+	uint64_t num_alloc_iocb_failed;
+	uint64_t num_term_xchg_sent;
+};
+
 struct qla_qpair;
 
 /* Response queue data structure */
@@ -3296,6 +3308,7 @@ struct qla_qpair {
 	struct list_head qp_list_elem; /* vha->qp_list */
 	struct list_head hints_list;
 	uint16_t cpuid;
+	struct qla_tgt_counters tgt_counters;
 };
 
 /* Place holder for FW buffer parameters */
@@ -3972,18 +3985,6 @@ struct qla_hw_data {
 	int	allow_cna_fw_dump;
 };
 
-struct qla_tgt_counters {
-	uint64_t qla_core_sbt_cmd;
-	uint64_t core_qla_que_buf;
-	uint64_t qla_core_ret_ctio;
-	uint64_t core_qla_snd_status;
-	uint64_t qla_core_ret_sta_ctio;
-	uint64_t core_qla_free_cmd;
-	uint64_t num_q_full_sent;
-	uint64_t num_alloc_iocb_failed;
-	uint64_t num_term_xchg_sent;
-};
-
 /*
  * Qlogic scsi host structure
  */
@@ -4148,7 +4149,6 @@ typedef struct scsi_qla_host {
 	struct fc_host_statistics fc_host_stat;
 	struct qla_statistics qla_stats;
 	struct bidi_statistics bidi_stats;
-
 	atomic_t	vref_count;
 	struct qla8044_reset_template reset_tmplt;
 	uint16_t	bbcr;
@@ -4157,7 +4157,6 @@ typedef struct scsi_qla_host {
 	int fcport_count;
 	wait_queue_head_t fcport_waitQ;
 	wait_queue_head_t vref_waitq;
-	struct qla_tgt_counters tgt_counters;
 } scsi_qla_host_t;
 
 struct qla27xx_image_status {
--- drivers/scsi/qla2xxx/qla_iocb.c
+++ drivers/scsi/qla2xxx/qla_iocb.c
@@ -2231,7 +2231,7 @@ skip_cmd_array:
 	}
 
 queuing_error:
-	vha->tgt_counters.num_alloc_iocb_failed++;
+	qpair->tgt_counters.num_alloc_iocb_failed++;
 	return pkt;
 }
 
