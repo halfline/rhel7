0f67104233dab50a3e28e6f8329ba927c2543078
Himanshu Madhani <hmadhani@redhat.com>
2017-09-27 21:44:43 -0400

    [scsi] qla2xxx: Fix task mgmt handling for NPIV
    Message-id: <20170927214454.8537-86-hmadhani@redhat.com>
    Patchwork-id: 186912
    O-Subject: [RHEL 7.5 e-stor PATCH v2 85/96] scsi: qla2xxx: Fix task mgmt handling for NPIV
    Bugzilla: 1460030
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Quinn Tran <quinn.tran@cavium.com>
    
    Bugzilla 1460030
    
    commit 72fcd4eb3c046cd59cb2ab5a7fc1555e8621a4b0
    
    Fix task management response for NPIV Target mode.
    Current code uses the wrong vp index.
    
    Signed-off-by: Quinn Tran <quinn.tran@cavium.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    ---
     drivers/scsi/qla2xxx/qla_target.c | 7 ++++++-
     drivers/scsi/qla2xxx/qla_target.h | 1 +
     2 files changed, 7 insertions(+), 1 deletion(-)
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/qla2xxx/qla_target.c
+++ drivers/scsi/qla2xxx/qla_target.c
@@ -1918,6 +1918,7 @@ static int __qlt_24xx_handle_abts(struct scsi_qla_host *vha,
 	mcmd->reset_count = ha->base_qpair->chip_reset;
 	mcmd->tmr_func = QLA_TGT_ABTS;
 	mcmd->qpair = ha->base_qpair;
+	mcmd->vha = vha;
 
 	rc = ha->tgt.tgt_ops->handle_tmr(mcmd, lun, mcmd->tmr_func,
 	    abts->exchange_addr_to_abort);
@@ -2016,7 +2017,7 @@ static void qlt_24xx_handle_abts(struct scsi_qla_host *vha,
 static void qlt_24xx_send_task_mgmt_ctio(struct qla_qpair *qpair,
 	struct qla_tgt_mgmt_cmd *mcmd, uint32_t resp_code)
 {
-	struct scsi_qla_host *ha = qpair->vha;
+	struct scsi_qla_host *ha = mcmd->vha;
 	struct atio_from_isp *atio = &mcmd->orig_iocb.atio;
 	struct ctio7_to_24xx *ctio;
 	uint16_t temp;
@@ -3479,6 +3480,9 @@ static int __qlt_send_term_exchange(struct qla_qpair *qpair,
 
 	ql_dbg(ql_dbg_tgt, vha, 0xe009, "Sending TERM EXCH CTIO (ha=%p)\n", ha);
 
+	if (cmd)
+		vha = cmd->vha;
+
 	pkt = (request_t *)qla2x00_alloc_iocbs_ready(qpair, NULL);
 	if (pkt == NULL) {
 		ql_dbg(ql_dbg_tgt, vha, 0xe050,
@@ -4365,6 +4369,7 @@ static int qlt_issue_task_mgmt(struct fc_port *sess, u64 lun,
 	mcmd->flags = flags;
 	mcmd->reset_count = ha->base_qpair->chip_reset;
 	mcmd->qpair = ha->base_qpair;
+	mcmd->vha = vha;
 
 	switch (fn) {
 	case QLA_TGT_LUN_RESET:
