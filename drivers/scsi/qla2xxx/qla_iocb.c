19ccf9782f9ad2236db533f51d51dc9c22778471
Himanshu Madhani <hmadhani@redhat.com>
2017-09-27 21:43:38 -0400

    [scsi] qla2xxx: Improve T10-DIF/PI handling in driver
    Message-id: <20170927214454.8537-21-hmadhani@redhat.com>
    Patchwork-id: 186848
    O-Subject: [RHEL 7.5 e-stor PATCH v2 20/96] qla2xxx: Improve T10-DIF/PI handling in driver.
    Bugzilla: 1460030
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Quinn Tran <quinn.tran@cavium.com>
    
    Bugzilla 1460030
    
    commit be25152c0d9e236076323abbe9def9714234b761
    
    Add routines to support T10 DIF tag.
    
    Signed-off-by: Quinn Tran <quinn.tran@cavium.com>
    Signed-off-by: Anil Gurumurthy <anil.gurumurthy@cavium.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    (cherry-picked from commit be25152c0d9e)
    
    [hmadhani: RH src has different placement of code than T10 DIF ]
    [ routines from upstream. no change in actual src. ]
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	drivers/scsi/qla2xxx/qla_dbg.h
    	drivers/scsi/qla2xxx/qla_gbl.h
    	drivers/scsi/qla2xxx/qla_target.c
    ---
     drivers/scsi/qla2xxx/qla_dbg.h     |   2 +
     drivers/scsi/qla2xxx/qla_def.h     |  10 +
     drivers/scsi/qla2xxx/qla_gbl.h     |   7 +-
     drivers/scsi/qla2xxx/qla_iocb.c    |  13 +-
     drivers/scsi/qla2xxx/qla_target.c  | 522 +++++++++++++++++++++++--------------
     drivers/scsi/qla2xxx/qla_target.h  |  39 ++-
     drivers/scsi/qla2xxx/tcm_qla2xxx.c |  49 ++--
     7 files changed, 408 insertions(+), 234 deletions(-)

--- drivers/scsi/qla2xxx/qla_iocb.c
+++ drivers/scsi/qla2xxx/qla_iocb.c
@@ -890,7 +890,7 @@ qla24xx_get_one_block_sg(uint32_t blk_sz, struct qla2_sgx *sgx,
 
 int
 qla24xx_walk_and_build_sglist_no_difb(struct qla_hw_data *ha, srb_t *sp,
-	uint32_t *dsd, uint16_t tot_dsds, struct qla_tgt_cmd *tc)
+	uint32_t *dsd, uint16_t tot_dsds, struct qla_tc_param *tc)
 {
 	void *next_dsd;
 	uint8_t avail_dsds = 0;
@@ -899,7 +899,6 @@ qla24xx_walk_and_build_sglist_no_difb(struct qla_hw_data *ha, srb_t *sp,
 	struct scatterlist *sg_prot;
 	uint32_t *cur_dsd = dsd;
 	uint16_t	used_dsds = tot_dsds;
-
 	uint32_t	prot_int; /* protection interval */
 	uint32_t	partial;
 	struct qla2_sgx sgx;
@@ -970,7 +969,7 @@ alloc_and_fill:
 			} else {
 				list_add_tail(&dsd_ptr->list,
 				    &(tc->ctx->dsd_list));
-				tc->ctx_dsd_alloced = 1;
+				*tc->ctx_dsd_alloced = 1;
 			}
 
 			/* add new list to cmd iocb or last list */
@@ -1008,7 +1007,7 @@ alloc_and_fill:
 
 int
 qla24xx_walk_and_build_sglist(struct qla_hw_data *ha, srb_t *sp, uint32_t *dsd,
-	uint16_t tot_dsds, struct qla_tgt_cmd *tc)
+	uint16_t tot_dsds, struct qla_tc_param *tc)
 {
 	void *next_dsd;
 	uint8_t avail_dsds = 0;
@@ -1072,7 +1071,7 @@ qla24xx_walk_and_build_sglist(struct qla_hw_data *ha, srb_t *sp, uint32_t *dsd,
 			} else {
 				list_add_tail(&dsd_ptr->list,
 				    &(tc->ctx->dsd_list));
-				tc->ctx_dsd_alloced = 1;
+				*tc->ctx_dsd_alloced = 1;
 			}
 
 
@@ -1099,7 +1098,7 @@ qla24xx_walk_and_build_sglist(struct qla_hw_data *ha, srb_t *sp, uint32_t *dsd,
 
 int
 qla24xx_walk_and_build_prot_sglist(struct qla_hw_data *ha, srb_t *sp,
-	uint32_t *dsd, uint16_t tot_dsds, struct qla_tgt_cmd *tc)
+	uint32_t *dsd, uint16_t tot_dsds, struct qla_tc_param *tc)
 {
 	void *next_dsd;
 	uint8_t avail_dsds = 0;
@@ -1165,7 +1164,7 @@ qla24xx_walk_and_build_prot_sglist(struct qla_hw_data *ha, srb_t *sp,
 			} else {
 				list_add_tail(&dsd_ptr->list,
 				    &(tc->ctx->dsd_list));
-				tc->ctx_dsd_alloced = 1;
+				*tc->ctx_dsd_alloced = 1;
 			}
 
 			/* add new list to cmd iocb or last list */
