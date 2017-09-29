1cb09e526c147e6bffcb2a7c12cd5bc3b1637e36
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-09-29 12:40:51 -0400

    [netdrv] netvsc: use ERR_PTR to avoid dereference issues
    Message-id: <20170929124142.12672-6-vkuznets@redhat.com>
    Patchwork-id: 187008
    O-Subject: [RHEL7.5 KERNEL PATCH 05/56] netvsc: use ERR_PTR to avoid dereference issues
    Bugzilla: 1474300 1477784
    RH-Acked-by: Eduardo Otubo <otubo@redhat.com>
    RH-Acked-by: Mohammed Gamal <mgamal@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    commit 9749fed5d43d84b86f1c98b70167c31c296bb6a6
    Author: stephen hemminger <stephen@networkplumber.org>
    Date:   Wed Jul 19 11:53:16 2017 -0700
    
        netvsc: use ERR_PTR to avoid dereference issues
    
        The rndis_filter_device_add function is called both in
        probe context and RTNL context,and creates the netvsc_device
        inner structure. It is easier to get the RTNL lock annotation
        correct if it returns the object directly, rather than implicitly
        by updating network device private data.
    
        Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	drivers/net/hyperv/netvsc.c (RHEL-only change in 79e8cbe7a7
                                         backport)
    	drivers/net/hyperv/netvsc_drv.c (skipping tree-wide d0c2c9973e)
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- drivers/net/hyperv/netvsc.c
+++ drivers/net/hyperv/netvsc.c
@@ -29,7 +29,9 @@
 #include <linux/slab.h>
 #include <linux/netdevice.h>
 #include <linux/if_ether.h>
+#include <linux/vmalloc.h>
 #include <linux/rtnetlink.h>
+
 #include <asm/sync_bitops.h>
 
 #include "hyperv_net.h"
@@ -1281,8 +1283,8 @@ void netvsc_channel_cb(void *context)
  * netvsc_device_add - Callback when the device belonging to this
  * driver is added
  */
-int netvsc_device_add(struct hv_device *device,
-		      const struct netvsc_device_info *device_info)
+struct netvsc_device *netvsc_device_add(struct hv_device *device,
+				const struct netvsc_device_info *device_info)
 {
 	int i, ret = 0;
 	int ring_size = device_info->ring_size;
@@ -1292,7 +1294,7 @@ int netvsc_device_add(struct hv_device *device,
 
 	net_device = alloc_net_device();
 	if (!net_device)
-		return -ENOMEM;
+		return ERR_PTR(-ENOMEM);
 
 	net_device->ring_size = ring_size;
 
@@ -1350,7 +1352,7 @@ int netvsc_device_add(struct hv_device *device,
 		goto close;
 	}
 
-	return ret;
+	return net_device;
 
 close:
 	netif_napi_del(&net_device->chan_table[0].napi);
@@ -1361,6 +1363,5 @@ close:
 cleanup:
 	free_netvsc_device(&net_device->rcu);
 
-	return ret;
-
+	return ERR_PTR(ret);
 }
