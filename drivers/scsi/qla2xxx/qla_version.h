28399bc685bbb8105cdda813476db4d8da490a04
Himanshu Madhani <hmadhani@redhat.com>
2017-10-01 04:26:01 -0400

    [scsi] qla2xxx: Update driver version to 10.00.00.00.07.5-k
    Message-id: <20171001042601.7988-24-hmadhani@redhat.com>
    Patchwork-id: 187300
    O-Subject: [RHEL 7.5 e-stor PATCH 23/23] qla2xxx: Update driver version to 10.00.00.00.07.5-k
    Bugzilla: 1316281
    RH-Acked-by: Chris Leech <cleech@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    commit none
    
    [ RH only patch, version update]
    
    Signed-off-by: Himanshu Madhani <hmadhani@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/qla2xxx/qla_version.h
+++ drivers/scsi/qla2xxx/qla_version.h
@@ -7,9 +7,9 @@
 /*
  * Driver version
  */
-#define QLA2XXX_VERSION      "9.00.00.00.07.5-k"
+#define QLA2XXX_VERSION      "10.00.00.00.07.5-k"
 
-#define QLA_DRIVER_MAJOR_VER	9
+#define QLA_DRIVER_MAJOR_VER	10
 #define QLA_DRIVER_MINOR_VER	0
 #define QLA_DRIVER_PATCH_VER	0
 #define QLA_DRIVER_BETA_VER	0
