e7f0c3fbc55b82883749e0a0a43e11217ede3b58
David Milburn <dmilburn@redhat.com>
2017-10-05 08:32:46 -0400

    [nvme] fc: use blk_mq_delay_run_hw_queue instead of open-coding it
    Message-id: <1507192397-86938-176-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 189029
    O-Subject: [RHEL7.5 PATCH v3 175/206] nvme-fc: use blk_mq_delay_run_hw_queue instead of open-coding it
    Bugzilla: 1457880 1456486 1454365
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Sagi Grimberg <sagi@grimberg.me>
    
    Cc: James Smart <james.smart@broadcom.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    (cherry picked from commit e5859d3a0ea07bf948f4538d47fe6547f6e115be)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	drivers/nvme/host/fc.c
    
    RHEL7 is missing upstream fc17b6534eb83 (blk-mq: switch ->queue_rq
    return value to blk_status_t).
    
    BZ 1454365
    BZ 1456486
    BZ 1457880
    
    Signed-off-by: David Milburn <dmilburn@redhat.com>

--- drivers/nvme/host/fc.c
+++ drivers/nvme/host/fc.c
@@ -1979,10 +1979,8 @@ nvme_fc_start_fcp_op(struct nvme_fc_ctrl *ctrl, struct nvme_fc_queue *queue,
 		if (ret != -EBUSY)
 			return BLK_MQ_RQ_QUEUE_ERROR;
 
-		if (op->rq) {
-			blk_mq_stop_hw_queues(op->rq->q);
-			blk_mq_delay_queue(queue->hctx, NVMEFC_QUEUE_DELAY);
-		}
+		if (op->rq)
+			blk_mq_delay_run_hw_queue(queue->hctx, NVMEFC_QUEUE_DELAY);
 		return BLK_MQ_RQ_QUEUE_BUSY;
 	}
 
