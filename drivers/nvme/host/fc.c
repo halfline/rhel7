9a8f0212acb81bfaaf939ffed7225194839a3662
David Milburn <dmilburn@redhat.com>
2017-10-05 08:30:41 -0400

    [nvme] fc: increment request retries counter before requeuing
    Message-id: <1507192397-86938-51-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 188931
    O-Subject: [RHEL7.5 PATCH v3 050/206] nvme-fc: increment request retries counter before requeuing
    Bugzilla: 1457880 1456486 1454365
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Sagi Grimberg <sagi@grimberg.me>
    
    This way our max retry limit holds as well.
    
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Max Gurtovoy <maxg@mellanox.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit f2cd54d3eba3a2048edcb91f145336180c55aee5)
    
    BZ 1454365
    BZ 1456486
    BZ 1457880
    
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/fc.c
+++ drivers/nvme/host/fc.c
@@ -1900,6 +1900,7 @@ nvme_fc_complete_rq(struct request *rq)
 
 	if (unlikely(rq->errors)) {
 		if (nvme_req_needs_retry(rq, rq->errors)) {
+			rq->retries++;
 			nvme_requeue_req(rq);
 			return;
 		}
