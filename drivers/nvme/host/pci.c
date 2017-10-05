0ee6bfd542dac1864bef23725c64ae85a96934d5
David Milburn <dmilburn@redhat.com>
2017-10-05 08:31:22 -0400

    [nvme] unmap CMB and remove sysfs file in reset path
    Message-id: <1507192397-86938-92-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 188956
    O-Subject: [RHEL7.5 PATCH v3 091/206] nvme: unmap CMB and remove sysfs file in reset path
    Bugzilla: 1457880 1456486 1454365
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jon Derrick <jonathan.derrick@intel.com>
    
    CMB doesn't get unmapped until removal while getting remapped on every
    reset. Add the unmapping and sysfs file removal to the reset path in
    nvme_pci_disable to match the mapping path in nvme_pci_enable.
    
    Fixes: 202021c1a ("nvme : Add sysfs entry for NVMe CMBs when appropriate")
    
    Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>
    Acked-by: Keith Busch <keith.busch@intel.com>
    Reviewed-By: Stephen Bates <sbates@raithlin.com>
    Cc: <stable@vger.kernel.org> # 4.9+
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit f63572dff1421b6ca6abce71d46e03411e605c94)
    
    BZ 1454365
    BZ 1456486
    BZ 1457880
    
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -1397,6 +1397,11 @@ static inline void nvme_release_cmb(struct nvme_dev *dev)
 	if (dev->cmb) {
 		iounmap(dev->cmb);
 		dev->cmb = NULL;
+		if (dev->cmbsz) {
+			sysfs_remove_file_from_group(&dev->ctrl.device->kobj,
+						     &dev_attr_cmb.attr, NULL);
+			dev->cmbsz = 0;
+		}
 	}
 }
 
@@ -1663,6 +1668,7 @@ static void nvme_pci_disable(struct nvme_dev *dev)
 {
 	struct pci_dev *pdev = to_pci_dev(dev->dev);
 
+	nvme_release_cmb(dev);
 	pci_free_irq_vectors(pdev);
 
 	if (pci_is_enabled(pdev)) {
@@ -2060,7 +2066,6 @@ static void nvme_remove(struct pci_dev *pdev)
 	flush_work(&dev->reset_work);
 	nvme_dev_remove_admin(dev);
 	nvme_free_queues(dev, 0);
-	nvme_release_cmb(dev);
 	nvme_release_prp_pools(dev);
 	nvme_dev_unmap(dev);
 	nvme_put_ctrl(&dev->ctrl);
