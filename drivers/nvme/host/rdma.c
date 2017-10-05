dc2118db8a6d44f8ca1a9f84e575ef3bcd696321
David Milburn <dmilburn@redhat.com>
2017-10-05 08:30:39 -0400

    [nvme] rdma: increment request retries counter before requeuing
    Message-id: <1507192397-86938-49-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 188905
    O-Subject: [RHEL7.5 PATCH v3 048/206] nvme-rdma: increment request retries counter before requeuing
    Bugzilla: 1457880 1456486 1454365
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Sagi Grimberg <sagi@grimberg.me>
    
    This way our max retry limit holds as well.
    
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Max Gurtovoy <maxg@mellanox.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit e806666e25dc29f81b659e12386f6168488fa9a6)
    
    BZ 1454365
    BZ 1456486
    BZ 1457880
    
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/nvme/host/rdma.c
+++ drivers/nvme/host/rdma.c
@@ -1516,6 +1516,7 @@ static void nvme_rdma_complete_rq(struct request *rq)
 
 	if (unlikely(rq->errors)) {
 		if (nvme_req_needs_retry(rq, rq->errors)) {
+			rq->retries++;
 			nvme_requeue_req(rq);
 			return;
 		}
