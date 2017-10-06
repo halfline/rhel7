7e6d9943d2f96fd4909277da422321bc7ac7cdd9
Paolo Bonzini <pbonzini@redhat.com>
2017-10-06 12:05:08 -0400

    [x86] kvm: Serialize wq active checks in kvm_vcpu_wake_up()
    Message-id: <20171006120601.5022-17-pbonzini@redhat.com>
    Patchwork-id: 189675
    O-Subject: [RHEL7.5 PATCH 16/69] kvm: Serialize wq active checks in kvm_vcpu_wake_up()
    Bugzilla: 1498473
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    
    From: Davidlohr Bueso <dave@stgolabs.net>
    
    This is a generic call and can be suceptible to races
    in reading the wq task_list while another task is adding
    itself to the list. Add a full barrier by using the
    swq_has_sleeper() helper.
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 5e0018b3e39e9b44dbfb380b83026e55d2f65b91)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2147,7 +2147,7 @@ void kvm_vcpu_wake_up(struct kvm_vcpu *vcpu)
 	struct swait_queue_head *wqp;
 
 	wqp = kvm_arch_vcpu_wq(vcpu);
-	if (swait_active(wqp)) {
+	if (swq_has_sleeper(wqp)) {
 		swake_up(wqp);
 		++vcpu->stat.halt_wakeup;
 	}
