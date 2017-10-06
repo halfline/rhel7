4834a118880ab57001b57a1bffbfb69297b59643
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2017-10-06 14:56:14 -0400

    [hid] i2c-hid: prevent buffer overflow in early IRQ
    Message-id: <20171006145658.18562-10-benjamin.tissoires@redhat.com>
    Patchwork-id: 189732
    O-Subject: [PATCH RHEL7.5 09/53] HID: i2c-hid: prevent buffer overflow in early IRQ
    Bugzilla: 1452237 1489630
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1489630
              https://bugzilla.redhat.com/show_bug.cgi?id=1452237
    Upstream: since v3.19
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14201393
    Test: tested by Karl Hastings on a Dell Precision 7720 and Peter Hutterer
          on a Dell XPS 13
    
    commit d1c7e29e8d276c669e8790bb8be9f505ddc48888
    Author: Gwendal Grignou <gwendal@chromium.org>
    Date:   Thu Dec 11 16:02:45 2014 -0800
    
        HID: i2c-hid: prevent buffer overflow in early IRQ
    
        Before ->start() is called, bufsize size is set to HID_MIN_BUFFER_SIZE,
        64 bytes. While processing the IRQ, we were asking to receive up to
        wMaxInputLength bytes, which can be bigger than 64 bytes.
    
        Later, when ->start is run, a proper bufsize will be calculated.
    
        Given wMaxInputLength is said to be unreliable in other part of the
        code, set to receive only what we can even if it results in truncated
        reports.
    
        Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
        Reviewed-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
        Cc: stable@vger.kernel.org
        Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/hid/i2c-hid/i2c-hid.c
+++ drivers/hid/i2c-hid/i2c-hid.c
@@ -371,7 +371,7 @@ static int i2c_hid_hwreset(struct i2c_client *client)
 static void i2c_hid_get_input(struct i2c_hid *ihid)
 {
 	int ret, ret_size;
-	int size = le16_to_cpu(ihid->hdesc.wMaxInputLength);
+	int size = ihid->bufsize;
 
 	if (size > ihid->bufsize)
 		size = ihid->bufsize;
