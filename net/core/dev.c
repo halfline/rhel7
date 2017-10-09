c09d325bac21a7b3eb0d302bed47084612d6a3a6
Ivan Vecera <ivecera@redhat.com>
2017-10-08 22:36:11 -0400

    [net] also make sch_handle_egress() drop monitor ready
    Message-id: <20171008224128.1458-150-ivecera@redhat.com>
    Patchwork-id: 190233
    O-Subject: [RHEL7.5 net PATCH 149/466] net: also make sch_handle_egress() drop monitor ready
    Bugzilla: 1445420
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ#1445420
    
    Upstream commit(s):
    commit 7e2c3aea4398d079745b9faa2c17b6cbd010f221
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date:   Sun May 15 23:28:29 2016 +0200
    
        net: also make sch_handle_egress() drop monitor ready
    
        Follow-up for 8a3a4c6e7b34 ("net: make sch_handle_ingress() drop
        monitor ready") to also make the egress side drop monitor ready.
    
        Also here only TC_ACT_SHOT is a clear indication that something
        went wrong. Hence don't provide false positives to drop monitors
        such as 'perf record -e skb:kfree_skb ...'.
    
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Acked-by: Alexei Starovoitov <ast@kernel.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3228,12 +3228,12 @@ sch_handle_egress(struct sk_buff *skb, int *ret, struct net_device *dev)
 	case TC_ACT_SHOT:
 		qdisc_qstats_cpu_drop(cl->q);
 		*ret = NET_XMIT_DROP;
-		goto drop;
+		kfree_skb(skb);
+		return NULL;
 	case TC_ACT_STOLEN:
 	case TC_ACT_QUEUED:
 		*ret = NET_XMIT_SUCCESS;
-drop:
-		kfree_skb(skb);
+		consume_skb(skb);
 		return NULL;
 	default:
 		break;
