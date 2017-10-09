8fdcd16bf8ce63818ae158373c85a42a2bbe17e0
Paolo Bonzini <pbonzini@redhat.com>
2017-10-09 12:19:24 -0400

    [x86] kvm: vmx: Fix invalid guest state detection after task-switch emulation
    Message-id: <20171009122028.31911-23-pbonzini@redhat.com>
    Patchwork-id: 190579
    O-Subject: [RHEL7.5 PATCH 22/86] KVM: VMX: Fix invalid guest state detection after task-switch emulation
    Bugzilla: 1469685
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    
    From: Wanpeng Li <wanpeng.li@hotmail.com>
    
    This can be reproduced by EPT=1, unrestricted_guest=N, emulate_invalid_state=Y
    or EPT=0, the trace of kvm-unit-tests/taskswitch2.flat is like below, it tries
    to emulate invalid guest state task-switch:
    
    kvm_exit: reason TASK_SWITCH rip 0x0 info 40000058 0
    kvm_emulate_insn: 42000:0:0f 0b (0x2)
    kvm_emulate_insn: 42000:0:0f 0b (0x2) failed
    kvm_inj_exception: #UD (0x0)
    kvm_entry: vcpu 0
    kvm_exit: reason TASK_SWITCH rip 0x0 info 40000058 0
    kvm_emulate_insn: 42000:0:0f 0b (0x2)
    kvm_emulate_insn: 42000:0:0f 0b (0x2) failed
    kvm_inj_exception: #UD (0x0)
    ......................
    
    It appears that the task-switch emulation updates rflags (and vm86
    flag) only after the segments are loaded, causing vmx->emulation_required
    to be set, when in fact invalid guest state emulation is not needed.
    
    This patch fixes it by updating vmx->emulation_required after the
    rflags (and vm86 flag) is updated in task-switch emulation.
    
    Thanks Radim for moving the update to vmx__set_flags and adding Paolo's
    suggestion for the check.
    
    Suggested-by: Nadav Amit <nadav.amit@gmail.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krm <rkrcmar@redhat.com>
    Cc: Nadav Amit <nadav.amit@gmail.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Signed-off-by: Radim Krm <rkrcmar@redhat.com>
    (cherry picked from commit f244deed7a254b4a61333e6886575ce0102e8356)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -2252,6 +2252,11 @@ static void vmx_fpu_activate(struct kvm_vcpu *vcpu)
 	vmcs_writel(CR0_GUEST_HOST_MASK, ~vcpu->arch.cr0_guest_owned_bits);
 }
 
+static bool emulation_required(struct kvm_vcpu *vcpu)
+{
+	return emulate_invalid_guest_state && !guest_state_valid(vcpu);
+}
+
 static void vmx_decache_cr0_guest_bits(struct kvm_vcpu *vcpu);
 
 /*
@@ -2316,6 +2321,8 @@ static unsigned long vmx_get_rflags(struct kvm_vcpu *vcpu)
 
 static void vmx_set_rflags(struct kvm_vcpu *vcpu, unsigned long rflags)
 {
+	unsigned long old_rflags = vmx_get_rflags(vcpu);
+
 	__set_bit(VCPU_EXREG_RFLAGS, (ulong *)&vcpu->arch.regs_avail);
 	to_vmx(vcpu)->rflags = rflags;
 	if (to_vmx(vcpu)->rmode.vm86_active) {
@@ -2323,6 +2330,9 @@ static void vmx_set_rflags(struct kvm_vcpu *vcpu, unsigned long rflags)
 		rflags |= X86_EFLAGS_IOPL | X86_EFLAGS_VM;
 	}
 	vmcs_writel(GUEST_RFLAGS, rflags);
+
+	if ((old_rflags ^ to_vmx(vcpu)->rflags) & X86_EFLAGS_VM)
+		to_vmx(vcpu)->emulation_required = emulation_required(vcpu);
 }
 
 static u32 vmx_get_interrupt_shadow(struct kvm_vcpu *vcpu)
@@ -3485,11 +3495,6 @@ static __init int alloc_kvm_area(void)
 		per_cpu(vmxarea, cpu) = vmcs;
 	}
 	return 0;
-}
-
-static bool emulation_required(struct kvm_vcpu *vcpu)
-{
-	return emulate_invalid_guest_state && !guest_state_valid(vcpu);
 }
 
 static void fix_pmode_seg(struct kvm_vcpu *vcpu, int seg,
