7f2ea470102d79b536fd4e184a0a1f3dd3d322bb
Ivan Vecera <ivecera@redhat.com>
2017-10-08 22:36:07 -0400

    [net] net sched: mirred action fix late binding
    Message-id: <20171008224128.1458-146-ivecera@redhat.com>
    Patchwork-id: 190232
    O-Subject: [RHEL7.5 net PATCH 145/466] net sched: mirred action fix late binding
    Bugzilla: 1445420
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ#1445420
    
    Upstream commit(s):
    commit 87dfbdc6c7478018c5489897d4495a27a626cda1
    Author: Jamal Hadi Salim <jhs@mojatatu.com>
    Date:   Tue May 10 16:49:28 2016 -0400
    
        net sched: mirred action fix late binding
    
        The process below was broken and is fixed with this patch.
    
        //add an mirred action and give it an instance id of 1
        sudo tc actions add action mirred egress mirror dev $MDEV  index 1
        //create a filter which binds to mirred action id 1
        sudo tc filter add dev $DEV parent ffff: protocol ip prio 1 u32\
        match ip dst 17.0.0.1/32 flowid 1:10 action mirred index 1
    
        Message before bug fix was:
        RTNETLINK answers: Invalid argument
        We have an error talking to the kernel
    
        Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
        Reviewed-by: Cong Wang <xiyou.wangcong@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sched/act_mirred.c
+++ net/sched/act_mirred.c
@@ -62,7 +62,7 @@ static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 	struct tc_mirred *parm;
 	struct tcf_mirred *m;
 	struct net_device *dev;
-	int ret, ok_push = 0;
+	int ret, ok_push = 0, exists = 0;
 
 	if (nla == NULL)
 		return -EINVAL;
@@ -72,17 +72,27 @@ static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 	if (tb[TCA_MIRRED_PARMS] == NULL)
 		return -EINVAL;
 	parm = nla_data(tb[TCA_MIRRED_PARMS]);
+
+	exists = tcf_hash_check(tn, parm->index, a, bind);
+	if (exists && bind)
+		return 0;
+
 	switch (parm->eaction) {
 	case TCA_EGRESS_MIRROR:
 	case TCA_EGRESS_REDIR:
 		break;
 	default:
+		if (exists)
+			tcf_hash_release(a, bind);
 		return -EINVAL;
 	}
 	if (parm->ifindex) {
 		dev = __dev_get_by_index(net, parm->ifindex);
-		if (dev == NULL)
+		if (dev == NULL) {
+			if (exists)
+				tcf_hash_release(a, bind);
 			return -ENODEV;
+		}
 		switch (dev->type) {
 		case ARPHRD_TUNNEL:
 		case ARPHRD_TUNNEL6:
@@ -100,7 +110,7 @@ static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 		dev = NULL;
 	}
 
-	if (!tcf_hash_check(tn, parm->index, a, bind)) {
+	if (!exists) {
 		if (dev == NULL)
 			return -EINVAL;
 		ret = tcf_hash_create(tn, parm->index, est, a,
@@ -109,9 +119,6 @@ static int tcf_mirred_init(struct net *net, struct nlattr *nla,
 			return ret;
 		ret = ACT_P_CREATED;
 	} else {
-		if (bind)
-			return 0;
-
 		tcf_hash_release(a, bind);
 		if (!ovr)
 			return -EEXIST;
