f4e17599e2779218abf3c437bb59a11483d2fb4d
Paolo Bonzini <pbonzini@redhat.com>
2017-10-09 12:20:23 -0400

    [x86] kvm: mmu: make spte mmio mask more explicit
    Message-id: <20171009122028.31911-82-pbonzini@redhat.com>
    Patchwork-id: 190637
    O-Subject: [RHEL7.5 PATCH 81/86] x86: kvm: mmu: make spte mmio mask more explicit
    Bugzilla: 1469685
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    
    From: Peter Feiner <pfeiner@google.com>
    
    Specify both a mask (i.e., bits to consider) and a value (i.e.,
    pattern of bits that indicates a special PTE) for mmio SPTEs. On
    Intel, this lets us pack even more information into the
    (SPTE_SPECIAL_MASK | EPT_VMX_RWX_MASK) mask we use for access
    tracking liberating all (SPTE_SPECIAL_MASK | (non-misconfigured-RWX))
    values.
    
    Signed-off-by: Peter Feiner <pfeiner@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit dcdca5fed5f6ef2521f927ba3b5cd6b328054be1)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -4986,7 +4986,8 @@ static void ept_set_mmio_spte_mask(void)
 	 * EPT Misconfigurations can be generated if the value of bits 2:0
 	 * of an EPT paging-structure entry is 110b (write/execute).
 	 */
-	kvm_mmu_set_mmio_spte_mask(VMX_EPT_MISCONFIG_WX_VALUE);
+	kvm_mmu_set_mmio_spte_mask(VMX_EPT_RWX_MASK,
+				   VMX_EPT_MISCONFIG_WX_VALUE);
 }
 
 /*
