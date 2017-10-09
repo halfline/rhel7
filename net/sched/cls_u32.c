53af59c0ed2d8dc216a61c04e8e014853f646bdd
Ivan Vecera <ivecera@redhat.com>
2017-10-08 22:34:42 -0400

    [net] sched: fix compile warning in cls_u32
    Message-id: <20171008224128.1458-61-ivecera@redhat.com>
    Patchwork-id: 190144
    O-Subject: [RHEL7.5 net PATCH 060/466] net: sched: fix compile warning in cls_u32
    Bugzilla: 1445420
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ#1445420
    
    Upstream commit(s):
    commit a2aeb02a8e6a9fef397c344245a54eeae67341f6
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Mon Sep 22 13:42:53 2014 -0700
    
        net: sched: fix compile warning in cls_u32
    
        $ grep CONFIG_CLS_U32_MARK .config
        # CONFIG_CLS_U32_MARK is not set
    
        net/sched/cls_u32.c: In function 'u32_change':
        net/sched/cls_u32.c:852:1: warning: label 'errout' defined but not used
        [-Wunused-label]
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- net/sched/cls_u32.c
+++ net/sched/cls_u32.c
@@ -1015,9 +1015,9 @@ static int u32_change(struct net *net, struct sk_buff *in_skb,
 errhw:
 #ifdef CONFIG_CLS_U32_MARK
 	free_percpu(n->pcpu_success);
+errout:
 #endif
 
-errout:
 #ifdef CONFIG_CLS_U32_PERF
 	free_percpu(n->pf);
 #endif
