d38d2ffd17cefc5f5628595760a8c70366dea13b
Jerry Snitselaar <jsnitsel@redhat.com>
2017-10-09 17:33:40 -0400

    [iommu] amd: Remove cmd_buf_size and evt_buf_size from struct amd_iommu
    Message-id: <20171009173625.17916-32-jsnitsel@redhat.com>
    Patchwork-id: 190689
    O-Subject: [RHEL7.5 PATCH 031/196] iommu/amd: Remove cmd_buf_size and evt_buf_size from struct amd_iommu
    Bugzilla: 1411581
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Suravee Suthikulpanit <ssuthiku@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/1411581
    
    commit deba4bce168a87ef90211ba69850d3428b453765
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Tue Oct 20 17:33:41 2015 +0200
    
        iommu/amd: Remove cmd_buf_size and evt_buf_size from struct amd_iommu
    
        The driver always uses a constant size for these buffers
        anyway, so there is no need to waste memory to store the
        sizes.
    
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Cc: Myron Stowe <myron.stowe@redhat.com>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Don Dutile <ddutile@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/iommu/amd_iommu_init.c
+++ drivers/iommu/amd_iommu_init.c
@@ -520,8 +520,6 @@ static u8 * __init alloc_command_buffer(struct amd_iommu *iommu)
 	if (cmd_buf == NULL)
 		return NULL;
 
-	iommu->cmd_buf_size = CMD_BUFFER_SIZE | CMD_BUFFER_UNINITIALIZED;
-
 	return cmd_buf;
 }
 
@@ -556,13 +554,11 @@ static void iommu_enable_command_buffer(struct amd_iommu *iommu)
 		    &entry, sizeof(entry));
 
 	amd_iommu_reset_cmd_buffer(iommu);
-	iommu->cmd_buf_size &= ~(CMD_BUFFER_UNINITIALIZED);
 }
 
 static void __init free_command_buffer(struct amd_iommu *iommu)
 {
-	free_pages((unsigned long)iommu->cmd_buf,
-		   get_order(iommu->cmd_buf_size & ~(CMD_BUFFER_UNINITIALIZED)));
+	free_pages((unsigned long)iommu->cmd_buf, get_order(CMD_BUFFER_SIZE));
 }
 
 /* allocates the memory where the IOMMU will log its events to */
@@ -573,8 +569,6 @@ static u8 * __init alloc_event_buffer(struct amd_iommu *iommu)
 
 	if (iommu->evt_buf == NULL)
 		return NULL;
-
-	iommu->evt_buf_size = EVT_BUFFER_SIZE;
 
 	return iommu->evt_buf;
 }
