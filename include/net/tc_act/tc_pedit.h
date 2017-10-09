336506c369b5057481dc77078b908b583516a031
Ivan Vecera <ivecera@redhat.com>
2017-10-08 22:38:41 -0400

    [net] act_pedit: Support using offset relative to the conventional network headers
    Message-id: <20171008224128.1458-300-ivecera@redhat.com>
    Patchwork-id: 190383
    O-Subject: [RHEL7.5 net PATCH 299/466] net/act_pedit: Support using offset relative to the conventional network headers
    Bugzilla: 1445420
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ#1445420
    
    Upstream commit(s):
    commit 71d0ed7079dffbc5cd0941d77d9b84e04109c9bb
    Author: Amir Vadai <amir@vadai.me>
    Date:   Tue Feb 7 09:56:07 2017 +0200
    
        net/act_pedit: Support using offset relative to the conventional network headers
    
        Extend pedit to enable the user setting offset relative to network
        headers. This change would enable to work with more complex header
        schemes (vs the simple IPv4 case) where setting a fixed offset relative
        to the network header is not enough.
    
        After this patch, the action has information about the exact header type
        and field inside this header. This information could be used later on
        for hardware offloading of pedit.
    
        Backward compatibility was being kept:
        1. Old kernel <-> new userspace
        2. New kernel <-> old userspace
        3. add rule using new userspace <-> dump using old userspace
        4. add rule using old userspace <-> dump using new userspace
    
        When using the extended api, new netlink attributes are being used. This
        way, operation will fail in (1) and (3) - and no malformed rule be added
        or dumped. Of course, new user space that doesn't need the new
        functionality can use the old netlink attributes and operation will
        succeed.
        Since action can support both api's, (2) should work, and it is easy to
        write the new user space to have (4) work.
    
        The action is having a strict check that only header types and commands
        it can handle are accepted. This way future additions will be much
        easier.
    
        Usage example:
        $ tc filter add dev enp0s9 protocol ip parent ffff: \
          flower \
            ip_proto tcp \
            dst_port 80 \
          action pedit munge tcp dport set 8080 pipe \
          action mirred egress redirect dev veth0
    
        Will forward tcp port whose original dest port is 80, while modifying
        the destination port to 8080.
    
        Signed-off-by: Amir Vadai <amir@vadai.me>
        Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/net/tc_act/tc_pedit.h
+++ include/net/tc_act/tc_pedit.h
@@ -4,11 +4,16 @@
 #include <net/act_api.h>
 #include <linux/tc_act/tc_pedit.h>
 
+struct tcf_pedit_key_ex {
+	enum pedit_header_type htype;
+};
+
 struct tcf_pedit {
 	struct tc_action	common;
 	unsigned char		tcfp_nkeys;
 	unsigned char		tcfp_flags;
 	struct tc_pedit_key	*tcfp_keys;
+	struct tcf_pedit_key_ex	*tcfp_keys_ex;
 };
 #define to_pedit(a) ((struct tcf_pedit *)a)
 
