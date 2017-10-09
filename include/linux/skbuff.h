0cacc8bf9ec9407351b5c2a8119af0051df0ee6d
Ivan Vecera <ivecera@redhat.com>
2017-10-08 22:37:04 -0400

    [net] simplify and make pkt_type_ok() available for other users
    Message-id: <20171008224128.1458-203-ivecera@redhat.com>
    Patchwork-id: 190286
    O-Subject: [RHEL7.5 net PATCH 202/466] net: simplify and make pkt_type_ok() available for other users
    Bugzilla: 1445420
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ#1445420
    
    Upstream commit(s):
    commit 8b10cab64c134ffbffac96edd1899d303d3afcac
    Author: Jamal Hadi Salim <jhs@mojatatu.com>
    Date:   Sat Jul 2 06:43:14 2016 -0400
    
        net: simplify and make pkt_type_ok() available for other users
    
        Suggested-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
        Acked-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -37,6 +37,7 @@
 #include <linux/sched.h>
 #include <net/flow_dissector.h>
 #include <linux/in6.h>
+#include <linux/if_packet.h>
 #include <net/flow.h>
 
 #include <linux/rh_kabi.h>
@@ -939,6 +940,15 @@ static inline bool skb_dst_is_noref(const struct sk_buff *skb)
 static inline struct rtable *skb_rtable(const struct sk_buff *skb)
 {
 	return (struct rtable *)skb_dst(skb);
+}
+
+/* For mangling skb->pkt_type from user space side from applications
+ * such as nft, tc, etc, we only allow a conservative subset of
+ * possible pkt_types to be set.
+*/
+static inline bool skb_pkt_type_ok(u32 ptype)
+{
+	return ptype <= PACKET_OTHERHOST;
 }
 
 static inline unsigned int skb_napi_id(const struct sk_buff *skb)
