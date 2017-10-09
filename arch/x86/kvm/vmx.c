d1caf0af922403928d86cb4f2493e1be103772fb
Paolo Bonzini <pbonzini@redhat.com>
2017-10-09 12:19:21 -0400

    [x86] kvm: nvmx: keep preemption timer enabled during L2 execution
    Message-id: <20171009122028.31911-20-pbonzini@redhat.com>
    Patchwork-id: 190574
    O-Subject: [RHEL7.5 PATCH 19/86] KVM: nVMX: keep preemption timer enabled during L2 execution
    Bugzilla: 1469685
    RH-Acked-by: Radim Krcmar <rkrcmar@redhat.com>
    RH-Acked-by: Bandan Das <bsd@redhat.com>
    
    Because the vmcs12 preemption timer is emulated through a separate hrtimer,
    we can keep on using the preemption timer in the vmcs02 to emulare L1's
    TSC deadline timer.
    
    However, the corresponding bit in the pin-based execution control field
    must be kept consistent between vmcs01 and vmcs02.  On vmentry we copy
    it into the vmcs02; on vmexit the preemption timer must be disabled in
    the vmcs01 if a preemption timer vmexit happened while in guest mode.
    
    The preemption timer value in the vmcs02 is set by vmx_vcpu_run, so it
    need not be considered in prepare_vmcs02.
    
    Cc: Yunhong Jiang <yunhong.jiang@intel.com>
    Cc: Haozhong Zhang <haozhong.zhang@intel.com>
    Tested-by: Wanpeng Li <kernellwp@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 9314006db8b781715658cd6a28994d84ccce5dee)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -9746,9 +9746,14 @@ static int prepare_vmcs02(struct kvm_vcpu *vcpu, struct vmcs12 *vmcs12,
 	vmcs_write64(VMCS_LINK_POINTER, -1ull);
 
 	exec_control = vmcs12->pin_based_vm_exec_control;
-	exec_control |= vmcs_config.pin_based_exec_ctrl;
-	exec_control &= ~PIN_BASED_VMX_PREEMPTION_TIMER;
 
+	/* Preemption timer setting is only taken from vmcs01.  */
+	exec_control &= ~PIN_BASED_VMX_PREEMPTION_TIMER;
+	exec_control |= vmcs_config.pin_based_exec_ctrl;
+	if (vmx->hv_deadline_tsc == -1)
+		exec_control &= ~PIN_BASED_VMX_PREEMPTION_TIMER;
+
+	/* Posted interrupts setting is only taken from vmcs12.  */
 	if (nested_cpu_has_posted_intr(vmcs12)) {
 		/*
 		 * Note that we use L0's vector here and in
@@ -10710,8 +10715,14 @@ static void nested_vmx_vmexit(struct kvm_vcpu *vcpu, u32 exit_reason,
 
 	load_vmcs12_host_state(vcpu, vmcs12);
 
-	/* Update TSC_OFFSET if TSC was changed while L2 ran */
+	/* Update any VMCS fields that might have changed while L2 ran */
 	vmcs_write64(TSC_OFFSET, vcpu->arch.tsc_offset);
+	if (vmx->hv_deadline_tsc == -1)
+		vmcs_clear_bits(PIN_BASED_VM_EXEC_CONTROL,
+				PIN_BASED_VMX_PREEMPTION_TIMER);
+	else
+		vmcs_set_bits(PIN_BASED_VM_EXEC_CONTROL,
+			      PIN_BASED_VMX_PREEMPTION_TIMER);
 
 	if (kvm_has_tsc_control)
 		decache_tsc_multiplier(vmx);
