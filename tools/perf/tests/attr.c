91b4716ac4068a5d574b925162c3c1abe54b1162
Jiri Olsa <jolsa@redhat.com>
2017-10-10 09:41:42 -0400

    [tools] perf tests attr: Do not store failed events
    Message-id: <20171010094159.11213-3-jolsa@redhat.com>
    Patchwork-id: 190870
    O-Subject: [PATCH RHEL7.5 BZ1308907 02/19] perf tests attr: Do not store failed events
    Bugzilla: 1308907
    RH-Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1308907
    https://bugzilla.redhat.com/show_bug.cgi?id=1308907
    
    upstream
    ========
    commit d78ada4a767a744cad5efa210b8acf57748b91d7
    Author: Jiri Olsa <jolsa@kernel.org>
    Date: Mon Jul 3 16:50:17 2017 +0200
    
    description
    ===========
    Do not mess up our temp space with files we don't
    need - failed event open attempts.
    
    RHEL7
    =====
    Conflict with upstream patch, which we did not backported yet:
      fef2a735167a perf tools: Kill die()
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- tools/perf/tests/attr.c
+++ tools/perf/tests/attr.c
@@ -138,7 +138,7 @@ void test_attr__open(struct perf_event_attr *attr, pid_t pid, int cpu,
 {
 	int errno_saved = errno;
 
-	if (store_event(attr, pid, cpu, fd, group_fd, flags))
+	if ((fd != -1) && store_event(attr, pid, cpu, fd, group_fd, flags))
 		die("test attr FAILED");
 
 	errno = errno_saved;
