1713dab7cafce252d1044eaf6145e0a13f08827b
Dick Kennedy <dkennedy@redhat.com>
2017-10-10 21:25:15 -0400

    [scsi] lpfc: nvme: Initiator: bind to nvme_fc api
    Message-id: <1507670879-17640-7-git-send-email-dkennedy@redhat.com>
    Patchwork-id: 191023
    O-Subject: [RHEL7.5 e-stor PATCH 006/170] lpfc: NVME Initiator: bind to nvme_fc api
    Bugzilla: 1385844 1461977 1387768
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Dick Kennedy <dick.kennedy@avagotech.com>
    
    NVME Initiator: Tie in to NVME Fabrics nvme_fc LLDD initiator api
    
    Adds the routines to:
    - register and deregister the FC port as a nvme-fc initiator localport
    - register and deregister remote FC ports as a nvme-fc remoteport
    - binding of nvme queues to adapter WQs
    - send/perform NVME LS's
    - send/perform NVME FCP initiator io operations
    
    Signed-off-by: Dick Kennedy <dick.kennedy@broadcom.com>
    Signed-off-by: James Smart <james.smart@broadcom.com>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    cherry picked from 01649561a8b4b77247bd234f240d737367bb8a52
    ---
     drivers/scsi/lpfc/Makefile         |    7 +-
     drivers/scsi/lpfc/lpfc.h           |    5 +
     drivers/scsi/lpfc/lpfc_crtn.h      |   10 +
     drivers/scsi/lpfc/lpfc_ct.c        |    2 +-
     drivers/scsi/lpfc/lpfc_els.c       |    4 +-
     drivers/scsi/lpfc/lpfc_hbadisc.c   |   15 +-
     drivers/scsi/lpfc/lpfc_init.c      |   39 +-
     drivers/scsi/lpfc/lpfc_nportdisc.c |    4 +-
     drivers/scsi/lpfc/lpfc_nvme.c      | 2319 ++++++++++++++++++++++++++++++++++++
     drivers/scsi/lpfc/lpfc_nvme.h      |    6 +
     drivers/scsi/lpfc/lpfc_sli.c       |   21 +-
     drivers/scsi/lpfc/lpfc_sli4.h      |    1 +
     drivers/scsi/lpfc/lpfc_vport.c     |   16 +-
     13 files changed, 2411 insertions(+), 38 deletions(-)
     create mode 100644 drivers/scsi/lpfc/lpfc_nvme.c
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/lpfc/lpfc_hbadisc.c
+++ drivers/scsi/lpfc/lpfc_hbadisc.c
@@ -908,7 +908,9 @@ lpfc_linkdown(struct lpfc_hba *phba)
 
 			vports[i]->fc_myDID = 0;
 
-			/* todo: init: revise localport nvme attributes */
+			if ((phba->cfg_enable_fc4_type == LPFC_ENABLE_BOTH) ||
+			    (phba->cfg_enable_fc4_type == LPFC_ENABLE_NVME))
+				lpfc_nvme_update_localport(vports[i]);
 		}
 	}
 	lpfc_destroy_vport_work_array(phba, vports);
@@ -3578,7 +3580,9 @@ lpfc_mbx_cmpl_reg_vpi(struct lpfc_hba *phba, LPFC_MBOXQ_t *pmb)
 		spin_unlock_irq(shost->host_lock);
 		vport->fc_myDID = 0;
 
-		/* todo: init: revise localport nvme attributes */
+		if ((phba->cfg_enable_fc4_type == LPFC_ENABLE_BOTH) ||
+		    (phba->cfg_enable_fc4_type == LPFC_ENABLE_NVME))
+			lpfc_nvme_update_localport(vport);
 		goto out;
 	}
 
@@ -3949,7 +3953,8 @@ out:
 
 		if ((phba->cfg_enable_fc4_type == LPFC_ENABLE_BOTH) ||
 		    (phba->cfg_enable_fc4_type == LPFC_ENABLE_NVME))
-			lpfc_ns_cmd(vport, SLI_CTNS_RFF_ID, 0, FC_TYPE_NVME);
+			lpfc_ns_cmd(vport, SLI_CTNS_RFF_ID, 0,
+				    FC_TYPE_NVME);
 
 		/* Issue SCR just before NameServer GID_FT Query */
 		lpfc_issue_els_scr(vport, SCR_DID, 0);
@@ -4143,7 +4148,7 @@ lpfc_nlp_state_cleanup(struct lpfc_vport *vport, struct lpfc_nodelist *ndlp,
 		    ((ndlp->nlp_fc4_type & NLP_FC4_NVME) ||
 		    (ndlp->nlp_DID == Fabric_DID))) {
 			vport->phba->nport_event_cnt++;
-			/* todo: init: unregister rport from nvme */
+			lpfc_nvme_unregister_port(vport, ndlp);
 		}
 	}
 
@@ -4168,7 +4173,7 @@ lpfc_nlp_state_cleanup(struct lpfc_vport *vport, struct lpfc_nodelist *ndlp,
 				 * the register.
 				 */
 				vport->phba->nport_event_cnt++;
-				/* todo: init: register rport with nvme */
+				lpfc_nvme_register_port(vport, ndlp);
 			}
 		}
 	}
