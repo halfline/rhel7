c4439679b1e06787e34d8aa3db8e445b9c7bbf84
Ming Lei <ming.lei@redhat.com>
2017-10-12 15:26:35 -0400

    [block] fix debugfs config conditional in struct request_queue
    Message-id: <20171012152744.9819-122-ming.lei@redhat.com>
    Patchwork-id: 191835
    O-Subject: [RHEL7.5 PATCH V2 121/190] block: fix debugfs config conditional in struct request_queue
    Bugzilla: 1458104
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Omar Sandoval <osandov@fb.com>
    
    BZ: 1458104
    
    commit 03796c149a99e14506db9de3adba710c26f83ba9
    Author: Omar Sandoval <osandov@fb.com>
    Date:   Tue Jan 31 14:53:18 2017 -0800
    
        block: fix debugfs config conditional in struct request_queue
    
        The debugfs dentries are only used for CONFIG_BLK_DEBUG_FS, so make them
        conditional on that instead of CONFIG_DEBUG_FS.
    
        Signed-off-by: Omar Sandoval <osandov@fb.com>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -551,7 +551,7 @@ struct request_queue {
 	RH_KABI_EXTEND(struct work_struct	timeout_work)
 	RH_KABI_EXTEND(struct delayed_work	requeue_work)
 	RH_KABI_EXTEND(struct blk_rq_stat	rq_stats[2])
-#ifdef CONFIG_DEBUG_FS
+#ifdef CONFIG_BLK_DEBUG_FS
 	RH_KABI_EXTEND(struct dentry		*debugfs_dir)
 	RH_KABI_EXTEND(struct dentry		*mq_debugfs_dir)
 #endif
