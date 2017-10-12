e8b5f0d7fac15e6993252add9f2f4eaba982357a
Ming Lei <ming.lei@redhat.com>
2017-10-12 15:25:13 -0400

    [block] blk-mq: only apply active queue tag throttling for driver tags
    Message-id: <20171012152744.9819-40-ming.lei@redhat.com>
    Patchwork-id: 191797
    O-Subject: [RHEL7.5 PATCH V2 039/190] blk-mq: only apply active queue tag throttling for driver tags
    Bugzilla: 1458104
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Jens Axboe <axboe@fb.com>
    
    BZ: 1458104
    
    commit 200e86b3372b51e136a382e007b6b904b1dac7e4
    Author: Jens Axboe <axboe@fb.com>
    Date:   Wed Jan 25 08:11:38 2017 -0700
    
        blk-mq: only apply active queue tag throttling for driver tags
    
        If we have a scheduler attached, we have two sets of tags. We don't
        want to apply our active queue throttling for the scheduler side
        of tags, that only applies to driver tags since that's the resource
        we need to dispatch an IO.
    
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -237,15 +237,14 @@ struct request *__blk_mq_alloc_request(struct blk_mq_alloc_data *data, int rw)
 
 		rq = tags->static_rqs[tag];
 
-		if (blk_mq_tag_busy(data->hctx)) {
-			rq->cmd_flags = REQ_MQ_INFLIGHT;
-			atomic_inc(&data->hctx->nr_active);
-		}
-
 		if (data->flags & BLK_MQ_REQ_INTERNAL) {
 			rq->tag = -1;
 			rq->internal_tag = tag;
 		} else {
+			if (blk_mq_tag_busy(data->hctx)) {
+				rq->cmd_flags = REQ_MQ_INFLIGHT;
+				atomic_inc(&data->hctx->nr_active);
+			}
 			rq->tag = tag;
 			rq->internal_tag = -1;
 		}
@@ -835,6 +834,10 @@ done:
 
 	rq->tag = blk_mq_get_tag(&data);
 	if (rq->tag >= 0) {
+		if (blk_mq_tag_busy(data.hctx)) {
+			rq->cmd_flags |= REQ_MQ_INFLIGHT;
+			atomic_inc(&data.hctx->nr_active);
+		}
 		data.hctx->tags->rqs[rq->tag] = rq;
 		goto done;
 	}
