9d9a10c4946d0ad8f0a627c9f690ed125acb4db2
Eric Sandeen <esandeen@redhat.com>
2017-10-12 15:31:48 -0400

    [fs] dax: rip out get_block based IO support
    Message-id: <e1a875b4-df27-41c9-a6b5-7b0f6dc17b73@redhat.com>
    Patchwork-id: 191783
    O-Subject: [RHEL7 fs 50/56 V2] dax: rip out get_block based IO support
    Bugzilla: 1496513
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    Bugzilla: 1496513
    Upstream Status: Committed upstream
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=14131636
    Tested: Locally with fstests
    
    RHEL: Again, our IO path differs, so this rip-it-out rips out
    the same functions, but they look different from upstream.
    
    Backport of the following upstream commit:
    
    commit dd936e4313fa3f60abd6e67abb3cb66fc9a018d1
    Author: Jan Kara <jack@suse.cz>
    Date:   Sun Nov 20 20:48:36 2016 -0500
    
        dax: rip out get_block based IO support
    
        No one uses functions using the get_block callback anymore. Rip them
        out and update documentation.
    
        Reviewed-by: Ross Zwisler <ross.zwisler@linux.intel.com>
        Signed-off-by: Jan Kara <jack@suse.cz>
        Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/dax.h
+++ include/linux/dax.h
@@ -39,11 +39,6 @@ struct iomap_ops;
 ssize_t dax_iomap_rw(int rw, struct kiocb *iocb, const struct iovec *iov,
 		unsigned long nr_segs, loff_t pos,
 		size_t count, struct iomap_ops *ops);
-ssize_t dax_do_io(int rw, struct kiocb *iocb, struct inode *inode,
-                  const struct iovec *iov, loff_t pos, unsigned long nr_segs,
-                  get_block_t get_block, dio_iodone_t end_io, int flags);
-int dax_zero_page_range(struct inode *, loff_t from, unsigned len, get_block_t);
-int dax_truncate_page(struct inode *, loff_t from, get_block_t);
 int dax_iomap_fault(struct vm_area_struct *vma, struct vm_fault *vmf,
 			struct iomap_ops *ops);
 int dax_fault(struct vm_area_struct *, struct vm_fault *, get_block_t);
@@ -75,12 +70,6 @@ static inline int __dax_zero_page_range(struct block_device *bdev,
 }
 #endif
 
-static inline int dax_pmd_fault(struct vm_area_struct *vma, unsigned long addr,
-				pmd_t *pmd, unsigned int flags, get_block_t gb)
-{
-	return VM_FAULT_FALLBACK;
-}
-
 #ifdef CONFIG_FS_DAX_PMD
 static inline unsigned int dax_radix_order(void *entry)
 {
@@ -103,7 +92,6 @@ static inline int dax_iomap_pmd_fault(struct vm_area_struct *vma,
 }
 #endif
 int dax_pfn_mkwrite(struct vm_area_struct *, struct vm_fault *);
-#define dax_mkwrite(vma, vmf, gb)	dax_fault(vma, vmf, gb)
 
 static inline bool vma_is_dax(struct vm_area_struct *vma)
 {
