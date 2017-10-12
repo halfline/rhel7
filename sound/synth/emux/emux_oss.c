bc30ce387ed1f44d783a66a8cd469fa92b3d0855
Jaroslav Kysela <jkysela@redhat.com>
2017-10-12 17:44:15 -0400

    [sound] alsa: seq: Allow the tristate build of OSS emulation
    Message-id: <1507829954160572-57-patchset-jkysela@redhat.com>
    Patchwork-id: 191962
    O-Subject: [RHEL 7.5 PATCH v2 0056/1088] ALSA: seq: Allow the tristate build of OSS emulation
    Bugzilla: 1463624
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Currently OSS sequencer emulation is tied with ALSA sequencer core,
    both are built in the same level; i.e. when CONFIG_SND_SEQUENCER=y,
    the OSS sequencer emulation is also always built-in, even though the
    functionality can be built as an individual module.
    
    This patch changes the rule and allows users to build snd-seq-oss
    module while others are built-in.  Essentially, it's just a few simple
    changes in Kconfig and Makefile.  Some driver codes like opl3 need to
    convert from the simple ifdef to IS_ENABLED().  But that's all.
    
    You might wonder how about the dependency: right, it can be messy, but
    it still works.  Since we rewrote the sequencer binding with the
    standard bus, the driver can be bound at any time on demand.  So, the
    synthesizer driver module can be loaded individually from the OSS
    emulation core before/after it.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    (cherry picked from commit 3d774d5ef066971aee1839150bf47d100d8e43d3)
    Bugzilla: 1463624
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- sound/synth/emux/emux_oss.c
+++ sound/synth/emux/emux_oss.c
@@ -23,8 +23,6 @@
  */
 
 
-#ifdef CONFIG_SND_SEQUENCER_OSS
-
 #include <linux/export.h>
 #include <linux/uaccess.h>
 #include <sound/core.h>
@@ -513,5 +511,3 @@ fake_event(struct snd_emux *emu, struct snd_emux_port *port, int ch, int param, 
 	ev.data.control.value = val;
 	snd_emux_event_input(&ev, 0, port, atomic, hop);
 }
-
-#endif /* CONFIG_SND_SEQUENCER_OSS */
