dbcf6a81ff08c7b481584725e98c361fdd6a35d0
Ming Lei <ming.lei@redhat.com>
2017-10-12 15:27:05 -0400

    [block] blk-mq: Let blk_mq_debugfs_register() look up the queue name
    Message-id: <20171012152744.9819-152-ming.lei@redhat.com>
    Patchwork-id: 191867
    O-Subject: [RHEL7.5 PATCH V2 151/190] blk-mq: Let blk_mq_debugfs_register() look up the queue name
    Bugzilla: 1458104
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Bart Van Assche <bart.vanassche@sandisk.com>
    
    BZ: 1458104
    
    commit 4c9e4019f1880bea7ae6e9603d14c2ed0f1c4e23
    Author: Bart Van Assche <bart.vanassche@sandisk.com>
    Date:   Wed Apr 26 13:47:49 2017 -0700
    
        blk-mq: Let blk_mq_debugfs_register() look up the queue name
    
        A later patch will move the call of blk_mq_debugfs_register() to
        a function to which the queue name is not passed as an argument.
        To avoid having to add a 'name' argument to multiple callers, let
        blk_mq_debugfs_register() look up the queue name.
    
        Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
        Reviewed-by: Omar Sandoval <osandov@fb.com>
        Reviewed-by: Hannes Reinecke <hare@suse.com>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- block/blk-mq.h
+++ block/blk-mq.h
@@ -97,13 +97,12 @@ extern int __blk_mq_register_dev(struct device *dev, struct request_queue *q);
  * debugfs helpers
  */
 #ifdef CONFIG_BLK_DEBUG_FS
-int blk_mq_debugfs_register(struct request_queue *q, const char *name);
+int blk_mq_debugfs_register(struct request_queue *q);
 void blk_mq_debugfs_unregister(struct request_queue *q);
 int blk_mq_debugfs_register_hctxs(struct request_queue *q);
 void blk_mq_debugfs_unregister_hctxs(struct request_queue *q);
 #else
-static inline int blk_mq_debugfs_register(struct request_queue *q,
-					  const char *name)
+static inline int blk_mq_debugfs_register(struct request_queue *q)
 {
 	return 0;
 }
