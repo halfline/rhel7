6fb9a6b3ad90c149fe7ae286babad05212f19cbc
Don Dutile <ddutile@redhat.com>
2017-10-11 20:00:32 -0400

    [infiniband] ib/mlx5: Add port counter support for Receive WQs
    Message-id: <20171011200214.23764-132-ddutile@redhat.com>
    Patchwork-id: 191477
    O-Subject: [PATCH RHEL-7.5 v2 131/233] IB/mlx5: Add port counter support for Receive WQs
    Bugzilla: 1385309 1385649 1386645 1409099 1456667 1456687
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    
    commit 23a6964e3adb0796e1633562a574839b92360cb6
    Author: Majd Dibbiny <majd@mellanox.com>
    Date:   Wed Jan 18 15:25:10 2017 +0200
    
        IB/mlx5: Add port counter support for Receive WQs
    
        Counters weren't updated due to Receive WQs' traffic since the
        counter-id was not associated with the RQ.
    
        Added support for associating the q-counter-id with the Receive WQ.
        The attachment is done only when changing WQ's state from RESET to
        READY in modify-WQ command.
    
        FW support is required for the above, without this support
        Receive WQ counters will not count.
    
        Signed-off-by: Majd Dibbiny <majd@mellanox.com>
        Signed-off-by: Leon Romanovsky <leon@kernel.org>
        Signed-off-by: Doug Ledford <dledford@redhat.com>
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -2440,7 +2440,7 @@ static int modify_raw_packet_qp_rq(struct mlx5_ib_dev *dev,
 	if (raw_qp_param->set_mask & MLX5_RAW_QP_MOD_SET_RQ_Q_CTR_ID) {
 		if (MLX5_CAP_GEN(dev->mdev, modify_rq_counter_set_id)) {
 			MLX5_SET64(modify_rq_in, in, modify_bitmask,
-				   MLX5_MODIFY_RQ_IN_MODIFY_BITMASK_MODIFY_RQ_COUNTER_SET_ID);
+				   MLX5_MODIFY_RQ_IN_MODIFY_BITMASK_RQ_COUNTER_SET_ID);
 			MLX5_SET(rqc, rqc, counter_set_id, raw_qp_param->rq_q_ctr_id);
 		} else
 			pr_info_once("%s: RAW PACKET QP counters are not supported on current FW\n",
@@ -4955,6 +4955,16 @@ int mlx5_ib_modify_wq(struct ib_wq *wq, struct ib_wq_attr *wq_attr,
 			MLX5_SET(rqc, rqc, vsd,
 				 (wq_attr->flags & IB_WQ_FLAGS_CVLAN_STRIPPING) ? 0 : 1);
 		}
+	}
+
+	if (curr_wq_state == IB_WQS_RESET && wq_state == IB_WQS_RDY) {
+		if (MLX5_CAP_GEN(dev->mdev, modify_rq_counter_set_id)) {
+			MLX5_SET64(modify_rq_in, in, modify_bitmask,
+				   MLX5_MODIFY_RQ_IN_MODIFY_BITMASK_RQ_COUNTER_SET_ID);
+			MLX5_SET(rqc, rqc, counter_set_id, dev->port->q_cnts.set_id);
+		} else
+			pr_info_once("%s: Receive WQ counters are not supported on current FW\n",
+				     dev->ib_dev.name);
 	}
 
 	err = mlx5_core_modify_rq(dev->mdev, rwq->core_qp.qpn, in, inlen);
