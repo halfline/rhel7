torvalds/linux

commit 5513e16421cb9538f4e394734e4b96ea0d857b23

net: ethernet: aquantia: Fixes for aq_ndev_change_mtu
1)Removed unnecessary comparsion "old_mtu == new_mtu".
This check is not needed. Function aq_ndev_change_mtu wont be called
if mtu has not changed.

2)Removed extra assignment ndev->mtu = new_mtu;
This assignment already done inside __dev_set_mtu().

3)Use core MTU checking for min_mtu.

Signed-off-by: Pavel Belous <pavel.belous@aquantia.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/aquantia/atlantic/aq_main.c
@@ -100,20 +100,10 @@ err_exit:
 static int aq_ndev_change_mtu(struct net_device *ndev, int new_mtu)
 {
 	struct aq_nic_s *aq_nic = netdev_priv(ndev);
-	int err = 0;
+	int err = aq_nic_set_mtu(aq_nic, new_mtu + ETH_HLEN);
 
-	if (new_mtu == ndev->mtu) {
-		err = 0;
-		goto err_exit;
-	}
-	if (new_mtu < 68) {
-		err = -EINVAL;
-		goto err_exit;
-	}
-	err = aq_nic_set_mtu(aq_nic, new_mtu + ETH_HLEN);
 	if (err < 0)
 		goto err_exit;
-	ndev->mtu = new_mtu;
 
 	if (netif_running(ndev)) {
 		aq_ndev_close(ndev);

drivers/net/ethernet/aquantia/atlantic/aq_nic.c
@@ -214,6 +214,7 @@ struct aq_nic_s *aq_nic_alloc_cold(const struct net_device_ops *ndev_ops,
 	SET_NETDEV_DEV(ndev, dev);
 
 	ndev->if_port = port;
+	ndev->min_mtu = ETH_MIN_MTU;
 	self->ndev = ndev;
 
 	self->aq_pci_func = aq_pci_func;

halfline/rhel7

commit 4280f4d8272e663ee013f2f343f5767bb4650a21

[netdrv] aquantia: set net_device mtu when mtu is changed
Message-id: <1507302172-139365-11-git-send-email-darcari@redhat.com>
Patchwork-id: 189781
O-Subject: [RHEL7.5 BZ 1452077 10/15] net: ethernet: aquantia: set net_device mtu when mtu is changed
Bugzilla: 1452077
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Steve Best <sbest@redhat.com>
RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1452077

commit bc9ab9231ec8c08352ea860480523d88a221a68f
Author: David Arcari <darcari@redhat.com>
Date:   Mon Mar 13 19:07:16 2017 -0400

    net: ethernet: aquantia: set net_device mtu when mtu is changed

    When the aquantia device mtu is changed the net_device structure is not
    updated.  As a result the ip command does not properly reflect the mtu change.

    Commit 5513e16421cb incorrectly assumed that __dev_set_mtu() was making the
    assignment ndev->mtu = new_mtu;  This is not true in the case where the driver
    has a ndo_change_mtu routine.

    Fixes: 5513e16421cb ("net: ethernet: aquantia: Fixes for aq_ndev_change_mtu")

    Cc: Pavel Belous <Pavel.Belous@aquantia.com>
    Signed-off-by: David Arcari <darcari@redhat.com>
    Tested-by: Pavel Belous <pavel.belous@aquantia.com>
    Reviewed-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

Cc: David Arcari <darcari@redhat.com>
Cc: Ivan Vecera <ivecera@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/net/ethernet/aquantia/atlantic/aq_main.c
@@ -98,6 +98,7 @@ static int aq_ndev_change_mtu(struct net_device *ndev, int new_mtu)
 
 	if (err < 0)
 		goto err_exit;
+	ndev->mtu = new_mtu;
 
 err_exit:
 	return err;