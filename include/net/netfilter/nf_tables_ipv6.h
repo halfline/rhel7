7220009991680c4322ef188a86c0e158781a2e0c
Stefano Brivio <sbrivio@redhat.com>
2017-10-12 16:01:51 -0400

    [net] netfilter: nf_tables: set pktinfo->thoff at AH header if found
    Message-id: <a1b443cd5baa7dc96be857e7e3a1efea989d3749.1507821896.git.sbrivio@redhat.com>
    Patchwork-id: 191849
    O-Subject: [RHEL7.5 net] netfilter: nf_tables: set pktinfo->thoff at AH header if found
    Bugzilla: 1358624
    RH-Acked-by: Matteo Croce <mcroce@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Eric Garver <egarver@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1358624
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14255529
    Tested: with reproducer on bug plus kernel Tier1 tests
    
    Conflicts: context differences due to missing commits 71212c9b04eb ("netfilter:
        nf_tables: don't drop IPv6 packets that cannot parse transport") and
        8df9e32e7ed2 ("netfilter: nf_tables_ipv6: setup pktinfo transport field on
        failure to parse")
    Backport notes: dropped hunk related to __nft_set_pktinfo_ipv6_validate() as
        RHEL7 lacks ddc8b6027ad0 ("netfilter: introduce
        nft_set_pktinfo_{ipv4, ipv6}_validate()"). No equivalent code paths to be
        taken care of because we also lack the netdev table for ingress filtering
        introduced upstream by ed6c4136f157 ("netfilter: nf_tables: add netdev table
        to filter from ingress")
    
    commit 568af6de058cb2b0c5b98d98ffcf37cdc6bc38a7
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Sat Mar 4 19:53:47 2017 +0100
    
        netfilter: nf_tables: set pktinfo->thoff at AH header if found
    
        Phil Sutter reports that IPv6 AH header matching is broken. From
        userspace, nft generates bytecode that expects to find the AH header at
        NFT_PAYLOAD_TRANSPORT_HEADER both for IPv4 and IPv6. However,
        pktinfo->thoff is set to the inner header after the AH header in IPv6,
        while in IPv4 pktinfo->thoff points to the AH header indeed. This
        behaviour is inconsistent. This patch fixes this problem by updating
        ipv6_find_hdr() to get the IP6_FH_F_AUTH flag so this function stops at
        the AH header, so both IPv4 and IPv6 pktinfo->thoff point to the AH
        header.
    
        This is also inconsistent when trying to match encapsulated headers:
    
        1) A packet that looks like IPv4 + AH + TCP dport 22 will *not* match.
        2) A packet that looks like IPv6 + AH + TCP dport 22 will match.
    
        Reported-by: Phil Sutter <phil@nwl.cc>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/net/netfilter/nf_tables_ipv6.h
+++ include/net/netfilter/nf_tables_ipv6.h
@@ -9,12 +9,13 @@ nft_set_pktinfo_ipv6(struct nft_pktinfo *pkt,
 		     struct sk_buff *skb,
 		     const struct nf_hook_state *state)
 {
+	unsigned int flags = IP6_FH_F_AUTH;
 	int protohdr, thoff = 0;
 	unsigned short frag_off;
 
 	nft_set_pktinfo(pkt, skb, state);
 
-	protohdr = ipv6_find_hdr(pkt->skb, &thoff, -1, &frag_off, NULL);
+	protohdr = ipv6_find_hdr(pkt->skb, &thoff, -1, &frag_off, &flags);
 	/* If malformed, drop it */
 	if (protohdr < 0)
 		return -1;
