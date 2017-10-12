e045fd62d5558a9288f5403d4dc0c889a8d44862
Jaroslav Kysela <jkysela@redhat.com>
2017-10-12 18:49:26 -0400

    [sound] alsa: asoc: topology: show index in debug when adding DAPM routes
    Message-id: <1507829954160572-795-patchset-jkysela@redhat.com>
    Patchwork-id: 192926
    O-Subject: [RHEL 7.5 PATCH v2 0794/1088] ALSA: ASoC: topology: show index in debug when adding DAPM routes
    Bugzilla: 1463624
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Makes the debug output much more useful.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    (cherry picked from commit b75a65118d287aadeade8b106ed0da7b5e42c167)
    Bugzilla: 1463624
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- sound/soc/soc-topology.c
+++ sound/soc/soc-topology.c
@@ -373,7 +373,7 @@ static int soc_tplg_dai_load(struct soc_tplg *tplg,
 	return 0;
 }
 
-/* pass link configurations to component driver for extra intialization */
+/* pass link configurations to component driver for extra initialization */
 static int soc_tplg_dai_link_load(struct soc_tplg *tplg,
 	struct snd_soc_dai_link *link)
 {
@@ -1171,7 +1171,8 @@ static int soc_tplg_dapm_graph_elems_load(struct soc_tplg *tplg,
 		return -EINVAL;
 	}
 
-	dev_dbg(tplg->dev, "ASoC: adding %d DAPM routes\n", count);
+	dev_dbg(tplg->dev, "ASoC: adding %d DAPM routes for index %d\n", count,
+		hdr->index);
 
 	for (i = 0; i < count; i++) {
 		elem = (struct snd_soc_tplg_dapm_graph_elem *)tplg->pos;
