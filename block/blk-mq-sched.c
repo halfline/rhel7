08bff84e176c8a02f34ba106f8f0171da6c621a1
Ming Lei <ming.lei@redhat.com>
2017-10-12 15:25:30 -0400

    [block] blk-mq: pass bio to blk_mq_sched_get_rq_priv
    Message-id: <20171012152744.9819-57-ming.lei@redhat.com>
    Patchwork-id: 193230
    O-Subject: [RHEL7.5 PATCH V2 056/190] blk-mq: pass bio to blk_mq_sched_get_rq_priv
    Bugzilla: 1458104
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    From: Paolo Valente <paolo.valente@linaro.org>
    
    BZ: 1458104
    
    commit f1ba82616c3368e1ae9e64ef29cf3edc1be0860d
    Author: Paolo Valente <paolo.valente@linaro.org>
    Date:   Tue Feb 7 18:24:43 2017 +0100
    
        blk-mq: pass bio to blk_mq_sched_get_rq_priv
    
        bio is used in bfq-mq's get_rq_priv, to get the request group. We could
        pass directly the group here, but I thought that passing the bio was
        more general, giving the possibility to get other pieces of information
        if needed.
    
        Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- block/blk-mq-sched.c
+++ block/blk-mq-sched.c
@@ -67,7 +67,9 @@ error:
 EXPORT_SYMBOL_GPL(blk_mq_sched_init_hctx_data);
 
 static void __blk_mq_sched_assign_ioc(struct request_queue *q,
-				      struct request *rq, struct io_context *ioc)
+				      struct request *rq,
+				      struct bio *bio,
+				      struct io_context *ioc)
 {
 	struct io_cq *icq;
 
@@ -82,7 +84,7 @@ static void __blk_mq_sched_assign_ioc(struct request_queue *q,
 	}
 
 	rq->elv.icq = icq;
-	if (!blk_mq_sched_get_rq_priv(q, rq)) {
+	if (!blk_mq_sched_get_rq_priv(q, rq, bio)) {
 		rq->cmd_flags |= REQ_ELVPRIV;
 		get_io_context(icq->ioc);
 		return;
@@ -98,7 +100,7 @@ static void blk_mq_sched_assign_ioc(struct request_queue *q,
 
 	ioc = rq_ioc(bio);
 	if (ioc)
-		__blk_mq_sched_assign_ioc(q, rq, ioc);
+		__blk_mq_sched_assign_ioc(q, rq, bio, ioc);
 }
 
 struct request *blk_mq_sched_get_request(struct request_queue *q,
