torvalds/linux

commit 920d13a884c0595451658a7b48af8ac16918628f

nvme-pci: factor out the cqe reading mechanics from __nvme_process_cq
Also, maintain a consumed counter to rely on for doorbell and
cqe_seen update instead of directly relying on the cq head and phase.

Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
Reviewed-by: Christoph Hellwig <hch@lst.de>
Signed-off-by: Keith Busch <keith.busch@intel.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

drivers/nvme/host/pci.c
@@ -770,36 +770,40 @@ static inline void nvme_handle_cqe(struct nvme_queue *nvmeq,
 	nvme_end_request(req, cqe->status, cqe->result);
 }
 
-static void __nvme_process_cq(struct nvme_queue *nvmeq, unsigned int *tag)
+static inline bool nvme_read_cqe(struct nvme_queue *nvmeq,
+		struct nvme_completion *cqe)
 {
-	u16 head, phase;
+	if (nvme_cqe_valid(nvmeq, nvmeq->cq_head, nvmeq->cq_phase)) {
+		*cqe = nvmeq->cqes[nvmeq->cq_head];
 
-	head = nvmeq->cq_head;
-	phase = nvmeq->cq_phase;
-
-	while (nvme_cqe_valid(nvmeq, head, phase)) {
-		struct nvme_completion cqe = nvmeq->cqes[head];
-
-		if (++head == nvmeq->q_depth) {
-			head = 0;
-			phase = !phase;
+		if (++nvmeq->cq_head == nvmeq->q_depth) {
+			nvmeq->cq_head = 0;
+			nvmeq->cq_phase = !nvmeq->cq_phase;
 		}
+		return true;
+	}
+	return false;
+}
 
-		if (tag && *tag == cqe.command_id)
-			*tag = -1;
+static void __nvme_process_cq(struct nvme_queue *nvmeq, int *tag)
+{
+	struct nvme_completion cqe;
+	int consumed = 0;
 
+	while (nvme_read_cqe(nvmeq, &cqe)) {
 		nvme_handle_cqe(nvmeq, &cqe);
+		consumed++;
+
+		if (tag && *tag == cqe.command_id) {
+			*tag = -1;
+			break;
+		}
 	}
 
-	if (head == nvmeq->cq_head && phase == nvmeq->cq_phase)
-		return;
-
-	nvmeq->cq_head = head;
-	nvmeq->cq_phase = phase;
-
-	nvme_ring_cq_doorbell(nvmeq);
-
-	nvmeq->cqe_seen = 1;
+	if (consumed) {
+		nvme_ring_cq_doorbell(nvmeq);
+		nvmeq->cqe_seen = 1;
+	}
 }
 
 static void nvme_process_cq(struct nvme_queue *nvmeq)

halfline/rhel7

commit cce07fc8b32160d3cdc61386cf0e2765b5f3f77d

[nvme] pci: set cqe_seen on polled completions
Message-id: <1507192397-86938-204-git-send-email-dmilburn@redhat.com>
Patchwork-id: 189058
O-Subject: [RHEL7.5 PATCH v3 203/206] nvme-pci: set cqe_seen on polled completions
Bugzilla: 1457880 1456486 1454365
RH-Acked-by: Ewan Milne <emilne@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

From: Keith Busch <keith.busch@intel.com>

Fixes: 920d13a884 ("nvme-pci: factor out the cqe reading mechanics from __nvme_process_cq")
Reported-by: Jens Axboe <axboe@kernel.dk>
Signed-off-by: Keith Busch <keith.busch@intel.com>
Signed-off-by: Christoph Hellwig <hch@lst.de>
(cherry picked from commit e9d8a0fdeacd843c85dcef480cdb2ab76bcdb6e4)
Signed-off-by: Rafael Aquini <aquini@redhat.com>

Conflicts:
	drivers/nvme/host/pci.c

RHEL7 doesn't upstream commit a0fa9647a54e8 (NVMe: add blk polling
support, so nvme_process_cq() returns value.

BZ 1454365
BZ 1456486
BZ 1457880

Signed-off-by: David Milburn <dmilburn@redhat.com>

drivers/nvme/host/pci.c
@@ -748,6 +748,7 @@ static inline void nvme_handle_cqe(struct nvme_queue *nvmeq,
 		return;
 	}
 
+	nvmeq->cqe_seen = 1;
 	req = blk_mq_tag_to_rq(*nvmeq->tags, cqe->command_id);
 	nvme_end_request(req, cqe->status, cqe->result);
 }
@@ -778,10 +779,8 @@ static int nvme_process_cq(struct nvme_queue *nvmeq)
 		consumed++;
 	}
 
-	if (consumed) {
+	if (consumed) 
 		nvme_ring_cq_doorbell(nvmeq);
-		nvmeq->cqe_seen = 1;
-	}
 
 	return 1;
 }