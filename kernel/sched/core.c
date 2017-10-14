a9038c75d9be3967a36955b431e0a62608d7fabb
Lauro Ramos Venancio <lvenanci@redhat.com>
2017-10-13 23:23:26 -0400

    [kernel] sched/topology: Simplify sched_group_mask() usage
    Message-id: <1507937007-9543-18-git-send-email-lvenanci@redhat.com>
    Patchwork-id: 193612
    O-Subject: [RHEL-7.5 PATCH 17/18] sched/topology: Simplify sched_group_mask() usage
    Bugzilla: 1373534
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1373534
    
    commit af218122b103900fa33d408aea0c2468791e698c
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Mon May 1 08:51:05 2017 +0200
    
        sched/topology: Simplify sched_group_mask() usage
    
        While writing the comments, it occurred to me that:
    
          sg_cpus & sg_mask == sg_mask
    
        at least conceptually; the !overlap case sets the all 1s mask. If we
        correct that we can simplify things and directly use sg_mask.
    
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Mike Galbraith <efault@gmx.de>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: linux-kernel@vger.kernel.org
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Lauro Ramos Venancio <lvenanci@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6907,7 +6907,7 @@ enum s_alloc {
  */
 int group_balance_cpu(struct sched_group *sg)
 {
-	return cpumask_first_and(sched_group_cpus(sg), sched_group_mask(sg));
+	return cpumask_first(sched_group_mask(sg));
 }
 
 
