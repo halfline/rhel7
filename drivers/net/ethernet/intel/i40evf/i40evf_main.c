6b046eaf46d3896f0eebf096e629b10daac42735
Stefan Assmann <sassmann@redhat.com>
2017-10-17 12:37:41 -0400

    [netdrv] intel: use core min/max MTU checking
    Message-id: <20171017123911.21976-2-sassmann@redhat.com>
    Patchwork-id: 194022
    O-Subject: [RHEL7.5 PATCH 01/91] ethernet/intel: use core min/max MTU checking
    Bugzilla: 1454890
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    From: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1454890
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14279804
    
    e100: min_mtu 68, max_mtu 1500
    - remove e100_change_mtu entirely, is identical to old eth_change_mtu,
      and no longer serves a purpose. No need to set min_mtu or max_mtu
      explicitly, as ether_setup() will already set them to 68 and 1500.
    
    e1000: min_mtu 46, max_mtu 16110
    
    e1000e: min_mtu 68, max_mtu varies based on adapter
    
    fm10k: min_mtu 68, max_mtu 15342
    - remove fm10k_change_mtu entirely, does nothing now
    
    i40e: min_mtu 68, max_mtu 9706
    
    i40evf: min_mtu 68, max_mtu 9706
    
    igb: min_mtu 68, max_mtu 9216
    - There are two different "max" frame sizes claimed and both checked in
      the driver, the larger value wasn't relevant though, so I've set max_mtu
      to the smaller of the two values here to retain identical behavior.
    
    igbvf: min_mtu 68, max_mtu 9216
    - Same issue as igb duplicated
    
    ixgb: min_mtu 68, max_mtu 16114
    - Also remove pointless old == new check, as that's done in dev_set_mtu
    
    ixgbe: min_mtu 68, max_mtu 9710
    
    ixgbevf: min_mtu 68, max_mtu dependent on hardware/firmware
    - Some hw can only handle up to max_mtu 1504 on a vf, others 9710
    
    CC: netdev@vger.kernel.org
    CC: intel-wired-lan@lists.osuosl.org
    CC: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Stefan Assmann <sassmann@redhat.com>
    (cherry picked from commit 91c527a55664ddf4bee26673a35f91748dae4142)
    
    Conflicts: Partial backport only.
    	   min_mtu, max_mtu are implemented via extended ndo.
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/intel/i40evf/i40evf_main.c
+++ drivers/net/ethernet/intel/i40evf/i40evf_main.c
@@ -2186,10 +2186,6 @@ static struct net_device_stats *i40evf_get_stats(struct net_device *netdev)
 static int i40evf_change_mtu(struct net_device *netdev, int new_mtu)
 {
 	struct i40evf_adapter *adapter = netdev_priv(netdev);
-	int max_frame = new_mtu + ETH_HLEN + ETH_FCS_LEN;
-
-	if ((new_mtu < 68) || (max_frame > I40E_MAX_RXBUFFER))
-		return -EINVAL;
 
 	netdev->mtu = new_mtu;
 	adapter->flags |= I40EVF_FLAG_RESET_NEEDED;
@@ -2286,7 +2282,7 @@ static const struct net_device_ops i40evf_netdev_ops = {
 	.ndo_set_rx_mode	= i40evf_set_rx_mode,
 	.ndo_validate_addr	= eth_validate_addr,
 	.ndo_set_mac_address	= i40evf_set_mac,
-	.ndo_change_mtu_rh74	= i40evf_change_mtu,
+	.extended.ndo_change_mtu	= i40evf_change_mtu,
 	.ndo_tx_timeout		= i40evf_tx_timeout,
 	.ndo_vlan_rx_add_vid	= i40evf_vlan_rx_add_vid,
 	.ndo_vlan_rx_kill_vid	= i40evf_vlan_rx_kill_vid,
@@ -2535,6 +2531,10 @@ static void i40evf_init_task(struct work_struct *work)
 	netdev->netdev_ops = &i40evf_netdev_ops;
 	i40evf_set_ethtool_ops(netdev);
 	netdev->watchdog_timeo = 5 * HZ;
+
+	/* MTU range: 68 - 9710 */
+	netdev->extended->min_mtu = ETH_MIN_MTU;
+	netdev->extended->max_mtu = I40E_MAX_RXBUFFER - (ETH_HLEN + ETH_FCS_LEN);
 
 	if (!is_valid_ether_addr(adapter->hw.mac.addr)) {
 		dev_info(&pdev->dev, "Invalid MAC address %pM, using random\n",
