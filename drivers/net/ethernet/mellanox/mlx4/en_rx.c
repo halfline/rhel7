torvalds/linux

commit dceeab0e5258fccad5dbf95b54821ae4e919e0b6

mlx4: support __GFP_MEMALLOC for rx
Commit 04aeb56a1732 ("net/mlx4_en: allocate non 0-order pages for RX
ring with __GFP_NOMEMALLOC") added code that appears to be not needed at
that time, since mlx4 never used __GFP_MEMALLOC allocations anyway.

As using memory reserves is a must in some situations (swap over NFS or
iSCSI), this patch adds this flag.

Note that this driver does not reuse pages (yet) so we do not have to
add anything else.

Signed-off-by: Eric Dumazet <edumazet@google.com>
Cc: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
Cc: Tariq Toukan <tariqt@mellanox.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/mellanox/mlx4/en_rx.c
@@ -706,7 +706,8 @@ static bool mlx4_en_refill_rx_buffers(struct mlx4_en_priv *priv,
 	do {
 		if (mlx4_en_prepare_rx_desc(priv, ring,
 					    ring->prod & ring->size_mask,
-					    GFP_ATOMIC | __GFP_COLD))
+					    GFP_ATOMIC | __GFP_COLD |
+					    __GFP_MEMALLOC))
 			break;
 		ring->prod++;
 	} while (--missing);

halfline/rhel7

commit 943e8fa4bef0829f6809f4d534c553411bba8ba5

[infiniband] lx4_en: Use __skb_fill_page_desc()
Message-id: <20171011200214.23764-202-ddutile@redhat.com>
Patchwork-id: 191547
O-Subject: [PATCH RHEL-7.5 v2 201/233] net/mlx4_en: Use __skb_fill_page_desc()
Bugzilla: 1385309 1385649 1386645 1409099 1456667 1456687
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>

commit 7f0137e2ef9f32143df623001a96f7aab61a9595
Author: Eric Dumazet <edumazet@google.com>
Date:   Thu Feb 23 12:02:45 2017 +0200

    net/mlx4_en: Use __skb_fill_page_desc()

    Or we might miss the fact that a page was allocated from memory reserves.

    Fixes: dceeab0e5258 ("mlx4: support __GFP_MEMALLOC for rx")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

Signed-off-by: Donald Dutile <ddutile@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/net/ethernet/mellanox/mlx4/en_rx.c
@@ -594,10 +594,10 @@ static int mlx4_en_complete_rx_desc(struct mlx4_en_priv *priv,
 		dma_sync_single_for_cpu(priv->ddev, dma, frag_info->frag_size,
 					DMA_FROM_DEVICE);
 
-		/* Save page reference in skb */
-		__skb_frag_set_page(&skb_frags_rx[nr], frags[nr].page);
-		skb_frag_size_set(&skb_frags_rx[nr], frag_info->frag_size);
-		skb_frags_rx[nr].page_offset = frags[nr].page_offset;
+		__skb_fill_page_desc(skb, nr, frags[nr].page,
+				     frags[nr].page_offset,
+				     frag_info->frag_size);
+
 		skb->truesize += frag_info->frag_stride;
 		frags[nr].page = NULL;
 	}