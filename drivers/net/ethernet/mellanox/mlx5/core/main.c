torvalds/linux

commit f66f049fb7385d01464a2126443addc2db54e473

net/mlx5_core: Request the mlx5 IB module on driver load
Call request module on mlx5_ib so it will be available for applications
requiring it, such as installers that require boot over IB.

Signed-off-by: Eli Cohen <eli@mellanox.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -43,6 +43,7 @@
 #include <linux/mlx5/qp.h>
 #include <linux/mlx5/srq.h>
 #include <linux/debugfs.h>
+#include <linux/kmod.h>
 #include <linux/mlx5/mlx5_ifc.h>
 #include "mlx5_core.h"
 
@@ -840,6 +841,8 @@ struct mlx5_core_event_handler {
 		      void *data);
 };
 
+#define MLX5_IB_MOD "mlx5_ib"
+
 static int init_one(struct pci_dev *pdev,
 		    const struct pci_device_id *id)
 {
@@ -877,6 +880,10 @@ static int init_one(struct pci_dev *pdev,
 		dev_err(&pdev->dev, "mlx5_register_device failed %d\n", err);
 		goto out_init;
 	}
+
+	err = request_module_nowait(MLX5_IB_MOD);
+	if (err)
+		pr_info("failed request module on %s\n", MLX5_IB_MOD);
 
 	return 0;
 

halfline/rhel7

commit d33a645e01bb83ce3536805772aa49cf098a931f

[infiniband] lx5: Remove information print after attempt to load mlx5_ib module
Message-id: <20171011200214.23764-65-ddutile@redhat.com>
Patchwork-id: 191401
O-Subject: [PATCH RHEL-7.5 v2 064/233] net/mlx5: Remove information print after attempt to load mlx5_ib module
Bugzilla: 1385309 1385649 1386645 1409099 1456667 1456687
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>

commit f82eed45239988db125d1fc8c52994f3684324c6
Author: Leon Romanovsky <leonro@mellanox.com>
Date:   Thu Dec 1 10:34:57 2016 +0200

    net/mlx5: Remove information print after attempt to load mlx5_ib module

    Infiniband part of mlx5 driver can be compiled as a module
    or as a part of bzImage (compiled in). In the second case,
    the call to request_module will return an error -ENOENT.
    It will cause to a misleading print "failed request module
    on mlx5_ib".

    This patch removes this print, In order to comply with mlx4.

    Fixes: f66f049fb738 ("net/mlx5_core: Request the mlx5 IB module on driver load")
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

Signed-off-by: Donald Dutile <ddutile@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1333,9 +1333,7 @@ static int init_one(struct pci_dev *pdev,
 		goto clean_health;
 	}
 
-	err = request_module_nowait(MLX5_IB_MOD);
-	if (err)
-		pr_info("failed request module on %s\n", MLX5_IB_MOD);
+	request_module_nowait(MLX5_IB_MOD);
 
 	err = devlink_register(devlink, &pdev->dev);
 	if (err)