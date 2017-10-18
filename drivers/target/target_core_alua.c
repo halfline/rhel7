4d916e2264a594fa842a5e90e51a2cbff9262677
Maurizio Lombardi <mlombard@redhat.com>
2017-10-18 13:21:23 -0400

    [target] Convert se_node_acl->device_list to RCU hlist
    Message-id: <1508333016-17227-77-git-send-email-mlombard@redhat.com>
    Patchwork-id: 194248
    O-Subject: [RHEL7.5 e-stor PATCH 076/209] target: Convert se_node_acl->device_list[] to RCU hlist
    Bugzilla: 1366062
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Mike Christie <mchristi@redhat.com>
    
    From: Nicholas Bellinger <nab@linux-iscsi.org>
    
    This patch converts se_node_acl->device_list[] table for mappedluns
    to modern RCU hlist_head usage in order to support an arbitrary number
    of node_acl lun mappings.
    
    It converts transport_lookup_*_lun() fast-path code to use RCU read path
    primitives when looking up se_dev_entry.  It adds a new hlist_head at
    se_node_acl->lun_entry_hlist for this purpose.
    
    For transport_lookup_cmd_lun() code, it works with existing per-cpu
    se_lun->lun_ref when associating se_cmd with se_lun + se_device.
    Also, go ahead and update core_create_device_list_for_node() +
    core_free_device_list_for_node() to use ->lun_entry_hlist.
    
    It also converts se_dev_entry->pr_ref_count access to use modern
    struct kref counting, and updates core_disable_device_list_for_node()
    to kref_put() and block on se_deve->pr_comp waiting for outstanding PR
    special-case PR references to drop, then invoke kfree_rcu() to wait
    for the RCU grace period to complete before releasing memory.
    
    So now that se_node_acl->lun_entry_hlist fast path access uses RCU
    protected pointers, go ahead and convert remaining non-fast path
    RCU updater code using ->lun_entry_lock to struct mutex to allow
    callers to block while walking se_node_acl->lun_entry_hlist.
    
    Finally drop the left-over core_clear_initiator_node_from_tpg() that
    originally cleared lun_access during se_node_acl shutdown, as post
    RCU conversion it now becomes duplicated logic.
    
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Sagi Grimberg <sagig@mellanox.com>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    (cherry picked from commit 29a05deebf6c2e3010934fb78ee65cab3d329470)
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/target/target_core_alua.c
+++ drivers/target/target_core_alua.c
@@ -998,7 +998,8 @@ static void core_alua_do_transition_ua(struct t10_alua_tg_pt_gp *tg_pt_gp)
 		spin_lock_bh(&port->sep_alua_lock);
 		list_for_each_entry(se_deve, &port->sep_alua_list,
 					alua_port_list) {
-			lacl = se_deve->se_lun_acl;
+			lacl = rcu_dereference_check(se_deve->se_lun_acl,
+					     lockdep_is_held(&port->sep_alua_lock));
 			/*
 			 * se_deve->se_lun_acl pointer may be NULL for a
 			 * entry created without explicit Node+MappedLUN ACLs
