torvalds/linux

commit ead0687fe304a66a24e3d809d1070684f3abee71

HID: i2c-hid: support regulator power on/off
On some boards, we need to enable a regulator before using the HID, and
it's also nice to save power in suspend by disabling it. Support an
optional "vdd-supply" and a companion initialization delay.

[jkosina@suse.cz: rebase on top of i2c-hid branch]
Signed-off-by: Brian Norris <briannorris@chromium.org>
Signed-off-by: Caesar Wang <wxt@rock-chips.com>
Cc: Jiri Kosina <jikos@kernel.org>
Cc: linux-input@vger.kernel.org
Reviewed-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
Acked-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
Signed-off-by: Jiri Kosina <jkosina@suse.cz>

drivers/hid/i2c-hid/i2c-hid.c
@@ -982,6 +982,10 @@ static int i2c_hid_of_probe(struct i2c_client *client,
 	}
 	pdata->hid_descriptor_address = val;
 
+	ret = of_property_read_u32(dev->of_node, "init-delay-ms", &val);
+	if (!ret)
+		pdata->init_delay_ms = val;
+
 	return 0;
 }
 
@@ -1041,6 +1045,24 @@ static int i2c_hid_probe(struct i2c_client *client,
 		ihid->pdata = *platform_data;
 	}
 
+	ihid->pdata.supply = devm_regulator_get(&client->dev, "vdd");
+ 	if (IS_ERR(ihid->pdata.supply)) {
+ 		ret = PTR_ERR(ihid->pdata.supply);
+ 		if (ret != -EPROBE_DEFER)
+ 			dev_err(&client->dev, "Failed to get regulator: %d\n",
+ 				ret);
+ 		return ret;
+ 	}
+ 
+	ret = regulator_enable(ihid->pdata.supply);
+	if (ret < 0) {
+		dev_err(&client->dev, "Failed to enable regulator: %d\n",
+			ret);
+		goto err;
+	}
+	if (ihid->pdata.init_delay_ms)
+		msleep(ihid->pdata.init_delay_ms);
+
 	i2c_set_clientdata(client, ihid);
 
 	ihid->client = client;
@@ -1056,7 +1078,7 @@ static int i2c_hid_probe(struct i2c_client *client,
 	 * real computation later. */
 	ret = i2c_hid_alloc_buffers(ihid, HID_MIN_BUFFER_SIZE);
 	if (ret < 0)
-		goto err;
+		goto err_regulator;
 
 	pm_runtime_get_noresume(&client->dev);
 	pm_runtime_set_active(&client->dev);
@@ -1113,6 +1135,9 @@ err_pm:
 	pm_runtime_put_noidle(&client->dev);
 	pm_runtime_disable(&client->dev);
 
+err_regulator:
+	regulator_disable(ihid->pdata.supply);
+
 err:
 	i2c_hid_free_buffers(ihid);
 	kfree(ihid);
@@ -1136,6 +1161,8 @@ static int i2c_hid_remove(struct i2c_client *client)
 
 	if (ihid->bufsize)
 		i2c_hid_free_buffers(ihid);
+
+	regulator_disable(ihid->pdata.supply);
 
 	kfree(ihid);
 
@@ -1187,6 +1214,11 @@ static int i2c_hid_suspend(struct device *dev)
 		else
 			hid_warn(hid, "Failed to enable irq wake: %d\n",
 				wake_status);
+	} else {
+		ret = regulator_disable(ihid->pdata.supply);
+		if (ret < 0)
+			hid_warn(hid, "Failed to disable supply: %d\n",
+				 ret);
 	}
 
 	return 0;
@@ -1200,13 +1232,20 @@ static int i2c_hid_resume(struct device *dev)
 	struct hid_device *hid = ihid->hid;
 	int wake_status;
 
-	if (device_may_wakeup(&client->dev) && ihid->irq_wake_enabled) {
+	if (!device_may_wakeup(&client->dev)) {
+		ret = regulator_enable(ihid->pdata.supply);
+		if (ret < 0)
+			hid_warn(hid, "Failed to enable supply: %d\n",
+					ret);
+		if (ihid->pdata.init_delay_ms)
+			msleep(ihid->pdata.init_delay_ms);
+	} else if (ihid->irq_wake_enabled) {
 		wake_status = disable_irq_wake(client->irq);
 		if (!wake_status)
 			ihid->irq_wake_enabled = false;
 		else
 			hid_warn(hid, "Failed to disable irq wake: %d\n",
-				wake_status);
+					wake_status);
 	}
 
 	/* We'll resume to full power */

include/linux/i2c/i2c-hid.h
@@ -14,9 +14,13 @@
 
 #include <linux/types.h>
 
+struct regulator;
+
 /**
  * struct i2chid_platform_data - used by hid over i2c implementation.
  * @hid_descriptor_address: i2c register where the HID descriptor is stored.
+ * @supply: regulator for powering on the device.
+ * @init_delay_ms: delay after powering on before device is usable.
  *
  * Note that it is the responsibility of the platform driver (or the acpi 5.0
  * driver, or the flattened device tree) to setup the irq related to the gpio in
@@ -31,6 +35,8 @@
  */
 struct i2c_hid_platform_data {
 	u16 hid_descriptor_address;
+	struct regulator *supply;
+	int init_delay_ms;
 };
 
 #endif /* __LINUX_I2C_HID_H */

halfline/rhel7

commit 61fae7289c9537c899e43618df774460ab47374a

[hid] i2c-hid: fix build
Message-id: <20171006145658.18562-25-benjamin.tissoires@redhat.com>
Patchwork-id: 189743
O-Subject: [PATCH RHEL7.5 24/53] HID: i2c-hid: fix build
Bugzilla: 1452237 1489630
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1489630
          https://bugzilla.redhat.com/show_bug.cgi?id=1452237
Upstream: since v4.10
Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14201393
Test: tested by Karl Hastings on a Dell Precision 7720 and Peter Hutterer
      on a Dell XPS 13

Signed-off-by: Rafael Aquini <aquini@redhat.com>

Conflicts:
    drivers/hid/i2c-hid/i2c-hid.c -> because 3d7d248cf484 is not backported
        in RHEL

commit ba1660f1791f31a76da247caebf273b3f3d5071c
Author: Jiri Kosina <jkosina@suse.cz>
Date:   Fri Dec 9 13:37:07 2016 +0100

    HID: i2c-hid: fix build

    Add a forgotten include that I've by mistake omitted when resolving
    merge conflict in ead0687fe30 ("HID: i2c-hid: support regulator power
    on/off").

    Fixes: ead0687fe30 ("HID: i2c-hid: support regulator power on/off")
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

drivers/hid/i2c-hid/i2c-hid.c
@@ -37,6 +37,7 @@
 #include <linux/hid.h>
 #include <linux/mutex.h>
 #include <linux/acpi.h>
+#include <linux/regulator/consumer.h>
 
 #include <linux/i2c/i2c-hid.h>
 