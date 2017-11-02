facfd3ecb121103df88ee831fa203717d5ec7808
Josh Poimboeuf <jpoimboe@redhat.com>
2017-11-02 01:48:57 -0400

    [kernel] ftrace/module: Call clean up function when module init fails early
    Message-id: <8ef5d1ac685bbbbdf30e678001fff4abe0272f2b.1509586025.git.jpoimboe@redhat.com>
    Patchwork-id: 195614
    O-Subject: [RHEL7.5 PATCH v2 052/141] ftrace/module: Call clean up function when module init fails early
    Bugzilla: 1430637
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Joe Lawrence <joe.lawrence@redhat.com>
    
    commit 049fb9bd416077b3622d317a45796be4f2431df3
    Author: Steven Rostedt (Red Hat) <rostedt@goodmis.org>
    Date:   Tue Jan 5 20:32:47 2016 -0500
    
        ftrace/module: Call clean up function when module init fails early
    
        If the module init code fails after calling ftrace_module_init() and before
        calling do_init_module(), we can suffer from a memory leak. This is because
        ftrace_module_init() allocates pages to store the locations that ftrace
        hooks are placed in the module text. If do_init_module() fails, it still
        calls the MODULE_GOING notifiers which will tell ftrace to do a clean up of
        the pages it allocated for the module. But if load_module() fails before
        then, the pages allocated by ftrace_module_init() will never be freed.
    
        Call ftrace_release_mod() on the module if load_module() fails before
        getting to do_init_module().
    
        Link: http://lkml.kernel.org/r/567CEA31.1070507@intel.com
    
        Reported-by: "Qiu, PeiyangX" <peiyangx.qiu@intel.com>
        Fixes: a949ae560a511 "ftrace/module: Hardcode ftrace_module_init() call into load_module()"
        Cc: stable@vger.kernel.org # v2.6.38+
        Acked-by: Rusty Russell <rusty@rustcorp.com.au>
        Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3549,6 +3549,12 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	wake_up_all(&module_wq);
 	mutex_unlock(&module_mutex);
  free_module:
+	/*
+	 * Ftrace needs to clean up what it initialized.
+	 * This does nothing if ftrace_module_init() wasn't called,
+	 * but it must be called outside of module_mutex.
+	 */
+	ftrace_release_mod(mod);
 	/* Free lock-classes; relies on the preceding sync_rcu() */
 	lockdep_free_key_range(mod->module_core, mod->core_size);
 
