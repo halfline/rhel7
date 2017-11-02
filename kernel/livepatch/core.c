e7398d9b8821f40573e0dbb89bd6e6c7e98af178
Josh Poimboeuf <jpoimboe@redhat.com>
2017-11-02 01:50:06 -0400

    [kernel] livepatch: unpatch all klp_objects if klp_module_coming fails
    Message-id: <45fcc806adf3001e9eb97289770144e14500effc.1509586025.git.jpoimboe@redhat.com>
    Patchwork-id: 195682
    O-Subject: [RHEL7.5 PATCH v2 121/141] livepatch: unpatch all klp_objects if klp_module_coming fails
    Bugzilla: 1430637
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Joe Lawrence <joe.lawrence@redhat.com>
    
    Upstream status: linux-next
    
    commit ef8daf8eeb5b8ab6bc356656163d19f20fb827ed
    Author: Joe Lawrence <joe.lawrence@redhat.com>
    Date:   Mon Oct 2 11:56:48 2017 -0400
    
        livepatch: unpatch all klp_objects if klp_module_coming fails
    
        When an incoming module is considered for livepatching by
        klp_module_coming(), it iterates over multiple patches and multiple
        kernel objects in this order:
    
                list_for_each_entry(patch, &klp_patches, list) {
                        klp_for_each_object(patch, obj) {
    
        which means that if one of the kernel objects fails to patch,
        klp_module_coming()'s error path needs to unpatch and cleanup any kernel
        objects that were already patched by a previous patch.
    
        Reported-by: Miroslav Benes <mbenes@suse.cz>
        Suggested-by: Petr Mladek <pmladek@suse.com>
        Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>
        Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
        Reviewed-by: Petr Mladek <pmladek@suse.com>
        Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/livepatch/core.c
+++ kernel/livepatch/core.c
@@ -837,6 +837,41 @@ int klp_register_patch(struct klp_patch *patch)
 }
 EXPORT_SYMBOL_GPL(klp_register_patch);
 
+/*
+ * Remove parts of patches that touch a given kernel module. The list of
+ * patches processed might be limited. When limit is NULL, all patches
+ * will be handled.
+ */
+static void klp_cleanup_module_patches_limited(struct module *mod,
+					       struct klp_patch *limit)
+{
+	struct klp_patch *patch;
+	struct klp_object *obj;
+
+	list_for_each_entry(patch, &klp_patches, list) {
+		if (patch == limit)
+			break;
+
+		klp_for_each_object(patch, obj) {
+			if (!klp_is_module(obj) || strcmp(obj->name, mod->name))
+				continue;
+
+			/*
+			 * Only unpatch the module if the patch is enabled or
+			 * is in transition.
+			 */
+			if (patch->enabled || patch == klp_transition_patch) {
+				pr_notice("reverting patch '%s' on unloading module '%s'\n",
+					  patch->mod->name, obj->mod->name);
+				klp_unpatch_object(obj);
+			}
+
+			klp_free_object_loaded(obj);
+			break;
+		}
+	}
+}
+
 int klp_module_coming(struct module *mod)
 {
 	int ret;
@@ -907,7 +942,7 @@ err:
 	pr_warn("patch '%s' failed for module '%s', refusing to load module '%s'\n",
 		patch->mod->name, obj->mod->name, obj->mod->name);
 	mod_ext->klp_alive = false;
-	klp_free_object_loaded(obj);
+	klp_cleanup_module_patches_limited(mod, patch);
 	mutex_unlock(&klp_mutex);
 
 	return ret;
@@ -915,8 +950,6 @@ err:
 
 void klp_module_going(struct module *mod)
 {
-	struct klp_patch *patch;
-	struct klp_object *obj;
 	struct module_ext *mod_ext;
 
 	if (WARN_ON(mod->state != MODULE_STATE_GOING &&
@@ -936,25 +969,7 @@ void klp_module_going(struct module *mod)
 	 */
 	mod_ext->klp_alive = false;
 
-	list_for_each_entry(patch, &klp_patches, list) {
-		klp_for_each_object(patch, obj) {
-			if (!klp_is_module(obj) || strcmp(obj->name, mod->name))
-				continue;
-
-			/*
-			 * Only unpatch the module if the patch is enabled or
-			 * is in transition.
-			 */
-			if (patch->enabled || patch == klp_transition_patch) {
-				pr_notice("reverting patch '%s' on unloading module '%s'\n",
-					  patch->mod->name, obj->mod->name);
-				klp_unpatch_object(obj);
-			}
-
-			klp_free_object_loaded(obj);
-			break;
-		}
-	}
+	klp_cleanup_module_patches_limited(mod, NULL);
 
 	mutex_unlock(&klp_mutex);
 }
