f031193ceab4753200e74f4c8fb60591af51d24a
Josh Poimboeuf <jpoimboe@redhat.com>
2017-11-02 01:49:20 -0400

    [x86] dumpstack: Remove 64-byte gap at end of irq stack
    Message-id: <d136e44020671bca9928ec894d74c3a877bb3e60.1509586025.git.jpoimboe@redhat.com>
    Patchwork-id: 195647
    O-Subject: [RHEL7.5 PATCH v2 075/141] x86/dumpstack: Remove 64-byte gap at end of irq stack
    Bugzilla: 1430637
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Joe Lawrence <joe.lawrence@redhat.com>
    
    commit 4950d6d48a0c43cc61d0bbb76fb10e0214b79c66
    Author: Josh Poimboeuf <jpoimboe@redhat.com>
    Date:   Thu Aug 18 10:59:08 2016 -0500
    
        x86/dumpstack: Remove 64-byte gap at end of irq stack
    
        There has been a 64-byte gap at the end of the irq stack for at least 12
        years.  It predates git history, and I can't find any good reason for
        it.  Remove it.  What's the worst that could happen?
    
        Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
        Cc: Andy Lutomirski <luto@amacapital.net>
        Cc: Andy Lutomirski <luto@kernel.org>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: Brian Gerst <brgerst@gmail.com>
        Cc: Byungchul Park <byungchul.park@lge.com>
        Cc: Denys Vlasenko <dvlasenk@redhat.com>
        Cc: Frederic Weisbecker <fweisbec@gmail.com>
        Cc: H. Peter Anvin <hpa@zytor.com>
        Cc: Kees Cook <keescook@chromium.org>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Nilay Vaish <nilayvaish@gmail.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Steven Rostedt <rostedt@goodmis.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Link: http://lkml.kernel.org/r/14f9281c5475cc44af95945ea7546bff2e3836db.1471535549.git.jpoimboe@redhat.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1272,7 +1272,7 @@ EXPORT_PER_CPU_SYMBOL(kernel_stack);
 EXPORT_PER_CPU_SYMBOL(__kernel_stack_70__);
 
 DEFINE_PER_CPU(char *, irq_stack_ptr) =
-	init_per_cpu_var(irq_stack_union.irq_stack) + IRQ_STACK_SIZE - 64;
+	init_per_cpu_var(irq_stack_union.irq_stack) + IRQ_STACK_SIZE;
 
 DEFINE_PER_CPU(unsigned int, irq_count) = -1;
 
