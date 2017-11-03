halfline/rhel7

commit b71ac7150d2dac323fb094f535a59e9dd972b2c3

[x86] cpufeature: Add AMD AVIC bit
Message-id: <1483943924-26394-2-git-send-email-ssuthiku@redhat.com>
Patchwork-id: 162184
O-Subject: [RHEL7.4 PATCH BZ 1133711 01/18] x86/cpufeature: Add AMD AVIC bit
Bugzilla: 1133711
RH-Acked-by: David Arcari <darcari@redhat.com>
RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1133711

commit a1ff5726081858a9ad98934eff7af6616c576875
Author: Borislav Petkov <bp@suse.de>
Date:   Sat Jan 16 10:58:12 2016 +0100

    x86/cpufeature: Add AMD AVIC bit

    CPUID Fn8000_000A_EDX[13] denotes support for AMD's Virtual
    Interrupt controller, i.e., APIC virtualization.

    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: David Kaplan <david.kaplan@amd.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Link: http://lkml.kernel.org/r/1452938292-12327-1-git-send-email-bp@alien8.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

Cc: David Arcari <darcari@redhat.com>
Cc: Jeff Moyer <jmoyer@redhat.com>
Cc: Yasuaki Ishimatsu <yishimat@redhat.com>
Cc: Kim Naru <knaru@redhat.com>
Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

arch/x86/kernel/cpu/scattered.c
@@ -57,7 +57,8 @@ void init_scattered_cpuid_features(struct cpuinfo_x86 *c)
 		{ X86_FEATURE_DECODEASSISTS,	CR_EDX, 7, 0x8000000a, 0 },
 		{ X86_FEATURE_PAUSEFILTER,	CR_EDX,10, 0x8000000a, 0 },
 		{ X86_FEATURE_PFTHRESHOLD,	CR_EDX,12, 0x8000000a, 0 },
-		{ 0, 0, 0, 0, 0 }
+		{ X86_FEATURE_AVIC,       	CR_EDX,13, 0x8000000a, 0 },
+		{ 0, 0, 0, 0 }
 	};
 
 	for (cb = cpuid_bits; cb->feature; cb++) {

arch/x86/include/asm/cpufeature.h
@@ -214,6 +214,7 @@
 #define X86_FEATURE_DECODEASSISTS (8*32+12) /* AMD Decode Assists support */
 #define X86_FEATURE_PAUSEFILTER (8*32+13) /* AMD filtered pause intercept */
 #define X86_FEATURE_PFTHRESHOLD (8*32+14) /* AMD pause filter threshold */
+#define X86_FEATURE_AVIC	(8*32+15) /* AMD Virtual Interrupt Controller */
 
 
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (ebx), word 9 */

commit 26f7918c17405a020c1d88e2e259e425a2287bc8

[x86] x86: cpufeature: Fix AMD AVIC bit
Message-id: <fb4148c2c2a3791f6eb36fbd73a8e6a6a9c788fa.1509637251.git.aquini@redhat.com>
Patchwork-id: 195755
O-Subject: [RHEL7 RHBZ#1048983 PATCH v2 1/3] x86: cpufeature: Fix AMD AVIC bit
Bugzilla: 1048983
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>

RHEL-only patch, not upstream

RHEL7 commit b71ac71 ("[x86] cpufeature: Add AMD AVIC bit") backported hunks
ended up way different from its upstream couterpart which caused
X86_FEATURE_AVIC overlapping with X86_FEATURE_VMMCALL.
This patch reverts that backport mistake and fix X86_FEATURE_AVIC
to reflect its insertion just as it was introduced by upstream commit
a1ff57 ("x86/cpufeature: Add AMD AVIC bit").

Fixes: b71ac71 ("[x86] cpufeature: Add AMD AVIC bit")

Signed-off-by: Rafael Aquini <aquini@redhat.com>

arch/x86/include/asm/cpufeatures.h
@@ -209,9 +209,6 @@
 #define X86_FEATURE_EPT         (8*32+ 3) /* Intel Extended Page Table */
 #define X86_FEATURE_VPID        (8*32+ 4) /* Intel Virtual Processor ID */
 
-#define X86_FEATURE_AVIC	(8*32+15) /* AMD Virtual Interrupt Controller */
-
-
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (ebx), word 9 */
 #define X86_FEATURE_FSGSBASE	(9*32+ 0) /* {RD/WR}{FS/GS}BASE instructions*/
 #define X86_FEATURE_TSC_ADJUST	(9*32+ 1) /* TSC adjustment MSR 0x3b */
@@ -283,6 +280,7 @@
 #define X86_FEATURE_DECODEASSISTS (15*32+ 7) /* Decode Assists support */
 #define X86_FEATURE_PAUSEFILTER (15*32+10) /* filtered pause intercept */
 #define X86_FEATURE_PFTHRESHOLD (15*32+12) /* pause filter threshold */
+#define X86_FEATURE_AVIC	(15*32+13) /* Virtual Interrupt Controller */
 
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (ecx), word 16 */
 #define X86_FEATURE_AVX512VBMI	(16*32+ 1) /* AVX512 Vector Bit Manipulation instructions*/

arch/x86/kernel/cpu/scattered.c
@@ -30,7 +30,6 @@ static const struct cpuid_bit cpuid_bits[] = {
 	{ X86_FEATURE_HW_PSTATE,	CPUID_EDX, 7, 0x80000007, 0 },
 	{ X86_FEATURE_CPB,		CPUID_EDX, 9, 0x80000007, 0 },
 	{ X86_FEATURE_PROC_FEEDBACK,	CPUID_EDX,11, 0x80000007, 0 },
-	{ X86_FEATURE_AVIC,       	CPUID_EDX,13, 0x8000000a, 0 },
 	{ 0, 0, 0, 0 }
 };
 