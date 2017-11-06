337c239ada2ed28beadbb5624babf2e36f4c1d27
Robert S Peterson <rpeterso@redhat.com>
2017-11-06 16:26:10 -0500

    [fs] gfs2: aggressively issue revokes in gfs2_log_flush
    Message-id: <20171106162615.5597-2-rpeterso@redhat.com>
    Patchwork-id: 195926
    O-Subject: [RHEL7 fs PATCH 1/6] GFS2: aggressively issue revokes in gfs2_log_flush
    Bugzilla: 1492678
    RH-Acked-by: Abhijith Das <adas@redhat.com>
    RH-Acked-by: Andreas Grunbacher <agruenba@redhat.com>
    
    From: Benjamin Marzinski <bmarzins@redhat.com>
    
    Bugzilla: 1492678
    Upstream Status: merged in v3.11
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14460553
    Tested: xfstest generic/269
    
    commit 5d054964f57ade1adb310313c3cfb8e93e804e8f
    Author: Benjamin Marzinski <bmarzins@redhat.com>
    Date:   Fri Jun 14 11:38:29 2013 -0500
    
        GFS2: aggressively issue revokes in gfs2_log_flush
    
        This patch looks at all the outstanding blocks in all the transactions
        on the log, and moves the completed ones to the ail2 list.  Then it
        issues revokes for these blocks.  This will hopefully speed things up
        in situations where there is a lot of contention for glocks, especially
        if they are acquired serially.
    
        revoke_lo_before_commit will issue at most one log block's full of these
        preemptive revokes. The amount of reserved log space that
        gfs2_log_reserve() ignores has been incremented to allow for this extra
        block.
    
        This patch also consolidates the common revoke instructions into one
        function, gfs2_add_revoke().
    
        Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
        Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/gfs2/meta_io.c
+++ fs/gfs2/meta_io.c
@@ -366,10 +366,6 @@ void gfs2_remove_from_journal(struct buffer_head *bh, struct gfs2_trans *tr, int
 	if (bd) {
 		spin_lock(&sdp->sd_ail_lock);
 		if (bd->bd_tr) {
-			bh->b_private = NULL;
-			bd->bd_blkno = bh->b_blocknr;
-			gfs2_remove_from_ail(bd);
-			bd->bd_bh = NULL;
 			gfs2_trans_add_revoke(sdp, bd);
 		} else if (was_pinned) {
 			bh->b_private = NULL;
