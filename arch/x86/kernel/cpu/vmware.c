5a890dd6edada8827fb462c7075abba1378c60c9
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-11-06 11:53:42 -0500

    [x86] x86/vmware: Skip lapic calibration on VMware
    Message-id: <20171106115343.22848-2-vkuznets@redhat.com>
    Patchwork-id: 195849
    O-Subject: [RHEL7.5 KERNEL PATCH 1/2] x86/vmware: Skip lapic calibration on VMware
    Bugzilla: 1507026 1507025
    RH-Acked-by: Mohammed Gamal <mgamal@redhat.com>
    RH-Acked-by: Cathy Avery <cavery@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    commit b91688f528fe96e09d17e6d87c1b2805eb0c445e
    Author: Renat Valiullin <rvaliullin@vmware.com>
    Date:   Tue Oct 4 13:11:48 2016 -0700
    
        x86/vmware: Skip lapic calibration on VMware
    
        In a virtualized environment the APIC timer calibration can go wrong when
        the host is overcommitted or the guest is running nested. This results
        in the APIC timers operating at an incorrect frequency.
    
        Since VMware supports a mechanism to retrieve the local APIC frequency we
        can ask the hypervisor for it and skip the APIC calibration loop.
    
        Signed-off-by: Renat Valiullin <rvaliullin@vmware.com>
        Acked-by: Alok N Kataria <akataria@vmware.com>
        Cc: virtualization@lists.linux-foundation.org
        Link: http://lkml.kernel.org/r/20161004201148.GA1421@uu64vm
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    
    Conflicts:
    	arch/x86/kernel/cpu/vmware.c (out of order 687bca8d66 backport,
                                          skipping 1b74dde7c47c1)
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- arch/x86/kernel/cpu/vmware.c
+++ arch/x86/kernel/cpu/vmware.c
@@ -26,6 +26,7 @@
 #include <asm/div64.h>
 #include <asm/x86_init.h>
 #include <asm/hypervisor.h>
+#include <asm/apic.h>
 
 #define CPUID_VMWARE_INFO_LEAF	0x40000000
 #define VMWARE_HYPERVISOR_MAGIC	0x564D5868
@@ -84,6 +85,13 @@ static void __init vmware_platform_setup(void)
 	if (ebx != UINT_MAX) {
 		x86_platform.calibrate_tsc = vmware_get_tsc_khz;
 		x86_platform.calibrate_cpu = vmware_get_tsc_khz;
+
+#ifdef CONFIG_X86_LOCAL_APIC
+		/* Skip lapic calibration since we know the bus frequency. */
+		lapic_timer_frequency = ecx / HZ;
+		pr_info("Host bus clock speed read from hypervisor : %u Hz\n",
+			ecx);
+#endif
 	} else {
 		printk(KERN_WARNING
 		       "Failed to get TSC freq from the hypervisor\n");
