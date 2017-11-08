6f78f540b4cfe0161f93f97ae8bc2ac9ecb91d93
Eric Sandeen <esandeen@redhat.com>
2017-11-07 22:13:54 -0500

    [fs] block_dev: remove DAX leftovers
    Message-id: <8f5a2a70-dd73-8ef5-3c1f-986e90911033@redhat.com>
    Patchwork-id: 196659
    O-Subject: [RHEL7 fs 01/56 V3] block_dev: remove DAX leftovers
    Bugzilla: 1496513
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    Bugzilla: 1496513
    Upstream Status: Committed upstream
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=14131636
    Tested: Locally with fstests
    
    Backport of the following upstream commit:
    
    commit 223757016837d5bc8546c5683e13fbafe6cb374d
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Wed Sep 14 11:56:13 2016 +0200
    
        block_dev: remove DAX leftovers
    
        DAX support for block devices was removed in commits 03cdad
        ("block: disable block device DAX by default") and 99a01cd
        ("block: remove BLK_DEV_DAX config option"), but we still kept a call to
        dax_do_io and some uneeded i_flags manipulations introduced in commit
        bbab37 ("block: Add support for DAX reads/writes to block devices").
    
        Remove those leftovers.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
        Acked-by: Dan Williams <dan.j.williams@intel.com>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    ---
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -187,9 +187,6 @@ blkdev_direct_IO(int rw, struct kiocb *iocb, const struct iovec *iov,
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = bdev_file_inode(file);
 
-	if (IS_DAX(inode))
-		return dax_do_io(rw, iocb, inode, iov, offset, nr_segs,
-				 blkdev_get_block, NULL, DIO_SKIP_DIO_COUNT);
 	return __blockdev_direct_IO(rw, iocb, inode, I_BDEV(inode), iov, offset,
 				    nr_segs, blkdev_get_block, NULL, NULL,
 				    DIO_SKIP_DIO_COUNT);
@@ -1289,7 +1286,6 @@ static int __blkdev_get(struct block_device *bdev, fmode_t mode, int for_part)
 		bdev->bd_disk = disk;
 		bdev->bd_queue = disk->queue;
 		bdev->bd_contains = bdev;
-		bdev->bd_inode->i_flags = 0;
 
 		if (!partno) {
 			struct backing_dev_info *bdi;
