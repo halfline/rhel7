torvalds/linux

commit 1865a9c382ede507065cf1575308b53495814c7d

USB: adutux: fix misuse of return value of copy_to_user()
copy_to_user() returns number of not copied bytes, not error code.

Signed-off-by: Kulikov Vasiliy <segooon@gmail.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

drivers/usb/misc/adutux.c
@@ -439,7 +439,7 @@ static ssize_t adu_read(struct file *file, __user char *buffer, size_t count,
 			/* drain secondary buffer */
 			int amount = bytes_to_read < data_in_secondary ? bytes_to_read : data_in_secondary;
 			i = copy_to_user(buffer, dev->read_buffer_secondary+dev->secondary_head, amount);
-			if (i < 0) {
+			if (i) {
 				retval = -EFAULT;
 				goto exit;
 			}

halfline/rhel7

commit 250dc06b88f4e1356125810023061839ca7aa7cd

[usb] misc: adutux: remove redundant error check on copy_to_user return code
Message-id: <1510099268-32320-103-git-send-email-torez@redhat.com>
Patchwork-id: 196764
O-Subject: [RHEL7.5 PATCH BZ1435752 102/168 v4] usb: misc: adutux: remove redundant error check on copy_to_user return code
Bugzilla: 1435752
RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
RH-Acked-by: Ewan Milne <emilne@redhat.com>
RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1435752
Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14475004
Testing: on local desktop and Dell Precision 5510 laptop

commit 453785c64e090d9b4169636b7276fcd6d7489d33
Author: Colin Ian King <colin.king@canonical.com>
Date:   Thu Feb 2 11:51:48 2017 +0000

    usb: misc: adutux: remove redundant error check on copy_to_user return code

    The 2nd check for a non-zero return from copy_to_user is redundant as
    it is has already been made a few lines earlier.  This check was made
    redundant because of previous fix to the copy_to_user error return
    check.

    Detected by CoverityScan, CID#114347 ("Logically Dead Code")

    Fixes: 1865a9c382ede ("USB: adutux: fix misuse of return value of copy_to_user()")

    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

Signed-off-by: Torez Smith <torez@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

drivers/usb/misc/adutux.c
@@ -389,10 +389,6 @@ static ssize_t adu_read(struct file *file, __user char *buffer, size_t count,
 			dev->secondary_head += (amount - i);
 			bytes_read += (amount - i);
 			bytes_to_read -= (amount - i);
-			if (i) {
-				retval = bytes_read ? bytes_read : -EFAULT;
-				goto exit;
-			}
 		} else {
 			/* we check the primary buffer */
 			spin_lock_irqsave (&dev->buflock, flags);