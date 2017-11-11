32c4c6bf3a0837a857e8cd993f6678f99786e777
Don Dutile <ddutile@redhat.com>
2017-11-11 04:49:07 -0500

    [netdrv] mlx5e: Use hard_mtu as part of the mlx5e_priv struct
    Message-id: <20171111045127.10299-112-ddutile@redhat.com>
    Patchwork-id: 197680
    O-Subject: [PATCH RHEL-7.5 111/251] net/mlx5e: Use hard_mtu as part of the mlx5e_priv struct
    Bugzilla: 1499363 1385325
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    
    commit c139dbfddd2c7848550ed06345060aa87701e818
    Author: Erez Shitrit <erezsh@mellanox.com>
    Date:   Thu May 18 17:03:21 2017 +0300
    
        net/mlx5e: Use hard_mtu as part of the mlx5e_priv struct
    
        The mtu extra space that kept for the HW is specific for each link type,
        and it is different in mlx5e and mlx5i modules.
        Now it is kept in the priv structures, set by the mlx5e/mlx5i driver
        accordingly.
    
        Signed-off-by: Erez Shitrit <erezsh@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    RHEL-7 NOTE:
    Previous backports had put HW2SW_MTU & SW2HW_MTU macros in en_main.c instead
    of en.h.  With this upstream commit, clean up the rhel-7-ism's of these macros
    to put them in the same source files and contents.
    Dropped en_rx.c patches -- xdp functions not in rhel-7.
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1385325
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -382,9 +382,6 @@ static void mlx5e_disable_async_events(struct mlx5e_priv *priv)
 	synchronize_irq(mlx5_get_msix_vec(priv->mdev, MLX5_EQ_VEC_ASYNC));
 }
 
-#define MLX5E_HW2SW_MTU(hwmtu) (hwmtu - (ETH_HLEN + VLAN_HLEN + ETH_FCS_LEN))
-#define MLX5E_SW2HW_MTU(swmtu) (swmtu + (ETH_HLEN + VLAN_HLEN + ETH_FCS_LEN))
-
 static inline int mlx5e_get_wqe_mtt_sz(void)
 {
 	/* UMR copies MTTs in units of MLX5_UMR_MTT_ALIGNMENT bytes.
@@ -616,7 +613,7 @@ static int mlx5e_alloc_rq(struct mlx5e_channel *c,
 
 		rq->buff.wqe_sz = params->lro_en  ?
 				params->lro_wqe_sz :
-				MLX5E_SW2HW_MTU(c->netdev->mtu);
+				MLX5E_SW2HW_MTU(c->priv, c->netdev->mtu);
 		byte_count = rq->buff.wqe_sz;
 
 		/* calc the required page order */
@@ -2287,7 +2284,7 @@ free_in:
 static int mlx5e_set_mtu(struct mlx5e_priv *priv, u16 mtu)
 {
 	struct mlx5_core_dev *mdev = priv->mdev;
-	u16 hw_mtu = MLX5E_SW2HW_MTU(mtu);
+	u16 hw_mtu = MLX5E_SW2HW_MTU(priv, mtu);
 	int err;
 
 	err = mlx5_set_port_mtu(mdev, hw_mtu, 1);
@@ -2309,7 +2306,7 @@ static void mlx5e_query_mtu(struct mlx5e_priv *priv, u16 *mtu)
 	if (err || !hw_mtu) /* fallback to port oper mtu */
 		mlx5_query_port_oper_mtu(mdev, &hw_mtu, 1);
 
-	*mtu = MLX5E_HW2SW_MTU(hw_mtu);
+	*mtu = MLX5E_HW2SW_MTU(priv, hw_mtu);
 }
 
 static int mlx5e_set_dev_port_mtu(struct mlx5e_priv *priv)
@@ -3596,6 +3593,7 @@ void mlx5e_build_nic_params(struct mlx5_core_dev *mdev,
 	mlx5e_set_rq_params(mdev, params);
 
 	/* HW LRO */
+
 	/* TODO: && MLX5_CAP_ETH(mdev, lro_cap) */
 	if (params->rq_wq_type == MLX5_WQ_TYPE_LINKED_LIST_STRIDING_RQ)
 		params->lro_en = hw_lro_heuristic(link_speed, pci_bw);
@@ -3636,6 +3634,7 @@ static void mlx5e_build_nic_netdev_priv(struct mlx5_core_dev *mdev,
 	priv->netdev      = netdev;
 	priv->profile     = profile;
 	priv->ppriv       = ppriv;
+	priv->hard_mtu = MLX5E_ETH_HARD_MTU;
 
 	mlx5e_build_nic_params(mdev, &priv->channels.params, profile->max_nch(mdev));
 
@@ -3879,7 +3878,7 @@ static void mlx5e_nic_enable(struct mlx5e_priv *priv)
 	/* MTU range: 68 - hw-specific max */
 	netdev->extended->min_mtu = ETH_MIN_MTU;
 	mlx5_query_port_max_mtu(priv->mdev, &max_mtu, 1);
-	netdev->extended->max_mtu = MLX5E_HW2SW_MTU(max_mtu);
+	netdev->extended->max_mtu = MLX5E_HW2SW_MTU(priv, max_mtu);
 	mlx5e_set_dev_port_mtu(priv);
 
 	mlx5_lag_add(mdev, netdev);
