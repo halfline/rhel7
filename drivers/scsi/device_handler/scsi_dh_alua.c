19789074ad0c87c6e6c80444e79e4cb03e0163d9
Mike Snitzer <snitzer@redhat.com>
2017-11-13 18:43:25 -0500

    [scsi] scsi_dh_alua: Fix RCU annotations
    Message-id: <20171113184325.GD32510@redhat.com>
    Patchwork-id: 197851
    O-Subject: [RHEL7.5 PATCH v3 47/58] scsi: scsi_dh_alua: Fix RCU annotations
    Bugzilla: 1499107
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    
    BZ: 1499107
    
    commit d29425b065cdab5c5e65a48108adde5ea72d4656
    Author: Bart Van Assche <bart.vanassche@sandisk.com>
    Date:   Thu Dec 1 11:30:50 2016 -0800
    
        scsi: scsi_dh_alua: Fix RCU annotations
    
        This patch avoids that sparse complains about RCU pointer dereferences.
    
        Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
        Cc: Hannes Reinecke <hare@suse.de>
        Cc: tang.junhui <tang.junhui@zte.com.cn>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/scsi/device_handler/scsi_dh_alua.c
+++ drivers/scsi/device_handler/scsi_dh_alua.c
@@ -101,7 +101,7 @@ struct alua_port_group {
 
 struct alua_dh_data {
 	struct scsi_dh_data	dh_data;
-	struct alua_port_group	*pg;
+	struct alua_port_group __rcu *pg;
 	int			group_id;
 	spinlock_t		pg_lock;
 	struct scsi_device	*sdev;
@@ -377,14 +377,16 @@ static int alua_check_vpd(struct scsi_device *sdev, struct alua_dh_data *h,
 
 	/* Check for existing port group references */
 	spin_lock(&h->pg_lock);
-	old_pg = h->pg;
+	old_pg = rcu_dereference_protected(h->pg, lockdep_is_held(&h->pg_lock));
 	if (old_pg != pg) {
 		/* port group has changed. Update to new port group */
 		rcu_assign_pointer(h->pg, pg);
 	}
 	if (sdev->synchronous_alua)
 		pg->flags |= ALUA_SYNC_STPG;
-	alua_rtpg_queue(h->pg, sdev, NULL, true);
+	alua_rtpg_queue(rcu_dereference_protected(h->pg,
+						  lockdep_is_held(&h->pg_lock)),
+			sdev, NULL, true);
 	spin_unlock(&h->pg_lock);
 
 	if (old_pg)
@@ -914,7 +916,7 @@ static int alua_initialize(struct scsi_device *sdev, struct alua_dh_data *h)
 static int alua_set_params(struct scsi_device *sdev, const char *params)
 {
 	struct alua_dh_data *h = get_alua_data(sdev);
-	struct alua_port_group __rcu *pg = NULL;
+	struct alua_port_group *pg = NULL;
 	unsigned int optimize = 0, argc;
 	const char *p = params;
 	int result = SCSI_DH_OK;
@@ -961,7 +963,7 @@ static int alua_activate(struct scsi_device *sdev,
 	struct alua_dh_data *h = get_alua_data(sdev);
 	int err = SCSI_DH_OK;
 	struct alua_queue_data *qdata;
-	struct alua_port_group __rcu *pg;
+	struct alua_port_group *pg;
 
 	qdata = kzalloc(sizeof(*qdata), GFP_KERNEL);
 	if (!qdata) {
@@ -1025,7 +1027,7 @@ static void alua_check(struct scsi_device *sdev, bool force)
 static int alua_prep_fn(struct scsi_device *sdev, struct request *req)
 {
 	struct alua_dh_data *h = get_alua_data(sdev);
-	struct alua_port_group __rcu *pg;
+	struct alua_port_group *pg;
 	int state = TPGS_STATE_OPTIMIZED;
 	int ret = BLKPREP_OK;
 
@@ -1099,7 +1101,7 @@ static void alua_bus_detach(struct scsi_device *sdev)
 	struct alua_port_group *pg;
 
 	spin_lock(&h->pg_lock);
-	pg = h->pg;
+	pg = rcu_dereference_protected(h->pg, lockdep_is_held(&h->pg_lock));
 	rcu_assign_pointer(h->pg, NULL);
 	h->sdev = NULL;
 	spin_unlock(&h->pg_lock);
